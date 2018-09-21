869db2180c504609b3a32194361470a279ca985b
Kira Evans <contact@kne42.me>
2018-09-20 21:24:57 -0700

    add setup information

--- napari_gui/__init__.py
+++ napari_gui/__init__.py
@@ -1,2 +1,6 @@
 from .elements import ImageWindow, ImageViewerWidget, ImageContainer
 from .util.misc import imshow
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
