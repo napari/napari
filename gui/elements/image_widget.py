45f32674c6d2d381d34cf0560527dc916ba51469
Kira Evans <contact@kne42.me>
2018-09-22 10:40:18 -0700

    refactor viewer update mechanisms...

--- gui/elements/image_widget.py
+++ gui/elements/image_widget.py
@@ -16,7 +16,7 @@ class ImageViewerWidget(QWidget):
     Parameters
     ----------
     parent : PyQt5.QWidget, optional
-        Parent widget.
+        Parent window.
     """
     def __init__(self, parent=None):
         super().__init__(parent=parent)
@@ -47,6 +47,17 @@ class ImageViewerWidget(QWidget):
         self.view.camera.flip = (0, 1, 0)
         self.view.camera.set_range()
         # view.camera.zoom(0.1, (250, 200))
+
+        self._max_dims = 0
+        self._max_shape = tuple()
+
+        # update flags
+        self._child_image_changed = False
+        self._need_redraw = False
+        self._need_slider_update = False
+
+        self._recalc_max_dims = False
+        self._recalc_max_shape = False
 
     def _axis_to_row(self, axis):
         dims = len(self.point)
@@ -82,7 +93,8 @@ class ImageViewerWidget(QWidget):
         container = ImageContainer(image, meta, self)
         self.containers.append(container)
 
-        self.update_sliders()
+        self._child_image_changed = True
+        self.update()
 
         return container
 
@@ -109,30 +121,7 @@ class ImageViewerWidget(QWidget):
 
         return self.add_image(image, meta)
 
-    def update_sliders(self):
-        """Updates the sliders according to the contained images.
-        """
-        max_shape = self.max_shape
-        max_dims = len(max_shape)
-
-        curr_dims = len(self.point)
-
-        if curr_dims > max_dims:
-            self.point = self.point[:max_dims]
-            dims = curr_dims
-        else:
-            dims = max_dims
-            self.point.extend([0] * (max_dims - curr_dims))
-
-        for dim in range(2, dims):  # do not create sliders for y/x-axes
-            try:
-                dim_len = max_shape[dim]
-            except IndexError:
-                dim_len = 0
-
-            self.update_slider(dim, dim_len)
-
-    def update_slider(self, axis, max_axis_length):
+    def _update_slider(self, axis, max_axis_length):
         """Updates a slider for the given axis or creates
         it if it does not already exist.
 
@@ -175,7 +164,8 @@ class ImageViewerWidget(QWidget):
 
             def value_changed():
                 self.point[axis] = slider.value()
-                self.update_images()
+                self._need_redraw = True
+                self.update()
 
             slider.valueChanged.connect(value_changed)
 
@@ -187,15 +177,37 @@ class ImageViewerWidget(QWidget):
         slider.setMaximum(max_axis_length - 1)
         return slider
 
-    def update_images(self):
+    def _update_sliders(self):
+        """Updates the sliders according to the contained images.
+        """
+        max_dims = self.max_dims
+        max_shape = self.max_shape
+
+        curr_dims = len(self.point)
+
+        if curr_dims > max_dims:
+            self.point = self.point[:max_dims]
+            dims = curr_dims
+        else:
+            dims = max_dims
+            self.point.extend([0] * (max_dims - curr_dims))
+
+        for dim in range(2, dims):  # do not create sliders for y/x-axes
+            try:
+                dim_len = max_shape[dim]
+            except IndexError:
+                dim_len = 0
+
+            self._update_slider(dim, dim_len)
+
+    def _update_images(self):
         """Updates the contained images.
         """
         for container in self.containers:
             container.set_view_slice(self.point)
 
-    @property
-    def max_dims(self):
-        """int: Maximum tunable dimensions for contained images.
+    def _calc_max_dims(self):
+        """Calculates the number of maximum dimensions in the contained images.
         """
         max_dims = 0
 
@@ -204,13 +216,47 @@ class ImageViewerWidget(QWidget):
             if dims > max_dims:
                 max_dims = dims
 
-        return max_dims
+        self._max_dims = max_dims
+
+    def _calc_max_shape(self):
+        """Calculates the maximum shape of the contained images.
+        """
+        shapes = (container.image.shape for container in self.containers)
+        self._max_shape = _compute_max_shape(shapes, self.max_dims)
+
+    def update(self):
+        """Updates the viewer.
+        """
+        if self._child_image_changed:
+            self._child_image_changed = False
+            self._recalc_max_dims = True
+            self._recalc_max_shape = True
+            self._need_slider_update = True
+
+        if self._need_redraw:
+            self._need_redraw = False
+            self._update_images()
+
+        if self._recalc_max_dims:
+            self._recalc_max_dims = False
+            self._calc_max_dims()
+
+        if self._recalc_max_shape:
+            self._recalc_max_shape = False
+            self._calc_max_shape()
+
+        if self._need_slider_update:
+            self._need_slider_update = False
+            self._update_sliders()
+
+    @property
+    def max_dims(self):
+        """int: Maximum tunable dimensions for contained images.
+        """
+        return self._max_dims
 
     @property
     def max_shape(self):
         """tuple: Maximum shape for contained images.
         """
-        shapes = (container.image.shape for container in self.containers)
-        max_shape = _compute_max_shape(shapes, self.max_dims)
-
-        return max_shape
+        return self._max_shape
