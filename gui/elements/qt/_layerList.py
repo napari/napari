1f05214f5d3ab3ad33efe9b00a456e7b272865f8
sofroniewn <sofroniewn@gmail.com>
2018-11-06 12:40:33 -0800

    redo layout add files

---
+++ gui/elements/qt/_layerList.py
@@ -0,0 +1,15 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QScrollArea
+
+
+class QtLayerList(QScrollArea):
+    def __init__(self):
+        super().__init__()
+
+        self.setWidgetResizable(True)
+        self.setFixedWidth(300)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        scrollWidget = QWidget()
+        self.setWidget(scrollWidget)
+        self.layersLayout = QVBoxLayout(scrollWidget)
+        self.layersLayout.addStretch(1)
