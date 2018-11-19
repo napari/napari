f23e281d55b0a3f593c9064783b2926fe170b25a
Nicholas Sofroniew <sofroniewn@gmail.com>
2018-11-19 12:35:41 -0800

    Drag drop layers to rearrange (#64)
    This PR adds the ability to rearrange layers through dragging and dropping them.

---
+++ gui/elements/qt/_layerDivider.py
@@ -0,0 +1,16 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QFrame
+
+class QtDivider(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.unselectedStlyeSheet = "QFrame {border: 3px solid rgb(236,236,236); background-color:rgb(236,236,236); border-radius: 3px;}"
+        self.selectedStlyeSheet = "QFrame {border: 3px solid rgb(71,143,205); background-color:rgb(71,143,205); border-radius: 3px;}"
+        self.setSelected(False)
+        self.setFixedHeight(4)
+
+    def setSelected(self, bool):
+        if bool:
+            self.setStyleSheet(self.selectedStlyeSheet)
+        else:
+            self.setStyleSheet(self.unselectedStlyeSheet)
