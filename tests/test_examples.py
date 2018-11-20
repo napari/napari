89710f08e97ae7d39d7c44870545091f5e7a288d
Kira Evans <contact@kne42.me>
2018-11-20 12:33:08 -0800

    test jupytext round-trip conversion

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -4,6 +4,7 @@ import os.path as osp
 import pytest
 
 import jupytext
+from jupytext import compare
 from nbconvert.preprocessors import ExecutePreprocessor
 
 
@@ -18,17 +19,33 @@ def jupytext_execute(path, format_name=None, freeze_metadata=False):
                          {'metadata': {'path': osp.dirname(path)}})
 
 
+def jupytext_round_trip(path, format_name=None, freeze_metadata=False,
+                        update=False, allow_expected_differences=True,
+                        stop_on_first_error=True):
+    notebook = jupytext.readf(path, format_name=format_name,
+                              freeze_metadata=freeze_metadata)
+    return compare.test_round_trip_conversion(notebook,
+                                              osp.splitext(path)[1],
+                                              format_name, update,
+                                              allow_expected_differences,
+                                              stop_on_first_error)
+
+
 examples_folder = osp.join(osp.dirname(osp.dirname(__file__)), 'examples')
 excludes = ['__init__.py', 'README.md', 'demo.py']
-filenames = []
+paths = []
 for filename in os.listdir(examples_folder):
     if filename not in excludes:
         for ext in jupytext.NOTEBOOK_EXTENSIONS:
             if filename.endswith(ext):
-                filenames.append(filename)
+                paths.append(osp.join(examples_folder, filename))
 
 
-@pytest.mark.parametrize('filename', filenames)
-def test_execute(filename):
-    path = osp.join(examples_folder, filename)
+@pytest.mark.parametrize('path', paths)
+def test_round_trip(path):
+    jupytext_round_trip(path)
+
+
+@pytest.mark.parametrize('path', paths)
+def test_execute(path):
     jupytext_execute(path)
