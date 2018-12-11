a86c605dd366cc96a559f996eca733a62fbd4f21
sofroniewn <sofroniewn@gmail.com>
2018-12-11 14:27:41 -0800

    move doc strings

--- gui/elements/_viewer.py
+++ gui/elements/_viewer.py
@@ -2,6 +2,21 @@ from .qt import QtViewer
 
 
 class Viewer:
+    """Viewer containing the rendered scene, layers, and controlling elements
+    including dimension sliders.
+
+    Parameters
+    ----------
+    parent : Window
+        Parent window.
+
+    Attributes
+    ----------
+    layers : LayerList
+        List of contained layers.
+    window : Window
+        Parent window
+    """
     def __init__(self):
         from ._layer_list import LayerList
         from ._controls import Controls
