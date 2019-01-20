164fc8a64ffcb4fbd6dff496ecfd11a8a027687a
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-01-20 15:20:31 +1100

    Fix error in colormaps file reading

--- gui/layers/colormaps.py
+++ gui/layers/colormaps.py
@@ -4,4 +4,4 @@ import os
 _matplotlib_list_file = os.path.join(os.path.dirname(__file__),
                                      'matplotlib_cmaps.txt')
 with open(_matplotlib_list_file) as fin:
-    matplotlib_colormaps = [line.rstrip() for line in _matplotlib_list_file]
+    matplotlib_colormaps = [line.rstrip() for line in fin]
