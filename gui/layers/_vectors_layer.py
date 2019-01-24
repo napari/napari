e9ff4bc6dda5d6ff70e81702d74c2227da829406
Bryant <bchhun@gmail.com>
2019-01-24 15:27:32 -0800

    final version of vectors layer

--- gui/layers/_vectors_layer.py
+++ gui/layers/_vectors_layer.py
@@ -1,30 +1,29 @@
 from typing import Union
-from collections import Iterable
 
 import numpy as np
 
 from ._base_layer import Layer
 from ._register import add_to_viewer
 from .._vispy.scene.visuals import Line as LinesNode
-# from vispy.visuals import LineVisual
 from vispy.color import get_color_names
 
 from .qt import QtVectorsLayer
+
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
 
 @add_to_viewer
 class Vectors(Layer):
     """Line layer.
 
-    See vispy's marker visual docs for more details:
-    http://api.vispy.org/en/latest/visuals.html#vispy.visuals.MarkersVisual
-
     """
+    # avg_changed = pyqtSignal(str)
 
     def __init__(
-        self, coords,
+        self, vectors,
             width=1,
-            color='orange',
-            connect='segments',
+            color='red',
+            connector='segments',
+            averaging='1x1',
             method='agg',
             antialias=True):
 
@@ -32,17 +31,21 @@ class Vectors(Layer):
         super().__init__(visual)
 
         # Save the line vertex coordinates
-        self._coords = coords
+        self._vectors = vectors
 
         # Save the line style params
         self._width = width
         self._color = color
-        self._connect = connect
+        self._connector = connector
         self._method = method
         self._antialias = antialias
 
-        self._connector_types = ['strip', 'segments']
+        self._connector_types = ['segments']
         self._colors = get_color_names()
+        
+        self._avg_dims = ['1x1','3x3','5x5','7x7','9x9','11x11']
+        self._averaging = averaging
+        self._observers = []
 
         # update flags
         self._need_display_update = False
@@ -53,16 +56,16 @@ class Vectors(Layer):
         self._selected_markers = None
 
     @property
-    def coords(self) -> np.ndarray:
+    def vectors(self) -> np.ndarray:
         """
 
         :return: coordinates of line vertices
         """
-        return self._coords
+        return self._vectors
 
-    @coords.setter
-    def coords(self, coords: np.ndarray):
-        self._coords = coords
+    @vectors.setter
+    def vectors(self, vectors: np.ndarray):
+        self._vectors = vectors
 
         self.viewer._child_layer_changed = True
         self._refresh()
@@ -73,16 +76,36 @@ class Vectors(Layer):
 
         :return: coordinates of line vertices
         """
-        return self._coords
+        return self._vectors
 
     @data.setter
     def data(self, data: np.ndarray) -> None:
-        self.coords = data
+        self.vectors = data
 
+    @property
+    def averaging(self) -> str:
+        """
+        
+        :return: string of averaging kernel size
+        """
+        return self._averaging
+    
+    @averaging.setter
+    def averaging(self, averaging: str):
+        self._averaging = averaging
+        # self.avg_changed.emit(averaging)
+        for callback in self._observers:
+            print('averaging changed, broadcasting to subscribers')
+            callback(self._averaging)
+
+    def bind_to(self, callback):
+        print('bound')
+        self._observers.append(callback)
+        
     @property
     def width(self) -> Union[int, float]:
         """
-        width of the line symbol in pixels
+        width of the line in pixels
             widths greater than 1px only guaranteed to work with "agg" method
         :return:
         """
@@ -90,28 +113,8 @@ class Vectors(Layer):
 
     @width.setter
     def width(self, width: Union[int, float]) -> None:
-
-        if np.isscalar(width):
-            self._width = width
-
-            self.refresh()
-
-        elif isinstance(width, Iterable):
-            assert len(width) == len(self._coords), \
-             'If size is a list/array, must be the same length as '\
-             'coords'
-
-            if isinstance(width, list):
-                self._width = np.asarray(width)
-
-            else:
-                self._width = width
-
-            self.refresh()
-
-        else:
-            raise TypeError('size should be float or ndarray')
-
+        self._width = width
+        self.refresh()
 
     @property
     def color(self) -> str:
@@ -125,17 +128,17 @@ class Vectors(Layer):
         self.refresh()
 
     @property
-    def connect(self):
+    def connector(self):
         """
         line connector.  One of _connector.types = "strip", "segments"
         or can receive an ndarray.
         :return: connector parameter
         """
-        return self._connect
+        return self._connector
 
-    @connect.setter
-    def connect(self, connector_type: Union[str, np.ndarray]):
-        self._connect = connector_type
+    @connector.setter
+    def connector(self, connector_type: Union[str, np.ndarray]):
+        self._connector = connector_type
         self.refresh()
 
     @property
@@ -165,10 +168,10 @@ class Vectors(Layer):
         self.refresh()
 
     def _get_shape(self):
-        if len(self.coords) == 0:
-            return np.ones(self.coords.shape,dtype=int)
+        if len(self.vectors) == 0:
+            return np.ones(self.vectors.shape,dtype=int)
         else:
-            return np.max(self.coords, axis=0) + 1
+            return np.max(self.vectors, axis=0) + 1
 
     def _update(self):
         """Update the underlying visual.
@@ -188,38 +191,37 @@ class Vectors(Layer):
         self._need_display_update = True
         self._update()
 
-
-    def _set_selected_markers(self, indices):
-        """Determines selected markers selected given indices.
-        Borrowed from Markers layer
-
-        Parameters
-        ----------
-        indices : sequence of int
-            Indices to check if marker at.
-        """
-        in_slice_markers, matches = self._slice_markers(indices)
-
-        # Display markers if there are any in this slice
-        if len(in_slice_markers) > 0:
-            distances = abs(in_slice_markers - np.broadcast_to(indices[:2], (len(in_slice_markers),2)))
-            # Get the marker sizes
-            if isinstance(self.width, (list, np.ndarray)):
-                widths = self.width[matches]
-            else:
-                widths = self.width
-            matches = np.where(matches)[0]
-            in_slice_matches = np.less_equal(distances, np.broadcast_to(widths/2, (2, len(in_slice_markers))).T)
-            in_slice_matches = np.all(in_slice_matches, axis=1)
-            indices = np.where(in_slice_matches)[0]
-            if len(indices) > 0:
-                matches = matches[indices[-1]]
-            else:
-                matches = None
-        else:
-            matches = None
-
-        self._selected_markers = matches
+    # def _set_selected_markers(self, indices):
+    #     """Determines selected markers selected given indices.
+    #     Borrowed from Markers layer
+    #
+    #     Parameters
+    #     ----------
+    #     indices : sequence of int
+    #         Indices to check if marker at.
+    #     """
+    #     in_slice_markers, matches = self._slice_markers(indices)
+    #
+    #     # Display markers if there are any in this slice
+    #     if len(in_slice_markers) > 0:
+    #         distances = abs(in_slice_markers - np.broadcast_to(indices[:2], (len(in_slice_markers),2)))
+    #         # Get the marker sizes
+    #         if isinstance(self.width, (list, np.ndarray)):
+    #             widths = self.width[matches]
+    #         else:
+    #             widths = self.width
+    #         matches = np.where(matches)[0]
+    #         in_slice_matches = np.less_equal(distances, np.broadcast_to(widths/2, (2, len(in_slice_markers))).T)
+    #         in_slice_matches = np.all(in_slice_matches, axis=1)
+    #         indices = np.where(in_slice_matches)[0]
+    #         if len(indices) > 0:
+    #             matches = matches[indices[-1]]
+    #         else:
+    #             matches = None
+    #     else:
+    #         matches = None
+    #
+    #     self._selected_markers = matches
 
     def _slice_markers(self, indices):
         """Determines the slice of markers given the indices.
@@ -229,16 +231,16 @@ class Vectors(Layer):
         indices : sequence of int or slice
             Indices to slice with.
         """
-        # Get a list of the coords for the markers in this slice
-        coords = self.coords
-        if len(coords) > 0:
+        # Get a list of the vectors for the markers in this slice
+        vectors = self.vectors
+        if len(vectors) > 0:
             matches = np.equal(
-                coords[:, 2:],
-                np.broadcast_to(indices[2:], (len(coords), len(indices) - 2)))
+                vectors[:, 2:],
+                np.broadcast_to(indices[2:], (len(vectors), len(indices) - 2)))
 
             matches = np.all(matches, axis=1)
 
-            in_slice_markers = coords[matches, :2]
+            in_slice_markers = vectors[matches, :2]
             return in_slice_markers, matches
         else:
             return [], []
@@ -272,9 +274,10 @@ class Vectors(Layer):
             sizes = 0
 
         self._node.set_data(
-            data[::-1], width=self.width,
+            data[::-1],
+            width=self.width,
             color=self.color,
-            connect=self.connect)
+            connect=self.connector)
             # method=self.method,
             # antialias=self.antialias)
         self._need_visual_update = True
