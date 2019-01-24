41dc77515ed200405b66c10f505508a419f2b4c5
Bryant <bchhun@gmail.com>
2019-01-24 15:27:32 -0800

    final version of vectors layer

--- gui/layers/qt/_vectors_layer.py
+++ gui/layers/qt/_vectors_layer.py
@@ -8,37 +8,39 @@
 # notes           :
 # python_version  :3.6
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QLabel, QComboBox, QSlider
-from collections import Iterable
-import numpy as np
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtWidgets import QLabel, QComboBox, QSpinBox
 from ._base_layer import QtLayer
 
 class QtVectorsLayer(QtLayer):
+
+    # compute_average = pyqtSignal(object)
+
     def __init__(self, layer):
         super().__init__(layer)
 
-        self.grid_layout.addWidget(QLabel('size:'), 3, 0)
+        #averaging combobox
+        averaging_combobox = QComboBox()
+        avg_dims = self.layer._avg_dims
+        for avg in avg_dims:
+            averaging_combobox.addItem(avg)
+        index = averaging_combobox.findText(self.layer.averaging, Qt.MatchFixedString)
+        if index >= 0:
+            averaging_combobox.setCurrentIndex(index)
+            averaging_combobox.activated[str].connect(lambda text=averaging_combobox: self.changeAvgType(text))
 
-        # set layer's width slider attributes and connect signals
-        sld = QSlider(Qt.Horizontal, self)
-        sld.setFocusPolicy(Qt.NoFocus)
-        #sld.setInvertedAppearance(True)
-        sld.setFixedWidth(75)
-        sld.setMinimum(0)
-        sld.setMaximum(100)
-        sld.setSingleStep(1)
+        self.grid_layout.addWidget(QLabel('averaging:'), 3, 0)
+        self.grid_layout.addWidget(averaging_combobox, 3, 1)
 
+        # line width in pixels
+        width_field = QSpinBox()
         value = self.layer.width
-        if isinstance(value, Iterable):
-            if isinstance(value, list):
-                value = np.asarray(value)
-            value = value.mean()
-        sld.setValue(int(value))
-        sld.valueChanged[int].connect(lambda value=sld: self.changeWidth(value))
-        self.grid_layout.addWidget(sld, 3, 1)
+        width_field.setValue(value)
+        width_field.valueChanged.connect(self.changeWidth)
+        self.grid_layout.addWidget(QLabel('size:'), 4, 0)
+        self.grid_layout.addWidget(width_field, 4, 1)
 
-        # marker face color adjustment and widget
+        # vector color adjustment and widget
         face_comboBox = QComboBox()
         colors = self.layer._colors
         for c in colors:
@@ -47,33 +49,32 @@ class QtVectorsLayer(QtLayer):
         if index >= 0:
            face_comboBox.setCurrentIndex(index)
         face_comboBox.activated[str].connect(lambda text=face_comboBox: self.changeFaceColor(text))
-        self.grid_layout.addWidget(QLabel('face_color:'), 4, 0)
-        self.grid_layout.addWidget(face_comboBox, 4, 1)
+        self.grid_layout.addWidget(QLabel('face_color:'), 5, 0)
+        self.grid_layout.addWidget(face_comboBox, 5, 1)
 
-        connector_comboBox = QComboBox()
-        connector_type = self.layer._connector_types
-        for c in connector_type:
-            connector_comboBox.addItem(c)
-        index = connector_comboBox.findText(self.layer.connect, Qt.MatchFixedString)
-        if index >= 0:
-            connector_comboBox.setCurrentIndex(index)
-        connector_comboBox.activated[str].connect(lambda text=connector_comboBox: self.changeConnectorType(text))
-        self.grid_layout.addWidget(QLabel('Connector:'), 5, 0)
-        self.grid_layout.addWidget(connector_comboBox, 5, 1)
+        # line connector type.  Only two built in: Segments or Connected
+        # connector_comboBox = QComboBox()
+        # connector_type = self.layer._connector_types
+        # for c in connector_type:
+        #     connector_comboBox.addItem(c)
+        # index = connector_comboBox.findText(self.layer.connector, Qt.MatchFixedString)
+        # if index >= 0:
+        #     connector_comboBox.setCurrentIndex(index)
+        # connector_comboBox.activated[str].connect(lambda text=connector_comboBox: self.changeConnectorType(text))
+        # self.grid_layout.addWidget(QLabel('Connector:'), 6, 0)
+        # self.grid_layout.addWidget(connector_comboBox, 6, 1)
 
         self.setExpanded(False)
 
     def changeFaceColor(self, text):
-        self.layer.face_color = text
+        self.layer.color = text
 
     def changeConnectorType(self, text):
-        self.layer.connect = text
+        self.layer.connector = text
 
-    # def changeEdgeColor(self, text):
-    #     self.layer.edge_color = text
-    #
-    # def changeSymbol(self, text):
-    #     self.layer.symbol = text
-    #
+    def changeAvgType(self, text):
+        self.layer.averaging = text
+        # self.compute_average.emit(text)
+
     def changeWidth(self, value):
         self.layer.width = value
