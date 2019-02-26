aae0c878fd7a8ceb188155f7a4b8d04a25094a0c
sofroniewn <sofroniewn@gmail.com>
2019-02-26 08:12:57 -0800

    make catch more specific

--- gui/components/__init__.py
+++ gui/components/__init__.py
@@ -12,8 +12,11 @@ Viewer
 """
 import warnings
 
+vispy_warning = "VisPy is not yet compatible with matplotlib 2.2+"
+
 with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=UserWarning)
+    warnings.filterwarnings("ignore", category=UserWarning,
+                            message=vispy_warning)
     from ._window import Window, QtApplication
 from ._viewer import Viewer
 from ._layers_list import LayersList
