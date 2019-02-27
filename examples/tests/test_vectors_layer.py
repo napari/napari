d1bae6dad33cb3c0254ea1a91c8b99ad5d43c70f
Bryant <bchhun@gmail.com>
2019-02-27 15:29:50 -0800

    vector layer tests

--- examples/tests/test_vectors_layer.py
+++ examples/tests/test_vectors_layer.py
@@ -13,89 +13,157 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 from napari_gui import Viewer, Window
+from napari_gui.layers._vectors_layer.model import InvalidDataFormatError
 
-from skimage import data
 import numpy as np
 
 
 class TestVectorsLayer(unittest.TestCase):
-    
-    def setup(self):
-        # create UI elements
-        self.application = QApplication(sys.argv)
+    """
+    these tests are performed on the same Napari viewer instance.
+    """
 
-        # create Viewer, Windows
-        self.view = Viewer()
-        self.win = Window(Viewer(), show=False)
-        self.viewer = self.win.viewer
-        print('creating test UI vectors')
-        return None
+    application = QApplication(sys.argv)
+    view = Viewer()
+    win = Window(Viewer(), show=False)
+    viewer = win.viewer
 
-    #test parameters, bounds and exception handling on parameters
-    def test_width(self):
-        self.setup()
-        layer = self.viewer.add_image(data.astronaut(), {})
+    def test_vectorData_datatype0(self):
+        """
+        test data of image-like shape
+        :return:
+        """
 
-
-        # test UI entry works
-        # test programmatic entry works
-        # test ipython entry works
-        # test invalid value type (int vs float etc..) and handling
-        return None
-    
-    def test_color(self):
-        #test that incorrect colors are handled
-        return None
-    
-    def test_vectorData_2d(self):
-        self.setup()
-        # pos1 = np.zeros(shape=(10,10,2), dtype=np.float32)
-        pos = np.zeros(shape = (10, 2), dtype=np.float32)
-        self.viewer.add_vectors(pos)
-        with self.assertRaises(NotImplementedError):
+        N=10
+        M=5
+        pos = np.zeros(shape=(N, M, 2), dtype=np.float32)
+        try:
             self.viewer.add_vectors(pos)
-        # test with good sample data
-        # test with bad sample data (error is handled)
+        except Exception as ex:
+            self.fail("exception thrown when creating coord-like vector layer: "+str(ex))
+        return None
+
+    def test_vectorData_datatype1(self):
+        """
+        test data of coord-like shape
+        :return:
+        """
+
+        N=10
+        pos = np.zeros(shape=(N, 4), dtype=np.float32)
+        try:
+            self.viewer.add_vectors(pos)
+        except Exception as ex:
+            self.fail("exception thrown when creating coord-like vector layer: "+str(ex))
+        return None
+
+    def test_vectorData_datatype_notimpl(self):
+        """
+        test data of improper shape
+        :return:
+        """
+        N=10
+        M=5
+        pos = np.zeros(shape=(N, M, 4), dtype=np.float32)
+        try:
+            self.viewer.add_vectors(pos)
+        except InvalidDataFormatError:
+            self.assertRaises(InvalidDataFormatError)
+        except Exception as ex:
+            self.fail("exception thrown when creating not implemented vector layer: "+str(ex))
+        return None
+
+    def test_vectorData_datatype_notimpl2(self):
+        """
+        test data of vispy-coordinate shape (not allowed)
+        :return:
+        """
+        # self.a_create_env()
+        N=10
+        pos = np.zeros(shape=(N, 2), dtype=np.float32)
+        try:
+            self.viewer.add_vectors(pos)
+        except InvalidDataFormatError:
+            self.assertRaises(InvalidDataFormatError)
+        except Exception as ex:
+            self.fail("exception thrown when creating not implemented vector layer: "+str(ex))
         return None
     
-    def test_vectorData_imageLike(self):
-        self.setup()
-        imvect = np.empty(shape=(10,10,2), dtype=np.float32)
-        space = np.linspace(0, 9, 10)
-        xv, yv = np.meshgrid(space, space)
-        imvect[:,:, 0] = xv
-        imvect[:,:, 1] = yv
-        self.viewer.add_vectors(imvect)
-        return None
-    
-    def test_connector_types(self):
+    def test_vectorData_image_assignment(self):
+        """
+        test replacing vector data after layer construction
+        :return:
+        """
+        # self.a_create_env()
+        N=10
+        pos = np.zeros(shape=(N, 4), dtype=np.float32)
+        pos2 = np.zeros(shape=(N, N, 2), dtype=np.float32)
+        try:
+            layer = self.viewer.add_vectors(pos)
+            layer.vectors = pos2
+        except Exception as ex:
+            self.fail("exception thrown when : "+str(ex))
         return None
     
     def test_averaging_callback(self):
-        # test UI entry works
-        # and programmatic entry
-        # test ipython entry works
-        # test invalid entries and handling (strings, tuples, floats, etc..)
+        """
+        test assignment of function to averaging callback
+        :return:
+        """
+        def testfunc(input):
+            return input
+
+        # self.a_create_env()
+        pos = np.zeros(shape=(10, 10, 2), dtype=np.float32)
+        try:
+            layer = self.viewer.add_vectors(pos)
+            layer.averaging_bind_to(testfunc)
+            layer.averaging_bind_to(testfunc)
+            layer.averaging_bind_to(testfunc)
+            self.assertEqual(len(layer._avg_observers), 3)
+        except Exception as ex:
+            self.fail("exception thrown when  "+str(ex))
         return None
 
-    def test_length(self):
-        # test UI entry works
-        # test programmatic entry works
-        # test ipython entry works
-        # test range (min max)
-        # test data type and handling (must be int or float)
+    def test_length_callback(self):
+        """
+        test assignment of function to length callback
+        :return:
+        """
+        def testfunc(input):
+            return input
+
+        # self.a_create_env()
+        pos = np.zeros(shape=(10, 10, 2), dtype=np.float32)
+        try:
+            layer = self.viewer.add_vectors(pos)
+            layer.length_bind_to(testfunc)
+            layer.length_bind_to(testfunc)
+            layer.length_bind_to(testfunc)
+            self.assertEqual(len(layer._len_observers), 3)
+        except Exception as ex:
+            self.fail("exception thrown when : "+str(ex))
         return None
-    
-    #test UI
-    def test_update(self):
-        return None
-    
-    def test_refresh(self):
-        return None
-    
-    def test_get_shape(self):
-        return None
-    
-    #can consider testing of slices across some vectors
-    def test_slices(self):
+
+    def test_callback(self):
+        """
+        test ability to change underlying data upon callback response
+        :return:
+        """
+        def testfunc(input):
+            layer.vectors = np.zeros(shape=(5,5,2), dtype=np.uint8)
+            return input
+
+        # self.a_create_env()
+        pos = np.zeros(shape=(10, 10, 2), dtype=np.float32)
+        try:
+            layer = self.viewer.add_vectors(pos)
+            self.assertEqual(layer._current_shape, (10,10,2))
+
+            layer.length_bind_to(testfunc)
+            layer.length = 1
+
+            self.assertEqual(layer._current_shape, (5,5,2))
+        except Exception as ex:
+            self.fail("exception thrown when : "+str(ex))
         return None
