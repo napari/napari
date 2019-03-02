e44bbb35b8101f0225803fd109e9c48879b9c86a
Bryant <bchhun@gmail.com>
2019-03-01 17:38:11 -0800

    adjusted to reflect new example format

--- examples/demo_vectors_coord-like.py
+++ examples/demo_vectors_coord-like.py
@@ -1,63 +1,41 @@
-#!/usr/bin/env python
-# title           : demo_vectors_coord-like.py
-# description     :demonstration of vector image using napari layers
-# author          :bryant.chhun
-# date            :1/16/19
-# version         :0.0
-# usage           :
-# notes           :
-# python_version  :3.6
-
-
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from napari_gui import Window, Viewer
-
-import numpy as np
-
-'''
+"""
 This example generates an image of vectors
 Vector data is an array of shape (N, 4)
 Each vector position is defined by an (x, y, x-proj, y-proj) element
     where x and y are the center points
     where x-proj and y-proj are the vector projections at each center
 
-'''
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from napari_gui import Window, Viewer
+
+import numpy as np
 
 
-class VectorWindowExample(QWidget):
+# starting
+application = QApplication(sys.argv)
 
-    def __init__(self, window):
-        super().__init__()
-        self.win = window
-        self.viewer = self.win.viewer
+# create the viewer and window
+viewer = Viewer()
+win = Window(viewer)
 
-        # sample vector coord-like data
-        n = 1000
-        pos = np.zeros((n, 4), dtype=np.float32)
-        phi_space = np.linspace(0, 4*np.pi, n)
-        radius_space = np.linspace(0, 100, n)
+# sample vector coord-like data
+n = 1000
+pos = np.zeros((n, 4), dtype=np.float32)
+phi_space = np.linspace(0, 4*np.pi, n)
+radius_space = np.linspace(0, 100, n)
 
-        # assign x-y position
-        pos[:, 0] = radius_space*np.cos(phi_space)
-        pos[:, 1] = radius_space*np.sin(phi_space)
+# assign x-y position
+pos[:, 0] = radius_space*np.cos(phi_space)
+pos[:, 1] = radius_space*np.sin(phi_space)
 
-        # assign x-y projection
-        pos[:, 2] = radius_space*np.cos(phi_space)
-        pos[:, 3] = radius_space*np.sin(phi_space)
+# assign x-y projection
+pos[:, 2] = radius_space*np.cos(phi_space)
+pos[:, 3] = radius_space*np.sin(phi_space)
 
-        self.viewer.add_vectors(pos)
+# add the vectors
+viewer.add_vectors(pos)
 
-        self.win.show()
-
-
-if __name__ == '__main__':
-    # starting
-    application = QApplication(sys.argv)
-
-    viewer = Viewer()
-    win = Window(Viewer(), show=False)
-
-    multi_win = VectorWindowExample(win)
-
-    sys.exit(application.exec_())
+sys.exit(application.exec_())
--- examples/demo_vectors_image-like.py
+++ examples/demo_vectors_image-like.py
@@ -1,59 +1,38 @@
-#!/usr/bin/env python
-# title           : demo_vectors_coord-like.py
-# description     :demonstration of vector image using napari layers
-# author          :bryant.chhun
-# date            :1/16/19
-# version         :0.0
-# usage           :
-# notes           :
-# python_version  :3.6
-
-
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from napari_gui import Window, Viewer
-
-import numpy as np
-
-'''
+"""
 This example generates an image of vectors
 Vector data is an array of shape (N, M, 2)
 Each vector position is defined by an (x-proj, y-proj) element
     where x-proj and y-proj are the vector projections at each center
     where each vector is centered on a pixel of the NxM grid
-'''
+"""
 
+import sys
+from PyQt5.QtWidgets import QApplication
+from napari_gui import Window, Viewer
 
-class VectorWindowExample(QWidget):
+import numpy as np
 
-    def __init__(self, window):
-        super().__init__()
-        self.win = window
-        self.viewer = self.win.viewer
+# starting
+application = QApplication(sys.argv)
 
-        # sample vector image-like data
-        # 50x25 grid of slanted lines
-        n = 50
-        m = 25
-        pos = np.zeros(shape=(n, m, 2), dtype=np.float32)
-        rand1 = np.random.random_sample(n*m)
-        rand2 = np.random.random_sample(n*m)
-        # assign projections for each vector
-        pos[:, :, 0] = rand1.reshape((n, m))
-        pos[:, :, 1] = rand2.reshape((n, m))
+# create the viewer and window
+viewer = Viewer()
+win = Window(viewer)
 
-        self.viewer.add_vectors(pos)
+# sample vector image-like data
+# 50x25 grid of slanted lines
+n = 50
+m = 25
+pos = np.zeros(shape=(n, m, 2), dtype=np.float32)
+rand1 = np.random.random_sample(n * m)
+rand2 = np.random.random_sample(n * m)
 
-        self.win.show()
+# assign projections for each vector
+pos[:, :, 0] = rand1.reshape((n, m))
+pos[:, :, 1] = rand2.reshape((n, m))
 
+# add the vectors
+viewer.add_vectors(pos)
 
-if __name__ == '__main__':
-    # starting
-    application = QApplication(sys.argv)
+sys.exit(application.exec_())
 
-    viewer = Viewer()
-    win = Window(Viewer(), show=False)
-
-    multi_win = VectorWindowExample(win)
-
-    sys.exit(application.exec_())
