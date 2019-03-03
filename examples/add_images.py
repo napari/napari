5ec2bcb34c8a25085cd80907e208c5dc1a3ac4ee
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2019-03-04 00:41:16 +1100

    Add application context for examples

--- examples/add_images.py
+++ examples/add_images.py
@@ -2,23 +2,17 @@
 Display two image layers using the add_image API
 """
 
-import sys
-from PyQt5.QtWidgets import QApplication
-
 from skimage import data
 from skimage.color import rgb2gray
 from napari import Window, Viewer
+from napari.util import app_context
 
 
-# starting
-application = QApplication(sys.argv)
-
-# create the viewer and window
-viewer = Viewer()
-window = Window(viewer)
-# add the first image
-viewer.add_image(rgb2gray(data.astronaut()))
-# add the second image
-viewer.add_image(data.camera(), {})
-
-sys.exit(application.exec())
+with app_context():
+    # create the viewer and window
+    viewer = Viewer()
+    window = Window(viewer)
+    # add the first image
+    viewer.add_image(rgb2gray(data.astronaut()))
+    # add the second image
+    viewer.add_image(data.camera(), {})
--- examples/lut.py
+++ examples/lut.py
@@ -4,27 +4,21 @@ limits and the colormap. Collectively these properties can be referred to as
 the look-up-table or lut.
 """
 
-import sys
-from PyQt5.QtWidgets import QApplication
-
 from skimage import data
 from skimage.color import rgb2gray
 from napari import Window, Viewer
+from napari.util import app_context
 
 
-# starting
-application = QApplication(sys.argv)
-
-# show grayscale image
-viewer = Viewer()
-window = Window(viewer)
-# add the image
-image = rgb2gray(data.astronaut())
-viewer.add_image(image)
-layer = viewer.layers[0]
-# change the clims
-layer.clim = [0.25, .75]
-# change the colormap
-layer.colormap = 'gray'
-
-sys.exit(application.exec())
+with app_context():
+    # show grayscale image
+    viewer = Viewer()
+    window = Window(viewer)
+    # add the image
+    image = rgb2gray(data.astronaut())
+    viewer.add_image(image)
+    layer = viewer.layers[0]
+    # change the clims
+    layer.clim = [0.25, .75]
+    # change the colormap
+    layer.colormap = 'gray'
