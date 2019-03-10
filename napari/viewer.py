fef3f7a4a7d1e193b9dd6b4ec6a7e45b3bc0c210
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-03-10 15:43:32 +1100

    Add initial full viewer object

---
+++ napari/viewer.py
@@ -0,0 +1,24 @@
+from . import Window, Viewer
+
+
+class ViewerApp:
+    """Napari ndarray viewer.
+
+    Parameters
+    ----------
+    *images : ndarray
+        Arrays to render as image layers.
+    **named_images : dict of str -> ndarray
+        Arrays to render as image layers, keyed by layer name.
+    """
+    def __init__(self, *images, **named_images):
+        v = Viewer()
+        for image in images:
+            v.add_image(image)
+        for name, image in named_images.items():
+            v.add_image(image, name=name)
+        self.viewer_widget = v
+        self.window = Window(v)
+
+    def add_layer(self, layer):
+        self.viewer_widget.add_layer(layer)
