07256f909ee3dc4398176d9819451e133c208b6a
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-03-14 17:49:19 +1100

    Add support for protected parameters in the viewerapp API

--- napari/viewer.py
+++ napari/viewer.py
@@ -8,17 +8,26 @@ class ViewerApp:
     ----------
     *images : ndarray
         Arrays to render as image layers.
-    **named_images : dict of str -> ndarray
+    meta : dictionary, optional
+        A dictionary of metadata attributes. If multiple images are provided,
+        the metadata applies to all of them.
+    multichannel : bool, optional
+        Whether to consider the last dimension of the image(s) as channels
+        rather than spatial attributes. If not provided, napari will attempt
+        to make an educated guess. If provided, and multiple images are given,
+        the same value applies to all images.
+    **named_images : dict of str -> ndarray, optional
         Arrays to render as image layers, keyed by layer name.
     """
-    def __init__(self, *images, **named_images):
+    def __init__(self, *images, meta=None, multichannel=None, **named_images):
         self.viewer_widget = Viewer()
         self.window = Window(self.viewer_widget)
         self.layers = self.viewer_widget.layers
         for image in images:
-            self.add_image(image)
+            self.add_image(image, meta=meta, multichannel=multichannel)
         for name, image in named_images.items():
-            self.add_image(image, name=name)
+            self.add_image(image, meta=meta, multichannel=multichannel,
+                           name=name)
 
     def add_layer(self, layer):
         self.viewer_widget.add_layer(layer)
