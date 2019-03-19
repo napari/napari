8b2ff684dbf34413013af9cdead0279c9c827cc9
Bryant <bchhun@gmail.com>
2019-03-18 18:10:07 -0700

    removed vector controls and unnecessary header

--- napari/layers/_vectors_layer/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# title           : this_python_file.py
-# description     :This will create a header for a python script.
-# author          :bryant.chhun
-# date            :1/25/19
-# version         :0.0
-# usage           :python this_python_file.py -flags
-# notes           :
-# python_version  :3.6
