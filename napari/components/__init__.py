5aa7af176c3901588cbee59338a1e6849221e38f
sofroniewn <sofroniewn@gmail.com>
2019-03-20 13:58:39 -0700

    fix warnings

--- napari/components/__init__.py
+++ napari/components/__init__.py
@@ -10,14 +10,8 @@ Viewer
     Data viewer displaying the currently rendered scene and
     layer-related controls.
 """
-import warnings
 
-vispy_warning = "VisPy is not yet compatible with matplotlib 2.2+"
-
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=UserWarning,
-                            message=vispy_warning)
-    from ._window import Window, QtApplication
+from ._window import Window, QtApplication
 from ._viewer import Viewer
 from ._layers_list import LayersList
 from ._dims import Dims
