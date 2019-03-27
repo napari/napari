1aa6b7c96771ae207c00dff48ae17741f40503dd
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-03-28 04:24:20 +1100

    Add labels 3D example

---
+++ examples/labels-3d.py
@@ -0,0 +1,17 @@
+"""
+Display a labels layer above of an image layer using the add_labels and
+add_image APIs
+"""
+
+from skimage import data
+from scipy import ndimage as ndi
+from napari import ViewerApp
+from napari.util import app_context
+
+print("click to add markers; close the window when finished.")
+
+with app_context():
+    blobs = data.binary_blobs(length=128, volume_fraction=0.1, n_dim=3)
+    v = ViewerApp(blobs=blobs)
+    labeled = ndi.label(blobs)[0]
+    label_layer = v.add_labels(labeled, name='blob ID')
