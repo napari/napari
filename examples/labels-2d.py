fc3cb419d3dc3ee65316e56cb36ab522a64b5ec8
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-03-28 04:14:23 +1100

    Clean up labels-2d example

--- examples/labels-2d.py
+++ examples/labels-2d.py
@@ -3,9 +3,7 @@ Display one markers layer ontop of one image layer using the add_markers and
 add_image APIs
 """
 
-import numpy as np
 from skimage import data
-from skimage.color import rgb2gray
 from skimage.segmentation import slic
 from napari import ViewerApp
 from napari.util import app_context
@@ -14,8 +12,8 @@ from napari.util import app_context
 with app_context():
     astro = data.astronaut()
     # initialise viewer with astro image
-    viewer = ViewerApp(astro, multichannel=True)
+    viewer = ViewerApp(astro, multichannel=True, name='astronaut')
     # add the labels
     labels = slic(astro, multichannel=True)
-    label_layer = viewer.add_labels(labels)
+    label_layer = viewer.add_labels(labels, name='SLIC segmentation')
 
