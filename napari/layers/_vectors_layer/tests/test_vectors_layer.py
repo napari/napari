dad96fac469dcb601752623ed40bcec1112ab420
Bryant <bchhun@gmail.com>
2019-03-29 10:14:01 -0700

    reformatted for pytest using app context

--- napari/layers/_vectors_layer/tests/test_vectors_layer.py
+++ napari/layers/_vectors_layer/tests/test_vectors_layer.py
@@ -1,94 +1,100 @@
-import unittest
-import sys
-
-from PyQt5.QtWidgets import QApplication
-from napari import Viewer, Window
 from ..model import InvalidDataFormatError
 
 import numpy as np
 
+from napari import ViewerApp
+from napari.util import app_context
 
-class TestVectorsLayer(unittest.TestCase):
+
+def test_vectorData_datatype0():
     """
-    these tests are performed on the same Napari viewer instance.
+    test data of image-like shape
+    :return:
     """
-
-    application = QApplication(sys.argv)
-    view = Viewer()
-    win = Window(Viewer(), show=False)
-    viewer = win.viewer
-
-    def test_vectorData_datatype0(self):
-        """
-        test data of image-like shape
-        :return:
-        """
+    with app_context():
+        # create the viewer and window
+        viewer = ViewerApp()
 
         N = 10
         M = 5
         pos = np.zeros(shape=(N, M, 2), dtype=np.float32)
         try:
-            self.viewer.add_vectors(pos)
+            viewer.add_vectors(pos)
+            assert(True)
         except Exception as ex:
-            self.fail("exception thrown when creating coord-like vector layer: "+str(ex))
-        return None
+            print("exception thrown when creating coord-like vector layer: "+str(ex))
 
-    def test_vectorData_datatype1(self):
-        """
-        test data of coord-like shape
-        :return:
-        """
+
+def test_vectorData_datatype1():
+    """
+    test data of coord-like shape
+    :return:
+    """
+    with app_context():
+        # create the viewer and window
+        viewer = ViewerApp()
 
         N = 10
         pos = np.zeros(shape=(N, 4), dtype=np.float32)
         try:
-            self.viewer.add_vectors(pos)
+            viewer.add_vectors(pos)
         except Exception as ex:
-            self.fail("exception thrown when creating coord-like vector layer: "+str(ex))
-        return None
+            print("exception thrown when creating coord-like vector layer: "+str(ex))
 
-    def test_vectorData_datatype_notimpl(self):
-        """
-        test data of improper shape
-        :return:
-        """
+
+def test_vectorData_datatype_notimpl():
+    """
+    test data of improper shape
+    :return:
+    """
+    with app_context():
+        # create the viewer and window
+        viewer = ViewerApp()
+
         N = 10
         M = 5
         pos = np.zeros(shape=(N, M, 4), dtype=np.float32)
         try:
-            self.viewer.add_vectors(pos)
+            viewer.add_vectors(pos)
         except InvalidDataFormatError:
-            self.assertRaises(InvalidDataFormatError)
+            print('invalid data format')
         except Exception as ex:
-            self.fail("exception thrown when creating not implemented vector layer: "+str(ex))
-        return None
+            print("exception thrown when creating not implemented vector layer: "+str(ex))
 
-    def test_vectorData_datatype_notimpl2(self):
-        """
-        test data of vispy-coordinate shape (not allowed)
-        :return:
-        """
+
+def test_vectorData_datatype_notimpl2(self):
+    """
+    test data of vispy-coordinate shape (not allowed)
+    :return:
+    """
+    with app_context():
+        # create the viewer and window
+        viewer = ViewerApp()
+
         N = 10
         pos = np.zeros(shape=(N, 2), dtype=np.float32)
         try:
-            self.viewer.add_vectors(pos)
+            viewer.add_vectors(pos)
         except InvalidDataFormatError:
-            self.assertRaises(InvalidDataFormatError)
+            print('invalid data format')
         except Exception as ex:
-            self.fail("exception thrown when creating not implemented vector layer: "+str(ex))
-        return None
-    
-    def test_vectorData_image_assignment(self):
-        """
-        test replacing vector data after layer construction
-        :return:
-        """
+            print("exception thrown when creating not implemented vector layer: "+str(ex))
+
+
+def test_vectorData_image_assignment(self):
+    """
+    test replacing vector data after layer construction
+    :return:
+    """
+    with app_context():
+        # create the viewer and window
+        viewer = ViewerApp()
+
         N = 10
         pos = np.zeros(shape=(N, 4), dtype=np.float32)
         pos2 = np.zeros(shape=(N, N, 2), dtype=np.float32)
         try:
-            layer = self.viewer.add_vectors(pos)
+            layer = viewer.add_vectors(pos)
             layer.vectors = pos2
         except Exception as ex:
-            self.fail("exception thrown when : "+str(ex))
-        return None
+            print("exception thrown when : "+str(ex))
