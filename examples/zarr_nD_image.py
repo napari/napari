f71d622d2b01448947e16ffc25f6d3efb1b046c3
John Kirkham <jakirkham@gmail.com>
2019-03-29 16:49:46 -0700

    Make example size smaller for CI

--- examples/zarr_nD_image.py
+++ examples/zarr_nD_image.py
@@ -11,8 +11,8 @@ from napari.util import app_context
 
 
 with app_context():
-    data = zarr.zeros((200, 210, 102_000), chunks=(200, 210, 1000))
-    data[100:110, 110:120, 53_000:54_000] = 1
+    data = zarr.zeros((200, 210, 102_000), chunks=(200, 210, 100))
+    data[100:110, 110:120, 53_000:53_100] = 1
 
     array = da.from_zarr(data)
     viewer = ViewerApp(array)
