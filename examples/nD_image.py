b49d449061a8ee60160f694d75b58f12c781f383
Loic Alain Royer <royerloic@gmail.com>
2019-04-03 15:45:59 -0700

    fixes

--- examples/nD_image.py
+++ examples/nD_image.py
@@ -12,5 +12,5 @@ with app_context():
     # TODO: change axis back to 0 once dims displayed in sane order
     blobs = np.stack([data.binary_blobs(length=128, blob_size_fraction=0.05,
                                         n_dim=3, volume_fraction=f)
-                     for f in np.linspace(0.05, 0.5, 10)], axis=-1)
+                     for f in np.linspace(0.05, 0.5, 10)], axis=0)
     viewer = ViewerApp(blobs.astype(float))
--- examples/nD_markers.py
+++ examples/nD_markers.py
@@ -14,7 +14,7 @@ with app_context():
     # TODO: change axis back to 0 once dims displayed in sane order
     blobs = np.stack([data.binary_blobs(length=128, blob_size_fraction=0.05,
                                         n_dim=3, volume_fraction=f)
-                      for f in np.linspace(0.05, 0.5, 10)], axis=-1)
+                      for f in np.linspace(0.05, 0.5, 10)], axis=0)
     viewer = ViewerApp(blobs.astype(float))
     # add the markers
     markers = np.array([[100, 100, 0, 0], [50, 120, 0, 0], [100, 40, 1, 0],
