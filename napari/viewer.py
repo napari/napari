cb0830c883bbb685eac9d4e5b84c4b51f0d09a65
Nicholas Sofroniew <sofroniewn@gmail.com>
2019-04-06 20:24:00 -0700

    Make clim range an input argument (#205)
    * comment out min / max
    
    * fix clim range default
    
    * improve clim range
    
    * add arg to viewer

--- napari/viewer.py
+++ napari/viewer.py
@@ -16,14 +16,23 @@ class ViewerApp(Viewer):
         rather than spatial attributes. If not provided, napari will attempt
         to make an educated guess. If provided, and multiple images are given,
         the same value applies to all images.
+    clim_range : list | array | None
+        Length two list or array with the default color limit range for the
+        image. If not passed will be calculated as the min and max of the
+        image. Passing a value prevents this calculation which can be useful
+        when working with very large datasets that are dynamically loaded.
+        If provided, and multiple images are given, the same value applies to
+        all images.
     **named_images : dict of str -> ndarray, optional
         Arrays to render as image layers, keyed by layer name.
     """
-    def __init__(self, *images, meta=None, multichannel=None, **named_images):
+    def __init__(self, *images, meta=None, multichannel=None, clim_range=None,
+                 **named_images):
         super().__init__()
         self.window = Window(self)
         for image in images:
-            self.add_image(image, meta=meta, multichannel=multichannel)
+            self.add_image(image, meta=meta, multichannel=multichannel,
+                           clim_range=clim_range)
         for name, image in named_images.items():
             self.add_image(image, meta=meta, multichannel=multichannel,
-                           name=name)
+                           clim_range=clim_range, name=name)
