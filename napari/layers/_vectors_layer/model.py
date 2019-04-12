6fd00278083dd42e242b5fe4bf58f30d8ebb393b
Nicholas Sofroniew <sofroniewn@gmail.com>
2019-04-12 16:18:17 -0700

    Use triangles for vectors (#215)
    Switch to using meshes for vectors

--- napari/layers/_vectors_layer/model.py
+++ napari/layers/_vectors_layer/model.py
@@ -5,8 +5,9 @@ from scipy import signal
 
 from .._base_layer import Layer
 from .._register import add_to_viewer
-from ..._vispy.scene.visuals import Line as LinesNode
+from ..._vispy.scene.visuals import Mesh
 from ...util.event import Event
+from ...util import segment_normal
 from vispy.color import get_color_names
 
 from .view import QtVectorsLayer
@@ -24,29 +25,19 @@ class Vectors(Layer):
             x and y are coordinates
             u and v are x and y projections of the vector
         (N, M, 2) is an (N, M) image of (u, v) projections
-
         Returns np.ndarray of the current display (including averaging, length)
-
     averaging : int
         (int, int) kernel over which to convolve and subsample the data
         not implemented for (N, 4) data
-
     width : int
         width of the line in pixels
-
     length : float
         length of the line
         not implemented for (N, 4) data
-
     color : str
         one of "get_color_names" from vispy.color
-
     mode : str
         control panel mode
-
-    See vispy's line visual docs for more details:
-    http://api.vispy.org/en/latest/scene.html
-    vispy.scene.visuals.Line
     """
 
     def __init__(self,
@@ -57,12 +48,13 @@ class Vectors(Layer):
                  length=1,
                  name=None):
 
-        visual = LinesNode(antialias=True)
+        visual = Mesh()
         super().__init__(visual)
 
         # events for non-napari calculations
         self.events.add(length=Event,
-                        average=Event)
+                        width=Event,
+                        averaging=Event)
 
         # Store underlying data model
         self._data_types = ('image', 'coords')
@@ -82,10 +74,14 @@ class Vectors(Layer):
         self._need_visual_update = False
 
         # assign vector data and establish default behavior
-        self._raw_dat = None
+        self._raw_data = None
         self._original_data = vectors
         self._current_data = vectors
+
         self._vectors = self._convert_to_vector_type(vectors)
+        vertices, triangles = self._generate_meshes(self._vectors, self.width)
+        self._mesh_vertices = vertices
+        self._mesh_triangles = triangles
 
         if name is None:
             self.name = 'vectors'
@@ -97,20 +93,20 @@ class Vectors(Layer):
     # ====================== Property getter and setters =====================
     @property
     def _original_data(self) -> np.ndarray:
-        return self._raw_dat
+        return self._raw_data
 
     @_original_data.setter
-    def _original_data(self, dat: np.ndarray):
+    def _original_data(self, data: np.ndarray):
         """Must preserve data used at construction. Specifically for default
         averaging/length adjustments.
         averaging/length adjustments recalculate the underlying data
 
         Parameters
         ----------
-        dat : np.ndarray
+        data : np.ndarray
         """
-        if self._raw_dat is None:
-            self._raw_dat = dat
+        if self._raw_data is None:
+            self._raw_data = data
 
     @property
     def vectors(self) -> np.ndarray:
@@ -135,8 +131,12 @@ class Vectors(Layer):
 
         self._vectors = self._convert_to_vector_type(self._current_data)
 
+        vertices, triangles = self._generate_meshes(self._vectors, self.width)
+        self._mesh_vertices = vertices
+        self._mesh_triangles = triangles
+
         self.viewer._child_layer_changed = True
-        self._refresh()
+        self.refresh()
 
     def _convert_to_vector_type(self, vectors):
         """Check on input data for proper shape and dtype
@@ -216,7 +216,7 @@ class Vectors(Layer):
         vect : np.ndarray of shape (N, 4)
         """
         # create empty vector of necessary shape
-        #   one coordinate for each endpoint of the vector
+        # one coordinate for each endpoint of the vector
         pos = np.empty((2 * len(vect), 2), dtype=np.float32)
 
         # create pairs of points
@@ -234,7 +234,7 @@ class Vectors(Layer):
     @property
     def averaging(self) -> int:
         return self._averaging
-    
+
     @averaging.setter
     def averaging(self, value: int):
         """Calculates an average vector over a kernel
@@ -245,26 +245,26 @@ class Vectors(Layer):
         """
         self._averaging = value
 
-        # emit signal back to qt_properties for averaging
-        self.events.average()
+        self.events.averaging()
+        self._update_avg()
 
-        self._refresh()
+        self.refresh()
 
-    def _default_avg(self):
-        """Default method for calculating average
+    def _update_avg(self):
+        """Method for calculating average
         Implemented ONLY for image-like vector data
         """
         if self._data_type == 'coords':
+            # default averaging is supported only for 'matrix' dataTypes
             return
-            # return "default averaging is supported only for 'matrix' dataTypes"
         elif self._data_type == 'image':
 
             x, y = self._averaging, self._averaging
 
             if (x,y) == (1, 1):
                 self.vectors = self._original_data
+                # calling original data
                 return
-                # return "calling original data"
 
             tempdat = self._original_data
             range_x = tempdat.shape[0]
@@ -275,14 +275,16 @@ class Vectors(Layer):
             kernel = np.ones(shape=(x, y)) / (x*y)
 
             output_mat = np.zeros_like(tempdat)
-            output_mat_x = signal.convolve2d(tempdat[:, :, 0], kernel, mode='same', boundary='wrap')
-            output_mat_y = signal.convolve2d(tempdat[:, :, 1], kernel, mode='same', boundary='wrap')
+            output_mat_x = signal.convolve2d(tempdat[:, :, 0], kernel,
+                                             mode='same', boundary='wrap')
+            output_mat_y = signal.convolve2d(tempdat[:, :, 1], kernel,
+                                             mode='same', boundary='wrap')
 
             output_mat[:, :, 0] = output_mat_x
             output_mat[:, :, 1] = output_mat_y
 
-            self.vectors = output_mat[x_offset:range_x-x_offset:x, y_offset:range_y-y_offset:y]
-            self.length = self._length
+            self.vectors = (output_mat[x_offset:range_x-x_offset:x,
+                                       y_offset:range_y-y_offset:y])
 
     @property
     def width(self) -> Union[int, float]:
@@ -294,7 +296,14 @@ class Vectors(Layer):
         widths greater than 1px only guaranteed to work with "agg" method
         """
         self._width = width
-        self._refresh()
+
+        vertices, triangles = self._generate_meshes(self.vectors, self._width)
+        self._mesh_vertices = vertices
+        self._mesh_triangles = triangles
+
+        self.events.width()
+
+        self.refresh()
 
     @property
     def length(self) -> Union[int, float]:
@@ -309,15 +318,14 @@ class Vectors(Layer):
         length : int or float multiplicative factor
         """
         self._length = length
-
-        # emit signal back to qt_properties for length
+        self._update_length()
         self.events.length()
 
-        self._refresh()
+        self.refresh()
 
-    def _default_length(self):
+    def _update_length(self):
         """
-        Default method for calculating vector lengths
+        Method for calculating vector lengths
         Implemented ONLY for image-like vector data
         """
 
@@ -325,6 +333,10 @@ class Vectors(Layer):
             return "length adjustment not allowed for coordinate-style data"
         elif self._data_type == 'image':
             self._vectors = self._convert_to_vector_type(self._current_data)
+            vertices, triangles = self._generate_meshes(self.vectors,
+                                                        self.width)
+            self._mesh_vertices = vertices
+            self._mesh_triangles = triangles
 
     @property
     def color(self) -> str:
@@ -335,7 +347,7 @@ class Vectors(Layer):
         """Color, ColorArray: color of the body of the marker
         """
         self._color = color
-        self._refresh()
+        self.refresh()
 
     # =========================== Napari Layer ABC methods ===================
     @property
@@ -358,6 +370,38 @@ class Vectors(Layer):
         self._need_display_update = True
         self._update()
 
+    def _generate_meshes(self, vectors, width):
+        """Generates list of mesh vertices and triangles from a list of vectors
+
+        Parameters
+        ----------
+        vectors : np.ndarray
+            Nx2 array where each pair of vertices corresponds to an independent
+            line segment
+        width : float
+            width of the line to be drawn
+
+        Returns
+        ----------
+        vertices : np.ndarray
+            2Nx2 array of vertices of all triangles for the lines
+        triangles : np.ndarray
+            Nx3 array of vertex indices that form the mesh triangles
+        """
+        centers = np.array([vectors[i//2] for i in range(2*len(vectors))])
+        offests = segment_normal(vectors[::2, :], vectors[1::2, :])
+        offests = offests[[i//4 for i in range(4*len(offests))]]
+        signs = np.ones((len(offests), 2))
+        signs[::2] = -1
+        offests = offests*signs
+
+        vertices = centers + width*offests/2
+        triangles = np.array([[2*i, 2*i+1, 2*i+2] if i % 2 == 0 else
+                              [2*i-1, 2*i, 2*i+1] for i in
+                              range(len(vectors))]).astype(np.uint32)
+
+        return vertices, triangles
+
     def _update(self):
         """Update the underlying visual.
         """
@@ -378,21 +422,15 @@ class Vectors(Layer):
         indices : sequence of int or slice
             Indices to slice with.
         """
-        
-        in_slice_vectors = self.vectors
 
-        # Display vectors if there are any in this slice
-        if len(in_slice_vectors) > 0:
-            data = np.array(in_slice_vectors) + 0.5
+        vertices = self._mesh_vertices
+        faces = self._mesh_triangles
+
+        if len(faces) == 0:
+            self._node.set_data(vertices=None, faces=None)
         else:
-            # if no vectors in this slice send dummy data
-            data = np.empty((0, 2))
+            self._node.set_data(vertices=vertices, faces=faces,
+                                color=self.color)
 
-        self._node.set_data(
-            data[::-1],
-            width=self.width,
-            color=self.color,
-            connect='segments')
         self._need_visual_update = True
         self._update()
-
