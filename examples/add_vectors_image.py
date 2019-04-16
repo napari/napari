44fdaf06c3af0b3cb9eb636d0bb7a1fa7196e342
sofroniewn <sofroniewn@gmail.com>
2019-04-15 22:05:35 -0700

    test osx CI

--- examples/add_vectors_image.py
+++ examples/add_vectors_image.py
@@ -15,8 +15,8 @@ with app_context():
     # create the viewer and window
     viewer = ViewerApp()
 
-    n = 50
-    m = 100
+    n = 5
+    m = 10
 
     image = 0.2*np.random.random((n, m)) + 0.5
     layer = viewer.add_image(image, clim_range=[0, 1], name='background')
