42bfff6924b8fcefcbeca49bc3246e7ca4c4a7a9
sofroniewn <sofroniewn@gmail.com>
2019-04-19 19:47:47 -0700

    undo screenshot move

--- napari/components/_viewer/viewer.py
+++ napari/components/_viewer/viewer.py
@@ -137,6 +137,31 @@ class Viewer:
         """
         self._qtviewer.view.camera.set_range()
 
+    def screenshot(self, region=None, size=None, bgcolor=None):
+        """Render the scene to an offscreen buffer and return the image array.
+
+        Parameters
+        ----------
+        region : tuple | None
+            Specifies the region of the canvas to render. Format is
+            (x, y, w, h). By default, the entire canvas is rendered.
+        size : tuple | None
+            Specifies the size of the image array to return. If no size is
+            given, then the size of the *region* is used, multiplied by the
+            pixel scaling factor of the canvas (see `pixel_scale`). This
+            argument allows the scene to be rendered at resolutions different
+            from the native canvas resolution.
+        bgcolor : instance of Color | None
+            The background color to use.
+
+        Returns
+        -------
+        image : array
+            Numpy array of type ubyte and shape (h, w, 4). Index [0, 0] is the
+            upper-left corner of the rendered region.
+        """
+        return self.canvas.render(region, size, bgcolor)
+        
     def add_layer(self, layer):
         """Adds a layer to the viewer.
 
