dcbd28b0802af0d6edbee4ea4ea31253e215779f
sofroniewn <sofroniewn@gmail.com>
2019-04-30 20:32:18 -0700

    fix API_NAME qtpy

--- napari/components/_window/__init__.py
+++ napari/components/_window/__init__.py
@@ -3,13 +3,13 @@ Custom Qt widgets that serve as native objects that the public-facing elements
 wrap.
 """
 from qtpy.QtWidgets import QApplication as QtApplication
+from qtpy import API_NAME
 from os import environ
 from .view import Window
 
 from vispy import app
 
-if 'QT_API' in environ:
-    app.use_app(environ['QT_API'])
-else:
-    app.use_app('pyqt5')
+# set vispy to use same backend as qtpy
+app.use_app(API_NAME)
+
 del app
