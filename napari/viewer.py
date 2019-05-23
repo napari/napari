887ba56a00745e96a5c01462d2997dcf24f5f942
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-05-23 13:02:15 +1000

    Remove image input to Viewer constructor

--- napari/viewer.py
+++ napari/viewer.py
@@ -2,7 +2,6 @@ import os.path as osp
 
 from .components._viewer.view import QtViewer
 from .components import Window, ViewerWidget
-from .layers._image_layer.model import Image
 from .resources import resources_dir
 from .util.theme import template, palettes
 from .util.misc import has_clims
@@ -13,40 +12,16 @@ class Viewer(ViewerWidget):
 
     Parameters
     ----------
-    *images : ndarray
-        Arrays to render as image layers.
-    meta : dictionary, optional
-        A dictionary of metadata attributes. If multiple images are provided,
-        the metadata applies to all of them.
-    multichannel : bool, optional
-        Whether to consider the last dimension of the image(s) as channels
-        rather than spatial attributes. If not provided, napari will attempt
-        to make an educated guess. If provided, and multiple images are given,
-        the same value applies to all images.
-    clim_range : list | array | None
-        Length two list or array with the default color limit range for the
-        image. If not passed will be calculated as the min and max of the
-        image. Passing a value prevents this calculation which can be useful
-        when working with very large datasets that are dynamically loaded.
-        If provided, and multiple images are given, the same value applies to
-        all images.
-    **named_images : dict of str -> ndarray, optional
-        Arrays to render as image layers, keyed by layer name.
+    title : string
+        The title of the viewer window.
     """
     with open(osp.join(resources_dir, 'stylesheet.qss'), 'r') as f:
         raw_stylesheet = f.read()
 
-    def __init__(self, *images, meta=None, multichannel=None, clim_range=None,
-                 title='napari', **named_images):
+    def __init__(self, title='napari'):
         super().__init__(title=title)
         self._qtviewer = QtViewer(self)
         self.window = Window(self)
-        for image in images:
-            self.add_image(image, meta=meta, multichannel=multichannel,
-                           clim_range=clim_range)
-        for name, image in named_images.items():
-            self.add_image(image, meta=meta, multichannel=multichannel,
-                           clim_range=clim_range, name=name)
         self.theme = 'dark'
 
     @property
