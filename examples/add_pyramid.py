bc8d614231cecd47ce896a1d76d0257c7a706774
sofroniewn <sofroniewn@gmail.com>
2019-05-26 09:42:59 -0700

    explain padding

--- examples/add_pyramid.py
+++ examples/add_pyramid.py
@@ -28,5 +28,10 @@ with app_context():
     viewer.add_pyramid(pyramid)
 
     camera = viewer.window.qt_viewer.view.camera
-    camera.rect = (-.1*pyramid[0].shape[1], 0, 1.2*pyramid[0].shape[1],
-                   pyramid[0].shape[0])
+
+    # Set the view box of the camera to include the whole base image of the
+    # pyramid with a little padding. The view box is a 4-tuple of the x, y
+    # corner position followed by width and height
+    base_shape = pyramid[0].shape
+    camera.rect = (-0.1 * base_shape[1], -0.1 * base_shape[0],
+                   1.2 * base_shape[1], 1.2 * base_shape[0])
