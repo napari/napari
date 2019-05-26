7334417add9040d26ac64fe2a00ea2953e1c4112
sofroniewn <sofroniewn@gmail.com>
2019-05-26 08:38:43 -0700

    use max_layer

--- examples/add_pyramid.py
+++ examples/add_pyramid.py
@@ -3,7 +3,6 @@ Displays an image pyramid
 """
 
 from skimage import data
-from skimage.color import rgb2gray
 from skimage.transform import pyramid_gaussian
 import napari
 from napari.util import app_context
@@ -13,11 +12,12 @@ import numpy as np
 image = data.astronaut()
 rows, cols, dim = image.shape
 
-
 # create pyramid from astronaut image
-astronaut = rgb2gray(data.astronaut())
+astronaut = data.astronaut().mean(axis=2) / 255
 base = np.tile(astronaut, (16, 16))
-pyramid = list(pyramid_gaussian(base, downscale=2, multichannel=False))[:-8]
+pyramid = list(pyramid_gaussian(base, downscale=2, max_layer=5,
+                                multichannel=False))
+pyramid = [(255*p).astype('uint8') for p in pyramid]
 print([p.shape[:2] for p in pyramid])
 
 with app_context():
