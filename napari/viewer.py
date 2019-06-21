14ac7f9fd4d7bbc55840d3c3d0ba38908e9376c4
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-06-21 21:14:50 +1000

    Pass the hide_menubar kwarg from Viewer to Window

--- napari/viewer.py
+++ napari/viewer.py
@@ -11,9 +11,9 @@ class Viewer(ViewerModel):
         The title of the viewer window.
     """
 
-    def __init__(self, title='napari'):
+    def __init__(self, title='napari', hide_menubar=True):
         super().__init__(title=title)
         qt_viewer = QtViewer(self)
-        self.window = Window(qt_viewer)
+        self.window = Window(qt_viewer, hide_menubar=hide_menubar)
         self.screenshot = self.window.qt_viewer.screenshot
         self.camera = self.window.qt_viewer.view.camera
