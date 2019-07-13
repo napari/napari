e48ec21ee2c3fe36dd3145772d7647bb568c0b7e
sofroniewn <sofroniewn@gmail.com>
2019-07-13 18:27:53 -0500

    xarray example

---
+++ examples/xarray_nD_image.py
@@ -0,0 +1,16 @@
+"""
+Displays an xarray
+"""
+
+import xarray as xr
+from napari import Viewer, gui_qt
+
+data = np.random.random((20, 40, 50))
+xdata = xr.DataArray(data, dims=['z', 'y', 'x'])
+
+with gui_qt():
+    # create an empty viewer
+    viewer = Viewer()
+
+    # add the xarray
+    layer = viewer.add_image(xdata, name='xarray')
