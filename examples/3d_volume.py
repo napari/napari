3ac1fc78faa61c75bd7af3a5e7db12a820120a5f
pranathivemuri <pranathi93.vemuri@gmail.com>
2019-07-15 21:22:41 -0700

    remove setting volume in view function

--- examples/3d_volume.py
+++ examples/3d_volume.py
@@ -2,20 +2,13 @@
 Display one 3-D volume layer using the add_volume API
 """
 
-import dask.array as da
-import numpy as np
 from skimage import data
 import napari
 
 
 with napari.gui_qt():
-    blobs = da.stack(
-        [
-            data.binary_blobs(
-                length=128, blob_size_fraction=0.05, n_dim=3, volume_fraction=f
-            )
-            for f in np.linspace(0.05, 0.5, 10)
-        ],
-        axis=0,
-    )
-    viewer = napari.view(blobs.astype(float))
+    blobs = data.binary_blobs(length=128, blob_size_fraction=0.05, n_dim=3)
+    blobs = blobs.astype(float) * 255
+    viewer = napari.Viewer()
+    # add the volume
+    viewer.add_volume(blobs)
