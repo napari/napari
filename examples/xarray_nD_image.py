1a87b2bfb2d3c79f8818e64e20576617e74221c7
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-07-16 17:23:34 -0500

    Use import napari instead of from

--- examples/xarray_nD_image.py
+++ examples/xarray_nD_image.py
@@ -4,14 +4,14 @@ Displays an xarray
 
 import numpy as np
 import xarray as xr
-from napari import Viewer, gui_qt
+import napari
 
 data = np.random.random((20, 40, 50))
 xdata = xr.DataArray(data, dims=['z', 'y', 'x'])
 
-with gui_qt():
+with napari.gui_qt():
     # create an empty viewer
-    viewer = Viewer()
+    viewer = napari.Viewer()
 
     # add the xarray
     layer = viewer.add_image(xdata, name='xarray')
