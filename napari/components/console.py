525bae54535642cbcef757b7e7b70823e4898e65
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-07-16 17:05:58 -0500

    Update with code from @Carreau

--- napari/components/console.py
+++ napari/components/console.py
@@ -1,4 +1,4 @@
-from IPython.core.interactiveshell import InteractiveShell
+from IPython import get_ipython
 
 
 CODE = '''from skimage import filters
@@ -9,10 +9,16 @@ viewer.add_image(output)
 
 def call_ipython_code(code=CODE):
     # get current running instance or create new instance
-    shell = InteractiveShell.instance()
+    shell = get_ipython()
     # note: shell.extract_input_lines will return existing input lines.
     # we should save them and restore them after running our command
-    # shell.set_next_input
-    # shell.set_next_input(code)
-    shell.pt_app.app.current_buffer.insert_text(code)
-    shell.pt_app.app.current_buffer.validate_and_handle()
+    if shell is None:  # pure-python interpreter
+        pass
+    elif hasattr(shell, 'pt_app'):  # ipython terminal
+        shell.pt_app.app.current_buffer.insert_text(code)
+        shell.pt_app.app.current_buffer.validate_and_handle()
+    else:  # ipython kernel
+        shell.kernel._publish_execute_input(
+            code, None, shell.kernel.execution_count
+        )
+        shell.run_cell(code)
