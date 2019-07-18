c68cbbf75d2aa03d9673c3c8c8d01bd30f247e82
sofroniewn <sofroniewn@gmail.com>
2019-07-18 14:50:47 -0700

    add_volume example

---
+++ examples/add_volume.py
@@ -0,0 +1,15 @@
+"""
+Display one 3-D volume layer using the add_volume API
+"""
+
+from skimage import data
+import napari
+
+
+with napari.gui_qt():
+    blobs = data.binary_blobs(length=64, volume_fraction=0.1, n_dim=3).astype(
+        float
+    )
+    viewer = napari.Viewer()
+    # add the volume
+    viewer.add_volume(blobs)
