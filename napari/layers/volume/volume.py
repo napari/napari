6c37290fd72603dca0834d66036e51e12e279a01
pranathivemuri <pranathi93.vemuri@gmail.com>
2019-07-19 17:11:50 -0700

    setting cmap in volume

--- napari/layers/volume/volume.py
+++ napari/layers/volume/volume.py
@@ -162,7 +162,7 @@ class Volume(Layer):
             warnings.warn(f'invalid value for colormap: {colormap}')
             name = self._colormap_name
         self._colormap_name = name
-        self._node._cmap = self._colormaps[name]
+        self._node.cmap = self._colormaps[name]
         self._update_thumbnail()
         self.events.colormap()
 
