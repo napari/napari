ac98b93169dbdb2b82dcdbf1e9978c039c170f99
Kira Evans <contact@kne42.me>
2019-07-23 09:50:07 -0700

    automate discovery of layer types

--- napari/tests/test_viewer_layer_parity.py
+++ napari/tests/test_viewer_layer_parity.py
@@ -3,27 +3,42 @@ Ensure that layers and their convenience methods on the viewer
 have the same signatures and docstrings.
 """
 
-from napari.layers import Image, Pyramid, Points, Labels, Shapes, Vectors
-from napari import Viewer
+import inspect
+
+import pytest
+
+from napari import layers as module, Viewer
 
 
-def test_docstrings():
-    for obj in (Image, Pyramid, Points, Labels, Shapes, Vectors):
-        method = f'add_{obj.__name__.lower()}'
-        obj_doc = obj.__doc__
-        i = [
-            idx
-            for idx, section in enumerate(obj.__doc__.split('\n\n'))
-            if section.strip().startswith('Parameters')
-        ][0]
-        # We only check the parameters section, which is the first
-        # section after separating by empty line. We also strip empty
-        # line and white space that occurs at the end of a class docstring
-        obj_param = obj_doc.split('\n\n')[i].rstrip('\n ')
-        method_doc = getattr(Viewer, method).__doc__
-        # For the method docstring, we also need to
-        # remove the extra indentation of the method docstring compared
-        # to the class
-        method_param = method_doc.split('\n\n')[i].replace('\n    ', '\n')[4:]
-        fail_msg = f"Docstrings don't match for class {obj.__name__}"
-        assert obj_param == method_param, fail_msg
+layers = []
+
+for name in dir(module):
+    obj = getattr(module, name)
+
+    if obj is module.Layer or not inspect.isclass(obj):
+        continue
+
+    if issubclass(obj, module.Layer):
+        layers.append(obj)
+
+
+@pytest.mark.parametrize('layer', layers, ids=lambda layer: layer.__name__)
+def test_docstring(layer):
+    method = f'add_{layer.__name__.lower()}'
+    layer_doc = layer.__doc__
+    i = [
+        idx
+        for idx, section in enumerate(layer.__doc__.split('\n\n'))
+        if section.strip().startswith('Parameters')
+    ][0]
+    # We only check the parameters section, which is the first
+    # section after separating by empty line. We also strip empty
+    # line and white space that occurs at the end of a class docstring
+    layer_param = layer_doc.split('\n\n')[i].rstrip('\n ')
+    method_doc = getattr(Viewer, method).__doc__
+    # For the method docstring, we also need to
+    # remove the extra indentation of the method docstring compared
+    # to the class
+    method_param = method_doc.split('\n\n')[i].replace('\n    ', '\n')[4:]
+    fail_msg = f"Docstrings don't match for class {layer.__name__}"
+    assert layer_param == method_param, fail_msg
