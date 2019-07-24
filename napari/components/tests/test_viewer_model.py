018ebbea27840ed7a6907bf4867ebfa92adbe238
sofroniewn <sofroniewn@gmail.com>
2019-07-24 11:41:19 -0700

    swappable points

--- napari/components/tests/test_viewer_model.py
+++ napari/components/tests/test_viewer_model.py
@@ -136,3 +136,88 @@ def test_new_shapes():
     viewer._new_shapes()
     assert len(viewer.layers) == 2
     assert len(viewer.layers[1].data) == 0
+
+
+def test_swappable_dims():
+    """Test swapping dims after adding layers."""
+    viewer = ViewerModel()
+    np.random.seed(0)
+    image_data = np.random.random((7, 12, 10, 15))
+    viewer.add_image(image_data)
+    points_data = np.random.randint(6, size=(10, 4))
+    viewer.add_points(points_data)
+
+    # Swap dims
+    viewer.dims.swap_display(1, 2)
+    assert viewer.dims.display == [False, True, False, True]
+
+
+# def test_add_volume():
+#     """Test adding volume."""
+#     viewer = ViewerModel()
+#     np.random.seed(0)
+#     data = np.random.random((10, 15, 20))
+#     viewer.add_volume(data)
+#     assert len(viewer.layers) == 1
+#     assert np.all(viewer.layers[0].data == data)
+#
+#
+# def test_add_pyramid():
+#     """Test adding image pyramid."""
+#     viewer = ViewerModel()
+#     shapes = [(40, 20), (20, 10), (10, 5)]
+#     np.random.seed(0)
+#     data = [np.random.random(s) for s in shapes]
+#     viewer.add_pyramid(data)
+#     assert len(viewer.layers) == 1
+#     assert np.all(viewer.layers[0].data == data)
+#
+#
+# def test_add_labels():
+#     """Test adding labels image."""
+#     viewer = ViewerModel()
+#     np.random.seed(0)
+#     data = np.random.randint(20, size=(10, 15))
+#     viewer.add_labels(data)
+#     assert len(viewer.layers) == 1
+#     assert np.all(viewer.layers[0].data == data)
+#
+#
+#
+#
+# def test_add_vectors():
+#     """Test adding vectors."""
+#     viewer = ViewerModel()
+#     np.random.seed(0)
+#     data = 20 * np.random.random((10, 2, 2))
+#     viewer.add_vectors(data)
+#     assert len(viewer.layers) == 1
+#     assert np.all(viewer.layers[0].data == data)
+#
+#
+# def test_add_shapes():
+#     """Test adding vectors."""
+#     viewer = ViewerModel()
+#     np.random.seed(0)
+#     data = 20 * np.random.random((10, 4, 2))
+#     viewer.add_shapes(data)
+#     assert len(viewer.layers) == 1
+#     assert np.all(viewer.layers[0].data == data)
+#
+#
+# def test_new_labels():
+#     """Test adding new labels image."""
+#     # Add labels to empty viewer
+#     viewer = ViewerModel()
+#     viewer._new_labels()
+#     assert len(viewer.layers) == 1
+#     assert np.max(viewer.layers[0].data) == 0
+#
+#     # Add labels with image already present
+#     viewer = ViewerModel()
+#     np.random.seed(0)
+#     data = np.random.random((10, 15))
+#     viewer.add_image(data)
+#     viewer._new_labels()
+#     assert len(viewer.layers) == 2
+#     assert np.max(viewer.layers[1].data) == 0
