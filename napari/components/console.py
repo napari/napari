50e4b211df486e1a234bcd40177426d2dc2b514a
sofroniewn <sofroniewn@gmail.com>
2019-07-25 10:49:47 -0700

    support for launching from jupyter

--- napari/components/console.py
+++ napari/components/console.py
@@ -1,44 +1,134 @@
+from qtpy.QtWidgets import QWidget
 from qtconsole.rich_jupyter_widget import RichJupyterWidget
 from qtconsole.inprocess import QtInProcessKernelManager
+from qtconsole.manager import QtKernelManager
+from qtconsole.client import QtKernelClient
+from IPython import get_ipython
+from IPython.terminal.interactiveshell import TerminalInteractiveShell
+from ipykernel.zmqshell import ZMQInteractiveShell
+from ipykernel.connect import get_connection_file
 
 
 def make_console(user_ns):
-    global ipython_widget  # Prevent from being garbage collected
+    # Prevent from being garbage collected
+    global ipython_widget
 
-    # Create an in-process kernel
-    kernel_manager = QtInProcessKernelManager()
-    kernel_manager.start_kernel(show_banner=False)
-    kernel = kernel_manager.kernel
-    kernel.gui = 'qt'
-    kernel.shell.user_ns.update(user_ns)
+    # get current running instance or create new instance
+    shell = get_ipython()
 
-    kernel_client = kernel_manager.client()
-    kernel_client.start_channels()
+    if shell is None:  # pure-python interpreter
+        # Create an in-process kernel
+        kernel_manager = QtInProcessKernelManager()
+        kernel_manager.start_kernel(show_banner=False)
+        kernel_manager.kernel.gui = 'qt'
+        kernel_manager.kernel.shell.user_ns.update(user_ns)
 
-    ipython_widget = RichJupyterWidget()
-    ipython_widget.kernel_manager = kernel_manager
-    ipython_widget.kernel_client = kernel_client
+        kernel_client = kernel_manager.client()
+        kernel_client.start_channels()
 
-    ipython_widget.setStyleSheet(
-        """QPlainTextEdit, QTextEdit {
-        background-color: black;
-        background-clip: padding;
-        color: white;
-        selection-background-color: #ccc;
-    }
-    .inverted {
-        background-color: white;
-        color: black;
-    }
-    .error { color: red; }
-    .in-prompt-number { font-weight: bold; }
-    .out-prompt-number { font-weight: bold; }
-    .in-prompt { color: green; }
-    .out-prompt { color: darkred; }
-    """
-    )
+        ipython_widget = RichJupyterWidget()
+        ipython_widget.kernel_manager = kernel_manager
+        ipython_widget.kernel_client = kernel_client
+
+        ipython_widget.setStyleSheet(
+            """QPlainTextEdit, QTextEdit {
+            background-color: black;
+            background-clip: padding;
+            color: white;
+            selection-background-color: #ccc;
+        }
+        .inverted {
+            background-color: white;
+            color: black;
+        }
+        .error { color: red; }
+        .in-prompt-number { font-weight: bold; }
+        .out-prompt-number { font-weight: bold; }
+        .in-prompt { color: green; }
+        .out-prompt { color: darkred; }
+        """
+        )
+
+    elif type(shell) == TerminalInteractiveShell:  # ipython terminal
+        ipython_widget = QWidget
+        print(type(shell))
+        # kernel_manager = QtInProcessKernelManager()
+        # #kernel_manager.start_kernel(show_banner=False)
+        # kernel_manager.kernel.gui = 'qt'
+        # kernel_manager.kernel.shell = shell
+        # kernel_manager.kernel.shell.user_ns.update(user_ns)
+        #
+        # kernel_client = kernel_manager.client()
+        # kernel_client.start_channels()
+        #
+        # ipython_widget = RichJupyterWidget()
+        # ipython_widget.kernel_manager = kernel_manager
+        # ipython_widget.kernel_client = kernel_client
+
+        # kernel_client = QtKernelClient(connection_file=get_connection_file())
+        # kernel_client.load_connection_file()
+        # kernel_client.start_channels()
+        #
+        # ipython_widget = RichJupyterWidget()
+        # ipython_widget.kernel_client = kernel_client
+        # ipython_widget.shell = shell
+        # ipython_widget.shell.user_ns.update(user_ns)
+
+    elif type(shell) == ZMQInteractiveShell:  # ipython kernel
+        kernel_client = QtKernelClient(connection_file=get_connection_file())
+        kernel_client.load_connection_file()
+        kernel_client.start_channels()
+
+        ipython_widget = RichJupyterWidget()
+        ipython_widget.kernel_client = kernel_client
+        ipython_widget.shell = shell
+        ipython_widget.shell.user_ns.update(user_ns)
+
+        ipython_widget.setStyleSheet(
+            """QPlainTextEdit, QTextEdit {
+            background-color: black;
+            background-clip: padding;
+            color: white;
+            selection-background-color: #ccc;
+        }
+        .inverted {
+            background-color: white;
+            color: black;
+        }
+        .error { color: red; }
+        .in-prompt-number { font-weight: bold; }
+        .out-prompt-number { font-weight: bold; }
+        .in-prompt { color: green; }
+        .out-prompt { color: darkred; }
+        """
+        )
+    else:
+        raise ValueError('ipython shell not recognized; ' f'got {type(shell)}')
 
     return ipython_widget
+
+
+# def make_console(user_ns):
+#     """Start a kernel, connect to it, and create a RichJupyterWidget to use it
+#     """
+#     kernel_manager = QtKernelManager(kernel_name='python3')
+#     kernel_manager.start_kernel()
+#
+#
+#     kernel_client = kernel_manager.client()
+#     kernel_client.start_channels()
+#
+#
+#     jupyter_widget = RichJupyterWidget()
+#     jupyter_widget.kernel_manager = kernel_manager
+#     jupyter_widget.kernel_client = kernel_client
+#
+#     #kernel_manager.kernel.shell.user_ns.update(user_ns)
+#     print(dir(jupyter_widget.kernel_manager.kernel))
+#     print(dir(jupyter_widget.kernel_manager))
+#     print(dir(jupyter_widget))
+#
+#     return jupyter_widget
 
 
 #
