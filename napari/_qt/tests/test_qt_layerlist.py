32f4667aa72fe8b2ad8fc6691933ff4d925bbeac
Kira Evans <contact@kne42.me>
2019-07-30 10:27:46 -0700

    update existing tests to use pytest-qt

--- napari/_qt/tests/test_qt_layerlist.py
+++ napari/_qt/tests/test_qt_layerlist.py
@@ -1,11 +1,8 @@
-import os
-from napari.components import LayerList
-from napari._qt.qt_layerlist import QtLayerList, QtDivider
-from napari import gui_qt
-from napari.layers import Image
 import numpy as np
 
-os.environ['NAPARI_TEST'] = '1'
+from napari.components import LayerList
+from napari.layers import Image
+from napari._qt.qt_layerlist import QtLayerList, QtDivider
 
 
 def check_layout_layers(layout, layers):
@@ -53,154 +50,159 @@ def check_layout_dividers(layout, nlayers):
     return dividers_layout == [QtDivider] * (1 + nlayers)
 
 
-def test_divider():
+def test_divider(qtbot):
     """
     Test creating the divider.
     """
-    with gui_qt():
-        divider = QtDivider()
+    divider = QtDivider()
 
-        # Check divider was created properly
-        assert type(divider) == QtDivider
+    qtbot.addWidget(divider)
 
-        # Check divider property defaults to False
-        assert divider.property('selected') == False
+    # Check divider was created properly
+    assert type(divider) == QtDivider
 
-        # Set divider property
-        divider.setSelected(True)
-        assert divider.property('selected') == True
-        divider.setSelected(False)
-        assert divider.property('selected') == False
+    # Check divider property defaults to False
+    assert divider.property('selected') == False
+
+    # Set divider property
+    divider.setSelected(True)
+    assert divider.property('selected') == True
+    divider.setSelected(False)
+    assert divider.property('selected') == False
 
 
-def test_creating_empty_view():
+def test_creating_empty_view(qtbot):
     """
     Test creating LayerList view.
     """
-    with gui_qt():
-        layers = LayerList()
-        view = QtLayerList(layers)
+    layers = LayerList()
+    view = QtLayerList(layers)
 
-        # Check that the layers model has been appended to the layers view
-        assert view.layers == layers
+    qtbot.addWidget(view)
 
-        # Check that vbox_layout only contains one QtDivider and one spacer
-        assert view.vbox_layout.count() == 2
-        assert check_layout_layers(view.vbox_layout, layers)
-        assert check_layout_dividers(view.vbox_layout, 0)
+    # Check that the layers model has been appended to the layers view
+    assert view.layers == layers
+
+    # Check that vbox_layout only contains one QtDivider and one spacer
+    assert view.vbox_layout.count() == 2
+    assert check_layout_layers(view.vbox_layout, layers)
+    assert check_layout_dividers(view.vbox_layout, 0)
 
 
-def test_adding_layers():
+def test_adding_layers(qtbot):
     """
     Test adding layers.
     """
-    with gui_qt():
-        layers = LayerList()
-        view = QtLayerList(layers)
+    layers = LayerList()
+    view = QtLayerList(layers)
 
-        # Check that new layer and divider get added to vbox_layout
-        layer_a = Image(np.random.random((10, 10)))
-        layers.append(layer_a)
-        assert view.vbox_layout.count() == 2 * (len(layers) + 1)
-        assert check_layout_layers(view.vbox_layout, layers)
-        assert check_layout_dividers(view.vbox_layout, len(layers))
+    qtbot.addWidget(view)
 
-        # Check that new layers and dividers get added to vbox_layout
-        layer_b = Image(np.random.random((15, 15)))
-        layer_c = Image(np.random.random((15, 15)))
-        layer_d = Image(np.random.random((15, 15)))
-        layers.append(layer_b)
-        layers.append(layer_c)
-        layers.append(layer_d)
-        assert view.vbox_layout.count() == 2 * (len(layers) + 1)
-        assert check_layout_layers(view.vbox_layout, layers)
-        assert check_layout_dividers(view.vbox_layout, len(layers))
+    # Check that new layer and divider get added to vbox_layout
+    layer_a = Image(np.random.random((10, 10)))
+    layers.append(layer_a)
+    assert view.vbox_layout.count() == 2 * (len(layers) + 1)
+    assert check_layout_layers(view.vbox_layout, layers)
+    assert check_layout_dividers(view.vbox_layout, len(layers))
+
+    # Check that new layers and dividers get added to vbox_layout
+    layer_b = Image(np.random.random((15, 15)))
+    layer_c = Image(np.random.random((15, 15)))
+    layer_d = Image(np.random.random((15, 15)))
+    layers.append(layer_b)
+    layers.append(layer_c)
+    layers.append(layer_d)
+    assert view.vbox_layout.count() == 2 * (len(layers) + 1)
+    assert check_layout_layers(view.vbox_layout, layers)
+    assert check_layout_dividers(view.vbox_layout, len(layers))
 
 
-def test_removing_layers():
+def test_removing_layers(qtbot):
     """
     Test removing layers.
     """
-    with gui_qt():
-        layers = LayerList()
-        view = QtLayerList(layers)
+    layers = LayerList()
+    view = QtLayerList(layers)
 
-        layer_a = Image(np.random.random((10, 10)))
-        layer_b = Image(np.random.random((15, 15)))
-        layer_c = Image(np.random.random((15, 15)))
-        layer_d = Image(np.random.random((15, 15)))
-        layers.append(layer_a)
-        layers.append(layer_b)
-        layers.append(layer_c)
-        layers.append(layer_d)
+    qtbot.addWidget(view)
 
-        # Check layout and layers list match after removing a layer
-        layers.remove(layer_b)
-        assert view.vbox_layout.count() == 2 * (len(layers) + 1)
-        assert check_layout_layers(view.vbox_layout, layers)
-        assert check_layout_dividers(view.vbox_layout, len(layers))
+    layer_a = Image(np.random.random((10, 10)))
+    layer_b = Image(np.random.random((15, 15)))
+    layer_c = Image(np.random.random((15, 15)))
+    layer_d = Image(np.random.random((15, 15)))
+    layers.append(layer_a)
+    layers.append(layer_b)
+    layers.append(layer_c)
+    layers.append(layer_d)
 
-        # Check layout and layers list match after removing a layer
-        layers.remove(layer_d)
-        assert view.vbox_layout.count() == 2 * (len(layers) + 1)
-        assert check_layout_layers(view.vbox_layout, layers)
-        assert check_layout_dividers(view.vbox_layout, len(layers))
+    # Check layout and layers list match after removing a layer
+    layers.remove(layer_b)
+    assert view.vbox_layout.count() == 2 * (len(layers) + 1)
+    assert check_layout_layers(view.vbox_layout, layers)
+    assert check_layout_dividers(view.vbox_layout, len(layers))
 
-        layers.append(layer_b)
-        layers.append(layer_d)
-        # Select first and third layers
-        for l, s in zip(layers, [True, True, False, False]):
-            l.selected = s
-        layers.remove_selected()
-        assert view.vbox_layout.count() == 2 * (len(layers) + 1)
-        assert check_layout_layers(view.vbox_layout, layers)
-        assert check_layout_dividers(view.vbox_layout, len(layers))
+    # Check layout and layers list match after removing a layer
+    layers.remove(layer_d)
+    assert view.vbox_layout.count() == 2 * (len(layers) + 1)
+    assert check_layout_layers(view.vbox_layout, layers)
+    assert check_layout_dividers(view.vbox_layout, len(layers))
+
+    layers.append(layer_b)
+    layers.append(layer_d)
+    # Select first and third layers
+    for l, s in zip(layers, [True, True, False, False]):
+        l.selected = s
+    layers.remove_selected()
+    assert view.vbox_layout.count() == 2 * (len(layers) + 1)
+    assert check_layout_layers(view.vbox_layout, layers)
+    assert check_layout_dividers(view.vbox_layout, len(layers))
 
 
-def test_reordering_layers():
+def test_reordering_layers(qtbot):
     """
     Test reordering layers.
     """
-    with gui_qt():
-        layers = LayerList()
-        view = QtLayerList(layers)
+    layers = LayerList()
+    view = QtLayerList(layers)
 
-        layer_a = Image(np.random.random((10, 10)), name='image_a')
-        layer_b = Image(np.random.random((15, 15)), name='image_b')
-        layer_c = Image(np.random.random((15, 15)), name='image_c')
-        layer_d = Image(np.random.random((15, 15)), name='image_d')
-        layers.append(layer_a)
-        layers.append(layer_b)
-        layers.append(layer_c)
-        layers.append(layer_d)
+    qtbot.addWidget(view)
 
-        # Check layout and layers list match after rearranging layers
-        layers[:] = layers[(1, 0, 3, 2)]
-        assert view.vbox_layout.count() == 2 * (len(layers) + 1)
-        assert check_layout_layers(view.vbox_layout, layers)
-        assert check_layout_dividers(view.vbox_layout, len(layers))
+    layer_a = Image(np.random.random((10, 10)), name='image_a')
+    layer_b = Image(np.random.random((15, 15)), name='image_b')
+    layer_c = Image(np.random.random((15, 15)), name='image_c')
+    layer_d = Image(np.random.random((15, 15)), name='image_d')
+    layers.append(layer_a)
+    layers.append(layer_b)
+    layers.append(layer_c)
+    layers.append(layer_d)
 
-        # Check layout and layers list match after swapping two layers
-        layers['image_b', 'image_c'] = layers['image_c', 'image_b']
-        assert view.vbox_layout.count() == 2 * (len(layers) + 1)
-        assert check_layout_layers(view.vbox_layout, layers)
-        assert check_layout_dividers(view.vbox_layout, len(layers))
+    # Check layout and layers list match after rearranging layers
+    layers[:] = layers[(1, 0, 3, 2)]
+    assert view.vbox_layout.count() == 2 * (len(layers) + 1)
+    assert check_layout_layers(view.vbox_layout, layers)
+    assert check_layout_dividers(view.vbox_layout, len(layers))
 
-        # Check layout and layers list match after reversing list
-        # TEST CURRENTLY FAILING
-        # layers.reverse()
-        # assert view.vbox_layout.count() == 2 * (len(layers) + 1)
-        # assert check_layout_layers(view.vbox_layout, layers)
-        # assert check_layout_dividers(view.vbox_layout, len(layers))
+    # Check layout and layers list match after swapping two layers
+    layers['image_b', 'image_c'] = layers['image_c', 'image_b']
+    assert view.vbox_layout.count() == 2 * (len(layers) + 1)
+    assert check_layout_layers(view.vbox_layout, layers)
+    assert check_layout_dividers(view.vbox_layout, len(layers))
 
-        # Check layout and layers list match after rearranging selected layers
-        layer_e = Image(np.random.random((15, 15)))
-        layer_f = Image(np.random.random((15, 15)))
-        layers.append(layer_e)
-        layers.append(layer_f)
-        for l, s in zip(layers, [False, True, False, False, True, False]):
-            l.selected = s
-        layers.move_selected(1, 2)
-        assert view.vbox_layout.count() == 2 * (len(layers) + 1)
-        assert check_layout_layers(view.vbox_layout, layers)
-        assert check_layout_dividers(view.vbox_layout, len(layers))
+    # Check layout and layers list match after reversing list
+    # TEST CURRENTLY FAILING
+    # layers.reverse()
+    # assert view.vbox_layout.count() == 2 * (len(layers) + 1)
+    # assert check_layout_layers(view.vbox_layout, layers)
+    # assert check_layout_dividers(view.vbox_layout, len(layers))
+
+    # Check layout and layers list match after rearranging selected layers
+    layer_e = Image(np.random.random((15, 15)))
+    layer_f = Image(np.random.random((15, 15)))
+    layers.append(layer_e)
+    layers.append(layer_f)
+    for l, s in zip(layers, [False, True, False, False, True, False]):
+        l.selected = s
+    layers.move_selected(1, 2)
+    assert view.vbox_layout.count() == 2 * (len(layers) + 1)
+    assert check_layout_layers(view.vbox_layout, layers)
+    assert check_layout_dividers(view.vbox_layout, len(layers))
