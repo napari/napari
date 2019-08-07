16b8ebf972c623b61baa9fabb90c3e31f975f888
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-08-07 02:27:41 -0500

    Add keys to hide/show layers from top

--- napari/viewer.py
+++ napari/viewer.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 from ._qt.qt_viewer import QtViewer
 from ._qt.qt_main_window import Window
 from .components import ViewerModel
@@ -21,7 +22,28 @@ class Viewer(ViewerModel):
 
 
 @Viewer.bind_key('v')
-def toggle_topmost_visible(viewer):
+def toggle_last_visible(viewer):
     if len(viewer.layers) > 0:
         layer = viewer.layers[-1]
         layer.visible = not layer.visible
+
+
+@Viewer.bind_key('[')
+def make_next_invisible_layer_visible(viewer):
+    if len(viewer.layers) == 1:
+        viewer.layers[0].visible = True
+    else:
+        last_layers = viewer.layers[-1:0:-1]
+        layers = viewer.layers[-2::-1]
+        for last_layer, layer in zip_longest(last_layers, layers):
+            if not last_layer.visible and (layer is None or layer.visible):
+                last_layer.visible = True
+                break
+
+
+@Viewer.bind_key(']')
+def make_next_visible_layer_invisible(viewer):
+    for layer in viewer.layers[::-1]:
+        if layer.visible:
+            layer.visible = False
+            break
