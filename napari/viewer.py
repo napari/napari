c324b63e4a37a0cadfc0be2df9f6be6ddce68148
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-10-29 18:06:18 -0500

    Raise informative warning when no Qt loop detected

--- napari/viewer.py
+++ napari/viewer.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from qtpy.QtWidgets import QApplication
 from ._qt.qt_viewer import QtViewer
 from ._qt.qt_main_window import Window
@@ -22,19 +24,29 @@ class Viewer(ViewerModel):
     def __init__(self, title='napari', ndisplay=2, order=None):
         # instance() returns the singleton instance if it exists, or None
         app = QApplication.instance()
-        # if None, we create our own and we execute after creating our object
+        # if None, wait two seconds, as it may still be instantiating when
+        # using %gui qt inside IPython
         if app is None:
-            app = QApplication([])
-            self_execute = True
-        # otherwise, we don't execute and we delete our reference to app
-        else:
-            self_execute = False
-            # appears to be required to not conflict with IPython Qt loop
-            del app
+            sleep(2)
+            app = QApplication.instance()
+            # if still none, raise a RuntimeError with the appropriate message
+            if app is None:
+                message = (
+                    "napari requires a Qt event loop to run. To create one, "
+                    "try one of the following: \n"
+                    "  - use the `napari.gui_qt()` context manager. See "
+                    "https://github.com/napari/napari/tree/master/examples for"
+                    " usage examples.\n"
+                    "  - In IPython or a local Jupyter instance, use the "
+                    "`%gui qt` magic command.\n"
+                    "  - Launch IPython with the option `--gui=qt`.\n"
+                    "  - (recommended) in your IPython configuration file, add"
+                    " or uncomment the line `c.TerminalIPythonApp.gui = 'qt'`."
+                    " Then, restart IPython."
+                )
+                raise RuntimeError(message)
         super().__init__(title=title, ndisplay=ndisplay, order=order)
         qt_viewer = QtViewer(self)
         self.window = Window(qt_viewer)
         self.screenshot = self.window.qt_viewer.screenshot
         self.update_console = self.window.qt_viewer.console.push
-        if self_execute:
-            app.exec_()
