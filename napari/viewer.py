590de00d583db15578507ee32fc1388d0f30b7b0
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-10-29 03:23:07 -0500

    Create a QApplication in QtViewer if one not present

--- napari/viewer.py
+++ napari/viewer.py
@@ -1,3 +1,4 @@
+from qtpy.QtWidgets import QApplication
 from ._qt.qt_viewer import QtViewer
 from ._qt.qt_main_window import Window
 from .components import ViewerModel
@@ -19,8 +20,21 @@ class Viewer(ViewerModel):
     """
 
     def __init__(self, title='napari', ndisplay=2, order=None):
+        # instance() returns the singleton instance if it exists, or None
+        app = QApplication.instance()
+        # if None, we create our own and we execute after creating our object
+        if app is None:
+            app = QApplication([])
+            self_execute = True
+        # otherwise, we don't execute and we delete our reference to app
+        else:
+            self_execute = False
+            # appears to be required to not conflict with IPython Qt loop
+            del app
         super().__init__(title=title, ndisplay=ndisplay, order=order)
         qt_viewer = QtViewer(self)
         self.window = Window(qt_viewer)
         self.screenshot = self.window.qt_viewer.screenshot
         self.update_console = self.window.qt_viewer.console.push
+        if self_execute:
+            app.exec_()
