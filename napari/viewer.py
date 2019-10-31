e686954df6a45de20b1cb2f212e2239b8e98dbb4
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-10-30 19:13:09 -0500

    Remove non-functional sleep trick

--- napari/viewer.py
+++ napari/viewer.py
@@ -24,27 +24,22 @@ class Viewer(ViewerModel):
     def __init__(self, title='napari', ndisplay=2, order=None):
         # instance() returns the singleton instance if it exists, or None
         app = QApplication.instance()
-        # if None, wait two seconds, as it may still be instantiating when
-        # using %gui qt inside IPython
+        # if None, raise a RuntimeError with the appropriate message
         if app is None:
-            sleep(2)
-            app = QApplication.instance()
-            # if still none, raise a RuntimeError with the appropriate message
-            if app is None:
-                message = (
-                    "napari requires a Qt event loop to run. To create one, "
-                    "try one of the following: \n"
-                    "  - use the `napari.gui_qt()` context manager. See "
-                    "https://github.com/napari/napari/tree/master/examples for"
-                    " usage examples.\n"
-                    "  - In IPython or a local Jupyter instance, use the "
-                    "`%gui qt` magic command.\n"
-                    "  - Launch IPython with the option `--gui=qt`.\n"
-                    "  - (recommended) in your IPython configuration file, add"
-                    " or uncomment the line `c.TerminalIPythonApp.gui = 'qt'`."
-                    " Then, restart IPython."
-                )
-                raise RuntimeError(message)
+            message = (
+                "napari requires a Qt event loop to run. To create one, "
+                "try one of the following: \n"
+                "  - use the `napari.gui_qt()` context manager. See "
+                "https://github.com/napari/napari/tree/master/examples for"
+                " usage examples.\n"
+                "  - In IPython or a local Jupyter instance, use the "
+                "`%gui qt` magic command.\n"
+                "  - Launch IPython with the option `--gui=qt`.\n"
+                "  - (recommended) in your IPython configuration file, add"
+                " or uncomment the line `c.TerminalIPythonApp.gui = 'qt'`."
+                " Then, restart IPython."
+            )
+            raise RuntimeError(message)
         super().__init__(title=title, ndisplay=ndisplay, order=order)
         qt_viewer = QtViewer(self)
         self.window = Window(qt_viewer)
