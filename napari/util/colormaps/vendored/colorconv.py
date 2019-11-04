dc1fa4fef1f36b21785ec5ca00cf6f624373dc0c
Talley Lambert <talley.lambert@gmail.com>
2019-11-03 20:20:59 -0500

    undo vendored changes

--- napari/util/colormaps/vendored/colorconv.py
+++ napari/util/colormaps/vendored/colorconv.py
@@ -93,12 +93,8 @@ def _prepare_colorarray(arr):
     arr = np.asanyarray(arr)
 
     if arr.shape[-1] != 3:
-        msg = (
-            "the input array must be have a shape == ([ ..,] 3)), "
-            + "got ("
-            + (", ".join(map(str, arr.shape)))
-            + ")"
-        )
+        msg = ("the input array must be have a shape == ([ ..,] 3)), " +
+               "got (" + (", ".join(map(str, arr.shape))) + ")")
         raise ValueError(msg)
 
     return arr.astype(float)
@@ -112,10 +108,8 @@ def _prepare_rgba_array(arr):
     arr = np.asanyarray(arr)
 
     if arr.shape[-1] != 4:
-        msg = (
-            "the input array must have a shape == ([ ..,] 4)), "
-            "got {0}".format(arr.shape)
-        )
+        msg = ("the input array must have a shape == ([ ..,] 4)), "
+               "got {0}".format(arr.shape))
         raise ValueError(msg)
 
     return arr.astype(float)
@@ -155,12 +149,9 @@ def rgba2rgb(rgba, background=(1, 1, 1)):
     """
     arr = _prepare_rgba_array(rgba)
     if isinstance(background, tuple) and len(background) != 3:
-        raise ValueError(
-            'the background must be a tuple with 3 items - the '
-            'RGB color of the background. Got {0} items.'.format(
-                len(background)
-            )
-        )
+        raise ValueError('the background must be a tuple with 3 items - the '
+                         'RGB color of the background. Got {0} items.'
+                         .format(len(background)))
 
     alpha = arr[..., -1]
     channels = arr[..., :-1]
@@ -169,9 +160,7 @@ def rgba2rgb(rgba, background=(1, 1, 1)):
     for ichan in range(channels.shape[-1]):
         out[..., ichan] = np.clip(
             (1 - alpha) * background[ichan] + alpha * channels[..., ichan],
-            a_min=0,
-            a_max=1,
-        )
+            a_min=0, a_max=1)
     return out
 
 
@@ -220,22 +209,22 @@ def rgb2hsv(rgb):
     # Ignore warning for zero divided by zero
     old_settings = np.seterr(invalid='ignore')
     out_s = delta / out_v
-    out_s[delta == 0.0] = 0.0
+    out_s[delta == 0.] = 0.
 
     # -- H channel
     # red is max
-    idx = arr[:, :, 0] == out_v
+    idx = (arr[:, :, 0] == out_v)
     out[idx, 0] = (arr[idx, 1] - arr[idx, 2]) / delta[idx]
 
     # green is max
-    idx = arr[:, :, 1] == out_v
-    out[idx, 0] = 2.0 + (arr[idx, 2] - arr[idx, 0]) / delta[idx]
+    idx = (arr[:, :, 1] == out_v)
+    out[idx, 0] = 2. + (arr[idx, 2] - arr[idx, 0]) / delta[idx]
 
     # blue is max
-    idx = arr[:, :, 2] == out_v
-    out[idx, 0] = 4.0 + (arr[idx, 0] - arr[idx, 1]) / delta[idx]
-    out_h = (out[:, :, 0] / 6.0) % 1.0
-    out_h[delta == 0.0] = 0.0
+    idx = (arr[:, :, 2] == out_v)
+    out[idx, 0] = 4. + (arr[idx, 0] - arr[idx, 1]) / delta[idx]
+    out_h = (out[:, :, 0] / 6.) % 1.
+    out_h[delta == 0.] = 0.
 
     np.seterr(**old_settings)
 
@@ -294,17 +283,12 @@ def hsv2rgb(hsv):
     v = arr[:, :, 2]
 
     hi = np.dstack([hi, hi, hi]).astype(np.uint8) % 6
-    out = np.choose(
-        hi,
-        [
-            np.dstack((v, t, p)),
-            np.dstack((q, v, p)),
-            np.dstack((p, v, t)),
-            np.dstack((p, q, v)),
-            np.dstack((t, p, v)),
-            np.dstack((v, p, q)),
-        ],
-    )
+    out = np.choose(hi, [np.dstack((v, t, p)),
+                         np.dstack((q, v, p)),
+                         np.dstack((p, v, t)),
+                         np.dstack((p, q, v)),
+                         np.dstack((t, p, v)),
+                         np.dstack((v, p, q))])
 
     return out
 
@@ -313,31 +297,24 @@ def hsv2rgb(hsv):
 # Primaries for the coordinate systems
 # ---------------------------------------------------------------
 cie_primaries = np.array([700, 546.1, 435.8])
-sb_primaries = np.array([1.0 / 155, 1.0 / 190, 1.0 / 225]) * 1e5
+sb_primaries = np.array([1. / 155, 1. / 190, 1. / 225]) * 1e5
 
 # ---------------------------------------------------------------
 # Matrices that define conversion between different color spaces
 # ---------------------------------------------------------------
 
 # From sRGB specification
-xyz_from_rgb = np.array(
-    [
-        [0.412453, 0.357580, 0.180423],
-        [0.212671, 0.715160, 0.072169],
-        [0.019334, 0.119193, 0.950227],
-    ]
-)
+xyz_from_rgb = np.array([[0.412453, 0.357580, 0.180423],
+                         [0.212671, 0.715160, 0.072169],
+                         [0.019334, 0.119193, 0.950227]])
 
 rgb_from_xyz = linalg.inv(xyz_from_rgb)
 
 # From https://en.wikipedia.org/wiki/CIE_1931_color_space
 # Note: Travis's code did not have the divide by 0.17697
-xyz_from_rgbcie = (
-    np.array(
-        [[0.49, 0.31, 0.20], [0.17697, 0.81240, 0.01063], [0.00, 0.01, 0.99]]
-    )
-    / 0.17697
-)
+xyz_from_rgbcie = np.array([[0.49, 0.31, 0.20],
+                            [0.17697, 0.81240, 0.01063],
+                            [0.00, 0.01, 0.99]]) / 0.17697
 
 rgbcie_from_xyz = linalg.inv(xyz_from_rgbcie)
 
@@ -346,22 +323,20 @@ rgbcie_from_rgb = rgbcie_from_xyz @ xyz_from_rgb
 rgb_from_rgbcie = rgb_from_xyz @ xyz_from_rgbcie
 
 
-gray_from_rgb = np.array([[0.2125, 0.7154, 0.0721], [0, 0, 0], [0, 0, 0]])
+gray_from_rgb = np.array([[0.2125, 0.7154, 0.0721],
+                          [0, 0, 0],
+                          [0, 0, 0]])
 
-yuv_from_rgb = np.array(
-    [
-        [0.299, 0.587, 0.114],
-        [-0.14714119, -0.28886916, 0.43601035],
-        [0.61497538, -0.51496512, -0.10001026],
-    ]
-)
+yuv_from_rgb = np.array([[ 0.299     ,  0.587     ,  0.114      ],
+                         [-0.14714119, -0.28886916,  0.43601035 ],
+                         [ 0.61497538, -0.51496512, -0.10001026 ]])
 
 rgb_from_yuv = linalg.inv(yuv_from_rgb)
 
 
 # CIE LAB constants for Observer=2A, Illuminant=D65
 # NOTE: this is actually the XYZ values for the illuminant above.
-lab_ref_white = np.array([0.95047, 1.0, 1.08883])
+lab_ref_white = np.array([0.95047, 1., 1.08883])
 
 # XYZ coordinates of the illuminants, scaled to [0, 1]. For each illuminant I
 # we have:
@@ -386,29 +361,19 @@ lab_ref_white = np.array([0.95047, 1.0, 1.08883])
 #    ----------
 #    .. [1] https://en.wikipedia.org/wiki/Standard_illuminant
 
-illuminants = {
-    "A": {
-        '2': (1.098466069456375, 1, 0.3558228003436005),
-        '10': (1.111420406956693, 1, 0.3519978321919493),
-    },
-    "D50": {
-        '2': (0.9642119944211994, 1, 0.8251882845188288),
-        '10': (0.9672062750333777, 1, 0.8142801513128616),
-    },
-    "D55": {
-        '2': (0.956797052643698, 1, 0.9214805860173273),
-        '10': (0.9579665682254781, 1, 0.9092525159847462),
-    },
-    "D65": {
-        '2': (0.95047, 1.0, 1.08883),  # This was: `lab_ref_white`
-        '10': (0.94809667673716, 1, 1.0730513595166162),
-    },
-    "D75": {
-        '2': (0.9497220898840717, 1, 1.226393520724154),
-        '10': (0.9441713925645873, 1, 1.2064272211720228),
-    },
-    "E": {'2': (1.0, 1.0, 1.0), '10': (1.0, 1.0, 1.0)},
-}
+illuminants = \
+    {"A": {'2': (1.098466069456375, 1, 0.3558228003436005),
+           '10': (1.111420406956693, 1, 0.3519978321919493)},
+     "D50": {'2': (0.9642119944211994, 1, 0.8251882845188288),
+             '10': (0.9672062750333777, 1, 0.8142801513128616)},
+     "D55": {'2': (0.956797052643698, 1, 0.9214805860173273),
+             '10': (0.9579665682254781, 1, 0.9092525159847462)},
+     "D65": {'2': (0.95047, 1., 1.08883),   # This was: `lab_ref_white`
+             '10': (0.94809667673716, 1, 1.0730513595166162)},
+     "D75": {'2': (0.9497220898840717, 1, 1.226393520724154),
+             '10': (0.9441713925645873, 1, 1.2064272211720228)},
+     "E": {'2': (1.0, 1.0, 1.0),
+           '10': (1.0, 1.0, 1.0)}}
 
 
 def get_xyz_coords(illuminant, observer):
@@ -442,13 +407,8 @@ def get_xyz_coords(illuminant, observer):
     try:
         return illuminants[illuminant][observer]
     except KeyError:
-        raise ValueError(
-            "Unknown illuminant/observer combination\
-        (\'{0}\', \'{1}\')".format(
-                illuminant, observer
-            )
-        )
-
+        raise ValueError("Unknown illuminant/observer combination\
+        (\'{0}\', \'{1}\')".format(illuminant, observer))
 
 # -------------------------------------------------------------
 # The conversion functions that make use of the matrices above
@@ -738,12 +698,12 @@ def xyz2lab(xyz, illuminant="D65", observer="2"):
     # Nonlinear distortion and linear transformation
     mask = arr > 0.008856
     arr[mask] = np.cbrt(arr[mask])
-    arr[~mask] = 7.787 * arr[~mask] + 16.0 / 116.0
+    arr[~mask] = 7.787 * arr[~mask] + 16. / 116.
 
     x, y, z = arr[..., 0], arr[..., 1], arr[..., 2]
 
     # Vector scaling
-    L = (116.0 * y) - 16.0
+    L = (116. * y) - 16.
     a = 500.0 * (x - y)
     b = 200.0 * (y - z)
 
@@ -794,15 +754,15 @@ def lab2xyz(lab, illuminant="D65", observer="2"):
     arr = _prepare_colorarray(lab).copy()
 
     L, a, b = arr[..., 0], arr[..., 1], arr[..., 2]
-    y = (L + 16.0) / 116.0
-    x = (a / 500.0) + y
-    z = y - (b / 200.0)
+    y = (L + 16.) / 116.
+    x = (a / 500.) + y
+    z = y - (b / 200.)
 
     out = np.stack([x, y, z], axis=-1)
 
     mask = out > 0.2068966
-    out[mask] = np.power(out[mask], 3.0)
-    out[~mask] = (out[~mask] - 16.0 / 116.0) / 7.787
+    out[mask] = np.power(out[mask], 3.)
+    out[~mask] = (out[~mask] - 16.0 / 116.) / 7.787
 
     # rescale to the reference white (illuminant)
     xyz_ref_white = get_xyz_coords(illuminant, observer)
@@ -941,7 +901,7 @@ def xyz2luv(xyz, illuminant="D65", observer="2"):
     xyz_ref_white = np.array(get_xyz_coords(illuminant, observer))
     L = y / xyz_ref_white[1]
     mask = L > 0.008856
-    L[mask] = 116.0 * np.cbrt(L[mask]) - 16.0
+    L[mask] = 116. * np.cbrt(L[mask]) - 16.
     L[~mask] = 903.3 * L[~mask]
 
     u0 = 4 * xyz_ref_white[0] / ([1, 15, 3] @ xyz_ref_white)
@@ -949,14 +909,14 @@ def xyz2luv(xyz, illuminant="D65", observer="2"):
 
     # u' and v' helper functions
     def fu(X, Y, Z):
-        return (4.0 * X) / (X + 15.0 * Y + 3.0 * Z + eps)
+        return (4. * X) / (X + 15. * Y + 3. * Z + eps)
 
     def fv(X, Y, Z):
-        return (9.0 * Y) / (X + 15.0 * Y + 3.0 * Z + eps)
+        return (9. * Y) / (X + 15. * Y + 3. * Z + eps)
 
     # compute u and v using helper functions
-    u = 13.0 * L * (fu(x, y, z) - u0)
-    v = 13.0 * L * (fv(x, y, z) - v0)
+    u = 13. * L * (fu(x, y, z) - u0)
+    v = 13. * L * (fv(x, y, z) - v0)
 
     return np.concatenate([q[..., np.newaxis] for q in [L, u, v]], axis=-1)
 
@@ -1009,7 +969,7 @@ def luv2xyz(luv, illuminant="D65", observer="2"):
     # compute y
     y = L.copy()
     mask = y > 7.999625
-    y[mask] = np.power((y[mask] + 16.0) / 116.0, 3.0)
+    y[mask] = np.power((y[mask] + 16.) / 116., 3.)
     y[~mask] = y[~mask] / 903.3
     xyz_ref_white = get_xyz_coords(illuminant, observer)
     y *= xyz_ref_white[1]
@@ -1020,13 +980,13 @@ def luv2xyz(luv, illuminant="D65", observer="2"):
     v0 = 9 * xyz_ref_white[1] / (uv_weights @ xyz_ref_white)
 
     # compute intermediate values
-    a = u0 + u / (13.0 * L + eps)
-    b = v0 + v / (13.0 * L + eps)
+    a = u0 + u / (13. * L + eps)
+    b = v0 + v / (13. * L + eps)
     c = 3 * y * (5 * b - 3)
 
     # compute x and z
     z = ((a - 4) * c - 15 * a * b * y) / (12 * b)
-    x = -(c / b + 3.0 * z)
+    x = -(c / b + 3. * z)
 
     return np.stack([x, y, z], axis=-1)
 
