fdcf366f99fedbfdb4446a63afed43ef76a04be1
Talley Lambert <talley.lambert@gmail.com>
2019-11-08 15:29:22 -0500

    click  background and drag moves slider too

--- napari/_qt/qt_scrollbar.py
+++ napari/_qt/qt_scrollbar.py
@@ -14,45 +14,55 @@ class ModifiedScrollBar(QScrollBar):
     fully to the clicked position.
     """
 
+    def _move_to_mouse_position(self, event):
+        opt = QStyleOptionSlider()
+        self.initStyleOption(opt)
+        control = self.style().hitTestComplexControl(
+            QStyle.CC_ScrollBar, opt, event.pos(), self
+        )
+        if (
+            control == QStyle.SC_ScrollBarAddPage
+            or control == QStyle.SC_ScrollBarSubPage
+        ):
+            # scroll here
+            gr = self.style().subControlRect(
+                QStyle.CC_ScrollBar, opt, QStyle.SC_ScrollBarGroove, self
+            )
+            sr = self.style().subControlRect(
+                QStyle.CC_ScrollBar, opt, QStyle.SC_ScrollBarSlider, self
+            )
+            if self.orientation() == Qt.Horizontal:
+                pos = event.pos().x()
+                sliderLength = sr.width()
+                sliderMin = gr.x()
+                sliderMax = gr.right() - sliderLength + 1
+                if self.layoutDirection() == Qt.RightToLeft:
+                    opt.upsideDown = not opt.upsideDown
+            else:
+                pos = event.pos().y()
+                sliderLength = sr.height()
+                sliderMin = gr.y()
+                sliderMax = gr.bottom() - sliderLength + 1
+            self.setValue(
+                QStyle.sliderValueFromPosition(
+                    self.minimum(),
+                    self.maximum(),
+                    pos - sliderMin - sliderLength // 2,
+                    sliderMax - sliderMin,
+                    opt.upsideDown,
+                )
+            )
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            # dragging with the mouse button down should move the slider
+            self._move_to_mouse_position(event)
+            return
+        return super().mouseMoveEvent(event)
+
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
-            opt = QStyleOptionSlider()
-            self.initStyleOption(opt)
-            control = self.style().hitTestComplexControl(
-                QStyle.CC_ScrollBar, opt, event.pos(), self
-            )
-            if (
-                control == QStyle.SC_ScrollBarAddPage
-                or control == QStyle.SC_ScrollBarSubPage
-            ):
-                # scroll here
-                gr = self.style().subControlRect(
-                    QStyle.CC_ScrollBar, opt, QStyle.SC_ScrollBarGroove, self
-                )
-                sr = self.style().subControlRect(
-                    QStyle.CC_ScrollBar, opt, QStyle.SC_ScrollBarSlider, self
-                )
-                if self.orientation() == Qt.Horizontal:
-                    pos = event.pos().x()
-                    sliderLength = sr.width()
-                    sliderMin = gr.x()
-                    sliderMax = gr.right() - sliderLength + 1
-                    if self.layoutDirection() == Qt.RightToLeft:
-                        opt.upsideDown = not opt.upsideDown
-                else:
-                    pos = event.pos().y()
-                    sliderLength = sr.height()
-                    sliderMin = gr.y()
-                    sliderMax = gr.bottom() - sliderLength + 1
-                self.setValue(
-                    QStyle.sliderValueFromPosition(
-                        self.minimum(),
-                        self.maximum(),
-                        pos - sliderMin - sliderLength // 2,
-                        sliderMax - sliderMin,
-                        opt.upsideDown,
-                    )
-                )
-                return
-
-        return super(ModifiedScrollBar, self).mousePressEvent(event)
+            # clicking the mouse button should move slider to the clicked point
+            self._move_to_mouse_position(event)
+            return
+        return super().mousePressEvent(event)
