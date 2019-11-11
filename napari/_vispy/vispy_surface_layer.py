d305332d04bd8aab3bf85e5a68ff336886788b5b
sofroniewn <sofroniewn@gmail.com>
2019-11-11 11:50:53 -0800

    simplify surface vispy

--- napari/_vispy/vispy_surface_layer.py
+++ napari/_vispy/vispy_surface_layer.py
@@ -25,29 +25,10 @@ class VispySurfaceLayer(VispyBaseLayer):
         )
         self.layer.events.gamma.connect(lambda e: self._on_gamma_change())
 
-        self._on_display_change()
+        self.reset()
         self._on_data_change()
 
-    def _on_display_change(self):
-        self.reset()
-        for b in range(self.layer.dims.ndisplay):
-            self.node.bounds(b)
-
     def _on_data_change(self):
-        # Check if ndisplay has changed current node type needs updating
-        if (
-            (self.node.mesh_data.get_bounds() is None)
-            or (
-                self.layer.dims.ndisplay == 3
-                and not len(self.node.mesh_data.get_bounds()) == 3
-            )
-            or (
-                self.layer.dims.ndisplay == 2
-                and not len(self.node.mesh_data.get_bounds()) == 2
-            )
-        ):
-            self._on_display_change()
-
         if len(self.layer._data_view) == 0 or len(self.layer._view_faces) == 0:
             vertices = None
             faces = None
