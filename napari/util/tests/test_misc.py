908b8ce472e807b040a35fb21aa05d8227b6be56
nhpatterson <nheathpatterson@gmail.com>
2019-11-26 15:26:35 -0500

    add dask based test for pyramid, fix formatting

--- napari/util/tests/test_misc.py
+++ napari/util/tests/test_misc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import dask.array as da
 from skimage.transform import pyramid_gaussian
 from napari.util.misc import (
     is_rgb,
@@ -97,9 +98,9 @@ def test_is_pyramid():
         tuple(pyramid_gaussian(np.random.random((10, 15)), multichannel=False))
     )
     assert is_pyramid(data)
-    
-    s = int(np.sqrt(2**32) + 2)
-    data = [np.zeros((s,) * 2,dtype=np.bool_),np.zeros((s//2,) * 2,dtype=np.bool_),np.zeros((s//4,) * 2,dtype=np.bool_)]
+
+    s = 8192
+    data = [da.ones((s,) * 3), da.ones((s // 2,) * 3), da.ones((s // 4,) * 3)]
     assert is_pyramid(data)
 
 
@@ -241,58 +242,58 @@ def test_get_pyramid_and_rgb():
 
 def test_callsignature():
     # no arguments
-    assert str(callsignature(lambda: None)) == '()'
+    assert str(callsignature(lambda: None)) == "()"
 
     # one arg
-    assert str(callsignature(lambda a: None)) == '(a)'
+    assert str(callsignature(lambda a: None)) == "(a)"
 
     # multiple args
-    assert str(callsignature(lambda a, b: None)) == '(a, b)'
+    assert str(callsignature(lambda a, b: None)) == "(a, b)"
 
     # arbitrary args
-    assert str(callsignature(lambda *args: None)) == '(*args)'
+    assert str(callsignature(lambda *args: None)) == "(*args)"
 
     # arg + arbitrary args
-    assert str(callsignature(lambda a, *az: None)) == '(a, *az)'
+    assert str(callsignature(lambda a, *az: None)) == "(a, *az)"
 
     # default arg
-    assert str(callsignature(lambda a=42: None)) == '(a=a)'
+    assert str(callsignature(lambda a=42: None)) == "(a=a)"
 
     # multiple default args
-    assert str(callsignature(lambda a=0, b=1: None)) == '(a=a, b=b)'
+    assert str(callsignature(lambda a=0, b=1: None)) == "(a=a, b=b)"
 
     # arg + default arg
-    assert str(callsignature(lambda a, b=42: None)) == '(a, b=b)'
+    assert str(callsignature(lambda a, b=42: None)) == "(a, b=b)"
 
     # arbitrary kwargs
-    assert str(callsignature(lambda **kwargs: None)) == '(**kwargs)'
+    assert str(callsignature(lambda **kwargs: None)) == "(**kwargs)"
 
     # default arg + arbitrary kwargs
-    assert str(callsignature(lambda a=42, **kwargs: None)) == '(a=a, **kwargs)'
+    assert str(callsignature(lambda a=42, **kwargs: None)) == "(a=a, **kwargs)"
 
     # arg + default arg + arbitrary kwargs
-    assert str(callsignature(lambda a, b=42, **kw: None)) == '(a, b=b, **kw)'
+    assert str(callsignature(lambda a, b=42, **kw: None)) == "(a, b=b, **kw)"
 
     # arbitary args + arbitrary kwargs
-    assert str(callsignature(lambda *args, **kw: None)) == '(*args, **kw)'
+    assert str(callsignature(lambda *args, **kw: None)) == "(*args, **kw)"
 
     # arg + default arg + arbitrary kwargs
     assert (
         str(callsignature(lambda a, b=42, *args, **kwargs: None))
-        == '(a, b=b, *args, **kwargs)'
+        == "(a, b=b, *args, **kwargs)"
     )
 
     # kwonly arg
-    assert str(callsignature(lambda *, a: None)) == '(a=a)'
+    assert str(callsignature(lambda *, a: None)) == "(a=a)"
 
     # arg + kwonly arg
-    assert str(callsignature(lambda a, *, b: None)) == '(a, b=b)'
+    assert str(callsignature(lambda a, *, b: None)) == "(a, b=b)"
 
     # default arg + kwonly arg
-    assert str(callsignature(lambda a=42, *, b: None)) == '(a=a, b=b)'
+    assert str(callsignature(lambda a=42, *, b: None)) == "(a=a, b=b)"
 
     # kwonly args + everything
     assert (
         str(callsignature(lambda a, b=42, *, c, d=5, **kwargs: None))
-        == '(a, b=b, c=c, d=d, **kwargs)'
+        == "(a, b=b, c=c, d=d, **kwargs)"
     )
