f6148e4752a0c0e812d4171cbac06aa72d4b249f
sofroniewn <sofroniewn@gmail.com>
2019-11-30 16:53:13 -0800

    minor edit

--- benchmarks/benchmark_qt_viewer_image.py
+++ benchmarks/benchmark_qt_viewer_image.py
@@ -7,40 +7,6 @@ import napari
 from qtpy.QtWidgets import QApplication
 
 
-class QtViewerSingleImage:
-    """Benchmarks for editing a single image layer in the viewer."""
-
-    def setup(self):
-        _ = QApplication.instance() or QApplication([])
-        np.random.seed(0)
-        self.data = np.random.random((128, 128, 128))
-        self.new_data = np.random.random((128, 128, 128))
-        self.viewer = napari.view_image(self.data)
-
-    def teardown(self):
-        self.viewer.window.close()
-
-    def time_set_data(self):
-        """Time to set view slice."""
-        self.viewer.layers[0].data = self.new_data
-
-    def time_set_view_slice(self):
-        """Time to set view slice."""
-        self.viewer.layers[0]._set_view_slice()
-
-    def time_update_thumbnail(self):
-        """Time to update thumbnail."""
-        self.viewer.layers[0]._update_thumbnail()
-
-    def time_get_value(self):
-        """Time to get current value."""
-        self.viewer.layers[0].get_value()
-
-    def time_ndisplay(self):
-        """Time to enter 3D rendering."""
-        self.viewer.dims.ndisplay = 3
-
-
 class QtViewerViewImageSuite:
     """Benchmarks for viewing images in the viewer."""
 
@@ -104,3 +70,37 @@ class QtViewerEditImageSuite:
     def time_get_value(self, n):
         """Time to get current value."""
         self.viewer.layers[0].get_value()
+
+
+class QtViewerSingleImage:
+    """Benchmarks for editing a single image layer in the viewer."""
+
+    def setup(self):
+        _ = QApplication.instance() or QApplication([])
+        np.random.seed(0)
+        self.data = np.random.random((128, 128, 128))
+        self.new_data = np.random.random((128, 128, 128))
+        self.viewer = napari.view_image(self.data)
+
+    def teardown(self):
+        self.viewer.window.close()
+
+    def time_set_data(self):
+        """Time to set view slice."""
+        self.viewer.layers[0].data = self.new_data
+
+    def time_set_view_slice(self):
+        """Time to set view slice."""
+        self.viewer.layers[0]._set_view_slice()
+
+    def time_update_thumbnail(self):
+        """Time to update thumbnail."""
+        self.viewer.layers[0]._update_thumbnail()
+
+    def time_get_value(self):
+        """Time to get current value."""
+        self.viewer.layers[0].get_value()
+
+    def time_ndisplay(self):
+        """Time to enter 3D rendering."""
+        self.viewer.dims.ndisplay = 3
