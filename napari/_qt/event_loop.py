37fda86f44ac530b3c65e294d1917267af1ba159
sofroniewn <sofroniewn@gmail.com>
2019-12-01 12:16:04 -0800

    add keyword arg

--- napari/_qt/event_loop.py
+++ napari/_qt/event_loop.py
@@ -7,8 +7,13 @@ from qtpy.QtWidgets import QApplication, QSplashScreen
 
 
 @contextmanager
-def gui_qt():
+def gui_qt(splash_screen=True):
     """Start a Qt event loop in which to run the application.
+
+    Parameters
+    ----------
+    splash_screen : bool
+        Show a splash screen with the napari logo during startup.
 
     Notes
     -----
@@ -18,9 +23,11 @@ def gui_qt():
     ``ipython --gui=qt``.
     """
     app = QApplication.instance() or QApplication(sys.argv)
-    logopath = join(dirname(__file__), '..', 'resources', 'logo.png')
-    splash_screen = QSplashScreen(QPixmap(logopath).scaled(400, 400))
-    splash_screen.show()
+    if splash_screen:
+        logopath = join(dirname(__file__), '..', 'resources', 'logo.png')
+        splash_widget = QSplashScreen(QPixmap(logopath).scaled(400, 400))
+        splash_widget.show()
     yield
-    splash_screen.close()
+    if splash_screen:
+        splash_widget.close()
     app.exec_()
