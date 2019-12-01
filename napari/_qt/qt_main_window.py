a6aad78d0e0b1ee5abfd70fd7ecfc55653a4a65d
sofroniewn <sofroniewn@gmail.com>
2019-11-30 19:36:08 -0800

    drop old

--- napari/_qt/qt_main_window.py
+++ napari/_qt/qt_main_window.py
@@ -169,14 +169,6 @@ class Window:
         )
         self.help_menu.addAction(about_action)
 
-        # keybidings_action = QAction("keybindings", self._qt_window)
-        # keybidings_action.setShortcut("Ctrl+/")
-        # keybidings_action.setStatusTip('About keybindings')
-        # keybidings_action.triggered.connect(
-        #     lambda e: QtAboutKeybindings.showAbout(self.qt_viewer)
-        # )
-        # self.help_menu.addAction(keybidings_action)
-
     def add_dock_widget(
         self,
         widget: QWidget,
