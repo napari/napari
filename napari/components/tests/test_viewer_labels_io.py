aee47f20db6e3f485b306dc617a91ce8616f23ee
sofroniewn <sofroniewn@gmail.com>
2019-12-02 08:50:16 -0800

    parameterize tests

--- napari/components/tests/test_viewer_labels_io.py
+++ napari/components/tests/test_viewer_labels_io.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from skimage.data import binary_blobs
 from imageio import imwrite
@@ -6,23 +7,13 @@ from tempfile import NamedTemporaryFile
 from napari.components import ViewerModel
 
 
-def test_png_labels():
+@pytest.mark.parametrize('suffix', ['.png', '.tiff'])
+def test_open_labels(suffix):
     viewer = ViewerModel()
     blobs = binary_blobs(length=128, volume_fraction=0.1, n_dim=2)
     labeled = ndi.label(blobs)[0].astype(np.uint8)
-    with NamedTemporaryFile(suffix='.png') as fout:
-        imwrite(fout, labeled, format='png')
-        viewer.add_labels(path=fout.name)
-        assert len(viewer.layers) == 1
-        assert np.all(labeled == viewer.layers[0].data)
-
-
-def test_tiff_labels():
-    viewer = ViewerModel()
-    blobs = binary_blobs(length=128, volume_fraction=0.1, n_dim=2)
-    labeled = ndi.label(blobs)[0].astype(np.uint8)
-    with NamedTemporaryFile(suffix='.tiff') as fout:
-        imwrite(fout, labeled, format='tiff')
+    with NamedTemporaryFile(suffix=suffix) as fout:
+        imwrite(fout, labeled, format=suffix)
         viewer.add_labels(path=fout.name)
         assert len(viewer.layers) == 1
         assert np.all(labeled == viewer.layers[0].data)
