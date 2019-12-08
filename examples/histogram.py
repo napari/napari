acd6b04627afbb665c95da14a02139ecd285f380
Talley Lambert <talley.lambert@gmail.com>
2019-12-08 15:53:36 -0500

    tweak histogram example

--- examples/histogram.py
+++ examples/histogram.py
@@ -1,13 +1,15 @@
 from napari._qt.qt_histogram import QtHistogramWidget
 import napari
-import numpy as np
+from scipy import stats
 
 
 with napari.gui_qt():
     v = napari.Viewer()
-    v.add_image(np.random.randn(10, 256, 256))
-    w = QtHistogramWidget(viewer=v)
-    dw = v.window.add_dock_widget(w, area='bottom')
+    v.add_image(
+        stats.distributions.gamma.rvs(3, 20, 1, 256 ** 2).reshape(256, 256)
+    )
+    w = QtHistogramWidget(viewer=v, vertical=True)
+    dw = v.window.add_dock_widget(w, area='right')
     # w2 = QtHistogramWidget(viewer=v, vertical=True)
     # v.window.add_dock_widget('right', w2)
 
