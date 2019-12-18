01419c263108a4dc1a3735515801018ad9bc3114
Talley Lambert <talley.lambert@gmail.com>
2019-12-17 20:57:19 -0500

    update comment, change logic

--- napari/_qt/qt_main_window.py
+++ napari/_qt/qt_main_window.py
@@ -274,16 +274,18 @@ class Window:
         """Resize, show, and bring forward the window."""
         self._qt_window.resize(self._qt_window.layout().sizeHint())
         self._qt_window.show()
-        # make sure window is not hidden, e.g. by browser window in Jupyter
-        # if the application was instantiated in gui_qt(), app_name will be
-        # "napari".  If it is the second time a _qt_window has been created,
-        # then isActiveWindow() will be True (otherwise false).
-        # this is the only combination that seems to fix #732
+
+        # We want to call Window._qt_window.raise_() in every case *except*
+        # when instantiating a viewer within a gui_qt() context for the
+        # _first_ time within the Qt app's lifecycle.
+        #
+        # `app_name` will be "napari" iff the application was instantiated in
+        # gui_qt(). isActiveWindow() will be True if it is the second time a
+        # _qt_window has been created. See #732
         app_name = QApplication.instance().applicationName()
-        if app_name == 'napari' and not self._qt_window.isActiveWindow():
-            return
-        self._qt_window.raise_()  # for MacOS
-        self._qt_window.activateWindow()  # for Windows
+        if app_name != 'napari' or self._qt_window.isActiveWindow():
+            self._qt_window.raise_()  # for macOS
+            self._qt_window.activateWindow()  # for Windows
 
     def _update_palette(self, palette):
         # set window styles which don't use the primary stylesheet
