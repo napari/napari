f825cd4aa8da19b86ff584803bf73613c8fea362
HagaiHargil <hagaihargil@gmail.com>
2019-12-22 20:32:18 +0200

    Fixed numpy 1.17 deprecation becoming hard error in 1.18

--- napari/layers/labels/tests/test_label_utils.py
+++ napari/layers/labels/tests/test_label_utils.py
@@ -5,10 +5,10 @@ from napari.layers.labels.labels_utils import interpolate_coordinates
 
 def test_interpolate_coordinates():
     # test when number of interpolated points > 1
-    old_coord = np.asarray([0, 1])
-    new_coord = np.asarray([0, 10])
+    old_coord = np.array([0, 1], dtype=np.int32)
+    new_coord = np.array([0, 10], dtype=np.int32)
     coords = interpolate_coordinates(old_coord, new_coord, brush_size=3)
-    expected_coords = np.asarray(
+    expected_coords = np.array(
         [
             [0, 1.75],
             [0, 2.5],
@@ -22,6 +22,7 @@ def test_interpolate_coordinates():
             [0, 8.5],
             [0, 9.25],
             [0, 10],
-        ]
+        ],
+        dtype=np.int32,
     )
     assert np.all(coords == expected_coords)
