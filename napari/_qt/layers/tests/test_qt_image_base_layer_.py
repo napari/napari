fa88133726ed3fdecaf5b5131eba8411699f95ec
Talley Lambert <talley.lambert@gmail.com>
2020-01-05 10:52:01 -0500

    break up tests

--- napari/_qt/layers/tests/test_qt_image_base_layer_.py
+++ napari/_qt/layers/tests/test_qt_image_base_layer_.py
@@ -7,18 +7,17 @@ from napari.layers import Image, Surface
 from napari._qt.layers.qt_image_base_layer import QtBaseImageControls
 
 
-@pytest.mark.parametrize('model', [Image, Surface])
-def test_base_controls_contrast_limits(qtbot, model):
-    if model == Image:
-        data = np.arange(100).astype(np.uint16).reshape((10, 10))
-    elif model == Surface:
-        vertices = np.random.random((10, 2))
-        faces = np.random.randint(10, size=(6, 3))
-        values = np.arange(100).astype(np.float)
-        data = (vertices, faces, values)
-    layer = model(data)
+_IMAGE = np.arange(100).astype(np.uint16).reshape((10, 10))
+_SURF = (
+    np.random.random((10, 2)),
+    np.random.randint(10, size=(6, 3)),
+    np.arange(100).astype(np.float),
+)
 
-    # create base layer controls
+
+@pytest.mark.parametrize('layer', [Image(_IMAGE), Surface(_SURF)])
+def test_base_controls_creation(qtbot, layer):
+    """Check basic creation of QtBaseImageControls works"""
     qtctrl = QtBaseImageControls(layer)
     original_clims = tuple(layer.contrast_limits)
     slider_clims = qtctrl.contrastLimitsSlider.values()
@@ -26,15 +25,32 @@ def test_base_controls_contrast_limits(qtbot, model):
     assert slider_clims[1] == 99
     assert tuple(slider_clims) == original_clims
 
-    # right clicking brings up the contrast limits menu
+
+@pytest.mark.parametrize('layer', [Image(_IMAGE), Surface(_SURF)])
+def test_clim_right_click_shows_popup(qtbot, layer):
+    """Right clicking on the contrast limits slider should show a popup."""
+    qtctrl = QtBaseImageControls(layer)
     qtbot.mousePress(qtctrl.contrastLimitsSlider, Qt.RightButton)
     assert hasattr(qtctrl, 'clim_pop')
     assert qtctrl.clim_pop.isVisible()
 
-    # changing the model updates the view
+
+@pytest.mark.parametrize('layer', [Image(_IMAGE), Surface(_SURF)])
+def test_changing_model_updates_view(qtbot, layer):
+    """Changing the model attribute should update the view"""
+    qtctrl = QtBaseImageControls(layer)
     new_clims = (20, 40)
     layer.contrast_limits = new_clims
     assert tuple(qtctrl.contrastLimitsSlider.values()) == new_clims
+
+
+@pytest.mark.parametrize('layer', [Image(_IMAGE), Surface(_SURF)])
+def test_range_popup_clim_buttons(qtbot, layer):
+    """The buttons in the clim_popup should adjust the contrast limits value"""
+    qtctrl = QtBaseImageControls(layer)
+    original_clims = tuple(layer.contrast_limits)
+    layer.contrast_limits = (20, 40)
+    qtbot.mousePress(qtctrl.contrastLimitsSlider, Qt.RightButton)
 
     # pressing the reset button returns the clims to the default values
     reset_button = qtctrl.clim_pop.findChild(QPushButton, "reset_clims_button")
@@ -44,7 +60,7 @@ def test_base_controls_contrast_limits(qtbot, model):
     rangebtn = qtctrl.clim_pop.findChild(QPushButton, "full_clim_range_button")
     # the data we created above was uint16 for Image, and float for Surface
     # Surface will not have a "full range button"
-    if model == Image:
+    if np.issubdtype(layer.dtype, np.integer):
         qtbot.mouseClick(rangebtn, Qt.LeftButton)
         assert tuple(layer.contrast_limits_range) == (0, 2 ** 16 - 1)
         assert tuple(qtctrl.contrastLimitsSlider.range) == (0, 2 ** 16 - 1)
