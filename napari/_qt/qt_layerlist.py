ca1c3d922c8d767db129a23f49179d601c43fef1
Talley Lambert <talley.lambert@gmail.com>
2020-01-08 16:11:31 -0500

    clean qt_layerlist

--- napari/_qt/qt_layerlist.py
+++ napari/_qt/qt_layerlist.py
@@ -46,7 +46,7 @@ class QtLayerList(QScrollArea):
 
         self.layers.events.added.connect(self._add)
         self.layers.events.removed.connect(self._remove)
-        self.layers.events.reordered.connect(lambda e: self._reorder())
+        self.layers.events.reordered.connect(self._reorder)
 
         self.drag_start_position = np.zeros(2)
         self.drag_name = None
@@ -74,7 +74,7 @@ class QtLayerList(QScrollArea):
         self.vbox_layout.removeWidget(divider)
         divider.deleteLater()
 
-    def _reorder(self):
+    def _reorder(self, event=None):
         """Reorders list of layer widgets by looping through all
         widgets in list sequentially removing them and inserting
         them into the correct place in final list.
