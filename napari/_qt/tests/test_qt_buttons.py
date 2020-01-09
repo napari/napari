d4a911dcff8854c8bf47824e5b098748b06b62f2
Talley Lambert <talley.lambert@gmail.com>
2020-01-09 11:39:57 -0500

    basic button test

---
+++ napari/_qt/tests/test_qt_buttons.py
@@ -0,0 +1,33 @@
+from qtpy.QtCore import Qt
+
+from napari._qt.qt_mode_buttons import QtModePushButton, QtModeRadioButton
+from napari.layers import Points
+from napari.layers.points._constants import Mode
+
+
+def test_radio_button(qtbot):
+    """Make sure the QtModeRadioButton works to change layer modes"""
+    layer = Points()
+    assert layer.mode != Mode.ADD
+
+    btn = QtModeRadioButton(layer, 'test_button', Mode.ADD, 'tooltip')
+    assert btn.property('mode') == 'test_button'
+    assert btn.toolTip() == 'tooltip'
+
+    qtbot.mouseClick(btn, Qt.LeftButton)
+    assert layer.mode == 'add'
+
+
+def test_push_button(qtbot):
+    """Make sure the QtModePushButton works with callbacks"""
+    layer = Points()
+
+    def set_test_prop():
+        layer.test_prop = True
+
+    btn = QtModePushButton(layer, 'test_button', set_test_prop, 'tooltip')
+    assert btn.property('mode') == 'test_button'
+    assert btn.toolTip() == 'tooltip'
+
+    qtbot.mouseClick(btn, Qt.LeftButton)
+    assert layer.test_prop
