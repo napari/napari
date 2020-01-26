419d74d5b5447bfa3c7fad739e9d2d898071285a
Talley Lambert <talley.lambert@gmail.com>
2020-01-25 20:38:09 -0500

    update types

--- napari/plugins/hookspecs.py
+++ napari/plugins/hookspecs.py
@@ -3,7 +3,9 @@ from typing import Callable, Optional, List, Tuple, Union, Any, Dict
 
 hookspec = pluggy.HookspecMarker("napari")
 
-LayerData = Union[Tuple[Any], Tuple[Any, Dict]]
+# layer data may be: (data,) (data, meta), or (data, meta, layer_type)
+# using "Any" for the data type for now
+LayerData = Union[Tuple[Any], Tuple[Any, Dict], Tuple[Any, Dict, str]]
 ReaderFunction = Callable[[str], List[LayerData]]
 
 
