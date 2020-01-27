59be812a066e316960fc0550743cd76e5b97d7c5
Talley Lambert <talley.lambert@gmail.com>
2020-01-27 09:36:11 -0500

    move add_layer to mixin

--- napari/components/add_layers_mixin.py
+++ napari/components/add_layers_mixin.py
@@ -20,6 +20,31 @@ class AddLayersMixin:
     easier to read and make these methods easier to maintain.
     """
 
+    def add_layer(self, layer):
+        """Add a layer to the viewer.
+
+        Parameters
+        ----------
+        layer : Layer
+            Layer to add.
+        """
+        layer.events.select.connect(self._update_active_layer)
+        layer.events.deselect.connect(self._update_active_layer)
+        layer.events.status.connect(self._update_status)
+        layer.events.help.connect(self._update_help)
+        layer.events.interactive.connect(self._update_interactive)
+        layer.events.cursor.connect(self._update_cursor)
+        layer.events.cursor_size.connect(self._update_cursor_size)
+        layer.events.data.connect(self._on_layers_change)
+        layer.dims.events.ndisplay.connect(self._on_layers_change)
+        layer.dims.events.order.connect(self._on_layers_change)
+        layer.dims.events.range.connect(self._on_layers_change)
+        self.layers.append(layer)
+        self._update_layers(layers=[layer])
+
+        if len(self.layers) == 1:
+            self.reset_view()
+
     def add_image(
         self,
         data=None,
