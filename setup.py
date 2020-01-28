fae599c2c20b39e449a8912b78ee60604f75aab3
Kira Evans <contact@kne42.me>
2020-01-28 12:08:34 -0800

    add Python 3.8 to PyPI tags (#917)

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Scientific/Engineering',
     'Topic :: Scientific/Engineering :: Visualization',
     'Topic :: Scientific/Engineering :: Information Analysis',
