ab141827d64f959cd30def7efbb46bb1468cdd73
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2020-01-28 18:37:23 -0800

    cleanup nD_points_with_properties

--- examples/nD_points_with_properties.py
+++ examples/nD_points_with_properties.py
@@ -13,7 +13,7 @@ import napari
 
 with napari.gui_qt():
     blobs = data.binary_blobs(
-                length=128, blob_size_fraction=0.05, n_dim=3, volume_fraction=0.05
+                length=100, blob_size_fraction=0.05, n_dim=3, volume_fraction=0.05
             )
     viewer = napari.view_image(blobs.astype(float))
 
@@ -21,29 +21,34 @@ with napari.gui_qt():
     points = []
     for z in range(blobs.shape[0]):
         points += [
-                [z, 43, 43],
-                [z, 43, 86],
-                [z, 86, 43],
-                [z, 86, 86]
+                [z, 25, 25],
+                [z, 25, 75],
+                [z, 75, 25],
+                [z, 75, 75]
         ]
 
+    # create the property for setting the face and edge color.
     face_property = np.array([True, True, True, True, False, False, False, False] * int((blobs.shape[0] / 2)))
-    edge_property = np.array(['A', 'B', 'C', 'D', 'E'] * ceil(len(points) / 5))[0:len(points)]
+    edge_property = np.array(['A', 'B', 'C', 'D', 'E'] * int(len(points) / 5))
 
     properties = {
         'face_property': face_property,
-        'edge_property': edge_property
+        'edge_property': edge_property,
     }
 
     points_layer = viewer.add_points(
-        points, properties=properties,
+        points,
+        properties=properties,
         size=3,
         edge_width=5,
         edge_color='edge_property',
         face_color='face_property',
-        n_dimensional=False
+        n_dimensional=False,
     )
 
-    # change the color cycles
+    # change the face color cycle
     points_layer.face_color_cycle = ['white', 'black']
+
+    # change the edge_color cycle.
+    # there are 4 colors for 5 categories, so 'c' will be recycled
     points_layer.edge_color_cycle = ['c', 'm', 'y', 'k']
