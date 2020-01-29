557a574a9ad093afa93a50ac7102136ff1887634
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2020-01-28 20:09:46 -0800

    add threshold to guess_continuous

--- napari/layers/points/points_utils.py
+++ napari/layers/points/points_utils.py
@@ -26,7 +26,7 @@ def dataframe_to_properties(dataframe) -> Dict[str, np.ndarray]:
 def guess_continuous(property: np.ndarray) -> bool:
     """Guess if the property is continuous (return True) or categorical (return False)"""
     # if the property is a floating type, guess continuous
-    if issubclass(property.dtype.type, np.floating):
+    if issubclass(property.dtype.type, np.floating) and len(property < 16):
         return True
     else:
         return False
