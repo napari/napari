c8d0d3ced67a449e8cce24c1a76c43f033a1e242
Talley Lambert <talley.lambert@gmail.com>
2020-02-01 13:58:14 -0500

    change discovery syntax

--- napari/plugins/manager.py
+++ napari/plugins/manager.py
@@ -31,11 +31,9 @@ class NapariPluginManager(pluggy.PluginManager):
         # register our own built plugins
         self.register(_builtins, name='builtins')
         # discover external plugins
-        if (
-            not os.environ.get("NAPARI_DISABLE_PLUGIN_AUTOLOAD")
-            and autodiscover
-        ):
-            self.discover()
+        if not os.environ.get("NAPARI_DISABLE_PLUGIN_AUTOLOAD"):
+            if autodiscover:
+                self.discover()
 
     def discover(self):
         # avoid circular import
