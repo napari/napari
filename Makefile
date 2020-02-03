0740a2d9d08c57d093365086398084585a159b13
Talley Lambert <talley.lambert@gmail.com>
2020-02-03 14:03:04 -0500

    remove warning, open page on docs build

--- Makefile
+++ Makefile
@@ -4,3 +4,4 @@ docs:
 	pip install -q -r requirements/docs.txt
 	make -C docs clean
 	make -C docs html
+	open docs/build/html/index.html
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ html_logo = "img/napari_logo.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 
 # -- Extension configuration -------------------------------------------------
