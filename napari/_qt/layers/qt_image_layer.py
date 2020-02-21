43f1cd6032423aef66081fe3fa1f9fd5f6c2b185
Johannes Elferich <elferich@ohsu.edu>
2020-02-20 20:41:21 -0800

    UI for transformation

--- napari/_qt/layers/qt_image_layer.py
+++ napari/_qt/layers/qt_image_layer.py
@@ -1,12 +1,21 @@
 from qtpy.QtCore import Qt
-from qtpy.QtWidgets import QLabel, QComboBox, QSlider
+from qtpy.QtWidgets import (
+    QLabel,
+    QComboBox,
+    QSlider,
+    QHBoxLayout,
+    QButtonGroup,
+)
 from .qt_image_base_layer import QtBaseImageControls
-from ...layers.image._constants import Interpolation, Rendering
+from ...layers.image._constants import Interpolation, Rendering, Mode
+from ..qt_mode_buttons import QtModeRadioButton
 
 
 class QtImageControls(QtBaseImageControls):
     def __init__(self, layer):
         super().__init__(layer)
+
+        self.layer.events.mode.connect(self.set_mode)
 
         self.layer.events.interpolation.connect(self._on_interpolation_change)
         self.layer.events.rendering.connect(self._on_rendering_change)
@@ -55,30 +64,57 @@ class QtImageControls(QtBaseImageControls):
         self.attenuationLabel = QLabel('attenuation:')
         self._on_ndisplay_change()
 
+        self.transform_button = QtModeRadioButton(
+            layer, 'transform', Mode.TRANSFORM, tooltip='Transform image'
+        )
+        self.panzoom_button = QtModeRadioButton(
+            layer, 'pan_zoom', Mode.PAN_ZOOM, tooltip='Pan/zoom', checked=True
+        )
+
+        self.button_group = QButtonGroup(self)
+        self.button_group.addButton(self.transform_button)
+        self.button_group.addButton(self.panzoom_button)
+
+        button_row = QHBoxLayout()
+        button_row.addWidget(self.transform_button)
+        button_row.addWidget(self.panzoom_button)
+        button_row.addStretch(1)
+        button_row.setSpacing(4)
+
         # grid_layout created in QtLayerControls
         # addWidget(widget, row, column, [row_span, column_span])
-        self.grid_layout.addWidget(QLabel('opacity:'), 0, 0)
-        self.grid_layout.addWidget(self.opacitySlider, 0, 1, 1, 2)
-        self.grid_layout.addWidget(QLabel('contrast limits:'), 1, 0)
-        self.grid_layout.addWidget(self.contrastLimitsSlider, 1, 1, 1, 2)
-        self.grid_layout.addWidget(QLabel('gamma:'), 2, 0)
-        self.grid_layout.addWidget(self.gammaSlider, 2, 1, 1, 2)
-        self.grid_layout.addWidget(self.isoThresholdLabel, 3, 0)
-        self.grid_layout.addWidget(self.isoThresholdSlider, 3, 1, 1, 2)
-        self.grid_layout.addWidget(self.attenuationLabel, 3, 0)
-        self.grid_layout.addWidget(self.attenuationSlider, 3, 1, 1, 2)
-        self.grid_layout.addWidget(QLabel('colormap:'), 4, 0)
-        self.grid_layout.addWidget(self.colormapComboBox, 4, 2)
-        self.grid_layout.addWidget(self.colorbarLabel, 4, 1)
-        self.grid_layout.addWidget(QLabel('blending:'), 5, 0)
-        self.grid_layout.addWidget(self.blendComboBox, 5, 1, 1, 2)
-        self.grid_layout.addWidget(self.renderLabel, 6, 0)
-        self.grid_layout.addWidget(self.renderComboBox, 6, 1, 1, 2)
-        self.grid_layout.addWidget(self.interpLabel, 7, 0)
-        self.grid_layout.addWidget(self.interpComboBox, 7, 1, 1, 2)
+        self.grid_layout.addLayout(button_row, 0, 1, 1, 2)
+        self.grid_layout.addWidget(QLabel('opacity:'), 1, 0)
+        self.grid_layout.addWidget(self.opacitySlider, 1, 1, 1, 2)
+        self.grid_layout.addWidget(QLabel('contrast limits:'), 2, 0)
+        self.grid_layout.addWidget(self.contrastLimitsSlider, 2, 1, 1, 2)
+        self.grid_layout.addWidget(QLabel('gamma:'), 3, 0)
+        self.grid_layout.addWidget(self.gammaSlider, 3, 1, 1, 2)
+        self.grid_layout.addWidget(self.isoThresholdLabel, 4, 0)
+        self.grid_layout.addWidget(self.isoThresholdSlider, 4, 1, 1, 2)
+        self.grid_layout.addWidget(self.attenuationLabel, 4, 0)
+        self.grid_layout.addWidget(self.attenuationSlider, 4, 1, 1, 2)
+        self.grid_layout.addWidget(QLabel('colormap:'), 5, 0)
+        self.grid_layout.addWidget(self.colormapComboBox, 5, 2)
+        self.grid_layout.addWidget(self.colorbarLabel, 5, 1)
+        self.grid_layout.addWidget(QLabel('blending:'), 6, 0)
+        self.grid_layout.addWidget(self.blendComboBox, 6, 1, 1, 2)
+        self.grid_layout.addWidget(self.renderLabel, 7, 0)
+        self.grid_layout.addWidget(self.renderComboBox, 7, 1, 1, 2)
+        self.grid_layout.addWidget(self.interpLabel, 8, 0)
+        self.grid_layout.addWidget(self.interpComboBox, 8, 1, 1, 2)
         self.grid_layout.setRowStretch(8, 1)
         self.grid_layout.setColumnStretch(1, 1)
         self.grid_layout.setVerticalSpacing(4)
+
+    def set_mode(self, event):
+        mode = event.mode
+        if mode == Mode.TRANSFORM:
+            self.transform_button.setChecked(True)
+        elif mode == Mode.PAN_ZOOM:
+            self.panzoom_button.setChecked(True)
+        else:
+            raise ValueError("Mode not recognized")
 
     def changeInterpolation(self, text):
         self.layer.interpolation = text
