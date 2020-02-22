cc6844e69b446affc01f84e2973fc104d636cd0e
Talley Lambert <talley.lambert@gmail.com>
2020-02-21 19:27:07 -0500

    update example

--- examples/add_image_complex.py
+++ examples/add_image_complex.py
@@ -4,16 +4,24 @@ Display one image using the add_image API.
 
 import napari
 
-# from skimage import data
-# from scipy.fftpack import fft2, fftshift
+import numpy as np
+from skimage import data
+from scipy.fftpack import fft2, fftshift, ifftshift
 
-# im = data.astronaut().sum(-1)
-# ny, nx = im.shape
-# beta = 5
-# win = np.stack(np.meshgrid(np.kaiser(ny, beta), np.kaiser(nx, beta))).prod(0)
-# imf = np.log(fftshift(fft2(im * win)))
+im = data.astronaut().sum(-1)
+ny, nx = im.shape
+beta = 5
+win = np.stack(np.meshgrid(np.kaiser(ny, beta), np.kaiser(nx, beta))).prod(0)
+imf = np.log(fftshift(fft2(ifftshift(im * win))))
 
 
 with napari.gui_qt():
     # create the viewer with an image
-    viewer = napari.view_image(napari.utils.complex.complex_ramp())
+    viewer = napari.Viewer()
+    viewer.add_image(im, name='astronaut')
+    viewer.add_image(imf, name='FFT of astronaut')
+
+    # a test sample in which the X dimension is a phase ramp from -ip to pi
+    # and Y is a magnitude ramp from 0 - 10
+    complex_ramp = napari.utils.complex.complex_ramp(512)
+    viewer.add_image(complex_ramp, name="complex ramp")
