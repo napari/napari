817c4f85e2a13a196f342254d0a9d41c7c56fcab
Johannes Elferich <elferich@ohsu.edu>
2020-02-25 19:59:59 -0800

    Test updating of InteractionBox

--- napari/layers/_tests/test_interaction_box.py
+++ napari/layers/_tests/test_interaction_box.py
@@ -46,3 +46,54 @@ def test_interaction_box_initialization():
 
     interaction_box = InteractionBox(points=[], show=True)
     assert interaction_box._box is None
+
+
+def test_interaction_box_update():
+    """Test the creation of the interaction box."""
+    shape = (10, 2)
+    np.random.seed(0)
+    data = 20 * np.random.random(shape)
+    interaction_box = InteractionBox()
+    (
+        vertices,
+        face_color,
+        edge_color,
+        pos,
+        width,
+    ) = interaction_box._compute_vertices_and_box()
+    assert len(vertices) == 0
+    assert pos is None
+
+    interaction_box.points = data
+    assert len(interaction_box._box) == 10
+    (
+        vertices,
+        face_color,
+        edge_color,
+        pos,
+        width,
+    ) = interaction_box._compute_vertices_and_box()
+    assert len(vertices) == 0
+    assert pos is None
+
+    interaction_box.show = True
+    (
+        vertices,
+        face_color,
+        edge_color,
+        pos,
+        width,
+    ) = interaction_box._compute_vertices_and_box()
+    assert len(vertices) == 9
+    assert len(pos) == 7
+
+    interaction_box.show = False
+    (
+        vertices,
+        face_color,
+        edge_color,
+        pos,
+        width,
+    ) = interaction_box._compute_vertices_and_box()
+    assert len(vertices) == 0
+    assert pos is None
