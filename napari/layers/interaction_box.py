2dc271ce1613c2297ad658c1fb8490499b0ec636
Johannes Elferich <elferich@ohsu.edu>
2020-02-29 23:19:13 -0800

    Implemented rotation of InteractionBox

--- napari/layers/interaction_box.py
+++ napari/layers/interaction_box.py
@@ -1,7 +1,42 @@
 import numpy as np
+from skimage.transform import SimilarityTransform
 
 from ._constants import Box
 from ..utils.event import EmitterGroup, Event
+
+
+def inside_boxes(boxes):
+    """Checks which boxes contain the origin. Boxes need not be axis aligned
+
+    Parameters
+    ----------
+    boxes : (N, 8, 2) array
+        Array of N boxes that should be checked
+
+    Returns
+    -------
+    inside : (N,) array of bool
+        True if corresponding box contains the origin.
+    """
+
+    AB = boxes[:, 0] - boxes[:, 6]
+    AM = boxes[:, 0]
+    BC = boxes[:, 6] - boxes[:, 4]
+    BM = boxes[:, 6]
+
+    ABAM = np.multiply(AB, AM).sum(1)
+    ABAB = np.multiply(AB, AB).sum(1)
+    BCBM = np.multiply(BC, BM).sum(1)
+    BCBC = np.multiply(BC, BC).sum(1)
+
+    c1 = 0 <= ABAM
+    c2 = ABAM <= ABAB
+    c3 = 0 <= BCBM
+    c4 = BCBM <= BCBC
+
+    inside = np.all(np.array([c1, c2, c3, c4]), axis=0)
+
+    return inside
 
 
 class InteractionBox:
@@ -31,6 +66,9 @@ class InteractionBox:
         self._show_handle = show_handle
 
         self._selected_vertex = None
+        self._drag_start = None
+        self._drag_start_angle = None
+        self._fixed_aspect = False
 
         self.events = EmitterGroup(
             source=self,
@@ -71,6 +109,12 @@ class InteractionBox:
     def show_handle(self, show_handle):
         self._show_handle = show_handle
         self.events.points_changed()
+
+    @property
+    def angle(self):
+        offset = self._box[Box.HANDLE] - self._box[Box.CENTER]
+        angle = -np.degrees(np.arctan2(offset[0], -offset[1]))
+        return angle
 
     def _compute_vertices_and_box(self):
         """Compute location of the box for rendering.
@@ -172,13 +216,72 @@ class InteractionBox:
         )
         self._box = box
 
+    def _set_drag_start_values(self, layer):
+        """ Gets called whenever a drag is started to remember starting values
+        """
+
+        self._drag_start_coordinates = [
+            layer.coordinates[i] for i in layer.dims.displayed
+        ]
+        self._drag_start_box = np.copy(self._box)
+        self._drag_start_angle = self.angle
+        self._drag_angle = 0
+
+    def _clear_drag_start_values(self):
+        """ Gets called at the end of a drag to reset remembered values
+        """
+
+        self._drag_start_coordinates = None
+        self._drag_start_box = None
+        self._drag_start_angle = None
+        self._drag_angle = 0
+
+    def _drag_transform_box(self):
+        """ Gets called when rotation,scale or translation change due to drag.
+            The function calculates a transform that is used to transform the box
+            and is passed back to the layer.
+        """
+        # rotate
+        center = self._drag_start_box[Box.CENTER]
+        tform1 = SimilarityTransform(translation=-center)
+        tform2 = SimilarityTransform(rotation=-np.radians(self._drag_angle))
+        tform3 = SimilarityTransform(translation=center)
+        transform = tform1 + tform2 + tform3
+
+        self._box = transform(self._drag_start_box)
+        self.events.points_changed()
+
+    def _on_rotation_drag(self, layer, event):
+        """ Gets called upon mouse_move in the case of a rotation
+        """
+
+        new_offset = [
+            layer.coordinates[i] for i in layer.dims.displayed
+        ] - self._box[Box.CENTER]
+        new_angle = -np.degrees(np.arctan2(new_offset[0], -new_offset[1]))
+
+        if np.linalg.norm(new_offset) < 1:
+            self._drag_angle = 0
+        elif self._fixed_aspect:
+            self._drag_angle = (
+                np.round(new_angle / 45) * 45 - self._drag_start_angle
+            )
+        else:
+            self._drag_angle = new_angle - self._drag_start_angle
+
+        self._drag_transform_box()
+        self.events.rotated(angle=self._drag_angle)
+
     def initialize_mouse_events(self, layer):
+        """ Adds event handling functions to the layer
+        """
+
         @layer.mouse_move_callbacks.append
         def mouse_move(layer, event):
             if not self.show or self._box is None:
                 return
 
-            box = self._box[Box.WITH_HANDLE]
+            box = self._box
             coord = [layer.coordinates[i] for i in layer.dims.displayed]
             distances = abs(box - coord)
 
@@ -189,6 +292,43 @@ class InteractionBox:
             matches = np.all(distances <= sizes, axis=1).nonzero()
             if len(matches[0]) > 0:
                 self._selected_vertex = matches[0][-1]
+                # Exclde center vertex
+                if self._selected_vertex == Box.CENTER:
+                    self._selected_vertex = None
             else:
                 self._selected_vertex = None
             self.events.points_changed()
+
+        @layer.mouse_drag_callbacks.append
+        def mouse_drag(layer, event):
+            if not self.show or self._box is None:
+                return
+
+            # Handling drag start, decide what action to take
+            self._set_drag_start_values(layer)
+            drag_callback = None
+            if self._selected_vertex is not None:
+                if self._selected_vertex == Box.HANDLE:
+                    drag_callback = self._on_rotation_drag
+                    yield
+                else:
+                    drag_callback = None
+                    yield
+            else:
+                if inside_boxes(
+                    np.array([self._box - self._drag_start_coordinates])
+                )[0]:
+                    drag_callback = None
+                    yield
+                else:
+                    self.points = None
+                    drag_callback = None
+                    yield
+
+            # Handle events during dragging
+            while event.type == 'mouse_move':
+                if drag_callback is not None:
+                    drag_callback(layer, event)
+                yield
+
+            self._clear_drag_start_values()
