fbdda5797d55404a28da572a0fa8ea725ab8d3aa
Talley Lambert <talley.lambert@gmail.com>
2020-03-09 07:40:47 -0400

    fix resource tests

--- napari/resources/test_resources.py
+++ napari/resources/test_resources.py
@@ -1,4 +1,12 @@
-def test_resources():
-    from . import qt
+from napari.resources import build_pyqt_resources
+import os
 
-    assert qt.QtCore.__package__ == 'qtpy'
+
+def test_resources():
+    """Test that we can build icons and resources."""
+    temp = os.path.join(os.path.dirname(__file__), '_test_qt.py')
+    build_pyqt_resources(out_path=temp, overwrite=True)
+    from . import _test_qt
+
+    assert _test_qt.QtCore.__package__ == 'qtpy'
+    os.remove(temp)
