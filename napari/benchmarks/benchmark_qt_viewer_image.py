ee58f4f991d7228d13765e7d7487e878aca31016
Talley Lambert <talley.lambert@gmail.com>
2020-03-15 21:44:30 -0400

    benchmarks

--- napari/benchmarks/benchmark_qt_viewer_image.py
+++ napari/benchmarks/benchmark_qt_viewer_image.py
@@ -165,3 +165,36 @@ class QtViewerSingleInvisbleImageSuite:
     def time_ndisplay(self):
         """Time to enter 3D rendering."""
         self.viewer.dims.ndisplay = 3
+
+
+class QtVolumeRenderingSuite:
+    """Benchmarks for a single image layer in the viewer."""
+
+    params = [2 ** i for i in range(4, 11)]
+
+    def setup(self, n):
+        _ = QApplication.instance() or QApplication([])
+        np.random.seed(0)
+        self.data = np.random.random((n // 2, n, n)) * 2 ** 12
+        self.viewer = napari.view_image(self.data, ndisplay=3)
+
+    def teardown(self, n):
+        self.viewer.close()
+
+    def time_change_contrast_range(self, n):
+        """Time to change contrast limits range."""
+        self.viewer.layers[0].contrast_limits_range = (100, 4000)
+        self.viewer.layers[0].contrast_limits_range = (150, 3950)
+        self.viewer.layers[0].contrast_limits_range = (200, 3900)
+
+    def time_change_contrast(self, n):
+        """Time to change contrast limits."""
+        self.viewer.layers[0].contrast_limits = (250, 3000)
+        self.viewer.layers[0].contrast_limits = (300, 2900)
+        self.viewer.layers[0].contrast_limits = (350, 2800)
+
+    def time_change_gamma(self, n):
+        """Time to change gamma."""
+        self.viewer.layers[0].gamma = 0.5
+        self.viewer.layers[0].gamma = 0.8
+        self.viewer.layers[0].gamma = 1.3
