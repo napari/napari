7ae1ea720d5dda1efe3f376532f6017a0bb9e0c2
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-03-20 21:12:02 -0700

    add del

--- napari/layers/shapes/__init__.py
+++ napari/layers/shapes/__init__.py
@@ -1,2 +1,7 @@
 from .shapes import Shapes
 from . import _shapes_key_bindings
+
+# Note that importing _viewer_key_bindings is needed as the Viewer gets
+# decorated with keybindings during that process, but it is not directly needed
+# by our users and is deleted below
+del _shapes_key_bindings
