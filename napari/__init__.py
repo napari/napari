40aae2d7fd4e6e03187500f10a3795fa9c20d670
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-03-20 21:13:04 -0700

    minor edit

--- napari/__init__.py
+++ napari/__init__.py
@@ -51,7 +51,7 @@ from .viewer import Viewer
 
 # Note that importing _viewer_key_bindings is needed as the Viewer gets
 # decorated with keybindings during that process, but it is not directly needed
-# by our users and is deleted below
+# by our users and so is deleted below
 from . import _viewer_key_bindings  # noqa: F401
 from .view_layers import (
     view_path,
