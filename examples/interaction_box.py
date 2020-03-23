6aac38a060346088f0c2d6ea3ec746086f530b71
Johannes Elferich <elferich@ohsu.edu>
2020-03-23 15:47:52 -0700

    Better example

--- examples/interaction_box.py
+++ examples/interaction_box.py
@@ -6,10 +6,15 @@ from skimage import data
 import napari
 import numpy as np
 
+def on_selection_box_drag(event):
+    sel_i = napari.layers.points.points.points_in_box(event.box,viewer.active_layer._data_view,viewer.active_layer._size_view)
+    
 
 with napari.gui_qt():
     # create the viewer with an image
-    viewer = napari.view_image(data.astronaut(), rgb=True)
+    X, Y = np.mgrid[-500:500:50, -500:500:50]
+    positions = np.dstack([X.ravel(), Y.ravel()])
+    viewer = napari.view_points(positions[0,:,:])
     viewer.active_layer.interactive = False
-    viewer.active_layer._interaction_box.points = np.array([[50,50],[300,302]])
-    viewer.active_layer._interaction_box.show = True
+    
+    viewer.active_layer._interaction_box.events.selection_box_changed_drag.connect(on_selection_box_drag)
