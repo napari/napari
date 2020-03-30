ac4432aaedde3c5ec0c39f9e8365f84fc714d706
Talley Lambert <talley.lambert@gmail.com>
2020-03-30 13:32:40 -0400

    lazy ipython with cgitb fallback

--- napari/plugins/exceptions.py
+++ napari/plugins/exceptions.py
@@ -1,15 +1,25 @@
 import sys
 from collections import defaultdict
 from types import TracebackType
-from typing import DefaultDict, Dict, List, Optional, Tuple, Type
+from typing import (
+    DefaultDict,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Type,
+    Callable,
+    Generator,
+)
 import re
-import IPython.core.ultratb
 
 
 # This is a mapping of plugin_name -> PluginError instances
 # all PluginErrors get added to this in PluginError.__init__
 PLUGIN_ERRORS: DefaultDict[str, List['PluginError']] = defaultdict(list)
 
+# standard tuple type returned from sys.exc_info()
+ExcInfoTuple = Tuple[Type[Exception], Exception, Optional[TracebackType]]
 
 if sys.version_info >= (3, 8):
     from importlib import metadata as importlib_metadata
@@ -33,9 +43,7 @@ class PluginError(Exception):
         The module of the plugin that had the error
     """
 
-    def __init__(
-        self, message: str, plugin_name: str, plugin_module: str
-    ) -> None:
+    def __init__(self, message: str, plugin_name: str, plugin_module: str):
         super().__init__(message)
         self.plugin_name = plugin_name
         self.plugin_module = plugin_module
@@ -59,9 +67,7 @@ class PluginError(Exception):
         msg += '\n'
         return msg
 
-    def info(
-        self,
-    ) -> Tuple[Type['PluginError'], 'PluginError', Optional[TracebackType]]:
+    def info(self,) -> ExcInfoTuple:
         """Return info as would be returned from sys.exc_info()."""
         return (self.__class__, self, self.__traceback__)
 
@@ -69,7 +75,7 @@ class PluginError(Exception):
 class PluginImportError(PluginError, ImportError):
     """Raised when a plugin fails to import."""
 
-    def __init__(self, plugin_name: str, plugin_module: str) -> None:
+    def __init__(self, plugin_name: str, plugin_module: str):
         msg = f"Failed to import plugin: '{plugin_name}'"
         super().__init__(msg, plugin_name, plugin_module)
 
@@ -77,18 +83,13 @@ class PluginImportError(PluginError, ImportError):
 class PluginRegistrationError(PluginError):
     """Raised when a plugin fails to register with pluggy."""
 
-    def __init__(self, plugin_name: str, plugin_module: str) -> None:
+    def __init__(self, plugin_name: str, plugin_module: str):
         msg = f"Failed to register plugin: '{plugin_name}'"
         super().__init__(msg, plugin_name, plugin_module)
 
 
-def format_exceptions(plugin_name: str, as_html: bool = False) -> str:
+def format_exceptions(plugin_name: str, as_html: bool = False):
     """Return formatted tracebacks for all exceptions raised by plugin.
-
-    This method takes advantage of ``IPython.core.ultratb.VerboseTB`` to create
-    a nicely formatted traceback.  If ``as_html`` is True, is then converted
-    from ansi to html.  (This approach was used instead of cgitb because the
-    IPython ultratb tracebacks were preferred).
 
     Parameters
     ----------
@@ -104,13 +105,13 @@ def format_exceptions(plugin_name: str, as_html: bool = False) -> str:
         A formatted string with traceback information for every exception
         raised by ``plugin_name`` during this session.
     """
-    from napari import __version__
-
-    if not PLUGIN_ERRORS.get(plugin_name):
+    _plugin_errors: List[PluginError] = PLUGIN_ERRORS.get(plugin_name)
+    if not _plugin_errors:
         return ''
 
-    color = 'Linux' if as_html else 'NoColor'
-    vbtb = IPython.core.ultratb.VerboseTB(color_scheme=color)
+    from napari import __version__
+
+    format_exc_info = get_tb_formatter()
 
     _linewidth = 80
     _pad = (_linewidth - len(plugin_name) - 18) // 2
@@ -120,7 +121,7 @@ def format_exceptions(plugin_name: str, as_html: bool = False) -> str:
         f'{"napari version": >16}: {__version__}',
     ]
     try:
-        err0 = PLUGIN_ERRORS[plugin_name][0]
+        err0 = _plugin_errors[0]
         package_meta = fetch_module_metadata(err0.plugin_module)
         if package_meta:
             msg.extend(
@@ -134,29 +135,117 @@ def format_exceptions(plugin_name: str, as_html: bool = False) -> str:
         pass
     msg.append('')
 
-    for n, err in enumerate(PLUGIN_ERRORS.get(plugin_name, [])):
+    for n, err in enumerate(_plugin_errors):
         _pad = _linewidth - len(str(err)) - 10
         msg += ['', f'ERROR #{n + 1}:  {str(err)} {"-" * _pad}', '']
-        etype, value, tb = err.info()
-        if as_html:
-            ansi_string = vbtb.text(etype, value, tb)
-            html = "".join(ansi2html(ansi_string.replace(" ", "&nbsp;")))
-            html = html.replace("\n", "<br>")
-            html = (
-                "<span style='font-family: monaco,courier,monospace;'>"
-                + html
-                + "</span>"
-            )
-            msg.append(html)
-        else:
-            msg.append(vbtb.text(etype, value, tb))
+        msg.append(format_exc_info(err.info(), as_html))
 
     msg.append('=' * _linewidth)
 
     return ("<br>" if as_html else "\n").join(msg)
 
 
-def fetch_module_metadata(distname: str) -> Optional[Dict[str, str]]:
+def get_tb_formatter() -> Callable[[ExcInfoTuple, bool], str]:
+    """Return a formatter callable that uses IPython VerboseTB if available.
+
+    Imports IPython lazily if available to take advantage of ultratb.VerboseTB.
+    If unavailable, cgitb is used instead, but this function overrides a lot of
+    the hardcoded citgb styles and adds error chaining (for exceptions that
+    result from other exceptions).
+
+    Returns
+    -------
+    callable
+        A function that accepts a 3-tuple and a boolean ``(exc_info, as_html)``
+        and returns a formatted traceback string. The ``exc_info`` tuple is of
+        the ``(type, value, traceback)`` format returned by sys.exc_info().
+        The ``as_html`` determines whether the traceback is formated in html
+        or plain text.
+    """
+    try:
+        import IPython.core.ultratb
+
+        def format_exc_info(info: ExcInfoTuple, as_html: bool) -> str:
+            color = 'Linux' if as_html else 'NoColor'
+            vbtb = IPython.core.ultratb.VerboseTB(color_scheme=color)
+            if as_html:
+                ansi_string = vbtb.text(*info).replace(" ", "&nbsp;")
+                html = "".join(ansi2html(ansi_string))
+                html = html.replace("\n", "<br>")
+                html = (
+                    "<span style='font-family: monaco,courier,monospace;'>"
+                    + html
+                    + "</span>"
+                )
+                return html
+            else:
+                return vbtb.text(*info)
+
+    except ImportError:
+        import cgitb
+        import traceback
+
+        # cgitb does not support error chaining...
+        # see https://www.python.org/dev/peps/pep-3134/#enhanced-reporting
+        # this is a workaround
+        def cgitb_chain(exc: Exception) -> Generator[str, None, None]:
+            """Recurse through exception stack and chain cgitb_html calls."""
+            if exc.__cause__:
+                yield from cgitb_chain(exc.__cause__)
+                yield (
+                    '<br><br><font color="#51B432">The above exception was '
+                    'the direct cause of the following exception:</font><br>'
+                )
+            elif exc.__context__:
+                yield from cgitb_chain(exc.__context__)
+                yield (
+                    '<br><br><font color="#51B432">During handling of the '
+                    'above exception, another exception occurred:</font><br>'
+                )
+            yield cgitb_html(exc)
+
+        def cgitb_html(exc: Exception) -> str:
+            """Format exception with cgitb.html."""
+            info = (type(exc), exc, exc.__traceback__)
+            return cgitb.html(info)
+
+        def format_exc_info(info: ExcInfoTuple, as_html: bool) -> str:
+            if as_html:
+                html = "\n".join(cgitb_chain(info[1]))
+                # cgitb has a lot of hardcoded colors that don't work for us
+                # remove bgcolor, and let theme handle it
+                html = re.sub('bgcolor="#.*"', '', html)
+                # remove superfluous whitespace
+                html = html.replace('<br>\n', '\n')
+                # but retain it around the <small> bits
+                html = re.sub(r'(<tr><td><small.*</tr>)', f'<br>\\1<br>', html)
+                # weird 2-part syntax is a workaround for hard-to-grep text.
+                html = html.replace(
+                    "<p>A problem occurred in a Python script.  "
+                    "Here is the sequence of",
+                    "",
+                )
+                html = html.replace(
+                    "function calls leading up to the error, "
+                    "in the order they occurred.</p>",
+                    "<br>",
+                )
+                # remove hardcoded fonts
+                html = html.replace('face="helvetica, arial"', "")
+                html = (
+                    "<span style='font-family: monaco,courier,monospace;'>"
+                    + html
+                    + "</span>"
+                )
+                return html
+            else:
+                # if we don't need HTML, just use traceback
+                return ''.join(traceback.format_exception(*info))
+
+    return format_exc_info
+
+
+def fetch_module_metadata(distname: str) -> Dict[str, str]:
     """Attempt to retrieve name, version, contact email & url for a package.
 
     Parameters
@@ -174,7 +263,7 @@ def fetch_module_metadata(distname: str) -> Optional[Dict[str, str]]:
     try:
         meta = importlib_metadata.metadata(distname)
     except importlib_metadata.PackageNotFoundError:
-        return None
+        return {}
     return {
         'name': meta.get('Name'),
         'version': meta.get('Version'),
@@ -203,7 +292,24 @@ ANSI_STYLES = {
 }
 
 
-def ansi2html(ansi_string, styles=ANSI_STYLES):
+def ansi2html(
+    ansi_string: str, styles: Dict[int, Dict[str, str]] = ANSI_STYLES
+) -> Generator[str, None, None]:
+    """Convert ansi string to colored HTML
+
+    Parameters
+    ----------
+    ansi_string : str
+        text with ANSI color codes.
+    styles : dict, optional
+        A mapping from ANSI codes to a dict of css kwargs:values,
+        by default ANSI_STYLES
+
+    Yields
+    -------
+    str
+        HTML strings that can be joined to form the final html
+    """
     previous_end = 0
     in_span = False
     ansi_codes = []
