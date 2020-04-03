a390d40882f066af97cd69d2fa8314230f02cb49
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-04-04 09:54:24 +1100

    Make variable names more concise

--- napari/_vispy/_tests/test_vispy_big_images.py
+++ napari/_vispy/_tests/test_vispy_big_images.py
@@ -25,27 +25,17 @@ def test_really_big_2d_image(viewer_factory):
 
     # Screenshot pixel coordinates to test (in format: row, column)
     center_coord = np.round(np.array(screenshot.shape[:2]) / 2).astype(np.int)
-    top_left_corner = [40, 36]
-    top_right_corner = [40, 763]
-    bottom_left_corner = [screenshot.shape[0] - 40, 36]
-    bottom_right_corner = [screenshot.shape[0] - 40, 763]
+    top_left = [40, 36]
+    top_right = [40, 763]
+    bottom_left = [screenshot.shape[0] - 40, 36]
+    bottom_right = [screenshot.shape[0] - 40, 763]
 
     expected_value = np.array([255, 255, 255, 255])  # white pixel value
     assert all(screenshot[center_coord[0], center_coord[1]] == expected_value)
-    assert all(
-        screenshot[top_left_corner[0], top_left_corner[1]] == expected_value
-    )
-    assert all(
-        screenshot[top_right_corner[0], top_right_corner[1]] == expected_value
-    )
-    assert all(
-        screenshot[bottom_left_corner[0], bottom_left_corner[1]]
-        == expected_value
-    )
-    assert all(
-        screenshot[bottom_right_corner[0], bottom_right_corner[1]]
-        == expected_value
-    )
+    assert all(screenshot[top_left[0], top_left[1]] == expected_value)
+    assert all(screenshot[top_right[0], top_right[1]] == expected_value)
+    assert all(screenshot[bottom_left[0], bottom_left[1]] == expected_value)
+    assert all(screenshot[bottom_right[0], bottom_right[1]] == expected_value)
 
 
 def test_big_3D_image(viewer_factory):
