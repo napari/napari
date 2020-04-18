3a6d8ec4e5ba4d70265d9679e1d5aa6b2535b807
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-04-18 15:54:00 -0700

    update

--- napari/_tests/test_viewer.py
+++ napari/_tests/test_viewer.py
@@ -168,21 +168,3 @@ def test_roll_traspose_update(viewer_factory, layer_class, data, ndim):
     # Transpose and check again:
     viewer.dims._transpose()
     check_view_transform_consistency(layer, viewer, transf_dict)
-
-
-def test_deleting_layer(viewer_factory):
-    """Test adding and then deleting a layer."""
-    view, viewer = viewer_factory()
-    assert len(viewer.layers) == 0
-    view.canvas.events.draw(region=None)
-
-    # Add layer
-    data = np.random.random((15, 10, 5))
-    layer = viewer.add_image(data)
-    assert len(viewer.layers) == 1
-    view.canvas.events.draw(region=None)
-
-    # Remove layer
-    viewer.layers.remove(layer)
-    assert len(viewer.layers) == 0
-    view.canvas.events.draw(region=None)
