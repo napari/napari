10ea0500b8ef606a2636d4a0e0f79617574769dc
Talley Lambert <talley.lambert@gmail.com>
2020-04-20 13:54:24 -0400

    close to working

--- napari/plugins/__init__.py
+++ napari/plugins/__init__.py
@@ -8,10 +8,12 @@ from . import hook_specifications
 from . import _builtins
 
 # the main plugin manager instance for the `napari` plugin namespace.
-plugin_manager = PluginManager('napari')
-plugin_manager.add_hookspecs(hook_specifications)
-plugin_manager.register(_builtins, name='builtins')
-plugin_manager.hook._needs_discovery = True
+plugin_manager = PluginManager(
+    'napari', discover_entrypoint='napari.plugin', discover_prefix='napari_'
+)
+with plugin_manager.discovery_blocked():
+    plugin_manager.add_hookspecs(hook_specifications)
+    plugin_manager.register(_builtins, name='builtins')
 
 
 __all__ = [
