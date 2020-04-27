774be8ab3025687f7a3a3c02fa5bd9da5246fd61
Talley Lambert <talley.lambert@gmail.com>
2020-04-27 19:01:22 -0400

    don't assert cache is none

--- napari/layers/_tests/test_dask_layers.py
+++ napari/layers/_tests/test_dask_layers.py
@@ -10,7 +10,6 @@ def test_dask_array_creates_cache():
     """Test that adding a dask array creates a dask cache and turns of fusion.
     """
     # by default we have no dask_cache and task fusion is active
-    assert utils.dask_cache is None
     assert dask.config.get("optimization.fuse.active")
 
     def mock_set_view_slice():
@@ -30,9 +29,10 @@ def test_dask_array_creates_cache():
         _ = Image(da.ones((100, 100)))
 
     assert 'upgrade Dask to v2.15.0 or later' in record[0].message.args[0]
+    dask.__version__ = _dask_ver
 
     # make sure we can resize the cache
-    assert utils.dask_cache.cache.total_bytes >= 1000
+    assert utils.dask_cache.cache.total_bytes > 1000
     utils.resize_dask_cache(1000)
     assert utils.dask_cache.cache.total_bytes <= 1000
 
@@ -44,16 +44,10 @@ def test_dask_array_creates_cache():
     layer2._set_view_slice = mock_set_view_slice2
     layer2.set_view_slice()
 
-    dask.__version__ = _dask_ver
-    utils.dask_cache = None
-
 
 def test_list_of_dask_arrays_creates_cache():
     """Test that adding a list of dask array also creates a dask cache."""
-    assert utils.dask_cache is None
     assert dask.config.get("optimization.fuse.active")
     _ = Image([da.ones((100, 100)), da.ones((20, 20))])
     assert isinstance(utils.dask_cache, dask.cache.Cache)
     assert dask.config.get("optimization.fuse.active")
-    # cleanup
-    utils.dask_cache = None
