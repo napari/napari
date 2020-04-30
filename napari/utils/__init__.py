c03481d5dc7ed650da2ad7a586ca2284e02a987a
Talley Lambert <talley.lambert@gmail.com>
2020-04-30 15:30:13 -0400

    create cache immediately

--- napari/utils/__init__.py
+++ napari/utils/__init__.py
@@ -1,5 +1,8 @@
 from .info import sys_info, citation_text
 from .misc import resize_dask_cache
+from dask.cache import Cache
 
 #: dask.cache.Cache, optional : A dask cache for opportunistic caching
-dask_cache = None
+dask_cache = Cache(0)  # use resize_dask_cache to actually register and resize.
+
+del Cache
