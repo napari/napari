fcab5e5afc4fc403cf823dbe5b162869c4d35fdf
Talley Lambert <talley.lambert@gmail.com>
2020-05-21 17:46:47 -0400

    modify shutil path

--- napari/resources/build_icons.py
+++ napari/resources/build_icons.py
@@ -6,6 +6,7 @@ for styling svg elements using qss
 import os
 import re
 import shutil
+import sys
 from subprocess import check_call
 from typing import Dict, List, Tuple
 
@@ -167,17 +168,22 @@ def build_pyqt_resources(out_path: str, overwrite: bool = False) -> str:
     )
 
     # then convert it to a python file
-    # this looks a little funny, but rather than pick the exact binary to try
-    # once, we use this try/catch loop because
     # 1. we always want to use pyrcc5 if it's available, regardless of API
     # 2. it will sometimes, (if not always) be named pyrcc5.bat on windows...
     #    but shutil.which() will find that too
-    # 3. We also want to prefer binaries higher up on the path
-    # 4. only then do we try pyside2-rcc
+    # 3. We also want to prefer binaries higher up on the path, and we add
+    #    sys.executable to the front of the path (and \\Scripts on windows)
+    # 4. after pyrcc5 we try pyside2-rcc
     # see https://github.com/napari/napari/issues/1221
     # and https://github.com/napari/napari/issues/1254
+    python_dir = os.path.dirname(sys.executable)
+    paths = [python_dir, os.environ.get("PATH", '')]
+    if os.name == 'nt':
+        paths.insert(0, os.path.join(python_dir, 'Scripts'))
+    path = os.pathsep.join(paths)
+
     for bin_name in ('pyrcc5', 'pyside2-rcc'):
-        rcc_binary = shutil.which(bin_name)
+        rcc_binary = shutil.which(bin_name, path=path)
         if rcc_binary:
             check_call([rcc_binary, '-o', out_path, qrc_path])
             break
