72b056265b7c06c5f02bc899b163ddaf2584a23d
ziyang <zliu@chanzuckerbreg.com>
2020-05-21 15:23:50 -0700

    update format

--- napari/_qt/layers/qt_labels_layer.py
+++ napari/_qt/layers/qt_labels_layer.py
@@ -107,7 +107,9 @@ class QtLabelsControls(QtLayerControls):
         self._on_n_dim_change()
 
         preserve_labels_cb = QCheckBox()
-        preserve_labels_cb.setToolTip('preserve existing labels while painting')
+        preserve_labels_cb.setToolTip(
+            'preserve existing labels while painting'
+        )
         preserve_labels_cb.stateChanged.connect(self.change_preserve_labels)
         self.preserveLabelsCheckBox = preserve_labels_cb
         self._on_preserve_labels_change()
