7f3b95656d45091c7c642cc8646135aabd714430
Talley Lambert <talley.lambert@gmail.com>
2020-05-25 08:54:52 -0400

    patch wxs file

--- .github/workflows/patch_briefcase.py
+++ .github/workflows/patch_briefcase.py
@@ -9,3 +9,13 @@ if sys.platform == 'darwin':
     source = source.replace('max(total_size / 1024', 'max(total_size / 1000')
     with open(core.__file__, 'w') as f:
         f.write(source)
+        print("patched dmgbuild")
+
+if sys.platform.startswith("win"):
+    # must run after briefcase build
+    fname = 'windows\\napari\\napari.wxs'
+    with open(fname, 'r') as f:
+        source = f.read()
+    with open(fname, 'w') as f:
+        f.write(source.replace('pythonw.exe', 'python.exe'))
+        print("patched pythonw.exe -> python.exe")
