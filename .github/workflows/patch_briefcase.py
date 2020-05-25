10e79c020d39d6ea561c4feef6f76f5612c89ca6
Talley Lambert <talley.lambert@gmail.com>
2020-05-25 09:42:13 -0400

    ad hoc code sign patch

--- .github/workflows/patch_briefcase.py
+++ .github/workflows/patch_briefcase.py
@@ -4,13 +4,25 @@ import os
 
 if sys.platform == 'darwin':
     from dmgbuild import core
+    from briefcase.platforms.macOS import app
 
     with open(core.__file__, 'r') as f:
         source = f.read()
     source = source.replace('max(total_size / 1024', 'max(total_size / 1000')
     with open(core.__file__, 'w') as f:
         f.write(source)
-        print("patched dmgbuild")
+        print("patched dmgbuild.core")
+
+    with open(app.__file__, 'r') as f:
+        source = f.read()
+    source = source.replace(
+        "# Obtain the valid codesigning identities.",
+        'if identity == "-": return "-"',
+    )
+    with open(app.__file__, 'w') as f:
+        f.write(source)
+        print("patched briefcase.platforms.macOS.app")
+
 
 if sys.platform.startswith("win"):
     # must run after briefcase build
