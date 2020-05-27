7b18a1f3d7cbeccd8d2724a97932a561c4235972
ziyang <zliu@chanzuckerbreg.com>
2020-05-27 12:19:10 -0700

    update doc

--- napari/layers/labels/_labels_mouse_bindings.py
+++ napari/layers/labels/_labels_mouse_bindings.py
@@ -3,7 +3,24 @@ from ._labels_constants import Mode
 
 
 def draw(layer, event):
-    """Paint with the currently selected label."""
+    """Draw with the currently selected label to a coordinate.
+
+    This method have different behavior when draw is called
+    with different labeling layer mode.
+
+    In PAINT mode the cursor functions like a paint brush changing any
+    pixels it brushes over to the current label. If the background label
+    `0` is selected than any pixels will be changed to background and this
+    tool functions like an eraser. The size and shape of the cursor can be
+    adjusted in the properties widget.
+
+    In FILL mode the cursor functions like a fill bucket replacing pixels
+    of the label clicked on with the current label. It can either replace
+    all pixels of that label or just those that are contiguous with the
+    clicked on pixel. If the background label `0` is selected than any
+    pixels will be changed to background and this tool functions like an
+    eraser
+    """
     # on press
     layer._save_history()
     layer._block_saving = True
