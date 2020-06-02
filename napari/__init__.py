38556e7dfcccf555073435c726e84a00a0f9f6f2
Talley Lambert <talley.lambert@gmail.com>
2020-06-01 23:08:23 -0400

    change version when not installed

--- napari/__init__.py
+++ napari/__init__.py
@@ -1,7 +1,7 @@
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = "unknown"
+    __version__ = "not-installed"
 
 import os
 from distutils.version import StrictVersion
