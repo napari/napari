55fc28cab909fa5ff992b39e02aea2ad595b939a
Chris Wood <cjw@stowers.org>
2020-06-05 13:13:04 -0500

    rename combine_layers to images_to_stack. Made suggested changes

--- napari/utils/axis_functions.py
+++ napari/utils/axis_functions.py
@@ -58,60 +58,41 @@ def stack_to_images(stack, axis):
     return imagelist
 
 
-def combine_layers(viewer, rgb=False, keep=True):
+def images_to_stack(images, axis=-1):
     """Function to combine selected image layers in one layer
 
     Parameters
     ----------
     viewer : napari.viewer.Viewer
         The viewer with the selected image
-    rgb : If rgb is True, add the new axis as the last dimension (-1) and
-          set rgb=True in viewer.add_imageß
-    keep : Boolean
-        If true keep the original active layer, otherwise delete it
+    axis : int
+        Index to to insert the new axis
+
+    Returns
+    -------
+    stack : napari.layers.Image
+        Combined image stack
     """
-    selected = viewer.layers.selected
-    nc = len(selected)
+
+    nc = len(images)
     if nc < 2:
         print("less than two layers selected")
-        return
-
-    if rgb is True and (nc != 3 or nc != 4):  # not sure about alpha channel
-        print("RGB image must have 3 or 4 channels")
-        return
-
-    shape = selected[0].shape
-    for i in range(nc):
-        if not isinstance(selected[i], Image):
-            print("Selected layer {} is not an image".format(selected[i].name))
-            return
-        if i == 0:
-            shape = selected[i].data.shape
-        else:
-            if shape != selected[i].data.shape:
-                print("Layers not the same size")
-                return
+        return None
 
     new_list = list()
     for i in range(nc):
-        new_list.append(selected[i].data)
-
-    # this is assuming the y and x dimensions are the last 2
-    if rgb:
-        axis = -1
-    else:
-        axis = -3
+        if not isinstance(images[i], Image):
+            print("Selected layer {} is not an image".format(images[i].name))
+            return None
+        if i == 0:
+            shape = images[i].data.shape
+        else:
+            if shape != images[i].data.shape:
+                print("Layers not the same size")
+                return None
+        new_list.append(images[i].data)
 
     new_data = np.stack(new_list, axis=axis)
-    viewer.add_image(new_data, rgb=rgb)
+    stack = Image(new_data)
 
-    if not keep:
-        for s in selected:
-            viewer.layers.remove(s)
-
-
-if __name__ == '__main__':
-    import napari
-
-    with napari.gui_qt():
-        v = napari.Viewer()
+    return stack
