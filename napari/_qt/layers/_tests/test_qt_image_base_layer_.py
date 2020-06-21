a24bc1a0922dca894b0a0b84dbade0a0f7b726ba
Talley Lambert <talley.lambert@gmail.com>
2020-06-21 13:11:59 -0400

    try showing on windows

--- napari/_qt/layers/_tests/test_qt_image_base_layer_.py
+++ napari/_qt/layers/_tests/test_qt_image_base_layer_.py
@@ -37,7 +37,7 @@ def test_base_controls_creation(qtbot, layer):
 def test_clim_right_click_shows_popup(mock_show, qtbot, layer):
     """Right clicking on the contrast limits slider should show a popup."""
     qtctrl = QtBaseImageControls(layer)
-    if not (os.name == 'nt' and os.getenv("CI")):
+    if os.name == 'nt' and os.getenv("CI"):
         qtctrl.show()
     qtbot.addWidget(qtctrl)
     qtbot.mousePress(qtctrl.contrastLimitsSlider, Qt.RightButton)
