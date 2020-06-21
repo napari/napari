52fbf4006c302be820eaabb85ed55a907017f4c3
Talley Lambert <talley.lambert@gmail.com>
2020-06-21 09:37:05 -0400

    docs

--- napari/_qt/exceptions.py
+++ napari/_qt/exceptions.py
@@ -10,6 +10,17 @@ from ..utils.misc import camel_to_spaces
 
 
 class QtErrorMessageBox(QMessageBox):
+    """A messagebox that formats and displays exceptions.
+
+    Parameters
+    ----------
+    exc_info : tuple, optional
+        a tuple of ``(etype, value, tb)``. as returned by
+        :func:`sys.exc_info()`.
+    parent : QWidget, optional
+        Parent widget, by default None
+    """
+
     def __init__(
         self,
         exc_info: Optional[ExcInfo] = None,
@@ -21,6 +32,7 @@ class QtErrorMessageBox(QMessageBox):
             self.format_exc_info(exc_info)
 
     def format_exc_info(self, exc_info: ExcInfo):
+        """Format exception info and populate the dialog."""
         etype, value, tb = exc_info
         title = camel_to_spaces(etype.__name__ if etype else 'Error')
         self.setWindowTitle(title or "Napari Error")
@@ -50,8 +62,6 @@ class ExceptionHandler(QObject):
 
     def __init__(self):
         super().__init__()
-        # could add a lot more logic about when we actually show the dialog
-        self.error.connect(self._show_error_dialog)
 
     def handler(
         self,
@@ -59,12 +69,35 @@ class ExceptionHandler(QObject):
         value: BaseException,
         tb: TracebackType,
     ):
+        """Our sys.excepthook handler.
+
+        This function handles uncaught exceptions and can delegates to a
+        secondary handler, whether it be a GUI dialog, or an IPython traceback
+        printout.
+
+        The three parameters here are what would be returned from
+        :func:`sys.exc_info()`.
+
+        Parameters
+        ----------
+        etype : Type[BaseException]
+            The type of error raised
+        value : BaseException
+            The error instance
+        tb : TracebackType
+            The traceback object associated with the error.
+        """
         # etype.__module__ contains the module raising the error
         # Custom exception classes can have different behavior
         # can add custom exception handlers here ...
         self.error.emit((etype, value, tb))
 
+        # NOTE: we don't *have* to always show the dialog... we can put
+        # additional logic here to dispatch to secondary handlers.
+        self._show_error_dialog((etype, value, tb))
+
     def _show_error_dialog(self, exc_info: ExcInfo):
+        """Find the current main window and show the error."""
         parent = None
         for wdg in QApplication.topLevelWidgets():
             if isinstance(wdg, QMainWindow):
