87866c6d66a4204de59ed3b8d61060766ae57f82
Philip Winston <pwinston@gmail.com>
2020-06-26 17:15:20 -0400

    Better documentation

--- napari/_qt/qt_chunk_receiver.py
+++ napari/_qt/qt_chunk_receiver.py
@@ -4,24 +4,28 @@ from ..utils.chunk_loader import ChunkRequest, CHUNK_LOADER
 
 
 class QtChunkReceiver(QObject):
-    """Receives newly loaded chunks from the ChunkLoader.
+    """Shuttles newly loaded chunks from the ChunkLoader to layers.
 
     Notes
-    ----
+    -----
     The ChunkLoader's chunk_loaded event might be signaled in a worker
     thread. The documentation only guarantees it will be thread in this
     process, in theory could be any thread.
 
-    We do not want to call into model/layer code in a worker thread
-    so we signal ourselves in the GUI thread. If we are already in the GUI
-    thread it does no harm.
+    We do not want to call into model/layer code in a worker thread so we
+    signal ourselves in the GUI thread. If we are already in the GUI thread
+    this does no harm and happens instantly.
     """
 
     chunk_loaded_gui = Signal(ChunkRequest)
 
     def __init__(self):
         super().__init__()
+
+        # ChunkLoader signals us when a chunk has been loaded.
         CHUNK_LOADER.events.chunk_loaded.connect(self._chunk_loaded_worker)
+
+        # We signal ourself to switch things to the GUI thread (if necessary).
         self.chunk_loaded_gui.connect(self._chunk_loaded_gui)
 
     def _chunk_loaded_worker(self, event) -> None:
@@ -29,8 +33,10 @@ class QtChunkReceiver(QObject):
         self.chunk_loaded_gui.emit(event.request)
 
     def _chunk_loaded_gui(self, request: ChunkRequest) -> None:
-        """A chunk was loaded (gui thread)."""
-        # If this chunk's layer still exists, pass it the chunk.
+        """A chunk was loaded (gui thread).
+
+        If this chunk's layer still exists, pass it the chunk.
+        """
         layer = request.layer_ref()
         if layer is not None:
             layer.chunk_loaded(request)
