8f27a2432c0909623a9cb2c1f777687d34c2c4a6
Talley Lambert <talley.lambert@gmail.com>
2020-07-13 10:12:27 -0400

    export names

--- napari/utils/events/__init__.py
+++ napari/utils/events/__init__.py
@@ -1,3 +1,12 @@
 from .event import EmitterGroup, Event, EventEmitter  # isort:skip
-from .containers._list import EventedList
+from .containers._list import EventedList, SupportsEvents
 from .containers._nested_list import NestableEventedList
+
+__all__ = [
+    'EmitterGroup',
+    'Event',
+    'EventEmitter',
+    'EventedList',
+    'SupportsEvents',
+    'NestableEventedList',
+]
--- napari/utils/events/containers/__init__.py
+++ napari/utils/events/containers/__init__.py
@@ -1,2 +1,4 @@
 from ._list import EventedList
 from ._nested_list import NestableEventedList
+
+__all__ = ['EventedList', 'NestableEventedList']
