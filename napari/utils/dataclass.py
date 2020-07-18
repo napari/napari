faee6efae9d8cbef0f45e0fd7bc4b80ba2b840d7
Talley Lambert <talley.lambert@gmail.com>
2020-07-18 17:14:19 -0400

    complete documentation

--- napari/utils/dataclass.py
+++ napari/utils/dataclass.py
@@ -1,4 +1,4 @@
-import dataclasses as _dataclasses
+import dataclasses as _dataclasses  # builtin private for ease of tab complete
 from typing import Any, Callable, Type, TypeVar
 
 import toolz as tz
@@ -10,13 +10,32 @@ T = TypeVar("T")
 
 
 def make_post_init(
-    orig_post_init: Callable[..., None] = None, events=False, properties=False,
+    cls: Type[T], events=False, properties=False
 ) -> Callable[..., None]:
-    def _event_post_init(self, *initvars) -> None:
+    """Return a new __post_init__ method wrapper with events & properties.
+
+    Parameters
+    ----------
+    cls : type
+        The class being decorated as a dataclass
+    events : bool, optional
+        Whether to add an `EmitterGroup` to the class, by default False
+    properties : bool, optional
+        Whether to convert the dataclass fields to properties, by default False
+
+    Returns
+    -------
+    Callable[..., None]
+        A modified __post_init__ method that wraps the original.
+    """
+
+    orig_post_init: Callable[..., None] = getattr(cls, '__post_init__', None)
+
+    def _event_post_init(self: T, *initvars) -> None:
         emitter_group = EmitterGroup(
             source=self,
             auto_connect=False,
-            **{name: None for name in self.__dataclass_fields__},
+            **{n: None for n in getattr(self, '__dataclass_fields__', {})},
         )
         object.__setattr__(self, 'events', emitter_group)
         if orig_post_init is not None:
@@ -31,6 +50,37 @@ def make_post_init(
 
 
 def setattr_with_events(self: T, name: str, value: Any) -> None:
+    """Modified __setattr__ method that emits an event when set.
+
+    Events will *only* be emitted if the ``name`` of the attribute being set
+    is one of the dataclass fields (i.e. ``name in self.__annotations__``),
+    and the dataclass ``__post_init__` method has already been called.
+
+    Also looks for and calls an optional ``_on_name_set()`` method afterwards.
+
+    Order of operations:
+        1. Call the original ``__setattr__`` function to set the value
+        2. Look for an ``_on_name_set`` method on the object
+            a. If present, call it with the current value
+            b. That method can do anything (including changing the value, or
+               emitting its own events if necessary).  If changing the value,
+               it should check to make sure that it is different than the
+               current value before setting, or a ``RecursionError`` may occur.
+            c. If that method returns ``True``. Return *without* emitting
+               an event.
+        3. If ``_on_name_set`` has not returned ``True``, then emit an event
+           from the EventEmitter with the corresponding ``name`` in the.
+           e.g. ``self.events.<name>(value=value)``.
+
+    Parameters
+    ----------
+    self : T
+        An instance of the decorated dataclass of Type[T]
+    name : str
+        The name of the attribute being set.
+    value : Any
+        The new value for the attribute.
+    """
     object.__setattr__(self, name, value)
     if name in self.__annotations__ and getattr(
         self, '_is_initialized', False
@@ -41,12 +91,15 @@ def setattr_with_events(self: T, name: str, value: Any) -> None:
             # the method can return True, if it wants to handle its own events
             if setter_method(getattr(self, name)):
                 return
-        # otherwise, we emit this event
+        # otherwise, we emit the event
         if name in self.events:  # type: ignore # (needs SupportsEvents)
+            # use gettattr again in case `_on_name_set` has modified it
             getattr(self.events, name)(value=getattr(self, name))  # type: ignore
 
 
 def make_getter(name):
+    """Make an fget function for creating a property descriptor."""
+
     def fget(self):
         return getattr(self, name)
 
@@ -54,13 +107,33 @@ def make_getter(name):
 
 
 def make_setter(name):
+    """Make an fset function for creating a property descriptor."""
+
     def fset(self, value):
         setattr(self, name, value)
 
     return fset
 
 
-def convert_fields_to_properties(obj):
+def convert_fields_to_properties(obj: T):
+    """Convert all fields in a dataclass instance to property descriptors.
+
+    Note: this modifies class Type[T] (the class that was decorated with
+    ``@dataclass``) *after* instantiation of the class.  In other words, for a
+    given field `f` on class `C`, `C.f` will *not* be a property descriptor
+    until *after* C has been instantiated: `c = C()`.  (And reminder: property
+    descriptors are class attributes).
+
+    The reason for this is that dataclasses can have "default factory"
+    functions that create default values for fields only during instantiation,
+    and we don't want to have to recreate that logic here, (but we do need to
+    know what the value of the field is).
+
+    Parameters
+    ----------
+    obj : T
+        An instance of class ``Type[T]`` that has been deorated as a dataclass.
+    """
     from numpydoc.docscrape import ClassDoc
 
     cls = obj.__class__
@@ -142,15 +215,19 @@ def dataclass(
     if properties and frozen:
         raise ValueError("`properties=True` cannot be used with `frozen=True`")
     if events or properties:
-        orig_post_init = getattr(cls, '__post_init__', None)
-        post_init = make_post_init(orig_post_init, events, properties)
+        # create a modified __post_init__ method that will create the
+        # EmitterGroup, and convert fields to properties (if requested)
+        post_init = make_post_init(cls, events, properties)
         setattr(cls, '__post_init__', post_init)
 
-    _cls = _dataclasses._process_class(  # type: ignore
+    # if neither events or properties are True, this function is exactly like
+    # the builtin `dataclasses.dataclass`
+    _cls = _dataclasses._process_class(
         cls, init, repr, eq, order, unsafe_hash, frozen
     )
 
     if events:
-        setattr(_cls, '_is_initialized', False)
+        setattr(_cls, '_is_initialized', False)  # made true in __post_init__
+        # modify __setattr__ with version that emits an event when setting
         _cls.__setattr__ = setattr_with_events
     return _cls
