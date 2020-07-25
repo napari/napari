e214dbf0b3740a59074d998b4fe0fd973a5318b0
Talley Lambert <talley.lambert@gmail.com>
2020-07-25 13:34:46 -0400

    cleanup main_window

--- napari/_qt/qt_main_window.py
+++ napari/_qt/qt_main_window.py
@@ -4,13 +4,9 @@ wrap.
 """
 import time
 
-from qtpy.QtCore import Qt  # noqa: E402
-from qtpy.QtGui import QIcon, QKeySequence  # noqa: E402
-
-# these "# noqa" comments are here to skip flake8 linting (E402),
-# these module-level imports have to come after `app.use_app(API)`
-# see discussion on #638
-from qtpy.QtWidgets import (  # noqa: E402
+from qtpy.QtCore import Qt
+from qtpy.QtGui import QIcon, QKeySequence
+from qtpy.QtWidgets import (
     QAction,
     QApplication,
     QDockWidget,
@@ -25,10 +21,8 @@ from qtpy.QtWidgets import (  # noqa: E402
 
 from ..resources import get_stylesheet
 from ..utils import perf
-
-# set vispy to use same backend as qtpy
 from ..utils.io import imsave
-from ..utils.theme import template  # noqa: E402
+from ..utils.theme import template
 from .qt_about import QtAbout
 from .qt_debug_menu import DebugMenu
 from .qt_plugin_report import QtPluginErrReporter
@@ -36,7 +30,7 @@ from .qt_plugin_sorter import QtPluginSorter
 from .qt_plugin_table import QtPluginTable
 from .qt_viewer import QtViewer
 from .qt_viewer_dock_widget import QtViewerDockWidget
-from .utils import QImg2array  # noqa: E402
+from .utils import QImg2array
 
 
 class Window:
