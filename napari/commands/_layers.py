9c8136df44afd6c787d0e9d9cc5c956ec2439487
Philip Winston <pwinston@gmail.com>
2020-08-06 15:33:38 -0400

    Cleanup, left align names column

--- napari/commands/_layers.py
+++ napari/commands/_layers.py
@@ -1,12 +1,13 @@
-"""LayerCommands and supporting classes.
+"""LayerCommands class and helpers.
 """
-from typing import List, Tuple
+from typing import List
 
 import dask.array as da
 import humanize
 
 from ._tables import print_property_table, RowTable
 from ._utils import highlight
+from ..layers.base import Layer
 
 
 HELP_STR = f"""
@@ -44,7 +45,18 @@ def _get_type_str(data) -> str:
 
 
 def _get_size_str(data) -> str:
-    """Return human readable size like 24.2G."""
+    """Return human readable size.
+
+    Parameters
+    ----------
+    data
+        Layer's data.
+
+    Returns
+    -------
+    str
+        A string size like "24.2G".
+    """
     if isinstance(data, list):
         nbytes = sum(level.nbytes for level in data)
     else:
@@ -53,7 +65,7 @@ def _get_size_str(data) -> str:
 
 
 class ListLayersTable:
-    """Table showing the layers and their names, types and shapes.
+    """Table showing information about each layer.
 
     Parameters
     ----------
@@ -66,10 +78,18 @@ class ListLayersTable:
         Formats our table for printing.
     """
 
-    def __init__(self, layers):
+    def __init__(self, layers: List[Layer]):
         self.layers = layers
         self.table = RowTable(
-            ["ID", "NAME", "LAYER", "DATA", "LEVELS", "SHAPE", "TOTAL"]
+            [
+                "ID",
+                {"name": "NAME", "align": "left"},
+                "LAYER",
+                "DATA",
+                "LEVELS",
+                "SHAPE",
+                "TOTAL",
+            ]
         )
         for i, layer in enumerate(self.layers):
             self._add_row(i, layer)
@@ -89,13 +109,32 @@ class ListLayersTable:
 
     @staticmethod
     def _get_num_levels(data) -> int:
-        """Get the number of levels of the data."""
+        """Get the number of levels of the data.
+
+        Parameters
+        ----------
+        data
+            Layer data.
+
+        Returns
+        -------
+        int
+            The number of levels of the data.
+        """
         if isinstance(data, list):
             return len(data)
         return 1
 
-    def _add_row(self, i, layer):
-        """Add row describing one layer."""
+    def _add_row(self, layer_id: int, layer: Layer) -> int:
+        """Add row describing one layer.
+
+        Parameters
+        ----------
+        layer_id : int
+            The layer id (from view.cmd.layers).
+        layer : Layer
+            The layer itself.
+        """
         layer_type = type(layer).__name__
         num_levels = self._get_num_levels(layer.data)
         shape_str = self._get_shape_str(layer.data)
@@ -104,7 +143,7 @@ class ListLayersTable:
 
         self.table.add_row(
             [
-                i,
+                layer_id,
                 layer.name,
                 layer_type,
                 data_type,
@@ -122,9 +161,15 @@ class ListLayersTable:
 class LevelsTable:
     """Table showing the levels in a single layer.
 
+    Parameters
+    ----------
+    layer_id : int
+        The ID of this layer.
+    layer : Layer
+        Show the levels of this layer.
     """
 
-    def __init__(self, layer_id, layer):
+    def __init__(self, layer_id: int, layer):
         self.layer_id = layer_id
         self.layer = layer
         self.table = RowTable(["LEVEL", "SHAPE", "TOTAL"])
@@ -144,31 +189,16 @@ class LevelsTable:
         self.table.print()
 
 
-def _print_summary(items: List[Tuple[str, str]]):
-    """Print a summary table with aligned headings.
-
-    For example prints:
-
-    Layer ID: 0
-        Name: numbered slices
-      Levels: 1
-       Shape: (20, 1024, 1024, 3)
+class LayerCommands:
+    """Layer related commands for the CommandProcessor.
 
     Parameters
     ----------
-        rows
-    """
-    width = max(len(heading) for heading, _ in items)
-    for heading, value in items:
-        aligned = f"{heading:>{width}}"
-        print(f"{highlight(aligned)}: {value}")
-
-
-class LayerCommands:
-    """Layer related commands for the CommandProcessor.
+    layerlist : List[Layer]
+        The current list of layers.
     """
 
-    def __init__(self, layerlist):
+    def __init__(self, layerlist: List[Layer]):
         self.layerlist = layerlist
 
     @property
@@ -176,7 +206,7 @@ class LayerCommands:
         """Print the current list of layers."""
         ListLayersTable(self.layerlist).print()
 
-    def levels(self, layer_id):
+    def levels(self, layer_id: int) -> None:
         """Print information on a single layer.
 
         Prints summary and if multiscale prints a table of the levels:
