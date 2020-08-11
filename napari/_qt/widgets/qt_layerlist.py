5c090d48beba08c791ef0ad61d032f33e7732090
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-08-11 09:01:57 -0700

    fix flake8

--- napari/_qt/widgets/qt_layerlist.py
+++ napari/_qt/widgets/qt_layerlist.py
@@ -303,7 +303,8 @@ class QtLayerList(QScrollArea):
         drag.setHotSpot(event.pos() - self.rect().topLeft())
         drag.exec_()
         # Check if dragged layer still exists or was deleted during drag
-        dragged_layer_exists = self._drag_name in [l.name for l in self.layers]
+        names = [layer.name for layer in self.layers]
+        dragged_layer_exists = self._drag_name in names
         if self._drag_name is not None and dragged_layer_exists:
             index = self.layers.index(self._drag_name)
             layer = self.layers[index]
