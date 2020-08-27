b1bde118bb129d0654d7e825adc38fc8652dcc4c
MaksHess <max.hess@imls.uzh.ch>
2020-08-27 19:09:11 +0200

    napari

--- napari/_vispy/_tests/test_statusbar.py
+++ napari/_vispy/_tests/test_statusbar.py
@@ -1,36 +1,39 @@
-from napari.layers import Image
-from napari._vispy.vispy_image_layer import VispyImageLayer
 import numpy as np
-from vispy.app import Canvas
-from vispy.gloo import gl
 import pytest
 
-c = Canvas(show=False)
-GL_MAX_TEXTURE_SIZE = gl.glGetParameter(gl.GL_MAX_TEXTURE_SIZE)
+from napari._vispy.vispy_base_layer import get_max_texture_sizes
+from napari._vispy.vispy_image_layer import VispyImageLayer
+from napari.layers import Image
+
+GL_MAX_TEXTURE_SIZE, _ = get_max_texture_sizes()
 
 
-@pytest.mark.parametrize("shape", [(2, 4),
-                                   (12, 3),
-                                   (256, 4048),
-                                   (2, GL_MAX_TEXTURE_SIZE * 2),
-                                   (GL_MAX_TEXTURE_SIZE * 2, 2),
-                                   (2, int(GL_MAX_TEXTURE_SIZE * 1.6)),
-                                   # (int(GL_MAX_TEXTURE_SIZE * 1.1), int(GL_MAX_TEXTURE_SIZE * 1.1)),
-                                   ])
+@pytest.mark.parametrize(
+    "shape",
+    [
+        (2, 4),
+        (12, 3),
+        (256, 4048),
+        (2, GL_MAX_TEXTURE_SIZE * 2),
+        (GL_MAX_TEXTURE_SIZE * 2, 2),
+        (2, int(GL_MAX_TEXTURE_SIZE * 1.6)),
+    ],
+)
 @pytest.mark.filterwarnings("ignore:data shape:UserWarning")
 def test_status_bar(shape):
     data = np.zeros(shape)
-    data[shape[0] // 2:, shape[1] // 2:] = 1
+    data[shape[0] // 2 :, shape[1] // 2 :] = 1
     layer = Image(data)
-    vispy_layer = VispyImageLayer(layer)
+    _ = VispyImageLayer(layer)
 
-    coordinates_shape = layer._transforms.inverse.simplified(data.shape)
+    coordinates_shape = layer._transforms['tile2data'].inverse(data.shape)
 
-    test_points = [(int(coordinates_shape[0] * 0.25), int(coordinates_shape[1] * 0.25)),
-                   (int(coordinates_shape[0] * 0.75), int(coordinates_shape[1] * 0.25)),
-                   (int(coordinates_shape[0] * 0.25), int(coordinates_shape[1] * 0.75)),
-                   (int(coordinates_shape[0] * 0.75), int(coordinates_shape[1] * 0.75)),
-                   ]
+    test_points = [
+        (int(coordinates_shape[0] * 0.25), int(coordinates_shape[1] * 0.25)),
+        (int(coordinates_shape[0] * 0.75), int(coordinates_shape[1] * 0.25)),
+        (int(coordinates_shape[0] * 0.25), int(coordinates_shape[1] * 0.75)),
+        (int(coordinates_shape[0] * 0.75), int(coordinates_shape[1] * 0.75)),
+    ]
     expected_values = [0.0, 0.0, 0.0, 1.0]
 
     for test_point, expected_value in zip(test_points, expected_values):
