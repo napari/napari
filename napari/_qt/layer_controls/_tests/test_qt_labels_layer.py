c0ed3124faa5b317e64d512924e5c4f37216326f
Draga Doncila Pop <ddon0001@student.monash.edu>
2020-08-31 13:41:54 +1000

    Remove test for changing gui updating layer

--- napari/_qt/layer_controls/_tests/test_qt_labels_layer.py
+++ napari/_qt/layer_controls/_tests/test_qt_labels_layer.py
@@ -1,5 +1,4 @@
 import numpy as np
-from qtpy.QtCore import Qt
 
 from napari._qt.layer_controls.qt_labels_controls import QtLabelsControls
 from napari.layers import Labels
@@ -19,15 +18,3 @@ def test_changing_layer_color_mode_updates_combo_box(qtbot):
 
     layer.color_mode = 'selected'
     assert layer.color_mode == qtctrl.colorModeComboBox.currentText()
-
-
-def test_changing_combo_box_updates_layer_color_mode(qtbot):
-    """Selecting a new color mode via combo box updates layer's color mode"""
-    layer = Labels(_LABELS)
-    qtctrl = QtLabelsControls(layer)
-    qtbot.addWidget(qtctrl)
-    color_mode_comboBox = qtctrl.colorModeComboBox
-
-    index = color_mode_comboBox.findText('selected', Qt.MatchFixedString)
-    color_mode_comboBox.setCurrentIndex(index)
-    assert layer.color_mode == color_mode_comboBox.currentText()
