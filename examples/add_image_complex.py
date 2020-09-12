d590c7793032d66e83b7bdda9e74b078fb61ef64
Talley Lambert <talley.lambert@gmail.com>
2020-09-12 13:10:21 -0400

    add example

---
+++ examples/add_image_complex.py
@@ -0,0 +1,24 @@
+"""
+Display one image using the add_image API.
+"""
+
+import napari
+import numpy as np
+
+
+def complex_ramp(size=256, phase_range=(-np.pi, np.pi), mag_range=(0, 10)):
+    """Returns a complex array where X ramps phase and Y ramps magnitude."""
+    p0, p1 = phase_range
+    phase_ramp = np.linspace(p0, p1 - 1 / size, size)
+    m0, m1 = mag_range
+    mag_ramp = np.linspace(m1, m0 + 1 / size, size)
+    phase_ramp, mag_ramp = np.meshgrid(phase_ramp, mag_ramp)
+    return mag_ramp * np.exp(1j * phase_ramp)
+
+
+with napari.gui_qt():
+    # create the viewer with an image
+    viewer = napari.Viewer()
+    # a test sample in which the X dimension is a phase ramp from -ip to pi
+    # and Y is a magnitude ramp from 0 - 10
+    viewer.add_image(complex_ramp(512), name="complex ramp")
