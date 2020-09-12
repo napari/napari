284895ee985c03433100ad4438c9a6083b21e3bd
Talley Lambert <talley.lambert@gmail.com>
2020-09-12 13:08:08 -0400

    simplify for now

--- napari/_vispy/vendored/complex_image.py
+++ napari/_vispy/vendored/complex_image.py
@@ -10,6 +10,8 @@ from vispy.ext.six import string_types
 
 
 class ComplexImageVisual(ImageVisual):
+    _complex_modes = {'magnitude', 'phase', 'real', 'imaginary'}
+
     def __init__(
         self,
         data=None,
@@ -19,7 +21,7 @@ class ComplexImageVisual(ImageVisual):
         clim='auto',
         gamma=1.0,
         interpolation='nearest',
-        complex_mode='phase',
+        complex_mode='magnitude',
         **kwargs,
     ):
         self._x = False
@@ -31,14 +33,11 @@ class ComplexImageVisual(ImageVisual):
             format='luminance',
         )
         super().__init__()
-        if self._interpolation == 'bilinear':
-            texture_interpolation = 'linear'
-        else:
-            texture_interpolation = 'nearest'
 
+        interp = 'linear' if self._interpolation == 'bilinear' else 'nearest'
         self._texture = Texture2D(
             np.zeros((1, 1)),
-            interpolation=texture_interpolation,
+            interpolation=interp,
             internalformat='r32f',
             format='luminance',
         )
@@ -73,42 +72,36 @@ class ComplexImageVisual(ImageVisual):
 
     @complex_mode.setter
     def complex_mode(self, value):
-        self._complex_mode = value
-        self._need_colortransform_update = True
-        self.update()
+        if value not in self._complex_modes:
+            raise ValueError(
+                "complex_mode must be one of %s"
+                % ', '.join(self._complex_modes)
+            )
+        if self._complex_mode != value:
+            self._complex_mode = value
+            self._need_colortransform_update = True
+            self.update()
 
     def _build_color_transform(self, data, clim, gamma, cmap):
 
-        self._complex_mode = 'magnitude'
-
-        _gray_funcs = {
+        mode_funcs = {
             'magnitude': _complex_mag,
             'phase': _complex_angle,
             'real': _complex_real,
             'imaginary': _complex_imaginary,
         }
-        print(cmap.glsl_map)
-        if self.complex_mode in _gray_funcs:
-            fclim = Function(_apply_clim)
-            fclim['clim'] = clim
-            fgamma = Function(_apply_gamma)
-            fgamma['gamma'] = gamma
-            chain = [
-                Function(_gray_funcs[self.complex_mode]),
-                fclim,
-                fgamma,
-                Function(cmap.glsl_map),
-            ]
-            fun = FunctionChain(None, chain)
-        elif self.complex_mode == 'colormap':
-            fclim = Function(_apply_clim)
-            fclim['clim'] = clim
-            chain = [
-                Function(_gray_funcs['phase']),
-                fclim,
-                Function(cmap.glsl_map),
-            ]
-        return fun
+
+        fclim = Function(_apply_clim)
+        fgamma = Function(_apply_gamma)
+        fclim['clim'] = clim
+        fgamma['gamma'] = gamma
+        chain = [
+            Function(mode_funcs[self.complex_mode]),
+            fclim,
+            fgamma,
+            Function(cmap.glsl_map),
+        ]
+        return FunctionChain(None, chain)
 
     _texture_lookup = """
         vec2 texture_lookup(vec2 texcoord) {
@@ -123,22 +116,22 @@ class ComplexImageVisual(ImageVisual):
 
 
 _complex_mag = """
-    float complex_mag(vec2 data) {
+    float comp2float(vec2 data) {
         return sqrt(data.x * data.x + data.y * data.y);
     }"""
 
 _complex_angle = """
-    float complex_angle(vec2 data) {
+    float comp2float(vec2 data) {
         return atan(data.y, data.x);
     }"""
 
 _complex_real = """
-    float complex_real(vec2 data) {
+    float comp2float(vec2 data) {
         return data.x;
     }"""
 
 _complex_imaginary = """
-    float complex_angle(vec2 data) {
+    float comp2float(vec2 data) {
         return data.y;
     }"""
 
