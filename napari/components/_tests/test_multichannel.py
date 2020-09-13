56c284a5d6ec192ea7afd233cae008c26d7299f8
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-09-13 15:59:05 -0700

    fix dask check

--- napari/components/_tests/test_multichannel.py
+++ napari/components/_tests/test_multichannel.py
@@ -191,7 +191,10 @@ def test_multichannel_dask_array():
     assert len(viewer.layers) == data.shape[0]
     for i in range(data.shape[0]):
         assert viewer.layers[i].data.shape == data.shape[1:]
-        assert isinstance(viewer.layers[i].data, da.Array)
+        # For compatibility with dask==0.2.1 check da.Array or da.core.Array
+        assert isinstance(viewer.layers[i].data, da.Array) or isinstance(
+            viewer.layers[i].data, da.core.Array
+        )
 
 
 def test_forgot_multichannel_error_hint():
