2eb049d8dac5c6b67bbe873103e14acb962d6eb8
Abhishek Patil <abhishek@zeroth.me>
2020-10-03 20:45:44 +1000

    Added the comments and updated the code to check the and of acquisition

--- examples/live_tiffs_generator.py
+++ examples/live_tiffs_generator.py
@@ -16,7 +16,7 @@ import tifffile
 parser = argparse.ArgumentParser()
 parser.add_argument('outdir', help='output directory for tiffs')
 parser.add_argument('--sleep-time', help='how long to sleep between volumes, in seconds', type=float, default=1.0)
-parser.add_argument('-n', help='total number of volumes', default=100)
+parser.add_argument('-n', help='total number of volumes', type=int, default=100)
 
 
 def main(argv=sys.argv[1:]):
@@ -27,11 +27,14 @@ def main(argv=sys.argv[1:]):
     fractions = np.linspace(0.05, 0.5, n)
     os.makedirs(outdir, exist_ok=True)
     for i, f in enumerate(fractions):
+        # We are using skimage binary_blobs which generate's synthetic binary 
+        # image with several rounded blob-like objects and write them into files.
         curr_vol = 255 * data.binary_blobs(
             length=128, blob_size_fraction=0.05, n_dim=3, volume_fraction=f
         ).astype(np.uint8)
         tifffile.imwrite(os.path.join(outdir, f'{i}.tiff'), curr_vol, compress=6)
         time.sleep(sleep_time)
+    # create a final.log file as an indicator for end of acquisition
     final_file = open(os.path.join(outdir, 'final.log'), 'w')
     final_file.close()
 
