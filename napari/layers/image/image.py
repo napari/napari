f4b32c3d7a70b1e43ae0ccfea41e545141b283ff
Max Hess <44779484+MaksHess@users.noreply.github.com>
2020-10-04 21:36:39 +0200

    Status bar value bug for automatically down-sampled images (#1577)
    * fix status bar bug
    
    * use simplified transform to fix status bar bug
    
    * use simplified transform to fix status bar bug
    
    * napari
    
    * use correct transform
    
    * update coord change code
    
    * fix tests
    
    * fix test
    
    Co-authored-by: HessMax <hess.max.timo@gmail.com>
    Co-authored-by: Nicholas Sofroniew <sofroniewn@gmail.com>

--- napari/layers/image/image.py
+++ napari/layers/image/image.py
@@ -728,7 +728,15 @@ class Image(IntensityVisualizationMixin, Layer):
         value : tuple
             Value of the data at the coord.
         """
-        coord = np.round(self.coordinates).astype(int)
+        if self.multiscale:
+            # for multiscale data map the coordinate from the data back to
+            # the tile
+            coord = self._transforms['tile2data'].inverse(self.coordinates)
+        else:
+            coord = self.coordinates
+
+        coord = np.round(coord).astype(int)
+
         raw = self._slice.image.raw
         if self.rgb:
             shape = raw.shape[:-1]
