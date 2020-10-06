e407be9efdc6a175bbca4c91f94ef2f6a943acf1
Philip Winston <pwinston@gmail.com>
2020-10-06 13:44:42 -0400

    Fix tests for viewer commands

--- napari/components/experimental/chunk/_commands/_tests/test_loader.py
+++ napari/components/experimental/chunk/_commands/_tests/test_loader.py
@@ -6,7 +6,7 @@ from napari.components.experimental.chunk._commands import LoaderCommands
 def test_help(make_test_viewer, capsys):
     """Test loader.help."""
     viewer = make_test_viewer()
-    loader = viewer.experimental.loader
+    loader = loader = viewer.experimental.cmds.loader
 
     loader.help
     out, _ = capsys.readouterr()
@@ -25,7 +25,7 @@ def test_no_layers(make_test_viewer, capsys):
 def test_one_layer(make_test_viewer, capsys):
     """Test loader.layer with one layer."""
     viewer = make_test_viewer()
-    loader = viewer.experimental.loader
+    loader = viewer.experimental.cmds.loader
 
     data = np.random.random((10, 15))
     viewer.add_image(data, name="pizza")
@@ -38,7 +38,7 @@ def test_one_layer(make_test_viewer, capsys):
 def test_many_layers(make_test_viewer, capsys):
     """Test loader.layer with many layers."""
     viewer = make_test_viewer()
-    loader = viewer.experimental.loader
+    loader = viewer.experimental.cmds.loader
 
     num_images = 10
     for i in range(num_images):
@@ -52,7 +52,7 @@ def test_many_layers(make_test_viewer, capsys):
 def test_levels(make_test_viewer, capsys):
     """Test loader.levels."""
     viewer = make_test_viewer()
-    loader = viewer.experimental.loader
+    loader = viewer.experimental.cmds.loader
 
     data = np.random.random((10, 15))
     viewer.add_image(data, name="pizza")
