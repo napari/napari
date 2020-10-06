48ae99ac763f6190384395819f51f674887ab4d4
Philip Winston <pwinston@gmail.com>
2020-10-06 11:51:16 -0400

    Fix LoaderComman test

--- napari/components/experimental/chunk/_commands/_tests/test_loader.py
+++ napari/components/experimental/chunk/_commands/_tests/test_loader.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-from napari._commands import LoaderCommands
+from napari.components.experimental.chunk._commands import LoaderCommands
 
 
 def test_help(make_test_viewer, capsys):
     """Test loader.help."""
     viewer = make_test_viewer()
-    viewer.loader.help
+    loader = viewer.experimental.loader
+
+    loader.help
     out, _ = capsys.readouterr()
     assert out.count('\n') >= 4
 
@@ -23,9 +25,11 @@ def test_no_layers(make_test_viewer, capsys):
 def test_one_layer(make_test_viewer, capsys):
     """Test loader.layer with one layer."""
     viewer = make_test_viewer()
+    loader = viewer.experimental.loader
+
     data = np.random.random((10, 15))
     viewer.add_image(data, name="pizza")
-    viewer.loader.layers
+    loader.layers
     out, _ = capsys.readouterr()
     assert "pizza" in out
     assert "(10, 15)" in out
@@ -34,11 +38,13 @@ def test_one_layer(make_test_viewer, capsys):
 def test_many_layers(make_test_viewer, capsys):
     """Test loader.layer with many layers."""
     viewer = make_test_viewer()
+    loader = viewer.experimental.loader
+
     num_images = 10
     for i in range(num_images):
         data = np.random.random((10, 15))
         viewer.add_image(data)
-    viewer.loader.layers
+    loader.layers
     out, _ = capsys.readouterr()
     assert out.count("(10, 15)") == num_images
 
@@ -46,9 +52,11 @@ def test_many_layers(make_test_viewer, capsys):
 def test_levels(make_test_viewer, capsys):
     """Test loader.levels."""
     viewer = make_test_viewer()
+    loader = viewer.experimental.loader
+
     data = np.random.random((10, 15))
     viewer.add_image(data, name="pizza")
-    viewer.loader.levels(0)
+    loader.levels(0)
     out, _ = capsys.readouterr()
 
     # Output has color escape codes to have to check one word at a time.
