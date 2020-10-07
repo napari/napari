cb37eb86a87155db19fa616f51418542850af035
Philip Winston <pwinston@gmail.com>
2020-10-07 18:10:06 -0400

    Add vispy_chunked_image_layer

--- napari/_vispy/utils.py
+++ napari/_vispy/utils.py
@@ -1,3 +1,5 @@
+import os
+
 from ..layers import Image, Points, Shapes, Surface, Tracks, Vectors
 from .vispy_image_layer import VispyImageLayer
 from .vispy_points_layer import VispyPointsLayer
@@ -6,8 +8,23 @@ from .vispy_surface_layer import VispySurfaceLayer
 from .vispy_tracks_layer import VispyTracksLayer
 from .vispy_vectors_layer import VispyVectorsLayer
 
+_use_async = os.getenv("NAPARI_ASYNC", "0") != "0"
+
+
+def _vispy_image_layer():
+    """Return which Vispy Image Layer to use."""
+    if _use_async:
+        from .experimental.vispy_chunked_image_layer import (
+            VispyChunkedImageLayer,
+        )
+
+        return VispyChunkedImageLayer
+    else:
+        return VispyImageLayer
+
+
 layer_to_visual = {
-    Image: VispyImageLayer,
+    Image: _vispy_image_layer(),
     Points: VispyPointsLayer,
     Shapes: VispyShapesLayer,
     Surface: VispySurfaceLayer,
