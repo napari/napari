ccbe8f5b67e26aeb05074680ab0a4302ee4225be
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2020-10-14 13:43:18 +0200

    fix test

--- napari/_qt/layer_controls/_tests/test_qt_tracks_layer.py
+++ napari/_qt/layer_controls/_tests/test_qt_tracks_layer.py
@@ -10,23 +10,26 @@ _PROPERTIES = {'speed': [50, 30], 'time': [0, 1]}
 
 def test_tracks_controls_color_by(qtbot):
     """Check updating of the color_by combobox."""
-    layer = Tracks(_TRACKS, properties=_PROPERTIES, color_by='speed')
+    inital_color_by = 'time'
+    layer = Tracks(_TRACKS, properties=_PROPERTIES, color_by=inital_color_by)
     qtctrl = QtTracksControls(layer)
     qtbot.addWidget(qtctrl)
 
     # verify the color_by argument is initialized correctly
-    assert layer.color_by == 'speed'
-    assert qtctrl.color_by_combobox.currentText() == 'speed'
+    assert layer.color_by == inital_color_by
+    assert qtctrl.color_by_combobox.currentText() == inital_color_by
 
     # update color_by from the layer model
-    layer.color_by = 'time'
-    assert layer.color_by == 'time'
-    assert qtctrl.color_by_combobox.currentText() == 'time'
+    layer_update_color_by = 'speed'
+    layer.color_by = layer_update_color_by
+    assert layer.color_by == layer_update_color_by
+    assert qtctrl.color_by_combobox.currentText() == layer_update_color_by
 
     # update color_by from the qt controls
+    qt_update_color_by = 'track_id'
     speed_index = qtctrl.color_by_combobox.findText(
-        'speed', Qt.MatchFixedString
+        qt_update_color_by, Qt.MatchFixedString
     )
     qtctrl.color_by_combobox.setCurrentIndex(speed_index)
-    assert layer.color_by == 'speed'
-    assert qtctrl.color_by_combobox.currentText() == 'speed'
+    assert layer.color_by == qt_update_color_by
+    assert qtctrl.color_by_combobox.currentText() == qt_update_color_by
