73287d6b3d9123210938477d72e00a721a254c14
ziyang <zliu@chanzuckerbreg.com>
2020-10-14 16:46:00 -0700

    try use one liner to add the listener

--- napari/_qt/qt_main_window.py
+++ napari/_qt/qt_main_window.py
@@ -71,7 +71,7 @@ class Window:
         self._qt_window.setUnifiedTitleAndToolBarOnMac(True)
 
         # since we initialize canvas before window, we need to manually connect them again.
-        if hasattr(self.qt_viewer.canvas, "_backend"):
+        if hasattr(self._qt_window, "windowHandle"):
             self._qt_window.windowHandle().screenChanged.connect(
                 self.qt_viewer.canvas._backend.screen_changed
             )
