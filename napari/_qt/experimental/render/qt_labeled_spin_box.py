c64456ea7d0ba76ff34f8bac00c22bfdd15215e2
Philip Winston <pwinston@gmail.com>
2020-10-23 09:11:26 -0400

    Cleanup the spin box class

--- napari/_qt/experimental/render/qt_labeled_spin_box.py
+++ napari/_qt/experimental/render/qt_labeled_spin_box.py
@@ -7,8 +7,8 @@ from qtpy.QtWidgets import QHBoxLayout, QLabel, QSpinBox
 class LabeledSpinBox:
     """A label plus a SpinBox for the QtRender widget.
 
-    This was cobbled together quickly for QtRender. Could we use some better
-    napari-standard control here instead?
+    This was cobbled together quickly for QtRender. We could probably use
+    some napari-standard control instead? This is probably not good Qt.
     """
 
     def __init__(
@@ -19,22 +19,18 @@ class LabeledSpinBox:
         spin_range: range,
         connect=None,
     ):
-        label = QLabel(label_text)
-        self.box = self._create_spin_box(initial_value, spin_range)
         self.connect = connect
 
-        if connect is not None:
-            self.box.valueChanged.connect(connect)
-
         layout = QHBoxLayout()
-        layout.addWidget(label)
-        layout.addWidget(self.box)
+
+        layout.addWidget(QLabel(label_text))
+        self.spin = self._create(initial_value, spin_range)
+        layout.addWidget(self.spin)
+
         parent.addLayout(layout)
 
-    def _create_spin_box(
-        self, initial_value: int, spin_range: range
-    ) -> QSpinBox:
-        """Return one configured QSpinBox.
+    def _create(self, initial_value: int, spin_range: range) -> QSpinBox:
+        """Return the configured QSpinBox.
 
         Parameters
         ----------
@@ -48,29 +44,49 @@ class LabeledSpinBox:
         QSpinBox
             The configured QSpinBox.
         """
-        box = QSpinBox()
-        box.setKeyboardTracking(False)
-        box.setMinimum(spin_range.start)
-        box.setMaximum(spin_range.stop)
-        box.setSingleStep(spin_range.step)
-        box.setAlignment(Qt.AlignCenter)
-        box.setValue(initial_value)
-        box.valueChanged.connect(self._on_change)
-        return box
+        spin = QSpinBox()
 
-    def _on_change(self, value) -> None:
-        """Called when the spin box value was changed."""
+        spin.setValue(initial_value)
+
+        spin.setMinimum(spin_range.start)
+        spin.setMaximum(spin_range.stop)
+        spin.setSingleStep(spin_range.step)
+
+        spin.setKeyboardTracking(False)  # correct?
+        spin.setAlignment(Qt.AlignCenter)
+
+        spin.valueChanged.connect(self._on_change)
+        return spin
+
+    def _on_change(self, value: int) -> None:
+        """Called when the spin spin value was changed.
+
+        value : int
+            The new value of the SpinBox.
+        """
         # We must clearFocus or it would double-step, no idea why.
-        self.box.clearFocus()
+        self.spin.clearFocus()
 
         # Notify any connection we have.
         if self.connect is not None:
             self.connect(value)
 
     def value(self) -> int:
-        """Return the current value of the QSpinBox."""
-        return self.box.value()
+        """Return the current value of the QSpinBox.
 
-    def set(self, value) -> None:
-        """Set the current value of the QSpinBox."""
-        self.box.setValue(value)
+        Return
+        ------
+        int
+            The current value of the SpinBox.
+        """
+        return self.spin.value()
+
+    def set(self, value: int) -> None:
+        """Set the current value of the SpinBox.
+
+        Parameters
+        ----------
+        value : int
+            Set SpinBox to this value.
+        """
+        self.spin.setValue(value)
