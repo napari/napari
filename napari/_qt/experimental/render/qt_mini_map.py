0533db43b71878768048990d20e9324126bcd5a9
Philip Winston <pwinston@gmail.com>
2020-10-27 14:25:01 -0400

    Add comments, factor out _draw_view() method

--- napari/_qt/experimental/render/qt_mini_map.py
+++ napari/_qt/experimental/render/qt_mini_map.py
@@ -61,36 +61,63 @@ class MiniMap(QLabel):
         Parameters
         ----------
         intersection : OctreeIntersection
-            The intersection we are drawing on the map.
+            Draw this intersection on the map.
         """
         tile_shape = intersection.info.tile_shape
         aspect = intersection.info.octree_info.aspect
 
+        # Shape of the map bitmap.
         map_shape = (MAP_WIDTH, math.ceil(MAP_WIDTH / aspect))
-        tile_size = math.ceil(map_shape[1] / tile_shape[1])
 
+        # The map shape with RGBA pixels
         bitmap_shape = map_shape + (4,)
+
+        # The bitmap data.
         data = np.zeros(bitmap_shape, dtype=np.uint8)
 
+        # Tile size in bitmap coordinates.
+        tile_size = math.ceil(map_shape[1] / tile_shape[1])
+
+        # Border between the tiles is twice this.
+        HALF_BORDER = 1
+
+        # TODO_OCTREE: Can we remove these for loops? We are only looping
+        # over tiles, not pixels. But will be slow with enough tiles.
         for row in range(0, tile_shape[0]):
             for col in range(0, tile_shape[1]):
-                visible = intersection.is_visible(row, col)
 
-                y0 = row * tile_size + 1
-                y1 = y0 + tile_size - 1
+                # Is this tile in the view?
+                seen = intersection.is_visible(row, col)
 
-                x0 = col * tile_size + 1
-                x1 = x0 + tile_size - 1
+                # Coordinate for this one tile.
+                y0 = row * tile_size + HALF_BORDER
+                y1 = y0 + tile_size - HALF_BORDER
 
-                data[y0:y1, x0:x1, :] = COLOR_SEEN if visible else COLOR_UNSEEN
+                x0 = col * tile_size + HALF_BORDER
+                x1 = x0 + tile_size - HALF_BORDER
 
-        max_y = map_shape[0] - 1
-        max_x = map_shape[1] - 1
+                # Draw one tile.
+                data[y0:y1, x0:x1, :] = COLOR_SEEN if seen else COLOR_UNSEEN
+
+        self._draw_view(data, intersection)
+        return data
+
+    def _draw_view(self, data, intersection: OctreeIntersection) -> None:
+        """Draw the view rectangle onto the map data.
+
+        Parameters
+        ----------
+        data : np.ndarray
+            Draw the view into this data.
+        intersection : OctreeIntersection
+            Draw the view in this intersection.
+        """
+
+        # Tiles have been draw. Now draw the view rectangle.
+        max_y = data.shape[0] - 1
+        max_x = data.shape[1] - 1
 
         rows = (intersection.normalized_rows * max_y).astype(int)
         cols = (intersection.normalized_cols * max_x).astype(int)
 
-        # TODO_OCTREE: more compact way to do this?
         data[rows[0] : rows[1], cols[0] : cols[1], :] = COLOR_VIEW
-
-        return data
