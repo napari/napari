d0751f9806944d76b0bafba59dbfc145481db8f6
Philip Winston <pwinston@gmail.com>
2020-10-27 14:35:47 -0400

    Comments

--- napari/_qt/experimental/render/qt_image_info.py
+++ napari/_qt/experimental/render/qt_image_info.py
@@ -6,7 +6,7 @@ from qtpy.QtWidgets import QFrame, QLabel, QVBoxLayout
 
 
 class QtImageInfoLayout(QVBoxLayout):
-    """Layout of the image info frame.
+    """Image info is generic to all image layers (not just Octree).
 
     Parameters
     ----------
@@ -18,13 +18,13 @@ class QtImageInfoLayout(QVBoxLayout):
         super().__init__()
 
         # TODO_OCTREE: This class was going to have more, if not
-        # we can get rid of it.
+        # we can role this into QtRender itself?
         shape = layer.data.shape
         self.addWidget(QLabel(f"Shape: {shape}"))
 
 
 class QtImageInfo(QFrame):
-    """Frame showing image shape and dimensions.
+    """Frame showing image shape.
 
     layer : Layer
         Show info about this layer.
