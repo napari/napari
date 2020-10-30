50375280a8e914b0473fbb6bd04417c84ee29fd0
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-10-30 09:05:08 -0700

    make main window private

--- napari/viewer.py
+++ napari/viewer.py
@@ -1,14 +1,15 @@
 import platform
 import sys
+import warnings
 from os.path import dirname, join
 
 from qtpy.QtGui import QIcon
-from qtpy.QtWidgets import QApplication
+from qtpy.QtWidgets import QApplication, QWidget
 
 from . import __version__
 from ._qt.qt_main_window import Window
 from ._qt.qt_viewer import QtViewer
-from ._qt.qthreading import create_worker, wait_for_workers_to_quit
+from ._qt.qthreading import wait_for_workers_to_quit
 from .components import ViewerModel
 from .utils import config
 from .utils.perf import perf_config
@@ -106,7 +107,20 @@ class Viewer(ViewerModel):
             axis_labels=axis_labels,
         )
         qt_viewer = QtViewer(self)
-        self.window = Window(qt_viewer, show=show)
+        self._window = Window(qt_viewer, show=show)
+
+    @property
+    def window(self):
+        warnings.warn(
+            (
+                "The viewer.window parameter is deprecated and will be removed in version 0.4.2."
+                " Instead you should use the viewer.add_dock_widget method to extend the GUI."
+                " Directly manipulation of the window is no longer supported."
+            ),
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
+        return self._window
 
     def update_console(self, variables):
         """Update console's namespace with desired variables.
@@ -121,10 +135,10 @@ class Viewer(ViewerModel):
             give (list/tuple/str) then the variable values looked up in the
             callers frame.
         """
-        if self.window.qt_viewer.console is None:
+        if self._window.qt_viewer.console is None:
             return
         else:
-            self.window.qt_viewer.console.push(variables)
+            self._window.qt_viewer.console.push(variables)
 
     def screenshot(self, path=None, *, canvas_only=True):
         """Take currently displayed screen and convert to an image array.
@@ -145,28 +159,58 @@ class Viewer(ViewerModel):
             upper-left corner of the rendered region.
         """
         if canvas_only:
-            image = self.window.qt_viewer.screenshot(path=path)
+            image = self._window.qt_viewer.screenshot(path=path)
         else:
-            image = self.window.screenshot(path=path)
+            image = self._window.screenshot(path=path)
         return image
 
-    def update(self, func, *args, **kwargs):
-        import warnings
+    def add_dock_widget(
+        self,
+        widget: QWidget,
+        *,
+        name: str = '',
+        area: str = 'bottom',
+        allowed_areas=None,
+        shortcut=None,
+    ):
+        """Add a Dock Widget to the main window to extend the GUI functionality.
 
-        warnings.warn(
-            "Viewer.update() is deprecated, use "
-            "create_worker(func, *args, **kwargs) instead",
-            DeprecationWarning,
+        Parameters
+        ----------
+        widget : QWidget
+            `widget` will be added as QDockWidget's main widget.
+        name : str, optional
+            Name of dock widget to appear in window menu.
+        area : str
+            Side of the main window to which the new dock widget will be added.
+            Must be in {'left', 'right', 'top', 'bottom'}
+        allowed_areas : list[str], optional
+            Areas, relative to main window, that the widget is allowed dock.
+            Each item in list must be in {'left', 'right', 'top', 'bottom'}
+            By default, all areas are allowed.
+        shortcut : str, optional
+            Keyboard shortcut to appear in dropdown menu.
+
+        Returns
+        -------
+        dock_widget : QtViewerDockWidget
+            `dock_widget` that can pass viewer events.
+        """
+        return self._window.add_dock_widget(
+            widget=widget,
+            name=name,
+            area=area,
+            allowed_areas=allowed_areas,
+            shortcut=shortcut,
         )
-        return create_worker(func, *args, **kwargs, _start_thread=True)
 
     def show(self):
         """Resize, show, and raise the viewer window."""
-        self.window.show()
+        self._window.show()
 
     def close(self):
         """Close the viewer window."""
-        self.window.close()
+        self._window.close()
 
         if config.async_loading:
             from .components.experimental.chunk import chunk_loader
