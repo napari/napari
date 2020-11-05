fix of scaling

napari/layers/labels/labels.py
@@ -766,7 +766,7 @@ class Labels(Image):
             self._save_history()
         brush_size_dims = [self.brush_size] * self.ndim
         # Scale the brush size from world coorinates to data coordinates
-        brush_size_dims = np.divide(brush_size_dims, self.scale)
+        # brush_size_dims = np.divide(brush_size_dims, self.scale)
 
         if not self.n_dimensional and self.ndim > 2:
             for i in self._dims.not_displayed: