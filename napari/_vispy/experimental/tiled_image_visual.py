833cf2086b191e8e7b9cddcad71ebcc6a5410390
Philip Winston <pwinston@gmail.com>
2020-11-06 12:35:06 -0500

    Better comment

--- napari/_vispy/experimental/tiled_image_visual.py
+++ napari/_vispy/experimental/tiled_image_visual.py
@@ -6,18 +6,28 @@ from .._vendored import ImageVisual
 class TiledImageVisual(ImageVisual):
     """Extend ImageVisual with a texture atlas of image tiles.
 
-    TiledImageVisual represents a single image that has been broken down
-    spatially into square tiles. The size of the tiles is configurable,
-    but for example we might use 256x256 pixel tiles.
+    TiledImageVisual draws a set of square image tiles. The size of the
+    tiles is configurable, but 256x256 or 512x512 might be good choices.
+    All the tiles in one TiledImageVisual are the same size.
 
-    The tiles are stored in large textures, also configurable. For example
-    a 4096x4096 texture could store 256 different 256x256 tiles.
+    The tiles are stored in larger textures as an "atlas", basically a grid
+    of the small tiles with no borders between them. The size of the larger
+    textures is also configurable. For example a single 4096x4096 texture
+    could store 256 different 256x256 tiles.
 
-    Adding or removing tiles from a TiledImageVisual is meant to be
-    efficient. You might add a number of different tiles in one frame. And
-    then drawing the full image using tiles is also supposed to be
-    efficient, since we minimize the number of texture swaps.
+    Adding or removing tiles from a TiledImageVisual is efficient. Only the
+    bytes in the tile being updated are send to the card. The Vispy
+    BaseTexture.set_data() method has an "offset" argument. When setting
+    data with an offset Vispy calls glTexSubImage2D() to modify only the
+    impacted sub-region inside the larger texture.
+
+    Uploading tiles does not cause the shader to rebuilt. This is another
+    reason TiledImageVisuals is faster than creating new ImageVisuals.
+
+    Finally, rendering the tiles is also efficient. In one draw pass
+    TiledImageVisual can render all the tiles. If all the tiles are stored
+    in one large texture, there will be zero texture swaps.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(*args, **kwargs):
         super().__init__(*args, **kwargs)
