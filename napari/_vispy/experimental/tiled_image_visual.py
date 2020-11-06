d17e266354c606902ffaa9a778674e75a48d081a
Philip Winston <pwinston@gmail.com>
2020-11-06 12:42:31 -0500

    Revise comment again

--- napari/_vispy/experimental/tiled_image_visual.py
+++ napari/_vispy/experimental/tiled_image_visual.py
@@ -4,29 +4,32 @@ from .._vendored import ImageVisual
 
 
 class TiledImageVisual(ImageVisual):
-    """Extend ImageVisual with a texture atlas of image tiles.
+    """A larger image that's drawn using some number of smaller tiles.
 
-    TiledImageVisual draws a set of square image tiles. The size of the
-    tiles is configurable, but 256x256 or 512x512 might be good choices.
-    All the tiles in one TiledImageVisual are the same size.
+    TiledImageVisual draws a single large image using a set of square image
+    tiles. The size of the tiles is configurable, but 256x256 or 512x512
+    might be good choices. All the tiles in one TiledImageVisual are the
+    same size.
 
-    The tiles are stored in larger textures as an "atlas", basically a grid
-    of the small tiles with no borders between them. The size of the larger
-    textures is also configurable. For example a single 4096x4096 texture
-    could store 256 different 256x256 tiles.
+    The tiles are stored in larger textures as an "atlas". An atlas is
+    basically just a texture which looks like a grid of smaller images. The
+    grid has no borders between the tiles. The size of the larger textures
+    is also configurable. For example a single 4096x4096 texture could
+    store 256 different 256x256 tiles.
 
     Adding or removing tiles from a TiledImageVisual is efficient. Only the
-    bytes in the tile being updated are send to the card. The Vispy
+    bytes in the tile being updated are sent to the card. The Vispy
     BaseTexture.set_data() method has an "offset" argument. When setting
-    data with an offset Vispy calls glTexSubImage2D() to modify only the
+    data with an offset Vispy calls glTexSubImage2D() to only write into
     impacted sub-region inside the larger texture.
 
-    Uploading tiles does not cause the shader to rebuilt. This is another
-    reason TiledImageVisuals is faster than creating new ImageVisuals.
+    In addition, uploading tiles does not cause the shader to rebuilt. This
+    is another reason TiledImageVisual is faster than creating a stand-alone
+    ImageVisuals to draw each tile.
 
     Finally, rendering the tiles is also efficient. In one draw pass
     TiledImageVisual can render all the tiles. If all the tiles are stored
-    in one large texture, there will be zero texture swaps.
+    in the same large texture, there will be zero texture swaps.
     """
 
     def __init__(*args, **kwargs):
