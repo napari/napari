351bbced9c46817f0e0c34b914ca94e04cf381a6
Philip Winston <pwinston@gmail.com>
2020-11-10 14:55:37 -0500

    Move shaders, etc into TiledImageVisuals
    Setting stage for TiledImageVisual actuall drawing its tiles.

--- napari/_vispy/experimental/tiled_image_visual.py
+++ napari/_vispy/experimental/tiled_image_visual.py
@@ -4,9 +4,18 @@ from typing import Dict, List, Optional, Set, Tuple
 
 import numpy as np
 from vispy.gloo.buffer import VertexBuffer
+from vispy.visuals.shaders import Function, FunctionChain
 
 from ...layers.image.experimental.octree_util import ChunkData
 from ..vendored import ImageVisual
+from ..vendored.image import (
+    _apply_clim,
+    _apply_clim_float,
+    _apply_gamma,
+    _apply_gamma_float,
+    _c2l,
+    _null_color_transform,
+)
 from .texture_atlas import TexInfo, TextureAtlas2D
 
 # Shape of she whole texture in tiles. Hardcode for now.
@@ -15,21 +24,71 @@ SHAPE_IN_TILES = (16, 16)
 
 # Two triangles to cover a [0..1, 0..1] quad.
 _QUAD = np.array(
-    [
-        [0, 0, 0],
-        [1, 0, 0],
-        [1, 1, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 0, 0],
-    ],
+    [[0, 0], [1, 0], [1, 1], [0, 0], [0, 0], [1, 1], [0, 1], [0, 0]],
     dtype=np.float32,
 )
 
 # Either 2d shape or 2d shape plus 3 or 4 colors.
 ImageShape = Tuple[int, int, Optional[int]]
+
+FRAG_SHADER = """
+uniform vec2 image_size;
+uniform int method;  // 0=subdivide, 1=impostor
+uniform sampler2D u_texture;
+varying vec2 v_texcoord;
+
+vec4 map_local_to_tex(vec4 x) {
+    // Cast ray from 3D viewport to surface of image
+    // (if $transform does not affect z values, then this
+    // can be optimized as simply $transform.map(x) )
+    vec4 p1 = $transform(x);
+    vec4 p2 = $transform(x + vec4(0, 0, 0.5, 0));
+    p1 /= p1.w;
+    p2 /= p2.w;
+    vec4 d = p2 - p1;
+    float f = p2.z / d.z;
+    vec4 p3 = p2 - d * f;
+
+    // finally map local to texture coords
+    return vec4(p3.xy / image_size, 0, 1);
+}
+
+
+void main()
+{
+    vec2 texcoord;
+    if( method == 0 ) {
+        texcoord = v_texcoord;
+    }
+    else {
+        // vertex shader outputs clip coordinates;
+        // fragment shader maps to texture coordinates
+        texcoord = map_local_to_tex(vec4(v_texcoord, 0, 1)).xy;
+    }
+
+    gl_FragColor = $color_transform($get_data(texcoord));
+}
+"""  # noqa
+
+
+# Slightly modified from ImageVisual._build_color_transform
+def _build_color_transform(data, clim, gamma, cmap):
+    # if data.ndim == 2 or data.shape[2] == 1:
+    if True:
+        fclim = Function(_apply_clim_float)
+        fgamma = Function(_apply_gamma_float)
+        fun = FunctionChain(
+            None, [Function(_c2l), fclim, fgamma, Function(cmap.glsl_map)]
+        )
+    else:
+        fclim = Function(_apply_clim)
+        fgamma = Function(_apply_gamma)
+        fun = FunctionChain(
+            None, [Function(_null_color_transform), fclim, fgamma]
+        )
+    fclim['clim'] = clim
+    fgamma['gamma'] = gamma
+    return fun
 
 
 class TileData:
@@ -116,6 +175,17 @@ class TiledImageVisual(ImageVisual):
         super().__init__(*args, **kwargs)
 
     @property
+    def size(self):
+        # TODO_OCTREE:
+        #
+        # ImageVisual.size() does
+        #     return self._data.shape[:2][::-1]
+        #
+        # We don't have a self._data so what do we put here? Maybe need
+        # a bounds for all the currently visible tiles?
+        return (0, 0)
+
+    @property
     def num_tiles(self) -> int:
         """Return the number tiles currently being drawn.
 
@@ -155,6 +225,7 @@ class TiledImageVisual(ImageVisual):
         tex_info = self.texture_atlas.add_tile(chunk_data.data)
         tile_index = tex_info.tile_index
         self.tiles[tile_index] = TileData(chunk_data, tex_info)
+        self._need_vertex_update = True
 
         return tile_index
 
@@ -190,7 +261,7 @@ class TiledImageVisual(ImageVisual):
         This overrides ImageVisual._build_vertex_data(), it is called from
         Image.Visual_prepare_draw().
         """
-        verts = np.zeros((0, 3), dtype=np.float32)
+        verts = np.zeros((0, 2), dtype=np.float32)
         tex_coords = np.zeros((0, 2), dtype=np.float32)
 
         # TODO_OCTREE: avoid vstack, create one buffer up front
@@ -207,3 +278,51 @@ class TiledImageVisual(ImageVisual):
         # Set ImageVisual _subdiv_ buffers
         self._subdiv_position = verts.astype('float32')
         self._subdiv_texcoord = tex_coords.astype('float32')
+
+    def _build_texture(self):
+        # TODO_OCTREE: Need to do the clim stuff in in the base
+        # ImageVisual._build_texture but do it for each tile?
+        self._clim = np.array([0, 1])
+
+        self._texture_limits = np.array([0, 1])  # hardcode
+        self._need_colortransform_update = True
+        # self._texture.set_data(data)
+        self._need_texture_upload = False
+
+    def _prepare_draw(self, view):
+        """Override of ImageVisual._prepare_draw()
+
+        We'll see how much of this actualy changes from the base class and
+        if we can avoid too much duplication.
+        """
+        # Comment out: we expect our self._data is None
+        # if self._data is None:
+        #    return False
+
+        if self._need_interpolation_update:
+            self._build_interpolation()
+
+        # Comment out for now but we need to this clim part?
+        if self._need_texture_upload:
+            self._build_texture()
+
+        # TODO_OCTREE: how does colortransform change?
+        if self._need_colortransform_update:
+            prg = view.view_program
+            self.shared_program.frag[
+                'color_transform'
+            ] = _build_color_transform(
+                self._data, self.clim_normalized, self.gamma, self.cmap
+            )
+            self._need_colortransform_update = False
+            prg['texture2D_LUT'] = (
+                self.cmap.texture_lut()
+                if (hasattr(self.cmap, 'texture_lut'))
+                else None
+            )
+
+        if self._need_vertex_update:
+            self._build_vertex_data()
+
+        if view._need_method_update:
+            self._update_method(view)
