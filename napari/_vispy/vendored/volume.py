fa2c38f17dc9bee709e4477019bbf7224b3e93d1
Alister Burt <alisterburt@gmail.com>
2020-11-12 21:46:55 +0100

    add docstring info for minimum intensity projection mode

--- napari/_vispy/vendored/volume.py
+++ napari/_vispy/vendored/volume.py
@@ -423,7 +423,7 @@ class VolumeVisual(Visual):
         The contrast limits. The values in the volume are mapped to
         black and white corresponding to these values. Default maps
         between min and max.
-    method : {'mip', 'translucent', 'additive', 'iso'}
+    method : {'mip', 'minip', 'translucent', 'additive', 'iso'}
         The render method to use. See corresponding docs for details.
         Default 'mip'.
     threshold : float
