024ddf1ef83e3eed85f43f2d5341a718ad35f72a
Philip Winston <pwinston@gmail.com>
2020-11-11 21:05:15 -0500

    Fix up styling, etc

--- napari/_qt/experimental/render/qt_frame_rate.py
+++ napari/_qt/experimental/render/qt_frame_rate.py
@@ -8,12 +8,12 @@ from qtpy.QtGui import QImage, QPixmap
 from qtpy.QtWidgets import QLabel
 
 # Shape of the frame rate display.
-BITMAP_SHAPE = (20, 200, 4)
+BITMAP_SHAPE = (40, 200, 4)
 
 # Left to right are bars.
 NUM_BARS = 9
 
-# Each bar has this many verticle segments.
+# Each bar has this many vertical segments.
 NUM_SEGMENTS = 5
 
 MIN_MS = 16.17
@@ -27,9 +27,11 @@ YELLOW = (252, 232, 3, 255)
 RED = (252, 78, 3, 255)
 
 SEGMENT_WIDTH = BITMAP_SHAPE[1] / NUM_BARS
-SEGMENT_HEIGHT = BITMAP_SHAPE[0] / NUM_SEGMENTS
 
-LIVE_SEGMENTS = 10
+SEGMENT_SPACING = BITMAP_SHAPE[0] / NUM_SEGMENTS
+SEGMENT_HEIGHT = SEGMENT_SPACING - 2
+
+LIVE_SEGMENTS = 20
 
 BAR_COLOR = [GREEN, GREEN, GREEN, YELLOW, YELLOW, YELLOW, RED, RED, RED]
 
@@ -61,26 +63,35 @@ class QtFrameRate(QLabel):
         delta_ms : float
             The current frame interval.
         """
-        if len(self.past_bars) > LIVE_SEGMENTS:
-            past_bar = self.past_bars.pop(0)
-            self.values[past_bar] -= 1
-        self.values = np.clip(self.values, 0, NUM_SEGMENTS)
+        self._remove_past_values()
 
         def _clamp(value, low, high):
             return max(min(value, high), low)
 
-        # So if MIN_MS is 16.7 then log_value for 16.7ms is 0
+        # Create log value where MIN_MS has the value zero.
         log_value = math.log10(delta_ms) - LOG_MIN_MS
 
-        # The log_fraction is [0..1] for the whole width (all bars).
-        log_fraction = _clamp(log_value / LOG_MAX_MS, 0, 1)
+        # Compute fraction [0..1] for the whole width (all bars)
+        # and then find the bar we need to increment.
+        fraction = _clamp(log_value / LOG_MAX_MS, 0, 1)
+        current_bar = int(fraction * (NUM_BARS - 1))
 
-        # Increment the bar for this value.
-        current_bar = int(log_fraction * (NUM_BARS - 1))
+        # Increment the bar and save off the value.
         self.values[current_bar] += 1
         self.past_bars.append(current_bar)
 
-        # Values should only be too high, but just clip.
+        # Make sure values are not too high.
+        self.values = np.clip(self.values, 0, NUM_SEGMENTS)
+
+    def _remove_past_values(self) -> None:
+        if len(self.past_bars) < LIVE_SEGMENTS:
+            return
+
+        # Remove the value at the front of the list.
+        past_bar = self.past_bars.pop(0)
+        self.values[past_bar] -= 1
+
+        # Make sure none go under 0.
         self.values = np.clip(self.values, 0, NUM_SEGMENTS)
 
     def _update_image_data(self) -> np.ndarray:
@@ -95,10 +106,10 @@ class QtFrameRate(QLabel):
         for bar_index in range(NUM_BARS):
             bar_value = self.values[bar_index]
             print(f"bar: {bar_index} -> {bar_value}")
-            for segment in range(bar_value):
+            for segment in range(bar_value + 1):
                 x0 = int(bar_index * SEGMENT_WIDTH)
                 x1 = int(x0 + SEGMENT_WIDTH)
-                y0 = BITMAP_SHAPE[0] - int(segment * SEGMENT_HEIGHT)
+                y0 = BITMAP_SHAPE[0] - int(segment * SEGMENT_SPACING)
                 y1 = int(y0 + SEGMENT_HEIGHT)
                 self.data[y0:y1, x0:x1] = BAR_COLOR[bar_index]
 
