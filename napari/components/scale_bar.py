6e3498603a13c1abe29a49fa5b5f479042f87add
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-11-20 14:56:41 -0800

    scalebar evented dataclass

--- napari/components/scale_bar.py
+++ napari/components/scale_bar.py
@@ -1,14 +1,15 @@
 import numpy as np
 
-from ..utils.colormaps.standardize_color import transform_color
-from ..utils.events import EmitterGroup
+from ..utils.colormaps.standardize_color import transform_single_color
+from ..utils.events.dataclass import Property, dataclass
 from ._viewer_constants import Position
 
 
+@dataclass(events=True, properties=True)
 class ScaleBar:
     """Scale bar indicating size in world coordinates.
 
-    Attributes
+    Parameters
     ----------
     background_color : np.ndarray
         Background color of canvas. If scale bar is not colored
@@ -30,79 +31,10 @@ class ScaleBar:
         If scale bar is visible or not.
     """
 
-    def __init__(self):
-
-        # Events:
-        self.events = EmitterGroup(
-            source=self,
-            auto_connect=True,
-            visible=None,
-            colored=None,
-            position=None,
-            ticks=None,
-        )
-        self._visible = False
-        self._colored = False
-        self._background_color = np.array([1, 1, 1])
-        self._ticks = True
-        self._position = Position('bottom_right')
-
-    @property
-    def visible(self):
-        """bool: If scale bar is visible or not."""
-        return self._visible
-
-    @visible.setter
-    def visible(self, visible):
-        self._visible = visible
-        self.events.visible()
-
-    @property
-    def colored(self):
-        """bool: If scale bar is colored or not."""
-        return self._colored
-
-    @colored.setter
-    def colored(self, colored):
-        self._colored = colored
-        self.events.colored()
-
-    @property
-    def background_color(self):
-        """np.ndarray: RGBA color."""
-        return self._background_color
-
-    @background_color.setter
-    def background_color(self, background_color):
-        self._background_color = transform_color(background_color)[0]
-        self.events.colored()
-
-    @property
-    def ticks(self):
-        """bool: If scale bar has ticks or not."""
-        return self._ticks
-
-    @ticks.setter
-    def ticks(self, ticks):
-        self._ticks = ticks
-        self.events.ticks()
-
-    @property
-    def position(self):
-        """Position: Location of scale bar in canvas.
-
-        Postion.TOP_LEFT
-             Top left of the canvas.
-        Postion.TOP_RIGHT
-             Top right of the canvas.
-        Postion.BOTTOM_RIGHT
-             Bottom right of the canvas.
-        Postion.BOTTOM_LEFT
-             Bottom left of the canvas.
-        """
-        return str(self._position)
-
-    @position.setter
-    def position(self, position):
-        self._position = Position(position)
-        self.events.position()
+    visible: bool = False
+    colored: bool = True
+    ticks: bool = True
+    position: Property[np.ndarray, str, Position] = Position.BOTTOM_RIGHT
+    background_color: Property[
+        np.ndarray, None, transform_single_color
+    ] = np.array([1, 1, 1, 1])
