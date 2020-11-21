fd60cf4303b01d3acc977a97cdd2d3cd7675d705
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-11-20 21:49:46 -0800

    evented camera

--- napari/components/camera.py
+++ napari/components/camera.py
@@ -1,6 +1,10 @@
-from ..utils.events import EmitterGroup
+from dataclasses import InitVar
+from typing import ClassVar, Tuple
+
+from ..utils.events.dataclass import Property, dataclass
 
 
+@dataclass(events=True, properties=True)
 class Camera:
     """Camera object modeling position and view of the camera.
 
@@ -29,41 +33,29 @@ class Camera:
         Scale from canvas pixels to world pixels.
     """
 
-    def __init__(self, dims, *, center=None, zoom=1, angles=(0, 0, 90)):
+    dims: InitVar
+    _dims: ClassVar
+
+    center: Property[Tuple, None, tuple] = (0, 0, 0)
+    zoom: int = 1
+    angles: int = (0, 0, 90)
+
+    def __post_init__(self, dims):
         self._dims = dims
+        self.center = self.center[-self._dims.ndisplay :]
 
-        if center is None:
-            center = (0,) * self._dims.ndisplay
-        center = center[-self._dims.ndisplay :]
-
-        self._zoom = zoom
-        self._angles = angles
-        self._center = center
-
-        self.events = EmitterGroup(
-            source=self,
-            auto_connect=True,
-            angles=None,
-            zoom=None,
-            center=None,
-        )
-
-    @property
-    def center(self):
-        """tuple: Center point of camera view for 2D or 3D viewing."""
-        return self._center
-
-    @center.setter
-    def center(self, center):
-        if self.center == tuple(center):
-            return
-        if self.ndisplay != len(center):
-            raise ValueError(
-                f'Center must be same length as currently displayed'
-                f' dimensions, got {len(center)} need {self.ndisplay}.'
-            )
-        self._center = tuple(center)
-        self.events.center()
+    # Should we include this error checking when setting the center?
+    # @center.setter
+    # def center(self, center):
+    #     if self.center == tuple(center):
+    #         return
+    #     if self.ndisplay != len(center):
+    #         raise ValueError(
+    #             f'Center must be same length as currently displayed'
+    #             f' dimensions, got {len(center)} need {self.ndisplay}.'
+    #         )
+    #     self._center = tuple(center)
+    #     self.events.center()
 
     @property
     def ndisplay(self):
@@ -74,29 +66,11 @@ class Camera:
     def ndisplay(self, ndisplay):
         self._dims.ndisplay = ndisplay
 
-    @property
-    def zoom(self):
-        """float: Scale from canvas pixels to world pixels."""
-        return self._zoom
-
-    @zoom.setter
-    def zoom(self, zoom):
-        if self._zoom == zoom:
-            return
-        self._zoom = zoom
-        self.events.zoom()
-
-    @property
-    def angles(self):
-        """3-tuple: Euler angles of camera in 3D viewing, in degrees."""
-        if self.ndisplay == 3:
-            return self._angles
-        else:
-            return (0, 0, 90)
-
-    @angles.setter
-    def angles(self, angles):
-        if self._angles == tuple(angles):
-            return
-        self._angles = tuple(angles)
-        self.events.angles()
+    # Should we include this special casing when ndisplay=2?
+    # @property
+    # def angles(self):
+    #     """3-tuple: Euler angles of camera in 3D viewing, in degrees."""
+    #     if self.ndisplay == 3:
+    #         return self._angles
+    #     else:
+    #         return (0, 0, 90)
