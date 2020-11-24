4d4dabc9475671e684687f237ad28f3f1f8e8782
Philip Winston <pwinston@gmail.com>
2020-11-24 13:59:59 -0500

    Cleanup queue usage / organization

--- napari/components/experimental/monitor/_api.py
+++ napari/components/experimental/monitor/_api.py
@@ -6,29 +6,40 @@ from queue import Empty, Queue
 
 from ...layerlist import LayerList
 
-queue = Queue()
-
-
-def get_queue():
-    return queue
-
 
 class MonitorApi:
-    """The Monitor API that client's can access.
+    """The API that monitor clients can access.
 
-    The "API" right now is just shared dict. In the dict the key
-    'commands' is a list. Clients can append to that list and we
-    can consume those commands.
+    There is only one API command right now:
+        command_queue
+
+    Which returned a Queue() that the client can add "commands" to which
+    we'll process when we are polled. The SharedMemoryManager provides the
+    same proxy objects as SyncManager including Queue, but also dict, list
+    and others. See the docs for multiprocessing.managers.SyncManager.
     """
+
+    # This can't be an attribute of MonitorApi or the manager will try to
+    # pickle it. Note this instance does not get updated. Only the proxy
+    # returned by manager.get_queue actually has items in it.
+    _queue = Queue()
+
+    def get_queue():
+        """Can't use a lambda or manager will try to pickle it."""
+        return MonitorApi._queue
 
     def __init__(self, layers: LayerList):
         self._log("starting")
         self.layers = None
 
-        SharedMemoryManager.register('command_queue', callable=get_queue)
+        # We need to register these before the instance of
+        # SharedMemoryManager is create inside MonitorService.
+        SharedMemoryManager.register(
+            'command_queue', callable=MonitorApi.get_queue
+        )
 
-        # Manager is set later once it is created.
-        self.manager = None
+        # Set once a manager is assigned.
+        self.command_queue = None
 
     def set_manager(self, manager: SharedMemoryManager) -> None:
         """Set the shared manager we will get values from.
@@ -38,18 +49,18 @@ class MonitorApi:
         manager : SharedMemoryManager
             Get values from this manager.
         """
-        self.manager = manager
+        # Now that there is a manager, we can get the proxy object.
+        self.command_queue = manager.command_queue()
 
     def poll(self):
         """Poll the shared dict for new commands."""
-        if self.manager is None:
+        if self.command_queue is None:
             return  # Nothing to poll yet.
 
         self._log("Checking command queue")
-        command_queue = self.manager.command_queue()
         while True:
             try:
-                command = command_queue.get_nowait()
+                command = self.command_queue.get_nowait()
                 self._process_command(command)
             except Empty:
                 self._log("EMPTY")
