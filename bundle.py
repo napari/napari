dabd83c990e780a36496bc3d5adafa7d27c90abc
Draga Doncila Pop <17995243+DragaDoncila@users.noreply.github.com>
2020-11-25 10:57:42 +1100

    Add pims to bundle (#1939)
    * Add pims to bundle
    
    * Update release notes
    
    * Update release notes with PR number
    
    Co-authored-by: Draga Doncila Pop <draga.doncila.pop@purple.telstra.com>

--- bundle.py
+++ bundle.py
@@ -18,7 +18,7 @@ APP = 'napari'
 # PySide2:
 # python bundle.py --add 'PySide2==5.15.0' 'ome-zarr'
 
-EXTRA_REQS = ["pip", "PySide2==5.14.2.3", "scikit-image", "zarr"]
+EXTRA_REQS = ["pip", "PySide2==5.14.2.3", "scikit-image", "zarr", "pims"]
 
 
 WINDOWS = os.name == 'nt'
