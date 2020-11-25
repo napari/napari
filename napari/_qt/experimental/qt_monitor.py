c1211a7747096e1ed75fbbbbbd18271494b37c2a
Philip Winston <pwinston@gmail.com>
2020-11-24 21:46:47 -0500

    Add time for now to poll

--- napari/_qt/experimental/qt_monitor.py
+++ napari/_qt/experimental/qt_monitor.py
@@ -1,9 +1,11 @@
 """QtMonitor
 """
-from qtpy.QtCore import QObject
+from qtpy.QtCore import QObject, QTimer
 
 from ...components.camera import Camera
 from ...components.experimental.monitor import monitor
+
+POLL_INTERVAL_MS = 33  # About 30Hz.
 
 
 class QtMonitor(QObject):
@@ -23,5 +25,20 @@ class QtMonitor(QObject):
         super().__init__(parent)
         camera.events.center.connect(self._on_camera_move)
 
-    def _on_camera_move(self, _event):
+        # For sending messages polling during _on_camera_move() might be
+        # enough. But for receiving messages right now we need to poll all
+        # the time. Although we can probably get rid of that requirement
+        # when we are bit fancier inside the MonitorServer as far as
+        # receiving data.
+        self.timer = QTimer()
+        self.timer.setInterval(POLL_INTERVAL_MS)
+        self.timer.timeout.connect(self._on_timer)
+        self.timer.start()
+
+    def _on_camera_move(self, _event) -> None:
+        """Called when camera was moved."""
+        monitor.poll()
+
+    def _on_timer(self) -> None:
+        """Called every POLL_INTERVAL_MS milliseconds."""
         monitor.poll()
