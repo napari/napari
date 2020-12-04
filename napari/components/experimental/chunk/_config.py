8217607495699f061f62861745dc83f51dbc16d3
Philip Winston <pwinston@gmail.com>
2020-12-04 18:14:42 -0500

    Fix config file log, fix force_sync

--- napari/components/experimental/chunk/_config.py
+++ napari/components/experimental/chunk/_config.py
@@ -98,27 +98,27 @@ def _get_config_data() -> dict:
     """
     value = os.getenv("NAPARI_ASYNC")
 
-    if (value in [None, "0"]) and not config.async_octree:
-        # Async is disabled. Note, while async is experimental if
-        # NAPARI_ASYNC and NAPARI_OCTREE are both not set, then actually
-        # this code will not be run. No async related code is even
-        # imported.
+    # If the env var is a file path.
+    if value not in [None, "0", "1"]:
+        return _load_config(value)  # Load the user's config file.
+
+    # If NAPARI_ASYNC is off and NAPARI_OCTREE is off, then we are sync.
+    if value in [None, "0"] and not config.async_octree:
+        # Note this means use the ChunkLoader, but set force_synchronous
+        # True so the ChunkLoader always loads synchronously.
         #
-        # However long term, using DEFAULT_SYNC_CONFIG means the
-        # ChunkLoader is being used, but ChunkLoader.force_synchronous is
-        # set True. Meaning every single load is synchronous.
-        #
-        # Once the feature is not experimental that will probably
-        # be the way to turn async "off". It will still run through
-        # the ChunkLoader, but the loads will be synchronous.
+        # However while async is experimental, if nothing is defined we
+        # won't even import any of this code at all. So this force_synchronous
+        # path is not used very much. Because it means "use async code but
+        # do not load async".
         return DEFAULT_SYNC_CONFIG
 
-    # Async is enabled with defaults.
+    # Either NAPARI_ASYNC as set, or async is on because NAPARI_ASYNC is
+    # on. Yet no config file was specified. So use defaults.
     async_defaults = DEFAULT_ASYNC_CONFIG
     async_defaults['octree'] = DEFAULT_OCTREE_CONFIG
+    print("DEFAULT ASYNC")
     return async_defaults
-
-    return _load_config(value)  # Load the user's JSON config file.
 
 
 def _create_async_config(data: dict) -> AsyncConfig:
@@ -142,7 +142,7 @@ def _create_async_config(data: dict) -> AsyncConfig:
 
     config = AsyncConfig(
         log_path=data.get("log_path"),
-        force_synchronous=data.get("synchronous", True),
+        force_synchronous=data.get("force_synchronous", True),
         num_workers=data.get("num_workers", 6),
         use_processes=data.get("use_processes", False),
         auto_sync_ms=data.get("auto_sync_ms", 30),
