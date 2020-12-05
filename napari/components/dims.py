a87d42b165e846745e9117af74affef22e41fd46
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-12-04 21:08:16 -0800

    clean up

--- napari/components/dims.py
+++ napari/components/dims.py
@@ -34,9 +34,52 @@ def reorder_after_dim_reduction(order):
     return arr.tolist()
 
 
+def assert_axis_in_bounds(axis: int, ndim: int) -> int:
+    """Assert a given value is inside the existing axes of the image.
+
+    Returns
+    -------
+    axis : int
+        The axis which was checked for validity.
+    ndim : int
+        The dimensionality of the layer.
+
+    Raises
+    ------
+    ValueError
+        The given axis index is out of bounds.
+    """
+    if axis not in range(-ndim, ndim):
+        msg = (
+            f'Axis {axis} not defined for dimensionality {ndim}. '
+            f'Must be in [{-ndim}, {ndim}).'
+        )
+        raise ValueError(msg)
+
+    return axis % ndim
+
+
 @evented_dataclass
 class Dims:
     """Dimensions object modeling slicing and displaying.
+
+    Parameters
+    ----------
+    ndim : int
+        Number of dimensions.
+    ndisplay : int
+        Number of displayed dimensions.
+    last_used : int
+        Dimension which was last used.
+    range : list of 3-tuple
+        List of tuples (min, max, step), one for each dimension. In a world
+        coordinates space.
+    current_step : tuple of int
+        Tuple the slider position for each dims slider, in slider coordinates.
+    order : tuple of int
+        Tuple of ordering the dimensions, where the last dimensions are rendered.
+    axis_labels : tuple of str
+        Tuple of labels for each dimension.
 
     Attributes
     ----------
@@ -56,16 +99,21 @@ class Dims:
     axis_labels : tuple of str
         Tuple of labels for each dimension.
     nsteps : tuple of int
-        Number of steps available to each slider.
+        Number of steps available to each slider. These are calculated from
+        the ``range``.
     point : list of float
         List of floats setting the current value of the range slider when in
-        POINT mode, one for each dimension. In a world coordinates space.
+        POINT mode, one for each dimension. In a world coordinates space. These
+        are calculated from the ``current_step`` and ``range``.
     displayed : tuple
-        List of dimensions that are displayed.
+        List of dimensions that are displayed. These are calculated from the
+        ``order`` and ``ndisplay``.
     not_displayed : tuple
-        List of dimensions that are not displayed.
+        List of dimensions that are not displayed. These are calculated from the
+        ``order`` and ``ndisplay``.
     displayed_order : tuple
-        Order of only displayed dimensions.
+        Order of only displayed dimensions. These are calculated from the
+        ``displayed`` dimensions.
     """
 
     ndim: int = 2
@@ -88,40 +136,9 @@ class Dims:
             len(self.range),
             len(self.current_step),
         )
-        if max_ndim > self.ndim:
-            self.ndim = max_ndim
-        else:
-            self._update(self.ndim)
+        self._on_ndim_set(max_ndim)
 
     def _on_ndim_set(self, ndim):
-        self._update(ndim)
-
-    def _on_order_set(self, order):
-        if not set(order) == set(range(self.ndim)):
-            raise ValueError(
-                f"Invalid ordering {order} for {self.ndim} dimensions"
-            )
-
-    def _on_axis_labels_set(self, axis_labels):
-        if not len(axis_labels) == self.ndim:
-            raise ValueError(
-                f"Invalid number of axis labels {len(axis_labels)} for {self.ndim} dimensions"
-            )
-
-    def _on_range_set(self, range_var):
-        if not len(range_var) == self.ndim:
-            raise ValueError(
-                f"Invalid length range {len(range_var)} for {self.ndim} dimensions"
-            )
-
-    def _update(self, ndim):
-        """Update with new dimensionality.
-
-        Parameters
-        ----------
-        ndim : int
-            New dimensionality
-        """
         if len(self.range) < ndim:
             # Range value is (min, max, step) for the entire slider
             self._range = ((0, 2, 1),) * (ndim - len(self.range)) + self.range
@@ -151,6 +168,25 @@ class Dims:
                     tuple(map(str, range(ndim - len(self.axis_labels))))
                     + self.axis_labels
                 )
+        self._ndim = ndim
+
+    def _on_order_set(self, order):
+        if not set(order) == set(range(self.ndim)):
+            raise ValueError(
+                f"Invalid ordering {order} for {self.ndim} dimensions"
+            )
+
+    def _on_axis_labels_set(self, axis_labels):
+        if not len(axis_labels) == self.ndim:
+            raise ValueError(
+                f"Invalid number of axis labels {len(axis_labels)} for {self.ndim} dimensions"
+            )
+
+    def _on_range_set(self, range_var):
+        if not len(range_var) == self.ndim:
+            raise ValueError(
+                f"Invalid length range {len(range_var)} for {self.ndim} dimensions"
+            )
 
     @property
     def nsteps(self):
@@ -190,13 +226,6 @@ class Dims:
         order[np.argsort(order)] = list(range(len(order)))
         return tuple(order)
 
-    def reset(self):
-        """Reset dims values to initial states."""
-        # Don't reset axis labels
-        self.range = ((0, 2, 1),) * self.ndim
-        self.current_step = (0,) * self.ndim
-        self.order = tuple(range(self.ndim))
-
     def set_range(self, axis: int, _range: Sequence[Union[int, float]]):
         """Sets the range (min, max, step) for a given dimension.
 
@@ -207,7 +236,7 @@ class Dims:
         _range : tuple
             Range specified as (min, max, step).
         """
-        axis = self._assert_axis_in_bounds(axis)
+        axis = assert_axis_in_bounds(axis, self.ndim)
         if self.range[axis] != _range:
             full_range = list(self.range)
             full_range[axis] = _range
@@ -227,7 +256,7 @@ class Dims:
         value : int or float
             Value of the point.
         """
-        axis = self._assert_axis_in_bounds(axis)
+        axis = assert_axis_in_bounds(axis, self.ndim)
         (min_val, max_val, step_size) = self._range[axis]
         raw_step = (value - min_val) / step_size
         self.set_current_step(axis, raw_step)
@@ -245,7 +274,7 @@ class Dims:
         value : int or float
             Value of the point.
         """
-        axis = self._assert_axis_in_bounds(axis)
+        axis = assert_axis_in_bounds(axis, self.ndim)
         step = np.round(np.clip(value, 0, self.nsteps[axis] - 1)).astype(int)
 
         if self._current_step[axis] != step:
@@ -253,6 +282,30 @@ class Dims:
             full_current_step[axis] = step
             self.current_step = full_current_step
         self.last_used = axis
+
+    def set_axis_label(self, axis: int, label: str):
+        """Sets a new axis label for the given axis.
+
+        Parameters
+        ----------
+        axis : int
+            Dimension index
+        label : str
+            Given label
+        """
+        axis = assert_axis_in_bounds(axis, self.ndim)
+        if self.axis_labels[axis] != str(label):
+            full_axis_labels = list(self.axis_labels)
+            full_axis_labels[axis] = str(label)
+            self.axis_labels = full_axis_labels
+        self.last_used = axis
+
+    def reset(self):
+        """Reset dims values to initial states."""
+        # Don't reset axis labels
+        self.range = ((0, 2, 1),) * self.ndim
+        self.current_step = (0,) * self.ndim
+        self.order = tuple(range(self.ndim))
 
     def _increment_dims_right(self, axis: int = None):
         """Increment dimensions to the right along given axis, or last used axis if None
@@ -303,45 +356,6 @@ class Dims:
         else:
             index = (sliders.index(self.last_used) - 1) % len(sliders)
             self.last_used = sliders[index]
-
-    def set_axis_label(self, axis: int, label: str):
-        """Sets a new axis label for the given axis.
-
-        Parameters
-        ----------
-        axis : int
-            Dimension index
-        label : str
-            Given label
-        """
-        axis = self._assert_axis_in_bounds(axis)
-        if self.axis_labels[axis] != str(label):
-            full_axis_labels = list(self.axis_labels)
-            full_axis_labels[axis] = str(label)
-            self.axis_labels = full_axis_labels
-        self.last_used = axis
-
-    def _assert_axis_in_bounds(self, axis: int) -> int:
-        """Assert a given value is inside the existing axes of the image.
-
-        Returns
-        -------
-        axis : int
-            The axis which was checked for validity.
-
-        Raises
-        ------
-        ValueError
-            The given axis index is out of bounds.
-        """
-        if axis not in range(-self.ndim, self.ndim):
-            msg = (
-                f'Axis {axis} not defined for dimensionality {self.ndim}. '
-                f'Must be in [{-self.ndim}, {self.ndim}).'
-            )
-            raise ValueError(msg)
-
-        return axis % self.ndim
 
     def _roll(self):
         """Roll order of dimensions for display."""
