da9b38f32205bc1e39d3cdcd1116cb941de886b4
Nicholas Sofroniew <sofroniewn@gmail.com>
2020-12-07 19:48:10 -0800

    weak ref

--- napari/utils/events/event_utils.py
+++ napari/utils/events/event_utils.py
@@ -1,3 +1,6 @@
+import weakref
+
+
 def disconnect_events(emitter, listener):
     """Disconnect all events between an emitter group and a listener.
 
@@ -16,6 +19,7 @@ def disconnect_events(emitter, listener):
             # will be kept. If *callback* is a callable object then it is
             # not attached to the listener and does not need to be
             # disconnected
-            if isinstance(callback, tuple) and callback[0] is listener:
-                pass
-                # em.disconnect(callback)
+            if isinstance(callback, tuple) and callback[0] is weakref.ref(
+                listener
+            ):
+                em.disconnect(callback)
