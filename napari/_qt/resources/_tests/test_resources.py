110d779d6716285abfe9ad77f5792adc2b0daac9
Talley Lambert <talley.lambert@gmail.com>
2020-12-26 17:04:10 -0500

    absolute import

--- napari/_qt/resources/_tests/test_resources.py
+++ napari/_qt/resources/_tests/test_resources.py
@@ -6,7 +6,7 @@ import sys
 import pytest
 import qtpy
 
-from .. import build_pyqt_resources, import_resources
+from napari._qt.resources import build_pyqt_resources, import_resources
 
 
 def test_resources():
