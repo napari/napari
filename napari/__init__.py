ce2dd64df9f9add0495e3bdd0b6f594561c25eaf
Talley Lambert <talley.lambert@gmail.com>
2020-12-30 13:36:15 -0500

    rearrange init

--- napari/__init__.py
+++ napari/__init__.py
@@ -15,6 +15,21 @@ from scipy import stats  # noqa: F401
 
 from ._event_loop import event_loop
 from .plugins.io import save_layers
+from .utils import _magicgui, sys_info
+from .view_layers import (  # type: ignore
+    view_image,
+    view_labels,
+    view_path,
+    view_points,
+    view_shapes,
+    view_surface,
+    view_tracks,
+    view_vectors,
+)
+
+# register napari object types with magicgui if it is installed
+_magicgui.register_types_with_magicgui()
+del _magicgui
 
 
 def __getattr__(name):
@@ -29,25 +44,7 @@ def __getattr__(name):
     raise AttributeError(f"module {__name__} has no attribute {name}")
 
 
-# register napari object types with magicgui if it is installed
-from .utils import _magicgui, sys_info
-from .view_layers import (  # type: ignore
-    view_image,
-    view_labels,
-    view_path,
-    view_points,
-    view_shapes,
-    view_surface,
-    view_tracks,
-    view_vectors,
-)
-
-_magicgui.register_types_with_magicgui()
-del _magicgui
-
-
 del stats
-
 __all__ = [
     'Viewer',
     'save_layers',
