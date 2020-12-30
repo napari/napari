a493da2947a85f65b5973c563c5fd8afd78e40ca
haesleinhuepf <robert_haase@gmx.de>
2020-12-30 12:51:06 +0100

    added example for the viewer.open method and how to retrieve the image source from metadata

---
+++ examples/open_image.py
@@ -0,0 +1,21 @@
+"""
+Display one image using the add_image API.
+"""
+
+image_web_url = 'https://github.com/napari/napari/raw/master/docs/source/img/napari_logo.png'
+image_local_filename = '../docs/source/img/napari_logo.png'
+
+from skimage import data
+import napari
+
+with napari.gui_qt():
+    # create the viewer
+    viewer = napari.Viewer()
+
+    # open locally stored image
+    layers = viewer.open(image_local_filename)
+    print("Image " + layers[0].metadata['source'] + " loaded.")
+
+    # open image from web
+    layers = viewer.open(image_web_url)
+    print("Image " + layers[0].metadata['source'] + " loaded.")
