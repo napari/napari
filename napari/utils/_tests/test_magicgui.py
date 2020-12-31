043dee7c9e0994f4b806eb7ca1db62e62225af69
Talley Lambert <talley.lambert@gmail.com>
2020-12-31 13:03:05 -0500

    Complete qt isolation (#2055)
    * deprecate gui_qt
    
    * more test isolation
    
    * remove tox.ini
    
    * rearrange init
    
    * change attribute error repr
    
    * revert event_loop name

--- napari/utils/_tests/test_magicgui.py
+++ napari/utils/_tests/test_magicgui.py
@@ -3,17 +3,18 @@ import sys
 
 import numpy as np
 import pytest
+from magicgui import magicgui
 
 from napari.layers import Image, Labels, Layer, Points
 
-magicgui = pytest.importorskip('magicgui', reason='please install magicgui.')
+qtpy = pytest.importorskip('qtpy', reason='Cannot test magicgui without qtpy.')
 
 
 def test_magicgui_returns_image(make_test_viewer):
     """make sure a magicgui function returning Image adds an Image."""
     viewer = make_test_viewer()
 
-    @magicgui.magicgui
+    @magicgui
     def add_image() -> Image:
         return np.random.rand(10, 10)
 
@@ -33,7 +34,7 @@ def test_magicgui_returns_label(make_test_viewer):
     """make sure a magicgui function returning Labels adds a Labels."""
     viewer = make_test_viewer()
 
-    @magicgui.magicgui
+    @magicgui
     def add_labels() -> Labels:
         return np.random.rand(10, 10)
 
@@ -53,7 +54,7 @@ def test_magicgui_returns_layer_tuple(make_test_viewer):
     """make sure a magicgui function returning Layer adds the right type."""
     viewer = make_test_viewer()
 
-    @magicgui.magicgui
+    @magicgui
     def add_layer() -> Layer:
         return [(np.random.rand(10, 3), {'size': 20, 'name': 'foo'}, 'points')]
 
