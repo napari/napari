e5b9473923ad4eab75d35228f29512ea6146077e
Talley Lambert <talley.lambert@gmail.com>
2021-01-02 15:50:53 -0500

    Update examples/without_gui_qt.py
    Co-authored-by: Nicholas Sofroniew <sofroniewn@gmail.com>

--- examples/without_gui_qt.py
+++ examples/without_gui_qt.py
@@ -6,5 +6,11 @@ from skimage import data
 import napari
 
 viewer = napari.view_image(data.astronaut(), rgb=True)
+# You can also take screenshots before running the app
+screenshot = viewer.screenshot()
 
+print('Maximum value', screenshot.max())
+
+# Run the app to see the napari viewer. If you only wanted the screenshot
+# then you could skip this entirely
 napari.run_app()
