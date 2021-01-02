65509c30f44a351add73c00919301fd95452a610
Talley Lambert <talley.lambert@gmail.com>
2021-01-02 10:08:18 -0500

    try module level qapp

--- napari/_qt/perf/_tests/test_perf.py
+++ napari/_qt/perf/_tests/test_perf.py
@@ -1,13 +1,17 @@
 import json
-import os
 from contextlib import contextmanager
 from pathlib import Path
-from tempfile import NamedTemporaryFile
 
 import numpy as np
+import pytest
 
 from napari._qt.perf.qt_event_tracing import QApplicationWithTracing
 from napari.utils.perf import get_perf_config, timers
+
+
+@pytest.fixture(scope="module")
+def qapp():
+    yield QApplicationWithTracing([])
 
 
 @contextmanager
@@ -18,25 +22,6 @@ def clear_perf_context():
     get_perf_config.cache_clear()
 
 
-@contextmanager
-def temporary_file(suffix=''):
-    """Yield a writeable temporary filename that is deleted on context exit.
-    Parameters
-    ----------
-    suffix : string, optional
-        The suffix for the file.
-    """
-    tempfile_stream = NamedTemporaryFile(suffix=suffix, delete=False)
-    tempfile = tempfile_stream.name
-    tempfile_stream.close()
-    yield tempfile
-    os.remove(tempfile)
-
-
-def _trace_file_okay(trace_path: str) -> bool:
-    """For now okay just means valid JSON and not empty."""
-
-
 def test_perfmon_off_by_default():
     """Make sure perfmon off by default."""
     # Check perfmon is not enabled
@@ -44,35 +29,32 @@ def test_perfmon_off_by_default():
         assert get_perf_config() is None
 
 
-def test_trace_on_start(monkeypatch, make_test_viewer):
+def test_trace_on_start(tmp_path, monkeypatch, make_test_viewer):
     """Make sure napari can write a perfmon trace file."""
-
-    def qapp():
-        yield QApplicationWithTracing([])
 
     monkeypatch.setenv('NAPARI_PERFMON', '1')
     # Check perfmon is enabled
     with clear_perf_context():
         assert get_perf_config()
 
-        with temporary_file('json') as trace_path:
-            timers.start_trace_file(trace_path)
+        trace_path = tmp_path / "trace.json"
+        timers.start_trace_file(trace_path)
 
-            viewer = make_test_viewer()
-            data = np.random.random((10, 15))
-            viewer.add_image(data)
-            viewer.close()
+        viewer = make_test_viewer()
+        data = np.random.random((10, 15))
+        viewer.add_image(data)
+        viewer.close()
 
-            timers.stop_trace_file()
+        timers.stop_trace_file()
 
-            # Make sure file exists and is not empty.
-            assert Path(trace_path).exists(), "Trace file not written"
-            assert Path(trace_path).stat().st_size > 0, "Trace file is empty"
+        # Make sure file exists and is not empty.
+        assert Path(trace_path).exists(), "Trace file not written"
+        assert Path(trace_path).stat().st_size > 0, "Trace file is empty"
 
-            # Assert every event contains every important field.
-            with open(trace_path) as infile:
-                data = json.load(infile)
-                assert len(data) > 0
-                for event in data:
-                    for field in ['pid', 'tid', 'name', 'ph', 'ts', 'args']:
-                        assert field in event
+        # Assert every event contains every important field.
+        with open(trace_path) as infile:
+            data = json.load(infile)
+            assert len(data) > 0
+            for event in data:
+                for field in ['pid', 'tid', 'name', 'ph', 'ts', 'args']:
+                    assert field in event
