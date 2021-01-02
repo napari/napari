8501f376d5db5c20d804859432a35920a9ae05c3
Talley Lambert <talley.lambert@gmail.com>
2021-01-02 16:07:28 -0500

    update example

--- examples/without_gui_qt.py
+++ examples/without_gui_qt.py
@@ -1,16 +1,27 @@
-"""
-Alternative to using napari.gui_qt() context manager. 
-"""
+"""Alternative to using napari.gui_qt() context manager."""
 
 from skimage import data
 import napari
+from collections import Counter
 
 viewer = napari.view_image(data.astronaut(), rgb=True)
-# You can also take screenshots before running the app
+
+# You can do anything you would normally do with the viewer object
+# like take a
 screenshot = viewer.screenshot()
 
 print('Maximum value', screenshot.max())
 
-# Run the app to see the napari viewer. If you only wanted the screenshot
-# then you could skip this entirely
+# To see the napari viewer and interact with the graphical user interface,
+# use `napari.run_app()`.  (it's similar to `plt.show` in matplotlib)
+# If you only wanted the screenshot then you could skip this entirely.
+# *run_app* will *block execution of your script* until the window is closed.
 napari.run_app()
+
+# When the window is closed, your script continues and you can still inspect
+# the viewer object.  For example, add click the buttons to add various layer
+# types when the window is open and see the result below:
+
+print("Your viewer has the following layers:")
+for name, n in Counter(type(x).__name__ for x in viewer.layers).most_common():
+    print(f"   {name:<7}: {n}")
