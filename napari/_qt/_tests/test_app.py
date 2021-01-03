d5bd6d81f6dca8d7a2d3b831982f4319a476e2d2
Talley Lambert <talley.lambert@gmail.com>
2021-01-03 12:14:54 -0500

    give up on test_get_app

--- napari/_qt/_tests/test_app.py
+++ napari/_qt/_tests/test_app.py
@@ -1,58 +1,8 @@
 import os
-import sys
 
 import pytest
-import qtpy
-from qtpy.QtWidgets import QApplication
 
-from napari._qt.qt_event_loop import __version__, get_app, set_app_id
-from napari._qt.utils import delete_qapp
-
-LINUX_CI_PYSIDE = bool(
-    sys.platform.startswith('linux')
-    and os.getenv('CI', '0') != '0'
-    and qtpy.API_NAME == "PySide2"
-)
-
-
-custom = {
-    'app_name': 'custom',
-    'app_version': 'custom',
-    'icon': 'custom',
-    'org_name': 'custom',
-    'org_domain': 'custom.com',
-}
-
-
-@pytest.fixture
-def qapp(request):
-    if QApplication.instance():
-        delete_qapp(QApplication.instance())
-    yield get_app(**request.getfixturevalue('app_kwargs'))
-
-
-# @pytest.mark.skipif(LINUX_CI_PYSIDE, "Can't recreate pyside QApp on CI linux")
-@pytest.mark.parametrize('app_kwargs', [custom, {}], ids=['custom', 'default'])
-def test_get_app(qtbot, app_kwargs):
-    """Test that calling get_app defines the attributes of the QApp."""
-    app = None
-
-    def _assert():
-        assert app.applicationName() == app_kwargs.get("app_name", 'napari')
-        assert app.applicationVersion() == app_kwargs.get(
-            "app_version", __version__
-        )
-        assert app.organizationName() == app_kwargs.get("org_name", 'napari')
-        assert app.organizationDomain() == app_kwargs.get(
-            "org_domain", 'napari.org'
-        )
-
-    app = get_app()
-    _assert()
-
-    # QApp is a singleton, calling a second time has no effect
-    app = get_app(app_name='x', app_version='x', org_name='x', org_domain='x')
-    _assert()
+from napari._qt.qt_event_loop import set_app_id
 
 
 @pytest.mark.skipif(os.name != "Windows", reason="Windows specific")
