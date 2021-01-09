83902371ef70a0fb2f783ff8003e389185543dfa
Talley Lambert <talley.lambert@gmail.com>
2021-01-09 08:49:22 -0500

    use custom qapp fixture

--- napari/_tests/test_examples.py
+++ napari/_tests/test_examples.py
@@ -10,20 +10,31 @@ skip = [
     '3d_kymograph.py',  # needs tqdm
     'live_tiffs.py',  # requires files
     'live_tiffs_generator.py',
-    # this works fine (it causes a deleted object error that is successfully
-    # caught and handled), but shows confusing a confusing warning *AFTER* the
-    # test has successfully exited.  So removing from tests to avoid confusion.
-    'interactive_scripting.py',
 ]
 example_dir = Path(napari.__file__).parent.parent / 'examples'
 examples = [f for f in example_dir.glob("*.py") if f.name not in skip]
+
+
+@pytest.fixture
+def qapp():
+    from napari._qt.qt_event_loop import get_app
+    from qtpy.QtCore import QTimer
+
+    # it's important that we use get_app so that it connects to the
+    # app.aboutToQuit.connect(wait_for_workers_to_quit)
+    app = get_app()
+
+    # quit examples that explicitly start the event loop with `napari.run()`
+    # so that tests aren't waiting on a manual exit
+    QTimer.singleShot(100, app.quit)
+
+    yield app
 
 
 @pytest.mark.parametrize("fname", examples, ids=lambda x: Path(x).name)
 def test_examples(qapp, fname, monkeypatch, capsys):
     """Test that all of our examples are still working without warnings."""
 
-    from qtpy.QtCore import QTimer
     from napari._qt.qt_main_window import Window
     from napari._qt.exceptions import ExceptionHandler
 
@@ -35,9 +46,6 @@ def test_examples(qapp, fname, monkeypatch, capsys):
         raise value
 
     monkeypatch.setattr(ExceptionHandler, 'handle', raise_errors)
-
-    # quit examples that explicitly start the event loop with `napari.run()`
-    QTimer.singleShot(0, qapp.quit)
 
     # run the example!
     assert runpy.run_path(str(fname))
