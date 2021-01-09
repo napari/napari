f3951b7414fc0b797268af4ed43903fd14acafff
Talley Lambert <talley.lambert@gmail.com>
2021-01-08 21:11:02 -0500

    test examples

---
+++ napari/_tests/test_examples.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import napari
+import pytest
+import runpy
+
+
+# not testing these examples
+skip = [
+    'surface_timeseries.py',  # needs nilearn
+    '3d_kymograph.py',  # needs tqdm
+    'live_tiffs.py',  # requires files
+    'live_tiffs_generator.py',
+]
+example_dir = Path(napari.__file__).parent.parent / 'examples'
+examples = [f for f in example_dir.glob("*.py") if f.name not in skip]
+
+
+@pytest.mark.parametrize("fname", examples, ids=lambda x: Path(x).name)
+def test_examples(qapp, fname, monkeypatch, capsys):
+    from qtpy.QtCore import QTimer
+    from napari._qt.qt_main_window import Window
+    from napari._qt.exceptions import ExceptionHandler
+
+    # hide viewer window
+    monkeypatch.setattr(Window, 'show', lambda *a: None)
+
+    # make sure our sys.excepthook override in gui_qt doesn't hide errors
+    def raise_errors(self, etype, value, tb):
+        raise value
+
+    monkeypatch.setattr(ExceptionHandler, 'handle', raise_errors)
+
+    # quit examples that explicitly start the event loop with `napari.run()`
+    QTimer.singleShot(10, qapp.quit)
+
+    # run the example!
+    assert runpy.run_path(fname)
