ba431711e4dfc89b07a12c92cbb828488508ac71
Nicholas Sofroniew <sofroniewn@gmail.com>
2021-01-18 19:19:18 -0800

    add tests

---
+++ napari/utils/_pydantic/_tests/test_evented_model.py
@@ -0,0 +1,8 @@
+from napari.utils._pydantic import EventedModel
+
+
+def test_creating_evented_model():
+    """Test creating an evented pydantic model."""
+    model = EventedModel()
+    assert model is not None
+    assert model.events is not None
