8bf4565ab95d33f9d119e46c8119e6299409f772
nhthayer <nhthayer@gmail.com>
2021-01-21 08:43:46 -0800

    added test to check keybindings are updated before dialog

--- napari/_qt/dialogs/_tests/test_qt_about_key_bindings.py
+++ napari/_qt/dialogs/_tests/test_qt_about_key_bindings.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from napari._qt.dialogs.qt_about_key_bindings import QtAboutKeyBindings
 
 
@@ -31,3 +33,33 @@ def test_show_key_bindings_dialog(make_test_viewer, monkeypatch):
     # create the dialog and make sure that it now exists
     view.show_key_bindings_dialog()
     assert isinstance(view.findChild(QtAboutKeyBindings), QtAboutKeyBindings)
+
+
+def test_updating_with_layer_change(make_test_viewer, monkeypatch):
+    """Test that the dialog text updates when the active layer is changed."""
+    viewer = make_test_viewer()
+    view = viewer.window.qt_viewer
+    # turn off showing the dialog for test
+    monkeypatch.setattr(QtAboutKeyBindings, 'show', lambda *a: None)
+    view.show_key_bindings_dialog()
+    dialog = view.findChild(QtAboutKeyBindings)
+
+    # add an image layer
+    viewer.add_image(np.random.random((5, 5, 10, 15)))
+    # capture dialog text after active_layer events
+    active_img_layer_text = dialog.textEditBox.toHtml()
+    dialog.update_active_layer()  # force an to update to dialog
+    # check that the text didn't update without a change in the active layer
+    assert dialog.textEditBox.toHtml() == active_img_layer_text
+
+    # add a shape layer (different keybindings)
+    viewer.add_shapes(None, shape_type='polygon')
+    # check that the new layer is the active_layer
+    assert viewer.active_layer == viewer.layers[1]
+    # capture dialog text after active_layer events
+    active_shape_layer_text = dialog.textEditBox.toHtml()
+    # check that the text has changed for the new key bindings
+    assert active_shape_layer_text != active_img_layer_text
+    dialog.update_active_layer()  # force an update to dialog
+    # check that the text didn't update without a change in the active layer
+    assert dialog.textEditBox.toHtml() == active_shape_layer_text
