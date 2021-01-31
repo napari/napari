19637f8d236736c0bd1aa2977a33df7c02407bb0
Nicholas Sofroniew <sofroniewn@gmail.com>
2021-01-30 19:44:00 -0800

    fix qt dims test

--- napari/_qt/widgets/_tests/test_qt_dims.py
+++ napari/_qt/widgets/_tests/test_qt_dims.py
@@ -15,7 +15,7 @@ def test_creating_view(qtbot):
     Test creating dims view.
     """
     ndim = 4
-    dims = Dims(ndim)
+    dims = Dims(ndim=ndim)
     view = QtDims(dims)
 
     qtbot.addWidget(view)
@@ -40,7 +40,7 @@ def test_changing_ndim(qtbot):
     Test changing the number of dimensions
     """
     ndim = 4
-    view = QtDims(Dims(ndim))
+    view = QtDims(Dims(ndim=ndim))
 
     qtbot.addWidget(view)
 
@@ -71,12 +71,12 @@ def test_changing_focus(qtbot):
     """Test changing focus updates the dims.last_used prop."""
     # too-few dims, should have no sliders to update
     ndim = 2
-    view = QtDims(Dims(ndim))
+    view = QtDims(Dims(ndim=ndim))
     qtbot.addWidget(view)
-    assert view.dims.last_used is None
+    assert view.dims.last_used == 0
     view.dims._focus_down()
     view.dims._focus_up()
-    assert view.dims.last_used is None
+    assert view.dims.last_used == 0
 
     view.dims.ndim = 5
     view.dims.last_used = 2
@@ -96,7 +96,7 @@ def test_changing_display(qtbot):
     Test changing the displayed property of an axis
     """
     ndim = 4
-    view = QtDims(Dims(ndim))
+    view = QtDims(Dims(ndim=ndim))
     qtbot.addWidget(view)
 
     assert view.nsliders == view.dims.ndim
@@ -125,7 +125,7 @@ def test_slider_values(qtbot):
     Test the values of a slider stays matched to the values of the dims point.
     """
     ndim = 4
-    view = QtDims(Dims(ndim))
+    view = QtDims(Dims(ndim=ndim))
     qtbot.addWidget(view)
 
     # Check that values of the dimension slider matches the values of the
@@ -149,7 +149,7 @@ def test_slider_range(qtbot):
     Tests range of the slider is matched to the range of the dims
     """
     ndim = 4
-    view = QtDims(Dims(ndim))
+    view = QtDims(Dims(ndim=ndim))
     qtbot.addWidget(view)
 
     # Check the maximum allowed value of the slider is one less
@@ -172,7 +172,7 @@ def test_singleton_dims(qtbot):
     Test singleton dims causes no slider.
     """
     ndim = 4
-    dims = Dims(ndim)
+    dims = Dims(ndim=ndim)
     dims.set_range(0, (0, 0, 1))
     view = QtDims(dims)
     qtbot.addWidget(view)
@@ -208,7 +208,7 @@ def test_order_when_changing_ndim(qtbot):
     Test order of the sliders when changing the number of dimensions.
     """
     ndim = 4
-    view = QtDims(Dims(ndim))
+    view = QtDims(Dims(ndim=ndim))
     qtbot.addWidget(view)
 
     # Check that values of the dimension slider matches the values of the
@@ -247,7 +247,7 @@ def test_update_dims_labels(qtbot):
     and vice versa.
     """
     ndim = 4
-    view = QtDims(Dims(ndim))
+    view = QtDims(Dims(ndim=ndim))
     qtbot.addWidget(view)
     view.dims.axis_labels = list('TZYX')
     assert [w.axis_label.text() for w in view.slider_widgets] == list('TZYX')
@@ -262,7 +262,7 @@ def test_update_dims_labels(qtbot):
 def test_slider_press_updates_last_used(qtbot):
     """pressing on the slider should update the dims.last_used property"""
     ndim = 5
-    view = QtDims(Dims(ndim))
+    view = QtDims(Dims(ndim=ndim))
     qtbot.addWidget(view)
 
     for i, widg in enumerate(view.slider_widgets):
@@ -277,7 +277,7 @@ def test_slider_press_updates_last_used(qtbot):
 def test_play_button(qtbot):
     """test that the play button and its popup dialog work"""
     ndim = 3
-    view = QtDims(Dims(ndim))
+    view = QtDims(Dims(ndim=ndim))
     qtbot.addWidget(view)
     button = view.slider_widgets[0].play_button
     qtbot.mouseClick(button, Qt.LeftButton)
