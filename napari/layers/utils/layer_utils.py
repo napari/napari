09715638bd17b1b25ea86df4e23be8cdcf08c990
Talley Lambert <talley.lambert@gmail.com>
2021-02-02 20:01:43 -0500

    add back combine_extents

--- napari/layers/utils/layer_utils.py
+++ napari/layers/utils/layer_utils.py
@@ -276,3 +276,28 @@ def compute_multiscale_level_and_corners(
     corners = np.array([np.floor(corners[0]), np.ceil(corners[1])]).astype(int)
 
     return level, corners
+
+
+def combine_extents(extents):
+    """Combine extents into a single extent that contains all individual ones.
+    Each extent can be cast to an ndim x 2 (min, max) array, but ndim
+    could be different for each extent because of broadcasting. In that case,
+    we need to *prepend* the row (0, 0) to each array so that they match in
+    size.
+    """
+    if len(extents) == 0:
+        return []
+    extent_arrays = [np.array(ex) for ex in extents]
+    ndims = [arr.shape[0] for arr in extent_arrays]
+    required_ndims = max(ndims)
+    dims_to_prepend = [required_ndims - d for d in ndims]
+    padded_arrays = [
+        np.concatenate((np.zeros((d, 2)), ex), axis=0)
+        for d, ex in zip(dims_to_prepend, extent_arrays)
+    ]
+    big_array = np.stack(padded_arrays, axis=0)
+    result = np.stack(
+        [np.min(big_array, axis=0)[:, 0], np.max(big_array, axis=0)[:, 1]],
+        axis=1,
+    )
+    return list(map(tuple, result))
