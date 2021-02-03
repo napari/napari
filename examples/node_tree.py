d123d3b2ecdae0891c8876a749d6c0ffc3f05fef
Talley Lambert <talley.lambert@gmail.com>
2021-02-03 13:35:57 -0500

    example

--- examples/node_tree.py
+++ examples/node_tree.py
@@ -1,4 +1,4 @@
-from qtpy.QtWidgets import QApplication
+from napari import gui_qt
 from napari._qt.tree.qt_tree_view import QtNodeTreeView
 from napari.utils.tree import Node, Group
 import logging
@@ -12,27 +12,16 @@ green = "\033[0;32m"
 colorlog_format = f'{green}%(levelname)9s:{end} {Dim}%(name)36s.{ResetDim}{red}%(funcName)-18s{end}{"%(message)s"}'
 logging.basicConfig(level=logging.DEBUG, format=colorlog_format)
 
-app = QApplication([])
-
-root = Group(
-    [
-        Node(name="1"),
-        Group(
-            [
-                Node(name="2"),
-                Group([Node(name="3"), Node(name="4")], name="g2"),
-                Node(name="5"),
-                Node(name="6"),
-                Node(name="7"),
-            ],
-            name="g1",
-        ),
-        Node(name="8"),
-        Node(name="9"),
-    ],
-    name="root",
-)
-tree = QtNodeTreeView(root)
-tree.show()
-
-app.exec_()
+with gui_qt():
+    tip = Node(name='tip')
+    lg2 = Group([Node(name='2'), Node(name='3')], name="g2")
+    lg1 = Group(
+        [Node(name='1'), lg2, Node(name='4'), Node(name='5'), tip], name="g1",
+    )
+    root = Group(
+        [Node(name='6'), lg1, Node(name='7'), Node(name='8'), Node(name='9')],
+        name="root",
+    )
+    tree = QtNodeTreeView(root)
+    model = tree.model()
+    tree.show()
