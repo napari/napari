973105dad5e519a60c3d4c7b6c912f061c6cad83
Talley Lambert <talley.lambert@gmail.com>
2021-02-05 14:12:11 -0500

    undo leak change

--- napari/utils/_testsupport.py
+++ napari/utils/_testsupport.py
@@ -1,4 +1,5 @@
 import sys
+import warnings
 from typing import List
 
 import pytest
@@ -21,12 +22,12 @@ def _strict_qtbot(qtbot):
 
     QApplication.processEvents()
     leaks = set(QApplication.topLevelWidgets()).difference(initial)
-    # still not sure how to clean up some of the remaining vispy and qtconsole
+    # still not sure how to clean up some of the remaining vispy
     # vispy.app.backends._qt.CanvasBackendDesktop widgets...
-    ignored_leaks = {'CanvasBackendDesktop', 'CompletionHtml', 'CallTipWidget'}
-    leaks = {lk for lk in leaks if lk.__class__.__name__ not in ignored_leaks}
-    if leaks:
+    if any([n.__class__.__name__ != 'CanvasBackendDesktop' for n in leaks]):
         raise AssertionError(f'Widgets leaked!: {leaks}')
+    if leaks:
+        warnings.warn(f'Widgets leaked!: {leaks}')
 
 
 @pytest.fixture(scope="function")
