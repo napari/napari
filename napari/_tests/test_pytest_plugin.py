dd28db73955a3b4a97551ea539bccebc44d5a2f2
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-02-11 12:21:17 +1100

    Revert "Add pluginmanager fixture (#2247)"
    This reverts commit 3b89a6c2e86f3ea38630ed69f85f882a9eca2e71.

--- napari/_tests/test_pytest_plugin.py
+++ napari/_tests/test_pytest_plugin.py
@@ -30,32 +30,3 @@ def test_make_napari_viewer(testdir):
 
     # check that all 1 test passed
     result.assert_outcomes(passed=1)
-
-
-def test_napari_plugin_tester(testdir):
-    """Make sure that our napari_plugin_tester fixture works."""
-
-    # create a temporary pytest test file
-    testdir.makepyfile(
-        """
-        from napari_plugin_engine import napari_hook_implementation
-        class Plugin:
-
-            @napari_hook_implementation
-            def napari_get_reader(path):
-                pass
-
-        def test_pm(napari_plugin_tester):
-            napari_plugin_tester.register(Plugin)
-            napari_plugin_tester.assert_plugin_name_registered("Plugin")
-            napari_plugin_tester.assert_module_registered(Plugin)
-            napari_plugin_tester.assert_implementations_registered(
-                Plugin, "napari_get_reader"
-            )
-    """
-    )
-    # run all tests with pytest
-    result = testdir.runpytest()
-
-    # check that all 1 test passed
-    result.assert_outcomes(passed=1)
