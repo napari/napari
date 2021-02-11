57dcc8e111a8eccd49d0e84ae3c9b3b234bd0ca0
Neil Weisenfeld <neil@10xgenomics.com>
2021-02-11 11:07:17 -0500

    parameterize test inputs

--- napari/utils/_tests/test_status.py
+++ napari/utils/_tests/test_status.py
@@ -1,30 +1,41 @@
 import numpy as np
+import pytest
 
 from napari.utils.status_messages import status_format
 
+STRING = "hello world"
+STRING_FORMATTED = STRING
+MISSING = None
+MISSING_FORMATTED = ""
+NUMERIC = [
+    1,
+    10,
+    100,
+    1000,
+    1e6,
+    -6.283,
+    123.932021,
+    1123.9392001,
+    2 * np.pi,
+    np.exp(1),
+]
+NUMERIC_FORMATTED = (
+    "[1, 10, 100, 1000, 1e+06, -6.28, 124, 1.12e+03, 6.28, 2.72]"
+)
+COMBINED = [1e6, MISSING, STRING]
+COMBINED_FORMATTED = f"[1e+06, {MISSING_FORMATTED}, {STRING_FORMATTED}]"
 
-def test_status_format():
+
+@pytest.mark.parametrize(
+    'input, expected',
+    [
+        [NUMERIC, NUMERIC_FORMATTED],
+        [STRING, STRING_FORMATTED],
+        [MISSING, MISSING_FORMATTED],
+        [COMBINED, COMBINED_FORMATTED],
+    ],
+)
+def test_status_format(input, expected):
     """ test various formatting cases embodied in utils.status_messages.status_format """
 
-    values = np.array(
-        [
-            1,
-            10,
-            100,
-            1000,
-            1e6,
-            -6.283,
-            123.932021,
-            1123.9392001,
-            2 * np.pi,
-            np.exp(1),
-        ]
-    )
-    assert (
-        status_format(values)
-        == '[1, 10, 100, 1e+03, 1e+06, -6.28, 124, 1.12e+03, 6.28, 2.72]'
-    )
-    assert status_format('hello') == 'hello'
-    assert (
-        status_format([1.23, -1.23, None, 4.5789]) == '[1.23, -1.23, , 4.58]'
-    )
+    assert status_format(input) == expected
