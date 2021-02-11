3b89a6c2e86f3ea38630ed69f85f882a9eca2e71
Talley Lambert <talley.lambert@gmail.com>
2021-02-10 19:56:06 -0500

    Add pluginmanager fixture (#2247)

--- napari/utils/_testsupport.py
+++ napari/utils/_testsupport.py
@@ -3,6 +3,7 @@ import warnings
 from typing import List
 
 import pytest
+from napari_plugin_engine import PluginManager
 
 
 def pytest_addoption(parser):
@@ -39,7 +40,7 @@ def _strict_qtbot(qtbot):
         warnings.warn(f'Widgets leaked!: {leaks}')
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def make_napari_viewer(_strict_qtbot, request):
     """A fixture function that creates a napari viewer for use in testing.
 
@@ -69,3 +70,59 @@ def make_napari_viewer(_strict_qtbot, request):
 
     for viewer in viewers:
         viewer.close()
+
+
+class _TestPluginManager(PluginManager):
+    def assert_plugin_name_registered(self, plugin_name):
+        assert plugin_name in self.plugins
+
+    def assert_module_registered(self, module):
+        if isinstance(module, str):
+            assert module in {m.__name__ for m in self.plugins.values()}
+        else:
+            assert module in self.plugins.values()
+
+    def assert_implementations_registered(self, plugin, hook_names=()):
+        from typing import Collection
+
+        plugin = self._ensure_plugin(plugin)
+        regnames = {hook.name for hook in self._plugin2hookcallers[plugin]}
+        _hook_names = (
+            hook_names
+            if isinstance(hook_names, Collection)
+            and not isinstance(hook_names, str)
+            else [hook_names]
+        )
+        if not _hook_names:
+            if not regnames:
+                raise AssertionError(
+                    f"No implementations were registered for plugin {plugin!r}"
+                )
+        else:
+            for hook in _hook_names:
+                if hook not in regnames:
+                    raise AssertionError(
+                        f"{hook!r} was not registered for plugin {plugin!r}"
+                    )
+
+
+@pytest.fixture
+def napari_plugin_tester():
+    """A fixture that can be used to test plugin registration.
+
+    See _TestPluginManager above for tests implementations:
+    Examples
+    --------
+    >>> def test_pm(napari_plugin_tester):
+    >>> napari_plugin_tester.assert_plugin_name_registered("test-plugin")
+    >>> napari_plugin_tester.assert_module_registered(_test)
+    >>> napari_plugin_tester.assert_implementations_registered(
+    >>>     "test-plugin", "napari_get_reader"
+    >>> )
+    """
+    from napari.plugins import hook_specifications
+
+    pm = _TestPluginManager('napari', discover_entry_point='napari.plugin')
+    pm.add_hookspecs(hook_specifications)
+    pm.discover()
+    return pm
