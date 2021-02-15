43d6c58d707d833ca4d39be56aa90c78683f5be8
Talley Lambert <talley.lambert@gmail.com>
2021-02-14 20:33:32 -0500

    wip

--- napari/_qt/tree/qt_layer_tree.py
+++ napari/_qt/tree/qt_layer_tree.py
@@ -6,13 +6,14 @@ from qtpy.QtCore import (
     QItemSelection,
     QItemSelectionModel,
     QModelIndex,
+    QRect,
     QSize,
     Qt,
 )
-from qtpy.QtGui import QImage, QPainter, QPixmap
+from qtpy.QtGui import QImage, QPainter, QPalette, QPixmap
 from qtpy.QtWidgets import (
     QApplication,
-    QCommonStyle,
+    QProxyStyle,
     QStyle,
     QStyledItemDelegate,
     QStyleOption,
@@ -78,33 +79,46 @@ class QtLayerTreeModel(QtNodeTreeModel):
 
 
 class LayerDelegate(QStyledItemDelegate):
+    pass
+
     def paint(
         self,
         painter: QPainter,
         option: QStyleOptionViewItem,
         index: QModelIndex,
     ):
+        print('ld', index.row(), option.rect)
+
         opt = option
         self.initStyleOption(opt, index)
         w = opt.widget
         p = painter
         style: QStyle = w.style() if w else QApplication.style()
         style.drawControl(QStyle.CE_ItemViewItem, option, p, w)
-        # style.drawPrimitive(QStyle.PE_IndicatorItemViewItemCheck, opt, p, w)
-
-        # checkRect = style.subElementRect(
-        #     QStyle.SE_ItemViewItemCheckIndicator, opt, w
-        # )
-        # iconRect = style.subElementRect(
-        #     QStyle.SE_ItemViewItemDecoration, opt, w
-        # )
-        # textRect = style.subElementRect(QStyle.SE_ItemViewItemText, opt, w)
-
-        # layer = index.internalPointer()
-        # super().paint(painter, option, index)
 
 
-class QtLayerTreeStyle(QCommonStyle):
+METRICS = {
+    QStyle.PM_IndicatorWidth: 16,
+    QStyle.PM_IndicatorHeight: 16,
+    QStyle.PM_FocusFrameHMargin: 2,
+}
+
+
+class QtLayerTreeStyle(QProxyStyle):
+    """A QProxyStyle wraps a QStyle (usually the default system style) for the
+    purpose of dynamically overriding painting or other style behavior.
+    """
+
+    def pixelMetric(
+        self,
+        metric: QStyle.PixelMetric,
+        option: QStyleOption = None,
+        widget: QWidget = None,
+    ) -> int:
+        if metric in METRICS:
+            return METRICS[metric]
+        return super().pixelMetric(metric, option=option, widget=widget)
+
     def drawPrimitive(
         self,
         pe: QStyle.PrimitiveElement,
@@ -113,19 +127,17 @@ class QtLayerTreeStyle(QCommonStyle):
         widget: QWidget,
     ) -> None:
         if pe == QStyle.PE_IndicatorItemViewItemCheck:
-            img = (
-                'visibility'
-                if (opt.state & QStyle.State_On)
-                else 'visibility_off'
-            )
-            p.drawImage(opt.rect, QImage(f":/themes/dark/{img}.svg"))
-            # if opt.state & QStyle.State_NoChange:
-            #     p.setPen(opt.palette.windowText().color())
-            #     p.fillRect(opt.rect, opt.palette.brush(QPalette.Button))
-            #     p.drawRect(opt.rect)
-            #     p.drawLine(opt.rect.topLeft(), opt.rect.bottomRight())
-            # else:
-            #     super().drawPrimitive(pe, opt, p, widget)
+            print('pe  ', opt.rect)
+            if opt.state & QStyle.State_On:
+                p.drawImage(opt.rect, QImage(":/themes/dark/visibility.svg"))
+            elif opt.state & QStyle.State_Off:
+                p.drawImage(
+                    opt.rect, QImage(":/themes/dark/visibility_off.svg")
+                )
+            elif opt.state & QStyle.State_NoChange:
+                p.setPen(opt.palette.color(QPalette.Dark))
+                p.fillRect(opt.rect, opt.palette.brush(QPalette.Background))
+                p.drawRect(opt.rect)
             return
         if pe == QStyle.PE_IndicatorBranch:
             pass
@@ -137,6 +149,11 @@ class QtLayerTreeView(QtNodeTreeView):
         super().__init__(root, parent)
         self.setItemDelegate(LayerDelegate())
         self.setStyle(QtLayerTreeStyle())
+        # self.setStyleSheet(
+        #     """
+        # QtLayerTreeView::branch { background-color: 'blue'; }
+        # """
+        # )
 
     def setRoot(self, root: LayerGroup):
         self.setModel(QtLayerTreeModel(root, self))
@@ -145,6 +162,9 @@ class QtLayerTreeView(QtNodeTreeView):
         for child in root.traverse():
             selected = getattr(child, 'selected', False)
             self._select(child.index_from_root(), selected)
+        self.model().dataChanged.emit(
+            self.model().index(0), self.model().index(len(root))
+        )
 
     def selectionChanged(
         self, selected: QItemSelection, deselected: QItemSelection
@@ -161,11 +181,13 @@ class QtLayerTreeView(QtNodeTreeView):
         s = getattr(QItemSelectionModel, 'Select' if selected else 'Deselect')
         self.selectionModel().select(idx, s)
 
-    # def drawBranches(
-    #     self,
-    #     painter: QPainter,
-    #     rect: QRect,
-    #     index: QModelIndex,
-    # ) -> None:
-    #     """Responsible for drawing the arrows."""
-    #     return super().drawBranches(painter, rect, index)
+    def drawBranches(
+        self,
+        painter: QPainter,
+        rect: QRect,
+        index: QModelIndex,
+    ) -> None:
+        """Responsible for drawing the arrows."""
+        # rect.setWidth(rect.width() * 3 // 4)
+        print('b ', index.row(), rect)
+        return super().drawBranches(painter, rect, index)
