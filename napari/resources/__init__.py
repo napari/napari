d588f450bb8fea36c3f76e38c441db4f9b091a78
Talley Lambert <talley.lambert@gmail.com>
2021-02-19 01:23:34 -0500

    QColoredSVGIcon class for dynamic colorized svg icons (#2279)
    * QColoredSVGIcon class
    
    * docstrings
    
    * add tests
    
    * add note
    
    * reorg
    
    * fix lru_cache for 3.7
    
    * only get .svg icons
    
    * typing and staticmethod
    
    * encode
    
    * move regex check
    
    * fix order
    
    * undo future annotations

--- napari/resources/__init__.py
+++ napari/resources/__init__.py
@@ -1,9 +1,14 @@
-import warnings
 from typing import List, Optional
+
+from ._icons import ICON_PATH, ICONS, get_colorized_svg, get_icon_path
+
+__all__ = ['get_colorized_svg', 'get_icon_path', 'ICON_PATH', 'ICONS']
 
 
 def get_stylesheet(extra: Optional[List[str]] = None) -> str:
     """For backward compatibility"""
+    import warnings
+
     warnings.warn(
         "Moved to module napari._qt.qt_resources. Will be removed after version 0.4.6.",
         category=DeprecationWarning,
