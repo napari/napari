18aeb69304862adfe20ceafa5c14369b2a2fd8fd
Talley Lambert <talley.lambert@gmail.com>
2021-02-22 10:27:38 -0500

    move example, provide docstring

--- examples/dev/node_tree.py
+++ examples/dev/node_tree.py
@@ -1,4 +1,21 @@
-from napari.qt import get_app, run
+"""Example of using low-level QtNodeTreeView with Node and Group
+
+:class:`napari.utils.tree.Node` is a class that may be used as a mixin that
+allows an object to be a member of a "tree".
+
+:class:`napari.utils.tree.Group` is a (nestable) mutable sequence of Nodes, and
+is also itself a Node (this is the "composite" patter):
+https://refactoring.guru/design-patterns/composite/python/example
+
+These two classes may be used to create tree-like data structures that behave
+like pure python lists of lists.
+
+This examples shows that :class:`napari._qt.tree.qt_tree_view.QtNodeTreeView`
+is capable of providing a basic GUI for any tree structure based on
+`napari.utils.tree.Group`.
+"""
+from qtpy.QtWidgets import QApplication
+
 from napari._qt.tree.qt_tree_view import QtNodeTreeView
 from napari.utils.tree import Node, Group
 import logging
@@ -14,7 +31,7 @@ green = "\033[0;32m"
 colorlog_format = f'{green}%(levelname)6s:{end} {Dim}%(name)43s.{ResetDim}{red}%(funcName)-18s{end}{"%(message)s"}'
 logging.basicConfig(level=logging.DEBUG, format=colorlog_format)
 
-get_app()
+app = QApplication([])
 
 tip = Node(name='tip')
 lg2 = Group([Node(name='2'), Node(name='3')], name="g2")
@@ -32,7 +49,6 @@ print(root)
 root.events.reordered.connect(lambda e: print(e.value))
 
 view = QtNodeTreeView(root)
-model = view.model()
 view.show()
 
-run()
+app.exec_()
