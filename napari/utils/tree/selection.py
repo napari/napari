ec6650c78dc79ec8fedad6f1882086341bd946ba
Talley Lambert <talley.lambert@gmail.com>
2021-02-22 15:26:08 -0500

    rename indexType

--- napari/utils/tree/selection.py
+++ napari/utils/tree/selection.py
@@ -2,20 +2,20 @@ from typing import Iterable, Optional, TypeVar
 
 from ...utils.events import EventedSet
 
-IndexType = TypeVar("IndexType")
+_T = TypeVar("_T")
 
 
-class Selection(EventedSet[IndexType]):
-    def __init__(self, data: Iterable[IndexType] = ()):
+class Selection(EventedSet[_T]):
+    def __init__(self, data: Iterable[_T] = ()):
         super().__init__(data=data)
         self.events.add(current=None)
-        self._current: Optional[IndexType] = None
+        self._current: Optional[_T] = None
 
     @property
-    def current(self) -> Optional[IndexType]:
+    def current(self) -> Optional[_T]:
         return self._current
 
     @current.setter
-    def current(self, index: Optional[IndexType]):
+    def current(self, index: Optional[_T]):
         previous, self._current = self._current, index
         self.events.current(value=index, previous=previous)
