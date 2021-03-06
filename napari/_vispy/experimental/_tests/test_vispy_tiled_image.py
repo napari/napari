fix clims scaling

napari/_vispy/experimental/_tests/test_vispy_tiled_image.py
@@ -17,8 +17,9 @@ skip_local_popups = pytest.mark.skipif(
     reason='Tests requiring GUI windows are skipped locally by default.',
 )
 
-# Add a loading delay in ms, any number > 0 seems to work.
-LOADING_DELAY = 1
+# Add a loading delay in ms.
+SHORT_LOADING_DELAY = 1
+LONG_LOADING_DELAY = 250
 
 # Test all dtypes
 dtypes = [
@@ -41,16 +42,19 @@ dtypes = [
 @skip_on_win_ci
 @skip_local_popups
 @pytest.mark.parametrize('dtype', dtypes)
-def test_tiled_screenshot(qtbot, make_napari_viewer, dtype):
+def test_tiled_screenshot(qtbot, monkeypatch, make_napari_viewer, dtype):
     """Test rendering of tiled data with screenshot."""
+    # Enable tiled rendering
+    monkeypatch.setenv("NAPARI_OCTREE", "1")
+
     viewer = make_napari_viewer(show=True)
     # Set canvas size to target amount
     viewer.window.qt_viewer.view.canvas.size = (800, 600)
 
     shapes = [(4000, 3000), (2000, 1500), (1000, 750), (500, 375)]
-    data = [np.ones(s, dtype) for s in shapes]
+    data = [100 * np.ones(s, dtype) for s in shapes]
     layer = viewer.add_image(
-        data, multiscale=True, contrast_limits=[0, 1], colormap='blue'
+        data, multiscale=True, contrast_limits=[0, 200], colormap='blue'
     )
 
     visual = viewer.window.qt_viewer.layer_to_visual[layer]
@@ -59,12 +63,12 @@ def test_tiled_screenshot(qtbot, make_napari_viewer, dtype):
     assert isinstance(visual, VispyTiledImageLayer)
 
     # Wait until the chunks have added
-    qtbot.wait(LOADING_DELAY)
+    qtbot.wait(SHORT_LOADING_DELAY)
 
     # Take the screenshot
     screenshot = viewer.screenshot(canvas_only=True)
     center_coord = np.round(np.array(screenshot.shape[:2]) / 2).astype(np.int)
-    target_center = np.array([0, 0, 255, 255], dtype='uint8')
+    target_center = np.array([0, 0, 128, 255], dtype='uint8')
     target_edge = np.array([0, 0, 0, 255], dtype='uint8')
     screen_offset = 3  # Offset is needed as our screenshots have black borders
 
@@ -80,8 +84,13 @@ def test_tiled_screenshot(qtbot, make_napari_viewer, dtype):
 @pytest.mark.async_only
 @skip_on_win_ci
 @skip_local_popups
-def test_tiled_changing_contrast_limits(qtbot, make_napari_viewer):
+def test_tiled_changing_contrast_limits(
+    qtbot, monkeypatch, make_napari_viewer
+):
     """Test changing contrast limits of tiled data."""
+    # Enable tiled rendering
+    monkeypatch.setenv("NAPARI_OCTREE", "1")
+
     viewer = make_napari_viewer(show=True)
     # Set canvas size to target amount
     viewer.window.qt_viewer.view.canvas.size = (800, 600)
@@ -98,7 +107,7 @@ def test_tiled_changing_contrast_limits(qtbot, make_napari_viewer):
     assert isinstance(visual, VispyTiledImageLayer)
 
     # Wait until the chunks have added
-    qtbot.wait(LOADING_DELAY)
+    qtbot.wait(SHORT_LOADING_DELAY)
 
     # Take the screenshot
     screenshot = viewer.screenshot(canvas_only=True)
@@ -118,6 +127,9 @@ def test_tiled_changing_contrast_limits(qtbot, make_napari_viewer):
 
     # Make clim data range so center pixel now appears fully saturated
     layer.contrast_limits = [0, 1]
+
+    # Required wait is longer
+    qtbot.wait(LONG_LOADING_DELAY)
 
     screenshot = viewer.screenshot(canvas_only=True)
     np.testing.assert_allclose(screenshot[tuple(center_coord)], target_center)

napari/_vispy/experimental/texture_atlas.py
@@ -253,13 +253,17 @@ class TextureAtlas2D(Texture2D):
 
         return _quad(shape, pos)
 
-    def add_tile(self, octree_chunk: OctreeChunk) -> Optional[AtlasTile]:
+    def add_tile(
+        self, octree_chunk: OctreeChunk, clim=None
+    ) -> Optional[AtlasTile]:
         """Add one tile to the atlas.
 
         Parameters
         ----------
         octree_chunk : np.ndarray
             The image data for this one tile.
+        clim : tuple, optional
+            Contrast limits to normalize by if provided.
 
         Returns
         -------
@@ -270,6 +274,16 @@ class TextureAtlas2D(Texture2D):
 
         if data.dtype == np.float64:
             data = data.astype(np.float32)
+
+        # normalize by contrast limits if provided. This normalization
+        # will not be required after https://github.com/vispy/vispy/pull/1920/
+        if clim is not None and (data.ndim == 2 or data.shape[2] == 1):
+            clim = np.asarray(clim, dtype=np.float32)
+            data = data - clim[0]  # not inplace so we don't modify orig data
+            if clim[1] - clim[0] > 0:
+                data /= clim[1] - clim[0]
+            else:
+                data[:] = 1 if data[0, 0] != 0 else 0
 
         assert isinstance(data, np.ndarray)
 

napari/_vispy/experimental/tiled_image_visual.py
@@ -99,30 +99,6 @@ class TiledImageVisual(ImageVisual):
         self._texture_atlas = self._create_texture_atlas(tile_shape)
         self.freeze()
 
-    @property
-    def clim(self):
-        return self._clim if isinstance(self._clim, str) else tuple(self._clim)
-
-    @clim.setter
-    def clim(self, clim):
-        if isinstance(clim, str):
-            if clim != 'auto':
-                raise ValueError('clim must be "auto" if a string')
-            self._need_texture_upload = True
-        else:
-            clim = np.array(clim, float)
-            if clim.shape != (2,):
-                raise ValueError('clim must have two elements')
-            if self._texture_limits is not None and (
-                (clim[0] < self._texture_limits[0])
-                or (clim[1] > self._texture_limits[1])
-            ):
-                self._need_texture_upload = True
-        self._clim = clim
-        if self._texture_limits is not None:
-            self.shared_program.frag['color_transform'][1]['clim'] = self.clim
-        self.update()
-
     def _create_texture_atlas(self, tile_shape: np.ndarray) -> TextureAtlas2D:
         """Create texture atlas up front or if we change texture shape.
 
@@ -255,7 +231,11 @@ class TiledImageVisual(ImageVisual):
             The newly added chunk's index.
         """
         # Add to the texture atlas.
-        atlas_tile = self._texture_atlas.add_tile(octree_chunk)
+        # Note that clim data is currently provided to do a normalization. This
+        # will not be required after https://github.com/vispy/vispy/pull/1920/
+        atlas_tile = self._texture_atlas.add_tile(
+            octree_chunk, clim=self._clim
+        )
 
         if atlas_tile is None:
             # TODO_OCTREE: No slot was available in the atlas. That's bad,
@@ -380,7 +360,7 @@ class TiledImageVisual(ImageVisual):
             self.shared_program.frag[
                 'color_transform'
             ] = _build_color_transform(
-                grayscale, self.clim, self.gamma, self.cmap
+                grayscale, self.clim_normalized, self.gamma, self.cmap
             )
             self._need_colortransform_update = False
             prg['texture2D_LUT'] = (