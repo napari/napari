0cf3ab31ae4e31661f60e26dd84573e0b683001d
Talley Lambert <talley.lambert@gmail.com>
2021-03-07 09:54:58 -0500

    add assertions to test_notification

--- napari/_tests/test_notification_manager.py
+++ napari/_tests/test_notification_manager.py
@@ -1,4 +1,7 @@
 import sys
+import warnings
+
+import pytest
 
 from napari.utils.notifications import notification_manager, show_info
 
@@ -12,18 +15,29 @@ def test_notification_manager_no_gui():
     """
 
     with notification_manager:
+
         show_info('this is one way of showing an information message')
+        assert len(notification_manager.records) == 1
         notification_manager.receive_info(
             'This is another information message'
         )
+        assert len(notification_manager.records) == 2
 
-        class PurposefullException(Exception):
+        # test that exceptions that go through sys.excepthook are catalogued
+
+        class PurposefulException(Exception):
             pass
 
-        try:
-            raise PurposefullException("this is an exception")
-        except PurposefullException:
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            notification_manager.receive_error(
-                exc_type, exc_value, exc_traceback
-            )
+        with pytest.raises(PurposefulException):
+            raise PurposefulException("this is an exception")
+
+        # pytest intercepts the error, so we can manually call sys.excepthook
+        assert sys.excepthook == notification_manager.receive_error
+        sys.excepthook(*sys.exc_info())
+        assert len(notification_manager.records) == 3
+
+        # test that warnings that go through showwarning are catalogued
+        # again, pytest intercepts this, so just manually trigger:
+        assert warnings.showwarning == notification_manager.receive_warning
+        warnings.showwarning('this is a warning', UserWarning, '', 0)
+        assert len(notification_manager.records) == 4
