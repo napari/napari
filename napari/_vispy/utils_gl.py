80e97b574f7272c050f75269fca347e370d89aa7
Nicholas Sofroniew <sofroniewn@gmail.com>
2021-03-06 20:22:39 -0800

    Fix rgb for octree (#2362)
    * fix dtypes
    
    * fix typo
    
    * fix typo

--- napari/_vispy/utils_gl.py
+++ napari/_vispy/utils_gl.py
@@ -4,9 +4,18 @@ from contextlib import contextmanager
 from functools import lru_cache
 from typing import Tuple
 
+import numpy as np
 from vispy.app import Canvas
 from vispy.gloo import gl
 from vispy.gloo.context import get_current_canvas
+
+texture_dtypes = [
+    np.dtype(np.int8),
+    np.dtype(np.uint8),
+    np.dtype(np.int16),
+    np.dtype(np.uint16),
+    np.dtype(np.float32),
+]
 
 
 @contextmanager
@@ -53,3 +62,34 @@ def get_max_texture_sizes() -> Tuple[int, int]:
     max_size_3d = 2048
 
     return max_size_2d, max_size_3d
+
+
+def fix_data_dtype(data):
+    """Makes sure the dtype of the data is accetpable to vispy.
+
+    Acceptable types are int8, uint8, int16, uint16, float32, float64.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Data that will need to be of right type.
+
+    Returns
+    -------
+    np.ndarray
+        Data that is of right type and will be passed to vispy.
+    """
+
+    dtype = np.dtype(data.dtype)
+    if dtype in texture_dtypes:
+        return data
+    else:
+        try:
+            dtype = dict(i=np.int16, f=np.float32, u=np.uint16, b=np.uint8)[
+                dtype.kind
+            ]
+        except KeyError:  # not an int or float
+            raise TypeError(
+                f'type {dtype} not allowed for texture; must be one of {set(texture_dtypes)}'  # noqa: E501
+            )
+        return data.astype(dtype)
