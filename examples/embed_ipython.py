1f0a52a3c7ad1cb70c01b0d639188fce6dabe412
Lorenzo Gaifas <brisvag@gmail.com>
2021-03-09 09:42:27 +0100

    clearer docstring and comments

--- examples/embed_ipython.py
+++ examples/embed_ipython.py
@@ -1,6 +1,13 @@
 """
-Start napari and land directly in an embedded ipython console with qt event loop
+Start napari and land directly in an embedded ipython console with qt event loop.
+
+A similar effect can be achieved more simply with `viewer.update_console(locals())`,
+such as shown in https://github.com/napari/napari/blob/master/examples/update_console.py.
+
+However, differently from `update_console`, this will start an independent
+ipython console which can outlive the viewer.
 """
+
 import napari
 from IPython.terminal.embed import InteractiveShellEmbed
 
@@ -12,7 +19,7 @@ viewer = napari.Viewer()
 
 # embed ipython and run the magic command to use the qt event loop
 sh = InteractiveShellEmbed()
-sh.run_cell('%gui qt')
-sh()  # the ipython shell will open
+sh.run_cell('%gui qt')  # any valid python or ipython command can be run like this!
+sh()  # open the embedded shell
 
 # From there, you can access the script's scope, such as the variables `text` and `viewer`
