381d816e7bb8fc2d1a1a4341eaba532166e633d9
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-03-21 11:52:02 -0700

    Add a button to Qt Error Notification to show tracebacks.
    And have the "View Traceback" button localized.

--- napari/utils/notifications.py
+++ napari/utils/notifications.py
@@ -106,6 +106,17 @@ class ErrorNotification(Notification):
         super().__init__(msg, NotificationSeverity.ERROR, actions)
         self.exception = exception
 
+    def as_html(self):
+        from ._tracebacks import get_tb_formatter
+
+        fmt = get_tb_formatter()
+        exc_info = (
+            self.exception.__class__,
+            self.exception,
+            self.exception.__traceback__,
+        )
+        return fmt(exc_info, as_html=True)
+
     def __str__(self):
         from ._tracebacks import get_tb_formatter
 
