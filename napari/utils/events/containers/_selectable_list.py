1946d8cc9404e3ef1cf5fb1dd6e27bd90600084d
Talley Lambert <talley.lambert@gmail.com>
2021-03-20 21:53:57 -0400

    remove from selection when removing from list

--- napari/utils/events/containers/_selectable_list.py
+++ napari/utils/events/containers/_selectable_list.py
@@ -8,3 +8,7 @@ _T = TypeVar("_T")
 
 class SelectableEventedList(Selectable[_T], EventedList[_T]):
     """List model that also supports selection."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.events.removed.connect(lambda e: self.selection.discard(e.value))
