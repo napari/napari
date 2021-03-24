c4ce21310c1985de97564de7167ffc351dfd5f91
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2021-03-24 11:40:43 +0100

    clean up set lim methods

--- napari/layers/image/image.py
+++ napari/layers/image/image.py
@@ -2,6 +2,7 @@
 """
 import types
 import warnings
+from typing import Tuple, Union
 
 import numpy as np
 from scipy import ndimage as ndi
@@ -487,22 +488,28 @@ class _ImageBase(IntensityVisualizationMixin, Layer):
         self._bounding_box_lims = bounding_box
         self.events.bounding_box()
 
-    def _set_xlim(self, xlim):
-        self._bounding_box = np.vstack(
-            (self._bounding_box_lims[:2, :], xlim)
-        ).astype(
-            np.float32
-        )  # reorder these to zyx?
+    def _set_bbox_lim(
+        self, lims: Union[np.ndarray, Tuple[float, float]], axis: int
+    ):
+        self._bounding_box_lims[axis, :] = np.asarray(lims, dtype=float)
+        self.events.bounding_box()
 
-    def _set_ylim(self, ylim):
-        self._bounding_box = np.vstack(
-            (self._bounding_box[0, :], ylim, self._bounding_box_lims[2, :])
-        ).astype(np.float32)
+    # def _set_xlim(self, xlim):
+    #     self._bounding_box = np.vstack(
+    #         (self._bounding_box_lims[:2, :], xlim)
+    #     ).astype(
+    #         np.float32
+    #     )
 
-    def _set_zlim(self, zlim):
-        self._bounding_box = np.vstack(
-            (zlim, self._bounding_box[1:, :])
-        ).astype(np.float32)
+    # def _set_ylim(self, ylim):
+    #     self._bounding_box = np.vstack(
+    #         (self._bounding_box[0, :], ylim, self._bounding_box_lims[2, :])
+    #     ).astype(np.float32)
+
+    # def _set_zlim(self, zlim):
+    #     self._bounding_box = np.vstack(
+    #         (zlim, self._bounding_box[1:, :])
+    #     ).astype(np.float32)
 
     @property
     def _view_bounding_box(self):
