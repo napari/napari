91cf935ecb213fff13f22f7cef433cd449692af0
Talley Lambert <talley.lambert@gmail.com>
2021-03-30 07:49:48 -0400

    Update napari/components/viewer_model.py
    Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

--- napari/components/viewer_model.py
+++ napari/components/viewer_model.py
@@ -279,7 +279,7 @@ class ViewerModel(KeymapProvider, MousemapProvider, EventedModel):
         warnings.warn(
             "'viewer.active_layer' is deprecated and will be removed in napari"
             " v0.4.9.  Please use 'viewer.layers.selection.active' instead.",
-            category=DeprecationWarning,
+            category=FutureWarning,
             stacklevel=2,
         )
         return self.layers.selection.active
