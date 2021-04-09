35bfde4354a562169a4f7240861ec55101445f06
Fifourche <faris.abouakil@gmail.com>
2021-04-09 23:21:49 +0200

    Compute angles separately, and do not define an inner function to cap theta_2.

--- napari/_vispy/quaternion.py
+++ napari/_vispy/quaternion.py
@@ -16,25 +16,24 @@ def quaternion2euler(quaternion, degrees=False):
     angles : 3-tuple
         Euler angles in (rx, ry, rz) order.
     """
-    
-    def capper(value):
-        # Cap the value to +1 or -1
-        value_capped = value if abs(value) < 1 else np.sign(value)
-        return value_capped
-    
-    
+
     q = quaternion
-    angles = (
-        np.arctan2(
+
+    theta_1 = np.arctan2(
             2 * (q.w * q.z + q.y * q.x),
             1 - 2 * (q.y * q.y + q.z * q.z),
-        ),
-        np.arcsin(capper(2 * (q.w * q.y - q.z * q.x))),
-        np.arctan2(
+        )
+
+    theta_2 = 2 * (q.w * q.y - q.z * q.x)
+    theta_2 = np.arcsin(theta_2) if abs(theta_2) < 1 else np.sign(theta_2)*np.pi/2
+
+    theta_3 = np.arctan2(
             2 * (q.w * q.x + q.y * q.z),
             1 - 2 * (q.x * q.x + q.y * q.y),
-        ),
-    )
+        )
+
+    angles = (theta_1, theta_2, theta_3)
+
     if degrees:
         return tuple(np.degrees(angles))
     else:
