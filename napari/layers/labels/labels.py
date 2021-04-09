34a76891d84515b85d92b03e7e7e9f92770cac26
Volker Hilsenstein <volker.hilsenstein@embl.de>
2021-04-09 09:15:51 +0200

    trigger IndexError instead of TypeError

--- napari/layers/labels/labels.py
+++ napari/layers/labels/labels.py
@@ -316,7 +316,7 @@ class Labels(_image_base_class):
         self._selected_color = self.get_color(self.selected_label)
         # invalidate _all_vals to trigger re-generation
         # in _raw_to_displayed
-        self._all_vals = None
+        self._all_vals = np.array([])
         self.refresh()
         self.events.selected_label()
 
@@ -718,7 +718,7 @@ class Labels(_image_base_class):
         ):
             try:
                 image = self._all_vals[raw]
-            except (IndexError, TypeError):
+            except IndexError:
                 max_val = np.max(raw)
                 self._all_vals = low_discrepancy_image(
                     np.arange(max_val + 1), self._seed
