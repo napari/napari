c7dad2d3b8d68007edd396f39823f208808b17f1
Fifourche <faris.abouakil@gmail.com>
2021-04-10 17:29:51 +0200

    Changed the unittest of quaternion2euler to compare the unit quaternions and not the Euler anglees themselves (they can always be degenerate).

--- napari/_vispy/_tests/test_utils.py
+++ napari/_vispy/_tests/test_utils.py
@@ -12,10 +12,16 @@ def test_quaternion2euler():
     for angles in angles_test:
         q = Quaternion.create_from_euler_angles(*angles, degrees=True)
         ea = quaternion2euler(q, degrees=True)
-        np.testing.assert_allclose(ea, angles)
+        q_p = Quaternion.create_from_euler_angles(*ea, degrees=True)
+        np.testing.assert_allclose(
+            list(q.__dict__.values()), list(q_p.__dict__.values())
+        )
 
         # Test roundtrip radians
         angles_rad = np.radians(angles)
         q = Quaternion.create_from_euler_angles(*angles_rad)
         ea = quaternion2euler(q)
-        np.testing.assert_allclose(ea, angles_rad)
+        q_p = Quaternion.create_from_euler_angles(*ea)
+        np.testing.assert_allclose(
+            list(q.__dict__.values()), list(q_p.__dict__.values())
+        )
