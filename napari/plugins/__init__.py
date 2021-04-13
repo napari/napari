c799d11049830ff0263df96cc1425770ae7f3def
Talley Lambert <talley.lambert@gmail.com>
2021-04-12 23:22:59 -0400

    fix set_call_order with missing plugin (#2543)
    * fix set_call_order with missing plugin
    
    * add comment

--- napari/plugins/__init__.py
+++ napari/plugins/__init__.py
@@ -115,8 +115,14 @@ class PluginManager(_PM):
         for spec_name, hook_caller in self.hooks.items():
             order = []
             for p in new_order.get(spec_name, []):
+                try:
+                    # if the plugin was uninstalled in the meantime,
+                    # we should just move on to the next plugin
+                    imp = hook_caller.get_plugin_implementation(p['plugin'])
+                except KeyError:
+                    continue
+                imp.enabled = p['enabled']
                 order.append(p['plugin'])
-                hook_caller._set_plugin_enabled(p['plugin'], p['enabled'])
             if order:
                 hook_caller.bring_to_front(order)
 
