1b3a07db9beacc4325fc54231d1ea7f15604ed11
Draga Doncila Pop <ddon0001@student.monash.edu>
2021-04-14 08:33:47 +1000

    Implement basic ProgressBar API and higher level progress API

--- napari/utils/progress.py
+++ napari/utils/progress.py
@@ -1,6 +1,6 @@
 from typing import Iterable, Optional
 
-from PyQt5.QtWidgets import QHBoxLayout, QLabel, QProgressBar
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QLabel, QProgressBar
 
 from .._qt.utils import get_viewer_instance
 
@@ -22,38 +22,56 @@ class progress:
 
         if iterable is not None:  # iterator takes priority over total
             try:
-                self._total = len(iterable)
+                self._total = len(iterable) - 1
             except TypeError:  # generator (total needed)
                 self._total = total if total is not None else 0
         else:
             if total is not None:
+                self._iterable = range(total + 1)
                 self._total = total
             else:
-                self._total = 0  # indeterminate bar
+                self._total = 0
 
-        self._pbar.set_total(self._total)
+        self._pbar._set_total(self._total)
+        self._pbar._set_description("Test Description")
+
+        QApplication.processEvents()
 
     def __iter__(self):
         for n, i in enumerate(self._iterable):
-            self._pbar.set_value(n)
+            self._pbar._set_value(n)
             yield i
+
+    def update(self, val, desc=None):
+        if val > self._total:
+            # exceeded total, become indeterminate
+            self._pbar._set_total(0)
+        else:
+            self._pbar._set_value(val)
+
+        if desc:
+            self._pbar._set_description(desc)
+        QApplication.processEvents()
 
 
 class ProgressBar:
-    def __init__(self) -> None:
+    def __init__(self, description="") -> None:
         self.pbar = QProgressBar()
-        self.label = QLabel("Test Label")
+        self.label = QLabel(description)
 
         layout = QHBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.pbar)
         self.layout = layout
 
-    def set_total(self, total):
-        self.pbar.setRange(0, total)
+    def _set_total(self, total):
+        if total > 0:
+            self.pbar.setMaximum(total)
+        else:
+            self.pbar.setRange(0, 0)
 
-    def set_value(self, value):
+    def _set_value(self, value):
         self.pbar.setValue(value)
 
-    def set_description(self, desc):
-        pass
+    def _set_description(self, desc):
+        self.label.setText(desc)
