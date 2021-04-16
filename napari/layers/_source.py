e7d7a17839ff537a6af188bc51964025da8efbca
Talley Lambert <talley.lambert@gmail.com>
2021-04-15 20:36:32 -0400

    add docstring

--- napari/layers/_source.py
+++ napari/layers/_source.py
@@ -45,10 +45,37 @@ _LAYER_SOURCE: ContextVar[dict] = ContextVar('_LAYER_SOURCE', default={})
 def layer_source(**source_kwargs):
     """Creates context in which all layers will be given `source_kwargs`.
 
+    The module-level variable `_LAYER_SOURCE` holds a set of key-value pairs
+    that can be used to create a new `Source` object.  Any routine in napari
+    that may result in the creation of a new layer (such as opening a file,
+    using a particular plugin, or calling a magicgui widget) can use this
+    context manager to declare that any layers created within the context
+    result from a specific source. (This applies even if the layer
+    isn't "directly" created in the context, but perhaps in some sub-function
+    within the context).
+
+    `Layer.__init__` will call :func:`current_source`, to query the current
+    state of the `_LAYER_SOURCE` variable.
+
+    Contexts may be stacked, meaning a given layer.source can reflect the
+    actions of multiple events (for instance, an `open_sample` call that in
+    turn resulted in a `reader_plugin` opening a file).  However, the "deepest"
+    context will "win" in the case where multiple calls to `layer_source`
+    provide conflicting values.
+
     Parameters
     ----------
     **source_kwargs :
         keys/values should be valid parameters for :class:`Source`.
+
+    Examples
+    --------
+
+    >>> with layer_source(path='file.ext', reader_plugin='plugin'):
+    >>>     points = some_function_that_creates_points()
+    ...
+    >>> assert points.source == Source(path='file.ext', reader_plugin='plugin')
+
     """
     token = _LAYER_SOURCE.set({**_LAYER_SOURCE.get(), **source_kwargs})
     try:
