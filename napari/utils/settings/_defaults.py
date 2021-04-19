007ed50fc22fea8ec030fe4889764cbb00d90cac
Talley Lambert <talley.lambert@gmail.com>
2021-04-19 08:03:11 -0400

    delay importing plugin init

--- napari/utils/settings/_defaults.py
+++ napari/utils/settings/_defaults.py
@@ -1,14 +1,14 @@
 """Settings management.
 """
+from __future__ import annotations
 
 import os
 from enum import Enum
 from pathlib import Path
-from typing import List, Tuple
+from typing import Dict, List, Tuple, TypedDict
 
 from pydantic import BaseSettings, Field
 
-from ...plugins import CallOrderDict
 from .._base import _DEFAULT_LOCALE
 from ..events.evented_model import EventedModel
 from ..notifications import NotificationSeverity
@@ -273,6 +273,16 @@ class ApplicationSettings(BaseNapariSettings):
             "open_history",
             "save_history",
         ]
+
+
+class PluginHookOption(TypedDict):
+    """Custom type specifying plugin and enabled state."""
+
+    plugin: str
+    enabled: bool
+
+
+CallOrderDict = Dict[str, List[PluginHookOption]]
 
 
 class PluginsSettings(BaseNapariSettings):
