44000b4b9631438b8b51067a1d4e64fc4cf7573a
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-04-21 12:55:50 +1000

    Make the example bigger, badder, and better

--- examples/points-over-time.py
+++ examples/points-over-time.py
@@ -4,9 +4,12 @@ import dask.array as da
 from skimage import data
 
 
-image4d = da.random.random((20, 32, 256, 256))
-pts_coordinates = np.random.random((300, 3)) * image4d.shape[1:]
-pts_values = np.random.random((300, 20))
+image4d = da.random.random(
+        (4000, 32, 256, 256),
+        chunks=(1, 32, 256, 256),
+        )
+pts_coordinates = np.random.random((50000, 3)) * image4d.shape[1:]
+pts_values = da.random.random((50000, 4000), chunks=(50000, 1))
 
 viewer = napari.Viewer(ndisplay=3)
 image_layer = viewer.add_image(
@@ -14,15 +17,16 @@ image_layer = viewer.add_image(
         )
 pts_layer = viewer.add_points(
         pts_coordinates,
-        properties={'value': pts_values[:, 0]},
+        properties={'value': np.asarray(pts_values[:, 0])},
         face_color='value',
+        size=2,
         )
 
 
 def set_pts_properties(pts_layer, values_table, step):
     # step is a 4D coordinate with the current slider position for each dim
     column = step[0]  # grab the leading ("time") coordinate
-    pts_layer.properties['value'] = values_table[:, column]
+    pts_layer.properties['value'] = np.asarray(values_table[:, column])
     pts_layer.face_color = 'value'  # force properties refresh
 
 
