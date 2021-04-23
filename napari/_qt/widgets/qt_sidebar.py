1cef7671ab305b9ef94568665e8f3733815c6ab6
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2021-04-22 23:55:52 -0500

    Updte styles

--- napari/_qt/widgets/qt_sidebar.py
+++ napari/_qt/widgets/qt_sidebar.py
@@ -1,26 +1,20 @@
-from qtpy.QtCore import QSize, Qt, Signal, QObject
-from qtpy.QtGui import QFont, QIntValidator
+from qtpy.QtCore import Qt
 from qtpy.QtWidgets import (
-    QToolBar,
-    QWidget,
-    QToolButton,
-    QVBoxLayout,
+    QButtonGroup,
     QHBoxLayout,
     QStackedWidget,
-    QAction,
-    QButtonGroup,
+    QToolBar,
+    QWidget,
 )
 
-from ...utils.translations import trans
 from .qt_viewer_buttons import QtStateButton
 
 
-class QtSideBarButton(QToolButton):
+class QtSideBarButton(QtStateButton):
     pass
 
 
 class QtSideBarWidget(QWidget):
-
     def __init__(self, parent: QWidget = None, name: str = None):
         super().__init__(parent)
 
@@ -32,11 +26,10 @@ class QtSideBarWidget(QWidget):
         # Widget setup
         self.setObjectName(name)
         self._buttons.setOrientation(Qt.Vertical)
-        # self._buttons.setVisible(False)
+        self._buttons.setVisible(False)
         self._stack.setVisible(False)
         self._group.setExclusive(False)
         self._group.buttonToggled.connect(self._toggle_widget)
-
 
         self._layout = QHBoxLayout()
         if name == "left":
@@ -53,10 +46,9 @@ class QtSideBarWidget(QWidget):
         self._buttons.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self._layout)
 
-    def add_widget(self, name, text, widget, location = "top"):
-        """
-        """
-        button = QtStateButton(name, self, "_test", None)
+    def add_widget(self, name, text, widget, location="top"):
+        """"""
+        button = QtSideBarButton(name, self, "_test", None)
         button.setToolTip(text)
         button._widget = widget
         self._button_list.append(button)
@@ -66,8 +58,7 @@ class QtSideBarWidget(QWidget):
         self._stack.addWidget(widget)
 
     def _toggle_widget(self, button, value):
-        """
-        """
+        """"""
         for btn in self._button_list:
             if btn != button:
                 btn.blockSignals(True)
@@ -83,8 +74,7 @@ class QtSideBarWidget(QWidget):
 
 
 class QtSideBar(QToolBar):
-    """
-    """
+    """"""
 
     def __init__(self, parent: QWidget = None, name: str = None):
         super().__init__(parent)
@@ -96,5 +86,5 @@ class QtSideBar(QToolBar):
         self.addWidget(self._widget)
         self.setContentsMargins(0, 0, 0, 0)
 
-    def add_widget(self, name, text, widget, location = "top"):
+    def add_widget(self, name, text, widget, location="top"):
         self._widget.add_widget(name, text, widget, location=location)
