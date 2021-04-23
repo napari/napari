0c0379c5cf1982ba54c4d2fdcf9030b0b8666430
Pam Wadhwa <pepainter@gmail.com>
2021-04-23 17:23:21 -0500

    update line width before draw outline

--- napari/_vispy/vispy_shapes_layer.py
+++ napari/_vispy/vispy_shapes_layer.py
@@ -59,10 +59,11 @@ class VispyShapesLayer(VispyBaseLayer):
         self.node.update()
 
     def _on_highlight_change(self, event=None):
-        # Compute the vertices and faces of any shape outlines
-        vertices, faces = self.layer._outline_shapes()
 
         self.layer._highlight_width = SETTINGS.appearance.highlight_thickness
+
+        # Compute the vertices and faces of any shape outlines
+        vertices, faces = self.layer._outline_shapes()
 
         if vertices is None or len(vertices) == 0 or len(faces) == 0:
             vertices = np.zeros((3, self.layer._ndisplay))
