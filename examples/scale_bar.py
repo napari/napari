15c5a1419b7862e1515c98faedcb70f7d0501708
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-04-29 13:25:16 +1000

    Use better data for scale bar example

--- examples/scale_bar.py
+++ examples/scale_bar.py
@@ -3,10 +3,18 @@ Display a 3D volume and the scale bar
 """
 import numpy as np
 import napari
+from skimage import data
 
-viewer = napari.Viewer()
+cells = data.cells3d()
 
-viewer.add_image(np.random.random((5, 5, 5)), colormap='red', opacity=0.8)
+viewer = napari.Viewer(ndisplay=3)
+
+viewer.add_image(
+    cells,
+    name=('membrane', 'nuclei'),
+    channel_axis=1,
+    scale=(0.29, 0.26, 0.26),
+)
 viewer.scale_bar.visible = True
 viewer.scale_bar.unit = "um"
 
