2c7a92130418f5a8e6c0496e21f01c01d6157b19
Talley Lambert <talley.lambert@gmail.com>
2021-05-01 13:17:33 -0400

    fix test

--- napari/_qt/_tests/test_qt_window.py
+++ napari/_qt/_tests/test_qt_window.py
@@ -1,4 +1,3 @@
-from napari import Viewer
 from napari._qt.qt_main_window import _QtMainWindow
 
 
@@ -7,35 +6,32 @@ def test_current_viewer(make_napari_viewer, qapp):
 
     ... where "current" means it was the last viewer the user interacted with.
     """
-    assert Viewer.current() is None
     assert _QtMainWindow.current() is None
 
     # when we create a new viewer it becomes accessible at Viewer.current()
     v1 = make_napari_viewer(title='v1')
-    assert Viewer.current() == v1
     assert _QtMainWindow._instances == [v1.window._qt_window]
     assert _QtMainWindow.current() == v1.window._qt_window
 
     v2 = make_napari_viewer(title='v2')
-    assert Viewer.current() == v2
     assert _QtMainWindow._instances == [
         v1.window._qt_window,
         v2.window._qt_window,
     ]
+    assert _QtMainWindow.current() == v2.window._qt_window
 
     # Viewer.current() will always give the most recently activated viewer.
     v1.window.activate()
-    assert Viewer.current() == v1
+    assert _QtMainWindow.current() == v1.window._qt_window
     v2.window.activate()
-    assert Viewer.current() == v2
+    assert _QtMainWindow.current() == v2.window._qt_window
 
     # The list remembers the z-order of previous viewers ...
     v2.close()
-    assert Viewer.current() == v1
+    assert _QtMainWindow.current() == v1.window._qt_window
     assert _QtMainWindow._instances == [v1.window._qt_window]
 
     # and when none are left, Viewer.current() becomes None again
     v1.close()
-    assert Viewer.current() is None
     assert _QtMainWindow._instances == []
     assert _QtMainWindow.current() is None
