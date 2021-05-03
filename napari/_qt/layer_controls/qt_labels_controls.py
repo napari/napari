6eb438d03ebdbfa4df3c087fe003b5c6a6dacb63
Draga Doncila Pop <ddon0001@student.monash.edu>
2021-05-03 10:21:04 +1000

    Replace QtLargeIntSpinBox with QtAbstractSpinBox

--- napari/_qt/layer_controls/qt_labels_controls.py
+++ napari/_qt/layer_controls/qt_labels_controls.py
@@ -91,7 +91,7 @@ class QtLabelsControls(QtLayerControls):
         self.layer.events.color_mode.connect(self._on_color_mode_change)
 
         # selection spinbox
-        self.selectionSpinBox = QtLargeIntSpinBox(self.layer.data.dtype)
+        self.selectionSpinBox = QtLargeIntSpinBox()
         self.selectionSpinBox.setKeyboardTracking(False)
         self.selectionSpinBox.setMinimum(0)
         self.selectionSpinBox.valueChanged.connect(self.changeSelection)
@@ -123,7 +123,7 @@ class QtLabelsControls(QtLayerControls):
         ndim_sb.setAlignment(Qt.AlignCenter)
         self._on_n_edit_dimensions_change()
 
-        contour_sb = QtLargeIntSpinBox(self.layer.data.dtype)
+        contour_sb = QtLargeIntSpinBox()
         contour_sb.setToolTip(trans._('display contours of labels'))
         contour_sb.valueChanged.connect(self.change_contour)
         self.contourSpinBox = contour_sb
--- napari/_qt/widgets/_tests/test_qt_large_int_spinbox.py
+++ napari/_qt/widgets/_tests/test_qt_large_int_spinbox.py
@@ -1,58 +1,36 @@
-import numpy as np
-import pytest
-
 from ..qt_large_int_spinbox import QtLargeIntSpinBox
 
 
-@pytest.mark.parametrize(
-    ["dtype"],
-    [
-        (np.uint8,),
-        (np.int8,),
-        (np.uint16,),
-        (np.int16,),
-        (np.uint32,),
-        (np.int32,),
-        ("uint8",),
-        (np.dtype("uint8"),),
-    ],
-)
-def test_clamp_dtype(qtbot, dtype):
-    spinbox = QtLargeIntSpinBox(dtype)
-    iinfo = np.iinfo(dtype)
-    assert spinbox.minimum() == iinfo.min
-    assert spinbox.maximum() == iinfo.max
+def test_large_ints(qtbot):
+    spinbox = QtLargeIntSpinBox()
 
+    spinbox.setValue(1e34)
+    assert spinbox.value() == spinbox.maximum()
 
-def test_clamp_float(qtbot):
-    dtype = np.uint64
-    spinbox = QtLargeIntSpinBox(dtype)
-    iinfo = np.iinfo(dtype)
-    assert spinbox.minimum() == iinfo.min
-    assert spinbox.maximum() < iinfo.max
+    spinbox.setValue(1e29)
+    assert spinbox.value() == spinbox.maximum()
+
+    spinbox.setValue(1e13)
+    assert spinbox.value() == int(1e13)
+
+    spinbox.setValue(2 ** 64 - 1)
+    assert spinbox.value() == spinbox.maximum()
 
 
 def test_digitize_value(qtbot):
-    dtype = np.dtype(np.uint32)
-    spinbox = QtLargeIntSpinBox(dtype)
-
-    assert isinstance(spinbox.value(), dtype.type)
+    spinbox = QtLargeIntSpinBox()
 
     spinbox.setValue(1.1)
-    assert isinstance(spinbox.value(), dtype.type)
     assert spinbox.value() == 1
 
     spinbox.setValue(1.9)
-    assert isinstance(spinbox.value(), dtype.type)
     assert spinbox.value() == 1
 
 
 def test_event_emits_int(qtbot):
-    dtype = np.dtype(np.int32)
-
     def fn(value):
         assert isinstance(value, int)
 
-    spinbox = QtLargeIntSpinBox(dtype)
+    spinbox = QtLargeIntSpinBox()
     spinbox.valueChanged.connect(fn)
     spinbox.setValue(1)
