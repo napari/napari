3b96b65f39ec4cfaa3f066a1a63e4e052c8ac469
Talley Lambert <talley.lambert@gmail.com>
2021-05-03 10:20:38 -0400

    more async only

--- napari/components/experimental/chunk/_tests/test_loader.py
+++ napari/components/experimental/chunk/_tests/test_loader.py
@@ -8,11 +8,11 @@ from napari.components.experimental.chunk._pool_group import (
 
 def test_get_loader_config_error():
     """Test that defaults are required."""
-    config = {}
     with pytest.raises(KeyError):
-        _get_loader_configs(config)
+        _get_loader_configs({})
 
 
+@pytest.mark.async_only
 def test_get_loader_config_defaults():
     """Test config that has defaults but no octree loaders."""
     config = {
@@ -44,6 +44,7 @@ TEST_CONFIG = {
 }
 
 
+@pytest.mark.async_only
 def test_get_loader_config_override():
     """Test two loaders that override the defaults."""
     configs = _get_loader_configs(TEST_CONFIG)
