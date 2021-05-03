8bebdf26a95144562db40918235b7bb3db424402
Draga Doncila Pop <ddon0001@student.monash.edu>
2021-05-03 10:56:52 +1000

    Revert "Replace QtLargeIntSpinBox with QtAbstractSpinBox"
    This reverts commit 6eb438d03ebdbfa4df3c087fe003b5c6a6dacb63.

--- napari/_qt/widgets/qt_large_int_spinbox.py
+++ napari/_qt/widgets/qt_large_int_spinbox.py
@@ -1,113 +1,90 @@
-from qtpy.QtCore import QSize, Signal
-from qtpy.QtGui import QFontMetrics, QValidator
-from qtpy.QtWidgets import QAbstractSpinBox, QStyle, QStyleOptionSpinBox
+import logging
+
+import numpy as np
+from qtpy.QtCore import Signal
+from qtpy.QtWidgets import QDoubleSpinBox
+
+logger = logging.getLogger(__name__)
 
 
-class AnyIntValidator(QValidator):
-    def __init__(self, parent=None) -> None:
-        super().__init__(parent)
-
-    def validate(self, input: str, pos: int):
-        if input.isnumeric():
-            return 2, input, len(input)
-        else:
-            return 0, input, len(input)
-
-
-class QtLargeIntSpinBox(QAbstractSpinBox):
-    """An integer spinboxes backed by unbound python integer
+class QtLargeIntSpinBox(QDoubleSpinBox):
+    """
+    A class for integer spinboxes backed by a double (i.e. float64).
 
     Qt's built-in ``QSpinBox`` is backed by a signed 32-bit integer.
     This could become limiting, particularly in large dense segmentations.
-    This class behaves like a ``QSpinBox`` backed by an unbound python int.
+    This class behaves like a ``QSpinBox`` backed by a signed 54-bit integer.
 
-    Does not yet support "prefix", "suffix" or "specialValue" like QSpinBox.
+    Instances are associated with a ``numpy.dtype``
+    which can further constrain their maximum range,
+    and ensures that values are clamped and cast correctly
+    when using setter and getter methods.
     """
 
-    textChanged = Signal(str)
+    MAX_FLOAT64_INT = 2 ** 53
+    MIN_FLOAT64_INT = -MAX_FLOAT64_INT
+
     valueChanged = Signal(int)
 
-    def __init__(self, parent=None) -> None:
+    def __init__(self, dtype=int, parent=None):
         super().__init__(parent)
-        self._value: int = 0
-        self._minimum: int = 0
-        self._maximum: int = 2 ** 64 - 1
-        self._single_step: int = 1
-        validator = AnyIntValidator(self)
-        self.lineEdit().setValidator(validator)
-        self.lineEdit().textChanged.connect(self._editor_text_changed)
-        self.setValue(0)
+        self._dtype = np.dtype(dtype)
+        if not np.issubdtype(self._dtype, np.integer):
+            raise ValueError(f"Spinbox dtype must be integral, got {dtype}")
+        iinfo = np.iinfo(self._dtype)
 
-    def value(self):
-        return self._value
+        self.setSingleStep(1)
+        super().setDecimals(0)
 
-    def setValue(self, value):
-        self._value, old = self._bound(int(value)), self._value
-        self._updateEdit()
-        self.update()
-        if self._value != old:
-            self.textChanged.emit(self.lineEdit().displayText())
-            self.valueChanged.emit(self._value)
+        self._min = self._dtype.type(max(iinfo.min, self.MIN_FLOAT64_INT))
+        self._max = self._dtype.type(min(iinfo.max, self.MAX_FLOAT64_INT))
+        self.setRange(self._min, self._max)
 
-    def _updateEdit(self):
-        new_text = str(self._value)
-        if self.lineEdit().text() == new_text:
-            return
-        self.lineEdit().setText(new_text)
+    def _cast(self, value):
+        actual_val = max(self._min, min(self._max, self._dtype.type(value)))
+        if actual_val != value:
+            logger.warning(
+                "Value %s is not representable by %s(%s); using %s",
+                value,
+                type(self).__qualname__,
+                self._dtype,
+                actual_val,
+            )
+        return actual_val
+
+    def setSingleStep(self, val):
+        return super().setSingleStep(int(val))
 
     def singleStep(self):
-        return self._single_step
-
-    def setSingleStep(self, step):
-        self._single_step = int(step)
-
-    def minimum(self):
-        return self._minimum
-
-    def setMinimum(self, min):
-        self._minimum = int(min)
-
-    def maximum(self):
-        return self._maximum
-
-    def setMaximum(self, max):
-        self._maximum = int(max)
-
-    def setRange(self, minimum, maximum):
-        self.setMinimum(minimum)
-        self.setMaximum(maximum)
+        return int(super().singleStep())
 
     def stepBy(self, steps: int) -> None:
-        step = self._single_step
-        self.setValue(self._bound(self._value + (step * steps)))
+        super().stepBy(steps)
+        self.valueChanged.emit(self.value())
 
-    def _editor_text_changed(self, t):
-        state, *_ = self.validate(t, self.lineEdit().cursorPosition())
-        if state == QValidator.Acceptable:
-            self.setValue(int(t))
+    def setDecimals(self):
+        raise NotImplementedError
 
-    def _bound(self, value):
-        return max(self._minimum, min(self._maximum, value))
+    def setValue(self, value):
+        val = self._cast(value)
+        super().setValue(val)
+        self.valueChanged.emit(val)
 
-    def stepEnabled(self):
-        flags = QAbstractSpinBox.StepNone
-        if self.isReadOnly():
-            return flags
-        if self._value < self._maximum:
-            flags |= QAbstractSpinBox.StepUpEnabled
-        if self._value > self._minimum:
-            flags |= QAbstractSpinBox.StepDownEnabled
-        return flags
+    def value(self):
+        return self._cast(super().value())
 
-    def sizeHint(self):
-        self.ensurePolished()
-        fm = QFontMetrics(self.font())
-        h = self.lineEdit().sizeHint().height()
-        w = fm.horizontalAdvance(str(self._value)) + 3
-        w = max(36, w)
-        opt = QStyleOptionSpinBox()
-        self.initStyleOption(opt)
-        hint = QSize(w, h)
-        return self.style().sizeFromContents(
-            QStyle.CT_SpinBox, opt, hint, self
-        )
+    def setMinimum(self, value):
+        super().setMinimum(self._cast(value))
+
+    def minimum(self):
+        return self._cast(super().minimum())
+
+    def setMaximum(self, value):
+        super().setMaximum(self._cast(value))
+
+    def maximum(self):
+        return self._cast(super().maximum())
+
+    def setRange(self, min_, max_):
+        self.setMinimum(min_)
+        self.setMaximum(max_)
