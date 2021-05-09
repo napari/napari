b7140c12429f529265f1d80cc3c3c8f2138c8790
Talley Lambert <talley.lambert@gmail.com>
2021-05-09 14:34:36 -0400

    remove setup.py changes

--- setup.py
+++ setup.py
@@ -2,23 +2,9 @@
 PEP 517 doesn’t support editable installs
 so this file is currently here to support "pip install -e ."
 """
-import subprocess
-import sys
-from distutils.command.build_py import build_py as _build_py
-
 from setuptools import setup
-
-
-class build_py(_build_py):
-    def run(self) -> None:
-        modules = ['napari.view_layers']
-        cmd = [sys.executable, '-m', 'napari.utils.stubgen']
-        subprocess.run(cmd + modules)
-        return super().run()
-
 
 setup(
     use_scm_version={"write_to": "napari/_version.py"},
     setup_requires=["setuptools_scm"],
-    cmdclass={"build_py": build_py},
 )
