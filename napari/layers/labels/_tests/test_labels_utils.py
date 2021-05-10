bfefb6056af4078d75147f3c08a17691498828e0
Draga Doncila Pop <17995243+DragaDoncila@users.noreply.github.com>
2021-05-10 12:35:29 +1000

    Fix setting label selection spinbox dtype (#2679)
    * Add get_dtype label utility
    
    * Add docstring, account for not having dtype
    
    * Fix accounting for no dtype
    
    * Add test for getting labels dtype
    
    * Add this PR to release notes

--- napari/layers/labels/_tests/test_labels_utils.py
+++ napari/layers/labels/_tests/test_labels_utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-from napari.layers.labels._labels_utils import interpolate_coordinates
+from napari.layers.labels import Labels
+from napari.layers.labels._labels_utils import (
+    get_dtype,
+    interpolate_coordinates,
+)
 
 
 def test_interpolate_coordinates():
@@ -25,3 +29,20 @@ def test_interpolate_coordinates():
         ]
     )
     assert np.all(coords == expected_coords)
+
+
+def test_get_dtype():
+    np.random.seed(0)
+    data = np.random.randint(20, size=(50, 50))
+    layer = Labels(data)
+
+    assert get_dtype(layer) == data.dtype
+
+    data2 = data[::2, ::2]
+    layer_data = [data, data2]
+    multiscale_layer = Labels(layer_data)
+    assert get_dtype(multiscale_layer) == layer_data[0].dtype
+
+    data = data.astype(int)
+    int_layer = Labels(data)
+    assert get_dtype(int_layer) == int
