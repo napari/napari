f37dad9dcf56d06977f106bd2036a16fb06adf37
Talley Lambert <talley.lambert@gmail.com>
2021-05-12 17:45:58 -0400

    add test

--- napari/_qt/widgets/_tests/test_qt_dock_widget.py
+++ napari/_qt/widgets/_tests/test_qt_dock_widget.py
@@ -1,4 +1,5 @@
 import pytest
+from PyQt5.QtWidgets import QTextEdit
 from qtpy.QtWidgets import (
     QDockWidget,
     QHBoxLayout,
@@ -107,3 +108,35 @@ def test_adding_modified_widget(make_napari_viewer):
     widg.layout = None
     dw = viewer.window.add_dock_widget(widg, name='test', area='right')
     assert dw.widget() is widg
+
+
+def test_adding_stretch(make_napari_viewer):
+    """Make sure that vertical stretch only gets added when appropriate."""
+    viewer = make_napari_viewer()
+
+    # adding a widget to the left/right will usually addStretch to the layout
+    widg = QWidget()
+    widg.setLayout(QVBoxLayout())
+    widg.layout().addWidget(QPushButton())
+    assert widg.layout().count() == 1
+    dw = viewer.window.add_dock_widget(widg, area='right')
+    assert widg.layout().count() == 2
+    dw.close()
+
+    # ... unless the widget has a widget with a large vertical sizePolicy
+    widg = QWidget()
+    widg.setLayout(QVBoxLayout())
+    widg.layout().addWidget(QTextEdit())
+    assert widg.layout().count() == 1
+    dw = viewer.window.add_dock_widget(widg, area='right')
+    assert widg.layout().count() == 1
+    dw.close()
+
+    # ... widgets on the bottom do not get stretch
+    widg = QWidget()
+    widg.setLayout(QHBoxLayout())
+    widg.layout().addWidget(QPushButton())
+    assert widg.layout().count() == 1
+    dw = viewer.window.add_dock_widget(widg, area='bottom')
+    assert widg.layout().count() == 1
+    dw.close()
