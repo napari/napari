08c4b2242b6001c5ba8eb100a5a7eb990a6aff4f
Pam Wadhwa <pepainter@gmail.com>
2021-05-13 10:56:55 -0500

    comment lines

--- napari/_qt/widgets/qt_sliders_with_labels.py
+++ napari/_qt/widgets/qt_sliders_with_labels.py
@@ -341,136 +341,136 @@ class QtLabeledSlider2(QWidget):
 
         self._refresh()
 
-    def _update_value(self, value):
-        """Update slider widget value.
+    # def _update_value(self, value):
+    #     """Update slider widget value.
 
-        Parameters
-        ----------
-        value : int
-            Widget value.
-        """
-        if value == "":
-            value = int(self._value)
+    #     Parameters
+    #     ----------
+    #     value : int
+    #         Widget value.
+    #     """
+    #     if value == "":
+    #         value = int(self._value)
 
-        value = int(value)
+    #     value = int(value)
 
-        if value > self._max_value:
-            value = self._max_value
-        elif value < self._min_value:
-            value = self._min_value
+    #     if value > self._max_value:
+    #         value = self._max_value
+    #     elif value < self._min_value:
+    #         value = self._min_value
 
-        if value != self._value:
-            self.valueChanged.emit(value)
+    #     if value != self._value:
+    #         self.valueChanged.emit(value)
 
-        self._value = value
-        self._refresh()
+    #     self._value = value
+    #     self._refresh()
 
-    def _refresh(self):
-        """Set every widget value to the new set value."""
-        self.blockSignals(True)
-        self._lineedit.setText(str(self._value))
-        self._slider.setValue(self._value)
-        self.blockSignals(False)
-        # self.valueChanged.emit(self._value)
+    # def _refresh(self):
+    #     """Set every widget value to the new set value."""
+    #     self.blockSignals(True)
+    #     self._lineedit.setText(str(self._value))
+    #     self._slider.setValue(self._value)
+    #     self.blockSignals(False)
+    #     # self.valueChanged.emit(self._value)
 
-    def setSingleStep(self, value):
-        """sets the single step of the slider"""
+    # def setSingleStep(self, value):
+    #     """sets the single step of the slider"""
 
-        self._slider.setSingleStep(value)
+    #     self._slider.setSingleStep(value)
 
-    def value(self):
-        """Return current value.
+    # def value(self):
+    #     """Return current value.
 
-        Returns
-        -------
-        int
-            Current value of highlight widget.
-        """
-        return self._value
+    #     Returns
+    #     -------
+    #     int
+    #         Current value of highlight widget.
+    #     """
+    #     return self._value
 
-    def setValue(self, value):
-        """Set new value and update widget.
+    # def setValue(self, value):
+    #     """Set new value and update widget.
 
-        Parameters
-        ----------
-        value : int
-            Highlight value.
-        """
-        self._update_value(value)
-        # self._refresh()
+    #     Parameters
+    #     ----------
+    #     value : int
+    #         Highlight value.
+    #     """
+    #     self._update_value(value)
+    #     # self._refresh()
 
-    def setMinimum(self, value):
-        """Set minimum widget value for slider.
+    # def setMinimum(self, value):
+    #     """Set minimum widget value for slider.
 
-        Parameters
-        ----------
-        value : int
-            Minimum widget value.
-        """
-        value = int(value)
-        if value < self._max_value:
-            self._min_value = value
-            self._slider_min_label.setText(str(value))
-            self._slider.setMinimum(value)
-            self._value = (
-                self._min_value
-                if self._value < self._min_value
-                else self._value
-            )
-            self._refresh()
-        else:
-            raise ValueError(
-                trans._(
-                    "Minimum value must be smaller than {max_value}",
-                    deferred=True,
-                    max_value=self._max_value,
-                )
-            )
+    #     Parameters
+    #     ----------
+    #     value : int
+    #         Minimum widget value.
+    #     """
+    #     value = int(value)
+    #     if value < self._max_value:
+    #         self._min_value = value
+    #         self._slider_min_label.setText(str(value))
+    #         self._slider.setMinimum(value)
+    #         self._value = (
+    #             self._min_value
+    #             if self._value < self._min_value
+    #             else self._value
+    #         )
+    #         self._refresh()
+    #     else:
+    #         raise ValueError(
+    #             trans._(
+    #                 "Minimum value must be smaller than {max_value}",
+    #                 deferred=True,
+    #                 max_value=self._max_value,
+    #             )
+    #         )
 
-    def minimum(self):
-        """Return minimum widget value.
+    # def minimum(self):
+    #     """Return minimum widget value.
 
-        Returns
-        -------
-        int
-            Minimum value of widget widget.
-        """
-        return self._min_value
+    #     Returns
+    #     -------
+    #     int
+    #         Minimum value of widget widget.
+    #     """
+    #     return self._min_value
 
-    def setMaximum(self, value):
-        """Set maximum widget value.
+    # def setMaximum(self, value):
+    #     """Set maximum widget value.
 
-        Parameters
-        ----------
-        value : int
-            Maximum widget value.
-        """
-        value = int(value)
-        if value > self._min_value:
-            self._max_value = value
-            self._slider_max_label.setText(str(value))
-            self._slider.setMaximum(value)
-            self._value = (
-                self._max_value
-                if self._value > self._max_value
-                else self._value
-            )
-            self._refresh()
-        else:
-            raise ValueError(
-                trans._(
-                    "Maximum value must be larger than {min_value}",
-                    deferred=True,
-                    min_value=self._min_value,
-                )
-            )
+    #     Parameters
+    #     ----------
+    #     value : int
+    #         Maximum widget value.
+    #     """
+    #     value = int(value)
+    #     if value > self._min_value:
+    #         self._max_value = value
+    #         self._slider_max_label.setText(str(value))
+    #         self._slider.setMaximum(value)
+    #         self._value = (
+    #             self._max_value
+    #             if self._value > self._max_value
+    #             else self._value
+    #         )
+    #         self._refresh()
+    #     else:
+    #         raise ValueError(
+    #             trans._(
+    #                 "Maximum value must be larger than {min_value}",
+    #                 deferred=True,
+    #                 min_value=self._min_value,
+    #             )
+    #         )
 
-    def maximum(self):
-        """Return maximum widget value.
+    # def maximum(self):
+    #     """Return maximum widget value.
 
-        Returns
-        -------
-        int
-            Maximum value of highlight widget.
-        """
-        return self._max_value
+    #     Returns
+    #     -------
+    #     int
+    #         Maximum value of highlight widget.
+    #     """
+    #     return self._max_value
