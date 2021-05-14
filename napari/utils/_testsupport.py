f2580e35f6c1a03c5aaade83e96c2bed11be60db
Talley Lambert <talley.lambert@gmail.com>
2021-05-14 08:15:04 -0400

    add strict as option

--- napari/utils/_testsupport.py
+++ napari/utils/_testsupport.py
@@ -1,8 +1,11 @@
 import sys
 import warnings
-from typing import List
+from typing import TYPE_CHECKING, List
 
 import pytest
+
+if TYPE_CHECKING:
+    from pytest import FixtureRequest
 
 
 def pytest_addoption(parser):
@@ -15,32 +18,7 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture
-def _strict_qtbot(qtbot):
-    """A modified qtbot fixture that makes sure no widgets have been leaked."""
-    from qtpy.QtWidgets import QApplication
-
-    initial = QApplication.topLevelWidgets()
-    prior_exception = getattr(sys, 'last_value', None)
-
-    yield qtbot
-
-    # if an exception was raised during the test, we should just quit now and
-    # skip looking for leaked widgets.
-    if getattr(sys, 'last_value', None) is not prior_exception:
-        return
-
-    QApplication.processEvents()
-    leaks = set(QApplication.topLevelWidgets()).difference(initial)
-    # still not sure how to clean up some of the remaining vispy
-    # vispy.app.backends._qt.CanvasBackendDesktop widgets...
-    if any([n.__class__.__name__ != 'CanvasBackendDesktop' for n in leaks]):
-        raise AssertionError(f'Widgets leaked!: {leaks}')
-    if leaks:
-        warnings.warn(f'Widgets leaked!: {leaks}')
-
-
-@pytest.fixture(scope="function")
-def make_napari_viewer(_strict_qtbot, request):
+def make_napari_viewer(qtbot, request: 'FixtureRequest'):
     """A fixture function that creates a napari viewer for use in testing.
 
     This uses a strict qtbot variant that asserts that no widgets are left over
@@ -57,20 +35,33 @@ def make_napari_viewer(_strict_qtbot, request):
     from napari.utils.settings import SETTINGS
 
     SETTINGS.reset()
-
     viewers: List[Viewer] = []
+    _strict = False
 
-    def actual_factory(*model_args, viewer_class=Viewer, **model_kwargs):
-        model_kwargs['show'] = model_kwargs.pop(
-            'show', request.config.getoption("--show-napari-viewer")
-        )
-        viewer = viewer_class(*model_args, **model_kwargs)
+    from qtpy.QtWidgets import QApplication
+
+    initial = QApplication.topLevelWidgets()
+    prior_exception = getattr(sys, 'last_value', None)
+    internal_test = request.module.__name__.startswith("napari.")
+
+    def actual_factory(
+        *model_args, ViewerClass=Viewer, strict=internal_test, **model_kwargs
+    ):
+        nonlocal _strict
+        _strict = strict
+
+        should_show = request.config.getoption("--show-napari-viewer")
+        model_kwargs['show'] = model_kwargs.pop('show', should_show)
+
+        viewer = ViewerClass(*model_args, **model_kwargs)
         viewers.append(viewer)
+
         return viewer
 
     yield actual_factory
 
-    # Some tests might have the viewer closed, so this call will not be able to access the window.
+    # Some tests might have the viewer closed, so this call will not be able
+    # to access the window.
     try:
         SETTINGS.reset()
     except AttributeError:
@@ -78,3 +69,15 @@ def make_napari_viewer(_strict_qtbot, request):
 
     for viewer in viewers:
         viewer.close()
+
+    # only check for leaked widgets if an exception was raised during the test,
+    # or "strict" mode was not used.
+    if _strict and getattr(sys, 'last_value', None) is prior_exception:
+        QApplication.processEvents()
+        leak = set(QApplication.topLevelWidgets()).difference(initial)
+        # still not sure how to clean up some of the remaining vispy
+        # vispy.app.backends._qt.CanvasBackendDesktop widgets...
+        if any([n.__class__.__name__ != 'CanvasBackendDesktop' for n in leak]):
+            # just a warning... but this can be converted to test errors
+            # in pytest with `-W error`
+            warnings.warn(f'Widgets leaked!: {leak}')
