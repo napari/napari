9d5a7700bb68dfe14d21c749d6eff2bce31bfee4
Draga Doncila Pop <ddon0001@student.monash.edu>
2021-05-17 17:13:13 +1000

    make large value smaller still

--- napari/layers/labels/_tests/test_labels.py
+++ napari/layers/labels/_tests/test_labels.py
@@ -871,7 +871,7 @@ def test_ndim_paint():
 
 
 def test_switching_display_func():
-    label_data = np.random.randint(2 ** 30, 2 ** 30 + 5, size=(50, 50))
+    label_data = np.random.randint(2 ** 25, 2 ** 25 + 5, size=(50, 50))
     layer = Labels(label_data)
     assert layer._color_lookup_func == layer._lookup_with_low_discrepancy_image
 
