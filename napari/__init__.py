ab719466da423f6593e69cd6f7b9d2bd485fe870
Talley Lambert <talley.lambert@gmail.com>
2021-05-23 09:42:40 -0400

    add comment

--- napari/__init__.py
+++ napari/__init__.py
@@ -3,6 +3,7 @@ try:
 except ImportError:
     __version__ = "not-installed"
 
+# Add everything that needs to be accessible from the napari namespace here.
 __all__ = [
     '__version__',
     'gui_qt',
@@ -24,6 +25,16 @@ __all__ = [
 
 def __dir__():
     return __all__
+
+
+# All imports in __init__ are hidden inside of `__getattr__` to prevent
+# importing the full chain of packages required when calling `import napari`.
+#
+# This has the biggest implications for running `napari` on the command line
+# (or running `python -m napari`) since `napari.__init__` gets imported
+# on the way to `napari.__main__`. Importing everything here has the
+# potential to take a second or more, so we definitely don't want to import it
+# just to access the CLI (which may not actually need any of the imports)
 
 
 def __getattr__(name):
