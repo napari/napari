07df2af8282265dbbdbb1fb0f8aeb3c218150f94
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2021-06-02 12:56:57 -0500

    Update settings descriptions (#2812)

--- napari/utils/settings/_defaults.py
+++ napari/utils/settings/_defaults.py
@@ -1,10 +1,8 @@
-"""Settings management.
-"""
+"""Settings management."""
+
 from __future__ import annotations
 
-import os
 from enum import Enum
-from pathlib import Path
 from typing import Any, Dict, List, Set, Tuple, Union
 
 from pydantic import BaseSettings, Field, ValidationError
@@ -161,14 +159,6 @@ class Language(str):
         return v
 
 
-class HighlightThickness(int):
-    """Highlight thickness to use when hovering over shapes/points."""
-
-    highlight_thickness = 2
-    minimum = 1
-    maximum = 10
-
-
 class QtBindingChoice(str, Enum):
     """Python Qt binding to use with the application."""
 
@@ -249,27 +239,29 @@ class AppearanceSettings(BaseNapariSettings):
     #    or if you want to *rename* options, then you need to do a MAJOR update in
     #    version, e.g. from 3.0.0 to 4.0.0
     # 3. You don't need to touch this value if you're just adding a new option
-
     schema_version: SchemaVersion = (0, 1, 0)
 
     theme: Theme = Field(
         "dark",
         title=trans._("Theme"),
-        description=trans._("Theme selection."),
+        description=trans._("Select the user interface theme."),
     )
 
-    highlight_thickness: HighlightThickness = Field(
-        HighlightThickness.highlight_thickness,
+    highlight_thickness: int = Field(
+        1,
+        title=trans._("Highlight thickness"),
         description=trans._(
-            "Customize the highlight weight indicating selected shapes and points."
+            "Select the highlight thickness when hovering over shapes/points."
         ),
+        ge=1,
+        le=10,
     )
 
     class Config:
         # Pydantic specific configuration
         schema_extra = {
             "title": trans._("Appearance"),
-            "description": trans._("Appearance settings."),
+            "description": trans._("User interface appearance settings."),
             "section": "appearance",
         }
 
@@ -285,59 +277,118 @@ class ApplicationSettings(BaseNapariSettings):
     #    or if you want to *rename* options, then you need to do a MAJOR update in
     #    version, e.g. from 3.0.0 to 4.0.0
     # 3. You don't need to touch this value if you're just adding a new option
-
     schema_version: SchemaVersion = (0, 1, 0)
-
-    first_time: bool = True
-
+    first_time: bool = Field(
+        True,
+        title=trans._('First time'),
+        description=trans._(
+            'Indicate if napari is running for the first time. This setting is managed by the application.'
+        ),
+    )
     ipy_interactive: bool = Field(
-        default=True,
+        True,
         title=trans._('IPython interactive'),
         description=trans._(
-            r'Use interactive %gui qt event loop when creating napari Viewers in IPython'
+            r'Toggle the use of interactive `%gui qt` event loop when creating napari Viewers in IPython.'
         ),
     )
 
     language: Language = Field(
         _DEFAULT_LOCALE,
         title=trans._("Language"),
-        description=trans._("Interface display language."),
+        description=trans._(
+            "Select the display language for the user interface."
+        ),
     )
 
     # Window state, geometry and position
     save_window_geometry: bool = Field(
         True,
-        title=trans._("Save Window Geometry"),
-        description=trans._("Save window size and position."),
+        title=trans._("Save window geometry"),
+        description=trans._(
+            "Toggle saving the main window size and position."
+        ),
     )
     save_window_state: bool = Field(
         True,
         title=trans._("Save Window State"),
         description=trans._("Save window state of dock widgets."),
     )
-    window_position: Tuple[int, int] = None
-    window_size: Tuple[int, int] = None
-    window_maximized: bool = None
-    window_fullscreen: bool = None
-    window_state: str = None
-    window_statusbar: bool = True
-    preferences_size: Tuple[int, int] = None
+    window_position: Tuple[int, int] = Field(
+        None,
+        title=trans._("Window position"),
+        description=trans._(
+            "Last saved x and y coordinates for the main window. This setting is managed by the application."
+        ),
+    )
+    window_size: Tuple[int, int] = Field(
+        None,
+        title=trans._("Window size"),
+        description=trans._(
+            "Last saved width and height for the main window. This setting is managed by the application."
+        ),
+    )
+    window_maximized: bool = Field(
+        None,
+        title=trans._("Window maximized state"),
+        description=trans._(
+            "Last saved maximized state for the main window. This setting is managed by the application."
+        ),
+    )
+    window_fullscreen: bool = Field(
+        None,
+        title=trans._("Window fullscreen"),
+        description=trans._(
+            "Last saved fullscreen state for the main window. This setting is managed by the application."
+        ),
+    )
+    window_state: str = Field(
+        None,
+        title=trans._("Window state"),
+        description=trans._(
+            "Last saved state of dockwidgets and toolbars for the main window. This setting is managed by the application."
+        ),
+    )
+    window_statusbar: bool = Field(
+        True,
+        title=trans._("Show status bar"),
+        description=trans._(
+            "Toggle diplaying the status bar for the main window."
+        ),
+    )
+    preferences_size: Tuple[int, int] = Field(
+        None,
+        title=trans._("Preferences size"),
+        description=trans._(
+            "Last saved width and height for the preferences dialog. This setting is managed by the application."
+        ),
+    )
     gui_notification_level: NotificationSeverity = Field(
         NotificationSeverity.INFO,
         title=trans._("GUI notification level"),
         description=trans._(
-            "Set the notification level for the notification manager."
+            "Select the notification level for the user interface."
         ),
     )
     console_notification_level: NotificationSeverity = Field(
         NotificationSeverity.NONE,
         title=trans._("Console notification level"),
+        description=trans._("Select the notification level for the console."),
+    )
+    open_history: List[str] = Field(
+        [],
+        title=trans._("Opened folders history"),
         description=trans._(
-            "Set the notification level for the console notifications."
+            "Last saved list of opened folders. This setting is managed by the application."
         ),
     )
-    open_history: List = [os.path.dirname(Path.home())]
-    save_history: List = [os.path.dirname(Path.home())]
+    save_history: List[str] = Field(
+        [],
+        title=trans._("Saved folders history"),
+        description=trans._(
+            "Last saved list of saved folders. This setting is managed by the application."
+        ),
+    )
 
     class Config:
         # Pydantic specific configuration
@@ -376,8 +427,12 @@ CallOrderDict = Dict[str, List[PluginHookOption]]
 
 
 class ShortcutsSettings(BaseNapariSettings):
-    """Shortcuts Settings."""
-
+    # 1. If you want to *change* the default value of a current option, you need to
+    #    do a MINOR update in config version, e.g. from 3.0.0 to 3.1.0
+    # 2. If you want to *remove* options that are no longer needed in the codebase,
+    #    or if you want to *rename* options, then you need to do a MAJOR update in
+    #    version, e.g. from 3.0.0 to 4.0.0
+    # 3. You don't need to touch this value if you're just adding a new option
     schema_version: SchemaVersion = (0, 1, 1)
     shortcuts: Dict[str, List[str]] = Field(
         default_shortcuts,
@@ -401,6 +456,12 @@ class ShortcutsSettings(BaseNapariSettings):
 
 
 class PluginsSettings(BaseNapariSettings):
+    # 1. If you want to *change* the default value of a current option, you need to
+    #    do a MINOR update in config version, e.g. from 3.0.0 to 3.1.0
+    # 2. If you want to *remove* options that are no longer needed in the codebase,
+    #    or if you want to *rename* options, then you need to do a MAJOR update in
+    #    version, e.g. from 3.0.0 to 4.0.0
+    # 3. You don't need to touch this value if you're just adding a new option
     schema_version: SchemaVersion = (0, 1, 1)
     call_order: CallOrderDict = Field(
         None,
