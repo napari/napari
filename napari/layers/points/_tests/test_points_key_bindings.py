23af2c9a2305e9e08709e1ecc8d4a920f8733d6e
Pam Wadhwa <pepainter@gmail.com>
2021-06-14 22:44:16 -0500

    fix test

--- napari/layers/points/_tests/test_points_key_bindings.py
+++ napari/layers/points/_tests/test_points_key_bindings.py
@@ -11,11 +11,11 @@ def test_select_all():
     assert len(layer.data) == 4
     assert len(layer.selected_data) == 0
 
-    layer.mode = 'select'
-    layer.class_keymap['Space'](layer)
-    assert len(layer.selected_data) == 4
+    # bind shortcuts from SETTINGS
+    for action, shortcuts in SETTINGS.shortcuts.shortcuts.items():
+        for shortcut in shortcuts:
+            action_manager.bind_shortcut(action, shortcut)
 
-    # look for 'A' in Points default shortcuts
-    actions = action_manager._get_layer_actions(Points).keys()
-    shortcuts = [SETTINGS.shortcuts.shortcuts[name] for name in actions]
-    assert ['A'] in shortcuts
+    layer.mode = 'select'
+    layer.class_keymap['A'](layer)
+    assert len(layer.selected_data) == 4
