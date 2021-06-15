d4cac38b38d0eb53ec5a3b04d01bcfc9d126088b
Pam Wadhwa <pepainter@gmail.com>
2021-06-14 22:58:02 -0500

    improve test

--- napari/layers/points/_tests/test_points_key_bindings.py
+++ napari/layers/points/_tests/test_points_key_bindings.py
@@ -1,20 +1,17 @@
 from napari.layers import Points
-from napari.utils.action_manager import action_manager
-from napari.utils.settings import SETTINGS
 
 
-def test_select_all():
+def test_select_all(make_napari_viewer):
     """Test select all key binding."""
+
+    # make viewer to bind shortcuts
+    viewer = make_napari_viewer()  # noqa: F841
+
     data = [[1, 3], [8, 4], [10, 10], [15, 4]]
     layer = Points(data, size=1)
 
     assert len(layer.data) == 4
     assert len(layer.selected_data) == 0
-
-    # bind shortcuts from SETTINGS
-    for action, shortcuts in SETTINGS.shortcuts.shortcuts.items():
-        for shortcut in shortcuts:
-            action_manager.bind_shortcut(action, shortcut)
 
     layer.mode = 'select'
     layer.class_keymap['A'](layer)
