08bc6166c42b10ac0bd71e7c12598ccae61d8124
Nicholas Sofroniew <sofroniewn@gmail.com>
2021-06-22 19:11:25 -0700

    Make maximum brush size flexible (#2901)
    * make brush size dynamic
    
    * Update napari/_qt/layer_controls/qt_labels_controls.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- napari/_qt/layer_controls/qt_labels_controls.py
+++ napari/_qt/layer_controls/qt_labels_controls.py
@@ -431,7 +431,9 @@ class QtLabelsControls(QtLayerControls):
         """
         with self.layer.events.brush_size.blocker():
             value = self.layer.brush_size
-            value = np.clip(int(value), 1, 40)
+            value = np.maximum(1, int(value))
+            if value > self.brushSizeSlider.maximum():
+                self.brushSizeSlider.setMaximum(int(value))
             self.brushSizeSlider.setValue(value)
 
     def _on_n_edit_dimensions_change(self, event=None):
