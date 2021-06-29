1f7d06bdabdbb906c1bc982315925164e55fff7b
Alister Burt <alisterburt@gmail.com>
2021-06-29 17:46:58 +0100

    add selectable evented list of planes

--- napari/components/experimental/_tests/test_plane.py
+++ napari/components/experimental/_tests/test_plane.py
@@ -1,9 +1,14 @@
-from .._plane import Plane, Slice
+from .._plane import Plane, PlaneList, Slice
 
 
 def test_plane_instantiation():
     plane = Plane(position=(64, 64, 64), normal=(1, 1, 1))
     assert isinstance(plane, Plane)
+
+
+def test_planelist_instantiation(plane):
+    planes = PlaneList([plane for _ in range(5)])
+    assert isinstance(planes, PlaneList)
 
 
 def test_slice_instantiation(plane):
