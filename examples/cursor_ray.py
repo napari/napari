4eec187d6bd54cd46794d90d6a7c24bf471ef684
Alister Burt <alisterburt@gmail.com>
2021-07-14 13:32:30 +0100

    add cursor_ray example

---
+++ examples/cursor_ray.py
@@ -0,0 +1,57 @@
+"""
+Depict a ray through a layer in 3D to demonstrate interactive 3D functionality
+"""
+import numpy as np
+
+import napari
+
+sidelength_data = 64
+n_points = 10
+
+# data to depict an empty volume, its bounding box and points along a ray
+# through the volume
+volume = np.zeros(shape=(sidelength_data, sidelength_data, sidelength_data))
+bounding_box = np.array(
+    [
+        [0, 0, 0],
+        [1, 0, 0],
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 0, 1],
+        [1, 0, 1],
+        [0, 1, 1],
+        [1, 1, 1],
+    ]
+) * sidelength_data
+points = np.zeros(shape=(n_points, 3))
+
+# point sizes
+point_sizes = np.linspace(0.5, 2, n_points, endpoint=True)
+
+# point colors
+green = [0, 1, 0, 1]
+red = [1, 0, 0, 1]
+point_colors = np.linspace(green, red, n_points, endpoint=True)
+
+# create viewer and add layers for each piece of data
+viewer = napari.Viewer(ndisplay=3)
+bounding_box_layer = viewer.add_points(
+    bounding_box, face_color='cornflowerblue', name='bounding box'
+)
+ray_layer = viewer.add_points(
+    points, face_color=point_colors, size=point_sizes, name='cursor ray'
+)
+volume_layer = viewer.add_image(volume, blending='additive')
+
+
+# callback function, called on mouse click when volume layer is active
+@volume_layer.mouse_drag_callbacks.append
+def on_click(layer, event):
+    world_to_canvas = viewer.window.qt_viewer.view.scene.transform
+    near_point, far_point = layer._cursor_ray(event, world_to_canvas)
+    ray_points = np.linspace(near_point, far_point, n_points, endpoint=True)
+    if ray_points.shape[1] != 0:
+        ray_layer.data = ray_points
+
+
+napari.run()
