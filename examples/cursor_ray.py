cbf57d683dbfadf220d4f534ef25a87a060e9ba7
alisterburt <alisterburt@gmail.com>
2021-07-14 14:20:46 +0100

    Update examples/cursor_ray.py

--- examples/cursor_ray.py
+++ examples/cursor_ray.py
@@ -47,7 +47,7 @@ volume_layer = viewer.add_image(volume, blending='additive')
 # callback function, called on mouse click when volume layer is active
 @volume_layer.mouse_drag_callbacks.append
 def on_click(layer, event):
-    near_point, far_point = layer._cursor_ray(event, world_to_canvas)
+    near_point, far_point = layer._cursor_ray(event)
     ray_points = np.linspace(near_point, far_point, n_points, endpoint=True)
     if ray_points.shape[1] != 0:
         ray_layer.data = ray_points
