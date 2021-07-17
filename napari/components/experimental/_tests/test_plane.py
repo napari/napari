981d4cb06758b8a02bd9c6c5d484a003ddea1671
Alister Burt <alisterburt@gmail.com>
2021-07-18 00:02:19 +0100

    move plane to layer utils

--- napari/components/experimental/_tests/test_plane.py
+++
@@ -1,8 +0,0 @@
-from .._plane import Plane3D
-
-
-def test_Plane3D_instantiation(plane):
-    slice = Plane3D(
-        position=(32, 32, 32), normal_vector=(1, 0, 0), thickness=10
-    )
-    assert isinstance(slice, Plane3D)
