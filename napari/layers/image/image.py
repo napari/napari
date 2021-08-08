c02ff26ddfa339b34c74d6c2d75e3b20b6ba3df3
Alister Burt <alisterburt@gmail.com>
2021-08-08 17:18:56 +0100

    fix plane attribute in Image.get_base_state

--- napari/layers/image/image.py
+++ napari/layers/image/image.py
@@ -829,7 +829,7 @@ class Image(_ImageBase):
                 'contrast_limits': self.contrast_limits,
                 'interpolation': self.interpolation,
                 'rendering': self.rendering,
-                'render_as_plane': self.render_as_plane,
+                'plane': self.plane,
                 'iso_threshold': self.iso_threshold,
                 'attenuation': self.attenuation,
                 'gamma': self.gamma,
