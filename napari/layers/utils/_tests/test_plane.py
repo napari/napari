b36575d6eb6e78cd1d0c465d192993b8ce6fadc9
Alister Burt <alisterburt@gmail.com>
2021-08-08 15:04:13 +0100

    fix tests and add test for from_points method

--- napari/layers/utils/_tests/test_plane.py
+++ napari/layers/utils/_tests/test_plane.py
@@ -1,20 +1,27 @@
 import numpy as np
 
-from napari.layers.utils.plane import Plane3D
+from napari.layers.utils.plane import PlaneManager
 
 
-def test_Plane3D_instantiation():
-    plane = Plane3D(
+def test_plane_manager_instantiation():
+    plane = PlaneManager(
         position=(32, 32, 32), normal_vector=(1, 0, 0), thickness=10
     )
-    assert isinstance(plane, Plane3D)
+    assert isinstance(plane, PlaneManager)
 
 
-def test_Plane3D_vector_normalisation():
-    plane = Plane3D(position=(0, 0, 0), normal_vector=(5, 0, 0), thickness=10)
+def test_plane_manager_vector_normalisation():
+    plane = PlaneManager(position=(0, 0, 0), normal_vector=(5, 0, 0))
     assert np.allclose(plane.normal_vector, (1, 0, 0))
 
 
-def test_Plane3D_vector_setter():
-    plane = Plane3D(position=(0, 0, 0), normal_vector=(1, 0, 0), thickness=10)
+def test_plane_manager_vector_setter():
+    plane = PlaneManager(position=(0, 0, 0), normal_vector=(1, 0, 0))
     plane.normal_vector = (1, 0, 0)
+
+
+def test_plane_manager_from_points():
+    points = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]])
+    plane = PlaneManager.from_points(points)
+    assert isinstance(plane, PlaneManager)
+    assert plane.normal_vector == (0, 0, 1)
