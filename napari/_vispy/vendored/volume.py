4e3bfb4de051d0a645b8eeb59db2af7d051ae31a
Lorenzo Gaifas <brisvag@gmail.com>
2021-08-10 15:19:41 +0200

    get usptream changes

--- napari/_vispy/vendored/volume.py
+++ napari/_vispy/vendored/volume.py
@@ -967,11 +967,9 @@ class VolumeVisual(Visual):
         return frag_dict[self.method]['after_loop']
 
     @staticmethod
-    @lru_cache(maxsize=None)
+    @lru_cache(maxsize=10)
     def _build_clipping_planes_func(n_planes):
-        """
-        build the code snippet used to clip the volume based on self.clipping_planes
-        """
+        """Build the code snippet used to clip the volume based on self.clipping_planes."""
         func = Function(
             '$vars\nfloat clip_planes(vec3 loc, vec3 vol_shape) { float is_shown = 1.0; $clips; return is_shown; }')
         # each plane is defined by a position and a normal vector
@@ -996,9 +994,9 @@ class VolumeVisual(Visual):
 
     @property
     def clipping_planes(self):
-        """
-        a set of planes used to clip the volume. Each plane is defined by a position and
-        a normal vector (magnitude is irrelevant). Shape: (n_planes, 2, 3)
+        """Get the set of planes used to clip the volume.
+
+        Each plane is defined by a position and a normal vector (magnitude is irrelevant). Shape: (n_planes, 2, 3)
         """
         return self._clipping_planes[:, :, ::-1]
 
