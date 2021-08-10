bab230def8253ad9ca71ff5c361b2211cadebb9b
Alister Burt <alisterburt@gmail.com>
2021-08-10 16:04:59 +0100

    float -> int

--- napari/_vispy/vendored/volume.py
+++ napari/_vispy/vendored/volume.py
@@ -366,7 +366,7 @@ void main() {
     int iter = 0;
     
     // Keep track of whether texture has been sampled
-    float texture_sampled = 0;
+    int texture_sampled = 0;
     
     while (iter < nsteps) {
         for (iter=iter; iter<nsteps; iter++)
@@ -378,7 +378,7 @@ void main() {
                 // Get sample color
                 vec4 color = $sample(u_volumetex, loc);
                 float val = color.r;
-                texture_sampled += 1;
+                texture_sampled = 1;
 
                 $in_loop
             }
@@ -388,7 +388,7 @@ void main() {
     }
     
     // discard fragment if texture not sampled
-    if ( texture_sampled < 1 ) {
+    if ( texture_sampled != 1 ) {
         discard;
     }
     
