987a87fbf1cb87b889198e60d562cb44fc4c92c4
alisterburt <alisterburt@gmail.com>
2021-08-14 05:52:59 +0100

    Make current viewer accessible from the napari module (#3149)
    * trans undefined error fix
    
    * napari.current_viewer
    
    * add example
    
    * add protection
    
    * remove protection, we're all adults here
    
    * add doc
    
    * add type stub for current_viewer
    
    * add test
    
    * make current viewer a function (suggestion from @tlambert03)
    
    * add docstring
    
    * store reference to current viewer on _QtMainWindow
    
    * Update napari/_qt/qt_main_window.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    * Update napari/viewer.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    * Update napari/viewer.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    * syntax
    
    * update type stub
    
    * add missing newline
    
    * no more type alias
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- napari/__init__.pyi
+++ napari/__init__.pyi
@@ -8,6 +8,7 @@ __version__: str
 
 notification_manager: napari.utils.notifications.NotificationManager
 Viewer = napari.viewer.Viewer
+current_viewer: Callable[[], Viewer]
 
 gui_qt = napari._qt.qt_event_loop.gui_qt
 run = napari._qt.qt_event_loop.run
