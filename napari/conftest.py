c6868cf07bdfb35c312f8523744166c65da89671
Talley Lambert <talley.lambert@gmail.com>
2021-08-15 08:12:12 -0400

    restore conftest

--- napari/conftest.py
+++ napari/conftest.py
@@ -346,12 +346,3 @@ def fresh_settings(monkeypatch):
     # this makes sure that we start with fresh settings for every test.
     settings._SETTINGS = None
     yield
-
-
-def pytest_unconfigure(config):
-    try:
-        from qtpy.QtWidgets import QApplication
-
-        print("TOP LEVEL WINDOWS:", QApplication.topLevelWindows())
-    except ImportError:
-        pass
