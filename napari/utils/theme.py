4d6f440764e5ffcc141fccc2097376130b18935c
Talley Lambert <talley.lambert@gmail.com>
2021-08-30 08:36:34 -0400

    revert part

--- napari/utils/theme.py
+++ napari/utils/theme.py
@@ -5,6 +5,7 @@ import warnings
 from ast import literal_eval
 from typing import Union
 
+from npe2 import plugin_manager as pm2
 from pydantic import validator
 from pydantic.color import Color
 
@@ -15,8 +16,6 @@ try:
     use_gradients = (int(major) >= 5) and (int(minor) >= 12)
 except Exception:
     use_gradients = False
-
-from npe2 import plugin_manager as pm2
 
 from ..utils.translations import trans
 from .events import EventedModel
@@ -273,34 +272,38 @@ def rebuild_theme_settings(event=None):
 _themes = EventedDict(
     {
         'dark': Theme(
-            name='dark',
-            background='rgb(38, 41, 48)',
-            foreground='rgb(65, 72, 81)',
-            primary='rgb(90, 98, 108)',
-            secondary='rgb(134, 142, 147)',
-            highlight='rgb(106, 115, 128)',
-            text='rgb(240, 241, 242)',
-            icon='rgb(209, 210, 212)',
-            warning='rgb(153, 18, 31)',
-            current='rgb(0, 122, 204)',
-            syntax_style='native',
-            console='rgb(0, 0, 0)',
-            canvas='black',
+            **{
+                'name': 'dark',
+                'background': 'rgb(38, 41, 48)',
+                'foreground': 'rgb(65, 72, 81)',
+                'primary': 'rgb(90, 98, 108)',
+                'secondary': 'rgb(134, 142, 147)',
+                'highlight': 'rgb(106, 115, 128)',
+                'text': 'rgb(240, 241, 242)',
+                'icon': 'rgb(209, 210, 212)',
+                'warning': 'rgb(153, 18, 31)',
+                'current': 'rgb(0, 122, 204)',
+                'syntax_style': 'native',
+                'console': 'rgb(0, 0, 0)',
+                'canvas': 'black',
+            }
         ),
         'light': Theme(
-            name='light',
-            background='rgb(239, 235, 233)',
-            foreground='rgb(214, 208, 206)',
-            primary='rgb(188, 184, 181)',
-            secondary='rgb(150, 146, 144)',
-            highlight='rgb(163, 158, 156)',
-            text='rgb(59, 58, 57)',
-            icon='rgb(107, 105, 103)',
-            warning='rgb(255, 18, 31)',
-            current='rgb(253, 240, 148)',
-            syntax_style='default',
-            console='rgb(255, 255, 255)',
-            canvas='white',
+            **{
+                'name': 'light',
+                'background': 'rgb(239, 235, 233)',
+                'foreground': 'rgb(214, 208, 206)',
+                'primary': 'rgb(188, 184, 181)',
+                'secondary': 'rgb(150, 146, 144)',
+                'highlight': 'rgb(163, 158, 156)',
+                'text': 'rgb(59, 58, 57)',
+                'icon': 'rgb(107, 105, 103)',
+                'warning': 'rgb(255, 18, 31)',
+                'current': 'rgb(253, 240, 148)',
+                'syntax_style': 'default',
+                'console': 'rgb(255, 255, 255)',
+                'canvas': 'white',
+            }
         ),
     },
     basetype=Theme,
