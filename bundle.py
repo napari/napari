43eac5672ae05f58d597608d438af1a55b04b05b
Jaime RGP <jaimergp@users.noreply.github.com>
2021-08-30 14:54:36 +0200

    export ARCH on Linux

--- bundle.py
+++ bundle.py
@@ -203,6 +203,10 @@ def patch_python_lib_location():
         print("symlinking", orig, "to", dest)
 
 
+def patch_environment_variables():
+    os.environ["ARCH"] = os.uname().machine
+
+
 def make_zip():
     import glob
     import zipfile
@@ -232,6 +236,9 @@ def bundle():
     if MACOS:
         patch_dmgbuild()
 
+    if LINUX:
+        patch_environment_variables()
+
     # smoke test, and build resources
     subprocess.check_call([sys.executable, '-m', APP, '--info'])
 
@@ -250,7 +257,6 @@ def bundle():
         elif MACOS:
             patch_python_lib_location()
 
-        sys.exit(0)
         # build
         cmd = ['briefcase', 'build'] + (['--no-docker'] if LINUX else [])
         subprocess.check_call(cmd)
