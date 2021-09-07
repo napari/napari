79f9f6e46d9551b7d4403a251ec8a4dcd8dd8a33
Talley Lambert <talley.lambert@gmail.com>
2021-09-07 19:40:26 -0400

    remove test menu and pref dialog

--- napari/_qt/menus/__init__.py
+++ napari/_qt/menus/__init__.py
@@ -2,7 +2,6 @@ from .debug_menu import DebugMenu
 from .file_menu import FileMenu
 from .help_menu import HelpMenu
 from .plugins_menu import PluginsMenu
-from .test_menu import TestMenu
 from .view_menu import ViewMenu
 from .window_menu import WindowMenu
 
@@ -12,6 +11,5 @@ __all__ = [
     'HelpMenu',
     'PluginsMenu',
     'ViewMenu',
-    'TestMenu',
     'WindowMenu',
 ]
