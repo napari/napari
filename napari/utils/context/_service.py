19259f2aad68ac009b16b56a314deedd23192bee
Talley Lambert <talley.lambert@gmail.com>
2021-09-08 12:16:20 -0400

    add Context object, more tests

--- napari/utils/context/_service.py
+++ napari/utils/context/_service.py
@@ -5,76 +5,119 @@ https://github.com/microsoft/vscode/blob/main/src/vs/platform/contextkey/browser
 """
 from __future__ import annotations
 
+import contextlib
 import pprint
 from abc import ABC, abstractmethod
 from contextlib import contextmanager
 from itertools import count
-from typing import Any, Dict, Iterable, Mapping, Optional, Union
+from typing import Any, Dict, Optional
 
 from napari.utils.events import EventEmitter
 
-Context = Dict[str, Any]
-PARENT_KEY = '__parent__'
+KEYBINDING_CONTEXT_ATTR = '_keybinding_context'
+
+
+class Context(dict):
+    """Just a dict that has a parent.
+
+    If `__getitem__` or `get` can't find a key, they check the parent.
+    use `collect` to get the merged dict (self takes precedence)
+    """
+
+    _parent: Optional[Context] = None
+    _id: int
+
+    def __missing__(self, name: str) -> Any:
+        return (self._parent or {})[name]
+
+    def get(self, key: str, default: Any = None) -> Any:
+        if key in self:
+            return self[key]
+        return (self._parent or {}).get(key, default)
+
+    def collect(self) -> dict:
+        """Collect all values including those provided by parent(s)"""
+        p = self._parent.collect() if self._parent is not None else {}
+        return {**p, **self}
+
+    def _spawn(self, *a, **k) -> Context:
+        ctx = Context(*a, **k)
+        ctx._parent = self
+        return ctx
+
+    def __repr__(self) -> str:
+        myvals = dict(self)
+        parvals = {k: v for k, v in self.collect().items() if k not in self}
+        parstring = f' # <{parvals}>' if parvals else ''
+        return f'{type(self).__name__}({myvals}){parstring}'
 
 
 class _AbsContextKeyService(ABC):
     _parent: Optional[_AbsContextKeyService] = None
 
+    # this is how things subscribe to changes
+    context_changed: EventEmitter
+
     def __init__(self, id: int) -> None:
         self._context_id = id
         self.context_changed = EventEmitter(self, 'context_changed')
 
-    @property
-    def context_id(self) -> int:
-        return self._context_id
-
     @contextmanager
     def buffered_change_events(self):
+        # used when you want to make a number of changes
+        # but only emit a single change event at the end
         ...  # TODO
 
-    def create_scoped(self, target=None):
+    def create_scoped(self, target: object = None):
         return ScopedContextKeyService(self, target)
 
-    # vscode: "getContextKeyValue"
+    # vscode: "getContextKeyValue" (used frequently outside the module)
     def __getitem__(self, key: str) -> Any:
-        if key in self._my_context:
-            return self._my_context[key]
-        if PARENT_KEY in self._my_context:
-            return self._my_context[PARENT_KEY][key]
+        return self._my_context[key]
 
-    # vscode: "setContext"
+    # vscode: "setContext" (only used in this module)
     def __setitem__(self, key: str, value: Any) -> None:
         if self._my_context.get(key, '__missing__') != value:
             self._my_context[key] = value
             self.context_changed(value=[key])
 
-    def update(
-        self, _m: Union[Mapping, Iterable, None] = None, **kwargs
-    ) -> None:
-        d = dict(_m) if _m is not None else {}
-        d.update(kwargs)
-        for k, v in d.items():
-            self[k] = v
-
-    # vscode: "removeContext"
+    # vscode: "removeContext" (only used in ContextKey.reset())
     def __delitem__(self, key: str) -> None:
         if key in self._my_context:
             del self._my_context[key]
             self.context_changed(value=[key])
 
-    @property
-    def _my_context(self):
-        return self.get_context(self._context_id)
+    # vscode: createKey
+    # one thing we don't have here is createKey, since we don't have
+    # ContextKey objects.
+    # outside of standard get/set methods the main thing this allows
+    # is `reset()`... but this can be accomplished with a RawContextKey
+    # and the descriptor protocol
 
-    # vscode: "getContextValuesContainer"
+    # vscode: "getContext"
+    # used all over the place to retrieve the context for a given object
+    # the context_attr is set on the object when creating a ScopedContext...
+    def get_context(self, target: object) -> Context:
+        """Return the context for an object (set with create_scoped)."""
+        id = getattr(target, KEYBINDING_CONTEXT_ATTR, 0)
+        return self._get_context_container(id)
+
+    @property
+    def _my_context(self) -> Context:
+        """Return context for this service."""
+        return self._get_context_container(self._context_id)
+
+    # vscode: "getContextValuesContainer" (only used in this module)
     @abstractmethod
-    def get_context(self, context_id: int) -> Context:
+    def _get_context_container(self, context_id: int) -> Context:
         """Get Context dict for `context_id`"""
 
+    # vscode: "disposeContext" (only used in this module)
     @abstractmethod
     def del_context(self, context_id: int) -> None:
-        """Get Context dict for `context_id`"""
+        """delete Context dict for `context_id`"""
 
+    # vscode: "createChildContext" (only used in this module when scoping)
     @abstractmethod
     def create_child_context(self, parent_ctx_id: Optional[int] = None) -> int:
         """Create a new child context for `parent_ctx_id`.
@@ -82,58 +125,60 @@ class _AbsContextKeyService(ABC):
         If not provided parent_ctx_id should be self._context_id
         """
 
-    def dict(self):
-        result = self._parent.dict() if self._parent else {}
-        result.update(self._my_context)
-        result.pop(PARENT_KEY, None)
-        return result
-
+    # not present in vscode ... just for dict-like interface
     def __contains__(self, key):
         return key in self._my_context
 
     def __iter__(self):
-        yield from self.dict().items()
+        yield from self._my_context.collect().items()
 
     def __len__(self):
-        return len(self.dict())
+        return len(self._my_context.collect())
 
-    def clear(self):
-        self._my_context.clear()
+    # def update(
+    #     self, _m: Union[Mapping, Iterable, None] = None, **kwargs
+    # ) -> None:
+    #     # TODO: this should be protected behind a change buffer
+    #     d = dict(_m) if _m is not None else {}
+    #     d.update(kwargs)
+    #     for k, v in d.items():
+    #         self[k] = v
 
 
 class ContextKeyService(_AbsContextKeyService):
+    """A root context."""
+
     _contexts: Dict[int, Context]
     __instance: Optional[ContextKeyService] = None
 
     def __init__(self) -> None:
         super().__init__(0)
         self._ctx_count: count[int] = count(1)
-        self._contexts = {self._context_id: {}}
+        self._contexts = {self._context_id: Context()}
 
-    def get_context(self, context_id: int) -> Context:
-        return self._contexts.get(context_id, {})  # Null context
+    def _get_context_container(self, context_id: int) -> Context:
+        return self._contexts.get(context_id, Context())  # Null context
 
     def del_context(self, context_id: int) -> None:
-        del self._contexts[context_id]
+        self._contexts.pop(context_id, None)
 
     def create_child_context(self, parent_ctx_id: Optional[int] = None) -> int:
-        parent_id = parent_ctx_id or self._context_id
-        ctx = {}
-        if parent_id in self._contexts:
-            ctx[PARENT_KEY] = self._contexts[parent_id]
-
         _id = next(self._ctx_count)
-        self._contexts[_id] = ctx
+        pid = parent_ctx_id or self._context_id
+        if pid in self._contexts:
+            self._contexts[_id] = self._contexts[pid]._spawn()
+        else:
+            self._contexts[_id] = Context()
         return _id
 
     def __repr__(self):
         s = ''
         for item in self._contexts.values():
             if item is self._my_context:
-                s += pprint.pformat(self.dict())
+                s += pprint.pformat(dict(self))
             else:
                 s += f', {id(item)}'
-        return f'ContextKeyService({s})'
+        return f'{type(self).__name__}({s})'
 
     @classmethod
     def instance(cls) -> ContextKeyService:
@@ -143,19 +188,41 @@ class ContextKeyService(_AbsContextKeyService):
 
 
 class ScopedContextKeyService(_AbsContextKeyService):
+    """A child context of ContextKeyService, scoped on some object."""
+
     _parent: _AbsContextKeyService
 
     # TODO: figure out whether we need the scope object
-    def __init__(self, parent: _AbsContextKeyService, scope=None) -> None:
+    def __init__(self, parent: _AbsContextKeyService, obj: object) -> None:
+        import weakref
+
+        if hasattr(obj, KEYBINDING_CONTEXT_ATTR):
+            c = getattr(obj, KEYBINDING_CONTEXT_ATTR)
+            raise RuntimeError(f"{obj} is already scoped on context {c}")
         super().__init__(parent.create_child_context())
         self._parent = parent
-        self._updateParentChangeListener()
-        self._scope = scope
-        # TODO: set KEYBINDING_CONTEXT_ATTR on scope
+        self._parent.context_changed.connect(self._reemit_event)
+
+        # when the object gets deleted, delete this scoped_context from root
+        weakref.finalize(obj, self._parent.del_context, self._context_id)
+
+        # save weakref to object, so we can cleanup the context_attr if this
+        # scoped context gets deleted.
+        self._obj_ref = weakref.ref(obj)
+        with contextlib.suppress(AttributeError):
+            setattr(obj, KEYBINDING_CONTEXT_ATTR, self._context_id)
+
+    def __del__(self):
+        """Delete this scoped service, cleanup parent, remove attr"""
+        if self._parent is not None:
+            self._parent.context_changed.disconnect(self._reemit_event)
+            self._parent.del_context(self._context_id)
+        with contextlib.suppress(AttributeError):
+            delattr(self._obj_ref(), KEYBINDING_CONTEXT_ATTR)
 
     # aka: getContextValuesContainer
-    def get_context(self, context_id: int) -> Context:
-        return self._parent.get_context(context_id)
+    def _get_context_container(self, context_id: int) -> Context:
+        return self._parent._get_context_container(context_id)
 
     def del_context(self, context_id: int) -> None:
         self._parent.del_context(context_id)
@@ -163,25 +230,9 @@ class ScopedContextKeyService(_AbsContextKeyService):
     def create_child_context(self, parent_ctx_id: Optional[int] = None) -> int:
         return self._parent.create_child_context(parent_ctx_id)
 
-    def update_parent(self, new_parent: _AbsContextKeyService) -> None:
-        old_vals = self.dict()
-        self._parent.context_changed.disconnect(self._reemit)
-        self._parent = new_parent
-        self._updateParentChangeListener()
-        new_ctx = self._parent.get_context(self._parent.context_id)
-        self[PARENT_KEY] = new_ctx
-        new_vals = self.dict()
-        changed_keys = [k for k, v in new_vals.items() if old_vals[k] != v]
-        self.context_changed(value=changed_keys)
-
-    def _updateParentChangeListener(self):
-        self._parent.context_changed.connect(self._reemit)
-
-    def _reemit(self, event):
+    def _reemit_event(self, event):
         self.context_changed(value=event.value)
 
     def __repr__(self):
         _id = hex(id(self))
-        return (
-            f'ScopedContextKeyService(id={_id}, {pprint.pformat(self.dict())})'
-        )
+        return f'{type(self).__name__}(id={_id}, {pprint.pformat(dict(self))})'
--- napari/utils/context/_tests/test_context_key_service.py
+++ napari/utils/context/_tests/test_context_key_service.py
@@ -1,11 +1,53 @@
-from napari.utils.context._service import ContextKeyService
+from unittest.mock import Mock
+
+import pytest
+
+from napari.utils.context._service import Context, ContextKeyService
+
+
+def test_context():
+    """test Context object is a dict that inherits values from parent."""
+    A_DICT = {'a': 0, 'b': 0, 'c': 0}
+    B_DICT = {'a': 1, 'd': 1}
+    C_DICT = {'a': 2, 'c': 2, 'e': 2}
+    a = Context(A_DICT)
+    b = a._spawn(B_DICT)
+    c = b._spawn(C_DICT)
+
+    assert isinstance(a, dict)
+
+    # dict() returns the context-local dict
+    assert dict(a) == A_DICT
+    assert dict(b) == B_DICT
+    assert dict(c) == C_DICT
+
+    # .collect() also includes parents
+    assert a.collect() == A_DICT
+    assert b.collect() == {**A_DICT, **B_DICT}
+    assert c.collect() == {**A_DICT, **B_DICT, **C_DICT}
+
+    # get pulls from parents when missing
+    assert a['b'] == b['b'] == c['b'] == 0
+    assert a.get('b') == b.get('b') == c.get('b') == 0
+    assert a.get('f') is b.get('f') is c.get('f') is None
+    assert a.get('f', 4) == b.get('f', 4) == c.get('f', 4) == 4
+
+    # raises key error when missing
+    with pytest.raises(KeyError):
+        c['f']
+
+    # repr shows parent keys in a comment when applicable
+    assert '#' not in repr(a)
+    assert "# <{'b': 0, 'c': 0}>" in repr(b)
+    assert "# <{'b': 0, 'd': 1}>" in repr(c)
 
 
 def test_root_service():
+    """Root service, behaves a lot like a dict (but really "manages" it.)"""
     root = ContextKeyService()
-    assert root.context_id == 0
+    assert root._context_id == 0
 
-    assert root.dict() == dict(root) == {}
+    assert dict(root) == {}
     _id = id(root._my_context)
 
     # set
@@ -14,32 +56,40 @@ def test_root_service():
     # get
     assert root['key'] == 1
     assert dict(root) == {'key': 1}
-    assert id(root._my_context) == _id
     assert len(root) == 1
     assert 'key' in root
 
     # update
     root['key'] = 2
     assert dict(root) == {'key': 2}
-    assert id(root._my_context) == _id
 
     # delete
     del root['key']
     assert dict(root) == {}
-    assert id(root._my_context) == _id
+    assert id(root._my_context) == _id  # make sure we haven't changed the obj
 
-    D = {"a": 1, "b": 2}
-    root.update(D)
-    assert dict(root) == D
     assert repr(root)
 
-    root.clear()
-    assert not root
+
+class T:
+    ...
 
 
 def test_scoped_service_inherits():
+    """Most contexts will be scoped on an object.
+
+    Keys set in the scope override the root keys, but missing keys are pulled
+    from the root context.
+    """
+    t = T()
     root = ContextKeyService()
-    scoped = root.create_scoped(None)
+    scoped = root.create_scoped(t)
+    repr(scoped)
+
+    assert root.get_context(t) == scoped._my_context
+
+    with pytest.raises(RuntimeError):
+        root.create_scoped(t)
 
     # add to the scoped dict ... the root is unaffected
     scoped['k'] = 0
@@ -64,8 +114,35 @@ def test_scoped_service_inherits():
     del scoped['k']
     assert dict(scoped) == {'k': 1}
 
-    # deleting from root affects them
+    # deleting from parent affects the scoped keys
     scoped['k2'] = 10
-    root.clear()
+    del root['k']
     assert dict(root) == {}
     assert dict(scoped) == {'k2': 10}
+
+    # make sure that deletion frees the object for rescoping
+    del scoped
+    newscope = root.create_scoped(t)
+    assert newscope is not None
+
+    # make sure that deletion of the object removes the scoped context from root
+    assert newscope._context_id in root._contexts
+    del t
+    assert newscope._context_id not in root._contexts
+    del newscope
+
+
+def test_service_events():
+    mock = Mock()
+    root = ContextKeyService()
+    scoped = root.create_scoped(T())
+    scoped.context_changed.connect(mock)
+
+    root['a'] = 1
+    event = mock.call_args_list[0][0][0]
+    assert event.value == ['a']
+    mock.reset_mock()
+
+    scoped['b'] = 1
+    event = mock.call_args_list[0][0][0]
+    assert event.value == ['b']
