ae87104f3ed24125adeac68d370519199e7d4307
Grzegorz Bokota <bokota+github@gmail.com>
2021-09-10 20:36:27 +0200

    fix getattr

--- napari/_qt/qthreading.py
+++ napari/_qt/qthreading.py
@@ -97,7 +97,9 @@ class WorkerBase(QRunnable):
             # (which is of type `SignalInstance` in PySide and
             # `pyqtBoundSignal` in PyQt)
             return getattr(self.signals, name)
-        return super().__getattr__(name)
+        raise AttributeError(
+            f"{self.__class__.__name__} has no attribute {name}"
+        )
 
     def quit(self) -> None:
         """Send a request to abort the worker.
