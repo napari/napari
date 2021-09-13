f78cfa88496d278bd68cc7c43e4e2912b727f21c
Eric Perlman <perlman@gmail.com>
2021-09-13 13:22:52 +0200

    Formatting.

--- napari/layers/image/_tests/test_image.py
+++ napari/layers/image/_tests/test_image.py
@@ -787,6 +787,8 @@ def test_instantiate_with_experimental_clipping_planes_dict():
 
 def test_tensorstore_image():
     """Test an image coming from a tensorstore array."""
-    data = ts.array(np.full(shape=(1024, 1024), fill_value=255, dtype=np.uint8))
+    data = ts.array(
+        np.full(shape=(1024, 1024), fill_value=255, dtype=np.uint8)
+    )
     layer = Image(data)
     assert np.all(layer.data == data)
