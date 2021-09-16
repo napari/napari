0da164d5c5f948a678a3ffeb742d876374c175ad
Andy Sweet <andrew.d.sweet@gmail.com>
2021-09-16 10:44:29 -0400

    Use an explicit store to clean up.

--- napari/layers/utils/style_encoding.py
+++ napari/layers/utils/style_encoding.py
@@ -8,49 +8,80 @@ from ...utils import Colormap
 from ...utils.colormaps import ValidColormapArg, ensure_colormap
 from ...utils.colormaps.categorical_colormap import CategoricalColormap
 from ...utils.colormaps.standardize_color import transform_color
-from ...utils.events import EventedModel, EventEmitter
+from ...utils.events import EventedModel
+from ...utils.events.custom_types import Array
 from .color_transformations import ColorType
 
 OutputType = TypeVar('OutputType')
 
 
-class StyleEncoding(EventedModel, Generic[OutputType], ABC):
-    _array: np.ndarray
-    _array_update: EventEmitter
+class StyleStore(EventedModel, Generic[OutputType], ABC):
+    array: Array = []
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._array = self._values_to_array([])
-        self._array_update = EventEmitter(source=self)
+        with self.events.array.blocker():
+            self.array = self._values_to_array([])
+
+    def update(self, values: List[OutputType]):
+        self.array = self._values_to_array(values)
+
+    def append(self, values: List[OutputType]):
+        array = self._values_to_array(values)
+        self.array = np.concatenate((self.array, array), axis=0)
+
+    def delete(self, indices: Iterable[int]):
+        self.array = np.delete(self.array, list(indices), axis=0)
+
+    def _values_to_array(self, values: List[OutputType]) -> np.ndarray:
+        return np.array(values)
+
+
+class ColorStore(StyleStore[ColorType]):
+    def _values_to_array(self, values: List[ColorType]) -> np.ndarray:
+        return transform_color(values) if len(values) > 0 else np.empty((0, 4))
+
+
+class StringStore(StyleStore[str]):
+    def _values_to_array(self, values: List[str]) -> np.ndarray:
+        return np.array(values, dtype=str)
+
+
+class StyleEncoding(EventedModel, Generic[OutputType], ABC):
+    _store: StyleStore[OutputType]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._store = self._make_store()
 
     @abstractmethod
     def apply_to_row(self, property_row: Dict[str, Any]) -> OutputType:
         pass
 
+    @abstractmethod
+    def _make_store(self):
+        pass
+
     def get_array(self):
-        return self._array
+        return self._store.array
 
     def connect(self, callback):
-        self._array_update = callback
-
-    def _values_to_array(self, values: List[OutputType]) -> np.ndarray:
-        return np.array(values)
+        self._store.events.array.connect(callback)
 
     def refresh(self, properties: Dict[str, np.ndarray]):
         num_values = _num_rows(properties)
         indices = range(0, num_values)
         values = self._apply_to_table(properties, indices)
-        self._array = self._values_to_array(values)
+        self._store.update(values)
 
     def add(self, properties: Dict[str, np.ndarray], num_to_add: int):
         num_values = _num_rows(properties)
         indices = range(num_values - num_to_add, num_values)
         values = self._apply_to_table(properties, indices)
-        array = self._values_to_array(values)
-        self._array = np.concatenate((self._array, array), axis=0)
+        self._store.append(values)
 
     def remove(self, indices: Iterable[int]):
-        self._array = np.delete(self._array, list(indices), axis=0)
+        self._store.delete(indices)
 
     def _apply_to_table(
         self, properties: Dict[str, np.ndarray], indices: Iterable[int]
@@ -69,8 +100,8 @@ def _num_rows(properties: Dict[str, np.ndarray]) -> int:
 
 
 class ColorEncodingBase(StyleEncoding[ColorType], ABC):
-    def _values_to_array(self, values: List[ColorType]) -> np.ndarray:
-        return transform_color(values) if len(values) > 0 else np.empty((0, 4))
+    def _make_store(self):
+        return ColorStore()
 
 
 class DirectColorEncoding(ColorEncodingBase):
@@ -83,7 +114,7 @@ class DirectColorEncoding(ColorEncodingBase):
         self._on_values_changed()
 
     def _on_values_changed(self, event=None):
-        self._array = self._values_to_array(self.values)
+        self._store.update(self.values)
 
     def apply_to_row(self, property_row: Dict[str, Any]) -> ColorType:
         # TODO: consider returning value if we have access to row index.
@@ -161,8 +192,8 @@ class ContinuousColorEncoding(ColorEncodingBase):
 
 
 class StringEncodingBase(StyleEncoding[str], ABC):
-    def _values_to_array(self, values: List[str]) -> np.ndarray:
-        return np.array(values, dtype=str)
+    def _make_store(self):
+        return StringStore()
 
 
 class FormatStringEncoding(StringEncodingBase):
@@ -191,7 +222,7 @@ class DirectStringEncoding(StringEncodingBase):
         self._on_values_changed()
 
     def _on_values_changed(self, event=None):
-        self._array = self._values_to_array(self.values)
+        self._store.update(self.values)
 
     def apply_to_row(self, property_row: Dict[str, Any]) -> str:
         # TODO: consider returning value if we have access to row index.
