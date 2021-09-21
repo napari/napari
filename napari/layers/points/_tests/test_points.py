347a31c7ca25d9a94cfdafe49c8871a7e9a04e66
Andy Sweet <andrew.d.sweet@gmail.com>
2021-09-21 10:51:13 -0700

    Make test more general.

--- napari/layers/points/_tests/test_points.py
+++ napari/layers/points/_tests/test_points.py
@@ -1535,8 +1535,7 @@ def test_thumbnail_non_square_data():
 
     See: https://github.com/napari/napari/issues/1450
     """
-    # Make the x coordinate range 16x the y range so that the points
-    # should be drawn on two rows of the 32x32 thumbnail.
+    # The points coordinates are in a short and wide range.
     data_range = [1, 32]
     np.random.seed(0)
     data = np.random.random((10, 2)) * data_range
@@ -1547,10 +1546,16 @@ def test_thumbnail_non_square_data():
 
     layer._update_thumbnail()
 
-    assert layer.thumbnail.shape == (32, 32, 4)
-    expected_zeros = np.zeros(shape=(15, 32, 3), dtype=np.uint8)
-    np.testing.assert_array_equal(layer.thumbnail[:15, :, :3], expected_zeros)
-    np.testing.assert_array_equal(layer.thumbnail[17:, :, :3], expected_zeros)
+    assert layer.thumbnail.shape == layer._thumbnail_shape
+    # Check that the thumbnail only contains non-zero RGB values in the middle two rows.
+    mid_row = layer.thumbnail.shape[0] // 2
+    expected_zeros = np.zeros(shape=(mid_row - 1, 32, 3), dtype=np.uint8)
+    np.testing.assert_array_equal(
+        layer.thumbnail[: mid_row - 1, :, :3], expected_zeros
+    )
+    np.testing.assert_array_equal(
+        layer.thumbnail[mid_row + 1 :, :, :3], expected_zeros
+    )
 
 
 def test_thumbnail_with_n_points_greater_than_max():
