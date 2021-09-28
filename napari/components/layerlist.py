Fix off-by-one bug in extent of Image and Labels layers (#3381)
* BUG: fix range of _extent_data in _ImageBase

The data extent should be equal the shape of the data if we assume a pixel
model rather than point model for images
see: https://github.com/napari/napari/issues/3318#issuecomment-910818247

This prevents the issue of scale=0 during ViewerModel.reset_view as
previously reported in
https://github.com/napari/napari/issues/3318

Removed increment by 1 in the Dims.nsteps property to maintain the
correct numer of steps for the slider widgets.

* TST: update expected world extents in test_layers_list.py

the extents are now larger by 1 element along each dimension

* TST: fix shape of labels layer generated by ViewerModel._new_labels

Remove the + 1 added to the extent here now that the extent itself is
larger by 1. This fixes the test_new_labels_empty test case.

Could probably also remove the " if s > 0 else 1" conditional

* TST: fix translate_grid extent calculation in ViewerModel._subplot

Remove +1 now that extent is larger by 1
Fixes test_grid test case.

* TST: update expected values in test_sliced_world_extent

Now larger by 1 pixel spacing

* TST: update expected camera center in test_camera

After the increment by 1 in extent it is exactly half the data shape

* TST: update expected viewer.dims.range in test_update_scale

Extent was increased by 1, so no longer subtract 1 from shape

* TST: update range checks in _ImageBase._set_view_slice

use np.greater_equal now that extent was updated by 1
avoids IndexError in test_translated_images

* TST: update expected ranges in other test_world_coordinates.py test cases

* TST: remove addition of 1 to extent in shape comparisons in Image layer tests

* TST: Fix expected extents in additional Image layer tests

In test_world_data_extent, no longer need to subtract 1 from shape now that extent was incremented by 1

In test_scale, no longer need to add layer.e

napari/components/layerlist.py
@@ -113,7 +113,8 @@ class LayerList(SelectableEventedList[Layer]):
     def _extent_world(self) -> np.ndarray:
         """Extent of layers in world coordinates.
 
-        Default to 2D with (0, 512) min/ max values if no data is present.
+        Default to 2D with (-0.5, 511.5) min/ max values if no data is present.
+        Corresponds to pixels centered at [0, ..., 511].
 
         Returns
         -------
@@ -124,7 +125,8 @@ class LayerList(SelectableEventedList[Layer]):
     def _get_extent_world(self, layer_extent_list):
         """Extent of layers in world coordinates.
 
-        Default to 2D with (0, 512) min/ max values if no data is present.
+        Default to 2D with (-0.5, 511.5) min/ max values if no data is present.
+        Corresponds to pixels centered at [0, ..., 511].
 
         Returns
         -------
@@ -159,9 +161,15 @@ class LayerList(SelectableEventedList[Layer]):
                     axis=1,
                 )
 
-        min_vals = np.nan_to_num(min_v[::-1])
-        max_vals = np.copy(max_v[::-1])
-        max_vals[np.isnan(max_vals)] = 511
+        try:
+            min_vals = np.nan_to_num(min_v[::-1], nan=-0.5)
+            max_vals = np.nan_to_num(max_v[::-1], nan=511.5)
+        except TypeError:
+            # In NumPy < 1.17, nan_to_num doesn't have a nan kwarg
+            min_vals = np.asarray(min_v[::-1])
+            min_vals[np.isnan(min_vals)] = -0.5
+            max_vals = np.asarray(max_v[::-1])
+            max_vals[np.isnan(max_vals)] = 511.5
 
         return np.vstack([min_vals, max_vals])
 