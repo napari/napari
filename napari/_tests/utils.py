fb9df2e18856cd3ae5c83ab9e878ef973979731b
Eric Perlman <perlman@gmail.com>
2021-10-12 23:06:51 +0200

    Skip tensorstore tests when using async.

--- napari/_tests/utils.py
+++ napari/_tests/utils.py
@@ -37,14 +37,20 @@ Used as pytest params for testing layer add and view functionality (Layer class,
 """
 layer_test_data = [
     (Image, np.random.random((10, 15)), 2),
-    (Image, ts.array(np.random.random((10, 15))), 2),
+    pytest.param(
+        Image,
+        ts.array(np.random.random((10, 15))),
+        2,
+        marks=pytest.mark.sync_only,
+    ),
     (Image, np.random.random((10, 15, 20)), 3),
     (Image, np.random.random((5, 10, 15, 20)), 4),
     (Image, [np.random.random(s) for s in [(40, 20), (20, 10), (10, 5)]], 2),
-    (
+    pytest.param(
         Image,
         [ts.array(np.random.random(s)) for s in [(40, 20), (20, 10), (10, 5)]],
         2,
+        marks=pytest.mark.sync_only,
     ),
     (Labels, np.random.randint(20, size=(10, 15)), 2),
     (Labels, np.random.randint(20, size=(6, 10, 15)), 3),
