ab7bb40a37dc6d2faa46e1b74f8ca93ed396d7a9
Talley Lambert <talley.lambert@gmail.com>
2021-10-12 12:15:48 -0400

    updates for magicgui 0.3.0 (#3465)

--- napari/viewer.py
+++ napari/viewer.py
@@ -1,5 +1,7 @@
 from typing import TYPE_CHECKING
 
+import magicgui as mgui
+
 from .components.viewer_model import ViewerModel
 from .utils import _magicgui, config
 
@@ -8,7 +10,7 @@ if TYPE_CHECKING:
     from ._qt.qt_main_window import Window
 
 
-@_magicgui.register_type(bind=_magicgui.find_viewer_ancestor)
+@mgui.register_type(bind=_magicgui.find_viewer_ancestor)
 class Viewer(ViewerModel):
     """Napari ndarray viewer.
 
