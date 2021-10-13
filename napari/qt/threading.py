09233a10151f4b3df8b91dbca07e878d8261908a
Talley Lambert <talley.lambert@gmail.com>
2021-10-13 11:10:25 -0400

    wip

--- napari/qt/threading.py
+++ napari/qt/threading.py
@@ -1,24 +1,12 @@
-from .._qt.qthreading import (
-    FunctionWorker,
-    GeneratorWorker,
-    GeneratorWorkerSignals,
-    WorkerBase,
-    WorkerBaseSignals,
-    active_thread_count,
-    create_worker,
-    set_max_thread_count,
-    thread_worker,
-)
+from superqt.utils import FunctionWorker, GeneratorWorker, WorkerBase
+
+from .._qt.qthreading import create_worker, thread_worker
 
 # all of these might be used by an end-user when subclassing
 __all__ = (
-    'active_thread_count',
     'create_worker',
     'FunctionWorker',
     'GeneratorWorker',
-    'GeneratorWorkerSignals',
-    'set_max_thread_count',
     'thread_worker',
     'WorkerBase',
-    'WorkerBaseSignals',
 )
