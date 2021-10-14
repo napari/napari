1e594a5845002b2d971bcdd90dbf02abba891550
Andy Sweet <andrew.d.sweet@gmail.com>
2021-10-14 10:58:17 -0700

    Remove mistakenly added benchmark.

--- napari/benchmarks/benchmark_fail.py
+++
@@ -1,8 +0,0 @@
-class FailSuite:
-    def setup(self):
-        print('setup')
-        self.values = [1, 2, 3]
-
-    def time_remove(self):
-        print('time_remove')
-        del self.values[2]
