a4f2528e2962b524d06d5f5d97f3f3f7f6b7c8e9
Andy Sweet <andrew.d.sweet@gmail.com>
2021-10-15 14:01:09 -0700

    Remove make_napari_viewer in vispy tests.

--- napari/_vispy/_tests/test_vispy_shapes_layer.py
+++ napari/_vispy/_tests/test_vispy_shapes_layer.py
@@ -1,15 +1,17 @@
 import numpy as np
 
+from napari._vispy.layers.shapes import VispyShapesLayer
+from napari.layers import Shapes
 
-def test_change_text_updates_node_string(make_napari_viewer):
-    viewer = make_napari_viewer()
+
+def test_change_text_updates_node_string():
     shapes = np.random.rand(3, 4, 2)
     properties = {
         'class': np.array(['A', 'B', 'C']),
         'name': np.array(['D', 'E', 'F']),
     }
-    layer = viewer.add_shapes(shapes, properties=properties, text='class')
-    vispy_layer = viewer.window.qt_viewer.layer_to_visual[layer]
+    layer = Shapes(shapes, properties=properties, text='class')
+    vispy_layer = VispyShapesLayer(layer)
     text_node = vispy_layer._get_text_node()
     np.testing.assert_array_equal(text_node.text, properties['class'])
 
@@ -18,13 +20,12 @@ def test_change_text_updates_node_string(make_napari_viewer):
     np.testing.assert_array_equal(text_node.text, properties['name'])
 
 
-def test_change_text_color_updates_node_color(make_napari_viewer):
-    viewer = make_napari_viewer()
+def test_change_text_color_updates_node_color():
     shapes = np.random.rand(3, 4, 2)
     properties = {'class': np.array(['A', 'B', 'C'])}
     text = {'text': 'class', 'color': [1, 0, 0]}
-    layer = viewer.add_shapes(shapes, properties=properties, text=text)
-    vispy_layer = viewer.window.qt_viewer.layer_to_visual[layer]
+    layer = Shapes(shapes, properties=properties, text=text)
+    vispy_layer = VispyShapesLayer(layer)
     text_node = vispy_layer._get_text_node()
     np.testing.assert_array_equal(text_node.color.rgb, [[1, 0, 0]])
 
@@ -33,12 +34,11 @@ def test_change_text_color_updates_node_color(make_napari_viewer):
     np.testing.assert_array_equal(text_node.color.rgb, [[0, 0, 1]])
 
 
-def test_change_properties_updates_node_strings(make_napari_viewer):
-    viewer = make_napari_viewer()
+def test_change_properties_updates_node_strings():
     shapes = np.random.rand(3, 4, 2)
     properties = {'class': np.array(['A', 'B', 'C'])}
-    layer = viewer.add_shapes(shapes, properties=properties, text='class')
-    vispy_layer = viewer.window.qt_viewer.layer_to_visual[layer]
+    layer = Shapes(shapes, properties=properties, text='class')
+    vispy_layer = VispyShapesLayer(layer)
     text_node = vispy_layer._get_text_node()
     np.testing.assert_array_equal(text_node.text, ['A', 'B', 'C'])
 
@@ -47,14 +47,11 @@ def test_change_properties_updates_node_strings(make_napari_viewer):
     np.testing.assert_array_equal(text_node.text, ['D', 'E', 'F'])
 
 
-def test_update_property_value_then_refresh_text_updates_node_strings(
-    make_napari_viewer,
-):
-    viewer = make_napari_viewer()
+def test_update_property_value_then_refresh_text_updates_node_strings():
     shapes = np.random.rand(3, 4, 2)
     properties = {'class': np.array(['A', 'B', 'C'])}
-    layer = viewer.add_shapes(shapes, properties=properties, text='class')
-    vispy_layer = viewer.window.qt_viewer.layer_to_visual[layer]
+    layer = Shapes(shapes, properties=properties, text='class')
+    vispy_layer = VispyShapesLayer(layer)
     text_node = vispy_layer._get_text_node()
     np.testing.assert_array_equal(text_node.text, ['A', 'B', 'C'])
 
