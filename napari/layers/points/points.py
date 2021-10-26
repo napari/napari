0a41ad30d206cb40bd62b1896d802a0a8cd8796f
Alister Burt <alisterburt@gmail.com>
2021-10-26 21:21:55 +0100

    override get_ray_intersections

--- napari/layers/points/points.py
+++ napari/layers/points/points.py
@@ -1347,6 +1347,64 @@ class Points(Layer):
         bounding_box[:, 1] += max_point_size / 2
         return bounding_box
 
+    def get_ray_intersections(
+        self,
+        position: List[float],
+        view_direction: np.ndarray,
+        dims_displayed: List[int],
+        world: bool = True,
+    ) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[None, None]]:
+        """Get the start and end point for the ray extending
+        from a point through the displayed bounding box.
+
+        This method overrides the base layer, replacing the bounding box used
+        to calculate intersections with a larger one which includes the size
+        of points in view.
+
+        Parameters
+        ----------
+        position
+            the position of the point in nD coordinates. World vs. data
+            is set by the world keyword argument.
+        view_direction : np.ndarray
+            a unit vector giving the direction of the ray in nD coordinates.
+            World vs. data is set by the world keyword argument.
+        dims_displayed
+            a list of the dimensions currently being displayed in the viewer.
+        world : bool
+            True if the provided coordinates are in world coordinates.
+            Default value is True.
+
+        Returns
+        -------
+        start_point : np.ndarray
+            The point on the axis-aligned data bounding box that the cursor click
+            intersects with. This is the point closest to the camera.
+            The point is the full nD coordinates of the layer data.
+            If the click does not intersect the axis-aligned data bounding box,
+            None is returned.
+        end_point : np.ndarray
+            The point on the axis-aligned data bounding box that the cursor click
+            intersects with. This is the point farthest from the camera.
+            The point is the full nD coordinates of the layer data.
+            If the click does not intersect the axis-aligned data bounding box,
+            None is returned.
+        """
+        original_bounding_box_method = self._display_bounding_box
+        augmented_bounding_box = self._display_bounding_box_augmented(
+            dims_displayed=dims_displayed
+        )
+        self._display_bounding_box = lambda x: augmented_bounding_box
+        start_point, end_point = Layer.get_ray_intersections(
+            self,
+            position=position,
+            view_direction=view_direction,
+            dims_displayed=dims_displayed,
+            world=world,
+        )
+        self._display_bounding_box = original_bounding_box_method
+        return start_point, end_point
+
     def _set_view_slice(self):
         """Sets the view given the indices to slice with."""
         # get the indices of points in view
