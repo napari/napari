617233a998507549064144c4ef86f4065d514030
Alister Burt <alisterburt@gmail.com>
2021-10-26 22:06:16 +0100

    simplify move point example

--- examples/interactive_move_point_3d.py
+++ examples/interactive_move_point_3d.py
@@ -6,6 +6,7 @@ from copy import copy
 import numpy as np
 
 import napari
+from napari.utils.geometry import project_point_onto_plane
 
 # Create viewer, point to move and bounding box
 viewer = napari.Viewer(ndisplay=3)
@@ -22,12 +23,14 @@ bounding_box_data = [
 ]
 bounding_box_layer = viewer.add_points(
     bounding_box_data,
+    name='bounding box',
     face_color='green',
     size=0.2,
     edge_width=0
 )
 point_layer = viewer.add_points(
     [0, 0, 0],
+    name='point',
     face_color='magenta',
     size=0.2,
     edge_width=0
@@ -43,36 +46,28 @@ def drag_along_camera_plane(layer, event):
     # disable interactivity during this drag event to fix the view direction
     layer.interactive = False
 
-    # store start position of point and mouse event data
+    # store start position of point
     original_position = copy(point_layer.data[0])
-    start_position_world = copy(event.position)
-    view_direction = copy(event.view_direction)
-
-    # Calculate two orthogonal unit vectors in camera plane (in data
-    # coordinates)
-    # These provide a basis for moving our point
-    arbitrary_vector = np.array([1, 1, 1])
-    v1 = np.cross(view_direction, arbitrary_vector)  # orthogonal to both
-    v2 = np.cross(view_direction, v1)  # orthogonal to both
-
-    basis_vectors = np.stack([v1, v2], axis=0)
-    basis_vectors = basis_vectors / np.linalg.norm(basis_vectors, axis=1)[:,
-                                    np.newaxis]
 
     yield
     while event.type == 'mouse_move':
-        # project drag vector onto orthogonal basis_vectors in pseudo-canvas
-        current_position_world = event.position
-        projected_distances = layer.projected_distance_from_mouse_drag(
-            start_position_world, current_position_world, view_direction,
-            basis_vectors
+        # Calculate click position in data coords
+        point_to_project = layer.world_to_displayed_data(event.position)
+        view_direction_data = np.asarray(layer._world_to_data_ray(
+            event.view_direction
+        ))[event.dims_displayed]
+
+        projected_position = project_point_onto_plane(
+            point=point_to_project,
+            plane_point=original_position,
+            plane_normal=view_direction_data,
         )
 
-        # Calculate shifts as projected distances multiplied by basis_vectors
-        shifts = projected_distances[:, np.newaxis] * basis_vectors
 
+        # Calculate shifts as projected distances multiplied by basis_vectors
+        shifts = projected_position - original_position
         # Update position
-        updated_position = original_position + np.sum(shifts, axis=0)
+        updated_position = original_position + shifts
 
         # Clamp updated position to bounding box
         clamped = np.where(updated_position > 1, 1, updated_position)
