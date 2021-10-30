41c2dee82aacad2ffddb0e1940561de9283634dd
Talley Lambert <talley.lambert@gmail.com>
2021-10-30 18:43:02 -0400

    fix threadpool example

--- examples/mgui_with_threadpoolexec.py
+++ examples/mgui_with_threadpoolexec.py
@@ -1,4 +1,6 @@
 """An example of calling a threaded function from a magicgui dock_widget.
+
+using ThreadPoolExecutor
 Note: this example requires python >= 3.9
 """
 import sys
@@ -7,7 +9,6 @@ from concurrent.futures import Future, ThreadPoolExecutor
 from magicgui import magic_factory
 from skimage import data
 from skimage.feature import blob_log
-from typing_extensions import Annotated
 
 import napari
 from napari.types import ImageData, LayerDataTuple
@@ -16,19 +17,25 @@ if sys.version_info < (3, 9):
     print('This example requires python >= 3.9')
     sys.exit(0)
 
+pool = ThreadPoolExecutor()
 
-@magic_factory
+
+@magic_factory(
+    min_sigma={"min": 0.5, "max": 15, "step": 0.5},
+    max_sigma={"min": 1, "max": 200, "step": 0.5},
+    num_sigma={"min": 1, "max": 20},
+    threshold={"min": 0, "max": 1000, "step": 0.1},
+)
 def make_widget(
     image: ImageData,
-    min_sigma: Annotated[float, {"min": 0.5, "max": 15, "step": 0.5}] = 5,
-    max_sigma: Annotated[float, {"min": 1, "max": 200, "step": 0.5}] = 30,
-    num_sigma: Annotated[int, {"min": 1, "max": 20}] = 10,
-    threshold: Annotated[float, {"min": 0, "max": 1000, "step": 0.1}] = 0.3,
+    min_sigma: float = 5,
+    max_sigma: float = 30,
+    num_sigma: int = 10,
+    threshold: float = 0.3,
 ) -> Future[LayerDataTuple]:
 
     # long running function
     def _make_blob():
-        print("running!")
         # skimage.feature may take a while depending on the parameters
         blobs = blob_log(
             image,
@@ -44,11 +51,9 @@ def make_widget(
             edge_width=2,
             face_color="transparent",
         )
-        print("finished")
         return (data, kwargs, 'points')
 
-    with ThreadPoolExecutor() as exec:
-        return exec.submit(_make_blob)
+    return pool.submit(_make_blob)
 
 
 viewer = napari.Viewer()
@@ -56,3 +61,4 @@ viewer.window.add_dock_widget(make_widget(), area="right")
 viewer.add_image(data.hubble_deep_field().mean(-1))
 
 napari.run()
+pool.shutdown(wait=True)
