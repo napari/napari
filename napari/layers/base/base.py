359b983e37326bca56c446fa67ad9520d2d78ea4
alisterburt <alisterburt@gmail.com>
2021-11-10 18:16:10 +0000

    Update napari/layers/base/base.py

--- napari/layers/base/base.py
+++ napari/layers/base/base.py
@@ -1263,8 +1263,8 @@ class Layer(KeymapProvider, MousemapProvider, ABC):
         view_direction: np.ndarray,
         dims_displayed: List,
     ) -> Tuple[np.ndarray, np.ndarray]:
-        """Calculate a (point, normal) plane parallel to the canvas in data
-        coordinates.
+       """Calculate a (point, normal) plane parallel to the canvas in data
+        coordinates, centered on the centre of rotation of the camera.
 
         Parameters
         ----------
