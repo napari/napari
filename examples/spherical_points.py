43542293a57630fbc032c30282f606fff37969b2
Lorenzo Gaifas <brisvag@gmail.com>
2021-11-25 10:52:52 +0100

    add example

---
+++ examples/spherical_points.py
@@ -0,0 +1,23 @@
+import napari
+import numpy as np
+
+
+np.random.seed()
+
+pts = np.random.rand(100, 3) * 100
+colors = np.random.rand(100, 3)
+sizes = np.random.rand(100) * 20 + 10
+
+viewer = napari.Viewer(ndisplay=3)
+pts_layer = viewer.add_points(
+    pts,
+    face_color=colors,
+    size=sizes,
+    spherical=True,
+    antialias=0,
+    edge_width=0,
+)
+
+viewer.reset_view()
+
+napari.run()
