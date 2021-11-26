375348a9c034c68858d71dbe55286bc4c315666e
Lorenzo Gaifas <brisvag@gmail.com>
2021-11-26 18:11:48 +0100

    test more dtypes

--- napari/_qt/_tests/test_qt_viewer.py
+++ napari/_qt/_tests/test_qt_viewer.py
@@ -275,32 +275,42 @@ def test_screenshot_dialog(make_napari_viewer, tmpdir):
 
 
 @pytest.mark.parametrize(
-    "dtype", ['int8', 'uint8', 'int16', 'uint16', 'float32']
+    "dtype",
+    [
+        'int8',
+        'uint8',
+        'int16',
+        'uint16',
+        'int32',
+        'float16',
+        'float32',
+        'float64',
+    ],
 )
 def test_qt_viewer_data_integrity(make_napari_viewer, dtype):
     """Test that the viewer doesn't change the underlying array."""
-
     image = np.random.rand(10, 32, 32)
     image *= 200 if dtype.endswith('8') else 2 ** 14
     image = image.astype(dtype)
     imean = image.mean()
 
     viewer = make_napari_viewer()
+    layer = viewer.add_image(image.copy())
+    data = layer.data
 
-    viewer.add_image(image.copy())
-    datamean = viewer.layers[0].data.mean()
+    datamean = np.mean(data)
     assert datamean == imean
     # toggle dimensions
     viewer.dims.ndisplay = 3
-    datamean = viewer.layers[0].data.mean()
+    datamean = np.mean(data)
     assert datamean == imean
     # back to 2D
     viewer.dims.ndisplay = 2
-    datamean = viewer.layers[0].data.mean()
+    datamean = np.mean(data)
     assert datamean == imean
     # also check that vispy gets (almost) the same data
-    datamean = fix_data_dtype(image).mean()
-    assert np.allclose(datamean, imean)
+    datamean = np.mean(fix_data_dtype(data))
+    assert np.allclose(datamean, imean, rtol=3e-04)
 
 
 def test_points_layer_display_correct_slice_on_scale(make_napari_viewer):
