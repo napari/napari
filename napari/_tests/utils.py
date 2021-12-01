13fec156b814a188051b3f198a6ec9d62f095e8d
Lorenzo Gaifas <brisvag@gmail.com>
2021-12-01 01:07:09 +0100

    more all

--- napari/_tests/utils.py
+++ napari/_tests/utils.py
@@ -135,7 +135,7 @@ def are_objects_equal(object1, object2):
     # equal_nan does not exist in array_equal in old numpy
     if tuple(int(v) for v in np.__version__.split('.')) < (1, 19):
         fixed = [(np.nan_to_num(a1), np.nan_to_num(a2)) for a1, a2 in items]
-        return np.all([a1 == a2 for a1, a2 in fixed])
+        return np.all([np.all(a1 == a2) for a1, a2 in fixed])
 
     try:
         return np.all(
