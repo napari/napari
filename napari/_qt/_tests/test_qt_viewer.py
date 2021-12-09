db50e716d181fe46ba8222f89493466422fca8b5
Talley Lambert <talley.lambert@gmail.com>
2021-12-08 20:03:43 -0500

    go back to wait

--- napari/_qt/_tests/test_qt_viewer.py
+++ napari/_qt/_tests/test_qt_viewer.py
@@ -480,14 +480,14 @@ def test_memory_leaking(qtbot, make_napari_viewer):
 
 
 @skip_local_popups
-def test_leaks_image(qapp, make_napari_viewer):
+def test_leaks_image(qtbot, make_napari_viewer):
 
     viewer = make_napari_viewer(show=True)
     lr = weakref.ref(viewer.add_image(np.random.rand(10, 10)))
     dr = weakref.ref(lr().data)
 
     viewer.layers.clear()
-    qapp.processEvents()
+    qtbot.wait(100)
     gc.collect()
     assert not gc.collect()
     assert not lr()
@@ -495,14 +495,14 @@ def test_leaks_image(qapp, make_napari_viewer):
 
 
 @skip_local_popups
-def test_leaks_labels(qapp, make_napari_viewer):
+def test_leaks_labels(qtbot, make_napari_viewer):
     viewer = make_napari_viewer(show=True)
     lr = weakref.ref(
         viewer.add_labels((np.random.rand(10, 10) * 10).astype(np.uint8))
     )
     dr = weakref.ref(lr().data)
     viewer.layers.clear()
-    qapp.processEvents()
+    qtbot.wait(100)
     gc.collect()
     assert not gc.collect()
     assert not lr()
