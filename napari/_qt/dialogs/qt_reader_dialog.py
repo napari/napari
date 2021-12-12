116e98f7f00b87eab3a61d23201bba8768198883
Draga Doncila Pop <ddoncila@gmail.com>
2021-12-12 13:11:25 +1100

    Document reader dialog

--- napari/_qt/dialogs/qt_reader_dialog.py
+++ napari/_qt/dialogs/qt_reader_dialog.py
@@ -1,4 +1,9 @@
 import os
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from PyQt5.QtWidgets import QWidget
+    from typing import Dict
 
 from qtpy.QtWidgets import (
     QButtonGroup,
@@ -12,12 +17,14 @@ from qtpy.QtWidgets import (
 
 
 class QtReaderDialog(QDialog):
+    """Dialog for user to select a reader plugin for a given file extension or folder"""
+
     def __init__(
         self,
-        pth=None,
-        parent=None,
-        readers=None,
-        error_message=None,
+        pth: str=None,
+        parent: "QWidget" =None,
+        readers: "Dict[str, str]"=None,
+        error_message: str=None,
     ):
         super().__init__(parent)
         self.setObjectName('Choose reader')
@@ -27,42 +34,47 @@ class QtReaderDialog(QDialog):
         self.setup_ui(error_message, readers)
 
     def setup_ui(self, error_message, readers):
+        """Build UI using given error_messsage and readers dict"""
+
+        # add instruction label
         layout = QVBoxLayout()
-        self.setLayout(layout)
+        label = QLabel(f"{error_message if error_message else ''}"+\
+            f"Choose reader for {self._current_file}:")
+        layout.addWidget(label)
 
-        label = QLabel()
-        label.setText(
-            f"{error_message if error_message else ''}Choose reader for file {self._current_file}:"
-        )
-        self.layout().addWidget(label)
-
+        # add radio button for each reader plugin
         self.reader_btn_group = QButtonGroup(self)
-        self.add_reader_buttons(readers)
+        self.add_reader_buttons(layout, readers)
         if self.reader_btn_group.buttons():
             self.reader_btn_group.buttons()[0].toggle()
 
+        # OK & cancel buttons for the dialog
+        btns = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        self.btn_box = QDialogButtonBox(btns)
+        self.btn_box.accepted.connect(self.accept)
+        self.btn_box.rejected.connect(self.reject)
+
+        # checkbox to remember the choice (doesn't pop up for folders)
         extension = os.path.splitext(self._current_file)[1]
         if extension:
             self.persist_checkbox = QCheckBox(
                 f'Remember this choice for files with a {extension} extension'
             )
             self.persist_checkbox.toggle()
-            self.layout().addWidget(self.persist_checkbox)
+            layout.addWidget(self.persist_checkbox)
+        
+        layout.addWidget(self.btn_box)
+        self.setLayout(layout)
 
-        btns = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
-        self.btn_box = QDialogButtonBox(btns)
-        self.btn_box.accepted.connect(self.accept)
-        self.btn_box.rejected.connect(self.reject)
-        self.layout().addWidget(self.btn_box)
-
-    def set_current_file(self, pth):
-        self._current_file = pth
-
-    def add_reader_buttons(self, readers):
+    def add_reader_buttons(self, layout, readers):
+        """Add radio button to layout for each reader in readers"""
         for display_name in readers:
             button = QRadioButton(f"{display_name}")
             self.reader_btn_group.addButton(button)
-            self.layout().addWidget(button)
+            layout.addWidget(button)
 
     def get_plugin_choice(self):
-        return self.reader_btn_group.checkedButton().text()
+        """Get user's plugin choice based on the checked button"""
+        checked_btn = self.reader_btn_group.checkedButton()
+        if checked_btn:
+            return checked_btn.text()
