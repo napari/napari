31917596d60db013c8ef76cd99600bb6b99c9597
Draga Doncila Pop <ddoncila@gmail.com>
2021-12-12 14:10:46 +1100

    Document utils

--- napari/plugins/utils.py
+++ napari/plugins/utils.py
@@ -1,7 +1,22 @@
+from typing import TYPE_CHECKING
 from . import _npe2, plugin_manager
 
+if TYPE_CHECKING:
+    from typing import Dict
 
-def get_potential_readers(filename):
+
+def get_potential_readers(filename: str) -> "Dict[str, str]":
+    """Given filename, returns all readers that may read the file.
+
+    Original plugin engine readers are checked based on returning
+    a function from `napari_get_reader`. Npe2 readers are iterated
+    based on file extension and accepting directories.
+
+    Returns
+    -------
+    Dict[str, str]
+        dictionary of display_name to registered name
+    """
     readers = _npe2.get_readers(filename)
 
     npe1_readers = {}
