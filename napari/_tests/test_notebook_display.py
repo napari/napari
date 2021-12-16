2a853a298b28099454055377d49753af710c9a21
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2021-12-16 21:22:15 +1100

    Test for alt-text in nbscreenshot HTML output

--- napari/_tests/test_notebook_display.py
+++ napari/_tests/test_notebook_display.py
@@ -11,8 +11,11 @@ def test_nbscreenshot(make_napari_viewer):
     data = np.random.random((10, 15))
     viewer.add_image(data)
 
-    rich_display_object = nbscreenshot(viewer)
+    rich_display_object = nbscreenshot(viewer, alt_text="My alt text")
     assert hasattr(rich_display_object, '_repr_png_')
     # Trigger method that would run in jupyter notebook cell automatically
     rich_display_object._repr_png_()
     assert rich_display_object.image is not None
+
+    html_output = rich_display_object._repr_html_()
+    assert 'alt="My alt text"' in html_output
