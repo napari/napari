fix rotation matrix bug

napari/utils/geometry.py
@@ -69,19 +69,30 @@ def project_points_onto_plane(
 def rotation_matrix_from_vectors_2d(
     vec_1: np.ndarray, vec_2: np.ndarray
 ) -> np.ndarray:
-    dot_prod = np.dot(vec_1, vec_2)
-    if np.allclose(dot_prod, 1):
-        # if the vectors are already aligned, return the identity
-        rotation_matrix = np.eye(2)
-    elif np.allclose(dot_prod, 0):
-        # if the vectors are in opposite direction, rotate 180 degrees
-        rotation_matrix = np.diag([-1, -1])
-    else:
-        diagonal_1 = (vec_1[0] * vec_2[0]) + (vec_1[1] * vec_2[1])
-        diagonal_2 = (vec_1[0] * vec_2[1]) - (vec_2[0] * vec_1[0])
-        rotation_matrix = np.array(
-            [[diagonal_1, -1 * diagonal_2], [diagonal_2, diagonal_1]]
-        )
+    """Calculate the 2D rotation matrix to rotate vec_1 onto vec_2
+
+    Parameters
+    ----------
+    vec_1 : np.ndarray
+        The (2,) array containing the starting vector.
+    vec_2 : np.ndarray
+        The (2,) array containing the destination vector.
+
+    Returns
+    -------
+    rotation_matrix : np.ndarray
+        The (2, 2) tranformation matrix that rotates vec_1 to vec_2.
+    """
+    # ensure unit vectors
+    vec_1 = vec_1 / np.linalg.norm(vec_1)
+    vec_2 = vec_2 / np.linalg.norm(vec_2)
+
+    # calculate the rotation matrix
+    diagonal_1 = (vec_1[0] * vec_2[0]) + (vec_1[1] * vec_2[1])
+    diagonal_2 = (vec_1[0] * vec_2[1]) - (vec_2[0] * vec_1[0])
+    rotation_matrix = np.array(
+        [[diagonal_1, -1 * diagonal_2], [diagonal_2, diagonal_1]]
+    )
 
     return rotation_matrix
 