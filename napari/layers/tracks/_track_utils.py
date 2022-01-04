d25f436693282298d6496ce10ba219abef037da8
Alister Burt <alisterburt@gmail.com>
2022-01-04 13:31:51 +0000

    fix bug

--- napari/layers/tracks/_track_utils.py
+++ napari/layers/tracks/_track_utils.py
@@ -123,10 +123,11 @@ class TrackManager:
         # here to make sure that we align with the napari dims index which
         # will be an integer - however, the time index does not necessarily
         # need to be an int, and the shader will render correctly.
-        frames = list(set(self._points[:, 0].astype(np.uint).tolist()))
+        time = self._points[:, 0].astype(np.uint)
+        frames = list(set(time.tolist()))
         self._points_lookup = {}
         for f in frames:
-            idx = np.where(self._points[:, 0] == f)[0]
+            idx = np.where(time == f)[0]
             self._points_lookup[f] = slice(min(idx), max(idx) + 1, 1)
 
         # make a second lookup table using a sparse matrix to convert track id
