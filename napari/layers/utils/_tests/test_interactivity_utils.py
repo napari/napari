16e2d38b8af07e84690844a22d07fc87e6245100
Alister Burt <alisterburt@gmail.com>
2022-01-18 19:44:57 +0000

    add test for orient_plane_normal_around_cursor

--- napari/layers/utils/_tests/test_interactivity_utils.py
+++ napari/layers/utils/_tests/test_interactivity_utils.py
@@ -3,6 +3,7 @@ import pytest
 
 from napari.layers.utils.interactivity_utils import (
     drag_data_to_projected_distance,
+    orient_plane_normal_around_cursor,
 )
 
 
@@ -30,3 +31,29 @@ def test_drag_data_to_projected_distance(
         start_position, end_position, view_direction, vector
     )
     assert np.allclose(result, expected_value)
+
+
+def test_orient_plane_normal_around_cursor(make_napari_viewer):
+    viewer = make_napari_viewer()
+    viewer.dims.ndisplay = 3
+    viewer.camera.angles = (0, 0, 90)
+    viewer.cursor.position = (14, 14, 14)
+
+    image_layer = viewer.add_image(np.zeros(shape=(28, 28, 28)))
+    image_layer.depiction = 'plane'
+    image_layer.plane.normal = (1, 0, 0)
+    image_layer.plane.position = (14, 14, 14)
+
+    # apply simple transformation on the volume
+    image_layer.translate = [1, 1, 1]
+
+    # orient plane normal
+    orient_plane_normal_around_cursor(
+        layer=image_layer, plane_normal=(1, 0, 1)
+    )
+
+    # check that plane normal has been updated
+    assert np.allclose(
+        image_layer.plane.normal, [1, 0, 1] / np.linalg.norm([1, 0, 1])
+    )
+    assert np.allclose(image_layer.plane.position, [14, 13, 13])
