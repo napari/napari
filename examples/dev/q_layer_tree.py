614c2a32a3860952524208ead4181f2d467463a8
Talley Lambert <talley.lambert@gmail.com>
2022-01-27 08:29:39 -0500

    add example

---
+++ examples/dev/q_layer_tree.py
@@ -0,0 +1,55 @@
+import napari
+from napari.qt import get_app, run
+from napari.layers import Points, Image
+from napari.layers.layergroup import LayerGroup
+from napari._qt.containers import QtLayerTreeView
+from skimage.data import grass
+
+app = get_app()
+
+tip = Points(name='tip')
+p2 = Points(name='2')
+lg2 = LayerGroup([p2], name="g2")
+lg1 = LayerGroup([lg2, Points(name='3'), tip, Points(name='1')], name="g1")
+root = LayerGroup(
+    [
+        lg1,
+        Points(name='4'),
+        Points(name='5'),
+        # Points(name='6'),
+        # Points(name='7'),
+        # Points(name='8'),
+        # Points(name='9'),
+    ],
+    name="root",
+)
+root.events.reordered.connect(lambda e: print(e.value))
+root.selection.active = lg1
+
+tree = QtLayerTreeView(root)
+
+# spy on events
+root.events.reordered.connect(lambda e: print("reordered to: ", e.value))
+root.selection.events.changed.connect(
+    lambda e: print(
+        f"selection changed.  added: {e.added}, removed: {e.removed}"
+    )
+)
+root.selection.events._current.connect(
+    lambda e: print(f"current item changed to: {e.value}")
+)
+
+# model = tree.model()
+# tree.show()
+
+# im, pt = Image(grass()), Points()
+# root2 = LayerGroup([LayerGroup([im, pt])])
+# tree2 = QtLayerTreeView(root2)
+v = napari.Viewer()
+
+# v.layers.extend([im, pt])
+v.layers.append(tip)
+v.layers.append(p2)
+v.window.add_dock_widget(tree, area='right')
+
+run()
