70b21b789d058a790dca9c383109ea6dafaa14ca
Andy Sweet <andrew.d.sweet@gmail.com>
2022-02-02 17:15:19 -0800

    Update examples

--- examples/show_points_based_on_feature.py
+++ examples/show_points_based_on_feature.py
@@ -11,7 +11,7 @@ colors = np.random.rand(100, 3)
 confidence = np.random.rand(100)
 
 viewer = napari.Viewer(ndisplay=3)
-points = viewer.add_points(points, face_color=colors, properties={'confidence': confidence})
+points = viewer.add_points(points, face_color=colors, features={'confidence': confidence})
 
 
 # create a simple widget with magicgui which provides a slider that controls the visilibility
@@ -21,7 +21,7 @@ points = viewer.add_points(points, face_color=colors, properties={'confidence': 
     threshold={'widget_type': 'FloatSlider', 'min': 0, 'max': 1}
 )
 def confidence_slider(layer: napari.layers.Points, threshold=0.5):
-    layer.shown = layer.properties['confidence'] > threshold
+    layer.shown = layer.features['confidence'] > threshold
 
 
 viewer.window.add_dock_widget(confidence_slider)
