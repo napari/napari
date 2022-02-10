b658bca4c7fe3b5703d7bf4c0005ef3d80db7f58
Lorenzo Gaifas <brisvag@gmail.com>
2022-02-10 12:09:25 +0100

    Revert "temp changes to test ci"
    This reverts commit 6eb234f21bd703cc7a00ff3906e121c937efb70b.

--- napari/_vispy/visuals/vectors.py
+++ napari/_vispy/visuals/vectors.py
@@ -1,21 +1,20 @@
-# import warnings
+import warnings
 
 from ..utils.gl import get_gl_extensions
 from .clipping_planes_mixin import ClippingPlanesMixin
 
 GLPLUS = 'geometry_shader' in get_gl_extensions()
-# GLPLUS = False  # TODO: remove this
+# GLPLUS = False
 
 if GLPLUS:
     from .lines import Line
 else:
     from vispy.scene.visuals import Line as OldLine
 
-    # TODO uncomment this before merge, it's just to test ci works fine
-    # warnings.warn(
-    # 'Could not use GL+ backend; some Line functionality may be limited',
-    # ImportWarning,
-    # )
+    warnings.warn(
+        'Could not use GL+ backend; some Line functionality may be limited',
+        ImportWarning,
+    )
 
     class Line(OldLine):
         def __init__(self, **kwargs):
@@ -39,12 +38,10 @@ else:
 
         @scaling.setter
         def scaling(self, value):
-            # TODO uncomment this before merge, it's just to test ci works fine
-            # warnings.warn(
-            # 'Line width cannot be scaled with zoom without GL+ backend',
-            # RuntimeWarning,
-            # )
-            pass
+            warnings.warn(
+                'Line width cannot be scaled with zoom without GL+ backend',
+                RuntimeWarning,
+            )
 
 
 class VectorsVisual(ClippingPlanesMixin, Line):
