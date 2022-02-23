27b96b017cb6444f532d3e17d9f7457deb2fcbb4
Jaime RGP <jaimergp@users.noreply.github.com>
2022-02-23 12:06:26 +0100

    conditionally xfail test if pyside2 not found (e.g. pyqt as backend)

--- napari/_qt/qt_resources/_tests/test_icons.py
+++ napari/_qt/qt_resources/_tests/test_icons.py
@@ -1,3 +1,5 @@
+import pytest
+
 from napari.resources._icons import ICON_PATH, ICONS
 from napari.utils.misc import dir_hash, paths_hash
 
@@ -13,7 +15,11 @@ def test_pyside2_rcc_first():
     Ensure pyside2-rcc is checked before rcc. Otherwise
     we might use an older qt's rcc with no -g support.
     """
-    from napari._qt.qt_resources._icons import _find_pyside2_rcc
-
-    exe_name = next(exe for _, exe in _find_pyside2_rcc())
-    assert "pyside2-" in exe_name
+    try:
+        from napari._qt.qt_resources._icons import _find_pyside2_rcc
+        exe_name = next(exe for _, exe in _find_pyside2_rcc())
+        assert "pyside2-" in exe_name
+    except ModuleNotFoundError as exc:
+        if exc.name == "PySide2":
+            pytest.xfail("Test requires PySide2")
+        raise exc
