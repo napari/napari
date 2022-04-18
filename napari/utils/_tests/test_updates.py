a843d4920b51726ea38c729f4c5a02998ff694fc
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2022-04-18 17:01:09 -0500

    Add update utils and tests

---
+++ napari/utils/_tests/test_updates.py
@@ -0,0 +1,46 @@
+import pytest
+
+from napari.utils import updates
+
+
+def test_get_napari_pypi_versions():
+    versions = updates._get_napari_pypi_versions()
+    assert '0.4.15' in versions
+    assert len(versions) > 0
+
+
+def test_get_napari_conda_versions():
+    versions = updates._get_napari_pypi_versions()
+    assert len(versions) > 0
+    assert '0.4.15' in versions
+
+
+def test_get_installed_versions():
+    pass
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("0.4", True),
+        ("0.4.15", True),
+        ("0.4.15rc1", False),
+        ("0.4.15dev0", False),
+        ("0.4.15beta", False),
+        ("0.4.15alfa", False),
+        (('0', '4'), True),
+        (('0', '4', '15'), True),
+        (('0', '4', '15', 'rc1'), False),
+        (('0', '4', '15', 'beta'), False),
+    ]
+)
+def test_is_stable_version(test_input, expected):
+    assert updates._is_stable_version(test_input) == expected
+
+
+def test_is_dev():
+    assert updates.is_dev() is True
+
+
+def test_check_updates():
+    pass
--- napari/utils/updates.py
+++ napari/utils/updates.py
@@ -1,34 +1,128 @@
-"""Napari updates utilities."""
+"""Napari update utilities."""
 
-def _get_installed_versions():
-    """"""
-    # Check the current conda prefix for installed versions
-    return []
+import json
+import re
+import sys
+from pathlib import Path
+from typing import Literal, List, Tuple, Union, Dict
+from urllib import request
 
-
-def _check_pypi_updates():
-    """"""
-    # https://pypi.org/pypi/napari/json
-    pass
-
-
-def _check_conda_updates():
-    pass
-    # https://api.anaconda.org/package/conda-forge/napari
-    # Filter versions
+from napari.plugins.pypi import get_package_versions
+from napari.utils.misc import parse_version, running_as_constructor_app
 
 
-def check_updates(installer=None):
-    """Check for updates."""
-    # Ensure they are strings
+InstallerTypes = Literal['pip', 'conda']
+LETTERS_PATTERN = re.compile(r'[a-zA-Z]')
+
+
+def _get_napari_pypi_versions() -> List[str]:
+    """Get the versions of the napari pypi package."""
+    return get_package_versions('napari')
+
+
+def _get_napari_conda_versions():
+    """Get the versions of the napari conda package."""
+    data = {}
+    with request.urlopen('https://api.anaconda.org/package/conda-forge/napari') as response:
+        try:
+            data = json.loads(response.read().decode())
+        except Exception:
+            pass
+    return data.get('versions', [])
+
+
+def _get_installed_versions() -> List[str]:
+    """Check the current conda prefix for installed versions."""
+    versions = []
+    path = Path(sys.prefix)
+    envs_folder = path.parent
+    if envs_folder.parts[-1] == "envs":
+        # Check environment name is starts with napari
+        env_paths = [p for p in envs_folder.iterdir() if p.stem.rsplit('-')[0] == 'napari']
+        for env_path in env_paths:
+            for p in (envs_folder / env_path / 'conda-meta').iterdir():
+                if p.suffix == '.json':
+                    # Check environment contains a napari package
+                    parts = p.stem.rsplit('-')
+                    if len(parts) == 3 and parts[-3] == 'napari':
+                        versions.append(tuple(parts[1:]))
+    return versions
+
+
+def _is_stable_version(version: Union[Tuple[str], str]) -> bool:
+    """
+    Return ``True`` if version is stable.
+
+    Stable version examples: ``0.4.12``, ``0.4.1``.
+    Non-stable version examples: ``0.4.15beta``, ``0.4.15rc1``, ``0.4.15dev0``.
+    """
+    if not isinstance(version, tuple):
+        version = version.split('.')
+
+    return not LETTERS_PATTERN.search(version[-1])
+
+
+def is_dev() -> bool:
+    """Check if napari is running from development version."""
+    dev_check = False
     try:
         from napari._version import __version__
     except ImportError:
-        __version__ = None
+        dev_check = True
+    return dev_check
 
+
+def check_updates(stable: bool = True, installer: InstallerTypes = None) -> Dict:
+    """Check for updates.
+
+    Parameters
+    ----------
+    stable : bool, optional
+        If ``True``, check for stable versions. Default is ``True``.
+    installer : str, optional
+        Installer type. Default is ``None``.
+
+    Returns
+    -------
+    dict
+        Dictionary containing the current and latest versions, found
+        installed versions and the installer type used.
+    """
+    try:
+        from napari._version import __version__
+    except ImportError:
+        __version__ = 'dev'
+
+    if installer is None:
+        if is_dev():
+            versions = [__version__]
+            installer = 'dev'
+        elif running_as_constructor_app():
+            installer = 'conda'
+            versions = _get_napari_conda_versions()
+        else:
+            installer = 'pip'
+            versions = _get_napari_pypi_versions()
+    elif installer == 'pip':
+        versions = _get_napari_pypi_versions()
+    elif installer == 'conda':
+        versions = _get_napari_conda_versions()
+
+    if stable:
+        versions = list(filter(_is_stable_version, versions))
+
+    update = False
+    if __version__ != 'dev':
+        update = parse_version(latest_version) > parse_version(__version__)
+
+    latest_version = versions[-1]
     data = {
         "current": __version__,
-        "latest": "",
-        "installer": "bundle",
+        "latest": latest_version,
+        "found": _get_installed_versions(),
+        "installer": installer,
+        "update": update,
     }
     return data
+
+print(check_updates(installer='conda'))
