8a012822abe84ad38dabe2ffd33225dc2c4c607f
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-04-21 22:11:34 +0200

    forgot to commit this :)

---
+++ tools/setup_cfg_to_yaml.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+setup_cfg_block_to_yaml_list () {
+    # 1. use awk in "paragraph mode" (RS= ) to get the block of contiguous lines
+    #    that contains the pattern passed in the command line
+    # 2. then get the lines that start with 1+ spaces (section indentation);
+    #    these are the dependencies we need to install with conda
+    # 3. then replace the spaces with the expected list marker for the yaml file
+    awk -v RS= "/$1/" setup.cfg | # (1)
+        grep -E "^ +\w"         | # (2)
+        sed -r "s/^ +/- /g"       # (3)
+}
+
+setup_cfg_block_to_yaml_list $1
