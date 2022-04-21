e93c05c87294f537360bb5ce5068278c3c1daf90
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-04-21 04:09:59 +0200

    Rewrite ellipse discretization. (#4330)
    There was many problem with ellipse discretization, which was losing a
    lot of informations about the ellipse orientation in 3D.
    
    This almost completely rewrite it to be more general, as well as a bit
    more efficient in the generation of the triangulation indices.
    
    I'm not sure it is more readable (but it looks way more generic).
    
    Main difficulty in reading this code is that the shapes of objects
    (transforms), depends on Wether you are in 2 or 3D, but the math is the
    same.
    
    I've tried to heavily comment it for the next modification, as I'm
    pretty sure we will hit some degenerate cases.
    
    I've also modified one of the existing example that display ellipses
    in addition to points where the 4 corners would be in order to properly
    visually check the rendering it correct.
    
    See #4326 for some visualisation of incorrect rendering.
    
    The triangle index generation at the end is a bit hard to read:
    
    ```
    -    triangles = np.array([[0, i + 1, i + 2] for i in range(num_segments)])
    +    triangles = (
    +        np.arange(num_segments) + np.array([[0], [1], [2]])
    +    ).T * np.array([0, 1, 1])
    ```
    
    So I'm' happy to revert it, but it is about an order of magnitude faster
    than the Python list comprehension.

--- examples/add_points_on_nD_shapes.py
+++ examples/add_points_on_nD_shapes.py
@@ -10,8 +10,7 @@ import napari
 import numpy as np
 
 # Create rectangles in 4D
-shapes_data = np.array(
-    [
+data = [
         [
             [0, 50, 75, 75],
             [0, 50, 125, 75],
@@ -31,7 +30,8 @@ shapes_data = np.array(
             [1, 50, 75, 125]
         ]
     ]
-)
+
+shapes_data = np.array(data)
 
 # add an empty 4d points layer
 viewer = napari.view_points(ndim=4, size=3)
@@ -39,29 +39,30 @@ points_layer = viewer.layers[0]
 
 # add the shapes layer to the viewer
 features = {'index': [0, 1, 2]}
-shapes_layer = viewer.add_shapes(
-    shapes_data,
-    face_color=['magenta', 'green', 'blue'],
-    edge_color='white',
-    blending='additive',
-    features=features,
-    text='index'
-)
-
-
-@shapes_layer.mouse_drag_callbacks.append
-def on_click(layer, event):
-
-    shape_index, intersection_point = layer.get_index_and_intersection(
-        event.position,
-        event.view_direction,
-        event.dims_displayed
+for shape_type, mult in {('ellipse', 1), ('rectangle', -1)}:
+    shapes_layer = viewer.add_shapes(
+        shapes_data * mult,
+        face_color=['magenta', 'green', 'blue'],
+        edge_color='white',
+        blending='additive',
+        features=features,
+        text='index',
+        shape_type=shape_type,
     )
 
-    if (shape_index is not None) and (intersection_point is not None):
-        points_layer.add(intersection_point)
+    @shapes_layer.mouse_drag_callbacks.append
+    def on_click(layer, event):
+
+        shape_index, intersection_point = layer.get_index_and_intersection(
+            event.position, event.view_direction, event.dims_displayed
+        )
+
+        if (shape_index is not None) and (intersection_point is not None):
+            points_layer.add(intersection_point)
 
 
+for d in data:
+    viewer.add_points(np.array(d))
 # set the viewer to 3D rendering mode with the first two rectangles in view
 viewer.dims.ndisplay = 3
 viewer.dims.set_point(axis=0, value=0)
