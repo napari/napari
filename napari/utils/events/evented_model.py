bd264dab78917c51ff2373c4bf6ae3848b213b77
Lorenzo Gaifas <brisvag@gmail.com>
2022-04-29 11:26:14 +0200

    merge-squash feature/evented-properties2

--- napari/utils/events/evented_model.py
+++ napari/utils/events/evented_model.py
@@ -198,13 +198,22 @@ class EventedModel(BaseModel, metaclass=EventedMetaclass):
 
         self._events.source = self
         # add event emitters for each field which is mutable
-        event_names = [
-            name
-            for name, field in self.__fields__.items()
-            if field.field_info.allow_mutation
-        ]
-        event_names.extend(self.__property_setters__)
-        self._events.add(**dict.fromkeys(event_names))
+        field_events = []
+        for name, field in self.__fields__.items():
+            value = getattr(self, name)
+            if field.field_info.allow_mutation or hasattr(value, 'events'):
+                field_events.append(name)
+
+        self._events.add(
+            **dict.fromkeys(field_events + list(self.__property_setters__))
+        )
+
+        # hook up events from children
+        for name in field_events:
+            value = getattr(self, name)
+            if hasattr(value, 'events'):
+                # TODO: won't track source all the way in?
+                value.events.connect(getattr(self.events, name))
 
     def _super_setattr_(self, name: str, value: Any) -> None:
         # pydantic will raise a ValueError if extra fields are not allowed
