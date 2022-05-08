27afddd97ff4846ad0fd0871269193169cbd4711
Alister Burt <alisterburt@gmail.com>
2022-05-07 17:45:14 -0700

    fix some get value tests...

--- napari/layers/_tests/test_layer_attributes.py
+++ napari/layers/_tests/test_layer_attributes.py
@@ -73,13 +73,13 @@ def _check_subpixel_values(layer, val_dict):
                         view_direction = np.asarray([1.0, 0, 0])
                         dims_displayed = [0, 1, 2]
 
-                val = layer.get_value(
+                response = layer.get_value(
                     position=position,
                     view_direction=view_direction,
                     dims_displayed=dims_displayed,
                     world=False,
                 )
-                assert val == expected_value
+                assert response.value == expected_value
 
 
 @pytest.mark.parametrize('ImageClass', [Image, Labels])
