00eaca54f2727456f6922796482bc6243789b649
Pam Wadhwa <pepainter@gmail.com>
2022-05-13 17:02:46 -0500

    clean up.  make update based on magic gui version

--- napari/_qt/widgets/magic_gui_base_widget.py
+++ napari/_qt/widgets/magic_gui_base_widget.py
@@ -13,15 +13,24 @@ class BaseMagicSetting(QObject):
         super().__init__()
         self._widget = self.__class__.MAGIC_GUI
         self._description = description
+
+        def _call_magic_gui(magic_gui):
+            try:
+                return magic_gui()
+            except TypeError:
+                # Older version of magic gui.
+                return magic_gui.value()
+
         self._widget.changed.connect(
-            lambda _: self.valueChanged.emit(_.value())
+            lambda magic_gui: self.valueChanged.emit(
+                _call_magic_gui(magic_gui)
+            )
         )
 
     def __getattr__(self, attribute):
         """Method that will retrieve needed information
         from the actual widget if not found in the class itself.
         """
-
         try:
             return self.__getattribute__(attribute)
         except AttributeError:
@@ -37,9 +46,7 @@ class BaseMagicSetting(QObject):
         ----------
         value: str
             Description for widget.
-
         """
-
         self._description = value
 
     def setToolTip(self, value):
@@ -52,8 +59,6 @@ class BaseMagicSetting(QObject):
         ----------
         value: str
             Tooltip for widget.
-
         """
-
-        for wdg in self._widget._list:
-            wdg.tooltip = value
+        for widget in self._widget._list:
+            widget.tooltip = value
