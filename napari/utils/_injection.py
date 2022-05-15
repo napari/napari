ecb4e6a1e60e950b9ea25c2137580e06a098c0aa
Talley Lambert <talley.lambert@gmail.com>
2022-05-15 18:38:02 -0400

    add setter

--- napari/utils/_injection.py
+++ napari/utils/_injection.py
@@ -5,30 +5,31 @@ from typing import Any, Callable, Dict, Optional, Type, TypeVar
 from typing_extensions import get_type_hints
 
 from .. import components, layers, viewer
+from ..viewer import current_viewer
 
 T = TypeVar("T")
+_NULL = object()
 
 
 def _get_active_layer() -> Optional[layers.Layer]:
-    if cur_viewer := viewer.current_viewer():
-        return cur_viewer.layers.selection.active
-    return None
+    return v.layers.selection.active if (v := current_viewer()) else None
 
 
 def _get_active_layer_list() -> Optional[components.LayerList]:
-    if cur_viewer := viewer.current_viewer():
-        return cur_viewer.layers
-    return None
+    return v.layers if (v := current_viewer()) else None
 
 
-_ACCESSORS: Dict[Type, Callable[[], Optional[object]]] = {
+# registry of Type -> "accessor function"
+# where each value is a function that is capable
+# of retrieving an instance of it's corresponding key type.
+_ACCESSORS: Dict[Type, Callable[..., Optional[object]]] = {
     layers.Layer: _get_active_layer,
-    viewer.Viewer: viewer.current_viewer,
+    viewer.Viewer: current_viewer,
     components.LayerList: _get_active_layer_list,
 }
 
 
-def get_accessor(type_: Type[T]) -> Optional[Callable[[], Optional[T]]]:
+def get_accessor(type_: Type[T]) -> Optional[Callable[..., Optional[T]]]:
     """Return object accessor function given a type.
 
     An object accessor is a function that returns an instance of a
@@ -39,11 +40,62 @@ def get_accessor(type_: Type[T]) -> Optional[Callable[[], Optional[T]]]:
     `inject_napari_dependencies`, allows us to inject current napari objects
     into functions based on type hints.
     """
+    if type_ in _ACCESSORS:
+        return _ACCESSORS[type_]
+
     if isinstance(type_, type):
         for key, val in _ACCESSORS.items():
             if issubclass(type, key):
                 return val  # type: ignore [return-type]
     return None
+
+
+class set_accessor:
+    """Set accessor(s) for given type(s).
+
+    "Acessors" are functions that can retrieve an instance of a given type.
+    For instance, `napari.viewer.current_viewer` is a function that can
+    retrieve an instance of `napari.Viewer`.
+
+    This is a class that behaves as a function or a context manager, that
+    allows one to set an accessor function for a given type.
+
+    Parameters
+    ----------
+    mapping : Dict[Type[T], Callable[..., Optional[T]]]
+        a map of type -> accessor function, where each value is a function
+        that is capable of retrieving an instance of the associated key/type.
+    clobber : bool, optional
+        Whether to override any existing accessor function, by default True.
+
+    Raises
+    ------
+    ValueError
+        if clobber is `True` and one of the keys in `mapping` is already
+        registered.
+    """
+
+    def __init__(
+        self, mapping: Dict[Type[T], Callable[..., Optional[T]]], clobber=True
+    ):
+        self._before = {}
+        for k, v in mapping.items():
+            if k in _ACCESSORS and clobber is False:
+                raise ValueError(
+                    f"Class {k} already has an accessor and clobber is False"
+                )
+            self._before[k] = _ACCESSORS.get(k, _NULL)
+        _ACCESSORS.update(mapping)
+
+    def __enter__(self):
+        return None
+
+    def __exit__(self, *_):
+        for key, val in self._before.items():
+            if val is _NULL:
+                del _ACCESSORS[key]
+            else:
+                _ACCESSORS[key] = val
 
 
 def napari_type_hints(obj: Any) -> Dict[str, Any]:
@@ -97,18 +149,16 @@ def inject_napari_dependencies(func: Callable) -> Callable:
     # get type hints for the object, with forward refs of napari hints resolved
     hints = napari_type_hints(func)
     # get accessor functions for each required parameter
-    accessors = {}
+    required = {}
     for name, hint in hints.items():
         if sig.parameters[name].default is sig.empty:
-            accessor = get_accessor(hint)
-            if accessor:
-                accessors[name] = accessor
+            required[name] = hint
 
     @wraps(func)
     def _exec(*args, **kwargs):
         # when we call the function, we call the accessor functions to get
         # the current napari objects
-        _kwargs = {k: accessor() for k, accessor in accessors.items()}
+        _kwargs = {n: get_accessor(hint)() for n, hint in required.items()}
         # but we use bind_partial to allow the caller to still provide
         # their own objects if desired.
         # (i.e. the injected deps are only used if needed)
@@ -129,7 +179,7 @@ def inject_napari_dependencies(func: Callable) -> Callable:
     # update the signature
     p = [
         p.replace(default=None, annotation=Optional[hints[p.name]])
-        if p.name in accessors and p.default is p.empty
+        if p.name in required
         else p
         for p in sig.parameters.values()
     ]
