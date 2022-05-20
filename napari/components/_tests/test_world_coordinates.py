154cd0cd2c63cd7299e9a0a844ebb6f7bf936760
Lorenzo Gaifas <brisvag@gmail.com>
2022-05-20 14:45:02 +0200

    some more test fixes

--- napari/components/_tests/test_world_coordinates.py
+++ napari/components/_tests/test_world_coordinates.py
@@ -13,13 +13,13 @@ def test_translated_images():
     data = np.random.random((10, 10, 10))
     viewer.add_image(data)
     viewer.add_image(data, translate=[10, 0, 0])
-    assert viewer.dims.range[0] == (0, 20, 1)
-    assert viewer.dims.range[1] == (0, 10, 1)
-    assert viewer.dims.range[2] == (0, 10, 1)
-    assert viewer.dims.nsteps == (20, 10, 10)
+    assert viewer.dims.range[0] == [0, 20]
+    assert viewer.dims.range[1] == [0, 10]
+    assert viewer.dims.range[2] == [0, 10]
+    assert viewer.dims.nsteps == [20, 10, 10]
     for i in range(viewer.dims.nsteps[0]):
-        viewer.dims.set_current_step(0, i)
-        assert viewer.dims.current_step[0] == i
+        viewer.dims._point_step[0] = i
+        assert viewer.dims._point_step[0] == i
 
 
 def test_scaled_images():
@@ -29,17 +29,16 @@ def test_scaled_images():
     data = np.random.random((10, 10, 10))
     viewer.add_image(data)
     viewer.add_image(data[::2], scale=[2, 1, 1])
-    assert viewer.dims.range[0] == (
+    assert viewer.dims.range[0] == [
         -0.5,
         10,
-        1,
-    )  # TODO: non-integer with mixed scale?
-    assert viewer.dims.range[1] == (0, 10, 1)
-    assert viewer.dims.range[2] == (0, 10, 1)
-    assert viewer.dims.nsteps == (10, 10, 10)
+    ]  # TODO: non-integer with mixed scale?
+    assert viewer.dims.range[1] == [0, 10]
+    assert viewer.dims.range[2] == [0, 10]
+    assert viewer.dims.nsteps == [10, 10, 10]
     for i in range(viewer.dims.nsteps[0]):
-        viewer.dims.set_current_step(0, i)
-        assert viewer.dims.current_step[0] == i
+        viewer.dims._point_step[0] = i
+        assert viewer.dims._point_step[0] == i
 
 
 def test_scaled_and_translated_images():
@@ -49,17 +48,16 @@ def test_scaled_and_translated_images():
     data = np.random.random((10, 10, 10))
     viewer.add_image(data)
     viewer.add_image(data[::2], scale=[2, 1, 1], translate=[10, 0, 0])
-    assert viewer.dims.range[0] == (
+    assert viewer.dims.range[0] == [
         0,
         19.5,
-        1,
-    )  # TODO: non-integer with mixed scale?
-    assert viewer.dims.range[1] == (0, 10, 1)
-    assert viewer.dims.range[2] == (0, 10, 1)
-    assert viewer.dims.nsteps == (19, 10, 10)
+    ]  # TODO: non-integer with mixed scale?
+    assert viewer.dims.range[1] == [0, 10]
+    assert viewer.dims.range[2] == [0, 10]
+    assert viewer.dims.nsteps == [19, 10, 10]
     for i in range(viewer.dims.nsteps[0]):
-        viewer.dims.set_current_step(0, i)
-        assert viewer.dims.current_step[0] == i
+        viewer.dims._point_step[0] = i
+        assert viewer.dims._point_step[0] == i
 
 
 def test_both_scaled_and_translated_images():
@@ -69,13 +67,13 @@ def test_both_scaled_and_translated_images():
     data = np.random.random((10, 10, 10))
     viewer.add_image(data, scale=[2, 1, 1])
     viewer.add_image(data, scale=[2, 1, 1], translate=[20, 0, 0])
-    assert viewer.dims.range[0] == (0, 40, 2)
-    assert viewer.dims.range[1] == (0, 10, 1)
-    assert viewer.dims.range[2] == (0, 10, 1)
-    assert viewer.dims.nsteps == (20, 10, 10)
+    assert viewer.dims.range[0] == [0, 40]
+    assert viewer.dims.range[1] == [0, 10]
+    assert viewer.dims.range[2] == [0, 10]
+    assert viewer.dims.nsteps == [20, 10, 10]
     for i in range(viewer.dims.nsteps[0]):
-        viewer.dims.set_current_step(0, i)
-        assert viewer.dims.current_step[0] == i
+        viewer.dims._point_step[0] = i
+        assert viewer.dims._point_step[0] == i
 
 
 def test_no_warning_non_affine_slicing():
