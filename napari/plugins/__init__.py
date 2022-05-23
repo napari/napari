76eb4112df1c5ee4fd0dc98fa1577f9596f74811
Talley Lambert <talley.lambert@gmail.com>
2022-05-23 13:41:23 -0400

    don't use mock

--- napari/plugins/__init__.py
+++ napari/plugins/__init__.py
@@ -7,19 +7,18 @@ from npe2 import PluginManifest
 
 from ..settings import get_settings
 from . import _npe2
+from ._plugin_manager import NapariPluginManager
 
 __all__ = ["plugin_manager", "menu_item_template"]
 
 #: Template to use for namespacing a plugin item in the menu bar
 menu_item_template = '{}: {}'
 # the main plugin manager instance for the `napari` plugin namespace.
-plugin_manager = None
+plugin_manager = NapariPluginManager()
 
 
 @lru_cache  # only call once
 def _initialize_plugins():
-    global plugin_manager
-
     _npe2pm = _PluginManager.instance()
 
     settings = get_settings()
@@ -42,26 +41,18 @@ def _initialize_plugins():
         mf.package_metadata = PackageMetadata.for_package('napari')
         _npe2pm.register(mf)
 
-    # if we're using the npe2 "shim" adaptor, we don't use npe1 at all
-    # we just make the plugin_manager a do-nothing mock object.
-    # In a later version, we can make the npe2 adapter always-on and remove
-    # the npe1 plugin_manager altogether.
-    if settings.plugins.use_npe2_adaptor:
-        from unittest.mock import MagicMock
-
-        plugin_manager = MagicMock()
-        return
-
-    from ._plugin_manager import NapariPluginManager
-
-    plugin_manager = NapariPluginManager()
     # Disable plugins listed as disabled in settings, or detected in npe2
     _from_npe2 = {m.name for m in _npe2pm.iter_manifests()}
     _from_npe2.add('napari')
     plugin_manager._skip_packages = _from_npe2
-
     plugin_manager._blocked.update(settings.plugins.disabled_plugins)
-    plugin_manager._initialize()
+
+    if settings.plugins.use_npe2_adaptor:
+        # prevent npe1 plugin_manager discovery
+        # (this doesn't prevent manual registration)
+        plugin_manager.discover = lambda *a, **k: None
+    else:
+        plugin_manager._initialize()
 
 
 _initialize_plugins()
