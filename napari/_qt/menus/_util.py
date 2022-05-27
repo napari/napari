d45e3ad15ab9981e124c2c9247789a7d0536ef7a
Grzegorz Bokota <bokota+github@gmail.com>
2022-05-27 17:59:33 +0200

    fix bug in menus for PySide

--- napari/_qt/menus/_util.py
+++ napari/_qt/menus/_util.py
@@ -87,7 +87,7 @@ def populate_menu(menu: QMenu, actions: List['MenuItem']):
             continue
         action: QAction = menu.addAction(ax['text'])
         if 'slot' in ax:
-            action.triggered.connect(ax['slot'])
+            action.triggered[bool].connect(ax['slot'])
         action.setShortcut(ax.get('shortcut', ''))
         action.setStatusTip(ax.get('statusTip', ''))
         if 'menuRole' in ax:
