abeac8c233ce06cfce2c8d902f79c3f7b28c78af
Pam Wadhwa <pepainter@gmail.com>
2022-05-27 23:09:16 -0500

    make update so test would pass

--- napari/_qt/qt_viewer.py
+++ napari/_qt/qt_viewer.py
@@ -299,25 +299,22 @@ class QtViewer(QSplitter):
         settings = get_settings()
         self._update_dask_settings(settings.application.dask)
 
-        settings.application.events.dask.connect(
-            lambda dask_event: self._update_dask_settings(dask_event.value)
-        )
+        settings.application.events.dask.connect(self._update_dask_settings)
 
         settings.application.events.dask.connect(self._update_dask_settings)
 
         for layer in self.viewer.layers:
             self._add_layer(layer)
 
-    def _update_dask_settings(self, dask_setting: dict):
+    def _update_dask_settings(self, dask_setting=None):
         """Update dask cache to match settings."""
+        if dask_setting:
+            if not isinstance(dask_setting, dict):
+                dask_setting = dask_setting.value
 
-        if dask_setting['enabled']:
-            # If dask is enabled, then resize cache.
-            # Value is in mb, need to convert to bytes.
-            resize_dask_cache(dask_setting['cache'] * 1e6)
-        else:
-            # Disable dask.
-            resize_dask_cache(0)
+            enabled = dask_setting['enabled']
+            size = dask_setting['cache']
+            resize_dask_cache(int(enabled) * size * 1e6)
 
     @property
     def controls(self) -> QtLayerControlsContainer:
