0263eb92100f59810874c12f56dac0221bf8969a
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-06-03 15:54:57 +0200

    add some more comments
    Co-authored-by: andy-sweet <andy-sweet@users.noreply.github.com>

--- napari/benchmarks/benchmark_text_manager.py
+++ napari/benchmarks/benchmark_text_manager.py
@@ -57,7 +57,9 @@ class TextManagerSuite:
     #   IndexError: index 32768 is out of bounds for axis 0 with size 32768
     #
     # Why? ASV will run the same function after setup several times in two
-    # occassions: warmup and timing itself. We disable warmup and only
+    # occasions: warmup and timing itself. We disable warmup and only
     # allow one execution per state with these method-specific options:
     time_remove_as_batch.number = 1
     time_remove_as_batch.warmup_time = 0
+    # See https://asv.readthedocs.io/en/stable/benchmarks.html#timing-benchmarks
+    # for more details
