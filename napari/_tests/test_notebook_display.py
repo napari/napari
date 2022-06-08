f8ad414b09099cef87319ea11f58246b8cfb6153
Talley Lambert <talley.lambert@gmail.com>
2022-06-07 23:13:13 -0400

    test: speed up notebook test by not using viewer to check alt text

--- napari/_tests/test_notebook_display.py
+++ napari/_tests/test_notebook_display.py
@@ -1,7 +1,24 @@
+from unittest.mock import Mock
 import numpy as np
 import pytest
-
+import html
 from napari.utils import nbscreenshot
+
+
+def test_nbscreenshot(make_napari_viewer):
+    """Test taking a screenshot."""
+    viewer = make_napari_viewer()
+
+    np.random.seed(0)
+    data = np.random.random((10, 15))
+    viewer.add_image(data)
+
+    rich_display_object = nbscreenshot(viewer)
+    assert hasattr(rich_display_object, '_repr_png_')
+    # Trigger method that would run in jupyter notebook cell automatically
+    rich_display_object._repr_png_()
+    assert rich_display_object.image is not None
+
 
 
 @pytest.mark.parametrize(
@@ -11,11 +28,8 @@ from napari.utils import nbscreenshot
         ("Good alt text", "Good alt text"),
         # Naughty strings https://github.com/minimaxir/big-list-of-naughty-strings
         # ASCII punctuation
-        (
-            r",./;'[]\-=",
-            ',./;&#x27;[]\\-=',
-        ),  # noqa: W605
-        ('<>?:"{}|_+', '&lt;&gt;?:&quot;{}|_+'),  # ASCII punctuation 2
+        (r",./;'[]\-=", ',./;&#x27;[]\\-='),  # noqa: W605
+        ('>?:"{}|_+', '&gt;?:&quot;{}|_+'),  # ASCII punctuation 2
         ("!@#$%^&*()`~", '!@#$%^&amp;*()`~'),  # ASCII punctuation 3
         # # Emjoi
         ("😍", "😍"),  # emoji 1
@@ -29,27 +43,11 @@ from napari.utils import nbscreenshot
         ("<script>alert(0)</script>", None),  # script injection 1
         ("&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;", None),
         ("<svg><script>123<1>alert(3)</script>", None),
-        ("<sc<script>ript>alert(13)</sc</script>ript>", None),
     ],
 )
-def test_nbscreenshot(make_napari_viewer, alt_text_input, expected_alt_text):
-    """Test taking a screenshot."""
-    viewer = make_napari_viewer()
-
-    np.random.seed(0)
-    data = np.random.random((10, 15))
-    viewer.add_image(data)
-
-    rich_display_object = nbscreenshot(viewer, alt_text=alt_text_input)
-    assert hasattr(rich_display_object, '_repr_png_')
-    # Trigger method that would run in jupyter notebook cell automatically
-    rich_display_object._repr_png_()
-    assert rich_display_object.image is not None
-
-    html_output = rich_display_object._repr_html_()
-
-    if expected_alt_text is None:
-        assert 'alt_text=' not in html_output
+def test_safe_alt_text(alt_text_input, expected_alt_text):
+    display_obj = nbscreenshot(Mock(), alt_text=alt_text_input)
+    if not expected_alt_text:
+        assert not display_obj.alt_text
     else:
-        expected_output = 'alt="' + str(expected_alt_text) + '"'
-        assert expected_output in html_output
+        assert html.escape(display_obj.alt_text) == expected_alt_text
