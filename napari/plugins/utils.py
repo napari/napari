a2fe9e46066024b3aaa0bf2bb45420b6ced42ae5
Talley Lambert <talley.lambert@gmail.com>
2022-06-10 17:24:15 -0400

    simplify get_potential_readers

--- napari/plugins/utils.py
+++ napari/plugins/utils.py
@@ -31,21 +31,13 @@ def get_potential_readers(filename: str) -> Dict[str, str]:
     Dict[str, str]
         dictionary of registered name to display_name
     """
-    readers = _npe2.get_readers(filename)
-    npe1_readers = {}
-    for spec, hook_caller in plugin_manager.hooks.items():
-        if spec == 'napari_get_reader':
-            potential_readers = hook_caller.get_hookimpls()
-            for get_reader in potential_readers:
-                reader = hook_caller._call_plugin(
-                    get_reader.plugin_name, path=filename
-                )
-                if callable(reader):
-                    npe1_readers[
-                        get_reader.plugin_name
-                    ] = get_reader.plugin_name
-    readers.update(npe1_readers)
-
+    readers = {}
+    hook_caller = plugin_manager.hook.napari_get_reader
+    for impl in hook_caller.get_hookimpls():
+        reader = hook_caller._call_plugin(impl.plugin_name, path=filename)
+        if callable(reader):
+            readers[impl.plugin_name] = impl.plugin_name
+    readers.update(_npe2.get_readers(filename))
     return readers
 
 
