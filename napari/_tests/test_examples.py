12e763c471a345a5e7072ef4fd0daf77eda8ff80
Grzegorz Bokota <bokota+github@gmail.com>
2022-06-11 15:38:50 +0200

    Move test examples on end of test run them in different CI job  (#4647)
    * move test examples on end
    
    * allow test because of test order
    
    * skip examples by marker
    
    * exclude examples from test run
    
    * fix tox after merge
    
    * fix conf
    
    * fix failing test
    
    * proper test order on windows
    
    * Apply suggestions from code review
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- napari/_tests/test_examples.py
+++ napari/_tests/test_examples.py
@@ -20,7 +20,6 @@ skip = [
     'live_tiffs_generator_.py',
     'points-over-time.py',  # too resource hungry
     'embed_ipython_.py',  # fails without monkeypatch
-    'custom_key_bindings.py',  # breaks EXPECTED_NUMBER_OF_VIEWER_METHODS later
     'new_theme.py',  # testing theme is extremely slow on CI
     'dynamic-projections-dask.py',  # extremely slow / does not finish
 ]
@@ -38,6 +37,7 @@ if os.getenv("CI") and os.name == 'nt' and 'to_screenshot.py' in examples:
     examples.remove('to_screenshot.py')
 
 
+@pytest.mark.examples
 @pytest.mark.filterwarnings("ignore")
 @pytest.mark.skipif(not examples, reason="No examples were found.")
 @pytest.mark.parametrize("fname", examples)
