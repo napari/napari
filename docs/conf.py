270f2b0a9d946a7bf1df3af0429f1971772968aa
Talley Lambert <talley.lambert@gmail.com>
2022-06-17 08:48:58 -0400

    fix conf again

--- docs/conf.py
+++ docs/conf.py
@@ -151,7 +151,9 @@ sphinx_gallery_conf = {
     'gallery_dirs': 'gallery',  # path to where to save gallery generated output
     'filename_pattern': '/*.py',
     'ignore_pattern': 'README.rst|/*_.py',
-    'default_thumb_file': Path(napari.__file__) / 'resources' / 'logo.png',
+    'default_thumb_file': Path(napari.__file__).parent
+    / 'resources'
+    / 'logo.png',
     'plot_gallery': True,
     'download_all_examples': False,
     'min_reported_time': 10,
