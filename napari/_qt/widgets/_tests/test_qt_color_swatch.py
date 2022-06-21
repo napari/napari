4e50e2704e9ccde043c23c39540ea4a529ea5073
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2022-06-21 14:50:10 +0200

    Check if classes are properly initialized

--- napari/_qt/widgets/_tests/test_qt_color_swatch.py
+++ napari/_qt/widgets/_tests/test_qt_color_swatch.py
@@ -1,28 +1,46 @@
+import numpy
 import pytest
-from qtpy.QtWidgets import QWidget
 
-from napari._qt.widgets.qt_color_swatch import QColorSwatch, QColorSwatchEdit
+from napari._qt.widgets.qt_color_swatch import (
+    TRANSPARENT,
+    QColorSwatch,
+    QColorSwatchEdit,
+)
 
 
-@pytest.mark.parametrize('parent', [None, True])
 @pytest.mark.parametrize('color', [None, [1, 1, 1, 1]])
 @pytest.mark.parametrize('tooltip', [None, 'This is a test'])
-def test_succesfull_create_qcolorswatchedit(qtbot, parent, color, tooltip):
-    if parent:
-        parent = QWidget()
-        qtbot.add_widget(parent)
-    qtbot.add_widget(
-        QColorSwatchEdit(parent, initial_color=color, tooltip=tooltip)
-    )
+def test_succesfull_create_qcolorswatchedit(qtbot, color, tooltip):
+    widget = QColorSwatchEdit(initial_color=color, tooltip=tooltip)
+    qtbot.add_widget(widget)
+    if color is None:
+        test_color = TRANSPARENT
+    else:
+        test_color = color
+
+    if tooltip is None:
+        test_tooltip = 'click to set color'
+    else:
+        test_tooltip = tooltip
+
+    assert widget.color_swatch.toolTip() == test_tooltip
+    numpy.testing.assert_array_equal(widget.color, test_color)
 
 
-@pytest.mark.parametrize('parent', [None, True])
 @pytest.mark.parametrize('color', [None, [1, 1, 1, 1]])
 @pytest.mark.parametrize('tooltip', [None, 'This is a test'])
-def test_succesfull_create_qcolorswatch(qtbot, parent, color, tooltip):
-    if parent:
-        parent = QWidget()
-        qtbot.add_widget(parent)
-    qtbot.add_widget(
-        QColorSwatch(parent, initial_color=color, tooltip=tooltip)
-    )
+def test_succesfull_create_qcolorswatch(qtbot, color, tooltip):
+    widget = QColorSwatch(initial_color=color, tooltip=tooltip)
+    qtbot.add_widget(widget)
+    if color is None:
+        test_color = TRANSPARENT
+    else:
+        test_color = color
+
+    if tooltip is None:
+        test_tooltip = 'click to set color'
+    else:
+        test_tooltip = tooltip
+
+    assert widget.toolTip() == test_tooltip
+    numpy.testing.assert_array_equal(widget.color, test_color)
