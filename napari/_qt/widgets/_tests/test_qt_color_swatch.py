32e3f786aca3ea1fc299d78217615c61e14810a7
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2022-06-21 12:28:42 +0200

    Change inner loop to pytest.mark.parametrize

--- napari/_qt/widgets/_tests/test_qt_color_swatch.py
+++ napari/_qt/widgets/_tests/test_qt_color_swatch.py
@@ -1,41 +1,28 @@
-import itertools
-
+import pytest
 from qtpy.QtWidgets import QWidget
 
 from napari._qt.widgets.qt_color_swatch import QColorSwatch, QColorSwatchEdit
 
 
-def test_succesfull_create_qcolorswatchedit(qtbot):
-    parents = [
-        None,
-        QWidget(),
-    ]
-    colors = [
-        None,
-        [1, 1, 1, 1],
-    ]
-    tooltips = [
-        None,
-        'This is a test',
-    ]
-
-    for parent, color, tooltip in itertools.product(parents, colors, tooltips):
+@pytest.mark.parametrize('parent', [None, True])
+@pytest.mark.parametrize('color', [None, [1, 1, 1, 1]])
+@pytest.mark.parametrize('tooltip', [None, 'This is a test'])
+def test_succesfull_create_qcolorswatchedit(qtbot, parent, color, tooltip):
+    if parent:
+        parent = QWidget()
+        qtbot.add_widget(parent)
+    qtbot.add_widget(
         QColorSwatchEdit(parent, initial_color=color, tooltip=tooltip)
+    )
 
 
-def test_succesfull_create_qcolorswatch(qtbot):
-    parents = [
-        None,
-        QWidget(),
-    ]
-    colors = [
-        None,
-        [1, 1, 1, 1],
-    ]
-    tooltips = [
-        None,
-        'This is a test',
-    ]
-
-    for parent, color, tooltip in itertools.product(parents, colors, tooltips):
+@pytest.mark.parametrize('parent', [None, True])
+@pytest.mark.parametrize('color', [None, [1, 1, 1, 1]])
+@pytest.mark.parametrize('tooltip', [None, 'This is a test'])
+def test_succesfull_create_qcolorswatch(qtbot, parent, color, tooltip):
+    if parent:
+        parent = QWidget()
+        qtbot.add_widget(parent)
+    qtbot.add_widget(
         QColorSwatch(parent, initial_color=color, tooltip=tooltip)
+    )
