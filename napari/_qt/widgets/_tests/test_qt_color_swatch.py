5584ac87b585cbdbe3685e274aa55c1860a6f771
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2022-06-21 12:06:03 +0200

    Add test covering the tooltip bug

---
+++ napari/_qt/widgets/_tests/test_qt_color_swatch.py
@@ -0,0 +1,23 @@
+import itertools
+
+from qtpy.QtWidgets import QWidget
+
+from napari._qt.widgets.qt_color_swatch import QColorSwatchEdit
+
+
+def test_succesfull_create_qcolorswatchedit(qtbot):
+    parents = [
+        None,
+        QWidget(),
+    ]
+    colors = [
+        None,
+        [1, 1, 1, 1],
+    ]
+    tooltips = [
+        None,
+        'This is a test',
+    ]
+
+    for parent, color, tooltip in itertools.product(parents, colors, tooltips):
+        QColorSwatchEdit(parent, initial_color=color, tooltip=tooltip)
