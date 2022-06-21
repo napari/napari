2896fd076760a671027199fb75f8d993f0a41754
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2022-06-21 12:10:37 +0200

    Add a new test for QColorSwatch

--- napari/_qt/widgets/_tests/test_qt_color_swatch.py
+++ napari/_qt/widgets/_tests/test_qt_color_swatch.py
@@ -2,7 +2,7 @@ import itertools
 
 from qtpy.QtWidgets import QWidget
 
-from napari._qt.widgets.qt_color_swatch import QColorSwatchEdit
+from napari._qt.widgets.qt_color_swatch import QColorSwatch, QColorSwatchEdit
 
 
 def test_succesfull_create_qcolorswatchedit(qtbot):
@@ -21,3 +21,21 @@ def test_succesfull_create_qcolorswatchedit(qtbot):
 
     for parent, color, tooltip in itertools.product(parents, colors, tooltips):
         QColorSwatchEdit(parent, initial_color=color, tooltip=tooltip)
+
+
+def test_succesfull_create_qcolorswatch(qtbot):
+    parents = [
+        None,
+        QWidget(),
+    ]
+    colors = [
+        None,
+        [1, 1, 1, 1],
+    ]
+    tooltips = [
+        None,
+        'This is a test',
+    ]
+
+    for parent, color, tooltip in itertools.product(parents, colors, tooltips):
+        QColorSwatch(parent, initial_color=color, tooltip=tooltip)
