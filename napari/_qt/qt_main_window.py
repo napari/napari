5a007cc8609b5ab6bd5b0a376e7d94acfcc03582
caronsso <constantin.aronssohn@inria.fr>
2022-06-24 11:43:33 +0200

    Add test and fix typo

--- napari/_qt/qt_main_window.py
+++ napari/_qt/qt_main_window.py
@@ -1059,7 +1059,7 @@ class Window:
             Height of the rectangle shape of the window.
         """
         rect = self._qt_window.geometry()
-        return rext.left(), rect.top(), rect.width(), rect.height()
+        return rect.left(), rect.top(), rect.width(), rect.height()
 
     def show(self, *, block=False):
         """Resize, show, and bring forward the window.
