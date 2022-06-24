05b2105284d330d89be66f0f77b738cd7f609c80
Talley Lambert <talley.lambert@gmail.com>
2022-06-23 22:49:43 -0400

    merge in deepcopy fix

--- napari/utils/context/__init__.py
+++ napari/utils/context/__init__.py
@@ -1,13 +1,12 @@
 from ._context import Context, create_context, get_context
-from ._context_keys import ContextKey
-from ._expressions import Expr
+from ._expressions import Expr, parse_expression
 from ._layerlist_context import LayerListContextKeys
 
 __all__ = [
     'Context',
-    'Expr',
-    'ContextKey',
     'create_context',
+    'Expr',
     'get_context',
     'LayerListContextKeys',
+    'parse_expression',
 ]
