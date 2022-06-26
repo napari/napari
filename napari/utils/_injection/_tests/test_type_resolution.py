2c2b0471d670998edb55f7cce7e63514b7cc4ee4
Talley Lambert <talley.lambert@gmail.com>
2022-06-26 12:56:48 -0400

    make new type resolution module

---
+++ napari/utils/_injection/_tests/test_type_resolution.py
@@ -0,0 +1,106 @@
+import types
+from typing import Any, Callable, Optional, Sequence
+
+import pytest
+
+from napari.utils._injection import (
+    resolve_single_type_hints,
+    resolve_type_hints,
+    type_resolved_signature,
+)
+
+
+def basic_sig(a: 'int', b: 'str', c: 'Optional[float]' = None) -> int:
+    ...
+
+
+def uses_napari(image: 'Image', layers: 'LayerList') -> 'Labels':  # type: ignore # noqa
+    ...
+
+
+def requires_unknown(param: 'Unknown', x) -> 'Unknown':  # type: ignore # noqa
+    ...
+
+
+def optional_unknown(param: 'Unknown' = 1) -> 'Unknown':  # type: ignore # noqa
+    ...
+
+
+def test_resolve_type_hints():
+    with pytest.raises(NameError, match='not defined'):
+        resolve_type_hints(uses_napari, inject_napari_namespace=False)
+    with pytest.raises(NameError):
+        resolve_type_hints(requires_unknown)
+
+    hints = resolve_type_hints(uses_napari, inject_napari_namespace=True)
+    from napari import layers
+
+    assert hints['image'] == layers.Image
+
+    hints = resolve_type_hints(basic_sig, inject_napari_namespace=False)
+    assert hints['c'] == Optional[float]
+
+    hints = resolve_type_hints(requires_unknown, localns={'Unknown': int})
+    assert hints['param'] == int
+
+
+def test_resolve_single_type_hints():
+    hints = resolve_single_type_hints(
+        int,
+        'Optional[int]',
+        'FunctionType',
+        'Callable[..., Any]',
+        'Optional[Sequence[Layer]]',
+        'ViewerModel',
+        'LayerList',
+        'Viewer',
+        inject_napari_namespace=True,
+    )
+
+    from napari import components, layers, viewer
+
+    assert hints == (
+        int,
+        Optional[int],
+        types.FunctionType,
+        Callable[..., Any],
+        Optional[Sequence[layers.Layer]],
+        components.viewer_model.ViewerModel,
+        components.layerlist.LayerList,
+        viewer.Viewer,
+    )
+
+
+def test_type_resolved_signature():
+    from napari import layers
+
+    sig = type_resolved_signature(basic_sig)
+    assert sig.parameters['c'].annotation == Optional[float]
+
+    sig = type_resolved_signature(uses_napari, inject_napari_namespace=True)
+    assert sig.parameters['image'].annotation == layers.Image
+    assert sig.return_annotation == layers.Labels
+
+    with pytest.raises(
+        NameError, match='use `raise_unresolved_optional_args=False`'
+    ):
+        type_resolved_signature(optional_unknown)
+
+    sig = type_resolved_signature(
+        optional_unknown, raise_unresolved_optional_args=False
+    )
+    assert sig.parameters['param'].annotation == 'Unknown'
+
+    with pytest.raises(NameError, match='Could not resolve all annotations'):
+        type_resolved_signature(requires_unknown)
+
+    with pytest.raises(
+        NameError,
+        match="Could not resolve hint for non-optional parameter 'param'",
+    ):
+        type_resolved_signature(
+            requires_unknown, raise_unresolved_optional_args=False
+        )
+
+    sig = type_resolved_signature(requires_unknown, localns={'Unknown': int})
+    assert sig.parameters['param'].annotation == int
---
+++ napari/utils/_injection/_type_resolution.py
@@ -0,0 +1,245 @@
+from __future__ import annotations
+
+import types
+import typing
+from functools import lru_cache
+from inspect import Signature
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, Tuple
+
+if TYPE_CHECKING:
+    from typing import _get_type_hints_obj_allowed_types
+
+
+@lru_cache(maxsize=1)
+def _napari_names() -> Dict[str, Any]:
+    """Napari names to inject into local namespace when evaluating type hints."""
+    import napari
+    from napari import components, layers, viewer
+
+    return {
+        'napari': napari,
+        **viewer.__dict__,
+        **layers.__dict__,
+        **components.__dict__,
+    }
+
+
+@lru_cache(maxsize=1)
+def _typing_names():
+    return {**typing.__dict__, **types.__dict__}  # noqa: TYP006
+
+
+def resolve_type_hints(
+    obj: _get_type_hints_obj_allowed_types,
+    globalns: Optional[dict] = None,
+    localns: Optional[dict] = None,
+    include_extras: bool = False,
+    inject_napari_namespace: bool = True,
+) -> Dict[str, Any]:
+    """Return type hints for an object.
+
+    This is a small wrapper around `typing.get_type_hints()` that adds napari
+    namespaces to the global and local namespaces.
+
+    see docstring for :func:`typing.get_type_hints`.
+
+    Parameters
+    ----------
+    obj : module, class, method, or function
+        must be a module, class, method, or function.
+    globalns : Optional[dict]
+        optional global namespace, by default None.
+    localns : Optional[dict]
+        optional local namespace, by default None.
+    include_extras : bool
+        If `False` (the default), recursively replaces all 'Annotated[T, ...]'
+        with 'T'.
+    inject_napari_namespace : bool
+        If `True` (the default), will inject all names from
+        `napari.components`, `napari.layers`, and `napari.viewer`
+        into the namespace when evaluating types.
+
+    Returns
+    -------
+    Dict[str, Any]
+        mapping of object name to type hint for all annotated attributes of `obj`.
+
+    Examples
+    --------
+    >>> def func(image: 'Image', layer_list: 'LayerList') -> 'Labels': ...
+    >>> resolve_type_hints(func)
+    {
+        'image': <class 'napari.layers.image.image.Image'>,
+        'layer_list': <class 'napari.components.layerlist.LayerList'>,
+        'return': <class 'napari.layers.labels.labels.Labels'>
+    }
+    """
+    _localns = dict(_typing_names())
+    if inject_napari_namespace:
+        _localns.update(_napari_names())
+    if localns:
+        _localns.update(localns)  # explicitly provided locals take precedence
+    return typing.get_type_hints(
+        obj, globalns=globalns, localns=_localns, include_extras=include_extras
+    )
+
+
+def resolve_single_type_hints(
+    *objs: Any,
+    localns: Optional[dict] = None,
+    include_extras: bool = False,
+    inject_napari_namespace: bool = True,
+) -> Tuple[Any, ...]:
+    """Get type hints for one or more isolated type annotations.
+
+    Wrapper around :func:`resolve_type_hints` (see docstring for that function for
+    parameter docs).
+
+    `typing.get_type_hints()` only works for modules, classes, methods, or functions,
+    but the typing module doesn't make the underlying type evaluation logic publicly
+    available. This function creates a small mock object with an `__annotations__`
+    dict that will work as an argument to `typing.get_type_hints()`.  It then extracts
+    the resolved hints back into a tuple of hints corresponding to the input objects.
+
+    Returns
+    -------
+    Tuple[Any, ...]
+        Tuple
+
+    Examples
+    --------
+    >>> resolve_single_type_hints(int, 'Optional[int]', 'Viewer', 'LayerList')
+    (
+        <class 'int'>,
+        typing.Optional[int],
+        <class 'napari.viewer.Viewer'>,
+        <class 'napari.components.layerlist.LayerList'>
+    )
+
+    >>> resolve_single_type_hints('hi', localns={'hi': typing.Any})
+    (typing.Any,)
+    """
+    annotations = {str(n): v for n, v in enumerate(objs)}
+    hints = resolve_type_hints(
+        type('_T', (), {'__annotations__': annotations})(),
+        localns=localns,
+        include_extras=include_extras,
+        inject_napari_namespace=inject_napari_namespace,
+    )
+    return tuple(hints[k] for k in annotations)
+
+
+def type_resolved_signature(
+    func: Callable,
+    *,
+    localns: Optional[dict] = None,
+    raise_unresolved_optional_args: bool = True,
+    inject_napari_namespace: bool = True,
+) -> Signature:
+    """Return a Signature object for a function with resolved type annotations.
+
+    Parameters
+    ----------
+    func : Callable
+        A callable object.
+    localns : Optional[dict]
+        Optional local namespace for name resolution, by default None
+    raise_unresolved_optional_args : bool
+        Whether to raise an exception when an optional parameter (one with a default
+        value) has an unresolvable type annotation, by default True
+    inject_napari_namespace : bool
+        If `True` (the default), will inject all names from
+        `napari.components`, `napari.layers`, and `napari.viewer`
+        into the namespace when evaluating types.
+
+    Returns
+    -------
+    Signature
+        :class:`inspect.Signature` object with fully resolved type annotations,
+        (or at least partially resolved type annotations if
+        `raise_unresolved_optional_args` is `False`).
+
+    Raises
+    ------
+    NameError
+        If a required argument has an unresolvable type annotation, or if
+        `raise_unresolved_optional_args` is `True` and an optional argument has
+        an unresolvable type annotation.
+    """
+    sig = Signature.from_callable(func)
+    try:
+        hints = resolve_type_hints(
+            func,
+            localns=localns,
+            inject_napari_namespace=inject_napari_namespace,
+        )
+    except NameError as err:
+        if raise_unresolved_optional_args:
+            raise NameError(
+                f'Could not resolve all annotations in signature {sig} ({err}). '
+                'To allow optional parameters and return types to remain unresolved, '
+                'use `raise_unresolved_optional_args=False`'
+            ) from err
+        hints = _resolve_mandatory_params(sig, inject_napari_namespace)
+
+    resolved_parameters = [
+        param.replace(annotation=hints.get(param.name, param.empty))
+        for param in sig.parameters.values()
+    ]
+    return sig.replace(
+        parameters=resolved_parameters,
+        return_annotation=hints.get('return', sig.empty),
+    )
+
+
+def _resolve_mandatory_params(
+    sig: Signature,
+    inject_napari_namespace: bool,
+    exclude_unresolved_optionals: bool = False,
+) -> Dict[str, Any]:
+    """Resolve all required param annotations in `sig`, but allow optional ones to fail.
+
+    Helper function for :func:`type_resolved_signature`.  This fallback function is
+    used if at least one parameter in `sig` has an unresolvable type annotation.
+    It resolves each parameter's type annotation independently, and only raises an
+    error if a parameter without a default value has an unresolvable type annotation.
+
+    If `exclude_unresolved_optionals` is `True`, then unresolved optional parameters
+    will not appear in the output dict
+
+    Returns
+    -------
+    Dict[str, Any]
+        mapping of parameter name to type hint.
+
+    Raises
+    ------
+    NameError
+        If a required argument has an unresolvable type annotation.
+    """
+    hints = {}
+    for name, param in sig.parameters.items():
+        if param.annotation is sig.empty:
+            continue
+        try:
+            hints[name] = resolve_single_type_hints(
+                param.annotation,
+                inject_napari_namespace=inject_napari_namespace,
+            )[0]
+        except NameError as e:
+            if param.default is param.empty:
+                raise NameError(
+                    f'Could not resolve type hint for required parameter {name!r}: {e}'
+                ) from e
+            elif not exclude_unresolved_optionals:
+                hints[name] = param.annotation
+    if sig.return_annotation is not sig.empty:
+        try:
+            hints['return'] = resolve_single_type_hints(
+                sig.return_annotation,
+                inject_napari_namespace=inject_napari_namespace,
+            )[0]
+        except NameError:
+            if not exclude_unresolved_optionals:
+                hints['return'] = sig.return_annotation
+    return hints
