2d6758d71ef77e3abcd87b1b2f1ff8ea1137625e
Grzegorz Bokota <bokota+github@gmail.com>
2022-06-27 16:56:41 +0200

    use `as_layer_data_tuple` to make copy

--- napari/layers/_tests/test_layer_actions.py
+++ napari/layers/_tests/test_layer_actions.py
@@ -56,7 +56,7 @@ def test_duplicate_layers():
         pass
 
     layer_list = LayerList()
-    layer_list.append(Points(name="test"))
+    layer_list.append(Points([[0, 0]], name="test"))
     layer_list.selection.active = layer_list[0]
     layer_list[0].events.data.connect(_dummy)
     assert len(layer_list[0].events.data.callbacks) == 2
