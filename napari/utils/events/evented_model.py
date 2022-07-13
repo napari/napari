e3bf22f3f5c56227604420b4036b65bd42d2a393
Lorenzo Gaifas <brisvag@gmail.com>
2022-07-13 19:27:18 +0200

    comma separated context managers

--- napari/utils/events/evented_model.py
+++ napari/utils/events/evented_model.py
@@ -191,9 +191,8 @@ class EventedMetaclass(pydantic_main.ModelMetaclass):
     """
 
     def __new__(mcs, name, bases, namespace, **kwargs):
-        with no_class_attributes():
-            with parametrized_generic_fix():
-                cls = super().__new__(mcs, name, bases, namespace, **kwargs)
+        with no_class_attributes(), parametrized_generic_fix():
+            cls = super().__new__(mcs, name, bases, namespace, **kwargs)
         cls.__eq_operators__ = {}
         for n, f in cls.__fields__.items():
             cls.__eq_operators__[n] = pick_equality_operator(f.type_)
