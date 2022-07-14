e226a2aba2b34d21b622a7c74a96f5bf46cec3cc
Markus Stabrin <markus.stabrin@gmail.com>
2022-07-14 03:30:41 +0200

    Add tests for shapes and points

--- napari/_qt/layer_controls/_tests/test_qt_points_layer.py
+++ napari/_qt/layer_controls/_tests/test_qt_points_layer.py
@@ -14,3 +14,46 @@ def test_out_of_slice_display_checkbox(qtbot):
     assert layer.out_of_slice_display is False
     combo.setChecked(True)
     assert layer.out_of_slice_display is True
+
+
+def test_select_all_selects_value_change(qtbot):
+    layer = Points(
+        np.random.rand(10, 2), size=5, edge_color='red', face_color='red'
+    )
+    qtctrl = QtPointsControls(layer)
+    qtbot.addWidget(qtctrl)
+    size_ctrl = qtctrl.sizeSlider
+    face_ctrl = qtctrl.faceColorEdit
+    edge_ctrl = qtctrl.edgeColorEdit
+    select_all_ctrl = qtctrl.selectAllCheckBox
+
+    np.testing.assert_array_equal(layer.size, [[5] * 2] * 10)
+    assert layer.current_size == 5
+    np.testing.assert_almost_equal(layer.edge_color, [[1, 0, 0, 1]] * 10)
+    assert layer.current_edge_color == 'red'
+    np.testing.assert_almost_equal(layer.face_color, [[1, 0, 0, 1]] * 10)
+    assert layer.current_face_color == 'red'
+
+    size_ctrl.setValue(10)
+    face_ctrl.setColor('green')
+    edge_ctrl.setColor('green')
+
+    np.testing.assert_array_equal(layer.size, [[5] * 2] * 10)
+    assert layer.current_size == 10
+    np.testing.assert_almost_equal(layer.face_color, [[1, 0, 0, 1]] * 10)
+    assert layer.current_face_color == 'green'
+    np.testing.assert_almost_equal(layer.edge_color, [[1, 0, 0, 1]] * 10)
+    assert layer.current_edge_color == 'green'
+
+    select_all_ctrl.setChecked(True)
+
+    size_ctrl.setValue(15)
+    face_ctrl.setColor('blue')
+    edge_ctrl.setColor('blue')
+
+    np.testing.assert_array_equal(layer.size, [[15] * 2] * 10)
+    assert layer.current_size == 15
+    np.testing.assert_almost_equal(layer.face_color, [[0, 0, 1, 1]] * 10)
+    assert layer.current_face_color == 'blue'
+    np.testing.assert_almost_equal(layer.edge_color, [[0, 0, 1, 1]] * 10)
+    assert layer.current_edge_color == 'blue'
--- napari/_qt/layer_controls/_tests/test_qt_shapes_layer.py
+++ napari/_qt/layer_controls/_tests/test_qt_shapes_layer.py
@@ -33,3 +33,51 @@ def test_shape_controls_edge_color(qtbot):
     layer.current_edge_color = 'red'
     target_color = transform_color(layer.current_edge_color)[0]
     np.testing.assert_almost_equal(qtctrl.edgeColorEdit.color, target_color)
+
+
+def test_select_all_selects_value_change(qtbot):
+    layer = Shapes(
+        _SHAPES,
+        shape_type='polygon',
+        edge_width=5,
+        edge_color='red',
+        face_color='red',
+    )
+
+    qtctrl = QtShapesControls(layer)
+    qtbot.addWidget(qtctrl)
+    size_ctrl = qtctrl.widthSlider
+    face_ctrl = qtctrl.faceColorEdit
+    edge_ctrl = qtctrl.edgeColorEdit
+    select_all_ctrl = qtctrl.selectAllCheckBox
+
+    np.testing.assert_array_equal(layer.edge_width, [5] * 10)
+    assert layer.current_edge_width == 5
+    np.testing.assert_almost_equal(layer.edge_color, [[1, 0, 0, 1]] * 10)
+    assert layer.current_edge_color == 'red'
+    np.testing.assert_almost_equal(layer.face_color, [[1, 0, 0, 1]] * 10)
+    assert layer.current_face_color == 'red'
+
+    size_ctrl.setValue(10)
+    face_ctrl.setColor('green')
+    edge_ctrl.setColor('green')
+
+    np.testing.assert_array_equal(layer.edge_width, [5] * 10)
+    assert layer.current_edge_width == 10
+    np.testing.assert_almost_equal(layer.face_color, [[1, 0, 0, 1]] * 10)
+    assert layer.current_face_color == 'green'
+    np.testing.assert_almost_equal(layer.edge_color, [[1, 0, 0, 1]] * 10)
+    assert layer.current_edge_color == 'green'
+
+    select_all_ctrl.setChecked(True)
+
+    size_ctrl.setValue(15)
+    face_ctrl.setColor('blue')
+    edge_ctrl.setColor('blue')
+
+    np.testing.assert_array_equal(layer.edge_width, [15] * 10)
+    assert layer.current_edge_width == 15
+    np.testing.assert_almost_equal(layer.face_color, [[0, 0, 1, 1]] * 10)
+    assert layer.current_face_color == 'blue'
+    np.testing.assert_almost_equal(layer.edge_color, [[0, 0, 1, 1]] * 10)
+    assert layer.current_edge_color == 'blue'
