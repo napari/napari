7aedb046d34e1eab87099af45810d59d556778bd
Talley Lambert <talley.lambert@gmail.com>
2022-07-18 16:21:02 -0400

    add plugin_aware_reg

--- napari/_app/_plugin_aware_registries.py
+++ napari/_app/_plugin_aware_registries.py
@@ -1,115 +1,111 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Iterator, List, Optional
+import contextlib
+from typing import List, Optional, Union
 
+from app_model.registries import CommandsRegistry, MenusRegistry
+from app_model.registries._commands_reg import _RegisteredCommand  # FIXME
+from app_model.types import CommandRule, MenuItem, MenuOrSubmenu, SubmenuItem
 from npe2 import plugin_manager
 from npe2.manifest import contributions
 
-from app_model.registries import CommandsRegistry, KeybindingsRegistry, MenuRegistry
-from app_model.types import (
-    CommandRule,
-    MenuItem,
-    SubmenuItem,
-    _RegisteredCommand,
-)
-
-if TYPE_CHECKING:
-    from ._plugin_aware_registries import MenuOrSubmenu
+CommandId = str
 
 
-def npe2_command_to_rule(cmd_id: str) -> CommandRule:
-    cmd_contrib = plugin_manager.get_command(cmd_id)
-    return CommandRule(
-        id=cmd_contrib.id,
-        title=cmd_contrib.title,
-        category=cmd_contrib.category,
-        icon=cmd_contrib.icon,
-        enablement=cmd_contrib.enablement,
-        short_title=cmd_contrib.short_title,
-    )
+def _partition_group_order(
+    group: Optional[str],
+) -> dict[str, Union[str, float, None]]:
+    """Split a npe2 group string into a dict of group and order.
 
-
-def convert_npe2_menu_item(npe2_item: contributions.MenuItem) -> MenuItem:
-    group, _, order = (npe2_item.group or '').partition("@")
+    Examples
+    --------
+    >>> _partition_group_order("my_group@4")
+    {"group": "my_group", "order": 4}
+    >>> _partition_group_order("my_group")
+    {"group": "my_group", "order": None}
+    >>> _partition_group_order("")
+    {"group": None, "order": None}
+    """
+    group, _, _order = (group or '').partition("@")
     try:
-        order = float(order)
+        order: Optional[float] = float(_order)
     except ValueError:
         order = None
+    return {'group': group or None, 'order': order}
 
-    if isinstance(npe2_item, contributions.MenuCommand):
-        return MenuItem(
-            command=npe2_command_to_rule(npe2_item.command),
-            when=npe2_item.when,
-            group=group,
-            order=order,
-        )
 
-    subm = plugin_manager.get_submenu(npe2_item.submenu)
-    return SubmenuItem(
-        submenu=subm.id,
-        title=subm.label,
-        icon=subm.icon,
-        when=npe2_item.when,
-        group=group,
-        order=order,
+def _npe2_command_to_app_model(
+    cmd: contributions.CommandContribution,
+) -> CommandRule:
+    """Convert a npe2 command contribution to an app_model command rule."""
+    return CommandRule(
+        id=cmd.id,
+        title=cmd.title,
+        category=cmd.category,
+        icon=cmd.icon,
+        enablement=cmd.enablement,
+        short_title=cmd.short_title,
     )
 
 
-# IMPROVE
+def _npe2_submenu_to_app_model(subm: contributions.Submenu) -> SubmenuItem:
+    """Convert a npe2 submenu contribution to an app_model SubmenuItem."""
+    contrib = plugin_manager.get_submenu(subm.submenu)
+    return SubmenuItem(
+        submenu=contrib.id,
+        title=contrib.label,
+        icon=contrib.icon,
+        when=subm.when,
+        **_partition_group_order(subm.group),
+        # enablement= ??  npe2 doesn't have this, but app_model does
+    )
+
+
+def _npe2_menu_cmd_to_app_model(
+    menu_cmd: contributions.MenuCommand,
+) -> MenuItem:
+    """Convert a npe2 menu command contribution to an app_model MenuItem."""
+    contrib = plugin_manager.get_command(menu_cmd.command)
+    return MenuItem(
+        command=_npe2_command_to_app_model(contrib),
+        when=menu_cmd.when,
+        **_partition_group_order(menu_cmd.group),
+    )
+
+
+def _npe2_menu_to_app_model(
+    npe2_item: contributions.MenuItem,
+) -> MenuOrSubmenu:
+    """Convert a npe2 MenuItem to an app_model MenuOrSubmenu.
+
+    just picks the appropriate function given the type of the npe2 item.
+    """
+    if isinstance(npe2_item, contributions.MenuCommand):
+        return _npe2_menu_cmd_to_app_model(npe2_item)
+    elif isinstance(npe2_item, contributions.Submenu):
+        return _npe2_submenu_to_app_model(npe2_item)
+    raise TypeError(f"Unknown npe2 MenuItem type: {type(npe2_item)}")
+
+
 class PluginAwareCommandsRegistry(CommandsRegistry):
-    __instance: Optional[PluginAwareCommandsRegistry] = None
-
-    @classmethod
-    def instance(cls) -> PluginAwareCommandsRegistry:
-        if cls.__instance is None:
-            cls.__instance = cls()
-        return cls.__instance
-
-    def __getitem__(self, id: CommandId) -> List[_RegisteredCommand]:
-        try:
-            self._commands[id]
-        except KeyError:
+    def __getitem__(self, id: CommandId) -> _RegisteredCommand:
+        with contextlib.suppress(KeyError):
             cmd = plugin_manager.get_command(id)
-            return [_RegisteredCommand(id, cmd.exec, cmd.title)]
+            # FIXME: this will probably not inject properly
+            _RegisteredCommand(id=id, callback=cmd.exec, title=cmd.title)
+        return super().__getitem__(id)
 
 
-class PluginAwareKeybindingsRegistry(KeybindingsRegistry):
-    __instance: Optional[PluginAwareKeybindingsRegistry] = None
-
-    @classmethod
-    def instance(cls) -> PluginAwareKeybindingsRegistry:
-        if cls.__instance is None:
-            cls.__instance = cls()
-        return cls.__instance
-
-
-class PluginAwareMenuRegistry(MenuRegistry):
-    __instance: Optional[PluginAwareMenuRegistry] = None
-
-    @classmethod
-    def instance(cls) -> PluginAwareMenuRegistry:
-        if cls.__instance is None:
-            cls.__instance = cls()
-        return cls.__instance
-
+class PluginAwareMenusRegistry(MenusRegistry):
     def get_menu(
         self, menu_id: str, include_plugins: bool = True
     ) -> List[MenuOrSubmenu]:
         items = self._menu_items.get(menu_id, [])
         if include_plugins:
-            menu_contribs = plugin_manager.iter_menu(menu_id)
-            items.extend([convert_npe2_menu_item(i) for i in menu_contribs])
+            items.extend(
+                _npe2_menu_to_app_model(i)
+                for i in plugin_manager.iter_menu(menu_id)
+            )
         if not items:
             raise KeyError("No menu found with id: {menu_id}")
         return items
-
-    def iter_menu_groups(
-        self, menu_id: str, include_plugins: bool = True
-    ) -> Iterator[List[MenuOrSubmenu]]:
-        menu_items = self.get_menu(menu_id, include_plugins)
-        yield from self._sorted_groups(menu_items)
-
-
-commands_registry = PluginAwareCommandsRegistry.instance()
-menu_registry = PluginAwareMenuRegistry.instance()
-keybindings_registry = PluginAwareKeybindingsRegistry.instance()
