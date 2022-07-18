078a01ba0bc4a9a37a1557f6b03a8f0e812e38b9
Talley Lambert <talley.lambert@gmail.com>
2022-07-18 19:38:22 -0400

    wip

--- napari/plugins/_npe2.py
+++ napari/plugins/_npe2.py
@@ -10,11 +10,14 @@ from typing import (
     Sequence,
     Set,
     Tuple,
+    Union,
 )
 
+from app_model.types import CommandRule, MenuItem, MenuOrSubmenu, SubmenuItem
 from npe2 import io_utils
+from npe2 import plugin_manager
 from npe2 import plugin_manager as pm
-from npe2.manifest.contributions import Submenu
+from npe2.manifest import contributions
 
 from ..utils.translations import trans
 
@@ -131,7 +134,7 @@ def populate_qmenu(menu: QMenu, menu_key: str):
     """Populate `menu` from a `menu_key` offering in the manifest."""
     # TODO: declare somewhere what menu_keys are valid.
     for item in pm.iter_menu(menu_key):
-        if isinstance(item, Submenu):
+        if isinstance(item, contributions.Submenu):
             subm_contrib = pm.get_submenu(item.submenu)
             subm = menu.addMenu(subm_contrib.label)
             populate_qmenu(subm, subm_contrib.id)
@@ -272,7 +275,11 @@ def get_sample_data(
 
 
 def _on_plugin_enablement_change(enabled: Set[str], disabled: Set[str]):
-    """Callback when any npe2 plugins are enabled or disabled"""
+    """Callback when any npe2 plugins are enabled or disabled.
+
+    'Disabled' means the plugin remains installed, but it cannot be activated,
+    and its contributions will not be indexed
+    """
     from .. import Viewer
     from ..settings import get_settings
 
@@ -287,6 +294,107 @@ def _on_plugin_enablement_change(enabled: Set[str], disabled: Set[str]):
         v.window.file_menu._rebuild_samples_menu()
 
 
+def _on_plugin_activation_change(activated: Set[str], deactivated: Set[str]):
+    """Callback when any npe2 plugins are activated or deactivated.
+
+    'Activated' means the plugin has been *imported*, and its
+    `on_activate` function was called.
+    """
+
+
+def _on_plugins_registered(manifests: Set[PluginManifest]):
+    """Callback when any npe2 plugins are registered.
+
+    'Registered' means that a manifest has been provided or discovered.
+    """
+    from .._app import app
+
+    for mf in manifests:
+        # FIXME: I think this should be app.register_action instead
+        to_add: List[Tuple[str, MenuOrSubmenu]] = []
+        for menu_id, items in mf.contributions.menus.items():
+            for item in items:
+                app_model_item = _npe2_menu_to_app_model(item)
+                to_add.append((menu_id, app_model_item))
+
+        if to_add:
+            dispose = app.menus.append_menu_items(to_add)
+            plugin_manager.get_context(mf.name).register_disposable(dispose)
+
+
 def index_npe1_adapters():
     """Tell npe2 to import and index any discovered npe1 plugins."""
     pm.index_npe1_adapters()
+
+
+def _partition_group_order(
+    group: Optional[str],
+) -> dict[str, Union[str, float, None]]:
+    """Split a npe2 group string into a dict of group and order.
+    Examples
+    --------
+    >>> _partition_group_order("my_group@4")
+    {"group": "my_group", "order": 4}
+    >>> _partition_group_order("my_group")
+    {"group": "my_group", "order": None}
+    >>> _partition_group_order("")
+    {"group": None, "order": None}
+    """
+    group, _, _order = (group or '').partition("@")
+    try:
+        order: Optional[float] = float(_order)
+    except ValueError:
+        order = None
+    return {'group': group or None, 'order': order}
+
+
+def _npe2_command_to_app_model(
+    cmd: contributions.CommandContribution,
+) -> CommandRule:
+    """Convert a npe2 command contribution to an app_model command rule."""
+    return CommandRule(
+        id=cmd.id,
+        title=cmd.title,
+        category=cmd.category,
+        icon=cmd.icon,
+        enablement=cmd.enablement,
+        short_title=cmd.short_title,
+    )
+
+
+def _npe2_submenu_to_app_model(subm: contributions.Submenu) -> SubmenuItem:
+    """Convert a npe2 submenu contribution to an app_model SubmenuItem."""
+    contrib = plugin_manager.get_submenu(subm.submenu)
+    return SubmenuItem(
+        submenu=contrib.id,
+        title=contrib.label,
+        icon=contrib.icon,
+        when=subm.when,
+        **_partition_group_order(subm.group),
+        # enablement= ??  npe2 doesn't have this, but app_model does
+    )
+
+
+def _npe2_menu_cmd_to_app_model(
+    menu_cmd: contributions.MenuCommand,
+) -> MenuItem:
+    """Convert a npe2 menu command contribution to an app_model MenuItem."""
+    contrib = plugin_manager.get_command(menu_cmd.command)
+    return MenuItem(
+        command=_npe2_command_to_app_model(contrib),
+        when=menu_cmd.when,
+        **_partition_group_order(menu_cmd.group),
+    )
+
+
+def _npe2_menu_to_app_model(
+    npe2_item: contributions.MenuItem,
+) -> MenuOrSubmenu:
+    """Convert a npe2 MenuItem to an app_model MenuOrSubmenu.
+    just picks the appropriate function given the type of the npe2 item.
+    """
+    if isinstance(npe2_item, contributions.MenuCommand):
+        return _npe2_menu_cmd_to_app_model(npe2_item)
+    elif isinstance(npe2_item, contributions.Submenu):
+        return _npe2_submenu_to_app_model(npe2_item)
+    raise TypeError(f"Unknown npe2 MenuItem type: {type(npe2_item)}")
