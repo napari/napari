e1f6779c2d117a34b0b3fcc8f5e3c169d0a20a8c
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-07-25 23:58:40 +0200

    Update deprecation warnings with versions (#4757)
    * Update deprecation warnings with versions
    
    Subset of #4756 that should be non-controversial.
    
    * Apply suggestions from code review
    
    * Update napari/layers/image/image.py
    
    Co-authored-by: Andy Sweet <andrew.d.sweet@gmail.com>
    
    Co-authored-by: Andy Sweet <andrew.d.sweet@gmail.com>

--- napari/layers/image/image.py
+++ napari/layers/image/image.py
@@ -540,7 +540,7 @@ class _ImageBase(IntensityVisualizationMixin, Layer):
         """
         warnings.warn(
             trans._(
-                "Interpolation attribute is deprecated. Please use interpolation2d or interpolation3d",
+                "Interpolation attribute is deprecated since 0.4.17. Please use interpolation2d or interpolation3d",
             ),
             category=DeprecationWarning,
             stacklevel=2,
@@ -556,7 +556,7 @@ class _ImageBase(IntensityVisualizationMixin, Layer):
         """Set current interpolation mode."""
         warnings.warn(
             trans._(
-                "Interpolation setting is deprecated. Please use interpolation2d or interpolation3d",
+                "Interpolation setting is deprecated since 0.4.17. Please use interpolation2d or interpolation3d",
             ),
             category=DeprecationWarning,
             stacklevel=2,
