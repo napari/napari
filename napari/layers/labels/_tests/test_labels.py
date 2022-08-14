819ec3da252c3e32df1faeebacf7bbe66ee1d102
Eric Perlman <perlman@gmail.com>
2022-08-13 21:48:23 -0400

    No more _color_lookup_func.

--- napari/layers/labels/_tests/test_labels.py
+++ napari/layers/labels/_tests/test_labels.py
@@ -906,16 +906,6 @@ def test_cursor_size_with_negative_scale():
     assert layer.cursor_size > 0
 
 
-def test_switching_display_func_during_slicing():
-    label_array = (5e6 * np.ones((2, 2, 2))).astype(np.uint64)
-    label_array[0, :, :] = [[0, 1], [2, 3]]
-    layer = Labels(label_array)
-    layer._dims_point = (1, 0, 0)
-    layer._set_view_slice()
-    assert layer._color_lookup_func == layer._lookup_with_low_discrepancy_image
-    assert layer._all_vals.size < 1026
-
-
 def test_fill_tensorstore():
     ts = pytest.importorskip('tensorstore')
 
