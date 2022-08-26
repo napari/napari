4cb37e0ee354dd92f089d60c86150606889e2a01
lucy liu <jliu176@gmail.com>
2022-08-26 10:52:33 +1000

    menu id

--- napari/_qt/_qapp_model/_menus.py
+++ napari/_qt/_qapp_model/_menus.py
@@ -1,13 +1,21 @@
+from typing import Optional
+
 from app_model.backends.qt import QModelMenu
+from qtpy.QtCore import QObject
 
 
-def build_qmodel_menu(menu_id: str) -> QModelMenu:
+def build_qmodel_menu(
+    menu_id: str, title: str = None, parent: Optional[QObject] = None
+) -> QModelMenu:
     """Build a QModelMenu from the napari app model
-
     Parameters
     ----------
     menu_id : str
         ID of a menu registered with napari._app_model.get_app().menus
+    title : str
+        Title of the menu
+    parent : QWidget
+        Parent of the menu
 
     Returns
     -------
@@ -16,4 +24,6 @@ def build_qmodel_menu(menu_id: str) -> QModelMenu:
     """
     from ..._app_model import get_app
 
-    return QModelMenu(menu_id=menu_id, app=get_app())
+    return QModelMenu(
+        menu_id=menu_id, app=get_app(), title=title, parent=parent
+    )
