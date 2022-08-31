1d7aa5302e0ae8f1cdc0bf69b56bd333010e586d
Kyle Harrington <czi@kyleharrington.com>
2022-08-31 08:01:39 -0400

    Add a small SMLM points example

---
+++ examples/dev/slicing/points_example_smlm.py
@@ -0,0 +1,31 @@
+import napari
+import pooch
+import csv
+import numpy as np
+
+"""
+This data comes from the Neurocyto Lab's description of the ThunderSTORM format.
+This file format is used to represent single molecule localizations.
+
+With respect to the napari async slicing work, this dataset is small enough that it performs well in synchronous mode.
+
+If someone is interested, then you can use the uncertainty_xy attribute from the STORM data to change the point size.
+
+More information is available here: http://www.neurocytolab.org/tscolumns/
+"""
+
+storm_path = pooch.retrieve(
+    url='http://www.neurocytolab.org/wp-content/uploads/2018/06/ThunderSTORM_TS3D.csv',
+    known_hash=None,
+)
+
+with open(storm_path) as csvfile:
+    data = list(csv.reader(csvfile))
+
+data = np.array(data[1:]).astype(float)
+data = data[:, 1:4]
+print('data shape', data.shape, data)
+
+viewer = napari.view_points(data)
+
+napari.run()
