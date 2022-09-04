8eb28addcff70b6a01a055acf5ccc6b31b153b5b
Grzegorz Bokota <bokota+github@gmail.com>
2022-09-04 20:05:36 +0000

    try fix tests

--- napari/_tests/test_multiple_viewers.py
+++ napari/_tests/test_multiple_viewers.py
@@ -1,9 +1,11 @@
+from unittest.mock import patch
+import gc
 import pytest
 
 from napari import Viewer
 
 
-@pytest.mark.skip(reason="problem with clean")
+# @pytest.mark.skip(reason="problem with clean")
 def test_multi_viewers_dont_clash(qtbot):
     v1 = Viewer(show=False, title='v1')
     v2 = Viewer(show=False, title='v2')
@@ -16,6 +18,13 @@ def test_multi_viewers_dont_clash(qtbot):
     assert not v2.grid.enabled
     assert v1.grid.enabled
 
-    v1.close()
-    v2.close()
+    with patch.object(
+                v1.window._qt_window, '_save_current_window_settings'
+            ):
+        v1.close()
+    with patch.object(
+                v2.window._qt_window, '_save_current_window_settings'
+            ):
+        v2.close()
     qtbot.wait(50)
+    gc.collect()
