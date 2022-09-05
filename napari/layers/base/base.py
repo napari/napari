94b4838dbdd948e48173259c0d734e283ad4fc0b
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-09-05 02:02:14 -0700

    Update napari/layers/base/base.py

--- napari/layers/base/base.py
+++ napari/layers/base/base.py
@@ -395,7 +395,7 @@ class Layer(KeymapProvider, MousemapProvider, ABC):
             callback_list.append(mode_dict[mode])
         self.cursor = self._cursor_modes[mode]
 
-        self.interactive = mode in {Modeclass.PAN_ZOOM, Modeclass.FILL_CONTOUR}
+        self.interactive = mode in {Modeclass.PAN_ZOOM, getattr(Modeclass, "FILL_CONTOUR", object())}
         return mode, True
 
     @classmethod
