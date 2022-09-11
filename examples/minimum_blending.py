b9061e1dcf29d7669d7ccfe6f90bf46f3e917de9
Peter Sobolewski <pete.sd@gmail.com>
2022-09-11 12:34:54 +0200

    Use added cmaps in the example

--- examples/minimum_blending.py
+++ examples/minimum_blending.py
@@ -7,21 +7,13 @@ Demonstrates how to use the `minimum` blending mode with inverted colormaps.
 `minimum` blending can be used to yield multichannel color images on a white 
 background, when the channels have inverted colormaps assigned.
 An inverted colormap is one where white [1, 1, 1] is used to represent the lowest 
-values, as opposed to the more conventional black [0, 0, 0].
+values, as opposed to the more conventional black [0, 0, 0]. For example, try the
+colormaps prefixed with *I*, such as *I Forest* or *I Bordeaux*, from 
+ChrisLUTs: https://github.com/cleterrier/ChrisLUTs .
 """
 
-import numpy as np
-from vispy.color import Colormap
 from skimage import data
 import napari
-
-# First, generate two inverted colormaps that go from white [1, 1, 1] 
-# to the chosen color.
-# These are from ChrisLUTs: https://github.com/cleterrier/ChrisLUTs
-# and correspond to the `green` and `magenta` colormaps used by default
-# when opening this sample from the File menu.
-I_Green = Colormap([[1, 1, 1], [0, 1, 0]])
-I_Magenta = Colormap([[1, 1, 1], [1, 0, 1]])
 
 # create a viewer
 viewer = napari.Viewer()
@@ -33,7 +25,7 @@ viewer.add_image(data.cells3d(),
                             name=["membrane", "nuclei"],
                             channel_axis=1,
                             contrast_limits = [[1110, 23855], [1600, 50000]],
-                            colormap = [("I_Magenta", I_Magenta), ("I_Green", I_Green)], 
+                            colormap = ["I Purple", "I Orange"], 
                             blending= ["translucent_no_depth", "minimum"]
                             )
 
