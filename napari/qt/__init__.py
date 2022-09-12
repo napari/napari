c9b2fefc064d476d5d984f18cc5a3529235c8da0
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-09-12 15:30:08 +0200

    MAINT: Remove QtStateButton, marked for removal in 0.4.14
    Extracted from #4756

--- napari/qt/__init__.py
+++ napari/qt/__init__.py
@@ -3,12 +3,12 @@ from napari._qt.qt_main_window import Window
 from napari._qt.qt_resources import get_current_stylesheet, get_stylesheet
 from napari._qt.qt_viewer import QtViewer
 from napari._qt.widgets.qt_tooltip import QtToolTipLabel
-from napari._qt.widgets.qt_viewer_buttons import QtStateButton, QtViewerButtons
+from napari._qt.widgets.qt_viewer_buttons import QtViewerButtons
 from napari.qt.threading import create_worker, thread_worker
+
 
 __all__ = (
     'create_worker',
-    'QtStateButton',
     'QtToolTipLabel',
     'QtViewer',
     'QtViewerButtons',
