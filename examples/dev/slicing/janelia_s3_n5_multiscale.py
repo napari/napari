97d47291d80ef03035a4ec701bdd634a5fd76150
Eric Perlman <perlman@gmail.com>
2022-09-25 15:10:17 -0400

    Add docstring.

--- examples/dev/slicing/janelia_s3_n5_multiscale.py
+++ examples/dev/slicing/janelia_s3_n5_multiscale.py
@@ -1,6 +1,16 @@
-import fsspec, zarr
+import zarr
 import dask.array as da
 import napari
+
+"""
+The sample data here is Interphase HeLa Cell [https://openorganelle.janelia.org/datasets/jrc_hela-3],
+from HHMI's OpenOrganelle [https://openorganelle.janelia.org].
+
+The data are hosted by Open Data on AWS on S3.
+
+This tests access to multi-scale remote data.
+"""
+
 
 # access the root of the n5 container
 group = zarr.open(zarr.N5FSStore('s3://janelia-cosem-datasets/jrc_hela-2/jrc_hela-2.n5', anon=True))
@@ -11,8 +21,8 @@ for i in range(0, 5):
     zarr_array = group[f'em/fibsem-uint16/s{i}']
     data.append(da.from_zarr(zarr_array, chunks=zarr_array.chunks))
 
-# This order presents a better visualization, but seems to break simple async.
-#viewer = napari.view_image(data, order=(1, 0, 2), contrast_limits=(18000, 40000), multiscale=True)
+# This order presents a better visualization, but seems to break simple async (issue #5106)
+# viewer = napari.view_image(data, order=(1, 0, 2), contrast_limits=(18000, 40000), multiscale=True)
 viewer = napari.view_image(data, contrast_limits=(18000, 40000), multiscale=True)
 
 if __name__ == '__main__':
