7f5a9870f9f43d4f3ce4ea5bb76bda7515015768
Eric Perlman <perlman@gmail.com>
2022-09-25 20:20:50 -0400

    Make n command-line arg.

--- examples/dev/slicing/random_points.py
+++ examples/dev/slicing/random_points.py
@@ -1,9 +1,18 @@
+import argparse
 from skimage import data
 import numpy as np
 import napari
 
+"""
+Stress the points layer by generating a large number of points.
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("n", type=int, default=10_000_000)
+args = parser.parse_args()
+
 np.random.seed(0)
-n = 10_000_000
+n = args.n
 data = 1000 * np.random.rand(n, 3)
 viewer = napari.view_points(data)
 
