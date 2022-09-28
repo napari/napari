43b5fc552a42f84822f37fbb2a2207ac8d05f749
Eric Perlman <perlman@gmail.com>
2022-09-28 19:20:23 -0400

    Make number of points optional.

--- examples/dev/slicing/random_points.py
+++ examples/dev/slicing/random_points.py
@@ -8,7 +8,9 @@ Stress the points layer by generating a large number of points.
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument("n", type=int, default=10_000_000)
+parser.add_argument(
+    "n", type=int, nargs='?', default=10_000_000, help="(default: %(default)s)"
+)
 args = parser.parse_args()
 
 np.random.seed(0)
