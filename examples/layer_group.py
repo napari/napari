e1fb8690d1414ae4de4d3a0d8330f81fa41350a1
Lorenzo Gaifas <brisvag@gmail.com>
2022-09-28 17:01:50 +0200

    more complicated example

--- examples/layer_group.py
+++ examples/layer_group.py
@@ -10,12 +10,12 @@ os.environ['ALLOW_LAYERGROUPS'] = '1'
 points = np.random.rand(10, 3) * (1, 2, 3) * 100
 colors = np.random.rand(10, 3)
 
-blobs = data.binary_blobs(length=100, volume_fraction=0.05, n_dim=3)
+blobs = data.binary_blobs(length=100, volume_fraction=0.05, n_dim=2)
 
 viewer = napari.Viewer(ndisplay=3)
 
 pl = napari.layers.Points(points, face_color=colors)
-il = napari.layers.Image(blobs, scale=(1, 2, 3), translate=(20, 10, 5))
+il = napari.layers.Image(blobs, scale=(1, 2), translate=(20, 15))
 
 viewer.add_layer([pl, il])
 
