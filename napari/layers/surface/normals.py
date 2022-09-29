5788d88f2fc9b4c7cea5f15bfdf42e704acf8880
Lorenzo Gaifas <brisvag@gmail.com>
2022-09-29 14:39:06 +0200

    fix merge errors

--- napari/layers/surface/normals.py
+++ napari/layers/surface/normals.py
@@ -22,9 +22,8 @@ class Normals(EventedModel):
     visible : bool
         Whether the normals are displayed.
     color : str, array-like
-        If string can be any color name recognized by vispy or hex value if
-        starting with `#`. If array-like must be 1-dimensional array with 3
-        or 4 elements.
+        The color of the normal lines.
+        See ``ColorValue.validate`` for supported values.
     width : float
         The width of the normal lines.
     length : float
@@ -33,7 +32,7 @@ class Normals(EventedModel):
 
     mode: NormalMode = Field(NormalMode.FACE, allow_mutation=False)
     visible: bool = False
-    color: ColorValue
+    color: ColorValue = 'black'
     width: float = 1
     length: float = 5
 
@@ -43,5 +42,5 @@ class SurfaceNormals(EventedModel):
     Represents both face and vertex normals for a surface mesh.
     """
 
-    face: Normals = Field(Normals(mode=NormalMode.FACE, color='orange'))
-    vertex: Normals = Field(Normals(mode=NormalMode.FACE, color='blue'))
+    face: Normals = Normals(mode=NormalMode.FACE, color='orange')
+    vertex: Normals = Normals(mode=NormalMode.FACE, color='blue')
