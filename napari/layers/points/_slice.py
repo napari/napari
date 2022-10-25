c2dccb1a56e5b0dde5c44957b7093a7adf07bf60
Kyle Harrington <czi@kyleharrington.com>
2022-10-25 12:11:09 -0400

    Refactor _get_static_data to not be static

--- napari/layers/points/_slice.py
+++ napari/layers/points/_slice.py
@@ -1,13 +1,10 @@
-import warnings
 from dataclasses import dataclass, field
-from functools import cached_property
-from typing import Any, Optional, Tuple, Union
+from typing import Any
 
 import numpy as np
 
 from napari.layers.utils._slice_input import _SliceInput
 from napari.utils.transforms import Affine
-from napari.utils.translations import trans
 
 
 @dataclass(frozen=True)
@@ -35,40 +32,25 @@ class _PointSliceRequest:
 
     def execute(self) -> _PointSliceResponse:
 
-        slice_indices, scale = _PointSliceRequest._get_slice_data(
-            data=self.data,
-            ndim=self.dims.ndim,
-            dims_indices=self.dims_indices,
-            dims_not_displayed=self.dims.not_displayed,
-            size=self.size,
-            out_of_slice_display=self.out_of_slice_display,
-        )
+        slice_indices, scale = self._get_slice_data()
 
         return _PointSliceResponse(
             data=self.data, indices=slice_indices, scale=scale
         )
 
-    @staticmethod
-    def _get_slice_data(
-        *,
-        data,
-        ndim,
-        dims_indices,
-        dims_not_displayed,
-        size,
-        out_of_slice_display,
-    ):
-        not_disp = list(dims_not_displayed)
+    def _get_slice_data(self):
+        """This method does the actual slicing work."""
+        not_disp = list(self.dims.not_displayed)
         # We want a numpy array so we can use fancy indexing with the non-displayed
-        # indices, but as dims_indices can (and often/always does) contain slice
+        # indices, but as self.dims_indices can (and often/always does) contain slice
         # objects, the array has dtype=object which is then very slow for the
         # arithmetic below. As Points._round_index is always False, we can safely
         # convert to float to get a major performance improvement.
-        not_disp_indices = np.array(dims_indices)[not_disp].astype(float)
-        if len(data) > 0:
-            if out_of_slice_display and ndim > 2:
-                distances = abs(data[:, not_disp] - not_disp_indices)
-                sizes = size[:, not_disp] / 2
+        not_disp_indices = np.array(self.dims_indices)[not_disp].astype(float)
+        if len(self.data) > 0:
+            if self.out_of_slice_display and self.dims.ndim > 2:
+                distances = abs(self.data[:, not_disp] - not_disp_indices)
+                sizes = self.size[:, not_disp] / 2
                 matches = np.all(distances <= sizes, axis=1)
                 size_match = sizes[matches]
                 size_match[size_match == 0] = 1
@@ -78,8 +60,8 @@ class _PointSliceRequest:
                 slice_indices = np.where(matches)[0].astype(int)
                 return slice_indices, scale
             else:
-                data = data[:, not_disp]
-                distances = np.abs(data - not_disp_indices)
+                self.data = self.data[:, not_disp]
+                distances = np.abs(self.data - not_disp_indices)
                 matches = np.all(distances <= 0.5, axis=1)
                 slice_indices = np.where(matches)[0].astype(int)
                 return slice_indices, 1
