dce5bc5e513aabdc4b9aff5162de56ce6dde2c19
lucy liu <jliu176@gmail.com>
2022-11-01 10:46:40 +1100

    add test dock widget back

--- napari/_qt/_qapp_model/_tests/test_dock_widget_toggler.py
+++ napari/_qt/_qapp_model/_tests/test_dock_widget_toggler.py
@@ -2,21 +2,21 @@ from napari._app_model._app import get_app
 from napari._qt._qapp_model.qactions._window import ToggleDockWidgetAction
 
 
-# def test_dock_widget_toggler(make_napari_viewer):
-#     """Tests `ToggleDockWidgetAction` toggling works."""
-#     viewer = make_napari_viewer(show=True)
-#     action = ToggleDockWidgetAction(
-#         id='some.command.id',
-#         title='Toggle Dock Widget',
-#         dock_widget='dockConsole',
-#     )
+def test_dock_widget_toggler(make_napari_viewer):
+    """Tests `ToggleDockWidgetAction` toggling works."""
+    viewer = make_napari_viewer(show=True)
+    action = ToggleDockWidgetAction(
+        id='some.command.id',
+        title='Toggle Dock Widget',
+        dock_widget='dockConsole',
+    )
 
-#     app = get_app()
-#     app.register_action(action)
+    app = get_app()
+    app.register_action(action)
 
-#     # with app.injection_store.register(providers={Window: lambda: viewer.window}):
-#     assert viewer.window._qt_viewer.dockConsole.isVisible() is False
-#     app.commands.execute_command('some.command.id')
-#     assert viewer.window._qt_viewer.dockConsole.isVisible() is True
-#     app.commands.execute_command('some.command.id')
-#     assert viewer.window._qt_viewer.dockConsole.isVisible() is False
+    # with app.injection_store.register(providers={Window: lambda: viewer.window}):
+    assert viewer.window._qt_viewer.dockConsole.isVisible() is False
+    app.commands.execute_command('some.command.id')
+    assert viewer.window._qt_viewer.dockConsole.isVisible() is True
+    app.commands.execute_command('some.command.id')
+    assert viewer.window._qt_viewer.dockConsole.isVisible() is False
