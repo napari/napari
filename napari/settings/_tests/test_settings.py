c75add1322df8c2fc324e235bab762ae25ed11b1
Peter Sobolewski <pete.sd@gmail.com>
2022-11-06 15:17:42 +0100

    fix test fail

--- napari/settings/_tests/test_settings.py
+++ napari/settings/_tests/test_settings.py
@@ -305,7 +305,7 @@ def test_get_prev_ver_settings(tmp_path):
     # prep a settings file for the previous version
     data = "appearance:\n   theme: light"
     prev_path = tmp_path / '0.4.17' / 'settings.yaml'
-    prev_path.parent.mkdir(parents=True)
+    prev_path.parent.mkdir(parents=True, exist_ok=True)
     prev_path.write_text(data)
     # current path based on napari version
     current_path = tmp_path / '0.4.18' / 'settings.yaml'
@@ -317,7 +317,7 @@ def test_get_parent_ver_settings(tmp_path):
     # prep a settings file in the parent directory
     data = "appearance:\n   theme: dark"
     prev_path = tmp_path / 'settings.yaml'
-    prev_path.parent.mkdir(parents=True)
+    prev_path.parent.mkdir(parents=True, exist_ok=True)
     prev_path.write_text(data)
     # current path based on napari version
     current_path = tmp_path / '0.4.18' / 'settings.yaml'
