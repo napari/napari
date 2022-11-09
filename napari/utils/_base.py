1c66a3e2d75a96584e654b61faa90432d353f91e
Peter Sobolewski <pete.sd@gmail.com>
2022-11-09 21:36:48 +0100

    put dev version settings in a dev subdir

--- napari/utils/_base.py
+++ napari/utils/_base.py
@@ -7,8 +7,19 @@ the translator before the settings manager is created.
 
 from appdirs import user_config_dir
 
+from napari._version import __version_tuple__
+
+if 'dev' in str(__version_tuple__):
+    _dev_version_config_dir = 'dev'
+else:
+    _dev_version_config_dir = None
+
 _FILENAME = "settings.yaml"
 _APPNAME = "Napari"
 _APPAUTHOR = "Napari"
 _DEFAULT_LOCALE = "en"
-_DEFAULT_CONFIG_PATH = user_config_dir(_APPNAME, _APPAUTHOR, _FILENAME)
+_DEFAULT_CONFIG_PATH = (
+    user_config_dir(_APPNAME, _APPAUTHOR, _dev_version_config_dir)
+    + '/'
+    + _FILENAME
+)
