5675491cbbb145c3192cfa81cc9646eb0b0eb185
Kyle Harrington <czi@kyleharrington.com>
2022-11-10 16:38:17 -0500

    Test slice individual indices as well

--- napari/layers/points/_slice.py
+++ napari/layers/points/_slice.py
@@ -53,36 +53,50 @@ class _PointSliceRequest:
     out_of_slice_display: bool = field(repr=False)
 
     def __call__(self) -> _PointSliceResponse:
-        slice_indices, scale = self._get_slice_data()
+        # Return early if no data
+        if len(self.data) == 0:
+            return _PointSliceResponse(indices=[], scale=np.empty(0))
 
-        return _PointSliceResponse(indices=slice_indices, scale=scale)
-
-    def _get_slice_data(self):
-        """This method does the actual slicing work."""
         not_disp = list(self.dims.not_displayed)
+        # Return early if we won't be slicing
+        if not not_disp:
+            return _PointSliceResponse(indices=[], scale=np.empty(0))
+
         # We want a numpy array so we can use fancy indexing with the non-displayed
         # indices, but as self.dims_indices can (and often/always does) contain slice
         # objects, the array has dtype=object which is then very slow for the
         # arithmetic below. As Points._round_index is always False, we can safely
         # convert to float to get a major performance improvement.
         not_disp_indices = np.array(self.dims_indices)[not_disp].astype(float)
-        if len(self.data) > 0:
-            if self.out_of_slice_display and self.dims.ndim > 2:
-                distances = abs(self.data[:, not_disp] - not_disp_indices)
-                sizes = self.size[:, not_disp] / 2
-                matches = np.all(distances <= sizes, axis=1)
-                size_match = sizes[matches]
-                size_match[size_match == 0] = 1
-                scale_per_dim = (size_match - distances[matches]) / size_match
-                scale_per_dim[size_match == 0] = 1
-                scale = np.prod(scale_per_dim, axis=1)
-                slice_indices = np.where(matches)[0].astype(int)
-                return slice_indices, scale
-            else:
-                data = self.data[:, not_disp]
-                distances = np.abs(data - not_disp_indices)
-                matches = np.all(distances <= 0.5, axis=1)
-                slice_indices = np.where(matches)[0].astype(int)
-                return slice_indices, 1
+
+        if self.out_of_slice_display and self.dims.ndim > 2:
+            slice_indices, scale = self._get_out_of_display_slice_data(
+                not_disp, not_disp_indices
+            )
         else:
-            return [], np.empty(0)
+            slice_indices, scale = self._get_slice_data(
+                not_disp, not_disp_indices
+            )
+
+        return _PointSliceResponse(indices=slice_indices, scale=scale)
+
+    def _get_out_of_display_slice_data(self, not_disp, not_disp_indices):
+        """This method slices in the out-of-display case."""
+        distances = abs(self.data[:, not_disp] - not_disp_indices)
+        sizes = self.size[:, not_disp] / 2
+        matches = np.all(distances <= sizes, axis=1)
+        size_match = sizes[matches]
+        size_match[size_match == 0] = 1
+        scale_per_dim = (size_match - distances[matches]) / size_match
+        scale_per_dim[size_match == 0] = 1
+        scale = np.prod(scale_per_dim, axis=1)
+        slice_indices = np.where(matches)[0].astype(int)
+        return slice_indices, scale
+
+    def _get_slice_data(self, not_disp, not_disp_indices):
+        """This method slices in the simpler case."""
+        data = self.data[:, not_disp]
+        distances = np.abs(data - not_disp_indices)
+        matches = np.all(distances <= 0.5, axis=1)
+        slice_indices = np.where(matches)[0].astype(int)
+        return slice_indices, 1
