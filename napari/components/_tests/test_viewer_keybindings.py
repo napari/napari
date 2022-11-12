62346e79785b3cd2ba340fce06546330cb66987b
Peter Sobolewski <pete.sd@gmail.com>
2022-11-12 23:12:41 +0100

    fix test?

--- napari/components/_tests/test_viewer_keybindings.py
+++ napari/components/_tests/test_viewer_keybindings.py
@@ -5,13 +5,12 @@ from napari.settings import get_settings
 def test_theme_toggle_keybinding(make_napari_viewer):
     viewer = make_napari_viewer()
     current_theme = get_settings().appearance.theme
-    assert current_theme == 'dark'
+    assert viewer.theme == current_theme
     toggle_theme(viewer)
     # toggle_theme should not change settings
     assert not current_theme == 'light'
-    # toggle_theme should change the current theme
+    # toggle_theme should change the viewer theme
     assert viewer.theme == 'light'
     toggle_theme(viewer)
-    # toggle_theme should toggle between themes
-    # currently only dark and light themes are available
-    assert viewer.theme == 'dark'
+    # toggle_theme should toggle between actual themes
+    assert not viewer.theme == 'system'
