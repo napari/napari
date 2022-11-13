8126e5aff67dbc7fce33c800fbdf014d2778c867
Peter Sobolewski <pete.sd@gmail.com>
2022-11-13 23:01:44 +0100

    Fix merge

--- napari/_qt/qt_main_window.py
+++ napari/_qt/qt_main_window.py
@@ -647,6 +647,8 @@ class Window:
         self.main_menu.addMenu(self.view_menu)
         self.plugins_menu = menus.PluginsMenu(self)
         self.main_menu.addMenu(self.plugins_menu)
+        self.window_menu = menus.WindowMenu(self)
+        self.main_menu.addMenu(self.window_menu)
         self.help_menu = build_qmodel_menu(
             MenuId.MENUBAR_HELP, title=trans._('&Help'), parent=self._qt_window
         )
