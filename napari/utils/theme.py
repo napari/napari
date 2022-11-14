6a1506cb1bc434185ef207659d5fb73251a0621a
Grzegorz Bokota <bokota+github@gmail.com>
2022-11-14 10:07:31 +0100

    remove obsolete function

--- napari/utils/theme.py
+++ napari/utils/theme.py
@@ -368,11 +368,6 @@ def _install_npe2_themes(_themes):
             register_theme(theme.id, d, mf.name)
 
 
-def refresh_themes():
-    """Refresh themes from npe2 plugins."""
-    _install_npe2_themes(_themes)
-
-
 _install_npe2_themes(_themes)
 _themes.events.added.connect(rebuild_theme_settings)
 _themes.events.removed.connect(rebuild_theme_settings)
