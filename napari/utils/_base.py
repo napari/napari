aaea9f066f8e4a3b6e990675b1d153300d32d606
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2022-11-14 13:02:42 +0100

    Quick silo dev version settings in a dev sub-directory (#5322)
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- napari/utils/_base.py
+++ napari/utils/_base.py
@@ -6,9 +6,21 @@ the translator before the settings manager is created.
 """
 
 from appdirs import user_config_dir
+from packaging import version
+
+from napari._version import __version__
+
+_dev_version_config_dir = (
+    'dev' if version.parse(__version__).is_devrelease else None
+)
+
 
 _FILENAME = "settings.yaml"
 _APPNAME = "Napari"
 _APPAUTHOR = "Napari"
 _DEFAULT_LOCALE = "en"
-_DEFAULT_CONFIG_PATH = user_config_dir(_APPNAME, _APPAUTHOR, _FILENAME)
+_DEFAULT_CONFIG_PATH = (
+    user_config_dir(_APPNAME, _APPAUTHOR, _dev_version_config_dir)
+    + '/'
+    + _FILENAME
+)
