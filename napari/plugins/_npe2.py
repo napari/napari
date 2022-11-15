346db173d1a88ad6ae075c0c45672decefe1f72b
Peter Sobolewski <pete.sd@gmail.com>
2022-11-15 22:19:22 +0100

    use pm.get_manigest per @nclack

--- napari/plugins/_npe2.py
+++ napari/plugins/_npe2.py
@@ -233,16 +233,14 @@ def iter_manifests(
 
 def widget_iterator() -> Iterator[Tuple[str, Tuple[str, Sequence[str]]]]:
     # eg ('dock', ('my_plugin', ('My widget', MyWidget)))
-    # Make a dict of display_name matching plugin_name
-    plugin_names = {}
-    for plugin in pm.iter_manifests():
-        plugin_names[plugin.name] = plugin.display_name
-
     wdgs: DefaultDict[str, List[str]] = DefaultDict(list)
     for wdg_contrib in pm.iter_widgets():
-        wdgs[plugin_names[wdg_contrib.plugin_name]].append(
-            wdg_contrib.display_name
-        )
+        # The plugin_display_name is distinct from the wdg_contrib.display_name
+        # One plugin might have many widgets.
+        plugin_display_name = pm.get_manifest(
+            wdg_contrib.plugin_name
+        ).display_name
+        wdgs[plugin_display_name].append(wdg_contrib.display_name)
     return (('dock', x) for x in wdgs.items())
 
 
