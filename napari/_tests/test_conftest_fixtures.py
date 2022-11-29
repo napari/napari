0548a49917533cee879c3bb80d777eaf61bd79c8
Grzegorz Bokota <bokota+github@gmail.com>
2022-11-29 17:58:16 +0100

    use mutex in tests

--- napari/_tests/test_conftest_fixtures.py
+++ napari/_tests/test_conftest_fixtures.py
@@ -1,25 +1,31 @@
-import time
-
 import pytest
-from qtpy.QtCore import QThread, QTimer
+from qtpy.QtCore import QMutex, QThread, QTimer
 
 
 class _TestThread(QThread):
+    def __init__(self):
+        super().__init__()
+        self.mutex = QMutex()
+
     def run(self):
-        time.sleep(1)
+        self.mutex.lock()
 
 
 @pytest.mark.disable_qthread_start
 def test_disable_qthread(qapp):
     t = _TestThread()
+    t.mutex.lock()
     t.start()
     assert not t.isRunning()
+    t.mutex.unlock()
 
 
 def test_qthread_running(qtbot):
     t = _TestThread()
+    t.mutex.lock()
     t.start()
     assert t.isRunning()
+    t.mutex.unlock()
     qtbot.waitUntil(t.isFinished, timeout=2000)
 
 
@@ -31,7 +37,9 @@ def test_disable_qtimer(qtbot):
     assert not t.isActive()
 
     th = _TestThread()
+    th.mutex.lock()
     th.start()
     assert th.isRunning()
+    th.mutex.unlock()
     qtbot.waitUntil(th.isFinished, timeout=2000)
     assert not th.isRunning()
