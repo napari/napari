dd3330313e3f9464c3a36d136a87f34025251c4b
Lorenzo Gaifas <brisvag@gmail.com>
2022-12-06 12:03:17 +0100

    fixed transforms!

--- napari/layers/base/_base_mouse_bindings.py
+++ napari/layers/base/_base_mouse_bindings.py
@@ -14,17 +14,18 @@ def highlight_box_handles(layer, event):
     if not len(event.dims_displayed) == 2:
         return
 
+    # we work in data space so we're axis aligned which simplifies calculation
     # same as Layer.world_to_data
-    data_to_world = layer._transforms.set_slice(
-        event.dims_displayed
-    ).simplified
-    pos_displayed = np.array(event.position)[event.dims_displayed]
+    world_to_data = (
+        layer._transforms[1:].set_slice(event.dims_displayed).inverse
+    )
+    pos = np.array(world_to_data(event.position))[event.dims_displayed]
 
     handle_coords = generate_transform_box_from_layer(
         layer, event.dims_displayed
     )
-    handle_coords = data_to_world(handle_coords)
-    nearby_handle = get_nearby_handle(pos_displayed, handle_coords)
+    # TODO: dynamically set tolerance based on canvas size so it's not hard to pick small layer
+    nearby_handle = get_nearby_handle(pos, handle_coords)
 
     # set the selected vertex of the box to the nearby_handle (can also be INSIDE or None)
     layer._overlays['transform_box'].selected_vertex = nearby_handle
@@ -34,54 +35,65 @@ def transform_with_box(layer, event):
     if not len(event.dims_displayed) == 2:
         return
 
-    # same as Layer.world_to_data
-    data_to_world = layer._transforms.set_slice(
+    # we work in data space so we're axis aligned which simplifies calculation
+    # same as Layer.data_to_world
+    data_to_world = layer._transforms[1:].simplified.set_slice(
         event.dims_displayed
-    ).simplified
-    pos_displayed = np.array(event.position)[event.dims_displayed]
+    )
+    world_to_data = data_to_world.inverse
+    pos = np.array(event.position)[event.dims_displayed]
+    pos_data = world_to_data(pos)
 
-    initial_handle_coords = generate_transform_box_from_layer(
+    initial_handle_coords_data = generate_transform_box_from_layer(
         layer, event.dims_displayed
     )
-    initial_handle_coords = data_to_world(initial_handle_coords)
-    nearby_handle = get_nearby_handle(pos_displayed, initial_handle_coords)
+    nearby_handle = get_nearby_handle(pos_data, initial_handle_coords_data)
 
     if nearby_handle is None:
         return
 
+    # now that we have the nearby handles, other calculations need
+    # the world space handle positions
+    initial_handle_coords = data_to_world(initial_handle_coords_data)
+
     # initial layer transform so we can calculate changes later
     initial_affine = layer.affine.set_slice(event.dims_displayed)
-    initial_position = pos_displayed
+    initial_data2physical = layer._transforms['data2physical'].set_slice(
+        event.dims_displayed
+    )
+    initial_position = pos
+
+    # needed for resize and rotate
+    center = np.mean(
+        initial_handle_coords[
+            [
+                InteractionBoxHandle.TOP_LEFT,
+                InteractionBoxHandle.BOTTOM_RIGHT,
+            ]
+        ],
+        axis=0,
+    )
 
     yield
 
     while event.type == 'mouse_move':
-        pos_displayed = np.array(event.position)[event.dims_displayed]
+        pos = np.array(event.position)[event.dims_displayed]
 
         if nearby_handle == InteractionBoxHandle.INSIDE:
-            offset = pos_displayed - initial_position
+            offset = pos - initial_position
             new_affine = Affine(translate=offset).compose(initial_affine)
             layer.affine = layer.affine.replace_slice(
                 event.dims_displayed, new_affine
             )
             yield
         elif nearby_handle == InteractionBoxHandle.ROTATION:
-            center = np.mean(
-                initial_handle_coords[
-                    [
-                        InteractionBoxHandle.TOP_LEFT,
-                        InteractionBoxHandle.BOTTOM_RIGHT,
-                    ]
-                ],
-                axis=0,
-            )
 
             # calculate the angle between the center-handle vector and the center-mouse vector
             center_to_handle = (
                 initial_handle_coords[InteractionBoxHandle.ROTATION] - center
             )
             center_to_handle /= np.linalg.norm(center_to_handle)
-            center_to_mouse = pos_displayed - center
+            center_to_mouse = pos - center
             center_to_mouse /= np.linalg.norm(center_to_mouse)
             angle = np.arctan2(
                 center_to_mouse[1], center_to_mouse[0]
@@ -98,6 +110,7 @@ def transform_with_box(layer, event):
             )
             yield
         else:
+            # scale transform
 
             if 'Shift' in event.modifiers:
                 if nearby_handle not in InteractionBoxHandle.corners():
@@ -108,23 +121,30 @@ def transform_with_box(layer, event):
             else:
                 locked_aspect_ratio = False
 
-            # calculate the distance to the opposite handle (which is fixed) before and after drag
-            opposite_handle = initial_handle_coords[
-                InteractionBoxHandle.opposite_handle(nearby_handle)
-            ]
-            opposite_to_handle = (
-                initial_handle_coords[nearby_handle] - opposite_handle
-            )
-            opposite_to_mouse = pos_displayed - opposite_handle
+            locked_center = 'Control' in event.modifiers
 
-            # TODO: prevent shear!
+            # work in data space
+
+            if locked_center:
+                scaling_center = world_to_data(center)
+            else:
+                # opposite handle
+                scaling_center = initial_handle_coords_data[
+                    InteractionBoxHandle.opposite_handle(nearby_handle)
+                ]
+
+            # calculate the distance to the scaling center (which is fixed) before and after drag
+            center_to_handle = (
+                initial_handle_coords_data[nearby_handle] - scaling_center
+            )
+            center_to_mouse = world_to_data(pos) - scaling_center
 
             # get per-dimension scale values
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore", RuntimeWarning)
-                scale = opposite_to_mouse / opposite_to_handle
+                scale = center_to_mouse / center_to_handle
             # infinite values (due to numerical imprecision) mean we are rescaling only
-            # one dimension, so we set to 1.
+            # one dimension (mid-side handles), so we set to 1.
             scale = np.nan_to_num(scale, posinf=1, neginf=1)
 
             if locked_aspect_ratio:
@@ -132,9 +152,17 @@ def transform_with_box(layer, event):
                 scale = [scale_factor, scale_factor]
 
             new_affine = (
-                Affine(translate=opposite_handle)
+                # bring layer to axis aligned space
+                initial_affine.compose(initial_data2physical)
+                # center opposite handle
+                .compose(Affine(translate=scaling_center))
+                # apply scale
                 .compose(Affine(scale=scale))
-                .compose(Affine(translate=-opposite_handle))
+                # undo all the above, backwards
+                .compose(Affine(translate=-scaling_center))
+                .compose(initial_data2physical.inverse)
+                .compose(initial_affine.inverse)
+                # compose with the original affine
                 .compose(initial_affine)
             )
             layer.affine = layer.affine.replace_slice(
