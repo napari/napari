3d19acf7b7b30018402c80f07e53e6d86e9929f3
Peter Sobolewski <pete.sd@gmail.com>
2022-12-11 17:18:02 +0100

    Make a dict of the URLs, per @goanpeca

--- napari/_app_model/actions/_help_actions.py
+++ napari/_app_model/actions/_help_actions.py
@@ -15,45 +15,45 @@ from napari._app_model.constants import CommandId, MenuId
 v = parse(__version__)
 VERSION = "dev" if v.is_devrelease else str(v)
 
+HELP_URLS = {
+    "getting_started": f'https://napari.org/{VERSION}/tutorials/start_index.html',
+    "tutorials": f'https://napari.org/{VERSION}/tutorials/index.html',
+    "layers_guide": f'https://napari.org/{VERSION}/howtos/layers/index.html',
+    "examples_gallery": f'https://napari.org/{VERSION}/gallery.html',
+    "release_notes": f'https://napari.org/{VERSION}/release/release_{VERSION.replace(".", "_")}.html',
+    "homepage": 'https://napari.org',
+}
 
 HELP_ACTIONS: List[Action] = [
     Action(
         id=CommandId.NAPARI_GETTING_STARTED,
         title=CommandId.NAPARI_GETTING_STARTED.title,
-        callback=lambda: webbrowser.open(
-            f'https://napari.org/{VERSION}/tutorials/start_index.html'
-        ),
+        callback=lambda: webbrowser.open(HELP_URLS['getting_started']),
         menus=[{'id': MenuId.MENUBAR_HELP}],
     ),
     Action(
         id=CommandId.NAPARI_TUTORIALS,
         title=CommandId.NAPARI_TUTORIALS.title,
-        callback=lambda: webbrowser.open(
-            f'https://napari.org/{VERSION}/tutorials/index.html'
-        ),
+        callback=lambda: webbrowser.open(HELP_URLS['tutorials']),
         menus=[{'id': MenuId.MENUBAR_HELP}],
     ),
     Action(
         id=CommandId.NAPARI_LAYERS_GUIDE,
         title=CommandId.NAPARI_LAYERS_GUIDE.title,
-        callback=lambda: webbrowser.open(
-            f'https://napari.org/{VERSION}/howtos/layers/index.html'
-        ),
+        callback=lambda: webbrowser.open(HELP_URLS['layers_guide']),
         menus=[{'id': MenuId.MENUBAR_HELP}],
     ),
     Action(
         id=CommandId.NAPARI_EXAMPLES,
         title=CommandId.NAPARI_EXAMPLES.title,
-        callback=lambda: webbrowser.open(
-            f'https://napari.org/{VERSION}/gallery.html'
-        ),
+        callback=lambda: webbrowser.open(HELP_URLS['examples_gallery']),
         menus=[{'id': MenuId.MENUBAR_HELP}],
     ),
     Action(
         id=CommandId.NAPARI_RELEASE_NOTES,
         title=CommandId.NAPARI_RELEASE_NOTES.title,
         callback=lambda: webbrowser.open(
-            f'https://napari.org/{VERSION}/release/release_{VERSION.replace(".", "_")}.html'
+            HELP_URLS['release_notes'],
         ),
         menus=[{'id': MenuId.MENUBAR_HELP}],
         enablement=VERSION != "dev",
@@ -61,7 +61,7 @@ HELP_ACTIONS: List[Action] = [
     Action(
         id=CommandId.NAPARI_HOMEPAGE,
         title=CommandId.NAPARI_HOMEPAGE.title,
-        callback=lambda: webbrowser.open('https://napari.org'),
+        callback=lambda: webbrowser.open(HELP_URLS['homepage']),
         menus=[{'id': MenuId.MENUBAR_HELP}],
     ),
 ]
