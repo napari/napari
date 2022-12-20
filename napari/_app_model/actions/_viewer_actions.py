2b1cf0f160919054150edea11ebc92eeb7d8e746
Ashley Anderson <aganders3@gmail.com>
2022-12-20 13:55:59 -0500

    Move viewer actions callbacks back to _viewer_key_bindings.py

--- napari/_app_model/actions/_viewer_actions.py
+++ napari/_app_model/actions/_viewer_actions.py
@@ -1,143 +1,77 @@
-"""Actions related to the 'View' menu that do not require Qt.
+"""Actions related to the viewer.
 
-View actions that do require Qt should go in
-`napari/_qt/_qapp_model/qactions/_view.py`.
+The Actions in VIEWER_ACTIONS are registered with the application when it is
+created in `_app_model._app`.  Modifying this list at runtime will have no
+effect.  Use `app.register_action` to register new actions at runtime.
 """
 from __future__ import annotations
-
-from typing import TYPE_CHECKING, List
 
 from app_model.types import Action
 
 from napari._app_model.constants import CommandId
-from napari.settings import get_settings
-from napari.utils.theme import available_themes
-
-if TYPE_CHECKING:
-    from napari.viewer import Viewer
-
-
-VIEWER_ACTIONS: List[Action] = []
-
-
-def _reset_scroll_progress(viewer: Viewer):
-    # on key press
-    viewer.dims._scroll_progress = 0
-    # TODO: app-model doesn't handle generators
-    # action-manager seems to use this for keydown/up
-    # yield
-    # on key release
-    # viewer.dims._scroll_progress = 0
-
-
-def _cycle_theme(viewer: Viewer):
-    """Toggle theme for viewer"""
-    settings = get_settings()
-    themes = available_themes()
-    current_theme = settings.appearance.theme
-    idx = (themes.index(current_theme) + 1) % len(themes)
-    settings.appearance.theme = themes[idx]
-
-
-def _reset_view(viewer: Viewer):
-    viewer.reset_view()
-
-
-def _increment_dims_left(viewer: Viewer):
-    viewer.dims._increment_dims_left()
-
-
-def _increment_dims_right(viewer: Viewer):
-    viewer.dims._increment_dims_right()
-
-
-def _focus_axes_up(viewer: Viewer):
-    viewer.dims._focus_up()
-
-
-def _focus_axes_down(viewer: Viewer):
-    viewer.dims._focus_down()
-
-
-def _roll_axes(viewer: Viewer):
-    viewer.dims._roll()
-
-
-def _transpose_axes(viewer: Viewer):
-    viewer.dims.transpose()
-
-
-def _toggle_selected_layer_visibility(viewer: Viewer):
-    viewer.layers.toggle_selected_visibility()
-
-
-def _toggle_console_visibility(viewer: Viewer):
-    viewer.window._qt_viewer.toggle_console_visibility()
-
+from napari.components import _viewer_key_bindings as _viewer_actions
 
 # actions ported to app_model from components/_viewer_key_bindings
-VIEWER_ACTIONS.extend(
-    [
-        Action(
-            id=CommandId.VIEWER_RESET_SCROLL,
-            title=CommandId.VIEWER_RESET_SCROLL.description,
-            short_title=CommandId.VIEWER_RESET_SCROLL.title,
-            callback=_reset_scroll_progress,
-        ),
-        Action(
-            id=CommandId.VIEWER_CYCLE_THEME,
-            title=CommandId.VIEWER_CYCLE_THEME.title,
-            callback=_cycle_theme,
-        ),
-        Action(
-            id=CommandId.VIEWER_RESET_VIEW,
-            title=CommandId.VIEWER_RESET_VIEW.description,
-            short_title=CommandId.VIEWER_RESET_VIEW.title,
-            callback=_reset_view,
-        ),
-        Action(
-            id=CommandId.VIEWER_INC_DIMS_LEFT,
-            title=CommandId.VIEWER_INC_DIMS_LEFT.title,
-            callback=_increment_dims_left,
-        ),
-        Action(
-            id=CommandId.VIEWER_INC_DIMS_RIGHT,
-            title=CommandId.VIEWER_INC_DIMS_RIGHT.title,
-            callback=_increment_dims_right,
-        ),
-        Action(
-            id=CommandId.VIEWER_FOCUS_AXES_UP,
-            short_title=CommandId.VIEWER_FOCUS_AXES_UP.title,
-            title=CommandId.VIEWER_FOCUS_AXES_UP.description,
-            callback=_focus_axes_up,
-        ),
-        Action(
-            id=CommandId.VIEWER_FOCUS_AXES_DOWN,
-            short_title=CommandId.VIEWER_FOCUS_AXES_DOWN.title,
-            title=CommandId.VIEWER_FOCUS_AXES_DOWN.description,
-            callback=_focus_axes_down,
-        ),
-        Action(
-            id=CommandId.VIEWER_ROLL_AXES,
-            short_title=CommandId.VIEWER_ROLL_AXES.title,
-            title=CommandId.VIEWER_ROLL_AXES.description,
-            callback=_roll_axes,
-        ),
-        Action(
-            id=CommandId.VIEWER_TRANSPOSE_AXES,
-            short_title=CommandId.VIEWER_TRANSPOSE_AXES.title,
-            title=CommandId.VIEWER_TRANSPOSE_AXES.description,
-            callback=_transpose_axes,
-        ),
-        Action(
-            id=CommandId.VIEWER_TOGGLE_SELECTED_LAYER_VISIBILITY,
-            title=CommandId.VIEWER_TOGGLE_SELECTED_LAYER_VISIBILITY.title,
-            callback=_toggle_selected_layer_visibility,
-        ),
-        Action(
-            id=CommandId.VIEWER_TOGGLE_CONSOLE_VISIBILITY,
-            title=CommandId.VIEWER_TOGGLE_CONSOLE_VISIBILITY.title,
-            callback=_toggle_console_visibility,
-        ),
-    ],
-)
+VIEWER_ACTIONS = [
+    Action(
+        id=CommandId.VIEWER_RESET_SCROLL,
+        title=CommandId.VIEWER_RESET_SCROLL.description,
+        short_title=CommandId.VIEWER_RESET_SCROLL.title,
+        callback=_viewer_actions.reset_scroll_progress,
+    ),
+    Action(
+        id=CommandId.VIEWER_CYCLE_THEME,
+        title=CommandId.VIEWER_CYCLE_THEME.title,
+        callback=_viewer_actions.cycle_theme,
+    ),
+    Action(
+        id=CommandId.VIEWER_RESET_VIEW,
+        title=CommandId.VIEWER_RESET_VIEW.description,
+        short_title=CommandId.VIEWER_RESET_VIEW.title,
+        callback=_viewer_actions.reset_view,
+    ),
+    Action(
+        id=CommandId.VIEWER_INC_DIMS_LEFT,
+        title=CommandId.VIEWER_INC_DIMS_LEFT.title,
+        callback=_viewer_actions.increment_dims_left,
+    ),
+    Action(
+        id=CommandId.VIEWER_INC_DIMS_RIGHT,
+        title=CommandId.VIEWER_INC_DIMS_RIGHT.title,
+        callback=_viewer_actions.increment_dims_right,
+    ),
+    Action(
+        id=CommandId.VIEWER_FOCUS_AXES_UP,
+        short_title=CommandId.VIEWER_FOCUS_AXES_UP.title,
+        title=CommandId.VIEWER_FOCUS_AXES_UP.description,
+        callback=_viewer_actions.focus_axes_up,
+    ),
+    Action(
+        id=CommandId.VIEWER_FOCUS_AXES_DOWN,
+        short_title=CommandId.VIEWER_FOCUS_AXES_DOWN.title,
+        title=CommandId.VIEWER_FOCUS_AXES_DOWN.description,
+        callback=_viewer_actions.focus_axes_down,
+    ),
+    Action(
+        id=CommandId.VIEWER_ROLL_AXES,
+        short_title=CommandId.VIEWER_ROLL_AXES.title,
+        title=CommandId.VIEWER_ROLL_AXES.description,
+        callback=_viewer_actions.roll_axes,
+    ),
+    Action(
+        id=CommandId.VIEWER_TRANSPOSE_AXES,
+        short_title=CommandId.VIEWER_TRANSPOSE_AXES.title,
+        title=CommandId.VIEWER_TRANSPOSE_AXES.description,
+        callback=_viewer_actions.transpose_axes,
+    ),
+    Action(
+        id=CommandId.VIEWER_TOGGLE_SELECTED_LAYER_VISIBILITY,
+        title=CommandId.VIEWER_TOGGLE_SELECTED_LAYER_VISIBILITY.title,
+        callback=_viewer_actions.toggle_selected_layer_visibility,
+    ),
+    Action(
+        id=CommandId.VIEWER_TOGGLE_CONSOLE_VISIBILITY,
+        title=CommandId.VIEWER_TOGGLE_CONSOLE_VISIBILITY.title,
+        callback=_viewer_actions.toggle_console_visibility,
+    ),
+]
--- napari/components/_viewer_key_bindings.py
+++ napari/components/_viewer_key_bindings.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from napari.viewer import Viewer
+
 from napari.components.viewer_model import ViewerModel
+from napari.settings import get_settings
 from napari.utils.action_manager import action_manager
+from napari.utils.theme import available_themes
 
 
 def register_viewer_action(description):
@@ -22,3 +29,57 @@ def register_viewer_action(description):
         return func
 
     return _inner
+
+
+def reset_scroll_progress(viewer: Viewer):
+    # on key press
+    viewer.dims._scroll_progress = 0
+    # TODO: app-model doesn't handle generators - action-manager seems to use them for keydown/up bindings
+    yield
+    # on key release
+    viewer.dims._scroll_progress = 0
+
+
+def cycle_theme(viewer: Viewer):
+    """Toggle theme for viewer"""
+    settings = get_settings()
+    themes = available_themes()
+    current_theme = settings.appearance.theme
+    idx = (themes.index(current_theme) + 1) % len(themes)
+    settings.appearance.theme = themes[idx]
+
+
+def reset_view(viewer: Viewer):
+    viewer.reset_view()
+
+
+def increment_dims_left(viewer: Viewer):
+    viewer.dims._increment_dims_left()
+
+
+def increment_dims_right(viewer: Viewer):
+    viewer.dims._increment_dims_right()
+
+
+def focus_axes_up(viewer: Viewer):
+    viewer.dims._focus_up()
+
+
+def focus_axes_down(viewer: Viewer):
+    viewer.dims._focus_down()
+
+
+def roll_axes(viewer: Viewer):
+    viewer.dims._roll()
+
+
+def transpose_axes(viewer: Viewer):
+    viewer.dims.transpose()
+
+
+def toggle_selected_layer_visibility(viewer: Viewer):
+    viewer.layers.toggle_selected_visibility()
+
+
+def toggle_console_visibility(viewer: Viewer):
+    viewer.window._qt_viewer.toggle_console_visibility()
