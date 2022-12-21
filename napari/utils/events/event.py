e861f169966561f556c26ba75453e8b934e34b19
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-12-21 17:25:43 +0100

    PERF: Event emissions and perf regression. (#5307)

--- napari/utils/events/event.py
+++ napari/utils/events/event.py
@@ -763,7 +763,9 @@ class EventEmitter:
                 self.disconnect(cb)
         finally:
             self._emitting = False
-            if event._pop_source() != self.source:
+            ps = event._pop_source()
+            if ps is not self.source:
+
                 raise RuntimeError(
                     trans._(
                         "Event source-stack mismatch.",
