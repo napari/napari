0c0d06327e4696fd586db3c864e53eff17cc0688
Kim Pevey <kcpevey@gmail.com>
2022-12-22 10:39:43 -0600

    attempt to trigger api change

--- napari/__main__.py
+++ napari/__main__.py
@@ -125,7 +125,6 @@ def parse_sys_argv():
         + "\n".join(kwarg_options),
     )
     parser.add_argument('paths', nargs='*', help='path(s) to view.')
-    parser.add_argument('path', nargs='*', help='path(s) to view.')
     parser.add_argument(
         '-v',
         '--verbose',
