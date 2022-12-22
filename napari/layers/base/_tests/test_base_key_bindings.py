38b8a40de0d4fdb37fdbac7c037c548894639cb4
Lorenzo Gaifas <brisvag@gmail.com>
2022-12-22 13:40:44 +0100

    better base key binding test

--- napari/layers/base/_tests/test_base_key_bindings.py
+++ napari/layers/base/_tests/test_base_key_bindings.py
@@ -1,3 +1,5 @@
+import pytest
+
 from napari.layers.base import _base_key_bindings as key_bindings
 from napari.layers.points import Points
 
@@ -8,4 +10,10 @@ def test_hold_to_pan_zoom(layer):
 
     layer.mode = 'transform'
     # need to go through the generator
-    _ = list(key_bindings.hold_to_pan_zoom(layer))
+    gen = key_bindings.hold_to_pan_zoom(layer)
+    assert layer.mode == 'transform'
+    next(gen)
+    assert layer.mode == 'pan_zoom'
+    with pytest.raises(StopIteration):
+        next(gen)
+    assert layer.mode == 'transform'
