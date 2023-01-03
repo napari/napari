6621212346d61eb8279a637a0a6e09fbe445abd2
Pam Wadhwa <pepainter@gmail.com>
2023-01-03 09:50:17 -0600

    fix spacing

--- napari/_qt/dialogs/qt_plugin_dialog.py
+++ napari/_qt/dialogs/qt_plugin_dialog.py
@@ -65,8 +65,9 @@ def is_conda_package(pkg: str):
     # Installed conda packages within a conda installation and environment can be identified as files
     # with the template `<package-name>-<version>-<build-string>.json` saved within a `conda-meta` folder within
     # the given environment of interest.
+
     conda_meta_dir = Path(sys.prefix) / 'conda-meta'
-	return any(True for _ in conda_meta_dir.glob(f"{name}-*-*.json"))
+    return any(True for _ in conda_meta_dir.glob(f"{pkg}-*-*.json"))
 
 
 class PluginListItem(QFrame):
@@ -148,7 +149,10 @@ class PluginListItem(QFrame):
         return p
 
     def set_busy(
-        self, text: str, action_name: Literal["install", "uninstall", "cancel"] = None, update: bool = False
+        self,
+        text: str,
+        action_name: Literal["install", "uninstall", "cancel"] = None,
+        update: bool = False,
     ):
         """Updates status text and what buttons are visible when any button is pushed.
 
