3bb636389d31a3399a0174b5f3857ff34600e649
Ashley Anderson <aganders3@gmail.com>
2023-01-04 16:36:23 -0500

    Remove unused functions for registering layer actions

--- napari/layers/utils/layer_utils.py
+++ napari/layers/utils/layer_utils.py
@@ -1,142 +1,14 @@
 from __future__ import annotations
 
-import functools
-import inspect
 from typing import Any, Dict, List, Optional, Sequence, Union
 
 import dask
 import numpy as np
 import pandas as pd
 
-from napari.utils.action_manager import action_manager
 from napari.utils.events.custom_types import Array
 from napari.utils.transforms import Affine
 from napari.utils.translations import trans
-
-
-def register_layer_action(
-    keymapprovider,
-    description: str,
-    repeatable: bool = False,
-    shortcuts: str = None,
-):
-    """
-    Convenient decorator to register an action with the current Layers
-
-    It will use the function name as the action name. We force the description
-    to be given instead of function docstring for translation purpose.
-
-
-    Parameters
-    ----------
-    keymapprovider : KeymapProvider
-        class on which to register the keybindings – this will typically be
-        the instance in focus that will handle the keyboard shortcut.
-    description : str
-        The description of the action, this will typically be translated and
-        will be what will be used in tooltips.
-    repeatable : bool
-        A flag indicating whether the action autorepeats when key is held
-    shortcuts : str | List[str]
-        Shortcut to bind by default to the action we are registering.
-
-    Returns
-    -------
-    function:
-        Actual decorator to apply to a function. Given decorator returns the
-        function unmodified to allow decorator stacking.
-
-    """
-
-    def _inner(func):
-        nonlocal shortcuts
-        name = 'napari:' + func.__name__
-
-        action_manager.register_action(
-            name=name,
-            command=func,
-            description=description,
-            keymapprovider=keymapprovider,
-            repeatable=repeatable,
-        )
-        if shortcuts:
-            if isinstance(shortcuts, str):
-                shortcuts = [shortcuts]
-
-            for shortcut in shortcuts:
-                action_manager.bind_shortcut(name, shortcut)
-        return func
-
-    return _inner
-
-
-def register_layer_attr_action(
-    keymapprovider,
-    description: str,
-    attribute_name: str,
-    shortcuts=None,
-):
-    """
-    Convenient decorator to register an action with the current Layers.
-    This will get and restore attribute from function first argument.
-
-    It will use the function name as the action name. We force the description
-    to be given instead of function docstring for translation purpose.
-
-    Parameters
-    ----------
-    keymapprovider : KeymapProvider
-        class on which to register the keybindings – this will typically be
-        the instance in focus that will handle the keyboard shortcut.
-    description : str
-        The description of the action, this will typically be translated and
-        will be what will be used in tooltips.
-    attribute_name : str
-        The name of the attribute to be restored if key is hold over `get_settings().get_settings().application.hold_button_delay.
-    shortcuts : str | List[str]
-        Shortcut to bind by default to the action we are registering.
-
-    Returns
-    -------
-    function:
-        Actual decorator to apply to a function. Given decorator returns the
-        function unmodified to allow decorator stacking.
-
-    """
-
-    def _handle(func):
-        sig = inspect.signature(func)
-        try:
-            first_variable_name = next(iter(sig.parameters))
-        except StopIteration:
-            raise RuntimeError(
-                trans._(
-                    "If actions has no arguments there is no way to know what to set the attribute to.",
-                    deferred=True,
-                ),
-            )
-
-        @functools.wraps(func)
-        def _wrapper(*args, **kwargs):
-            if args:
-                obj = args[0]
-            else:
-                obj = kwargs[first_variable_name]
-            prev_mode = getattr(obj, attribute_name)
-            func(*args, **kwargs)
-
-            def _callback():
-                setattr(obj, attribute_name, prev_mode)
-
-            return _callback
-
-        repeatable = False  # attribute actions are always non-repeatable
-        register_layer_action(
-            keymapprovider, description, repeatable, shortcuts
-        )(_wrapper)
-        return func
-
-    return _handle
 
 
 def _nanmin(array):
