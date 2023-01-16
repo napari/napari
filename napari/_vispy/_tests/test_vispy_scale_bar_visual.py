dbe6161b045244b9d84a459162ae1082c5bed841
Lorenzo Gaifas <brisvag@gmail.com>
2023-01-16 12:14:20 +0100

    minimal instantiation tests

--- napari/_vispy/_tests/test_vispy_scale_bar_visual.py
+++ napari/_vispy/_tests/test_vispy_scale_bar_visual.py
@@ -2,11 +2,7 @@ from napari._vispy.overlays.scale_bar import VispyScaleBarOverlay
 from napari.components.overlays import ScaleBarOverlay
 
 
-def test_scale_bar(make_napari_viewer):
+def test_scale_bar_instantiation(make_napari_viewer):
     viewer = make_napari_viewer()
     model = ScaleBarOverlay()
-    visual = VispyScaleBarOverlay(overlay=model, viewer=viewer)
-
-    assert visual.node.box.visible is False
-    model.box = True
-    assert visual.node.box.visible is True
+    VispyScaleBarOverlay(overlay=model, viewer=viewer)
--- napari/_vispy/_tests/test_vispy_text_visual.py
+++ napari/_vispy/_tests/test_vispy_text_visual.py
@@ -2,10 +2,7 @@ from napari._vispy.overlays.text import VispyTextOverlay
 from napari.components.overlays import TextOverlay
 
 
-def test_text(make_napari_viewer):
+def test_text_instantiation(make_napari_viewer):
     viewer = make_napari_viewer()
     model = TextOverlay()
-    visual = VispyTextOverlay(overlay=model, viewer=viewer)
-
-    model.visible = False
-    assert visual.node.visible is False
+    VispyTextOverlay(overlay=model, viewer=viewer)
