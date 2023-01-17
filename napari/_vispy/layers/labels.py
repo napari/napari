42decdc7e848010ff556b0c01705ccd7393601fc
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2023-01-17 21:21:21 +1100

    Add some infrastructure for direct colormaps

--- napari/_vispy/layers/labels.py
+++ napari/_vispy/layers/labels.py
@@ -15,6 +15,7 @@ from napari.utils.colormaps.colormap import LabelColormap
 
 
 def _glsl_label_step(controls=None, colors=None, texture_map_data=None):
+    # TODO: Clean up useless shit
     ncolors = len(controls) - 1
 
     LUT = texture_map_data
@@ -68,6 +69,39 @@ def _glsl_label_step(controls=None, colors=None, texture_map_data=None):
     return low_disc_plus_cmap
 
 
+direct_colors_in_vispy = """
+uniform sampler2D texture2D_keys;
+uniform sampler2D texture2D_values;
+uniform vec2 shape;
+
+vec4 hash_2d_get(float t) {
+    bg_key = 0.;
+    vec2 inc = vec2(0., 1.);
+    vec2 pos = vec2(mod(t, shape.x), mod(t, shape.y));
+    float found = texture2D(
+        texture2D_keys,
+        pos / shape
+    );
+    while(found != t || found != bg_key) {
+        pos = mod(pos + inc, shape);
+        found = texture2D(
+            texture2D_keys,
+            pos / shape
+        );
+    }
+    return texture2D(
+        texture2D_values,
+        pos / shape
+    );
+}
+
+vec4 low_disc_plus_cmap(float t) {
+    return hash_2d_get(t);
+}
+
+"""
+
+
 class LabelVispyColormap(VispyColormap):
     def __init__(
         self,
@@ -92,10 +126,82 @@ class LabelVispyColormap(VispyColormap):
             .replace('$use_selection', str(self.use_selection).lower())
             .replace('$selection', str(self.selection))
         )
+        # Lorenzo says: the VisPy Way(TM)
+        # map_func = Function(_glsl_label_step(
+        #        self._controls, self.colors, self.texture_map_data
+        # ))
+        # map_func['seed'] = str(self.seed)
+        # self.glsl_map = map_func
 
     def map(self, x):
         tp = np.where(x == 0, 0, low_discrepancy_image(x, self.seed))
         return super().map(tp)
+
+
+class LabelVispyDirectColormap(VispyColormap):
+    def __init__(
+        self,
+        color_dict,
+        use_selection=False,
+        selection=0.0,
+    ):
+        super().__init__(
+            np.zeros((1, 4)), np.array([0.0, 1.0]), interpolation='zero'
+        )
+        self.use_selection = use_selection
+        self.selection = selection
+        self.update_shader()
+
+    def update_shader(self):
+        self.glsl_map = (
+            _glsl_label_step(
+                self._controls, self.colors, self.texture_map_data
+            )
+            .replace('$seed', str(self.seed))
+            .replace('$use_selection', str(self.use_selection).lower())
+            .replace('$selection', str(self.selection))
+        )
+        # Lorenzo says: the VisPy Way(TM)
+        # map_func = Function(_glsl_label_step(
+        #        self._controls, self.colors, self.texture_map_data
+        # ))
+        # map_func['seed'] = str(self.seed)
+        # self.glsl_map = map_func
+
+    def map(self, x):
+        tp = np.where(x == 0, 0, low_discrepancy_image(x, self.seed))
+        return super().map(tp)
+
+
+def idx_to_2D(idx, shape):
+    return (idx % shape[0], idx % shape[1])
+
+
+def hash2d_get(key, keys, values, empty_val=0.0):
+    pos = idx_to_2D(key, keys.shape)
+    while keys[pos] != key and keys[pos] != empty_val:
+        pos = (pos[0], (pos[1] + 1) % keys.shape[1])
+    if keys[pos] == key:
+        return pos, values[pos]
+    else:
+        return None, None
+
+
+def hash2d_set(key, value, keys, values, empty_val=0.0):
+    pos = (key % keys.shape[0], key % keys.shape[1])
+    while keys[pos] != empty_val:
+        pos = (pos[0], (pos[1] + 1) % keys.shape[1])
+    keys[pos] = key
+    values[pos] = value
+
+
+def build_textures_from_dict(color_dict):
+    shape = (1019, 1021)
+    keys = np.zeros(shape, dtype=np.float32)
+    values = np.zeros(shape + (4,), dtype=np.float32)
+    for key, value in color_dict.items():
+        hash2d_set(key, value, keys, values)
+    return keys, values
 
 
 class VispyLabelsLayer(VispyImageLayer):
@@ -107,13 +213,16 @@ class VispyLabelsLayer(VispyImageLayer):
             layer_node_class=LabelLayerNode,
         )
 
+        self.layer.events.color_mode.connect(self._on_colormap_change)
+
     def _on_colormap_change(self, event=None):
         # self.layer.colormap is a labels_colormap, which is an evented model
         # from napari.utils.colormaps.Colormap (or similar). If we use it
         # in our constructor, we have access to the texture data we need
         colormap = self.layer.colormap
+        mode = self.layer.color_mode
 
-        if isinstance(colormap, LabelColormap):
+        if isinstance(colormap, LabelColormap) and mode == 'auto':
             self.node.cmap = LabelVispyColormap(
                 colors=colormap.colors,
                 controls=colormap.controls,
@@ -121,6 +230,12 @@ class VispyLabelsLayer(VispyImageLayer):
                 use_selection=colormap.use_selection,
                 selection=colormap.selection,
             )
+        elif mode == 'direct':
+            color_dict = self.layer.colors
+            key_texture, val_texture = build_textures_from_dict(color_dict)
+            self.node.shared_program['texture2D_keys'] = key_texture
+            self.node.shared_program['texture2D_colors'] = val_texture
+            self.node.cmap = LabelVispyColormap()
         else:
             self.node.cmap = VispyColormap(*colormap)
 
