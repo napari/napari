520dcfc4c593eb8667de4bdbea73d1fb82d92731
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2023-01-18 15:56:45 +1100

    Update priority function; rearrange code

--- examples/are_the_points_in_view.py
+++ examples/are_the_points_in_view.py
@@ -1,26 +1,17 @@
 import numpy as np
 import pandas as pd
 import napari
+import toolz as tz
 
 from scipy.spatial.transform import Rotation as R
-
-# Just make a grid of points that we can colour as we like
-grid_coord = np.linspace(0, 500, num=10)
-grid = np.stack(
-    np.meshgrid(*[grid_coord] * 3),
-    axis=-1,
-).reshape((-1, 3))
-
-viewer = napari.Viewer(ndisplay=3)
 
 
 def rotation_matrix_from_camera(camera: napari.components.Camera) -> np.ndarray:
     return R.from_euler(seq='yzx', angles=camera.angles, degrees=True)
 
 
-def position_along_camera_centre_line(points, camera):
-    """Compute distance from a point or array of points to the camera position.
-    """
+def visual_depth(points, camera):
+    """Compute visual depth from camera position to a point or array of points. """
     view_direction = camera.view_direction
     points_relative_to_camera = points - camera.center
     projected_length = points_relative_to_camera @ view_direction
@@ -34,35 +25,70 @@ def distance_from_camera_centre_line(points, camera):
     the camera's center point, aka camera.position.
     """
     view_direction = camera.view_direction
-    projected_length = position_along_camera_centre_line(points, camera)
+    projected_length = visual_depth(points, camera)
     projected = view_direction * np.reshape(projected_length, (-1, 1))
     points_relative_to_camera = points - camera.center  # for performance, don't compute this twice in both functions 
     distances = np.linalg.norm(projected - points_relative_to_camera, axis=-1)
     return distances
 
-initial_dist = position_along_camera_centre_line(grid, viewer.camera)
 
-viewer.add_points(
-    grid, features={'distance': initial_dist}, face_color='distance'
-)
+def prioritised_chunk_loading(depth, distance, zoom, alpha=1.):
+    chunk_load_priority = depth + alpha * zoom * distance
+    return chunk_load_priority
 
 
-def update_point_colors(event):
+@tz.curry
+def update_point_colors(event, viewer):
     """Update the points based on their distance to current camera.
 
     Parameters:
     -----------
-    viewer : naperi.Viewer
+    viewer : napari.Viewer
         Current viewer
     event : camera.events.angles event
         The event triggered by changing the camera angles
     """
-    points = viewer.layers['grid']
-    new_distances = position_along_camera_centre_line(points.data, viewer.camera)
-    points.features = pd.DataFrame({'distance': new_distances})
-    points.face_color = 'distance'
-    points.refresh()
+    points_layer = viewer.layers['grid']
+    points = points_layer.data
+    distances = distance_from_camera_centre_line(points, viewer.camera)
+    depth = visual_depth(points, viewer.camera)
+    priorities = prioritised_chunk_loading(depth, distances, viewer.camera.zoom)
+    points_layer.features = pd.DataFrame({
+        'distance': distances, 'depth': depth, 'priority': priorities
+    })
+    points_layer.face_color = 'priority'
+    points_layer.refresh()
 
 
-viewer.camera.events.connect(update_point_colors)
-napari.run()
+if __name__ == '__main__':
+    # Just make a grid of points that we can colour as we like
+    grid_coord = np.linspace(0, 500, num=10)
+    grid = np.stack(
+        np.meshgrid(*[grid_coord] * 3),
+        axis=-1,
+    ).reshape((-1, 3))
+
+    viewer = napari.Viewer(ndisplay=3)
+
+    initial_dist = distance_from_camera_centre_line(grid, viewer.camera)
+    initial_depth = visual_depth(grid, viewer.camera)
+    initial_priority = prioritised_chunk_loading(
+        initial_depth, initial_dist, viewer.camera.zoom
+    )
+    features = pd.DataFrame({
+        'distance': initial_dist,
+        'depth': initial_depth,
+        'priority': initial_priority,
+    })
+
+    viewer.add_points(
+        grid,
+        features={
+            'distance': initial_dist,
+            'depth': initial_depth,
+            'priority': initial_priority
+        },
+        face_color='priority',
+    )
+    viewer.camera.events.connect(update_point_colors(viewer=viewer))
+    napari.run()
