cee3f74401047d534d3a8ca7c81af7090bbb27d4
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2023-01-23 16:11:53 +1100

    Split io utilities into separate functions, to de-duplicate code elsewhere

--- napari/utils/io.py
+++ napari/utils/io.py
@@ -23,51 +23,59 @@ def imsave(filename: str, data: np.ndarray):
         ext = ".png"
     # Save screenshot image data to output file
     if ext in [".png"]:
-        import PIL
-        from imageio.v3 import imwrite
-
-        pnginfo = PIL.PngImagePlugin.PngInfo()
-        pnginfo.add_text(
-            "Software", f"napari version {__version__} https://napari.org/"
-        )
-        imwrite(
-            filename,
-            data,
-            extension='.png',
-            plugin='pillow',
-            pnginfo=pnginfo,
-        )
+        imsave_png(filename, data)
     elif ext in [".tif", ".tiff"]:
-        import tifffile
-
-        compression_instead_of_compress = False
-        try:
-            current_version = tuple(
-                int(x) for x in tifffile.__version__.split('.')[:3]
-            )
-            compression_instead_of_compress = current_version >= (2021, 6, 6)
-        except Exception:
-            # Just in case anything goes wrong in parsing version number
-            # like repackaging on linux or anything else we fallback to
-            # using compress
-            warnings.warn(
-                trans._(
-                    'Error parsing tiffile version number {version_number}',
-                    deferred=True,
-                    version_number=f"{tifffile.__version__:!r}",
-                )
-            )
-
-        if compression_instead_of_compress:
-            # 'compression' scheme is more complex. See:
-            # https://forum.image.sc/t/problem-saving-generated-labels-in-cellpose-napari/54892/8
-            tifffile.imwrite(filename, data, compression=('zlib', 1))
-        else:  # older version of tifffile since 2021.6.6  this is deprecated
-            tifffile.imwrite(filename, data, compress=1)
+        imsave_tiff(filename, data)
     else:
         imsave(
             filename, data
         )  # scikit-image imsave method used to write all other file types
+
+
+def imsave_png(filename, data):
+    import PIL
+    from imageio.v3 import imwrite
+
+    pnginfo = PIL.PngImagePlugin.PngInfo()
+    pnginfo.add_text(
+        "Software", f"napari version {__version__} https://napari.org/"
+    )
+    imwrite(
+        filename,
+        data,
+        extension='.png',
+        plugin='pillow',
+        pnginfo=pnginfo,
+    )
+
+
+def imsave_tiff(filename, data):
+    import tifffile
+
+    compression_instead_of_compress = False
+    try:
+        current_version = tuple(
+            int(x) for x in tifffile.__version__.split('.')[:3]
+        )
+        compression_instead_of_compress = current_version >= (2021, 6, 6)
+    except Exception:
+        # Just in case anything goes wrong in parsing version number
+        # like repackaging on linux or anything else we fallback to
+        # using compress
+        warnings.warn(
+            trans._(
+                'Error parsing tiffile version number {version_number}',
+                deferred=True,
+                version_number=f"{tifffile.__version__:!r}",
+            )
+        )
+
+    if compression_instead_of_compress:
+        # 'compression' scheme is more complex. See:
+        # https://forum.image.sc/t/problem-saving-generated-labels-in-cellpose-napari/54892/8
+        tifffile.imwrite(filename, data, compression=('zlib', 1))
+    else:  # older version of tifffile since 2021.6.6  this is deprecated
+        tifffile.imwrite(filename, data, compress=1)
 
 
 def __getattr__(name: str):
