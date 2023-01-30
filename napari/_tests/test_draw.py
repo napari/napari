c09f480a981db7521a21f99bbfb4fd7a6627ea3f
wmv_hpomen <w-mv@hotmail.com>
2023-01-30 21:19:33 +0100

    Fixed attribute error in test_draw

--- napari/_tests/test_draw.py
+++ napari/_tests/test_draw.py
@@ -21,15 +21,15 @@ def test_canvas_drawing(make_napari_viewer):
 
     # Check canvas context is not none before drawing, as currently on
     # some of our CI a proper canvas context is not made
-    view.canvas.events.draw()
+    view.canvas.scene_canvas.events.draw()
 
     # Add layer
     data = np.random.random((15, 10, 5))
     layer = viewer.add_image(data)
     assert len(viewer.layers) == 1
-    view.canvas.events.draw()
+    view.canvas.scene_canvas.events.draw()
 
     # Remove layer
     viewer.layers.remove(layer)
     assert len(viewer.layers) == 0
-    view.canvas.events.draw()
+    view.canvas.scene_canvas.events.draw()
