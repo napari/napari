b9ef423e1b1024d2a24b1a7912ae6645df4eeb87
Andy Sweet <andrew.d.sweet@gmail.com>
2023-02-01 13:26:41 -0800

    Add test to cover changing check state

--- napari/_qt/containers/_tests/test_qt_layer_list.py
+++ napari/_qt/containers/_tests/test_qt_layer_list.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 from qtpy.QtCore import Qt
 
@@ -6,17 +8,36 @@ from napari.components import LayerList
 from napari.layers import Image
 
 
-def test_set_layer_visible_changes_checked_state(qtbot):
-    image = Image(np.zeros((4, 3)))
-    layers = LayerList([image])
-    view = QtLayerList(layers)
-    qtbot.addWidget(view)
+def test_set_layer_invisible_makes_item_unchecked(qtbot):
+    view, image = make_qt_layer_list_with_layer(qtbot)
     assert image.visible
     assert check_state_at_layer_index(view, 0) == Qt.CheckState.Checked
 
     image.visible = False
 
     assert check_state_at_layer_index(view, 0) == Qt.CheckState.Unchecked
+
+
+def test_set_item_unchecked_makes_layer_invisible(qtbot):
+    view, image = make_qt_layer_list_with_layer(qtbot)
+    assert check_state_at_layer_index(view, 0) == Qt.CheckState.Checked
+    assert image.visible
+
+    view.model().setData(
+        view.model().index(0, 0, view.rootIndex()),
+        Qt.CheckState.Unchecked,
+        Qt.ItemDataRole.CheckStateRole,
+    )
+
+    assert not image.visible
+
+
+def make_qt_layer_list_with_layer(qtbot) -> Tuple[QtLayerList, Image]:
+    image = Image(np.zeros((4, 3)))
+    layers = LayerList([image])
+    view = QtLayerList(layers)
+    qtbot.addWidget(view)
+    return view, image
 
 
 def check_state_at_layer_index(
