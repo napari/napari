f3a8fdb4860208fdeb183e14dcaca27b5ec3125a
Andy Sweet <andrew.d.sweet@gmail.com>
2023-02-01 13:18:59 -0800

    Clean up test

--- napari/_qt/containers/_tests/test_qt_layer_list.py
+++ napari/_qt/containers/_tests/test_qt_layer_list.py
@@ -9,26 +9,19 @@ from napari.layers import Image
 def test_set_layer_visible_changes_checked_state(qtbot):
     image = Image(np.zeros((4, 3)))
     layers = LayerList([image])
-    qt_layers_view = QtLayerList(layers)
-    qtbot.addWidget(qt_layers_view)
+    view = QtLayerList(layers)
+    qtbot.addWidget(view)
     assert image.visible
-    assert (
-        check_state_at_layer_index(qt_layers_view, 0) == Qt.CheckState.Checked
-    )
+    assert check_state_at_layer_index(view, 0) == Qt.CheckState.Checked
 
     image.visible = False
 
-    assert (
-        check_state_at_layer_index(qt_layers_view, 0)
-        == Qt.CheckState.Unchecked
-    )
+    assert check_state_at_layer_index(view, 0) == Qt.CheckState.Unchecked
 
 
 def check_state_at_layer_index(
-    qt_layers_view: QtLayerList, index: int
+    view: QtLayerList, layer_index: int
 ) -> Qt.CheckState:
-    qt_layers_model = qt_layers_view.model()
-    qt_layer_index = qt_layers_model.index(
-        index, 0, qt_layers_view.rootIndex()
-    )
-    return qt_layers_model.data(qt_layer_index, Qt.ItemDataRole.CheckStateRole)
+    model = view.model()
+    model_index = model.index(layer_index, 0, view.rootIndex())
+    return model.data(model_index, Qt.ItemDataRole.CheckStateRole)
