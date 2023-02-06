cc56567b69c37b921d0cb15edc1b06cf71ba5d32
Lorenzo Gaifas <brisvag@gmail.com>
2023-02-06 13:34:12 +0100

    Update napari/_vispy/canvas.py
    Co-authored-by: Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>

--- napari/_vispy/canvas.py
+++ napari/_vispy/canvas.py
@@ -78,10 +78,8 @@ class VispyCanvas(SceneCanvas):
 
     @bgcolor.setter
     def bgcolor(self, value):
-        color = Color(self._background_color_override or value)
-        # to get correct blending, set canvas alpha to zero
-        color.alpha = 0
-        SceneCanvas.bgcolor.fset(self, color)
+        _value = self._background_color_override or value
+        SceneCanvas.bgcolor.fset(self, _value)
 
     @property
     def central_widget(self):
