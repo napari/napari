1dab621b67131bfadfc8b5f58b92c0569cbb9eb6
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2023-02-10 10:25:10 +0100

    fix vectors layer checkbox docstring

--- napari/_qt/layer_controls/qt_vectors_controls.py
+++ napari/_qt/layer_controls/qt_vectors_controls.py
@@ -204,8 +204,8 @@ class QtVectorsControls(QtLayerControls):
 
         Parameters
         ----------
-        state : QCheckBox
-            Checkbox to indicate whether to render out of slice.
+        state : int
+             Integer value of Qt.CheckState that indicates the check state of outOfSliceCheckBox
         """
         self.layer.out_of_slice_display = (
             Qt.CheckState(state) == Qt.CheckState.Checked
