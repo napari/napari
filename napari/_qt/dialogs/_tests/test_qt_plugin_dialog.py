619574a845457300b3910ef20d664a5317a4e2e6
dalthviz <d.althviz10@uniandes.edu.co>
2023-02-15 12:43:25 -0500

    Explicitly stop timer after waiting for worker in the test

--- napari/_qt/dialogs/_tests/test_qt_plugin_dialog.py
+++ napari/_qt/dialogs/_tests/test_qt_plugin_dialog.py
@@ -288,7 +288,9 @@ def test_plugin_list_handle_action(plugin_dialog, qtbot):
         item, 'test-name-1', InstallerActions.CANCEL, update=False, version='3'
     )
 
+    # Wait for refresh worker and stop timer
     qtbot.waitUntil(lambda: not plugin_dialog.worker.is_running)
+    plugin_dialog._add_items_timer.stop()
 
 
 def test_on_enabled_checkbox(plugin_dialog, qtbot):
