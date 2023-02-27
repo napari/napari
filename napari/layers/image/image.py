db2fcefc2e2e0164fc6e03b6b21343cf3d0b5169
Lukasz Migas <lukas.migas@yahoo.com>
2023-02-27 23:31:39 +0100

    Update image.py

--- napari/layers/image/image.py
+++ napari/layers/image/image.py
@@ -90,7 +90,7 @@ class _ImageBase(IntensityVisualizationMixin, Layer):
         modes.
     depiction : str
         3D Depiction mode. Must be one of {'volume', 'plane'}.
-        The default value is 'volume'.
+        The default value is 'volume'.f
     iso_threshold : float
         Threshold for isosurface.
     attenuation : float
