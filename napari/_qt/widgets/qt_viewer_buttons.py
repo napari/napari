napari/napari

commit 767f4bb2282f1ee965a041992d4ab801c3a6cc44

Use new selection model on existing `LayerList` (#2441)
* selectable mixin and selectable evented list

* add group

* typo

* bring over layerlist changes

* bring over qt_layerlist

* update example/test

* add future annotations

* cleanup

* remove from selection when removing from list

* clean test

* add selected deprecation

* comments

* deprecate active_layer on viewer

* deprecate unselect_all

* add deprecated layer.selected

* more deprecation stuff

* add comments

* remove unnecessary test

* add comment

* working on current

* wip

* single changed event on evented_set

* working active layer

* improve key/mouse behavior

* remove active layer from qt_viewer

* dry

* cleanup syntax

* fix selection clear active

* fix selection test

* select_only

* comment

* small

* comment

* add selection add hook

* make selection hashable

* fix selection on shift click

* avoid index error

* update deprecation warning

* make current private

* Update napari/utils/events/event.py

Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

* Update napari/layers/base/base.py

Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

* Update napari/utils/events/containers/_selectable_list.py

Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

* Update napari/layers/base/base.py

Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

* Update napari/components/viewer_model.py

Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

* Update napari/components/viewer_model.py

Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

* code review

* Update napari/utils/events/containers/_selection.py

Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

* current

* change test assertion Warning type

napari/components/_viewer_key_bindings.py
@@ -128,8 +128,7 @@ select_all.__doc__ = trans._("Selected all layers.")
 @ViewerModel.bind_key('Control-Shift-Delete')
 def remove_all_layers(viewer):
     """Remove all layers."""
-    viewer.layers.select_all()
-    viewer.layers.remove_selected()
+    viewer.layers.clear()
 
 
 remove_all_layers.__doc__ = trans._("Remove all layers.")

examples/add_points.py
@@ -18,7 +18,7 @@ with napari.gui_qt():
     viewer.add_points(points, size=size)
 
     # unselect the image layer
-    viewer.layers[0].selected = False
+    viewer.layers.selection.discard(viewer.layers[0])
 
     # adjust some of the points layer properties
     layer = viewer.layers[1]
@@ -30,9 +30,11 @@ with napari.gui_qt():
     layer.visible = False
     layer.visible = True
 
-    # change the layer selection
-    layer.selected = False
-    layer.selected = True
+    # select the layer
+    viewer.layers.selection.add(layer)
+    # deselect the layer
+    viewer.layers.selection.remove(layer)
+    # or: viewer.layers.selection.discard(layer)
 
     # change the layer opacity
     layer.opacity = 0.9
@@ -45,4 +47,3 @@ with napari.gui_qt():
 
     # change the layer mode
     layer.mode = 'add'
-

examples/dev/node_tree.py
@@ -49,7 +49,11 @@ root = Group(
 # pretty repr makes nested tree structure more interpretable
 print(root)
 root.events.reordered.connect(lambda e: print(e.value))
-root.selection.events.connect(lambda e: print("selection", e.type, e.value))
+root.selection.events.changed.connect(
+    lambda e: print(
+        f"selection changed.  added: {e.added}, removed: {e.removed}"
+    )
+)
 view = QtNodeTreeView(root)
 
 view.show()

napari/_qt/dialogs/_tests/test_qt_about_key_bindings.py
@@ -55,7 +55,7 @@ def test_updating_with_layer_change(make_napari_viewer, monkeypatch):
     # add a shape layer (different keybindings)
     viewer.add_shapes(None, shape_type='polygon')
     # check that the new layer is the active_layer
-    assert viewer.active_layer == viewer.layers[1]
+    assert viewer.layers.selection.active == viewer.layers[1]
     # capture dialog text after active_layer events
     active_shape_layer_text = dialog.textEditBox.toHtml()
     # check that the text has changed for the new key bindings

napari/_qt/layer_controls/qt_layer_controls_container.py
@@ -87,7 +87,7 @@ class QtLayerControlsContainer(QStackedWidget):
 
         self.viewer.layers.events.inserted.connect(self._add)
         self.viewer.layers.events.removed.connect(self._remove)
-        self.viewer.events.active_layer.connect(self._display)
+        viewer.layers.selection.events.active.connect(self._display)
 
     def _display(self, event):
         """Change the displayed controls to be those of the target layer.

napari/_qt/qt_viewer.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os.path
 import warnings
 from pathlib import Path
@@ -39,6 +41,9 @@ from .._vispy import (  # isort:skip
     VispyWelcomeVisual,
     create_vispy_visual,
 )
+
+if TYPE_CHECKING:
+    from ..viewer import Viewer
 
 
 class QtViewer(QSplitter):
@@ -84,7 +89,7 @@ class QtViewer(QSplitter):
         Button controls for the napari viewer.
     """
 
-    def __init__(self, viewer, welcome=False):
+    def __init__(self, viewer: Viewer, welcome=False):
 
         # Avoid circular import.
         from .layer_controls import QtLayerControlsContainer
@@ -105,7 +110,6 @@ class QtViewer(QSplitter):
         self._key_map_handler = KeymapHandler()
         self._key_map_handler.keymap_providers = [self.viewer]
         self._key_bindings_dialog = None
-        self._active_layer = None
         self._console = None
 
         layerList = QWidget()
@@ -182,9 +186,8 @@ class QtViewer(QSplitter):
             'standard': QCursor(),
         }
 
-        self._on_active_layer_change()
-
-        self.viewer.events.active_layer.connect(self._on_active_layer_change)
+        self._on_active_change()
+        viewer.layers.selection.events.active.connect(self._on_active_change)
         self.viewer.camera.events.interactive.connect(self._on_interactive)
         self.viewer.cursor.events.style.connect(self._on_cursor)
         self.viewer.cursor.events.size.connect(self._on_cursor)
@@ -338,7 +341,7 @@ class QtViewer(QSplitter):
         else:
             self.controls.setMaximumWidth(220)
 
-    def _on_active_layer_change(self, event=None):
+    def _on_active_change(self, event=None):
         """When active layer changes change keymap handler.
 
         Parameters
@@ -346,15 +349,12 @@ class QtViewer(QSplitter):
         event : napari.utils.event.Event
             The napari event that triggered this method.
         """
-        active_layer = self.viewer.active_layer
-
-        if self._active_layer in self._key_map_handler.keymap_providers:
-            self._key_map_handler.keymap_providers.remove(self._active_layer)
+        active_layer = self.viewer.layers.selection.active
+        if active_layer in self._key_map_handler.keymap_providers:
+            self._key_map_handler.keymap_providers.remove(active_layer)
 
         if active_layer is not None:
             self._key_map_handler.keymap_providers.insert(0, active_layer)
-
-        self._active_layer = active_layer
 
         # If a QtAboutKeyBindings exists, update its text.
         if self._key_bindings_dialog is not None:
@@ -675,7 +675,7 @@ class QtViewer(QSplitter):
         event = ReadOnlyWrapper(event)
         mouse_callbacks(self.viewer, event)
 
-        layer = self.viewer.active_layer
+        layer = self.viewer.layers.selection.active
         if layer is not None:
             mouse_callbacks(layer, event)
 

napari/_qt/tree/qt_tree_view.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from itertools import chain, repeat
 from typing import TYPE_CHECKING
 
 from qtpy.QtCore import QItemSelection, QModelIndex, Qt
@@ -70,7 +71,7 @@ class QtNodeTreeView(QTreeView):
 
     def currentChanged(self, current: QModelIndex, previous: QModelIndex):
         """The Qt current item has changed. Update the python model."""
-        self._root.selection.current = current.internalPointer()
+        self._root.selection._current = current.internalPointer()
         return super().currentChanged(current, previous)
 
     def selectionChanged(
@@ -92,9 +93,12 @@ class QtNodeTreeView(QTreeView):
                 idx = self.model().findIndex(event.value)
                 sel_model.setCurrentIndex(idx, sel_model.Current)
             return
-        t = sel_model.Select if event.type == 'added' else sel_model.Deselect
-        for idx in event.value:
-            model_idx = self.model().findIndex(idx)
-            if not model_idx.isValid():
-                continue
-            sel_model.select(model_idx, t)
+        elif event.type == 'changed':
+            for idx, sel in chain(
+                zip(event.added, repeat(sel_model.Select)),
+                zip(event.removed, repeat(sel_model.DeSelect)),
+            ):
+                model_idx = self.model().findIndex(idx)
+                if not model_idx.isValid():
+                    continue
+                sel_model.select(model_idx, sel)

napari/_qt/widgets/_tests/test_qt_layerlist.py
@@ -151,9 +151,8 @@ def test_removing_layers(qtbot):
 
     layers.append(layer_b)
     layers.append(layer_d)
-    # Select first and third layers
-    for layer, s in zip(layers, [True, True, False, False]):
-        layer.selected = s
+    # Select first two
+    layers.selection = layers[:2]
     layers.remove_selected()
     assert view.vbox_layout.count() == 2 * (len(layers) + 1)
     assert check_layout_layers(view.vbox_layout, layers)
@@ -222,8 +221,7 @@ def test_reordering_layers(qtbot):
     layer_f = Image(np.random.random((15, 15)))
     layers.append(layer_e)
     layers.append(layer_f)
-    for layer, s in zip(layers, [False, True, False, False, True, False]):
-        layer.selected = s
+    layers.selection = {layers[1], layers[4]}
     layers.move_selected(1, 2)
     assert view.vbox_layout.count() == 2 * (len(layers) + 1)
     assert check_layout_layers(view.vbox_layout, layers)

commit 9bd9d2f8ed95b2f11d23546045a4db9d2eb0b363

Bugfix: Convert Viewer Delete button to QtViewerPushButton with action and shortcut (#5636)
# Description

The current trash-icon delete button in the viewer when clicked deletes
selected layers, but is also supposed to accept drag-n-drop events,
making it work like the Trash on say macOS (and other OS with a similar
desktop model).
However, this behavior was broken in
https://github.com/napari/napari/pull/2441

This (broken) behavior makes no sense, because napari *doesn't use* a
desktop model, so the Trash icon is not a folder, like the Trash bin in
a OS, a temporary storage for unwanted items. You can't open the napari
trash button to restore the layers you drag there.

The Trash icon in napari should be simply a button—and is stylized as
one—that responds to a single-click to delete selected layers.

This PR removes the broken behavior—eliminating the source of error. In
this way it simplifies the code, to treat the delete button the same as
other viewer buttons (QtViewerPushButton). as a result, the delete
button now also has a registered action and keybinding that is editable
in the Preferences, rather than hard-coded.


## Type of change
<!-- Please delete options that are not relevant. -->
- [x] Bug-fix (non-breaking change which fixes an issue)
- [ ] New feature (non-breaking change which adds functionality)
- [x] Breaking change (fix or feature that would cause existing
functionality to not work as expected): it's been broken for 2 years
without being reported...
- [x] This change requires a documentation update: docs PR:
https://github.com/napari/docs/pull/123

# References
closes https://github.com/napari/napari/issues/5629
See also discussion on zulip:
https://napari.zulipchat.com/#narrow/stream/212875-general/topic/Drag.20layer.20to.20trash

# How has this been tested?
<!-- Please describe the tests that you ran to verify your changes. -->
- [ ] example: the test suite for my feature covers cases x, y, and z

napari/_qt/qt_resources/styles/01_buttons.qss
@@ -1,15 +1,6 @@
 
 /* ----------------- Buttons -------------------- */
 
-QtDeleteButton {
-   image: url("theme_{{ id }}:/delete.svg");
-   min-width : 28px;
-   max-width : 28px;
-   min-height : 28px;
-   max-height : 28px;
-   padding: 0px;
-}
-
 QtViewerPushButton{
    min-width : 28px;
    max-width : 28px;
@@ -17,6 +8,10 @@ QtViewerPushButton{
    max-height : 28px;
    padding: 0px;
    
+}
+
+QtViewerPushButton[mode="delete_button"] {
+   image: url("theme_{{ id }}:/delete.svg");
 }
 
 QtViewerPushButton[mode="new_points"] {

napari/_qt/widgets/qt_viewer_buttons.py
@@ -18,7 +18,6 @@ from napari._qt.widgets.qt_dims_sorter import QtDimsSorter
 from napari._qt.widgets.qt_spinbox import QtSpinBox
 from napari._qt.widgets.qt_tooltip import QtToolTipLabel
 from napari.utils.action_manager import action_manager
-from napari.utils.interactions import Shortcut
 from napari.utils.misc import in_ipython, in_jupyter, in_python_repl
 from napari.utils.translations import trans
 
@@ -52,7 +51,11 @@ class QtLayerButtons(QFrame):
         super().__init__()
 
         self.viewer = viewer
-        self.deleteButton = QtDeleteButton(self.viewer)
+
+        self.deleteButton = QtViewerPushButton(
+            'delete_button', action='napari:delete_selected_layers'
+        )
+
         self.newPointsButton = QtViewerPushButton(
             'new_points',
             trans._('New points layer'),
@@ -335,80 +338,6 @@ class QtViewerButtons(QFrame):
         """
 
         self.viewer.grid.shape = (value, self.viewer.grid.shape[1])
-
-
-class QtDeleteButton(QPushButton):
-    """Delete button to remove selected layers.
-
-    Parameters
-    ----------
-    viewer : napari.components.ViewerModel
-        Napari viewer containing the rendered scene, layers, and controls.
-
-    Attributes
-    ----------
-    hover : bool
-        Hover is true while mouse cursor is on the button widget.
-    viewer : napari.components.ViewerModel
-        Napari viewer containing the rendered scene, layers, and controls.
-    """
-
-    def __init__(self, viewer) -> None:
-        super().__init__()
-
-        self.viewer = viewer
-        self.setToolTip(
-            trans._(
-                "Delete selected layers ({shortcut})",
-                shortcut=Shortcut("Control-Backspace"),
-            )
-        )
-        self.setAcceptDrops(True)
-        self.clicked.connect(lambda: self.viewer.layers.remove_selected())
-
-    def dragEnterEvent(self, event):
-        """The cursor enters the widget during a drag and drop operation.
-
-        Parameters
-        ----------
-        event : qtpy.QtCore.QEvent
-            Event from the Qt context.
-        """
-        event.accept()
-        self.hover = True
-        self.update()
-
-    def dragLeaveEvent(self, event):
-        """The cursor leaves the widget during a drag and drop operation.
-
-        Using event.ignore() here allows the event to pass through the
-        parent widget to its child widget, otherwise the parent widget
-        would catch the event and not pass it on to the child widget.
-
-        Parameters
-        ----------
-        event : qtpy.QtCore.QEvent
-            Event from the Qt context.
-        """
-        event.ignore()
-        self.hover = False
-        self.update()
-
-    def dropEvent(self, event):
-        """The drag and drop mouse event is completed.
-
-        Parameters
-        ----------
-        event : qtpy.QtCore.QDropEvent
-            Event from the Qt context.
-        """
-        event.accept()
-        layer_name = event.mimeData().text()
-        layer = self.viewer.layers[layer_name]
-        if not layer.selected:
-            self.viewer.layers.remove(layer)
-        else:
-            self.viewer.layers.remove_selected()
 
 
 def _omit_viewer_args(constructor):

napari/components/_viewer_key_bindings.py
@@ -76,6 +76,11 @@ def reset_view(viewer: Viewer):
     viewer.reset_view()
 
 
+@register_viewer_action(trans._("Delete selected layers."))
+def delete_selected_layers(viewer: Viewer):
+    viewer.layers.remove_selected()
+
+
 @register_viewer_action(trans._("Increment dimensions slider to the left."))
 def increment_dims_left(viewer: Viewer):
     viewer.dims._increment_dims_left()

napari/utils/shortcuts.py
@@ -9,6 +9,7 @@ default_shortcuts = {
     'napari:toggle_ndisplay': [KeyMod.CtrlCmd | KeyCode.KeyY],
     'napari:toggle_theme': [KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KeyT],
     'napari:reset_view': [KeyMod.CtrlCmd | KeyCode.KeyR],
+    'napari:delete_selected_layers': [KeyMod.CtrlCmd | KeyCode.Delete],
     'napari:show_shortcuts': [KeyMod.CtrlCmd | KeyMod.Alt | KeyCode.Slash],
     'napari:increment_dims_left': [KeyCode.LeftArrow],
     'napari:increment_dims_right': [KeyCode.RightArrow],