944ec32a8815850481fa8fbf6ccddd69ccd5092f
Peter Sobolewski <pete.sd@gmail.com>
2023-03-25 22:41:13 +0100

    Ensure new _base_key_bindings are used

--- napari/layers/base/_base_key_bindings.py
+++ napari/layers/base/_base_key_bindings.py
@@ -3,7 +3,7 @@ from app_model.types import KeyCode
 from napari.layers.base.base import Layer
 
 
-@Layer.bind_key(KeyCode.Space)
+@Layer.bind_key(KeyCode.Space, overwrite=True)
 def hold_to_pan_zoom(layer):
     """Hold to pan and zoom in the viewer."""
     if layer._mode != layer._modeclass.PAN_ZOOM:
