2c00be47330216316213bcc9b126e00c80aca544
Peter Sobolewski <pete.sd@gmail.com>
2023-03-30 20:18:12 +0200

    Make hold-for-pan/zoom a regular viewer keybiding

--- napari/layers/base/__init__.py
+++ napari/layers/base/__init__.py
@@ -1,10 +1,3 @@
-from napari.layers.base import _base_key_bindings
 from napari.layers.base.base import Layer, no_op
-
-# Note that importing _base_key_bindings is needed as layers gets
-# decorated with keybindings during that process, but it is not directly needed
-# by our users and so is deleted below
-del _base_key_bindings
-
 
 __all__ = ['Layer', 'no_op']
--- napari/layers/base/_base_key_bindings.py
+++ napari/layers/base/_base_key_bindings.py
@@ -1,17 +1,17 @@
-from app_model.types import KeyCode
+# from app_model.types import KeyCode
 
-from napari.layers.base.base import Layer
+# from napari.layers.base.base import Layer
 
 
-@Layer.bind_key(KeyCode.Space, overwrite=True)
-def hold_to_pan_zoom(layer):
-    """Hold to pan and zoom in the viewer."""
-    if layer._mode != layer._modeclass.PAN_ZOOM:
-        # on key press
-        prev_mode = layer.mode
-        layer.mode = layer._modeclass.PAN_ZOOM
+# @Layer.bind_key(KeyCode.Space, overwrite=True)
+# def hold_to_pan_zoom(layer):
+#     """Hold to pan and zoom in the viewer."""
+#     if layer._mode != layer._modeclass.PAN_ZOOM:
+#         # on key press
+#         prev_mode = layer.mode
+#         layer.mode = layer._modeclass.PAN_ZOOM
 
-        yield
+#         yield
 
-        # on key release
-        layer.mode = prev_mode
+#         # on key release
+#         layer.mode = prev_mode
