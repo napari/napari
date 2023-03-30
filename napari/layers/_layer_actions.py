222bfd8f96f67f7fb493d95dd7cab84f36c8429a
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2023-03-30 19:36:14 +0200

    Bugfix: Don't double toggle visibility for linked layers (#5656)
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>
    
    # Description
    
    This PR stores the layer visibility states prior to toggling layers and
    then checks against it to ensure that layers are toggled relative to
    that initial state. This makes sure they are not double toggled if
    linked layers are involved.
    Also added a test for the interaction between linked layers and
    toggle_visibility that fails without this PR but now passes.
    
    ## Type of change
    <!-- Please delete options that are not relevant. -->
    - [x] Bug-fix (non-breaking change which fixes an issue)
    
    # References
    Closes #5655 
    
    # How has this been tested?
    <!-- Please describe the tests that you ran to verify your changes. -->
    - [x] I added a test that fails on main but passes here
    - [x] all tests pass with my change 
    
    ## Final checklist:
    - [x] My PR is the minimum possible work for the desired functionality
    
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>

--- napari/layers/_layer_actions.py
+++ napari/layers/_layer_actions.py
@@ -89,8 +89,13 @@ def _merge_stack(ll: LayerList, rgb=False):
 
 
 def _toggle_visibility(ll: LayerList):
-    for lay in ll.selection:
-        lay.visible = not lay.visible
+    current_visibility_state = []
+    for layer in ll.selection:
+        current_visibility_state.append(layer.visible)
+
+    for visibility, layer in zip(current_visibility_state, ll.selection):
+        if layer.visible == visibility:
+            layer.visible = not visibility
 
 
 def _link_selected_layers(ll: LayerList):
