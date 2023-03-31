76a57d0e33fe44b2753eca888a504b16f68921f8
Andy Sweet <andrew.d.sweet@gmail.com>
2023-03-31 11:09:15 -0700

    Move example

--- examples/add_images_with_axis_labels.py
+++ examples/add_images_with_axis_labels.py
@@ -5,27 +5,27 @@ import napari
 viewer = napari.Viewer()
 viewer.axes.visible = True
 
-print(f'{viewer.axis_labels=}')  # -> []
+print(f'{viewer.axis_labels=}')  # -> ()
 
 image = viewer.add_image(
     np.ones((5, 3, 2)),
     axis_labels=("time", "y", "x"),
     colormap='red',
 )
-print(f'{viewer.axis_labels=}')  # -> []
+print(f'{viewer.axis_labels=}')  # -> ("time", "y", "x")
 
 image = viewer.add_image(
     np.ones((4, 3, 2)),
     axis_labels=("z", "y", "x"),
     colormap='green',
 )
-print(f'{viewer.axis_labels=}')  # -> ["z", "time", "y", "x"]
+print(f'{viewer.axis_labels=}')  # -> ("z", "time", "y", "x")
 
 image = viewer.add_image(
     np.ones((6, 4, 3, 2)),
     axis_labels=["freq", "z", "y", "x"],
     colormap='blue',
 )
-print(f'{viewer.axis_labels=}')  # -> ["freq", "z", "time", "y", "x"]
+print(f'{viewer.axis_labels=}')  # -> ("freq", "z", "time", "y", "x")
 
 #napari.run()
