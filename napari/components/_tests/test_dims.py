2602cd6c2b557477bd93d148ec20e838aa05dc2a
Lorenzo Gaifas <brisvag@gmail.com>
2023-04-05 17:57:42 +0200

    revert useless test changes

--- napari/components/_tests/test_dims.py
+++ napari/components/_tests/test_dims.py
@@ -173,12 +173,12 @@ def test_range_set_multiple():
     dims = Dims(ndim=4)
     assert dims.range == ((0, 2, 1),) * 4
 
-    dims.set_range((0, 3), [(0, 6, 1), (0, 9, 1)])
-    assert dims.range == ((0, 6, 1),) + ((0, 2, 1),) * 2 + ((0, 9, 1),)
+    dims.set_range((0, 3), [(0, 6, 3), (0, 9, 3)])
+    assert dims.range == ((0, 6, 3),) + ((0, 2, 1),) * 2 + ((0, 9, 3),)
 
     # last_used will be set to the smallest axis in range
     dims.set_range(range(1, 4), ((0, 5, 1),) * 3)
-    assert dims.range == ((0, 6, 1),) + ((0, 5, 1),) * 3
+    assert dims.range == ((0, 6, 3),) + ((0, 5, 1),) * 3
 
     # test with descending axis order
     dims.set_range(axis=(3, 0), _range=[(0, 4, 1), (0, 6, 1)])
@@ -186,11 +186,11 @@ def test_range_set_multiple():
 
     # out of range axis raises a ValueError
     with pytest.raises(ValueError):
-        dims.set_range((dims.ndim, 0), [(0.0, 4.0, 1)] * 2)
+        dims.set_range((dims.ndim, 0), [(0.0, 4.0, 1.0)] * 2)
 
     # sequence lengths for axis and _range do not match
     with pytest.raises(ValueError):
-        dims.set_range((0, 1), [(0.0, 4.0, 1)] * 3)
+        dims.set_range((0, 1), [(0.0, 4.0, 1.0)] * 3)
 
 
 def test_axis_labels():
