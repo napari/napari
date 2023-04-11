ca3a1b8fa2983e57e885dc48297c45706536a497
Grzegorz Bokota <bokota+github@gmail.com>
2023-04-11 09:53:04 +0200

    fix `__init__.pyi` file

--- napari/__init__.pyi
+++ napari/__init__.pyi
@@ -1,24 +1,34 @@
-import napari._qt.qt_event_loop
-import napari.plugins.io
 import napari.utils.notifications
-import napari.view_layers
-import napari.viewer
+from napari._qt.qt_event_loop import gui_qt as gui_qt
+from napari._qt.qt_event_loop import run as run
+from napari.plugins.io import save_layers as save_layers
+from napari.view_layers import (
+    view_image as view_image,
+)
+from napari.view_layers import (
+    view_labels as view_labels,
+)
+from napari.view_layers import (
+    view_path as view_path,
+)
+from napari.view_layers import (
+    view_points as view_points,
+)
+from napari.view_layers import (
+    view_shapes as view_shapes,
+)
+from napari.view_layers import (
+    view_surface as view_surface,
+)
+from napari.view_layers import (
+    view_tracks as view_tracks,
+)
+from napari.view_layers import (
+    view_vectors as view_vectors,
+)
+from napari.viewer import Viewer as Viewer
+from napari.viewer import current_viewer as current_viewer
 
 __version__: str
 
 notification_manager: napari.utils.notifications.NotificationManager
-Viewer = ...
-current_viewer = ...
-
-gui_qt = ...
-run = ...
-save_layers = ...
-
-view_image = ...
-view_labels = ...
-view_path = ...
-view_points = ...
-view_shapes = ...
-view_surface = ...
-view_tracks = ...
-view_vectors = ...
