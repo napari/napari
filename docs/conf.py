da3bc0aba624b000708539b5fdf5376c64968f7b
Melissa Weber Mendonça <melissawm@gmail.com>
2023-04-14 23:55:55 -0300

    Update URL of version switcher (#139)
    # Description
    Update URL of version switcher to use
    https://napari.org/dev/_static/version_switcher.json
    
    This brings us closer to the goal of napari.github.io containing
    only built artifacts, not source materials.
    
    ## Type of change
    <!-- Please delete options that are not relevant. -->
    - [ ] Fixes or improves existing content
    - [ ] Adds new content page(s)
    - [x] Fixes or improves workflow, documentation build or deployment
    
    # References
    This is a follow-up on #109.
    
    ## Final checklist:
    - [x] My PR is the minimum possible work for the desired functionality
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have added [alt text](https://webaim.org/techniques/alttext/) to
    new images included in this PR

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,7 @@ tags_extension = ["md", "rst"]
 html_theme = 'napari'
 
 # Define the json_url for our version switcher.
-json_url = "https://napari.org/version_switcher.json"
+json_url = "https://napari.org/dev/_static/version_switcher.json"
 
 version_match = "latest" if version == "dev" else release
 
