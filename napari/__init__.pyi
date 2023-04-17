558ca48a2489d12f032e4a647d624deaba1b4293
Grzegorz Bokota <bokota+github@gmail.com>
2023-04-17 12:48:42 +0200

    do not use explicit reexport

--- napari/__init__.pyi
+++ napari/__init__.pyi
@@ -1,18 +1,36 @@
 import napari.utils.notifications
-from napari._qt.qt_event_loop import gui_qt as gui_qt, run as run
-from napari.plugins.io import save_layers as save_layers
+from napari._qt.qt_event_loop import gui_qt, run
+from napari.plugins.io import save_layers
 from napari.view_layers import (
-    view_image as view_image,
-    view_labels as view_labels,
-    view_path as view_path,
-    view_points as view_points,
-    view_shapes as view_shapes,
-    view_surface as view_surface,
-    view_tracks as view_tracks,
-    view_vectors as view_vectors,
+    view_image,
+    view_labels,
+    view_path,
+    view_points,
+    view_shapes,
+    view_surface,
+    view_tracks,
+    view_vectors,
 )
-from napari.viewer import Viewer as Viewer, current_viewer as current_viewer
+from napari.viewer import Viewer, current_viewer
 
 __version__: str
 
 notification_manager: napari.utils.notifications.NotificationManager
+
+__all__ = (
+    'Viewer',
+    'current_viewer',
+    'view_image',
+    'view_labels',
+    'view_path',
+    'view_points',
+    'view_shapes',
+    'view_surface',
+    'view_tracks',
+    'view_vectors',
+    'save_layers',
+    'gui_qt',
+    'run',
+    'notification_manager',
+    '__version__',
+)
