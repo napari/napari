33f2e570cb312461674218e35908cf0622526154
Lorenzo Gaifas <brisvag@gmail.com>
2023-04-19 16:45:17 +0200

    slice is now rounded up

--- napari/components/_tests/test_viewer_model.py
+++ napari/components/_tests/test_viewer_model.py
@@ -318,7 +318,7 @@ def test_view_centering_with_points_add():
 
     viewer = ViewerModel()
     viewer.add_image(image)
-    assert tuple(viewer.dims.point) == (2, 5, 5)
+    assert tuple(viewer.dims.point) == (2.5, 5, 5)
 
     viewer.dims.set_point(0, 0)
     # viewer point shouldn't change after this
@@ -358,7 +358,7 @@ def test_swappable_dims():
     image_data = np.random.random((7, 12, 10, 15))
     image_name = viewer.add_image(image_data).name
     assert np.all(
-        viewer.layers[image_name]._data_view == image_data[3, 6, :, :]
+        viewer.layers[image_name]._data_view == image_data[4, 6, :, :]
     )
 
     points_data = np.random.randint(6, size=(10, 4))
@@ -372,17 +372,17 @@ def test_swappable_dims():
     # midpoints indices into the data below depend on the data range.
     # This depends on the values in vectors_data and thus the random seed.
     assert np.all(
-        viewer.layers[labels_name]._slice.image.raw == labels_data[3, 6, :, :]
+        viewer.layers[labels_name]._slice.image.raw == labels_data[4, 6, :, :]
     )
 
     # Swap dims
     viewer.dims.order = [0, 2, 1, 3]
     assert viewer.dims.order == (0, 2, 1, 3)
     assert np.all(
-        viewer.layers[image_name]._data_view == image_data[3, :, 5, :]
+        viewer.layers[image_name]._data_view == image_data[4, :, 5, :]
     )
     assert np.all(
-        viewer.layers[labels_name]._slice.image.raw == labels_data[3, :, 5, :]
+        viewer.layers[labels_name]._slice.image.raw == labels_data[4, :, 5, :]
     )
 
 
