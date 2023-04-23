4ee8a14e93037d7ef7b70312d37459d9fdf77b3f
Konstantin Sofiiuk <k.sofiyuk@gmail.com>
2023-04-23 20:21:06 +0200

    Remove frozen

--- napari/components/overlays/brush_circle.py
+++ napari/components/overlays/brush_circle.py
@@ -6,4 +6,3 @@ from napari.components.overlays.base import CanvasOverlay
 class BrushCircleOverlay(CanvasOverlay):
     size: int = 10
     position: Tuple[int, int] = (0, 0)
-    frozen: bool = False
