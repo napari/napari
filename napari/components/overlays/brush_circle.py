6316b9316edc2045213430b0bbbec62f56f47676
Konstantin Sofiiuk <k.sofiyuk@gmail.com>
2023-04-24 22:53:20 +0200

    Add docstring to BrushCircleOverlay

--- napari/components/overlays/brush_circle.py
+++ napari/components/overlays/brush_circle.py
@@ -4,5 +4,16 @@ from napari.components.overlays.base import CanvasOverlay
 
 
 class BrushCircleOverlay(CanvasOverlay):
+    """
+    Overlay that displays a circle for a brush on a canvas.
+
+    Attributes
+    ----------
+    size : int
+        The diameter of the brush circle in canvas pixels.
+    position : Tuple[int, int]
+        The position (x, y) of the center of the brush circle on the canvas.
+    """
+
     size: int = 10
     position: Tuple[int, int] = (0, 0)
