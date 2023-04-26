2800657b75c56cf5adb8742245b3be1b72c9ed54
Konstantin Sofiiuk <sofiyuk@gmail.com>
2023-04-26 12:37:42 +0200

    Update napari/_vispy/overlays/brush_circle.py
    Co-authored-by: Lorenzo Gaifas <brisvag@gmail.com>

--- napari/_vispy/overlays/brush_circle.py
+++ napari/_vispy/overlays/brush_circle.py
@@ -66,8 +66,7 @@ class VispyBrushCircleOverlay(ViewerOverlayMixin, VispyCanvasOverlay):
             event.old.events.mouse_move.disconnect(self._on_mouse_move)
 
     def _on_mouse_over_canvas(self):
-        if self.overlay.visible:
-            self.node.visible = self.viewer.mouse_over_canvas
+        self.node.visible = self.overlay.visible and self.viewer.mouse_over_canvas
 
     def reset(self):
         super().reset()
