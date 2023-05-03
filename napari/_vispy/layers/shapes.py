fix shapes scaling

napari/_vispy/layers/shapes.py
@@ -81,10 +81,10 @@ class VispyShapesLayer(VispyBaseLayer):
             face_color,
             edge_color,
             pos,
-            width,
+            _,
         ) = self.layer._compute_vertices_and_box()
 
-        width = settings.appearance.highlight_thickness
+        width = settings.appearance.highlight_thickness / self.layer.scale[-1]
 
         if vertices is None or len(vertices) == 0:
             vertices = np.zeros((1, self.layer._slice_input.ndisplay))

napari/layers/shapes/_shapes_mouse_bindings.py
@@ -102,7 +102,7 @@ def select(layer, event):
 
 def add_line(layer, event):
     """Add a line."""
-    size = layer._vertex_size * layer.scale_factor / 4
+    size = layer._vertex_size * layer.scale_factor / layer.scale[-1] / 4
     full_size = np.zeros(layer.ndim, dtype=float)
     for i in layer._slice_input.displayed:
         full_size[i] = size
@@ -119,7 +119,7 @@ def add_line(layer, event):
 
 def add_ellipse(layer, event):
     """Add an ellipse."""
-    size = layer._vertex_size * layer.scale_factor / 4
+    size = layer._vertex_size * layer.scale_factor / layer.scale[-1] / 4
     size_h = np.zeros(layer.ndim, dtype=float)
     size_h[layer._slice_input.displayed[0]] = size
     size_v = np.zeros(layer.ndim, dtype=float)
@@ -137,7 +137,7 @@ def add_ellipse(layer, event):
 
 def add_rectangle(layer, event):
     """Add a rectangle."""
-    size = layer._vertex_size * layer.scale_factor / 4
+    size = layer._vertex_size * layer.scale_factor / layer.scale[-1] / 4
     size_h = np.zeros(layer.ndim, dtype=float)
     size_h[layer._slice_input.displayed[0]] = size
     size_v = np.zeros(layer.ndim, dtype=float)
@@ -455,7 +455,9 @@ def _move(layer, coordinates):
                 np.linalg.norm(box[Box.TOP_CENTER] - c),
                 np.linalg.norm(box[Box.LEFT_CENTER] - c),
             ]
-            threshold = layer._vertex_size * layer.scale_factor / 2
+            threshold = (
+                layer._vertex_size * layer.scale_factor / layer.scale[-1] / 2
+            )
             scale[abs(scale * size) < threshold] = 1
 
             # check orientation of box

napari/layers/shapes/shapes.py
@@ -2316,7 +2316,11 @@ class Shapes(Layer):
                 box[Box.BOTTOM_LEFT] - box[Box.TOP_LEFT]
             )
             if length_box > 0:
-                r = self._rotation_handle_length * self.scale_factor
+                r = (
+                    self._rotation_handle_length
+                    * self.scale_factor
+                    / self.scale[-1]
+                )
                 rot = (
                     rot
                     - r
@@ -2354,7 +2358,10 @@ class Shapes(Layer):
 
             centers, offsets, triangles = self._data_view.outline(index)
             vertices = centers + (
-                self.scale_factor * self._highlight_width * offsets
+                self.scale_factor
+                * self._highlight_width
+                * offsets
+                / self.scale[-1]
             )
             vertices = vertices[:, ::-1]
         else:
@@ -2598,7 +2605,11 @@ class Shapes(Layer):
         box = self._selected_box - center
         box = np.array(box * scale)
         if not np.all(box[Box.TOP_CENTER] == box[Box.HANDLE]):
-            r = self._rotation_handle_length * self.scale_factor
+            r = (
+                self._rotation_handle_length
+                * self.scale_factor
+                / self.scale[-1]
+            )
             handle_vec = box[Box.HANDLE] - box[Box.TOP_CENTER]
             cur_len = np.linalg.norm(handle_vec)
             box[Box.HANDLE] = box[Box.TOP_CENTER] + r * handle_vec / cur_len
@@ -2617,7 +2628,11 @@ class Shapes(Layer):
         box = self._selected_box - center
         box = box @ transform.T
         if not np.all(box[Box.TOP_CENTER] == box[Box.HANDLE]):
-            r = self._rotation_handle_length * self.scale_factor
+            r = (
+                self._rotation_handle_length
+                * self.scale_factor
+                / self.scale[-1]
+            )
             handle_vec = box[Box.HANDLE] - box[Box.TOP_CENTER]
             cur_len = np.linalg.norm(handle_vec)
             box[Box.HANDLE] = box[Box.TOP_CENTER] + r * handle_vec / cur_len
@@ -2658,7 +2673,9 @@ class Shapes(Layer):
                 distances = abs(box - coord)
 
                 # Get the vertex sizes
-                sizes = self._vertex_size * self.scale_factor / 2
+                sizes = (
+                    self._vertex_size * self.scale_factor / self.scale[-1] / 2
+                )
 
                 # Check if any matching vertices
                 matches = np.all(distances <= sizes, axis=1).nonzero()
@@ -2673,7 +2690,9 @@ class Shapes(Layer):
                 distances = abs(vertices - coord)
 
                 # Get the vertex sizes
-                sizes = self._vertex_size * self.scale_factor / 2
+                sizes = (
+                    self._vertex_size * self.scale_factor / self.scale[-1] / 2
+                )
 
                 # Check if any matching vertices
                 matches = np.all(distances <= sizes, axis=1).nonzero()[0]