d278b5a9ab779ef2719cc089c37079bd52d74503
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2023-05-22 11:11:22 -0700

    add new delaunay graph example

---
+++ examples/nuclei_segmentation_graph.py
@@ -0,0 +1,42 @@
+"""
+Nuclei Segmentation Graph
+===============
+
+Creates a delaunay graph from maxima of cell nuclei.
+
+.. tags:: visualization-nD
+"""
+from itertools import combinations
+
+import numpy as np
+from napari_graph import UndirectedGraph
+from scipy.spatial import Delaunay
+from skimage import data, feature, filters
+
+import napari
+
+
+def delaunay_edges(points: np.ndarray) -> np.ndarray:
+    delaunay = Delaunay(points)
+    edges = []
+    for simplex in delaunay.simplices:
+        edges += list(combinations(simplex, 2))
+
+    return edges
+
+
+cells = data.cells3d()
+
+nuclei = cells[:, 1]
+smooth = filters.gaussian(nuclei, sigma=10)
+nodes_coords = feature.peak_local_max(smooth)
+edges = delaunay_edges(nodes_coords)
+graph = UndirectedGraph(edges, nodes_coords)
+viewer = napari.view_image(
+    cells, channel_axis=1, name=['membranes', 'nuclei'], ndisplay=3
+)
+viewer.add_graph(graph)
+viewer.camera.angles = (10, -20, 130)
+
+if __name__ == '__main__':
+    napari.run()
