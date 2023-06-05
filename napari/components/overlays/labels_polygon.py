60e5ecbd5011a7555a87b7e07d277cab944e0d79
Konstantin Sofiiuk <k.sofiyuk@gmail.com>
2023-06-05 23:39:00 +0300

    Refactor the overlay docstring
    Co-authored-by: Juan Nunez-Iglesias <jni@fastmail.com>

--- napari/components/overlays/labels_polygon.py
+++ napari/components/overlays/labels_polygon.py
@@ -5,14 +5,18 @@ from napari.layers import Labels
 class LabelsPolygonOverlay(SceneOverlay):
     """Overlay that displays a polygon on a scene.
 
-        It handles the following mouse events to update the overlay:
-        - Mouse move: Continuously redraw the latest polygon point with the current mouse position.
-        - Mouse press (left button): Adds the current mouse position as a new polygon point.
-        - Mouse double click (left button): If there are at least three points in the polygon and
-                      the double-click position is within completion_radius from the first vertex,
-                      the polygon will be painted in the image using the current label.
-
-        - Mouse press (right button): Removes the most recent polygon point from the list.
+    This overlay was created for drawing polygons on Labels layers. It handles
+    the following mouse events to update the overlay:
+    - Mouse move: Continuously redraw the latest polygon point with the current
+    mouse position.
+    - Mouse press (left button): Adds the current mouse position as a new
+    polygon point.
+    - Mouse double click (left button): If there are at least three points in
+    the polygon and the double-click position is within completion_radius
+    from the first vertex, the polygon will be painted in the image using the
+    current label.
+    - Mouse press (right button): Removes the most recent polygon point from
+    the list.
 
     Attributes
     ----------
