77afa9b064f4ae137d544a906b1b91aa7e150446
Grzegorz Bokota <bokota+github@gmail.com>
2023-06-07 17:03:31 +0200

    add script to list opened PR for a given release
    [skip ci]

---
+++ tools/release/list_opened_pr.py
@@ -0,0 +1,33 @@
+import argparse
+
+from release_utils import (
+    GH_REPO,
+    GH_USER,
+    get_github,
+    setup_cache,
+)
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument('milestone', help='The milestone to list')
+
+args = parser.parse_args()
+
+setup_cache()
+
+pull_list = []
+
+
+for pull_issue in tqdm(
+    get_github().search_issues(
+        f'repo:{GH_USER}/{GH_REPO} is:pr is:open sort:created-asc'
+    ),
+    desc='Pull Requests...',
+):
+    pull = pull_issue.as_pull_request()
+    if pull.milestone and pull.milestone.title == args.milestone:
+        pull_list.append(pull)
+
+print(f"## {len(pull_list)} opened PRs for milestone {args.milestone}")
+for pull in pull_list:
+    print(f"* [ ] #{pull.number}")
