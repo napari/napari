21b0b643b0afa18c84f46c93cf98297ab1fc6d30
Peter Sobolewski <pete.sd@gmail.com>
2023-06-13 19:36:47 +0200

    Add tests for all 3 cases of alt-click behavior

--- napari/_qt/containers/_tests/test_qt_layer_list.py
+++ napari/_qt/containers/_tests/test_qt_layer_list.py
@@ -35,16 +35,14 @@ def test_set_item_unchecked_makes_layer_invisible(qtbot):
 
 
 def test_alt_click_to_show_single_layer(qtbot):
-    image1 = Image(np.zeros((4, 3)))
-    image2 = Image(np.zeros((4, 3)))
-    image3 = Image(np.zeros((4, 3)))
-
-    layers = LayerList([image1, image2, image3])
-    # this will make the list have image2 on top of image1
-    view = QtLayerList(layers)
-    qtbot.addWidget(view)
-
-    delegate = LayerDelegate()
+    (
+        image1,
+        image2,
+        image3,
+        layers,
+        view,
+        delegate,
+    ) = make_qt_layer_list_with_delegate(qtbot)
 
     # hide the middle-layer, image2 and ensure it's unchecked
     image2.visible = False
@@ -56,9 +54,15 @@ def test_alt_click_to_show_single_layer(qtbot):
     assert image1.visible
     assert check_state_at_layer_index(view, 2) == Qt.CheckState.Checked
 
+    # alt-click state should be None
+    assert delegate._alt_click_layer() is None
+
     # mock an alt-click on bottom-most layer, image1
     index = layer_to_model_index(view, 2)
     delegate._show_on_alt_click_hide_others(view.model(), index)
+
+    # alt-click state should be set to image1
+    assert delegate._alt_click_layer() == image1
 
     # only image1 should be shown, while image3, image2 be hidden
     assert not image3.visible
@@ -67,6 +71,58 @@ def test_alt_click_to_show_single_layer(qtbot):
     assert check_state_at_layer_index(view, 1) == Qt.CheckState.Unchecked
     assert image1.visible
     assert check_state_at_layer_index(view, 2) == Qt.CheckState.Checked
+
+
+def test_second_alt_click_to_show_different_layer(qtbot):
+    (
+        image1,
+        image2,
+        image3,
+        layers,
+        view,
+        delegate,
+    ) = make_qt_layer_list_with_delegate(qtbot)
+
+    # mock an alt-click on bottom-most layer, image1
+    index = layer_to_model_index(view, 2)
+    delegate._show_on_alt_click_hide_others(view.model(), index)
+
+    # alt-click state should be set to image1
+    assert delegate._alt_click_layer() == image1
+
+    # image2 should be hidden
+    assert not image2.visible
+    assert check_state_at_layer_index(view, 1) == Qt.CheckState.Unchecked
+
+    # mock an alt-click on middle layer, image2
+    index2 = layer_to_model_index(view, 1)
+    delegate._show_on_alt_click_hide_others(view.model(), index2)
+
+    # alt-click state should be set to image2
+    assert delegate._alt_click_layer() == image2
+
+    # only image2 should be shown, while image3, image1 be hidden
+    assert not image3.visible
+    assert check_state_at_layer_index(view, 0) == Qt.CheckState.Unchecked
+    assert not image1.visible
+    assert check_state_at_layer_index(view, 2) == Qt.CheckState.Unchecked
+    assert image2.visible
+    assert check_state_at_layer_index(view, 1) == Qt.CheckState.Checked
+
+
+def test_second_alt_click_to_restore_layer_state(qtbot):
+    (
+        image1,
+        image2,
+        image3,
+        layers,
+        view,
+        delegate,
+    ) = make_qt_layer_list_with_delegate(qtbot)
+
+    # mock an alt-click on bottom-most layer, image1
+    index = layer_to_model_index(view, 2)
+    delegate._show_on_alt_click_hide_others(view.model(), index)
 
     # add a layer (will be at position 0)
     image4 = Image(np.zeros((4, 3)))
@@ -84,6 +140,23 @@ def test_alt_click_to_show_single_layer(qtbot):
     assert image4.visible
     assert image1.visible
     assert not image2.visible
+
+    # alt-click state should be cleared
+    assert delegate._alt_click_layer() is None
+
+
+def make_qt_layer_list_with_delegate(qtbot):
+    image1 = Image(np.zeros((4, 3)))
+    image2 = Image(np.zeros((4, 3)))
+    image3 = Image(np.zeros((4, 3)))
+
+    layers = LayerList([image1, image2, image3])
+    # this will make the list have image2 on top of image1
+    view = QtLayerList(layers)
+    qtbot.addWidget(view)
+
+    delegate = LayerDelegate()
+    return image1, image2, image3, layers, view, delegate
 
 
 def make_qt_layer_list_with_layer(qtbot) -> Tuple[QtLayerList, Image]:
