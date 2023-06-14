5d7ff239010e1b30f1ef588dad0d78f16bedb17c
Wouter-Michiel Vierdag <w-mv@hotmail.com>
2023-06-14 14:43:52 +0200

    refactor
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>

--- napari/_tests/test_windowsettings.py
+++ napari/_tests/test_windowsettings.py
@@ -22,12 +22,6 @@ def test_singlescreen_window_settings(make_napari_viewer):
 
     # Invalid right of screen
     settings.application.window_position = (0, 40000)
-    (
-        _,
-        _,
-        window_position,
-        _,
-        _,
-    ) = viewer.window._qt_window._load_window_settings()
+    window_position = viewer.window._qt_window._load_window_settings()[2]
     assert window_position == default_window_position
     viewer.close()
