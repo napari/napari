b51c35f67f1bfdea96f045bb693fe4baee15bdb8
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2023-06-20 08:18:22 +0200

    Feature: alt-click visibility eye to toggle show just that layer (hide others) (#5618)
    # Description
    
    This PR implements an alt/option-click on the layer visibility icon
    (eye), that mimics Photoshop:
    - On Alt/Option-click, that singular layer is shown and everything else
    is hidden.
    - If you alt-click the *same layer* again, then the visibility state of
    all layers will be restored.
    - If you alt-click a different layer, that one will be shown and
    everything else will be hidden.
    
    To achieve this, the visibility state of layers is stored when toggled.
    
    Overall, this feature is super handy for isolating a single layer but
    not losing the overall visibility of other layers in the case of a
    complex stack.
    
    ## Type of change
    - [x] New feature (non-breaking change which adds functionality)
    - [x] This change requires a documentation update: please see docs PR:
    https://github.com/napari/docs/pull/121
    
    # References
    Partially addresses: https://github.com/napari/napari/issues/5253 and
    https://github.com/napari/napari/issues/1338
    - it doesn't use double click, because I think that could be error
    prone, instead alt/option-click (like Photoshop), which is more of a
    deliberate decision.
    
    cc: @haesleinhuepf @GenevieveBuckley
    
    Partially addresses https://github.com/napari/napari/issues/2550
     - it doesn't change the colormap
    
    Note: this was split off, as feature creep, from
    https://github.com/napari/napari/pull/5574
     
    # How has this been tested?
    <!-- Please describe the tests that you ran to verify your changes. -->
    - [x] example: added a test to ensure the alt-click logic works properly
    
    ---------
    
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>

--- napari/_qt/containers/_tests/test_qt_layer_list.py
+++ napari/_qt/containers/_tests/test_qt_layer_list.py
@@ -5,6 +5,7 @@ from qtpy.QtCore import QModelIndex, Qt
 from qtpy.QtWidgets import QLineEdit, QStyleOptionViewItem
 
 from napari._qt.containers import QtLayerList
+from napari._qt.containers._layer_delegate import LayerDelegate
 from napari.components import LayerList
 from napari.layers import Image
 
@@ -31,6 +32,131 @@ def test_set_item_unchecked_makes_layer_invisible(qtbot):
     )
 
     assert not image.visible
+
+
+def test_alt_click_to_show_single_layer(qtbot):
+    (
+        image1,
+        image2,
+        image3,
+        layers,
+        view,
+        delegate,
+    ) = make_qt_layer_list_with_delegate(qtbot)
+
+    # hide the middle-layer, image2 and ensure it's unchecked
+    image2.visible = False
+    assert check_state_at_layer_index(view, 1) == Qt.CheckState.Unchecked
+
+    # ensure the other layers are visible & checked
+    assert image3.visible
+    assert check_state_at_layer_index(view, 0) == Qt.CheckState.Checked
+    assert image1.visible
+    assert check_state_at_layer_index(view, 2) == Qt.CheckState.Checked
+
+    # alt-click state should be None
+    assert delegate._alt_click_layer() is None
+
+    # mock an alt-click on bottom-most layer, image1
+    index = layer_to_model_index(view, 2)
+    delegate._show_on_alt_click_hide_others(view.model(), index)
+
+    # alt-click state should be set to image1
+    assert delegate._alt_click_layer() == image1
+
+    # only image1 should be shown, while image3, image2 be hidden
+    assert not image3.visible
+    assert check_state_at_layer_index(view, 0) == Qt.CheckState.Unchecked
+    assert not image2.visible
+    assert check_state_at_layer_index(view, 1) == Qt.CheckState.Unchecked
+    assert image1.visible
+    assert check_state_at_layer_index(view, 2) == Qt.CheckState.Checked
+
+
+def test_second_alt_click_to_show_different_layer(qtbot):
+    (
+        image1,
+        image2,
+        image3,
+        layers,
+        view,
+        delegate,
+    ) = make_qt_layer_list_with_delegate(qtbot)
+
+    # mock an alt-click on bottom-most layer, image1
+    index = layer_to_model_index(view, 2)
+    delegate._show_on_alt_click_hide_others(view.model(), index)
+
+    # alt-click state should be set to image1
+    assert delegate._alt_click_layer() == image1
+
+    # image2 should be hidden
+    assert not image2.visible
+    assert check_state_at_layer_index(view, 1) == Qt.CheckState.Unchecked
+
+    # mock an alt-click on middle layer, image2
+    index2 = layer_to_model_index(view, 1)
+    delegate._show_on_alt_click_hide_others(view.model(), index2)
+
+    # alt-click state should be set to image2
+    assert delegate._alt_click_layer() == image2
+
+    # only image2 should be shown, while image3, image1 be hidden
+    assert not image3.visible
+    assert check_state_at_layer_index(view, 0) == Qt.CheckState.Unchecked
+    assert not image1.visible
+    assert check_state_at_layer_index(view, 2) == Qt.CheckState.Unchecked
+    assert image2.visible
+    assert check_state_at_layer_index(view, 1) == Qt.CheckState.Checked
+
+
+def test_second_alt_click_to_restore_layer_state(qtbot):
+    (
+        image1,
+        image2,
+        image3,
+        layers,
+        view,
+        delegate,
+    ) = make_qt_layer_list_with_delegate(qtbot)
+
+    # mock an alt-click on bottom-most layer, image1
+    index = layer_to_model_index(view, 2)
+    delegate._show_on_alt_click_hide_others(view.model(), index)
+
+    # add a layer (will be at position 0)
+    image4 = Image(np.zeros((4, 3)))
+    layers.append(image4)
+    assert image4.visible
+
+    # remove a layer (image3, which has been pushed down to position 1
+    layers.pop(1)
+
+    # mock second alt-click on image1, which should restore initial state
+    delegate._show_on_alt_click_hide_others(view.model(), index)
+
+    # image4 should remain visible (not part of initial state)
+    # image2 should be not visible--that was the initial state
+    assert image4.visible
+    assert image1.visible
+    assert not image2.visible
+
+    # alt-click state should be cleared
+    assert delegate._alt_click_layer() is None
+
+
+def make_qt_layer_list_with_delegate(qtbot):
+    image1 = Image(np.zeros((4, 3)))
+    image2 = Image(np.zeros((4, 3)))
+    image3 = Image(np.zeros((4, 3)))
+
+    layers = LayerList([image1, image2, image3])
+    # this will make the list have image2 on top of image1
+    view = QtLayerList(layers)
+    qtbot.addWidget(view)
+
+    delegate = LayerDelegate()
+    return image1, image2, image3, layers, view, delegate
 
 
 def make_qt_layer_list_with_layer(qtbot) -> Tuple[QtLayerList, Image]:
