877d3a5142844623bd39b042d964eb3f7fa695e3
Chris-N-K <chnauroth@web.de>
2023-06-22 15:51:06 +0200

    first draft lock_dims_while_rolling

--- napari/_qt/widgets/qt_dims_sorter.py
+++ napari/_qt/widgets/qt_dims_sorter.py
@@ -6,6 +6,7 @@ from qtpy.QtWidgets import QGridLayout, QLabel, QWidget
 from napari._qt.containers import QtListView
 from napari._qt.widgets.qt_tooltip import QtToolTipLabel
 from napari.components import Dims
+from napari.settings import get_settings
 from napari.utils.events import SelectableEventedList
 from napari.utils.translations import trans
 
@@ -37,9 +38,11 @@ class AxisModel:
 
 
 def set_dims_order(dims: Dims, order: Tuple[int, ...]):
+    dims_order = list(dims.order)
     if type(order[0]) == AxisModel:
         order = [a.axis for a in order]
-    dims.order = order
+    dims_order[-dims.n_moveable_dims:] = order  # only modify the allowed number of dimensions
+    dims.order = tuple(dims_order)
 
 
 def _array_in_range(arr: np.ndarray, low: int, high: int) -> bool:
@@ -51,6 +54,10 @@ def move_indices(axes_list: SelectableEventedList, order: Tuple[int, ...]):
         if tuple(axes_list) == tuple(order):
             return
         axes = [a.axis for a in axes_list]
+        # only handle the allowed number of dimensions
+        n_moveable_dims = axes_list[0].dims.n_moveable_dims
+        order = order[-n_moveable_dims:]
+
         ax_to_existing_position = {a: ix for ix, a in enumerate(axes)}
         move_list = np.asarray(
             [(ax_to_existing_position[order[i]], i) for i in range(len(order))]
@@ -71,20 +78,23 @@ class QtDimsSorter(QWidget):
 
     def __init__(self, viewer: 'Viewer', parent=None) -> None:
         super().__init__(parent=parent)
-        dims = viewer.dims
-        root = SelectableEventedList(
-            [AxisModel(dims, dims.order[i]) for i in range(dims.ndim)]
+        self.dims = viewer.dims
+        self.axes_list= SelectableEventedList(
+            [AxisModel(self.dims, self.dims.order[-i]) for i in range(1, self.dims.n_moveable_dims + 1)][::-1]
         )
-        root.events.reordered.connect(
-            lambda event: set_dims_order(dims, event.value)
+        self.axes_list.events.reordered.connect(
+            lambda event: set_dims_order(self.dims, event.value)
         )
-        dims.events.order.connect(
-            lambda event: move_indices(root, event.value)
+        self.dims.events.order.connect(
+            lambda event: move_indices(self.axes_list, event.value)
         )
-        view = QtListView(root)
+        # BUG: If one switches to n_movable_dims 2 rolls and goes back to more n_movable_dims rolling will still be just through two dims despite showing more in the QtDimsSorter, reordering in the widget resolves the bug. I probably have to connect an additional event somewhere.
+        get_settings().application.events.n_moveable_dims.connect(
+            # TODO: I would love to connect it to dims.events.n_movable_dims instead and use the same approach than the other two connections, but this causes update issues
+            self._update_axes_list
+        )
+        view = QtListView(self.axes_list)
         view.setSizeAdjustPolicy(QtListView.AdjustToContents)
-
-        self.axes_list = root
 
         layout = QGridLayout()
         self.setLayout(layout)
@@ -98,3 +108,10 @@ class QtDimsSorter(QWidget):
         self.layout().addWidget(widget_title, 0, 0)
         self.layout().addWidget(widget_tooltip, 0, 1)
         self.layout().addWidget(view, 1, 0)
+    
+    def _update_axes_list(self, event):
+        if len(self.axes_list) != event.value:
+            self.axes_list.clear()
+            self.axes_list.extend(
+                [AxisModel(self.dims, self.dims.order[-i]) for i in range(1, self.dims.n_moveable_dims + 1)][::-1]
+            )
