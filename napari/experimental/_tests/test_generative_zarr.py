73d6613e4ebc67b86bc275bf9fd3c369c8ea64f4
Kim Pevey <kpevey@quansight.com>
2023-07-07 13:56:55 -0500

    cleanup

--- napari/experimental/_tests/test_generative_zarr.py
+++ napari/experimental/_tests/test_generative_zarr.py
@@ -1,27 +1,32 @@
 import pytest
 
-from napari.experimental._generative_zarr import MandelbrotStore, \
-    create_meta_store, tile_bounds, mandelbrot, hypercomplex_exponentiation, \
-    mandelbulb, MandelbulbStore
+from napari.experimental._generative_zarr import (
+    MandelbrotStore,
+    MandelbulbStore,
+)
 
 
 @pytest.mark.parametrize('max_level', [8, 14])
 def test_MandlebrotStore(max_level):
     store = MandelbrotStore(
-        levels=max_level, 
-        tilesize=512, 
-        compressor=None, 
-        maxiter=255
+        levels=max_level,
+        tilesize=512,
+        compressor=None,
+        maxiter=255,
     )
-    assert len(store.listdir()) == max_level + 2  # number of levels + .zattrs and .zgroup)
+    assert (
+        len(store.listdir()) == max_level + 2
+    )  # number of levels + .zattrs and .zgroup
 
 
 @pytest.mark.parametrize('max_level', [8, 14])
 def test_MandelbulbStore(max_level):
     store = MandelbulbStore(
-            levels=max_level,
-            tilesize=32,
-            compressor=None,
-            maxiter=255
-        )
-    assert len(store.listdir()) == max_level + 2  # number of levels + .zattrs and .zgroup)
+        levels=max_level,
+        tilesize=32,
+        compressor=None,
+        maxiter=255,
+    )
+    assert (
+        len(store.listdir()) == max_level + 2
+    )  # number of levels + .zattrs and .zgroup
