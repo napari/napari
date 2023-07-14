45383530eedaec1befd87d1da4ce6b97862f29fe
Andy Sweet <andrew.d.sweet@gmail.com>
2023-07-14 16:17:32 -0700

    Simplify typing

--- napari/layers/utils/_state_dict.py
+++ napari/layers/utils/_state_dict.py
@@ -2,18 +2,18 @@ import warnings
 from typing import Any
 
 
-class LayerStateDict(dict[str, Any]):
+class LayerStateDict(dict):
     def __init__(self, *args, **kwargs) -> None:
         # Maps from deprecations keys to their deprecation messages.
         self.deprecations: dict[str, str] = {}
         super().__init__(*args, **kwargs)
 
-    def __getitem__(self, key: str) -> Any:
+    def __getitem__(self, key: Any) -> Any:
         if message := self.deprecations.get(key):
             warnings.warn(message, DeprecationWarning)
         return super().__getitem__(key)
 
-    def __setitem__(self, key: str, value: Any) -> None:
+    def __setitem__(self, key: Any, value: Any) -> None:
         if message := self.deprecations.get(key):
             warnings.warn(message, DeprecationWarning)
         return super().__setitem__(key, value)
