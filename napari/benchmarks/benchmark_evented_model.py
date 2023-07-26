45a18c4eedb3f1c3dd660c957a5f55ef6abc17e4
Grzegorz Bokota <bokota+github@gmail.com>
2023-07-26 22:17:21 +0200

    add assigment
    add second test
    
    use fibonacci for benchmark

--- napari/benchmarks/benchmark_evented_model.py
+++ napari/benchmarks/benchmark_evented_model.py
@@ -1,50 +1,38 @@
-import numpy as np
-from pydantic import Field
+from math import ceil
 
 from napari.utils.events import EventedModel
-
-
-def np_generator(ndim: int = 3):
-    data = np.zeros((ndim, ndim))
-    for i in range(ndim):
-        data[i, ndim - i - 1] = 1
-    return data
 
 
 def empty(event):
     pass
 
 
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
 class Model(EventedModel):
     a: int = 3
     b: float = 2.0
-    c: np.ndarray = Field(default_factory=np_generator)
-
-    @property
-    def c_inv(self):
-        return np.linalg.inv(self.c)
-
-    @c_inv.setter
-    def c_inv(self, value):
-        self.c = np.linalg.inv(value)
+    c: int = 3
 
     @property
     def d(self):
-        return self.a**self.b
+        return (self.c + self.a) ** self.b
 
     @d.setter
     def d(self, value):
+        self.c = value
         self.a = value
-        self.b = value / 2
-        self.c = np_generator(value)
+        self.b = value * 1.1
 
     @property
     def e(self):
-        return (self.c + self.a) ** self.b
-
-    @property
-    def f(self):
-        return self.c_inv**self.a
+        return (fibonacci(self.c) + fibonacci(self.a)) ** fibonacci(
+            ceil(self.b)
+        )
 
 
 class EventedModelSuite:
@@ -55,9 +43,17 @@ class EventedModelSuite:
         self.model.events.a.connect(empty)
         self.model.events.b.connect(empty)
         self.model.events.c.connect(empty)
-        self.model.events.c_inv.connect(empty)
         self.model.events.e.connect(empty)
-        self.model.events.f.connect(empty)
 
     def time_event_firing(self):
         self.model.d = 4
+        self.model.d = 18
+
+    def time_long_connection(self):
+        def long_connection(event):
+            for _i in range(5):
+                fibonacci(event.source.c)
+
+        self.model.events.e.connect(long_connection)
+
+        self.model.d = 15
