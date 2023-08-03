aee177134c202757be44cde69babe62b658d1279
Chris-N-K <chnauroth@web.de>
2023-08-03 10:05:20 +0200

    added parent widget, test for callback management

--- napari/_qt/widgets/_tests/test_qt_dims_sorter.py
+++ napari/_qt/widgets/_tests/test_qt_dims_sorter.py
@@ -1,25 +1,45 @@
+from qtpy.QtWidgets import QWidget
+
 from napari._qt.widgets.qt_dims_sorter import QtDimsSorter
 from napari.components.viewer_model import ViewerModel
 
 
 def test_dims_sorter(qtbot):
     viewer = ViewerModel()
-    dim_sorter = QtDimsSorter(viewer)
+    parent = QWidget()
+    dim_sorter = QtDimsSorter(viewer, parent)
     qtbot.addWidget(dim_sorter)
-    assert tuple(dim_sorter.axes_list) == (0, 1)
+    assert tuple(dim_sorter.axis_list) == (0, 1)
 
     viewer.dims.axis_labels = ('y', 'x')
-    assert tuple(dim_sorter.axes_list) == ('y', 'x')
+    assert tuple(dim_sorter.axis_list) == ('y', 'x')
 
-    dim_sorter.axes_list.move(1, 0)
-    assert tuple(dim_sorter.axes_list) == ('x', 'y')
+    dim_sorter.axis_list.move(1, 0)
+    assert tuple(dim_sorter.axis_list) == ('x', 'y')
     assert tuple(viewer.dims.order) == (1, 0)
+
+
+def test_dims_sorter_callback_management(qtbot):
+    viewer = ViewerModel()
+    parent = QWidget()
+    base_callback_count = len(viewer.dims.events.order.callbacks)
+    dim_sorter = QtDimsSorter(viewer, parent)
+    qtbot.addWidget(dim_sorter)
+
+    # assert callback hook up
+    assert len(viewer.dims.events.order.callbacks) == base_callback_count + 1
+    assert len(dim_sorter.axis_list.events.reordered.callbacks) == 2
+
+    # assert callback termination
+    parent.destroyed.emit()
+    assert len(viewer.dims.events.order.callbacks) == base_callback_count
+    assert len(dim_sorter.axis_list.events.reordered.callbacks) == 1
 
 
 def test_dims_sorter_with_reordered_init(qtbot):
     viewer = ViewerModel()
+    parent = QWidget()
     viewer.dims.order = (1, 0)
-
-    dim_sorter = QtDimsSorter(viewer)
+    dim_sorter = QtDimsSorter(viewer, parent)
     qtbot.addWidget(dim_sorter)
-    assert tuple(dim_sorter.axes_list) == tuple(viewer.dims.order)
+    assert tuple(dim_sorter.axis_list) == tuple(viewer.dims.order)
