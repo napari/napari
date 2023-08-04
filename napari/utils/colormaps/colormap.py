c2616689599a885aacf5b2cee592531c6d7f7412
Egor Zindy <ezindy@gmail.com>
2023-08-04 02:27:36 +0200

    Automatic recognition of hex colour strings in layer data (#6102)
    This PR adds the automatic recognition of hex colour strings in layer
    data and if necessary, generates a linear colormap from black to that
    colour and adds it to the list of available colormaps. If the hex colour
    string defines a bright {white | red | green | blue | cyan | magenta |
    yellow} colour, then the corresponding colormap will be used instead of
    adding a new one.
    
    My goal with this fix is to simplify colour handling in reader plugins
    (I especially had
    [napari-czifile2](https://github.com/BodenmillerGroup/napari-czifile2)
    in mind) by being able to just specify a channel colour as a hex string
    and let napari generate (or reuse) a colormap for this particular
    colour.
    
    I first mentioned this work on the [image.sc
    forum](https://forum.image.sc/t/napari-development-question-about-colormaps-in-the-image-layer/84264)
    and got one favourable return (thanks Juan!).
    
    <!-- Please delete options that are not relevant. -->
    - [x] New feature (non-breaking change which adds functionality)
    
    I used
    [napari-czifile2](https://github.com/BodenmillerGroup/napari-czifile2)
    to check my addition with the following code (in Jupyter):
    
    ```python
    import napari_czifile2
    import napari
    
    fn = 'somefile.czi'
    layer = napari_czifile2._reader.reader_function(fn)[0]
    
    layer[1]['colormap'] = ['#f0F','#fdff00fF']
    
    viewer = napari.Viewer()
    viewer.add_image(layer[0], **layer[1])
    napari.run()
    ```
    
    - #rgb #rgba #rrggbb #rrggbbaa combinations
    - used in combination with `None` in the colormap list
    - case (in)sensitivity
    - one of the colours for which there is a colormap already
    (r,g,b,c,m,y,w)
    - custom colours and how their name look in the napari viewer colormap
    list
    - malformed hex colours
    - missing or extra elements in colormap (extra is fine, missing is not)
    
    ```
    IndexError: Error adding multichannel image with data shape (1, 121, 2, 1024, 1024).
    Requested channel_axis (2) had length 2, but the 'colormap' argument only provided 1 values.
    ```
    
    The existing exception error message should be enough to understand what
    happened (hex colour string used was `'#ff'`) :
    ```
    KeyError: 'Colormap "#ff" not found in either vispy or matplotlib. Recognized colormaps are: "GrBu", "GrBu_d", "PiYG", "PuGr", "RdBu", "RdYeBuCy", "autumn", "blues", "cool", "coolwarm", "cubehelix", "diverging", "fire", "gist_earth", "gray", "gray_r", "grays", "greens", "hot", "hsl", "hsv", "husl", "ice", "inferno", "light_blues", "magma", "orange", "plasma", "reds", "single_hue", "spring", "summer", "turbo", "twilight", "twilight_shifted", "viridis", "winter"'
    ```
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>
    Co-authored-by: Lorenzo Gaifas <brisvag@gmail.com>

--- napari/utils/colormaps/colormap.py
+++ napari/utils/colormaps/colormap.py
@@ -1,9 +1,9 @@
 from collections import defaultdict
 from enum import Enum
-from typing import Optional
+from typing import Optional, cast
 
 import numpy as np
-from pydantic import PrivateAttr, validator
+from pydantic import Field, PrivateAttr, validator
 
 from napari.utils.color import ColorArray
 from napari.utils.colormaps.colorbars import make_colorbar
@@ -51,7 +51,7 @@ class Colormap(EventedModel):
     name: str = 'custom'
     _display_name: Optional[str] = PrivateAttr(None)
     interpolation: ColormapInterpolationMode = ColormapInterpolationMode.LINEAR
-    controls: Array[np.float32, (-1,)] = None
+    controls: Array = Field(default_factory=lambda: cast(Array, []))
 
     def __init__(
         self, colors, display_name: Optional[str] = None, **data
@@ -63,7 +63,7 @@ class Colormap(EventedModel):
         self._display_name = display_name
 
     # controls validator must be called even if None for correct initialization
-    @validator('controls', pre=True, always=True)
+    @validator('controls', pre=True, always=True, allow_reuse=True)
     def _check_controls(cls, v, values):
         # If no control points provided generate defaults
         if v is None or len(v) == 0:
