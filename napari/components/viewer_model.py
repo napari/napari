cd0c74ba16cc8a5af5f9c5384178ec0d51257def
Juan Nunez-Iglesias <jni@fastmail.com>
2023-08-05 10:57:03 +1000

    Add support for specific list of layers in reset_view

--- napari/components/viewer_model.py
+++ napari/components/viewer_model.py
@@ -343,7 +343,7 @@ class ViewerModel(KeymapProvider, MousemapProvider, EventedModel):
         """Simple string representation"""
         return f'napari.Viewer: {self.title}'
 
-    def _sliced_extent_world_augmented(self) -> np.ndarray:
+    def _sliced_extent_world_augmented(self, layers=None) -> np.ndarray:
         """Extent of layers in world coordinates after slicing.
 
         D is either 2 or 3 depending on if the displayed data is 2D or 3D.
@@ -352,20 +352,30 @@ class ViewerModel(KeymapProvider, MousemapProvider, EventedModel):
         -------
         sliced_extent_world : array, shape (2, D)
         """
-        # if not layers are present, assume image-like with dimensions of size 512
+        layers = LayerList(layers) if layers is not None else self.layers
         if len(self.layers) == 0:
+            # if no layers are present, assume image-like
+            # with dimensions of size 512
             return np.vstack(
                 [np.full(self.dims.ndim, -0.5), np.full(self.dims.ndim, 511.5)]
             )
-        return self.layers._extent_world_augmented[:, self.dims.displayed]
+        return layers._extent_world_augmented[:, self.dims.displayed]
 
-    def reset_view(self):
-        """Reset the camera view."""
+    def reset_view(self, layers=None):
+        """Reset the camera view.
 
-        extent = self._sliced_extent_world_augmented()
+        Parameters
+        ----------
+        layers : optional list of layers
+            If given, only consider the extent of the given layers when
+            resetting the view. Otherwise, all layers in viewer.layers are
+            used.
+        """
+
+        extent = self._sliced_extent_world_augmented(layers)
         scene_size = extent[1] - extent[0]
         corner = extent[0]
-        grid_size = list(self.grid.actual_shape(len(self.layers)))
+        grid_size = list(self.grid.actual_shape(self.layers))
         if len(scene_size) > len(grid_size):
             grid_size = [1] * (len(scene_size) - len(grid_size)) + grid_size
         size = np.multiply(scene_size, grid_size)
