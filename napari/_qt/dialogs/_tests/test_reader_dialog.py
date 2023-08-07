f695e7a8dba4ee59e874c102911d489a130990ae
Lucy Liu <jliu176@gmail.com>
2023-08-07 15:34:27 +1000

    just clear qtviewer

--- napari/_qt/dialogs/_tests/test_reader_dialog.py
+++ napari/_qt/dialogs/_tests/test_reader_dialog.py
@@ -13,6 +13,7 @@ from napari._qt.dialogs.qt_reader_dialog import (
     open_with_dialog_choices,
     prepare_remaining_readers,
 )
+from napari._qt.qt_viewer import QtViewer
 from napari.errors.reader_errors import ReaderPluginError
 from napari.settings import get_settings
 
@@ -154,7 +155,7 @@ def test_open_sample_data_shows_all_readers(
 
     app = get_app()
     # required so setup steps run in init of `Viewer` and `Window`
-    viewer = make_napari_viewer()
+    make_napari_viewer()
     with mock.patch(
         'napari._qt.dialogs.qt_reader_dialog.prepare_remaining_readers'
     ) as mock_prepare_readers, mock.patch.object(
@@ -165,9 +166,8 @@ def test_open_sample_data_shows_all_readers(
     mock_prepare_readers.assert_called_once_with(
         ['some-path/some-file.fake'], None, None
     )
-    sample_action = viewer.window.file_menu.findAction('tmp_plugin.tmp-sample')
-    sample_action._destroy()
-    # QtViewer._instances.clear()
+    # Ensure no QtViewer leak
+    QtViewer._instances.clear()
 
 
 def test_open_with_dialog_choices_persist(
