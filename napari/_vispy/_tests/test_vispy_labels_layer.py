0c6f45f5e446919d4fae68b1edc2e3f704ae0eb3
Juan Nunez-Iglesias <jni@fastmail.com>
2023-08-11 13:30:30 +1000

    Add test for only updating indices in slice

--- napari/_vispy/_tests/test_vispy_labels_layer.py
+++ napari/_vispy/_tests/test_vispy_labels_layer.py
@@ -48,6 +48,47 @@ def test_labels_painting(make_napari_viewer, array_type):
 
 @skip_local_popups
 @pytest.mark.parametrize('array_type', ['numpy', 'zarr', 'tensorstore'])
+def test_labels_fill_slice(make_napari_viewer, array_type):
+    """Check that painting labels paints only on current slice.
+
+    This should work regardless of array type. See:
+    https://github.com/napari/napari/issues/6079
+    """
+    viewer = make_napari_viewer(show=True)
+    with tempfile.TemporaryDirectory(suffix='.zarr') as fn:
+        if array_type == 'numpy':
+            labels = np.zeros((3, 20, 20), dtype=np.uint32)
+        elif array_type in {'zarr', 'tensorstore'}:
+            labels = zarr.open(
+                fn, shape=(3, 20, 20), dtype=np.uint32, chunks=(1, 10, 10)
+            )
+        if array_type == 'tensorstore':
+            ts = pytest.importorskip('tensorstore')
+            spec = {
+                'driver': 'zarr',
+                'kvstore': {'driver': 'file', 'path': fn},
+                'path': '',
+                'metadata': {
+                    'dtype': labels.dtype.str,
+                    'order': labels.order,
+                    'shape': labels.shape,
+                },
+            }
+            labels = ts.open(spec, create=False, open=True).result()
+
+        labels[0, :, :] = 1
+        labels[1, 10, 10] = 1
+        labels[2, :, :] = 1
+        layer = viewer.add_labels(labels)
+        layer.n_edit_dimensions = 3
+        QCoreApplication.instance().processEvents()
+        layer.fill((1, 10, 10), 13, refresh=True)
+        visual = viewer.window._qt_viewer.layer_to_visual[layer]
+        assert np.sum(visual.node._data) == 13
+
+
+@skip_local_popups
+@pytest.mark.parametrize('array_type', ['numpy', 'zarr', 'tensorstore'])
 def test_labels_painting_with_mouse(
     MouseEvent, make_napari_viewer, array_type
 ):
