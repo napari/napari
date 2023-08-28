82730f645bdfe8f2998e4703483efa84a206955f
Lucy Liu <jliu176@gmail.com>
2023-08-28 16:49:15 +1000

    revert

--- napari/plugins/_npe2.py
+++ napari/plugins/_npe2.py
@@ -382,7 +382,6 @@ def _npe2_manifest_to_actions(
                 else:
                     subitem = _npe2_submenu_to_app_model(item)
                     submenus.append((menu_id, subitem))
-                    print('ss')
 
     actions: List[Action] = [
         Action(
