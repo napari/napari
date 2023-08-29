a71879f224d5cefc738a5654dd6b71aab50f59cf
Grzegorz Bokota <bokota+github@gmail.com>
2023-08-29 11:25:47 +0200

    fix E721

--- napari/layers/shapes/_tests/test_shapes.py
+++ napari/layers/shapes/_tests/test_shapes.py
@@ -2102,7 +2102,7 @@ def test_message():
     data = 20 * np.random.random(shape)
     layer = Shapes(data)
     msg = layer.get_status((0,) * 2)
-    assert type(msg) == dict
+    assert isinstance(msg, dict)
 
 
 def test_message_3d():
@@ -2114,7 +2114,7 @@ def test_message_3d():
     msg = layer.get_status(
         (0, 0, 0), view_direction=[1, 0, 0], dims_displayed=[0, 1, 2]
     )
-    assert type(msg) == dict
+    assert isinstance(msg, dict)
 
 
 def test_thumbnail():
