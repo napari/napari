7d08f78a5715c50f9c153556a3665fe33d65d989
Grzegorz Bokota <bokota+github@gmail.com>
2023-09-06 13:21:30 +0200

    fix tooltip test

--- napari/_qt/widgets/_tests/test_qt_tooltip.py
+++ napari/_qt/widgets/_tests/test_qt_tooltip.py
@@ -1,15 +1,9 @@
-import sys
-
-import pytest
+from qtpy.QtCore import QPoint
 from qtpy.QtWidgets import QToolTip
 
 from napari._qt.widgets.qt_tooltip import QtToolTipLabel
 
 
-@pytest.mark.skipif(
-    sys.platform.startswith('linux') or sys.platform == 'darwin',
-    reason='Timeouts when running on CI with Linux or macOS',
-)
 def test_qt_tooltip_label(qtbot):
     tooltip_text = "Test QtToolTipLabel showing a tooltip"
     widget = QtToolTipLabel("Label with a tooltip")
@@ -18,6 +12,10 @@ def test_qt_tooltip_label(qtbot):
     widget.show()
 
     assert QToolTip.text() == ""
+    # put mouse outside of widget
+    qtbot.mouseMove(widget, widget.rect().bottomRight() + QPoint(10, 10))
+    qtbot.wait(50)
     qtbot.mouseMove(widget)
+
     qtbot.waitUntil(lambda: QToolTip.isVisible())
     qtbot.waitUntil(lambda: QToolTip.text() == tooltip_text)
