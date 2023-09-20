87c62bb823e9bd1086562d27c2cf375de5e281b7
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-09-20 10:05:49 +0200

    MAINT: Use scipy builtins rotation to compute quaternions.
    As often, it is better to rely on existing battle
    tested code than roll our own. This also help testing the upstream code
    more.
    
    This replace our own quaternion to euler angles with using the Rotations
    call in scipy.
    
    This file comes from far at the beginning of napari, and had one
    significant modification in #2530 (WRT degenerate case), which I believe
    scipy already handle with the gimbals lock warning and is tested.
    
    Then α, β, γ is not unique when β=±π/2,
    then current new implementation would have `α=<some value>`, `γ=0`, the old code
    `α=0`, `γ=2.π-<some value>`.
    
    Note that the outputs are slightly different, with sometime the angles
    being +/- p , +/- zero i in some case.
    
    You can test this branch with this test script that has a few hardcoded
    degenerate case, as well as pick ~10k random quaternion and compare the
    resutls.
    
    https://gist.github.com/Carreau/53dfaefa0c07aa3a7a0bd90e890810bb
    
    ```
    +/- zero
    [-2.86, -3.12, -2.88, -2.64]
       old  1.491, 0.000, 1.658
       new  1.491, -0.000, 1.658
       diff 0.000, 0.000, -0.000
    ```
    
    ```
     +/- pi
    [1.44, 0.48, 3.93, -1.31]
       old  -3.142, 0.702, -2.498
       new  3.142, 0.702, -2.498
       diff 6.283, 0.000, 0.000
    ```
    
    ```
    gimbal lock:
    
    [0.09, -0.78, 0.09, 0.78]
       old  0.000, 1.571, -2.912
       new  2.912, 1.571, 0.000
       diff 2.912, 0.000, -2.912
    
    [-0.64, -0.58, -0.64, 0.58]
       old  0.000, 1.571, 1.473
       new  4.811, 1.571, 0.000
       diff 4.811, 0.000, 1.473
    ```

--- napari/_vispy/utils/quaternion.py
+++ napari/_vispy/utils/quaternion.py
@@ -1,4 +1,7 @@
+import warnings
+
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 
 def quaternion2euler(quaternion, degrees=False):
@@ -23,6 +26,29 @@ def quaternion2euler(quaternion, degrees=False):
     -------
     angles : 3-tuple
         Euler angles in (rx, ry, rz) order.
+    """
+    q = quaternion
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            action='ignore',
+            message='Gimbal lock detected.*',
+            category=UserWarning,
+        )
+
+        # despite both SciPy and  previous implementation advertising
+        # the  x,y,z order, it appear we need to reverse the order to get
+        # the same results a previously.
+        # as_euler('zyx') does not work as composition order matters
+        z, y, x = Rotation.from_quat([q.x, q.y, q.z, q.w]).as_euler(
+            'xyz', degrees=degrees
+        )
+    return (x, y, z)
+
+
+def _old_quaterion2euler(quaternion, degrees=False):
+    """
+    old custom implementation of the above
+
     """
     epsilon = 1e-10
 
