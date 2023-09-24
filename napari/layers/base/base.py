d7390c25a178f16139314c9475c3535ee939dead
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-09-24 03:18:00 -0700

    Silence mypy errors

--- napari/layers/base/base.py
+++ napari/layers/base/base.py
@@ -464,8 +464,8 @@ class Layer(KeymapProvider, MousemapProvider, ABC):
         mode = self._modeclass(mode_in)
         # Sub-classes can have their own Mode enum, so need to get members
         # from the specific mode class set on this layer.
-        PAN_ZOOM = self._modeclass.PAN_ZOOM
-        TRANSFORM = self._modeclass.TRANSFORM
+        PAN_ZOOM = self._modeclass.PAN_ZOOM # type: ignore[attr-defined]
+        TRANSFORM = self._modeclass.TRANSFORM # type: ignore[attr-defined]
         assert mode is not None
 
         if not self.editable:
