17f8f7af9bab89037cf5b751a39dcdf642a3d3b6
Wouter-Michiel Vierdag <w-mv@hotmail.com>
2023-10-05 16:49:24 +0200

    make public
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>

--- napari/layers/surface/surface.py
+++ napari/layers/surface/surface.py
@@ -260,7 +260,7 @@ class Surface(IntensityVisualizationMixin, Layer):
             self.contrast_limits_range = contrast_limits
         else:
             if contrast_limits_range := calc_data_range(self._vertex_values):
-                self._contrast_limits_range = contrast_limits_range
+                self.contrast_limits_range = contrast_limits_range
             # TODO: should there be a default here?
 
         self._contrast_limits = self._contrast_limits_range
