232aacd576a0a248311ca1fe14fc8cac6fddb80b
Grzegorz Bokota <bokota+github@gmail.com>
2023-11-04 15:36:32 +0100

    save texture format for future use

--- napari/_vispy/layers/labels.py
+++ napari/_vispy/layers/labels.py
@@ -578,3 +578,14 @@ class LabelNode(BaseLabel):  # type: ignore [valid-type,misc]
             return 0, 0
         else:
             return 0, self.size[axis]
+
+    def __init__(self, *args, texture_format, **kwargs):
+        super().__init__(*args, texture_format=texture_format, **kwargs)
+        # save texture format to not depend on vispy's private api
+        self.unfreeze()
+        self._texture_format = texture_format
+        self.freeze()
+
+    @property
+    def texture_format(self):
+        return self._texture_format
--- napari/_vispy/visuals/volume.py
+++ napari/_vispy/visuals/volume.py
@@ -204,3 +204,14 @@ class Volume(BaseVolume):
     # add the new rendering method to the snippets dict
     _shaders = shaders
     _rendering_methods = rendering_methods
+
+    def __init__(self, *args, texture_format, **kwargs):
+        super().__init__(*args, texture_format=texture_format, **kwargs)
+        # save texture format to not depend on vispy's private api
+        self.unfreeze()
+        self._texture_format = texture_format
+        self.freeze()
+
+    @property
+    def texture_format(self):
+        return self._texture_format
