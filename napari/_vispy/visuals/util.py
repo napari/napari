728d68c1753f6d4692d11c5f186cc552ece830cf
Grzegorz Bokota <bokota+github@gmail.com>
2023-11-07 16:09:25 +0100

    add explanation

--- napari/_vispy/visuals/util.py
+++ napari/_vispy/visuals/util.py
@@ -1,4 +1,10 @@
 class TextureMixin:
+    """
+    This mixin is created to avoid using vispy's private api.
+    vispy stores texture format `node._texture.internalformat`
+    And we should not relay on it if it is not needed.
+    """
+
     def __init__(self, *args, texture_format, **kwargs):
         super().__init__(*args, texture_format=texture_format, **kwargs)
         # save texture format to not depend on vispy's private api
