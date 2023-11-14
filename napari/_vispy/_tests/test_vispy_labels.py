74b8ccb099c185f88a9d4a22cbb9266732e955ec
Grzegorz Bokota <bokota+github@gmail.com>
2023-11-14 21:07:56 +0100

    fix test

--- napari/_vispy/_tests/test_vispy_labels.py
+++ napari/_vispy/_tests/test_vispy_labels.py
@@ -7,8 +7,9 @@ from napari._vispy.layers.labels import (
 
 def test_build_textures_from_dict():
     values = build_textures_from_dict(
-        {0: (0, 0, 0, 0), 1: (1, 1, 1, 1), 2: (2, 2, 2, 2)}
+        {0: (0, 0, 0, 0), 1: (1, 1, 1, 1), 2: (2, 2, 2, 2)},
+        max_size=10,
     )
-    assert values.shape == (3, 4)
+    assert values.shape == (3, 1, 4)
     assert np.array_equiv(values[1], (1, 1, 1, 1))
     assert np.array_equiv(values[2], (2, 2, 2, 2))
