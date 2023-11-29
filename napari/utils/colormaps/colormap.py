dd1cbcd2692369f7d6188ed2252ebf7b1bc41175
Grzegorz Bokota <bokota+github@gmail.com>
2023-11-29 13:01:25 +0100

    move comment to the proper place

--- napari/utils/colormaps/colormap.py
+++ napari/utils/colormaps/colormap.py
@@ -153,6 +153,10 @@ class Colormap(EventedModel):
 
 class LabelColormapBase(Colormap):
     class Config:
+        # this config is to avoid deepcopy of cached_property
+        # see https://github.com/pydantic/pydantic/issues/2763
+        # it is required until drop pydantic 1 or pythin 3.11 and older
+        # need to validate after drop pydantic 1
         keep_untouched = (cached_property,)
 
     @cached_property
@@ -187,13 +191,6 @@ class LabelColormap(LabelColormapBase):
     selection: int = 0
     interpolation: ColormapInterpolationMode = ColormapInterpolationMode.ZERO
     background_value: int = 0
-
-    class Config:
-        # this config is to avoid deepcopy of cached_property
-        # see https://github.com/pydantic/pydantic/issues/2763
-        # it is required until drop pydantic 1 or pythin 3.11 and older
-        # need to validate after drop pydantic 1
-        keep_untouched = (cached_property,)
 
     @cached_property
     def _uint8_colors(self) -> np.ndarray:
