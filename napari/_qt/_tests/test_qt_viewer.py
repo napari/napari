0312cacdb231687e6d6474d70b3eb2097c12ff1d
Grzegorz Bokota <bokota+github@gmail.com>
2023-12-05 13:50:14 +0100

    Pass key event from Main window to our internal mechanism (#6419)
    # References and relevant issues
    closes #6416
    
    # Description
    
    We implement passing key pres event in `QtViewer` class, but sometimes
    there is no focus on this widget and shortcuts need to be handled in the
    main window.
    
    This PR does not remove handling from `QtViewer` as it may be useful in
    some scenarios and we already release three rc so I do not want to break
    someone's workflow.
    
    But It looks like we may do this in for 0.5.0.

--- napari/_qt/_tests/test_qt_viewer.py
+++ napari/_qt/_tests/test_qt_viewer.py
@@ -11,7 +11,8 @@ import numpy.testing as npt
 import pytest
 from imageio import imread
 from pytestqt.qtbot import QtBot
-from qtpy.QtGui import QGuiApplication
+from qtpy.QtCore import QEvent, Qt
+from qtpy.QtGui import QGuiApplication, QKeyEvent
 from qtpy.QtWidgets import QApplication, QMessageBox
 from scipy import ndimage as ndi
 
@@ -892,3 +893,25 @@ def test_background_color(qtbot, qt_viewer: QtViewer, dtype):
         npt.assert_array_equal(
             color_pixel, color, err_msg=f"background {background}"
         )
+
+
+def test_shortcut_passing(make_napari_viewer):
+    viewer = make_napari_viewer(ndisplay=3)
+    layer = viewer.add_labels(
+        np.zeros((2, 2, 2), dtype=np.uint8), scale=(1, 2, 4)
+    )
+    layer.mode = "fill"
+
+    qt_window = viewer.window._qt_window
+
+    qt_window.keyPressEvent(
+        QKeyEvent(
+            QEvent.Type.KeyPress, Qt.Key.Key_1, Qt.KeyboardModifier.NoModifier
+        )
+    )
+    qt_window.keyReleaseEvent(
+        QKeyEvent(
+            QEvent.Type.KeyPress, Qt.Key.Key_1, Qt.KeyboardModifier.NoModifier
+        )
+    )
+    assert layer.mode == "erase"
