bd5f490ab93ed26f1abdf134a1bf41f111e7d360
Grzegorz Bokota <bokota+github@gmail.com>
2023-12-12 15:04:18 +0100

    pre allocate arrays for proper memory benchamrking

--- napari/utils/colormaps/colormap.py
+++ napari/utils/colormaps/colormap.py
@@ -640,7 +640,7 @@ def _zero_preserving_modulo_numpy(
     return res
 
 
-def _zero_preserving_modulo_jit(
+def _zero_preserving_modulo_impl(
     values: np.ndarray, n: int, dtype: np.dtype, to_zero: int = 0
 ) -> np.ndarray:
     """``(values - 1) % n + 1``, but with one specific value mapped to 0.
@@ -667,14 +667,43 @@ def _zero_preserving_modulo_jit(
         everywhere else.
     """
     result = np.empty_like(values, dtype=dtype)
+    # need to preallocate numpy array for asv memory benchmarks
+    return _zero_preserving_modulo_jit(values, n, result, to_zero)
 
+
+def _zero_preserving_modulo_jit(
+    values: np.ndarray, n: int, out: np.ndarray, to_zero: int = 0
+) -> np.ndarray:
+    """``(values - 1) % n + 1``, but with one specific value mapped to 0.
+
+    This ensures (1) an output value in [0, n] (inclusive), and (2) that
+    no nonzero values in the input are zero in the output, other than the
+    ``to_zero`` value.
+
+    Parameters
+    ----------
+    values : np.ndarray
+        The dividend of the modulo operator.
+    n : int
+        The divisor.
+    out : np.ndarray
+        preallocated output array
+    to_zero : int, optional
+        A specific value to map to 0. (By default, 0 itself.)
+
+    Returns
+    -------
+    np.ndarray
+        The result: 0 for the ``to_zero`` value, ``values % n + 1``
+        everywhere else.
+    """
     for i in prange(values.size):
         if values.flat[i] == to_zero:
-            result.flat[i] = 0
+            out.flat[i] = 0
         else:
-            result.flat[i] = (values.flat[i] - 1) % n + 1
+            out.flat[i] = (values.flat[i] - 1) % n + 1
 
-    return result
+    return out
 
 
 def _cast_labels_data_to_texture_dtype_direct(
@@ -757,27 +786,26 @@ def _labels_raw_to_texture_direct_typed_dict(
     target_dtype = minimum_dtype_for_labels(
         direct_colormap._num_unique_colors + 2
     )
+    result_array = np.zeros_like(data, dtype=target_dtype)
     return _labels_raw_to_texture_direct_typed_dict_impl(
-        data, dkt, target_dtype
+        data, dkt, result_array
     )
 
 
 def _labels_raw_to_texture_direct_typed_dict_impl(
-    data: np.ndarray, dkt: 'typed.Dict', target_dtype: np.dtype
+    data: np.ndarray, dkt: 'typed.Dict', out: np.ndarray
 ) -> np.ndarray:
     """
     Relabel data using typed dict with mapping unknown labels to default value
     """
     # The numba typed dict does not provide official Api for
     # determine key and value types
-    result_array = np.zeros_like(data, dtype=target_dtype)
-
     for i in prange(data.size):
         val = data.flat[i]
         if val in dkt:
-            result_array.flat[i] = dkt[data.flat[i]]
+            out.flat[i] = dkt[data.flat[i]]
 
-    return result_array
+    return out
 
 
 def _texture_dtype(num_colors: int, dtype: np.dtype) -> np.dtype:
@@ -822,9 +850,10 @@ except ModuleNotFoundError:
     _labels_raw_to_texture_direct = _labels_raw_to_texture_direct_numpy
     prange = range
 else:
-    _zero_preserving_modulo = numba.njit(parallel=True)(
+    _zero_preserving_modulo_jit = numba.njit(parallel=True)(
         _zero_preserving_modulo_jit
     )
+    _zero_preserving_modulo = _zero_preserving_modulo_impl
     _labels_raw_to_texture_direct = _labels_raw_to_texture_direct_typed_dict
     _labels_raw_to_texture_direct_typed_dict_impl = numba.njit(parallel=True)(
         _labels_raw_to_texture_direct_typed_dict_impl
