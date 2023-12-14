153ec15af6d6c96629e07fbebcdc76ecd58d9248
Lucy Liu <jliu176@gmail.com>
2023-12-14 12:23:30 +1100

    update provider test

--- napari/_tests/test_providers.py
+++ napari/_tests/test_providers.py
@@ -3,21 +3,42 @@
 Because `_provide_viewer` needs `_QtMainWindow` (otherwise returns `None`)
 tests are here in `napari/_tests`, which are not run in headless mode.
 """
-import pytest
+from app_model.types import Action
 
+from napari._app_model._app import get_app
 from napari._app_model.injection._providers import _provide_viewer
-from napari.utils._proxies import PublicOnlyProxy
+from napari.viewer import Viewer
 
 
-def test_publicproxy_viewer(make_napari_viewer):
-    """Test `_provide_viewer` outputs a `PublicOnlyProxy` when `Viewer` exists.
+def test_publicproxy_viewer(capsys, make_napari_viewer):
+    """Test `_provide_viewer` outputs a `PublicOnlyProxy` when appropriate.
 
-    Also check error raised when `Viewer` does not exist.
+    Check manual (e.g., internal) `_provide_viewer` calls can disable
+    `PublicOnlyProxy` via `public_proxy` parameter but `PublicOnlyProxy` is always
+    used when it is used as a provider.
     """
-    with pytest.raises(RuntimeError, match="No current `Viewer` found"):
-        viewer = _provide_viewer()
+    # No current viewer, `None` should be returned
+    viewer = _provide_viewer()
+    assert viewer is None
 
     # Create a viewer
     make_napari_viewer()
-    viewer = _provide_viewer()
-    assert isinstance(viewer, PublicOnlyProxy)
+    # Ensure we can disable via `public_proxy`
+    viewer = _provide_viewer(public_proxy=False)
+    assert isinstance(viewer, Viewer)
+
+    # Ensure we get a `PublicOnlyProxy` when used as a provider
+    def my_viewer(viewer: Viewer) -> Viewer:
+        # Allows us to check type when `Action` executed
+        print(type(viewer))
+
+    action = Action(
+        id='some.command.id',
+        title='some title',
+        callback=my_viewer,
+    )
+    app = get_app()
+    app.register_action(action)
+    app.commands.execute_command('some.command.id')
+    captured = capsys.readouterr()
+    assert "napari.utils._proxies.PublicOnlyProxy" in captured.out
