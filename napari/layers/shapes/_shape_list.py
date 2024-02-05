Remove scaling of highlight width, fix showing shapes for uneven coordinates (#6629)
# References and relevant issues

This PR is created as result of debug
https://github.com/napari/napari/issues/6627#issuecomment-1914804363
As 0.4.19 branch has fewer problems than #6628 is created for 0.4.19. 

# Description

Fix logic for selecting of visible shapes based on idea from #2289. It
is lucky that we do not hit this earlier.

Removes scaling of width of markers border (this is same like in #6628)

To improve readability, this update `ShapesVisual` to have named
subvisuals.

napari/layers/shapes/_shape_list.py
@@ -271,7 +271,9 @@ class ShapeList:
         # Slice key must exactly match mins and maxs of shape as then the
         # shape is entirely contained within the current slice.
         if len(self.shapes) > 0:
-            self._displayed = np.all(self.slice_keys == slice_key, axis=(1, 2))
+            self._displayed = np.all(
+                np.abs(self.slice_keys - slice_key) < 0.5, axis=(1, 2)
+            )
         else:
             self._displayed = np.array([])
         disp_indices = np.where(self._displayed)[0]