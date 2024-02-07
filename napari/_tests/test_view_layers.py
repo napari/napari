e8807200dc2ac86ab45108f8fe70e7afbb055414
Grzegorz Bokota <bokota+github@gmail.com>
2024-02-08 00:52:44 +0100

    fix tests

--- napari/_tests/test_view_layers.py
+++ napari/_tests/test_view_layers.py
@@ -42,7 +42,7 @@ def test_docstring(layer):
     # check summary section
     method_summary = ' '.join(method_doc['Summary'])  # join multi-line summary
 
-    summary_format = 'Add an? .+? layer to the layer list.'
+    summary_format = 'Add an? .+? layers? to the layer list.'
 
     assert re.match(
         summary_format, method_summary
