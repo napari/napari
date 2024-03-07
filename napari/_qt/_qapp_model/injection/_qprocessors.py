fd45dba88a56546a8dcda08380cfa0ef199389b2
Lucy Liu <jliu176@gmail.com>
2024-03-07 11:15:42 +1100

    lint

--- napari/_qt/_qapp_model/injection/_qprocessors.py
+++ napari/_qt/_qapp_model/injection/_qprocessors.py
@@ -26,7 +26,6 @@ def _add_plugin_dock_widget(
         viewer = _provide_viewer_or_raise(
             msg='Widgets cannot be opened in headless mode.',
         )
-
     widget, full_name = widget_name_tuple
     viewer.window.add_dock_widget(widget, name=full_name)
 
