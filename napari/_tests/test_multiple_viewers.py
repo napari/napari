3efcb2d78ec803a3480961de0a1f1eb142b7599d
Lucy Liu <jliu176@gmail.com>
2024-03-22 14:49:28 +1100

    add show

--- napari/_tests/test_multiple_viewers.py
+++ napari/_tests/test_multiple_viewers.py
@@ -3,8 +3,8 @@ from unittest.mock import patch
 
 
 def test_multi_viewers_dont_clash(make_napari_viewer, qtbot):
-    v1 = make_napari_viewer(title='v1')
-    v2 = make_napari_viewer(title='v2')
+    v1 = make_napari_viewer(show=False, title='v1')
+    v2 = make_napari_viewer(show=False, title='v2')
     assert not v1.grid.enabled
     assert not v2.grid.enabled
 
