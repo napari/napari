f3fae85b9b2f786b91d28b6db237d461793f4b11
Grzegorz Bokota <bokota+github@gmail.com>
2024-03-27 18:05:42 +0100

    Implement validation of docstrings and kwargs order

--- napari/utils/_test_utils.py
+++ napari/utils/_test_utils.py
@@ -2,10 +2,12 @@
 File with things that are useful for testing, but not to be fixtures
 """
 
+import inspect
 from dataclasses import dataclass, field
 from typing import Optional, Union
 
 import numpy as np
+from docstring_parser import parse
 
 from napari.utils._proxies import ReadOnlyWrapper
 
@@ -35,3 +37,33 @@ def read_only_mouse_event(*args, **kwargs):
     return ReadOnlyWrapper(
         MouseEvent(*args, **kwargs), exceptions=('handled',)
     )
+
+
+def validate_docstring_all_params_in(func):
+    assert func.__doc__ is not None, f'Function {func} has no docstring'
+
+    parsed = parse(func.__doc__)
+    params = [x for x in parsed.params if x.args[0] == 'param']
+    # get only parameters from docstring
+
+    signature = inspect.signature(func)
+    assert set(signature.parameters.keys()) == {
+        x.arg_name for x in params
+    }, 'Parameters in signature and docstring do not match'
+    for sig, doc in zip(signature.parameters.values(), params):
+        assert (
+            sig.name == doc.arg_name
+        ), 'Parameters in signature and docstring do not in same order.'
+        # assert sig.annotation == doc.type_name, f"Type of parameter {sig.name} in signature and docstring do not match"
+
+
+def validate_kwargs_sorted(func):
+    signature = inspect.signature(func)
+    kwargs_list = [
+        x.name
+        for x in signature.parameters.values()
+        if x.kind == inspect.Parameter.KEYWORD_ONLY
+    ]
+    assert kwargs_list == sorted(
+        kwargs_list
+    ), 'Keyword arguments are not sorted in function signature'
