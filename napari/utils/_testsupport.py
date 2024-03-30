7132c9cbb40421a01aaf36e46c61c10e67bec707
Grzegorz Bokota <bokota+github@gmail.com>
2024-03-30 21:05:16 +0100

    decrease depth

--- napari/utils/_testsupport.py
+++ napari/utils/_testsupport.py
@@ -62,7 +62,7 @@ def fail_obj_graph(Klass):
         ).absolute()
         objgraph.show_backrefs(
             list(Klass._instances),
-            max_depth=110,
+            max_depth=100,
             filename=str(file_path),
         )
 
