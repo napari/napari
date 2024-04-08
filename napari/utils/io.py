ac5ff82634bc121ae2c69573a6ee1c1710fa2998
jules-vanaret <jules.vanaret@univ-amu.fr>
2024-04-08 15:50:46 +0200

    reverted back to no compression for bool

--- napari/utils/io.py
+++ napari/utils/io.py
@@ -1,4 +1,3 @@
-import importlib
 import os
 import warnings
 from typing import TYPE_CHECKING
@@ -96,19 +95,12 @@ def imsave_tiff(filename, data):
         )
 
     if compression_instead_of_compress:
-        # 'compression' scheme is more complex. See:
-        # https://forum.image.sc/t/problem-saving-generated-labels-in-cellpose-napari/54892/8
-
-        imagecodecs_available = (
-            importlib.util.find_spec('imagecodecs') is not None
-        )
-
-        if data.dtype == bool and not imagecodecs_available:
+        if data.dtype == bool:
             tifffile.imwrite(filename, data)
         else:
-            # boolean data needs special compression scheme
-            compression = 'lzw' if data.dtype == bool else ('zlib', 1)
-            tifffile.imwrite(filename, data, compression=compression)
+            # 'compression' scheme is more complex. See:
+            # https://forum.image.sc/t/problem-saving-generated-labels-in-cellpose-napari/54892/8
+            tifffile.imwrite(filename, data, compression=('zlib', 1))
     else:  # older version of tifffile since 2021.6.6  this is deprecated
         tifffile.imwrite(filename, data, compress=1)
 
