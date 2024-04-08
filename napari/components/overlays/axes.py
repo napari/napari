e46ed6d24a77f08b29adc92173679689a51dead2
Grzegorz Bokota <bokota+github@gmail.com>
2024-04-08 16:27:50 +0200

    Rename `axes_labels` to `axis_labels`

--- napari/components/overlays/axes.py
+++ napari/components/overlays/axes.py
@@ -8,7 +8,7 @@ class AxesOverlay(SceneOverlay):
     ----------
     labels : bool
         If axes labels are visible or not. Not the actual
-        axes labels are stored in `viewer.dims.axes_labels`.
+        axis labels are stored in `viewer.dims.axis_labels`.
     colored : bool
         If axes are colored or not. If colored then default
         coloring is x=cyan, y=yellow, z=magenta. If not
