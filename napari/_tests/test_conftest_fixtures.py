a6c94698da8748322ba45b00c6eef739649f3dbd
Grzegorz Bokota <bokota+github@gmail.com>
2024-04-18 12:00:24 +0200

    fix test_lack_disable_throttle test

--- napari/_tests/test_conftest_fixtures.py
+++ napari/_tests/test_conftest_fixtures.py
@@ -63,11 +63,13 @@ def test_disable_throttle(start_mock):
     mock.assert_called_once()
 
 
-@patch('qtpy.QtCore.QTimer.start')
-@patch('qtpy.QtCore.QTimer.isActive', return_value=True)
-def test_lack_disable_throttle(start_mock, monkeypatch):
+def test_lack_disable_throttle(monkeypatch):
     """This is test showing that if we do not use disable_throttling then timer is started"""
     mock = Mock()
+    start_mock = Mock()
+    active_mock = Mock(return_value=True)
+    monkeypatch.setattr('qtpy.QtCore.QTimer.start', start_mock)
+    monkeypatch.setattr('qtpy.QtCore.QTimer.isActive', active_mock)
 
     @qdebounced(timeout=50)
     def f() -> str:
