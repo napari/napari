napari/napari

commit 3e9cb4ef8684852f47e49715273e237500a8fc49

Vispy 0.12: per-point symbol and several bugfixes (#5312)

napari/_vispy/visuals/points.py
@@ -21,15 +21,6 @@ class PointsVisual(ClippingPlanesMixin, Compound):
         self.scaling = True
 
     @property
-    def symbol(self):
-        return self._subvisuals[0].symbol
-
-    @symbol.setter
-    def symbol(self, value):
-        for marker in self._subvisuals[:2]:
-            marker.symbol = value
-
-    @property
     def scaling(self):
         """
         Scaling property for both the markers visuals. If set to true,

napari/_vispy/layers/points.py
@@ -19,7 +19,7 @@ class VispyPointsLayer(VispyBaseLayer):
         node = PointsVisual()
         super().__init__(layer, node)
 
-        self.layer.events.symbol.connect(self._on_symbol_change)
+        self.layer.events.symbol.connect(self._on_data_change)
         self.layer.events.edge_width.connect(self._on_data_change)
         self.layer.events.edge_width_is_relative.connect(self._on_data_change)
         self.layer.events.edge_color.connect(self._on_data_change)
@@ -39,24 +39,24 @@ class VispyPointsLayer(VispyBaseLayer):
         self._on_data_change()
 
     def _on_data_change(self):
-        if len(self.layer._indices_view) > 0:
-            edge_color = self.layer._view_edge_color
-            face_color = self.layer._view_face_color
-        else:
-            edge_color = np.array([[0.0, 0.0, 0.0, 1.0]], dtype=np.float32)
-            face_color = np.array([[1.0, 1.0, 1.0, 1.0]], dtype=np.float32)
-
         # Set vispy data, noting that the order of the points needs to be
         # reversed to make the most recently added point appear on top
         # and the rows / columns need to be switched for vispy's x / y ordering
         if len(self.layer._indices_view) == 0:
+            # always pass one invisible point to avoid issues
             data = np.zeros((1, self.layer._slice_input.ndisplay))
             size = [0]
+            edge_color = np.array([[0.0, 0.0, 0.0, 1.0]], dtype=np.float32)
+            face_color = np.array([[1.0, 1.0, 1.0, 1.0]], dtype=np.float32)
             edge_width = [0]
+            symbol = ['o']
         else:
             data = self.layer._view_data
             size = self.layer._view_size
+            edge_color = self.layer._view_edge_color
+            face_color = self.layer._view_face_color
             edge_width = self.layer._view_edge_width
+            symbol = self.layer._view_symbol
 
         set_data = self.node._subvisuals[0].set_data
 
@@ -74,15 +74,13 @@ class VispyPointsLayer(VispyBaseLayer):
         set_data(
             data[:, ::-1],
             size=size,
-            **edge_kw,
+            symbol=symbol,
             edge_color=edge_color,
             face_color=face_color,
+            **edge_kw,
         )
 
         self.reset()
-
-    def _on_symbol_change(self):
-        self.node.symbol = self.layer.symbol
 
     def _on_highlight_change(self):
         settings = get_settings()
@@ -92,13 +90,16 @@ class VispyPointsLayer(VispyBaseLayer):
             if data.ndim == 1:
                 data = np.expand_dims(data, axis=0)
             size = self.layer._view_size[self.layer._highlight_index]
+            symbol = self.layer._view_symbol[self.layer._highlight_index]
         else:
             data = np.zeros((1, self.layer._slice_input.ndisplay))
             size = 0
+            symbol = ['o']
 
         self.node._subvisuals[1].set_data(
             data[:, ::-1],
             size=size,
+            symbol=symbol,
             edge_width=settings.appearance.highlight_thickness,
             edge_color=self._highlight_color,
             face_color=transform_color('transparent'),
@@ -179,7 +180,6 @@ class VispyPointsLayer(VispyBaseLayer):
     def reset(self):
         super().reset()
         self._update_text(update_node=False)
-        self._on_symbol_change()
         self._on_highlight_change()
         self._on_antialiasing_change()
         self._on_shading_change()

napari/layers/points/points.py
@@ -9,15 +9,11 @@ import pandas as pd
 from scipy.stats import gmean
 
 from napari.layers.base import Layer, no_op
-from napari.layers.points._points_constants import (
-    SYMBOL_ALIAS,
-    Mode,
-    Shading,
-    Symbol,
-)
+from napari.layers.points._points_constants import Mode, Shading
 from napari.layers.points._points_mouse_bindings import add, highlight, select
 from napari.layers.points._points_utils import (
     _create_box_from_corners_3d,
+    coerce_symbols,
     create_box,
     fix_data_points,
     points_to_squares,
@@ -73,8 +69,8 @@ class Points(Layer):
         A dictionary can be provided with keyword arguments to set the text values
         and display properties. See TextManager.__init__() for the valid keyword arguments.
         For example usage, see /napari/examples/add_points_with_text.py.
-    symbol : str
-        Symbol to be used for the point markers. Must be one of the
+    symbol : str, array
+        Symbols to be used for the point markers. Must be one of the
         following: arrow, clobber, cross, diamond, disc, hbar, ring,
         square, star, tailed_arrow, triangle_down, triangle_up, vbar, x.
     size : float, array
@@ -180,8 +176,8 @@ class Points(Layer):
         that property will be displayed. Multiple properties can be composed using f-string-like
         syntax (e.g., '{property_1}, {float_property:.2f}).
         For example usage, see /napari/examples/add_points_with_text.py.
-    symbol : str
-        Symbol used for all point markers.
+    symbol : array of str
+        Array of symbols for each point.
     size : array (N, D)
         Array of sizes for each point in each dimension. Must have the same
         shape as the layer `data`.
@@ -213,6 +209,8 @@ class Points(Layer):
         of the specified property that are mapped to 0 and 1, respectively.
         The default value is None. If set the none, the clims will be set to
         (property.min(), property.max())
+    current_symbol : Symbol
+        Symbol for the next point to be added or the currently selected points.
     current_size : float
         Size of the marker for the next point to be added or the currently
         selected point.
@@ -270,6 +268,8 @@ class Points(Layer):
         2D coordinates of points in the currently viewed slice.
     _view_size : array (M, )
         Size of the point markers in the currently viewed slice.
+    _view_symbol : array (M, )
+        Symbols of the point markers in the currently viewed slice.
     _view_edge_width : array (M, )
         Edge width of the point markers in the currently viewed slice.
     _indices_view : array (M, )
@@ -394,6 +394,13 @@ class Points(Layer):
         self._edge_width_is_relative = False
         self._shown = np.empty(0).astype(bool)
 
+        # Indices of selected points
+        self._selected_data = set()
+        self._selected_data_stored = set()
+        self._selected_data_history = set()
+        # Indices of selected points within the currently viewed slice
+        self._selected_view = []
+
         # The following point properties are for the new points that will
         # be added. For any given property, if a list is passed to the
         # constructor so each point gets its own value then the default
@@ -402,12 +409,10 @@ class Points(Layer):
         self._current_edge_width = (
             np.asarray(edge_width) if np.isscalar(edge_width) else 0.1
         )
-        # Indices of selected points
-        self._selected_data = set()
-        self._selected_data_stored = set()
-        self._selected_data_history = set()
-        # Indices of selected points within the currently viewed slice
-        self._selected_view = []
+        self.current_symbol = (
+            np.asarray(symbol) if np.isscalar(symbol) else 'o'
+        )
+
         # Index of hovered point
         self._value = None
         self._value_stored = None
@@ -500,6 +505,7 @@ class Points(Layer):
                         self._shown = self._shown[: len(data)]
                         self._size = self._size[: len(data)]
                         self._edge_width = self._edge_width[: len(data)]
+                        self._symbol = self._symbol[: len(data)]
 
                     elif len(data) > cur_npoints:
                         # If there are now more points, add the size and colors of the
@@ -524,6 +530,12 @@ class Points(Layer):
                             [new_edge_width], adding, axis=0
                         )
 
+                        if len(self._symbol) > 0:
+                            new_symbol = copy(self._symbol[-1])
+                        else:
+                            new_symbol = self.current_symbol
+                        symbol = np.repeat([new_symbol], adding, axis=0)
+
                         # add new colors
                         self._edge._add(n_colors=adding)
                         self._face._add(n_colors=adding)
@@ -536,6 +548,9 @@ class Points(Layer):
                         self.size = np.concatenate((self._size, size), axis=0)
                         self.edge_width = np.concatenate(
                             (self._edge_width, edge_width), axis=0
+                        )
+                        self.symbol = np.concatenate(
+                            (self._symbol, symbol), axis=0
                         )
                         self.selected_data = set(
                             np.arange(cur_npoints, len(data))
@@ -717,21 +732,29 @@ class Points(Layer):
         self.out_of_slice_display = value
 
     @property
-    def symbol(self) -> str:
+    def symbol(self) -> np.ndarray:
         """str: symbol used for all point markers."""
-        return str(self._symbol)
+        return self._symbol
 
     @symbol.setter
-    def symbol(self, symbol: Union[str, Symbol]) -> None:
-        if isinstance(symbol, str):
-            # Convert the alias string to the deduplicated string
-            if symbol in SYMBOL_ALIAS:
-                symbol = SYMBOL_ALIAS[symbol]
-            else:
-                symbol = Symbol(symbol)
-        self._symbol = symbol
+    def symbol(self, symbol: Union[str, np.ndarray, list]) -> None:
+        symbol = np.broadcast_to(symbol, self.data.shape[0])
+        self._symbol = coerce_symbols(symbol)
         self.events.symbol()
         self.events.highlight()
+
+    @property
+    def current_symbol(self) -> Union[int, float]:
+        """float: symbol of marker for the next added point."""
+        return self._current_symbol
+
+    @current_symbol.setter
+    def current_symbol(self, symbol: Union[None, float]) -> None:
+        symbol = coerce_symbols(np.array([symbol]))[0]
+        self._current_symbol = symbol
+        if self._update_properties and len(self.selected_data) > 0:
+            self.symbol[list(self.selected_data)] = symbol
+            self.events.symbol()
 
     @property
     def size(self) -> np.ndarray:
@@ -1168,7 +1191,9 @@ class Points(Layer):
         state = self._get_base_state()
         state.update(
             {
-                'symbol': self.symbol,
+                'symbol': self.symbol
+                if self.data.size
+                else [self.current_symbol],
                 'edge_width': self.edge_width,
                 'edge_width_is_relative': self.edge_width_is_relative,
                 'face_color': self.face_color
@@ -1247,6 +1272,9 @@ class Points(Layer):
         ) is not None:
             with self.block_update_properties():
                 self.current_edge_width = unique_edge_width
+        if (unique_symbol := _unique_element(self.symbol[index])) is not None:
+            with self.block_update_properties():
+                self.current_symbol = unique_symbol
 
         unique_properties = {}
         for k, v in self.properties.items():
@@ -1424,6 +1452,17 @@ class Points(Layer):
             # if no points, return an empty list
             sizes = np.array([])
         return sizes
+
+    @property
+    def _view_symbol(self) -> np.ndarray:
+        """Get the symbols of the points in view
+
+        Returns
+        -------
+        symbol : (N,) np.ndarray
+            Array of symbol strings for the N points in view
+        """
+        return self.symbol[self._indices_view]
 
     @property
     def _view_edge_width(self) -> np.ndarray:
@@ -1823,14 +1862,15 @@ class Points(Layer):
         colormapped[..., 3] *= self.opacity
         self.thumbnail = colormapped
 
-    def add(self, coord):
-        """Adds point at coordinate.
+    def add(self, coords):
+        """Adds points at coordinates.
 
         Parameters
         ----------
-        coord : sequence of indices to add point at
+        coords : array
+            Point or points to add to the layer data.
         """
-        self.data = np.append(self.data, np.atleast_2d(coord), axis=0)
+        self.data = np.append(self.data, np.atleast_2d(coords), axis=0)
 
     def remove_selected(self):
         """Removes selected points if any."""
@@ -1839,6 +1879,7 @@ class Points(Layer):
         if len(index):
             self._shown = np.delete(self._shown, index, axis=0)
             self._size = np.delete(self._size, index, axis=0)
+            self._symbol = np.delete(self._symbol, index, axis=0)
             self._edge_width = np.delete(self._edge_width, index, axis=0)
             with self._edge.events.blocker_all():
                 self._edge._remove(indices_to_remove=index)
@@ -1934,6 +1975,9 @@ class Points(Layer):
             self._size = np.append(
                 self.size, deepcopy(self._clipboard['size']), axis=0
             )
+            self._symbol = np.append(
+                self.symbol, deepcopy(self._clipboard['symbol']), axis=0
+            )
 
             self._feature_table.append(self._clipboard['features'])
 
@@ -1975,6 +2019,7 @@ class Points(Layer):
                 'face_color': deepcopy(self.face_color[index]),
                 'shown': deepcopy(self.shown[index]),
                 'size': deepcopy(self.size[index]),
+                'symbol': deepcopy(self.symbol[index]),
                 'edge_width': deepcopy(self.edge_width[index]),
                 'features': deepcopy(self.features.iloc[index]),
                 'indices': self._slice_indices,

examples/point_cloud.py
@@ -1,0 +1,24 @@
+"""
+Point cloud
+===========
+
+Display 3D points with combinations of different renderings.
+
+.. tags:: visualization-basic
+"""
+
+import numpy as np
+import napari
+
+n_points = 100
+
+points = np.random.normal(10, 100, (n_points, 3))
+symbols = np.random.choice(['o', 's', '*'], n_points)
+sizes = np.random.rand(n_points) * 10 + 10
+colors = np.random.rand(n_points, 3)
+
+viewer = napari.Viewer(ndisplay=3)
+viewer.add_points(points, symbol=symbols, size=sizes, face_color=colors)
+
+if __name__ == '__main__':
+    napari.run()

napari/_qt/_tests/test_qt_viewer.py
@@ -332,6 +332,7 @@ def test_points_layer_display_correct_slice_on_scale(make_napari_viewer):
     np.testing.assert_equal(response.indices, [0])
 
 
+@skip_on_win_ci
 def test_qt_viewer_clipboard_with_flash(make_napari_viewer, qtbot):
     viewer = make_napari_viewer()
     # make sure clipboard is empty
@@ -379,6 +380,7 @@ def test_qt_viewer_clipboard_with_flash(make_napari_viewer, qtbot):
     assert not hasattr(viewer.window._qt_window, "_flash_animation")
 
 
+@skip_on_win_ci
 def test_qt_viewer_clipboard_without_flash(make_napari_viewer):
     viewer = make_napari_viewer()
     # make sure clipboard is empty

napari/_qt/layer_controls/_tests/test_qt_image_layer.py
@@ -11,7 +11,7 @@ def test_interpolation_combobox(qtbot):
     qtbot.addWidget(qtctrl)
     combo = qtctrl.interpComboBox
     opts = {combo.itemText(i) for i in range(combo.count())}
-    assert opts == {'bicubic', 'linear', 'kaiser', 'nearest', 'spline36'}
+    assert opts == {'cubic', 'linear', 'kaiser', 'nearest', 'spline36'}
     # programmatically adding approved interpolation works
     layer.interpolation2d = 'lanczos'
     assert combo.findText('lanczos') == 5

napari/_qt/layer_controls/qt_points_controls.py
@@ -12,7 +12,11 @@ from napari._qt.widgets.qt_mode_buttons import (
     QtModePushButton,
     QtModeRadioButton,
 )
-from napari.layers.points._points_constants import SYMBOL_TRANSLATION, Mode
+from napari.layers.points._points_constants import (
+    SYMBOL_TRANSLATION,
+    SYMBOL_TRANSLATION_INVERTED,
+    Mode,
+)
 from napari.utils.action_manager import action_manager
 from napari.utils.events import disconnect_events
 from napari.utils.translations import trans
@@ -117,17 +121,25 @@ class QtPointsControls(QtLayerControls):
         self.faceColorEdit.color_changed.connect(self.changeFaceColor)
         self.edgeColorEdit.color_changed.connect(self.changeEdgeColor)
 
-        self.symbolComboBox = QComboBox()
+        sym_cb = QComboBox()
+        sym_cb.setToolTip(
+            trans._(
+                "Change the symbol of currently selected points and any added afterwards."
+            )
+        )
         current_index = 0
-        for index, (data, text) in enumerate(SYMBOL_TRANSLATION.items()):
-            data = data.value
-            self.symbolComboBox.addItem(text, data)
+        for index, (symbol_string, text) in enumerate(
+            SYMBOL_TRANSLATION.items()
+        ):
+            symbol_string = symbol_string.value
+            sym_cb.addItem(text, symbol_string)
 
-            if data == self.layer.symbol:
+            if symbol_string == self.layer.current_symbol:
                 current_index = index
 
-        self.symbolComboBox.setCurrentIndex(current_index)
-        self.symbolComboBox.currentTextChanged.connect(self.changeSymbol)
+        sym_cb.setCurrentIndex(current_index)
+        sym_cb.currentTextChanged.connect(self.changeSymbol)
+        self.symbolComboBox = sym_cb
 
         self.outOfSliceCheckBox = QCheckBox()
         self.outOfSliceCheckBox.setToolTip(trans._('Out of slice display'))
@@ -228,7 +240,7 @@ class QtPointsControls(QtLayerControls):
         text : int
             Index of current marker symbol of points, eg: '+', '.', etc.
         """
-        self.layer.symbol = self.symbolComboBox.currentData()
+        self.layer.current_symbol = SYMBOL_TRANSLATION_INVERTED[text]
 
     def changeSize(self, value):
         """Change size of points on the layer model.
@@ -276,7 +288,7 @@ class QtPointsControls(QtLayerControls):
         """Receive marker symbol change event and update the dropdown menu."""
         with self.layer.events.symbol.blocker():
             self.symbolComboBox.setCurrentIndex(
-                self.symbolComboBox.findData(self.layer.symbol)
+                self.symbolComboBox.findData(self.layer.current_symbol.value)
             )
 
     def _on_size_change(self):

napari/_tests/test_mouse_bindings.py
@@ -3,7 +3,10 @@ from unittest.mock import Mock
 
 import numpy as np
 
+from napari._tests.utils import skip_on_win_ci
 
+
+@skip_on_win_ci
 def test_viewer_mouse_bindings(make_napari_viewer):
     """Test adding mouse bindings to the viewer"""
     np.random.seed(0)
@@ -77,6 +80,7 @@ def test_viewer_mouse_bindings(make_napari_viewer):
     mock_move.method.assert_not_called()
 
 
+@skip_on_win_ci
 def test_layer_mouse_bindings(make_napari_viewer):
     """Test adding mouse bindings to a layer that is selected"""
     np.random.seed(0)
@@ -152,6 +156,7 @@ def test_layer_mouse_bindings(make_napari_viewer):
     mock_move.method.assert_not_called()
 
 
+@skip_on_win_ci
 def test_unselected_layer_mouse_bindings(make_napari_viewer):
     """Test adding mouse bindings to a layer that is not selected"""
     np.random.seed(0)

commit 4a56237e94480934b1b9f5df5c63949a4059b1f8

Fix use of scaling in vispy points (#6894)
# References and relevant issues
In #6025, the vispy update broke the efforts from #5312. This is
actually because the changes in vispy apparently do not handle the
`scaling` kwarg properly at initialization. We can fix it here by not
passing it.

I can't believe this has been wrong for so long, and I somehow never
connected this issue that kept bugging me with that PR... but here we
are.

# Description
Try the following on main and here and move the camera around:
```py
import napari
import numpy as np
v = napari.Viewer(ndisplay=3)
pl = v.add_points(np.array([[1, 1, 1], [10, 10, 10]]), scale=[1, 5, 5])
pv = v.window._qt_viewer.canvas.layer_to_visual[pl]
```
This fixes two things:
- point sizes being all messed up by scaling
- the anisotropic scale rotation wobblyness you see above

This returns to the correct, pre-#6025 situation.

napari/_vispy/visuals/points.py
@@ -21,8 +21,8 @@ class PointsVisual(ClippingPlanesMixin, Compound):
     def __init__(self) -> None:
         super().__init__(
             [
-                Markers(scaling='visual'),
-                Markers(scaling='visual'),
+                Markers(),
+                Markers(),
                 Line(),
                 Text(),
             ]