947ce48f2fd6272a285cf54ba2a061611c26a6ad
Peter Sobolewski <pete.sd@gmail.com>
2024-05-19 15:42:15 -0400

    silence mypy to get tests to run

--- napari/layers/utils/plane.py
+++ napari/layers/utils/plane.py
@@ -237,5 +237,6 @@ class ClippingPlaneList(SelectableEventedList):
                 )
         return cls(planes)
 
-    def add_plane(self, *args, **kwargs) -> None:
+    def add_plane(self, *args, **kwargs) -> None:  # type: ignore
+        """Add a clipping plane to the list."""
         self.append(ClippingPlane(*args, **kwargs))
