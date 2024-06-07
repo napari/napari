7e091ea9159c007eb17dfc5b44533480b0d55831
Grzegorz Bokota <bokota+github@gmail.com>
2024-06-07 11:16:25 +0200

    Do not crash napari viewer if error happen on open file via cli

--- napari/__main__.py
+++ napari/__main__.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from textwrap import wrap
 from typing import Any
 
+from napari.errors import ReaderPluginError
 from napari.utils.translations import trans
 
 
@@ -343,13 +344,18 @@ def _run() -> None:
                 stacklevel=3,
             )
             args.stack = True
-        viewer._window._qt_viewer._qt_open(
-            args.paths,
-            stack=args.stack,
-            plugin=args.plugin,
-            layer_type=args.layer_type,
-            **kwargs,
-        )
+        try:
+            viewer._window._qt_viewer._qt_open(
+                args.paths,
+                stack=args.stack,
+                plugin=args.plugin,
+                layer_type=args.layer_type,
+                **kwargs,
+            )
+        except ReaderPluginError:
+            logging.exception(
+                'Error happen during load file while starting napari'
+            )
 
         if args.with_:
             # Non-npe2 plugins disappear on tabify or if tabified npe2 plugins are loaded after them.
