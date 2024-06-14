cb1e9517de8e6b75cdaa5177f69eec56387ed01a
wmv_hpomen <w-mv@hotmail.com>
2024-06-14 22:10:29 +0200

    fix test

--- napari/utils/migrations.py
+++ napari/utils/migrations.py
@@ -65,7 +65,7 @@ def rename_argument(
                         version=version,
                         since_version=since_version,
                     ),
-                    category=FutureWarning,
+                    category=DeprecationWarning,
                     stacklevel=2,
                 )
                 kwargs = kwargs.copy()
