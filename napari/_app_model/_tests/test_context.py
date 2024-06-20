b9864656dc7b3554cf9eaecaa088e88b2f8ffab5
Grzegorz Bokota <bokota+github@gmail.com>
2024-06-20 09:57:16 +0200

    add context integration test

--- napari/_app_model/_tests/test_context.py
+++ napari/_app_model/_tests/test_context.py
@@ -2,7 +2,11 @@ from unittest.mock import Mock
 
 import pytest
 
-from napari._app_model.context._context import ContextMapping
+from napari._app_model.context._context import (
+    ContextMapping,
+    create_context,
+    get_context,
+)
 
 
 def test_simple_mapping():
@@ -29,3 +33,23 @@ def test_callable_value():
     assert mapping['b'] == 2
     assert mapping['b'] == 2  # it is important to use [] twice in this test
     data['b'].assert_called_once()
+
+
+def test_context_integration():
+    obj = {1, 2, 3}
+    ctx = create_context(obj)
+    ctx['a'] = 1
+    ctx['b'] = Mock(return_value=2)
+
+    assert isinstance(get_context(obj), ContextMapping)
+    mapping = get_context(obj)
+    assert mapping['a'] == 1
+    assert mapping['b'] == 2
+    assert mapping['b'] == 2  # it is important to use [] twice in this test
+    ctx['b'].assert_called_once()
+
+    mapping2 = get_context(obj)
+    assert mapping2['b'] == 2
+    assert ctx['b'].call_count == 2
+    assert mapping2['b'] == 2
+    assert ctx['b'].call_count == 2
