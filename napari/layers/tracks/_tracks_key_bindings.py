068b6e01a64ee16ad13620b01a7a745611879ce7
David Stansby <dstansby@gmail.com>
2024-06-21 11:11:19 +0100

    Some tracks typing fixes

--- napari/layers/tracks/_tracks_key_bindings.py
+++ napari/layers/tracks/_tracks_key_bindings.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from napari.layers.base._base_constants import Mode
 from napari.layers.tracks.tracks import Tracks
 from napari.layers.utils.layer_utils import (
@@ -6,14 +8,16 @@ from napari.layers.utils.layer_utils import (
 )
 from napari.utils.translations import trans
 
-from typing import Callable
 
-
-def register_tracks_action(description: str, repeatable: bool = False) -> Callable[[Callable], Callable]:
+def register_tracks_action(
+    description: str, repeatable: bool = False
+) -> Callable[[Callable], Callable]:
     return register_layer_action(Tracks, description, repeatable)
 
 
-def register_tracks_mode_action(description: str) -> Callable[[Callable], Callable]:
+def register_tracks_mode_action(
+    description: str,
+) -> Callable[[Callable], Callable]:
     return register_layer_attr_action(Tracks, description, 'mode')
 
 
