6541724de326a1b9ae3f8412cf5a942431df2b90
Peter Sobolewski <pete.sd@gmail.com>
2024-06-23 13:50:59 -0400

    add test for y_offset changing with position and font_size

--- napari/_vispy/_tests/test_vispy_scale_bar_visual.py
+++ napari/_vispy/_tests/test_vispy_scale_bar_visual.py
@@ -1,4 +1,5 @@
 from napari._vispy.overlays.scale_bar import VispyScaleBarOverlay
+from napari.components import ViewerModel
 from napari.components.overlays import ScaleBarOverlay
 
 
@@ -6,3 +7,29 @@ def test_scale_bar_instantiation(make_napari_viewer):
     viewer = make_napari_viewer()
     model = ScaleBarOverlay()
     VispyScaleBarOverlay(overlay=model, viewer=viewer)
+
+
+def test_scale_bar_positioning():
+    viewer = ViewerModel()
+    model = ScaleBarOverlay()
+    scale_bar = VispyScaleBarOverlay(overlay=model, viewer=viewer)
+
+    assert model.position == 'bottom_right'
+    assert model.font_size == 10
+    assert scale_bar.y_offset == 20
+
+    # moving scale bar to top should increase y_offset to 30
+    model.position = 'top_right'
+    assert scale_bar.y_offset == 30
+
+    # increasing size while at top should increase y_offset to 10 + 2*font_size
+    model.font_size = 30
+    assert scale_bar.y_offset == 70
+
+    # moving scale bar back to bottom should reset y_offset to 20
+    model.position = 'bottom_right'
+    assert scale_bar.y_offset == 20
+
+    # changing font_size at bottom should have no effect
+    model.font_size = 30
+    assert scale_bar.y_offset == 20
