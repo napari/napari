6b5de69a925133507e6bc6d33744dfa06b2da012
dalthviz <16781833+dalthviz@users.noreply.github.com>
2024-07-02 12:06:01 -0500

    Use 'make_napari_viewer' fixture for layer context menu actions

--- napari/_qt/_qapp_model/_tests/test_layerlist_context_actions.py
+++ napari/_qt/_qapp_model/_tests/test_layerlist_context_actions.py
@@ -4,26 +4,24 @@ from napari._app_model import get_app
 from napari._app_model.actions._layerlist_context_actions import (
     LAYERLIST_CONTEXT_ACTIONS,
 )
-from napari.components.layerlist import LayerList
 
 
 @pytest.mark.parametrize('layer_action', LAYERLIST_CONTEXT_ACTIONS)
-def test_layer_actions_ctx_menu_execute_command(layer_action):
+def test_layer_actions_ctx_menu_execute_command(
+    layer_action, make_napari_viewer
+):
     app = get_app()
+    make_napari_viewer()
     command_id = layer_action.id
 
-    def provide_layer_list() -> 'LayerList':
-        return LayerList()
-
-    with app.injection_store.register(providers=[(provide_layer_list,)]):
-        if command_id == 'napari.layer.merge_stack':
-            with pytest.raises(IndexError, match=r'images list is empty'):
-                app.commands.execute_command(command_id)
-        elif command_id in [
-            'napari.layer.link_selected_layers',
-            'napari.layer.unlink_selected_layers',
-        ]:
-            with pytest.raises(ValueError, match=r'at least one'):
-                app.commands.execute_command(command_id)
-        else:
+    if command_id == 'napari.layer.merge_stack':
+        with pytest.raises(IndexError, match=r'images list is empty'):
             app.commands.execute_command(command_id)
+    elif command_id in [
+        'napari.layer.link_selected_layers',
+        'napari.layer.unlink_selected_layers',
+    ]:
+        with pytest.raises(ValueError, match=r'at least one'):
+            app.commands.execute_command(command_id)
+    else:
+        app.commands.execute_command(command_id)
