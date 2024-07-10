7532b9a4a80ee3dd20696367811a0faf41bc1275
Juan Nunez-Iglesias <jni@fastmail.com>
2024-07-10 17:23:12 +1000

    Add output annotation to add_plane
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>

--- napari/layers/utils/plane.py
+++ napari/layers/utils/plane.py
@@ -237,6 +237,6 @@ class ClippingPlaneList(SelectableEventedList):
                 )
         return cls(planes)
 
-    def add_plane(self, *args, **kwargs):
+    def add_plane(self, *args, **kwargs) -> None:
         """Add a clipping plane to the list."""
         self.append(ClippingPlane(*args, **kwargs))
