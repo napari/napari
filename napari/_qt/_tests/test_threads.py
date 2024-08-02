bac75bac4d78286ab4eef72c2370eab386e469a5
Grzegorz Bokota <bokota+github@gmail.com>
2024-08-02 14:31:21 +0200

    add StatusChecker tests

---
+++ napari/_qt/_tests/test_threads.py
@@ -0,0 +1,43 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from napari._qt.threads.status_checker import StatusChecker
+from napari.components import ViewerModel
+
+
+@pytest.mark.usefixtures('qapp')
+def test_create():
+    StatusChecker(ViewerModel())
+
+
+@pytest.mark.usefixtures('qapp')
+def test_no_emmit_no_ref(monkeypatch):
+    model = ViewerModel()
+    status_checker = StatusChecker(model)
+    monkeypatch.setattr(
+        status_checker,
+        'status_and_tooltip_changed',
+        MagicMock(side_effect=RuntimeError('Should not emit')),
+    )
+    del model
+    status_checker.calculate_status()
+
+
+def test_waiting_on_no_request(monkeypatch, qtbot):
+    def _check_status(value):
+        return value == ('Ready', '')
+
+    model = ViewerModel()
+    model.mouse_over_canvas = True
+    status_checker = StatusChecker(model)
+    status_checker.start()
+    with qtbot.waitSignal(
+        status_checker.status_and_tooltip_changed,
+        timeout=1000,
+        check_params_cb=_check_status,
+    ):
+        status_checker.trigger_status_update()
+    status_checker.terminate()
+
+    qtbot.wait_until(lambda: status_checker.isFinished())
