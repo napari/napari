92f7aca9f51966d94bce7e16bb903bcbec676bc6
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2024-08-08 10:45:41 +0200

    Update napari/_vispy/_tests/test_vispy_scale_bar_visual.py
    Co-authored-by: Lorenzo Gaifas <brisvag@gmail.com>

--- napari/_vispy/_tests/test_vispy_scale_bar_visual.py
+++ napari/_vispy/_tests/test_vispy_scale_bar_visual.py
@@ -5,11 +5,7 @@ from napari.components.overlays import ScaleBarOverlay
 def test_scale_bar_instantiation(make_napari_viewer):
     viewer = make_napari_viewer()
     model = ScaleBarOverlay()
-    model_fixed = ScaleBarOverlay(fixed_width=50)
     vispy_scale_bar = VispyScaleBarOverlay(overlay=model, viewer=viewer)
-    vispy_scale_bar_fixed = VispyScaleBarOverlay(
-        overlay=model_fixed, viewer=viewer
-    )
-
     assert vispy_scale_bar.overlay.fixed_width is None
-    assert vispy_scale_bar_fixed.overlay.fixed_width == 50
+    model.fixed_width = 50
+    assert vispy_scale_bar.overlay.fixed_width == 50
