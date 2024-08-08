0520ee6db981514093ce07f94e0b8c7bd545d160
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-08-08 10:36:16 +0200

    merged testing functionality for fixed width into existing test

--- napari/_vispy/_tests/test_vispy_scale_bar_visual.py
+++ napari/_vispy/_tests/test_vispy_scale_bar_visual.py
@@ -5,11 +5,9 @@ from napari.components.overlays import ScaleBarOverlay
 def test_scale_bar_instantiation(make_napari_viewer):
     viewer = make_napari_viewer()
     model = ScaleBarOverlay()
-    VispyScaleBarOverlay(overlay=model, viewer=viewer)
-
-
-def test_scale_bar_fixed_width(make_napari_viewer):
-    viewer = make_napari_viewer()
-    model = ScaleBarOverlay(fixed_width=50)
+    model_fixed = ScaleBarOverlay(fixed_width=50)
     vispy_scale_bar = VispyScaleBarOverlay(overlay=model, viewer=viewer)
-    assert vispy_scale_bar.overlay.fixed_width == 50
+    vispy_scale_bar_fixed = VispyScaleBarOverlay(overlay=model_fixed, viewer=viewer)
+
+    assert vispy_scale_bar.overlay.fixed_width is None
+    assert vispy_scale_bar_fixed.overlay.fixed_width == 50
