72d0de079049c92b26804a8cf9543de6c4b2a75f
Lucy Liu <jliu176@gmail.com>
2024-08-09 18:20:40 +1000

    FIX `QtViewer._open_files_dialog` handing of `stack` (#7172)
    # References and relevant issues
    
    closes #7165
    
    # Description
    
    Correctly handles `stack` parameter in `_open_files_dialog`. This was
    due to a bad refactor in https://github.com/napari/napari/pull/4865
    
    I noticed that we don't test any of the `QtViewer` `open_files...` or
    `open_folder...` methods. (Some are tested with the file menu but none
    of these seem to be tested in isolation?)
    I've added a new test file for these but only added a test for this
    instance. Maybe additional tests can be added in a follow up PR, in
    order to get this fix in quicker?

--- napari/_qt/qt_viewer.py
+++ napari/_qt/qt_viewer.py
@@ -914,13 +914,8 @@ class QtViewer(QSplitter):
         """Add files from the menubar."""
         filenames = self._open_file_dialog_uni(trans._('Select file(s)...'))
 
-        if (filenames != []) and (filenames is not None):
-            for filename in filenames:
-                self._qt_open(
-                    [filename],
-                    choose_plugin=choose_plugin,
-                    stack=stack,
-                )
+        if filenames:
+            self._qt_open(filenames, choose_plugin=choose_plugin, stack=stack)
             update_open_history(filenames[0])
 
     def _open_files_dialog_as_stack_dialog(self, choose_plugin=False):
