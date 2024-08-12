9c88951d8b9dab931cffe7a27f87e6eb41890e94
Lucy Liu <jliu176@gmail.com>
2024-08-13 02:10:19 +1000

    MNT Parametrize `test_open_files_dialog` to check for `stack` `True` and `False` (#7176)
    # References and relevant issues
    Follow up to #7172
    
    # Description
    
    Parametrizes test to check for stack True and False. I forgot to add
    this to the original PR, sorry for the extra noise.

--- napari/_qt/_tests/test_open_file.py
+++ napari/_qt/_tests/test_open_file.py
@@ -1,7 +1,10 @@
 from unittest import mock
 
+import pytest
 
-def test_open_files_dialog(make_napari_viewer):
+
+@pytest.mark.parametrize('stack', [True, False])
+def test_open_files_dialog(make_napari_viewer, stack):
     """Check `QtViewer._open_files_dialog` correnct when `stack=True`."""
     viewer = make_napari_viewer()
     with (
@@ -10,9 +13,9 @@ def test_open_files_dialog(make_napari_viewer):
         ) as mock_file,
         mock.patch('napari._qt.qt_viewer.QtViewer._qt_open') as mock_open,
     ):
-        viewer.window._qt_viewer._open_files_dialog(stack=True)
+        viewer.window._qt_viewer._open_files_dialog(stack=stack)
     mock_open.assert_called_once_with(
         mock_file.return_value,
         choose_plugin=False,
-        stack=True,
+        stack=stack,
     )
