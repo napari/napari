6bb81c8c540c9e79bf51e39c68ef7e3ee50cda2d
dalthviz <16781833+dalthviz@users.noreply.github.com>
2024-09-02 11:25:33 -0500

    Initial attempt to set empty .svg for actions icons

--- napari/_qt/_qapp_model/qactions/_viewer.py
+++ napari/_qt/_qapp_model/qactions/_viewer.py
@@ -8,6 +8,7 @@ from app_model.types import Action, KeyCode, KeyMod, ToggleRule
 from napari._app_model.constants import MenuId
 from napari._app_model.context import ViewerContextKeys as VCK
 from napari.components import _viewer_key_bindings
+from napari.resources._icons import _theme_path
 from napari.utils.misc import in_ipython, in_jupyter, in_python_repl
 from napari.utils.translations import trans
 
@@ -15,6 +16,7 @@ if TYPE_CHECKING:
     from napari.viewer import Viewer, ViewerModel
 
 
+EMPTY_SVG_PATH = f'file:///{_theme_path("dark")}/empty.svg'
 logger = getLogger(__name__)
 
 
@@ -49,7 +51,7 @@ def add_new_labels(viewer: Union['ViewerModel', 'Viewer']) -> None:
 Q_VIEWER_ACTIONS: list[Action] = [
     Action(
         id='napari.viewer.toggle_console_visibility',
-        title=trans._(''),
+        title=trans._('Toggle console visibility'),
         menus=[
             {
                 'id': MenuId.VIEWER_CONTROLS,
@@ -63,10 +65,11 @@ Q_VIEWER_ACTIONS: list[Action] = [
             'Show/Hide IPython console (only available when napari started as standalone application).'
         ),
         enablement=not (in_ipython() or in_jupyter() or in_python_repl()),
+        icon=EMPTY_SVG_PATH,
     ),
     Action(
         id='napari.viewer.toggle_ndisplay',
-        title=trans._(''),
+        title=trans._('Toggle 2D/3D view'),
         menus=[
             {
                 'id': MenuId.VIEWER_CONTROLS,
@@ -78,10 +81,11 @@ Q_VIEWER_ACTIONS: list[Action] = [
         toggled=ToggleRule(
             condition=VCK.is_viewer_3d, get_current=_get_viewer_ndisplay_status
         ),
+        icon=EMPTY_SVG_PATH,
     ),
     Action(
         id='napari.viewer.roll_axes',
-        title=trans._(''),
+        title=trans._('Roll axes'),
         menus=[
             {
                 'id': MenuId.VIEWER_CONTROLS,
@@ -92,10 +96,11 @@ Q_VIEWER_ACTIONS: list[Action] = [
         tooltip=trans._(
             'Change order of the visible axes, e.g.\u00a0[0,\u00a01,\u00a02]\u00a0\u2011>\u00a0[2,\u00a00,\u00a01].'
         ),
+        icon=EMPTY_SVG_PATH,
     ),
     Action(
         id='napari.viewer.transpose_axes',
-        title=trans._(''),
+        title=trans._('Transpose axes'),
         menus=[
             {
                 'id': MenuId.VIEWER_CONTROLS,
@@ -106,10 +111,11 @@ Q_VIEWER_ACTIONS: list[Action] = [
         tooltip=trans._(
             'Transpose order of the last two visible axes, e.g.\u00a0[0,\u00a01]\u00a0\u2011>\u00a0[1,\u00a00].'
         ),
+        icon=EMPTY_SVG_PATH,
     ),
     Action(
         id='napari.viewer.toggle_grid',
-        title=trans._(''),
+        title=trans._('Toggle grid mode'),
         menus=[
             {
                 'id': MenuId.VIEWER_CONTROLS,
@@ -122,10 +128,11 @@ Q_VIEWER_ACTIONS: list[Action] = [
             condition=VCK.is_viewer_grid_enabled,
             get_current=_get_viewer_grid_status,
         ),
+        icon=EMPTY_SVG_PATH,
     ),
     Action(
         id='napari.viewer.reset_view',
-        title=trans._(''),
+        title=trans._('Reset view'),
         menus=[
             {
                 'id': MenuId.VIEWER_CONTROLS,
@@ -134,37 +141,42 @@ Q_VIEWER_ACTIONS: list[Action] = [
         callback=_viewer_key_bindings.reset_view,
         keybindings=[{'primary': KeyMod.CtrlCmd | KeyCode.KeyR}],
         tooltip=trans._('Reset view to original state.'),
+        icon=EMPTY_SVG_PATH,
     ),
 ]
 
 Q_VIEWER_NEW_DELETE_ACTIONS: list[Action] = [
     Action(
         id='napari.viewer.new_layer.new_points',
-        title=trans._(''),
+        title=trans._('New points layer'),
         menus=[{'id': MenuId.VIEWER_NEW_DELETE_LAYER}],
         callback=add_new_points,
         tooltip=trans._('New points layer.'),
+        icon=EMPTY_SVG_PATH,
     ),
     Action(
         id='napari.viewer.new_layer.new_shapes',
-        title=trans._(''),
+        title=trans._('New shapes layer'),
         menus=[{'id': MenuId.VIEWER_NEW_DELETE_LAYER}],
         callback=add_new_shapes,
         tooltip=trans._('New shapes layer.'),
+        icon=EMPTY_SVG_PATH,
     ),
     Action(
         id='napari.viewer.new_layer.new_labels',
-        title=trans._(''),
+        title=trans._('New labels layer'),
         menus=[{'id': MenuId.VIEWER_NEW_DELETE_LAYER}],
         callback=add_new_labels,
         tooltip=trans._('New labels layer.'),
+        icon=EMPTY_SVG_PATH,
     ),
     Action(
         id='napari.viewer.delete_selected_layers',
-        title=trans._(''),
+        title=trans._('Delete selected layers'),
         menus=[{'id': MenuId.VIEWER_NEW_DELETE_LAYER}],
         callback=_viewer_key_bindings.delete_selected_layers,
         keybindings=[{'primary': KeyMod.CtrlCmd | KeyCode.Delete}],
         tooltip=trans._('Delete selected layers.'),
+        icon=EMPTY_SVG_PATH,
     ),
 ]
