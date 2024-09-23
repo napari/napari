e24c46b3e268d8b3c43920f04cc98d8889878ca3
Peter Sobolewski <pete.sd@gmail.com>
2024-09-22 22:46:03 -0400

    make zoom in on clicked point

--- napari/components/_viewer_mouse_bindings.py
+++ napari/components/_viewer_mouse_bindings.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def dims_scroll(viewer, event):
     """Scroll the dimensions slider."""
     if 'Control' not in event.modifiers:
@@ -16,10 +19,23 @@ def dims_scroll(viewer, event):
 
 
 def double_click_to_zoom(viewer, event):
-    """Zoom on double click."""
-    if viewer.layers.selection.active.mode != 'pan_zoom':
+    """Zoom in on double click by zoom_factor; zoom out with Alt."""
+    if (
+        viewer.layers.selection.active
+        and viewer.layers.selection.active.mode != 'pan_zoom'
+    ):
         return
-    if 'Alt' in event.modifiers:
-        viewer.camera.zoom /= 2
+    # if Alt held down, zoomt out instead
+    zoom_factor = 0.5 if 'Alt' in event.modifiers else 2
+
+    viewer.camera.zoom *= zoom_factor
+    if viewer.dims.ndisplay == 3:
+        viewer.camera.center = np.asarray(viewer.camera.center) + (
+            np.asarray(event.position)[np.asarray(viewer.dims.displayed)]
+            - np.asarray(viewer.camera.center)
+        ) * (1 - 1 / zoom_factor)
     else:
-        viewer.camera.zoom *= 2
+        viewer.camera.center = np.asarray(viewer.camera.center)[-2:] + (
+            np.asarray(event.position)[-2:]
+            - np.asarray(viewer.camera.center)[-2:]
+        ) * (1 - 1 / zoom_factor)
