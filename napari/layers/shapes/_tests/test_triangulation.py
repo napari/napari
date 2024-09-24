ca19212a869124490299b15b2476ab6afa1032d5
Grzegorz Bokota <bokota+github@gmail.com>
2024-09-24 21:55:28 +0200

    add basic tests

---
+++ napari/layers/shapes/_tests/test_triangulation.py
@@ -0,0 +1,40 @@
+from unittest.mock import patch
+from napari.layers.shapes import _accelerated_triangulate as ac
+import numpy as np
+import pytest
+import importlib
+
+
+@pytest.fixture
+def _disable_jit(monkeypatch):
+    """Fixture to temporarily disable numba JIT during testing.
+
+    This helps to measure coverage and in debugging. *However*, reloading a
+    module can cause issues with object instance / class relationships, so
+    the `_accelerated_cmap` module should be as small as possible and contain
+    no class definitions, only functions.
+    """
+    pytest.importorskip('numba')
+    with patch('numba.core.config.DISABLE_JIT', True):
+        importlib.reload(ac)
+        yield
+    importlib.reload(ac)
+
+
+
+@pytest.mark.parametrize(
+    ("path","closed", "bevel", "expeected"),
+    [
+        ([[0, 0], [0, 10], [10, 10], [10, 0]], True, False, 10),
+        ([[0, 0], [0, 10], [10, 10], [10, 0]], False, False, 8),
+        ([[0, 0], [0, 10], [10, 10], [10, 0]], True, True, 14),
+        ([[0, 0], [0, 10], [10, 10], [10, 0]], False, True, 10),
+        ([[2, 10], [0, -5], [-2, 10], [-2, -10], [2, -10]], True, False, 15)
+    ]
+)
+@pytest.mark.usefixtures('_disable_jit')
+def test_generate_2D_edge_meshes(path, closed, bevel, expeected):
+    centers, offsets, triangles = ac.generate_2D_edge_meshes(np.array(path, dtype='float32'), closed=closed, bevel=bevel)
+    assert centers.shape == offsets.shape
+    assert centers.shape[0] == expeected
+    assert triangles.shape[0] == expeected-2
