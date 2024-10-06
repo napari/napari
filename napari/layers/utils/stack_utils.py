2066bdd0d14889edf043647b365552d85e46bb3f
Peter Sobolewski <pete.sd@gmail.com>
2024-10-05 22:03:39 -0400

    fix handling of scale & translate

--- napari/layers/utils/stack_utils.py
+++ napari/layers/utils/stack_utils.py
@@ -288,14 +288,17 @@ def images_to_stack(images: list[Image], axis: int = 0, **kwargs) -> Image:
 
     data, meta, _ = images[0].as_layer_data_tuple()
 
+    # RGB images do not need extra dimensions inserted into metadata
+    if 'rgb' not in kwargs:
+        kwargs.setdefault('scale', np.insert(meta['scale'], axis, 1))
+        kwargs.setdefault('translate', np.insert(meta['translate'], axis, 0))
+
     meta.update(kwargs)
     new_data = np.stack([image.data for image in images], axis=axis)
 
     # RGB images do not need extra dimensions inserted into metadata
     # They can use the meta dict from one of the source image layers
     if not meta['rgb']:
-        kwargs.setdefault('scale', np.insert(meta['scale'], axis, 1))
-        kwargs.setdefault('translate', np.insert(meta['translate'], axis, 0))
         meta['units'] = (pint.get_application_registry().pixel,) + meta[
             'units'
         ]
