310d116dac5c5484ca84cd5e3795bbb68d13f721
Grzegorz Bokota <bokota+github@gmail.com>
2024-10-15 21:23:35 +0200

    more clean cache

--- napari/layers/shapes/_shape_list.py
+++ napari/layers/shapes/_shape_list.py
@@ -757,6 +757,7 @@ class ShapeList:
             indices = self._index == index
             self._vertices[indices] = shape.data_displayed
             self._update_displayed()
+        self._clear_cache()
 
     @_batch_dec
     def _update_z_order(self):
