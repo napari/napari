bb3a7df6555c8ceae3ee50cc118049cfe2b5366e
dalthviz <16781833+dalthviz@users.noreply.github.com>
2024-11-08 14:05:19 -0500

    Typing

--- napari/_qt/layer_controls/widgets/qt_widget_controls_base.py
+++ napari/_qt/layer_controls/widgets/qt_widget_controls_base.py
@@ -11,7 +11,7 @@ class QtWrappedLabel(QLabel):
     to the right and vertically centered by default.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.setWordWrap(True)
         self.setAlignment(
