ead82abe186b51efc636ad2a101ad90b2ae8f05b
Grzegorz Bokota <bokota+github@gmail.com>
2024-11-25 23:11:51 +0100

    Fix remainning errors

--- napari/layers/points/points.py
+++ napari/layers/points/points.py
@@ -653,10 +653,9 @@ class Points(Layer):
         """Set the data array and emit a corresponding event."""
         prior_data = len(self.data) > 0
         data_not_empty = (
-            (data is not None
-            and (isinstance(data, np.ndarray) and data.size > 0))
-            or (isinstance(data, list) and len(data) > 0)
-        )
+            data is not None
+            and (isinstance(data, np.ndarray) and data.size > 0)
+        ) or (isinstance(data, list) and len(data) > 0)
         kwargs = {
             'value': self.data,
             'vertex_indices': ((),),
