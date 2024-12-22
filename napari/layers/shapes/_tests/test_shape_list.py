napari/napari

commit d6f40870c7268450c7f3dd0eec38028b05b31129

Speed up `get_status` for Shapes layer by using bounding boxes (#7144)
# References and relevant issues

# Description

When investigating why napari interface is so lagging with big number of
layers, I have spotted that Shapes triangulation is used to determine if
the mouse is inside some shape. However, a single shape could be
constructed from multiple polygons. Also, edge polygons are used.

This PR adds information about bounding box to Shape object. Then
filtering of Shapes based on belonging cursor position to bounding box
of shape.

Then checking if the cursor is in such a filtered list of shapes is
performed based on triangles (same as previous).

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

napari/layers/shapes/_shape_list.py
@@ -1,7 +1,7 @@
 import typing
 from collections.abc import Generator, Iterable, Sequence
 from contextlib import contextmanager
-from functools import wraps
+from functools import cached_property, wraps
 from typing import Literal, Union
 
 import numpy as np
@@ -495,6 +495,7 @@ class ShapeList:
         if z_refresh:
             # Set z_order
             self._update_z_order()
+        self._clear_cache()
 
     def _add_multiple_shapes(
         self,
@@ -658,6 +659,7 @@ class ShapeList:
         if z_refresh:
             # Set z_order
             self._update_z_order()
+        self._clear_cache()
 
     @_batch_dec
     def remove_all(self):
@@ -1018,6 +1020,7 @@ class ShapeList:
         self.remove(index, renumber=False)
         self.add(shape, shape_index=index)
         self._update_z_order()
+        self._clear_cache()
 
     def outline(
         self, indices: Union[int, Sequence[int]]
@@ -1098,6 +1101,11 @@ class ShapeList:
 
         return shapes
 
+    @cached_property
+    def _bounding_boxes(self):
+        data = np.array([s.bounding_box for s in self.shapes])
+        return data[:, 0], data[:, 1]
+
     def inside(self, coord):
         """Determines if any shape at given coord by looking inside triangle
         meshes. Looks only at displayed shapes
@@ -1113,17 +1121,30 @@ class ShapeList:
             Index of shape if any that is at the coordinates. Returns `None`
             if no shape is found.
         """
-        triangles = self._mesh.vertices[self._mesh.displayed_triangles]
-        indices = inside_triangles(triangles - coord)
-        shapes = self._mesh.displayed_triangles_index[indices, 0]
-
-        if len(shapes) == 0:
+        if not self.shapes:
             return None
-
-        z_list = self._z_order.tolist()
-        order_indices = np.array([z_list.index(m) for m in shapes])
-        ordered_shapes = shapes[np.argsort(order_indices)]
-        return ordered_shapes[0]
+        bounding_boxes = self._bounding_boxes
+        in_bbox = np.all(
+            (bounding_boxes[0] <= coord) * (bounding_boxes[1] >= coord),
+            axis=1,
+        )
+        inside_indices = np.flatnonzero(in_bbox)
+        if inside_indices.size == 0:
+            return None
+        try:
+            z_index = [self.shapes[i].z_index for i in inside_indices]
+            pos = np.argsort(z_index)
+            return next(
+                inside_indices[p]
+                for p in pos[::-1]
+                if np.any(
+                    inside_triangles(
+                        self.shapes[inside_indices[p]]._all_triangles() - coord
+                    )
+                )
+            )
+        except StopIteration:
+            return None
 
     def _inside_3d(self, ray_position: np.ndarray, ray_direction: np.ndarray):
         """Determines if any shape is intersected by a ray by looking inside triangle
@@ -1322,7 +1343,7 @@ class ShapeList:
         # If there are too many shapes to render responsively, just render
         # the top max_shapes shapes
         if max_shapes is not None and len(z_order_in_view) > max_shapes:
-            z_order_in_view = z_order_in_view[0:max_shapes]
+            z_order_in_view = z_order_in_view[:max_shapes]
 
         for ind in z_order_in_view:
             mask = self.shapes[ind].to_mask(
@@ -1335,3 +1356,6 @@ class ShapeList:
             colors[mask, :] = col
 
         return colors
+
+    def _clear_cache(self):
+        self.__dict__.pop('_bounding_boxes', None)

napari/layers/shapes/_shapes_models/shape.py
@@ -119,6 +119,7 @@ class Shape(ABC):
         self.name = ''
 
         self._data: npt.NDArray
+        self._bounding_box = np.empty((0, self.ndisplay))
 
     @property
     @abstractmethod
@@ -163,6 +164,11 @@ class Shape(ABC):
     def dims_displayed(self):
         """tuple: Dimensions that are displayed."""
         return self.dims_order[-self.ndisplay :]
+
+    @property
+    def bounding_box(self) -> np.ndarray:
+        """(2, N) array, bounding box of the object."""
+        return self._bounding_box[:, self.dims_displayed]
 
     @property
     def dims_not_displayed(self):
@@ -254,6 +260,23 @@ class Shape(ABC):
             self._face_vertices = np.empty((0, self.ndisplay))
             self._face_triangles = np.empty((0, 3), dtype=np.uint32)
 
+    def _all_triangles(self):
+        """Return all triangles for the shape
+
+        Returns
+        -------
+        np.ndarray
+            Nx3 array of vertex indices that form the triangles for the shape
+        """
+        return np.vstack(
+            [
+                self._face_vertices[self._face_triangles],
+                (self._edge_vertices + self.edge_width * self._edge_offsets)[
+                    self._edge_triangles
+                ],
+            ]
+        )
+
     def transform(self, transform: npt.NDArray) -> None:
         """Performs a linear transform on the shape
 
@@ -276,6 +299,12 @@ class Shape(ABC):
         self._edge_vertices = centers
         self._edge_offsets = offsets
         self._edge_triangles = triangles
+        self._bounding_box = np.array(
+            [
+                np.min(self._data, axis=0),
+                np.max(self._data, axis=0),
+            ]
+        )
 
     def shift(self, shift: npt.NDArray) -> None:
         """Performs a 2D shift on the shape
@@ -291,6 +320,9 @@ class Shape(ABC):
         self._edge_vertices = self._edge_vertices + shift
         self._box = self._box + shift
         self._data[:, self.dims_displayed] = self.data_displayed + shift
+        self._bounding_box[:, self.dims_displayed] = (
+            self._bounding_box[:, self.dims_displayed] + shift
+        )
 
     def scale(self, scale, center=None):
         """Performs a scaling on the shape

napari/benchmarks/benchmark_shapes_layer.py
@@ -46,7 +46,8 @@ class Shapes2DSuite:
 
     def time_get_value(self, n):
         """Time to get current value."""
-        self.layer.get_value((0,) * 2)
+        for i in range(100):
+            self.layer.get_value((i,) * 2)
 
     def mem_layer(self, n):
         """Memory used by layer."""

napari/layers/shapes/_shapes_models/_polygon_base.py
@@ -80,6 +80,12 @@ class PolygonBase(Shape):
             )
 
         self._data = data
+        self._bounding_box = np.array(
+            [
+                np.min(data, axis=0),
+                np.max(data, axis=0),
+            ]
+        )
         self._update_displayed_data()
 
     def _update_displayed_data(self) -> None:
@@ -117,10 +123,6 @@ class PolygonBase(Shape):
         self._set_meshes(data, face=self._filled, closed=self._closed)
         self._box = create_box(self.data_displayed)
 
-        data_not_displayed = self.data[:, self.dims_not_displayed]
         self.slice_key = np.round(
-            [
-                np.min(data_not_displayed, axis=0),
-                np.max(data_not_displayed, axis=0),
-            ]
+            self._bounding_box[:, self.dims_not_displayed]
         ).astype('int')

napari/layers/shapes/_shapes_models/_tests/test_shapes_models.py
@@ -1,6 +1,7 @@
 import sys
 
 import numpy as np
+import numpy.testing as npt
 import pytest
 from vispy.geometry import PolygonData
 
@@ -30,6 +31,29 @@ def test_rectangle():
     assert len(shape.data) == 4
     assert shape.data_displayed.shape == (4, 2)
     assert shape.slice_key.shape == (2, 0)
+
+
+def test_rectangle_shift():
+    shape = Rectangle(np.array([[0, 0], [1, 0], [1, 1], [0, 1]]))
+    npt.assert_array_equal(shape.bounding_box, np.array([[0, 0], [1, 1]]))
+
+    shape.shift((1, 1))
+    npt.assert_array_equal(
+        shape.data, np.array([[1, 1], [2, 1], [2, 2], [1, 2]])
+    )
+    npt.assert_array_equal(shape.bounding_box, np.array([[1, 1], [2, 2]]))
+
+
+def test_rectangle_rotate():
+    shape = Rectangle(np.array([[1, 2], [-1, 2], [-1, -2], [1, -2]]))
+    npt.assert_array_equal(shape.bounding_box, np.array([[-1, -2], [1, 2]]))
+    shape.rotate(-90)
+    npt.assert_array_almost_equal(
+        shape.data, np.array([[-2, 1], [-2, -1], [2, -1], [2, 1]])
+    )
+    npt.assert_array_almost_equal(
+        shape.bounding_box, np.array([[-2, -1], [2, 1]])
+    )
 
 
 def test_nD_rectangle():
@@ -219,3 +243,26 @@ def test_nD_ellipse():
 
     shape.ndisplay = 3
     assert shape.data_displayed.shape == (4, 3)
+
+
+def test_ellipse_shift():
+    shape = Ellipse(np.array([[0, 0], [1, 0], [1, 1], [0, 1]]))
+    npt.assert_array_equal(shape.bounding_box, np.array([[0, 0], [1, 1]]))
+
+    shape.shift((1, 1))
+    npt.assert_array_equal(
+        shape.data, np.array([[1, 1], [2, 1], [2, 2], [1, 2]])
+    )
+    npt.assert_array_equal(shape.bounding_box, np.array([[1, 1], [2, 2]]))
+
+
+def test_ellipse_rotate():
+    shape = Ellipse(np.array([[1, 2], [-1, 2], [-1, -2], [1, -2]]))
+    npt.assert_array_equal(shape.bounding_box, np.array([[-1, -2], [1, 2]]))
+    shape.rotate(-90)
+    npt.assert_array_almost_equal(
+        shape.data, np.array([[-2, 1], [-2, -1], [2, -1], [2, 1]])
+    )
+    npt.assert_array_almost_equal(
+        shape.bounding_box, np.array([[-2, -1], [2, 1]])
+    )

napari/layers/shapes/_shapes_models/ellipse.py
@@ -77,6 +77,12 @@ class Ellipse(Shape):
             )
 
         self._data = data
+        self._bounding_box = np.round(
+            [
+                np.min(data, axis=0),
+                np.max(data, axis=0),
+            ]
+        )
         self._update_displayed_data()
 
     def _update_displayed_data(self) -> None:
@@ -88,13 +94,9 @@ class Ellipse(Shape):
         self._face_triangles = triangles
         self._box = rectangle_to_box(self.data_displayed)
 
-        data_not_displayed = self.data[:, self.dims_not_displayed]
-        self.slice_key = np.round(
-            [
-                np.min(data_not_displayed, axis=0),
-                np.max(data_not_displayed, axis=0),
-            ]
-        ).astype('int')
+        self.slice_key = self._bounding_box[:, self.dims_not_displayed].astype(
+            'int'
+        )
 
     def transform(self, transform):
         """Performs a linear transform on the shape
@@ -118,3 +120,9 @@ class Ellipse(Shape):
         self._edge_vertices = centers
         self._edge_offsets = offsets
         self._edge_triangles = triangles
+        self._bounding_box = np.array(
+            [
+                np.min(self._data, axis=0),
+                np.max(self._data, axis=0),
+            ]
+        )

napari/layers/shapes/_shapes_models/line.py
@@ -62,6 +62,13 @@ class Line(Shape):
             )
 
         self._data = data
+        self._bounding_box = np.array(
+            [
+                np.min(data, axis=0),
+                np.max(data, axis=0),
+            ]
+        )
+
         self._update_displayed_data()
 
     def _update_displayed_data(self) -> None:
@@ -70,10 +77,6 @@ class Line(Shape):
         self._set_meshes(self.data_displayed, face=False, closed=False)
         self._box = create_box(self.data_displayed)
 
-        data_not_displayed = self.data[:, self.dims_not_displayed]
         self.slice_key = np.round(
-            [
-                np.min(data_not_displayed, axis=0),
-                np.max(data_not_displayed, axis=0),
-            ]
+            self._bounding_box[:, self.dims_not_displayed]
         ).astype('int')

napari/layers/shapes/_shapes_models/rectangle.py
@@ -68,6 +68,12 @@ class Rectangle(Shape):
             )
 
         self._data = data
+        self._bounding_box = np.array(
+            [
+                np.min(data, axis=0),
+                np.max(data, axis=0),
+            ]
+        )
         self._update_displayed_data()
 
     def _update_displayed_data(self) -> None:
@@ -77,10 +83,6 @@ class Rectangle(Shape):
         self._face_vertices = self.data_displayed
         self._face_triangles = np.array([[0, 1, 2], [0, 2, 3]])
         self._box = rectangle_to_box(self.data_displayed)
-        data_not_displayed = self.data[:, self.dims_not_displayed]
-        self.slice_key = np.round(
-            [
-                np.min(data_not_displayed, axis=0),
-                np.max(data_not_displayed, axis=0),
-            ]
-        ).astype('int')
+        self.slice_key = self._bounding_box[:, self.dims_not_displayed].astype(
+            'int'
+        )

commit 681132f908e57475d3f00d7d3482b32fcf1e13f2

Fix selection of nD-sliced shapes (#7459)
# References and relevant issues

closes #7458  

# Description

This PR closes bug with selecting shapes only from current slice,
introduced in #7144.

The source of the bug is not filtering out-of-slice shapes when
searching for intersecting triangles.

napari/layers/shapes/_shape_list.py
@@ -255,6 +255,7 @@ class ShapeList:
         slice_key = list(slice_key)
         if not np.array_equal(self._slice_key, slice_key):
             self._slice_key = slice_key
+            self._clear_cache()
             self._update_displayed()
 
     def _update_displayed(self) -> None:
@@ -1106,8 +1107,21 @@ class ShapeList:
         return shapes
 
     @cached_property
+    def _visible_shapes(self):
+        slice_key = self.slice_key
+        if len(slice_key):
+            return [
+                (i, s)
+                for i, s in enumerate(self.shapes)
+                if s.slice_key[0] <= slice_key <= s.slice_key[1]
+            ]
+        return list(enumerate(self.shapes))
+
+    @cached_property
     def _bounding_boxes(self):
-        data = np.array([s.bounding_box for s in self.shapes])
+        data = np.array([s[1].bounding_box for s in self._visible_shapes])
+        if data.size == 0:
+            return np.empty((0, self.ndisplay)), np.empty((0, self.ndisplay))
         return data[:, 0], data[:, 1]
 
     def inside(self, coord):
@@ -1136,17 +1150,24 @@ class ShapeList:
         if inside_indices.size == 0:
             return None
         try:
-            z_index = [self.shapes[i].z_index for i in inside_indices]
+            z_index = [
+                self._visible_shapes[i][1].z_index for i in inside_indices
+            ]
             pos = np.argsort(z_index)
-            return next(
-                inside_indices[p]
-                for p in pos[::-1]
-                if np.any(
-                    inside_triangles(
-                        self.shapes[inside_indices[p]]._all_triangles() - coord
+            return self._visible_shapes[
+                next(
+                    inside_indices[p]
+                    for p in pos[::-1]
+                    if np.any(
+                        inside_triangles(
+                            self._visible_shapes[inside_indices[p]][
+                                1
+                            ]._all_triangles()
+                            - coord
+                        )
                     )
                 )
-            )
+            ][0]
         except StopIteration:
             return None
 
@@ -1363,3 +1384,4 @@ class ShapeList:
 
     def _clear_cache(self):
         self.__dict__.pop('_bounding_boxes', None)
+        self.__dict__.pop('_visible_shapes', None)

napari/layers/shapes/_shapes_models/shape.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import Optional
 
 import numpy as np
 import numpy.typing as npt
@@ -129,7 +128,7 @@ class Shape(ABC):
     ) -> None:
         self._dims_order = dims_order or list(range(2))
         self._ndisplay = ndisplay
-        self.slice_key: Optional[npt.NDArray] = None
+        self.slice_key: npt.NDArray
 
         self._face_vertices = np.empty((0, self.ndisplay))
         self._face_triangles = np.empty((0, 3), dtype=np.uint32)

napari/layers/shapes/_tests/test_shape_list.py
@@ -150,3 +150,14 @@ def test_bad_color_array(attribute):
     bad_color_array = np.array([[0, 0, 0, 1], [1, 1, 1, 1]])
     with pytest.raises(ValueError, match='must have shape'):
         setattr(shape_list, f'{attribute}_color', bad_color_array)
+
+
+def test_inside():
+    shape1 = Polygon(np.array([[0, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]]))
+    shape2 = Polygon(np.array([[1, 0, 0], [1, 1, 0], [1, 1, 1], [1, 0, 1]]))
+    shape3 = Polygon(np.array([[2, 0, 0], [2, 1, 0], [2, 1, 1], [2, 0, 1]]))
+
+    shape_list = ShapeList()
+    shape_list.add([shape1, shape2, shape3])
+    shape_list.slice_key = (1,)
+    assert shape_list.inside((0.5, 0.5)) == 1