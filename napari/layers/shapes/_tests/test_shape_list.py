napari/napari

commit cab7c661bef5daf9a74b312b390ea10715eefe63

Speed up highlighting in the Shapes layer (#7223)
# References and relevant issues

This PR fixes highlight performance problems in the Shapes layer. #7162
tried to fix them by reducing the frequency of highlight events, but
that had to be reverted (#7187, #7201). Instead, this dramatically
speeds up highlighting when the event is fired.

# Description

For performance reasons, the Shapes layer creates a *single* mesh
containing all shapes and shape edges, and this is what is sent to VisPy
to draw.

When highlighting, prior to this PR, we needed to find the Shape edges
that are being highlighted within that giant mesh (linear in the number
of vertices, and iterated in a Python for-loop).

With this PR, instead, we look at the precomputed mesh stored in each
highlighted shape and stack them together. Therefore, the complexity is
linear in the number of vertices in the highlighted shapes, and the
constant factors should also be reduced.

Before this PR:
<img width="343" alt="Screenshot 2024-08-29 at 11 50 34"
src="https://github.com/user-attachments/assets/bf2fb01d-494a-42c4-8f06-53dc14dbac0b">

After This PR

<img width="273" alt="Screenshot 2024-08-29 at 11 50 43"
src="https://github.com/user-attachments/assets/52720dad-19bc-42c4-949d-e62bd5544e34">

napari/layers/shapes/_shape_list.py
@@ -1019,7 +1019,9 @@ class ShapeList:
         self.add(shape, shape_index=index)
         self._update_z_order()
 
-    def outline(self, indices: Union[int, Sequence[int]]):
+    def outline(
+        self, indices: Union[int, Sequence[int]]
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         """Finds outlines of shapes listed in indices
 
         Parameters
@@ -1038,10 +1040,17 @@ class ShapeList:
             Mx3 array of any indices of vertices for triangles of outline
         """
         if not isinstance(indices, Sequence):
-            indices = [indices]
+            shape = self.shapes[indices]
+            return (
+                shape._edge_vertices,
+                shape._edge_offsets,
+                shape._edge_triangles,
+            )
         return self.outlines(indices)
 
-    def outlines(self, indices: Sequence[int]):
+    def outlines(
+        self, indices: Sequence[int]
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         """Finds outlines of shapes listed in indices
 
         Parameters
@@ -1058,31 +1067,10 @@ class ShapeList:
         triangles : np.ndarray
             Mx3 array of any indices of vertices for triangles of outline
         """
-        indices_set = set(indices)
-        meshes = self._mesh.triangles_index
-        triangle_indices = [
-            i
-            for i, x in enumerate(meshes)
-            if x[0] in indices_set and x[1] == 1
-        ]
-        meshes = self._mesh.vertices_index
-        vertices_indices = [
-            i
-            for i, x in enumerate(meshes)
-            if x[0] in indices_set and x[1] == 1
-        ]
-
-        offsets = self._mesh.vertices_offsets[vertices_indices]
-        centers = self._mesh.vertices_centers[vertices_indices]
-        triangles = self._mesh.triangles[triangle_indices]
-
-        t_ind = self._mesh.triangles_index[triangle_indices][:, 0]
-        inds = self._mesh.vertices_index[vertices_indices][:, 0]
-        starts = np.unique(inds, return_index=True)[1]
-        for i, ind in enumerate(indices):
-            inds = t_ind == ind
-            adjust_index = starts[i] - vertices_indices[starts[i]]
-            triangles[inds] = triangles[inds] + adjust_index
+        shapes_list = [self.shapes[i] for i in indices]
+        offsets = np.vstack([s._edge_offsets for s in shapes_list])
+        centers = np.vstack([s._edge_vertices for s in shapes_list])
+        triangles = np.vstack([s._edge_triangles for s in shapes_list])
 
         return centers, offsets, triangles
 

commit 282aae62d77a473dcea554a1e18188f64c04f2d3

Fix highlighting artifacts when selecting multiple shapes (#7457)
# References and relevant issues

closes #7244

# Description

This PR fixes highlighting by updating triangle indices when stacking
all shape triangles together.

Fixes the bug first introduced in #7223

napari/layers/shapes/_shape_list.py
@@ -1078,7 +1078,12 @@ class ShapeList:
         shapes_list = [self.shapes[i] for i in indices]
         offsets = np.vstack([s._edge_offsets for s in shapes_list])
         centers = np.vstack([s._edge_vertices for s in shapes_list])
-        triangles = np.vstack([s._edge_triangles for s in shapes_list])
+        vert_count = np.cumsum(
+            [0] + [len(s._edge_vertices) for s in shapes_list]
+        )
+        triangles = np.vstack(
+            [s._edge_triangles + c for s, c in zip(shapes_list, vert_count)]
+        )
 
         return centers, offsets, triangles
 

napari/layers/shapes/_tests/test_shape_list.py
@@ -110,6 +110,18 @@ def test_shape_list_outline():
         assert np.array_equal(value_by_idx, value_by_idx_np)
 
 
+def test_shape_list_outline_two_shapes():
+    shape1 = Polygon([[0, 0], [0, 10], [10, 10], [10, 0]])
+    shape2 = Polygon([[20, 20], [20, 30], [30, 30], [30, 20]])
+    shape_list = ShapeList()
+    shape_list.add([shape1, shape2])
+
+    # check if the outline contains triangle with vertex of number 16
+
+    triangles = shape_list.outline([0, 1])[2]
+    assert np.any(triangles == 16)
+
+
 def test_nD_shapes():
     """Test adding shapes to ShapeList."""
     np.random.seed(0)