136b2dfb7bfe4a0f63c1b49ce79109e34c496a39
Peter Sobolewski <pete.sd@gmail.com>
2025-01-11 22:20:21 -0500

    fix test

--- napari/_vispy/_tests/test_vispy_scale_bar_visual.py
+++ napari/_vispy/_tests/test_vispy_scale_bar_visual.py
@@ -1,4 +1,3 @@
-from sys import platform
 from unittest.mock import MagicMock
 
 import pytest
@@ -27,24 +26,17 @@ def test_scale_bar_positioning(make_napari_viewer):
     assert model.font_size == 10
     assert scale_bar.y_offset == 20
 
-    # moving scale bar to top should increase y_offset to 30
     model.position = 'top_right'
-    if platform == 'darwin':
-        assert scale_bar.y_offset == 30
-    else:
-        assert scale_bar.y_offset == pytest.approx(36.667, abs=0.01)
+    assert scale_bar.y_offset == pytest.approx(20.333, abs=0.1)
 
-    # increasing size while at top should increase y_offset to 10 + 2*font_size
+    # increasing size while at top should increase y_offset to 7 + font_size*1.33
     model.font_size = 30
-    if platform == 'darwin':
-        assert scale_bar.y_offset == 70
-    else:
-        assert scale_bar.y_offset == pytest.approx(90.0, abs=0.01)
+    assert scale_bar.y_offset == pytest.approx(47, abs=0.1)
 
     # moving scale bar back to bottom should reset y_offset to 20
     model.position = 'bottom_right'
     assert scale_bar.y_offset == 20
 
     # changing font_size at bottom should have no effect
-    model.font_size = 30
+    model.font_size = 50
     assert scale_bar.y_offset == 20
