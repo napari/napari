e953a2e2cb04321f5d364b74744f5bbe079824e4
Grzegorz Bokota <bokota+github@gmail.com>
2025-02-03 15:05:34 +0100

    Apply suggestions from code review
    Co-authored-by: Juan Nunez-Iglesias <jni@fastmail.com>

--- napari/_vispy/mouse_event.py
+++ napari/_vispy/mouse_event.py
@@ -12,7 +12,6 @@ if TYPE_CHECKING:
 class NapariMouseEvent(MouseEvent):
     """NapariMouseEvent extends MouseEvent to include additional information.
 
-
     Parameters
     ----------
     event : MouseEvent
