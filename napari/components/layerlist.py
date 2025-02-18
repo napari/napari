cedba6ac72bf21fbf44e466a4646d94ee8271008
Juan Nunez-Iglesias <jni@fastmail.com>
2025-02-18 21:18:12 +1100

    Use plural on the changed overload docstring for multiple elements

--- napari/components/layerlist.py
+++ napari/components/layerlist.py
@@ -49,9 +49,11 @@ class LayerList(SelectableEventedList[Layer]):
     moved : (index: int, new_index: int, value: T)
         emitted after ``value`` is moved from ``index`` to ``new_index``
     changed : (index: int, old_value: T, value: T)
-        emitted when item at ``index`` is changed from ``old_value`` to ``value``
+        emitted when item at ``index`` is changed from ``old_value`` to
+        ``value``
     changed <OVERLOAD> : (index: slice, old_value: List[_T], value: List[_T])
-        emitted when item at ``index`` is changed from ``old_value`` to ``value``
+        emitted when items at ``index``es are changed from ``old_value`` to
+        ``value``
     reordered : (value: self)
         emitted when the list is reordered (eg. moved/reversed).
     selection.events.changed : (added: Set[_T], removed: Set[_T])
