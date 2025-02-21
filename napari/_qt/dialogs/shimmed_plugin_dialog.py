c6ee2f429b56ede9b4bbc580ef4ed4477f06a454
Draga Doncila <ddon0001@student.monash.edu>
2025-02-21 16:08:45 +1100

    Bump minor schema version, remove cancel button

--- napari/_qt/dialogs/shimmed_plugin_dialog.py
+++ napari/_qt/dialogs/shimmed_plugin_dialog.py
@@ -16,33 +16,32 @@ from napari.utils.translations import trans
 class ShimmedPluginDialog(QDialog):
     def __init__(self, parent: QWidget, plugins: set[str]) -> None:
         super().__init__(parent)
-        cancel_btn = QPushButton(trans._('Cancel'))
-        close_btn = QPushButton(trans._('Close'))
+        okay_btn = QPushButton(trans._('Okay'))
         icon_label = QWidget()
         icon_label.setObjectName('warning_icon_element')
         plugin_info_text = """
-The following plugins use napari's old plugin engine. These plugins have been
-automatically converted to the new plugin engine, npe2:
+The following plugins use napari's old plugin engine. These plugins have been automatically converted to the new plugin engine, npe2:
         """
         plugin_text = '\n'.join(plugins)
         info_text = """
 While the conversion should work for these plugins, some plugin functionality, such as code designed to run on import, may not work as expected.
 
-If you notice an error in any of these plugins, drop back to
-using the old plugin engine by turning off
-the 'Use npe2 adaptor' setting in the plugin preferences.
+If you notice an error in any of these plugins, drop back to using the old plugin engine by turning off the 'Use npe2 adaptor' setting in the plugin preferences.
 
-The 'Use npe2 adaptor' setting will be removed in napari 0.7.0, and plugins
-will only be usable if they can be automatically converted or updated to use npe2.
+The 'Use npe2 adaptor' setting will be removed in napari 0.7.0, and plugins will only be usable if they can be automatically converted or updated to use npe2.
 
 For plugin upgrades, contact the plugin's author and request npe2 updates.
         """
         self.only_new_checkbox = QCheckBox(
             trans._('Only warn me about newly installed plugins.')
         )
+        new_only = (
+            get_settings().plugins.warn_on_shimmed_plugin
+            == PluginShimWarningLevel.NEW
+        )
+        self.only_new_checkbox.setChecked(new_only)
 
-        cancel_btn.clicked.connect(self.reject)
-        close_btn.clicked.connect(self.accept)
+        okay_btn.clicked.connect(self.accept)
 
         layout = QVBoxLayout()
         layout2 = QHBoxLayout()
@@ -56,8 +55,7 @@ For plugin upgrades, contact the plugin's author and request npe2 updates.
         layout4 = QHBoxLayout()
         layout4.addWidget(self.only_new_checkbox)
         layout4.addStretch(1)
-        layout4.addWidget(cancel_btn)
-        layout4.addWidget(close_btn)
+        layout4.addWidget(okay_btn)
         layout.addLayout(layout2)
         layout.addLayout(layout3)
         layout.addLayout(layout4)
@@ -65,12 +63,15 @@ For plugin upgrades, contact the plugin's author and request npe2 updates.
 
         # for test purposes because of problem with shortcut testing:
         # https://github.com/pytest-dev/pytest-qt/issues/254
-        self.close_btn = close_btn
-        self.cancel_btn = cancel_btn
+        self.okay_btn = okay_btn
 
     def accept(self) -> None:
         if self.only_new_checkbox.isChecked():
             get_settings().plugins.warn_on_shimmed_plugin = (
                 PluginShimWarningLevel.NEW
+            )
+        else:
+            get_settings().plugins.warn_on_shimmed_plugin = (
+                PluginShimWarningLevel.ALWAYS
             )
         super().accept()
