1e8b7da0ddba4c87d7f5555f1dc4e866f977354e
Juan Nunez-Iglesias <jni@fastmail.com>
2025-03-04 19:06:31 +1100

    Mark the right test as xfail 🤦

--- napari/_vispy/_tests/test_vispy_camera.py
+++ napari/_vispy/_tests/test_vispy_camera.py
@@ -120,15 +120,6 @@ def test_vispy_camera_update_from_model_3D(make_napari_viewer):
     np.testing.assert_almost_equal(viewer.camera.zoom, vispy_camera.zoom)
 
 
-@pytest.mark.xfail(
-    condition=(
-        sys.version_info >= (3, 13)
-        and sys.platform.startswith('darwin')
-        and os.getenv('CI', '0') != '0'
-        and PYQT5
-    ),
-    reason='test fails on this specific CI config for some reason',
-)
 def test_camera_model_update_from_vispy_3D(make_napari_viewer):
     """Test camera model updates from vispy in 3D."""
     viewer = make_napari_viewer()
@@ -192,6 +183,15 @@ def test_camera_orientation_2d(make_napari_viewer):
     assert np.all(avg_col_intensity_grad2 <= 0)
 
 
+@pytest.mark.xfail(
+    condition=(
+        sys.version_info >= (3, 13)
+        and sys.platform.startswith('darwin')
+        and os.getenv('CI', '0') != '0'
+        and PYQT5
+    ),
+    reason='test fails on this specific CI config for some reason',
+)
 def test_camera_orientation_3d(make_napari_viewer):
     """Test that flipping camera orientation in 3D flips volume as expected."""
     viewer = make_napari_viewer()
