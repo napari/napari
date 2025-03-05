ad6080f43a67a83c5d020ebc6b0a6d057383053d
Juan Nunez-Iglesias <jni@fastmail.com>
2025-03-05 19:02:49 +1100

    Add API to Camera model to flip axes (#7663)
    In #7554 / #7488 we changed the default orientation of axes in 3D mode.
    This may be undesirable to some users, so this PR adds the ability to
    flip them.
    
    It also turns out to be handy when displaying data which has specific
    absolute coordinates as well as a common convention for the display
    direction, such as geographical data. See the included example which
    both demonstrates how to use napari with xarray as well as how to use
    this new camera axis-flip API. Currently I'm pretty happy with the API
    (see below), but I'm very much open to alternative suggestions!
    
    ## API description
    
    This adds an `orientation` attribute to our Camera model. The scene is
    assumed to have three axes, z, y, and x, which are aligned with the
    depth, vertical, and horizontal axes of the camera. Each of these axes
    can then point either:
    - away or towards the camera
    - down or up
    - right or left
    
    I used a StrEnum for each of these values, which makes the API both
    robust for type checking and easy to use, e.g.:
    
    ```python
    viewer.camera.orientation = ('away', 'up', 'right')
    ```
    
    I also added an `orientation2d` property, because it's strange to set
    three values to flip two axes, even though that is the Vispy model.
    (camera.flip is a 3-tuple even for 2D cameras in Vispy.) You can see its
    use in the example. To show latitude (-90 for south, 90 for north), we
    want the vertical axis to point up, not down like in the napari default.
    So we do:
    
    ```python
    viewer.camera.orientation2d = ('up', 'right')
    ```
    
    and the geographic raster of temperatures is displayed correctly:
    
    ![CleanShot 2025-03-03 at 12 42
    18@2x](https://github.com/user-attachments/assets/03ee83dd-9272-4991-bf28-524ecd5ff321)
    
    Without this API, the map ends up upside down:
    
    ![CleanShot 2025-03-04 at 18 29
    50@2x](https://github.com/user-attachments/assets/bb51c197-9fe4-42c8-bd28-687309b46829)
    
    ---------
    
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- napari/_vispy/_tests/test_vispy_camera.py
+++ napari/_vispy/_tests/test_vispy_camera.py
@@ -1,4 +1,9 @@
+import os
+import sys
+
 import numpy as np
+import pytest
+from qtpy import PYQT5
 
 
 def test_camera(make_napari_viewer):
@@ -138,3 +143,76 @@ def test_camera_model_update_from_vispy_3D(make_napari_viewer):
     np.testing.assert_almost_equal(viewer.camera.angles, vispy_camera.angles)
     np.testing.assert_almost_equal(viewer.camera.center, vispy_camera.center)
     np.testing.assert_almost_equal(viewer.camera.zoom, vispy_camera.zoom)
+
+
+def test_camera_orientation_2d(make_napari_viewer):
+    """Test that flipping orientation of the camera flips displayed image."""
+    viewer = make_napari_viewer()
+    data = np.arange(16).reshape((4, 4))
+    _ = viewer.add_image(data, interpolation2d='linear')
+
+    # in the default axis orientation of (down, right), the values in a
+    # screenshot should continually increase as you go down in the image.
+    # We take only the first channel in the RGBA array for simplicity, since
+    # this is a grayscale image.
+    sshot0 = viewer.screenshot(canvas_only=True, flash=False)[..., 0]
+    # check that the values are monotonically increasing down:
+    avg_row_intensity_grad0 = np.diff(np.mean(sshot0, axis=1))
+    assert np.all(avg_row_intensity_grad0 >= 0)
+
+    # same but to the right
+    avg_col_intensity_grad0 = np.diff(np.mean(sshot0, axis=0))
+    assert np.all(avg_col_intensity_grad0 >= 0)
+
+    # now we reverse the orientation of the vertical axis, and check that the
+    # row gradient has changed direction but not the col gradient
+    viewer.camera.orientation2d = ('up', 'right')
+    sshot1 = viewer.screenshot(canvas_only=True, flash=False)[..., 0]
+    avg_row_intensity_grad1 = np.diff(np.mean(sshot1, axis=1))
+    assert np.all(avg_row_intensity_grad1 <= 0)  # note inverted sign
+    avg_col_intensity_grad1 = np.diff(np.mean(sshot1, axis=0))
+    assert np.all(avg_col_intensity_grad1 >= 0)
+
+    # finally, reverse orientation of horizontal axis, check that col gradient
+    # has now also changed direction
+    viewer.camera.orientation2d = ('up', 'left')
+    sshot2 = viewer.screenshot(canvas_only=True, flash=False)[..., 0]
+    avg_row_intensity_grad2 = np.diff(np.mean(sshot2, axis=1))
+    assert np.all(avg_row_intensity_grad2 <= 0)  # note inverted sign
+    avg_col_intensity_grad2 = np.diff(np.mean(sshot2, axis=0))
+    assert np.all(avg_col_intensity_grad2 <= 0)
+
+
+@pytest.mark.xfail(
+    condition=(
+        sys.version_info >= (3, 13)
+        and sys.platform.startswith('darwin')
+        and os.getenv('CI', '0') != '0'
+        and PYQT5
+    ),
+    reason='test sometimes fails on this specific CI config for some reason',
+    strict=False,
+)
+def test_camera_orientation_3d(make_napari_viewer):
+    """Test that flipping camera orientation in 3D flips volume as expected."""
+    viewer = make_napari_viewer()
+    viewer.dims.ndisplay = 3
+    gradient_z = np.arange(16).reshape((16, 1, 1))
+    image = np.ones((16, 16))
+    image_3d = gradient_z * image
+    _ = viewer.add_image(image_3d)
+
+    # We have a 3D image with brightness *increasing with z* (the 0th axis).
+    # Therefore, when using a perspective projection, if we point z *away* from
+    # us, the bright part of the image will be small, whereas if we point z
+    # *towards* us, it will be large. We test the dimension flip by comparing
+    # the overall brightness of the image when pointing z in different
+    # directions
+
+    viewer.camera.perspective = 60
+    viewer.camera.orientation = ('away', 'down', 'right')
+    sshot_away = viewer.screenshot(canvas_only=True, flash=False)[..., 0]
+    viewer.camera.orientation = ('towards', 'down', 'right')
+    sshot_towards = viewer.screenshot(canvas_only=True, flash=False)[..., 0]
+
+    assert np.mean(sshot_towards) > np.mean(sshot_away)
