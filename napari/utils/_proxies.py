20e9548e314c792d1bfe18e13cfb52769986ed06
Carol Willing <carolcode@willingconsulting.com>
2025-03-18 12:04:57 -0700

    add back attribute error handling for private attributes

--- napari/utils/_proxies.py
+++ napari/utils/_proxies.py
@@ -74,6 +74,16 @@ class PublicOnlyProxy(wrapt.ObjectProxy, Generic[_T]):
             stacklevel=3,
         )
 
+        # This is code prepared for a moment where we want to block access to private attributes
+        raise AttributeError(
+            trans._(
+                "Private attribute set/access ('{typ}.{name}') not allowed in this context.",
+                deferred=True,
+                name=name,
+                typ=typ,
+            )
+        )
+
     @staticmethod
     def _is_called_from_napari() -> bool:
         """
