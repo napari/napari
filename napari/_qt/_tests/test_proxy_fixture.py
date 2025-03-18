7e82d71d237c69981714dca701f75ce3b9678f86
Carol Willing <carolcode@willingconsulting.com>
2025-03-18 00:01:03 -0700

    remove proxy fixture test for deprecation warning

--- napari/_qt/_tests/test_proxy_fixture.py
+++ napari/_qt/_tests/test_proxy_fixture.py
@@ -1,15 +1,5 @@
 import pytest
 
-from napari.utils import misc
-
-
-def test_proxy_fixture_warning(make_napari_viewer_proxy, monkeypatch):
-    viewer = make_napari_viewer_proxy()
-
-    monkeypatch.setattr(misc, 'ROOT_DIR', '/some/other/package')
-    with pytest.warns(FutureWarning, match='Private attribute access'):
-        viewer.window._qt_window
-
 
 def test_proxy_fixture_thread_error(
     make_napari_viewer_proxy, single_threaded_executor
