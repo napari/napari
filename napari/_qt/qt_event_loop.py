50611441aa9ebe70f56864c555f5f9416a5baf57
Carol Willing <carolcode@willingconsulting.com>
2025-03-19 10:05:45 -0700

    Remove get_app in qt_event_loop and get_gui_qt

--- napari/_qt/qt_event_loop.py
+++ napari/_qt/qt_event_loop.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import os
 import sys
-from contextlib import contextmanager
 from typing import TYPE_CHECKING, cast
 from warnings import warn
 
@@ -89,21 +88,6 @@ def _focus_changed(old: QWidget | None, new: QWidget | None):
     else:
         for notification in notifications:
             notification.timer_stop()
-
-
-# TODO: Remove in napari 0.6.0
-def get_app(*args, **kwargs) -> QApplication:
-    """Get or create the Qt QApplication. Now deprecated, use `get_qapp`."""
-    warn(
-        trans._(
-            '`QApplication` instance access through `get_app` is deprecated and will be removed in 0.6.0.\n'
-            'Please use `get_qapp` instead.\n',
-            deferred=True,
-        ),
-        category=FutureWarning,
-        stacklevel=2,
-    )
-    return get_qapp(*args, **kwargs)
 
 
 def get_qapp(
@@ -302,53 +286,6 @@ def quit_app():
         from napari.components.experimental.monitor import monitor
 
         monitor.stop()
-
-
-@contextmanager
-def gui_qt(*, startup_logo=False, gui_exceptions=False, force=False):
-    """Start a Qt event loop in which to run the application.
-
-    NOTE: This context manager is deprecated!. Prefer using :func:`napari.run`.
-
-    Parameters
-    ----------
-    startup_logo : bool, optional
-        Show a splash screen with the napari logo during startup.
-    gui_exceptions : bool, optional
-        Whether to show uncaught exceptions in the GUI, by default they will be
-        shown in the console that launched the event loop.
-    force : bool, optional
-        Force the application event_loop to start, even if there are no top
-        level widgets to show.
-
-    Notes
-    -----
-    This context manager is not needed if running napari within an interactive
-    IPython session. In this case, use the ``%gui qt`` magic command, or start
-    IPython with the Qt GUI event loop enabled by default by using
-    ``ipython --gui=qt``.
-    """
-    warn(
-        trans._(
-            "\nThe 'gui_qt()' context manager is deprecated.\nIf you are running napari from a script, please use 'napari.run()' as follows:\n\n    import napari\n\n    viewer = napari.Viewer()  # no prior setup needed\n    # other code using the viewer...\n    napari.run()\n\nIn IPython or Jupyter, 'napari.run()' is not necessary. napari will automatically\nstart an interactive event loop for you: \n\n    import napari\n    viewer = napari.Viewer()  # that's it!\n",
-            deferred=True,
-        ),
-        FutureWarning,
-        stacklevel=2,
-    )
-
-    app = get_app()
-    splash = None
-    if startup_logo and app.applicationName() == 'napari':
-        from napari._qt.widgets.qt_splash_screen import NapariSplashScreen
-
-        splash = NapariSplashScreen()
-        splash.close()
-    try:
-        yield app
-    except Exception:  # noqa: BLE001
-        notification_manager.receive_error(*sys.exc_info())
-    run(force=force, gui_exceptions=gui_exceptions, _func_name='gui_qt')
 
 
 def _ipython_has_eventloop() -> bool:
