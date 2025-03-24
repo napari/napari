78f682cbac5b1d3e4ada085cc848b78a1194dda5
Carol Willing <carolcode@willingconsulting.com>
2025-03-23 23:56:22 -0700

    Remove deprecation in test

--- napari/_vispy/_tests/test_image_rendering.py
+++ napari/_vispy/_tests/test_image_rendering.py
@@ -20,11 +20,7 @@ def test_image_rendering(make_napari_viewer):
     assert layer.rendering == 'mip'
 
     # Change the interpolation property
-    with pytest.deprecated_call():
-        layer.interpolation = 'linear'
     assert layer.interpolation2d == 'nearest'
-    with pytest.deprecated_call():
-        assert layer.interpolation == 'linear'
     assert layer.interpolation3d == 'linear'
 
     # Change rendering property
