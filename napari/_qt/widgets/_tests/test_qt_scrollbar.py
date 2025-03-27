19a1700bc45218e9e2b80abc0213e7f826cda8c9
TimMonko <47310455+TimMonko@users.noreply.github.com>
2025-03-26 22:26:16 -0500

    enable high dpi scaling

--- napari/_qt/widgets/_tests/test_qt_scrollbar.py
+++ napari/_qt/widgets/_tests/test_qt_scrollbar.py
@@ -1,6 +1,11 @@
 from qtpy.QtCore import QPoint, Qt
+from qtpy.QtWidgets import QApplication
 
 from napari._qt.widgets.qt_scrollbar import ModifiedScrollBar
+
+# Enable high DPI scaling prior to instantiating QApplication
+# This is required for the test to pass on high DPI displays
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
 
 
 def test_modified_scrollbar_click(qtbot):
