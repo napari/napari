8ef00b6b91632b4341afebbd58c1caff7f32bf70
TimMonko <47310455+TimMonko@users.noreply.github.com>
2025-04-02 21:50:38 -0500

    remove deprecation, maintain original reset_view functionality

--- napari/components/viewer_model.py
+++ napari/components/viewer_model.py
@@ -383,7 +383,7 @@ class ViewerModel(KeymapProvider, MousemapProvider, EventedModel):
         return self.layers._extent_world_augmented[:, self.dims.displayed]
 
     def reset_view(
-        self, *, margin: float = 0.05, reset_camera_angle: bool | None = None
+        self, *, margin: float = 0.05, reset_camera_angle: bool = True
     ) -> None:
         """Reset the camera and fit the current layers to the canvas.
 
@@ -399,24 +399,8 @@ class ViewerModel(KeymapProvider, MousemapProvider, EventedModel):
         reset_camera_angle : bool
             Whether to reset the camera angles to (0, 0, 90) before fitting
             to view. Default is True.
-
-            .. deprecated:: 0.6.0
-
-                The `reset_camera_angle` paramater is deprecated and will be
-                removed in 0.7.0.
-                Continue to use `reset_view()` to reset the camera angles.
-                Use `fit_to_view()` to adjust the camera without
-                resetting the camera angles.
         """
-        if reset_camera_angle is not None:
-            warnings.warn(
-                'reset_camera_angle is deprecated since napari 0.6.0 and will be removed in 0.7.0.'
-                'Continue to use `viewer.reset_view()` to reset the camera angles.'
-                'Use `viewer.fit_to_view()` to adjust the camera without resetting the camera angles.',
-                category=FutureWarning,
-            )
-
-        if reset_camera_angle or reset_camera_angle is None:
+        if self.dims.ndisplay == 3 and reset_camera_angle:
             self.camera.angles = (0, 0, 90)
         self.fit_to_view(margin=margin)
 
