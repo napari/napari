c3daa90681be475be6cf82a32ade5aa86c855a97
Lorenzo Gaifas <brisvag@gmail.com>
2025-04-18 16:27:20 +0200

    flip grid according to camera

--- napari/_vispy/overlays/grid.py
+++ napari/_vispy/overlays/grid.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from napari._vispy.overlays.base import ViewerOverlayMixin, VispySceneOverlay
 from napari._vispy.visuals.grid import Grid
 
@@ -11,13 +13,19 @@ class VispyGridOverlay(ViewerOverlayMixin, VispySceneOverlay):
             parent=parent,
         )
 
+        self._old_directions = [1, 1, 1]
+
         self.viewer.dims.events.order.connect(self._on_data_change)
         self.viewer.dims.events.range.connect(self._on_data_change)
         self.viewer.dims.events.ndisplay.connect(self._on_data_change)
+        self.viewer.camera.events.angles.connect(
+            self._on_view_direction_change
+        )
 
         self.reset()
 
     def _on_data_change(self):
+        # napari dims are zyx, but vispy uses xyz
         displayed = self.viewer.dims.displayed[::-1]
         ndim = len(displayed)
         ranges = []
@@ -32,6 +40,26 @@ class VispyGridOverlay(ViewerOverlayMixin, VispySceneOverlay):
 
         self.node.set_extents(ranges)
 
+    def _on_view_direction_change(self):
+        if self.viewer.dims.ndisplay == 2:
+            return
+
+        # this works but it's EXPENSIVE, we need to debounce or find a cheaper check
+        directions = np.sign(self.viewer.camera.view_direction)[::-1]
+        if np.array_equal(self._old_directions, directions):
+            return
+
+        # flip to xyz
+        displayed = self.viewer.dims.order[-3:][::-1]  # so it's always 3
+        top_bounds = [self.viewer.dims.range[i].stop for i in displayed]
+
+        self.node.set_view_directions(
+            top_bounds,
+            directions,
+        )
+        self._old_directions = directions
+
     def reset(self):
         super().reset()
         self._on_data_change()
+        self._on_view_direction_change()
--- napari/_vispy/visuals/grid.py
+++ napari/_vispy/visuals/grid.py
@@ -8,11 +8,15 @@ class Grid(Node):
         # compound does not play well with sub-transforms for some reason
         # so we use a simple empty node with children instead
         self.grids = []
-        for i in range(3):
+        for _ in range(3):
             grid = GridLines(parent=self, border_width=0)
-            grid.transform = MatrixTransform()
-            grid.transform.rotate(angle=120 * i, axis=(1, 1, 1))
             self.grids.append(grid)
+        self.reset_grid_transforms()
+
+    def reset_grid_transforms(self):
+        for i in range(3):
+            self.grids[i].transform = MatrixTransform()
+            self.grids[i].transform.rotate(angle=120 * i, axis=(1, 1, 1))
 
     def set_gl_state(self, *args, **kwargs):
         for grid in self.grids:
@@ -21,3 +25,11 @@ class Grid(Node):
     def set_extents(self, bounds):
         for grid, bound in zip(self.grids, bounds, strict=False):
             grid.grid_bounds = bound
+
+    def set_view_directions(self, bounds, directions):
+        self.reset_grid_transforms()
+        for i in range(3):
+            if directions[i] == 1:
+                trans = [0, 0, 0]
+                trans[i] = bounds[i]
+                self.grids[(i + 1) % 3].transform.translate(trans)
