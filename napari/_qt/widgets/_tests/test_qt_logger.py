9c257ed11a28892da9c2e7d66bbcdd237e416613
Grzegorz Bokota <bokota+github@gmail.com>
2025-04-22 20:45:37 +0200

    fix

--- napari/_qt/widgets/_tests/test_qt_logger.py
+++ napari/_qt/widgets/_tests/test_qt_logger.py
@@ -3,7 +3,7 @@ import pytest
 from napari._qt.widgets.qt_logger import LogWidget
 
 
-@pytest.skip('This test is not currently working')
+@pytest.mark.skip('This test is not currently working')
 def test_qt_logger(qtbot):
     widget = LogWidget()
     qtbot.addWidget(widget)
