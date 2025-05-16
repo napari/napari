751b4dad74984f95949687cbf8293b317e5f4465
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-05-16 17:54:59 +0200

    feat: extend interaction box with rotation option and add example
    - example currently not working, investigating why

---
+++ examples/dev/interaction_box.py
@@ -0,0 +1,103 @@
+import warnings
+
+import numpy as np
+from magicgui import magicgui
+
+import napari
+from napari._vispy.overlays.base import LayerOverlayMixin, VispySceneOverlay
+from napari._vispy.overlays.interaction_box import (
+    InteractionBox,
+)
+from napari._vispy.utils.visual import overlay_to_visual
+from napari.components.overlays.interaction_box import SelectionBoxOverlay
+from napari.layers import Image
+
+
+# define a model for the selection box overlay;
+# we subclass from SelectionBoxOverlay to get the
+# default behavior of the selection box; we redefine
+# the handles attribute to True to show the handles
+class SelectionBoxNoRotation(SelectionBoxOverlay):
+    """Selection box overlay with no rotation handle."""
+
+    rotation: bool = False
+    handles: bool = True
+
+
+# we also need to define an equivalent vispy overlay;
+# again, we subclass from VispySelectionBoxOverlay
+class VispySelectionBoxNoRotation(LayerOverlayMixin, VispySceneOverlay):
+    """Vispy selection box overlay with no rotation handle."""
+
+    node: InteractionBox
+
+    def __init__(self, *, layer, overlay, parent=None) -> None:
+        super().__init__(
+            node=InteractionBox(),
+            layer=layer,
+            overlay=overlay,
+            parent=parent,
+        )
+        self.overlay.events.bounds.connect(self._on_bounds_change)
+        self.overlay.events.handles.connect(self._on_bounds_change)
+        self.overlay.events.selected_handle.connect(self._on_bounds_change)
+        self.overlay.events.visible.connect(self._on_visible_change)
+        self.layer.events.set_data.connect(self._on_visible_change)
+
+    def _on_bounds_change(self) -> None:
+        if self.layer._slice_input.ndisplay == 2:
+            top_left, bot_right = self.overlay.bounds
+            self.node.set_data(
+                # invert axes for vispy
+                top_left[::-1],
+                bot_right[::-1],
+                handles=self.overlay.handles,
+                selected=self.overlay.selected_handle,
+                rotation=self.overlay.rotation,
+            )
+
+    def _on_visible_change(self) -> None:
+        if self.layer._slice_input.ndisplay == 2:
+            super()._on_visible_change()
+            self._on_bounds_change()
+        else:
+            self.node.visible = False
+
+    def reset(self):
+        super().reset()
+        self._on_bounds_change()
+
+
+# register the new overlay classes
+overlay_to_visual[SelectionBoxNoRotation] = VispySelectionBoxNoRotation
+
+viewer = napari.Viewer()
+
+data = np.random.randint(0, 255, size=(1024, 1024), dtype=np.uint8)
+image = viewer.add_image(
+    data,
+    name='image',
+)
+
+# just for type checking
+assert isinstance(image, Image)
+
+image._overlays['selection_no_rotation'] = SelectionBoxNoRotation(
+    bounds=[(0, 0), data.shape]
+)
+
+
+@magicgui
+def toggle_overlay(
+    viewer: napari.Viewer, transform_box: bool = False, selection_no_rotation: bool = False
+) -> None:
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        viewer.layers['image']._overlays['selection_no_rotation'].visible = selection_no_rotation
+        viewer.layers['image']._overlays["transform_box"].visible = transform_box
+
+
+viewer.window.add_dock_widget(toggle_overlay)
+
+if __name__ == '__main__':
+    napari.run()
--- napari/_vispy/visuals/interaction_box.py
+++ napari/_vispy/visuals/interaction_box.py
@@ -1,4 +1,4 @@
-from typing import Any, ClassVar, Optional
+from typing import Any, ClassVar
 
 import numpy as np
 import numpy.typing as npt
@@ -11,15 +11,21 @@ from napari.layers.utils.interaction_box import (
 
 
 class InteractionBox(Compound):
-    # vertices are generated according to the following scheme:
-    # (y is actually upside down in the canvas)
-    #      8
-    #      |
-    #  0---4---2    1 = position
-    #  |       |
-    #  5   9   6
-    #  |       |
-    #  1---7---3
+    """Vispy element for displaying an interaction box.
+
+    Visualizes a rectangle with handles at the corners and midpoints,
+    and a rotation handle above the top center of the rectangle.
+
+    Vertices are generated according to the following scheme:
+    (y is actually upside down in the canvas)
+        8
+        |
+    0---4---2    1 = position
+    |       |
+    5   9   6
+    |       |
+    1---7---3
+    """
     _edges: ClassVar[npt.NDArray[Any]] = np.array(
         [
             [0, 1],
@@ -42,10 +48,12 @@ class InteractionBox(Compound):
 
     @property
     def line(self) -> Line:
+        """The interaction box line visual component."""
         return self._subvisuals[0]
 
     @property
     def markers(self) -> Markers:
+        """The interaction box markers visual component."""
         return self._subvisuals[1]
 
     def set_data(
@@ -53,13 +61,30 @@ class InteractionBox(Compound):
         top_left: tuple[float, float],
         bot_right: tuple[float, float],
         handles: bool = True,
-        selected: Optional[int] = None,
+        selected: int | None = None,
+        rotation: bool = True,
     ) -> None:
+        """Update the visualized interaction box with new data.
+
+        Parameters
+        ----------
+        top_left : tuple[float, float]
+            The top left corner of the interaction box.
+        bot_right : tuple[float, float]
+            The bottom right corner of the interaction box.
+        handles : bool
+            Whether to show the handles of the interaction box.
+        selected : int | None
+            The index of the selected handle. If None, no handle is selected.
+        rotation : bool
+            Whether to show the rotation handle. Default is True.
+        """
         vertices = generate_interaction_box_vertices(
-            top_left, bot_right, handles=handles
+            top_left, bot_right, handles=handles, rotation=rotation
         )
 
         edges = self._edges if handles else self._edges[:4]
+        markers = self._marker_symbol if rotation else self._marker_symbol[:7]
 
         self.line.set_data(pos=vertices, connect=edges)
 
@@ -72,7 +97,7 @@ class InteractionBox(Compound):
                 pos=vertices,
                 size=self._marker_size,
                 face_color=self._marker_color,
-                symbol=self._marker_symbol,
+                symbol=markers,
                 edge_width=marker_edges,
                 edge_color=self._edge_color,
             )
