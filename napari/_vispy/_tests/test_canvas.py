64e9d87223ea68e2399a8771d45bca9ad5c3fcfd
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2025-05-22 12:27:45 +0200

    fix test_viewer_overlays

--- napari/_vispy/_tests/test_canvas.py
+++ napari/_vispy/_tests/test_canvas.py
@@ -16,28 +16,28 @@ def test_viewer_overlays(make_napari_viewer):
                 for visual in canvas._overlay_to_visuals[overlay]
             )
         else:
-            assert (
-                canvas._overlay_to_visual[overlay].node
-                in canvas.view.scene.children
+            assert all(
+                visual.node in canvas.view.scene.children
+                for visual in canvas._overlay_to_visuals[overlay]
             )
 
-    old_vispy_overlays = list(canvas._overlay_to_visual.values())
+    old_vispy_overlays = list(canvas._overlay_to_visuals.values())
 
     new_overlay = ScaleBarOverlay()
     viewer._overlays['test'] = new_overlay
 
-    assert new_overlay in canvas._overlay_to_visual
-    new_overlay_node = canvas._overlay_to_visual[new_overlay].node
+    assert new_overlay in canvas._overlay_to_visuals
+    new_overlay_node = canvas._overlay_to_visuals[new_overlay][0].node
     assert new_overlay_node not in canvas.view.scene.children
     assert new_overlay_node in canvas.view.children
 
     # old visuals should be removed, as everything was recreated
     for old_ov in old_vispy_overlays:
-        assert old_ov.node not in canvas.view.scene.children
-        assert old_ov.node not in canvas.view.children
+        assert old_ov[0].node not in canvas.view.scene.children
+        assert old_ov[0].node not in canvas.view.children
 
     viewer._overlays.pop('test')
-    assert new_overlay not in canvas._overlay_to_visual
+    assert new_overlay not in canvas._overlay_to_visuals
     assert new_overlay_node not in canvas.view.children
 
 
