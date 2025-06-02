c7a9af82e563d417e7ba37fc99fd829ee1342a99
TimMonko <47310455+TimMonko@users.noreply.github.com>
2025-06-02 16:50:14 -0500

    add example and new README image

---
+++ examples/view_image.py
@@ -0,0 +1,23 @@
+"""
+View Image and Launch napari
+============================
+
+Launch an instance of napari and simultaneously display an image using the
+:func:`view_image` API. This example can be run to recreate the README image.
+
+.. tags:: visualization-basic
+"""
+
+from skimage import data
+
+import napari
+
+viewer = napari.view_image(data.cells3d(), channel_axis=1, ndisplay=3)
+
+# To recreate the README image uncomment the following lines:
+# viewer.window._qt_window.resize(800, 600)
+# napari._qt.qt_event_loop.get_qapp().processEvents()  # Ensure the window is done rendering before taking a screenshot
+# viewer.screenshot('./src/napari/resources/multichannel_cells.png', canvas_only=False)
+
+if __name__ == '__main__':
+    napari.run()
