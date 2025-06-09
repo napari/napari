54faedd33ab3781bc8947f89ed9f5944decc1d72
Lukasz Migas <lukas.migas@yahoo.com>
2025-06-09 21:29:03 +0100

    Update zoom.py

--- src/napari/_vispy/utils/zoom.py
+++ src/napari/_vispy/utils/zoom.py
@@ -19,11 +19,11 @@ def calculate_zoom_proportion(
     x_center = (x_min + x_max) / 2
     y_center = (y_min + y_max) / 2
     # using the viewer's scene size to calculate zoom
-    _, scene_size, _, _ = viewer._get_scene_parameters()
+    _, _, _, total_size = viewer._get_scene_parameters()
     y_diff = y_max - y_min
     x_diff = x_max - x_min
     # calculate average zoom based on the size of the rectangle
-    zoom = np.mean(scene_size / (y_diff, x_diff))
+    zoom = np.mean(total_size / (y_diff, x_diff))
     # ensure zoom is a valid number
     if np.isinf(zoom) or np.isnan(zoom) or zoom == 0:
         zoom = 1
