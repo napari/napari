ddeed5d9dbb9f52bd4c06978f324924a2ef49c57
Lorenzo Gaifas <brisvag@gmail.com>
2025-06-11 15:42:02 +0200

    happy mypy

--- src/napari/_qt/widgets/qt_mirrored_sliders_popup.py
+++ src/napari/_qt/widgets/qt_mirrored_sliders_popup.py
@@ -1,5 +1,5 @@
 from qtpy.QtCore import Qt
-from qtpy.QtWidgets import QApplication, QHBoxLayout, QPushButton
+from qtpy.QtWidgets import QApplication, QHBoxLayout, QPushButton, QWidget
 from superqt import QLabeledSlider
 
 from napari._qt.dialogs.qt_modal import QtPopup
@@ -20,7 +20,7 @@ class QMirroredSlidersPopup(QtPopup):
         Slider widget.
     """
 
-    def __init__(self, parent=None) -> None:
+    def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
 
         self.toggle_lock = QPushButton()
@@ -34,13 +34,7 @@ class QMirroredSlidersPopup(QtPopup):
         self.left_slider.setEdgeLabelPosition(
             QLabeledSlider.LabelPosition.LabelsLeft
         )
-        # TODO: also move label to the left (see pyapp-kit/superqt#294)
-        self.left_slider.label_shift_x = 2
-        self.left_slider.label_shift_y = 2
-
         self.right_slider = QLabeledSlider(Qt.Orientation.Horizontal, parent)
-        self.right_slider.label_shift_x = 2
-        self.right_slider.label_shift_y = 2
 
         self.left_slider.setFocus()
 
@@ -56,31 +50,16 @@ class QMirroredSlidersPopup(QtPopup):
         self.left_slider.valueChanged.connect(self._on_left_slider_change)
         self.right_slider.valueChanged.connect(self._on_right_slider_change)
 
-    def _on_left_slider_change(self, value):
+    def _on_left_slider_change(self, value: int) -> None:
         if self.toggle_lock.isChecked() and self.right_slider.value() != value:
             self.right_slider.setValue(value)
 
-    def _on_right_slider_change(self, value):
+    def _on_right_slider_change(self, value: int) -> None:
         if self.toggle_lock.isChecked() and self.left_slider.value() != value:
             self.left_slider.setValue(value)
 
-    def _on_toggle_change(self):
+    def _on_toggle_change(self) -> None:
         if self.toggle_lock.isChecked():
             mean = (self.left_slider.value() + self.right_slider.value()) // 2
             self.left_slider.setValue(mean)
             self.right_slider.setValue(mean)
-
-    def keyPressEvent(self, event):
-        """On key press lose focus of the lineEdits.
-
-        Parameters
-        ----------
-        event : qtpy.QtCore.QKeyEvent
-            Event from the Qt context.
-        """
-        # we override the parent keyPressEvent so that hitting enter does not
-        # hide the window... but we do want to lose focus on the lineEdits
-        if event.key() in (Qt.Key.Key_Return, Qt.Key.Key_Enter):
-            self.left_slider.setFocus()
-            return
-        super().keyPressEvent(event)
