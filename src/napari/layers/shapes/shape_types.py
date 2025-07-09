ce3b10b04ab6cd3528ec92889665eb11d23012d4
Grzegorz Bokota <bokota+github@gmail.com>
2025-07-09 08:24:37 +0200

    sort type definitions

--- src/napari/layers/shapes/shape_types.py
+++ src/napari/layers/shapes/shape_types.py
@@ -21,12 +21,13 @@ __all__ = (
 )
 
 
-IndexDtype = np.int32
-ZOrderDtype = np.int32
 CoordinateDtype = np.float32
+IndexDtype = np.int32
 ShapeColorDtype = np.float32
 TriangleDtype = np.int32
+ZOrderDtype = np.int32
 
+BoxArray = np.ndarray[tuple[Literal[9], int], np.dtype[np.float32]]
 CoordinateArray2D = np.ndarray[
     tuple[int, Literal[2]], np.dtype[CoordinateDtype]
 ]
@@ -35,9 +36,8 @@ CoordinateArray3D = np.ndarray[
 ]
 CoordinateArray = CoordinateArray2D | CoordinateArray3D
 EdgeArray = np.ndarray[tuple[int, Literal[2]], np.dtype[np.int64]]
+IndexArray = np.ndarray[tuple[int], np.dtype[IndexDtype]]
+ShapeColor = np.ndarray[tuple[Literal[4]], np.dtype[ShapeColorDtype]]
+ShapeColorArray = np.ndarray[tuple[int, Literal[4]], np.dtype[ShapeColorDtype]]
 TriangleArray = np.ndarray[tuple[int, Literal[3]], np.dtype[TriangleDtype]]
 ZOrderArray = np.ndarray[tuple[int], np.dtype[ZOrderDtype]]
-BoxArray = np.ndarray[tuple[Literal[9], int], np.dtype[np.float32]]
-ShapeColorArray = np.ndarray[tuple[int, Literal[4]], np.dtype[ShapeColorDtype]]
-ShapeColor = np.ndarray[tuple[Literal[4]], np.dtype[ShapeColorDtype]]
-IndexArray = np.ndarray[tuple[int], np.dtype[IndexDtype]]
