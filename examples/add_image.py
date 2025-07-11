89d2179e2b87f785895a57946c7a3508d9c0aaa1
a <a>
2025-07-11 21:17:45 +0530

    Replace  from `view_image` to `add_image`

--- examples/add_image.py
+++ examples/add_image.py
@@ -12,7 +12,8 @@ from skimage import data
 import napari
 
 # create the viewer with an image
-viewer = napari.view_image(data.astronaut(), rgb=True)
+viewer = napari.Viewer()
+layer = viewer.add_image(data.astronaut(), rgb=True)
 
 if __name__ == '__main__':
     napari.run()
