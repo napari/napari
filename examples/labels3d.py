46a05ffbbfbe7820d121bc78705ec0d267f57c64
a <a>
2025-07-14 13:12:03 +0530

    Fix variable assignment for image layers in labels3d example

--- examples/labels3d.py
+++ examples/labels3d.py
@@ -16,7 +16,7 @@ import napari
 
 cells3d = data.cells3d()
 viewer = napari.Viewer()
-layer = viewer.add_image(
+membranes_layer, nuclei_layer = viewer.add_image(
     cells3d, channel_axis=1, name=['membranes', 'nuclei']
 )
 membrane, nuclei = cells3d.transpose((1, 0, 2, 3)) / np.max(cells3d)
