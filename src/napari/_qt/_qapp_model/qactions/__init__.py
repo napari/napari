5721d4d88db6ba1c59afa4cfd55c74f8fecf1145
Grzegorz Bokota <bokota+github@gmail.com>
2025-07-15 06:05:08 +0200

    Move non-qt file actions from qactions module (#8076)
    # References and relevant issues
    
    
    # Description
    
    Move `Actions` that do not require qt from `qactions/_file.py` to
    `actions/_file.py`.

--- src/napari/_qt/_qapp_model/qactions/__init__.py
+++ src/napari/_qt/_qapp_model/qactions/__init__.py
@@ -35,7 +35,6 @@ def init_qactions() -> None:
         Q_DEBUG_ACTIONS,
     )
     from napari._qt._qapp_model.qactions._file import (
-        FILE_SUBMENUS,
         Q_FILE_ACTIONS,
     )
     from napari._qt._qapp_model.qactions._help import Q_HELP_ACTIONS
@@ -86,7 +85,7 @@ def init_qactions() -> None:
 
     # register menubar submenus
     app.menus.append_menu_items(
-        chain(FILE_SUBMENUS, VIEW_SUBMENUS, DEBUG_SUBMENUS, LAYERS_SUBMENUS)
+        chain(VIEW_SUBMENUS, DEBUG_SUBMENUS, LAYERS_SUBMENUS)
     )
 
 
