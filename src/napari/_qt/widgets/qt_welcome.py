a66904234a77e90d8cd21fffe93be58ef673cad8
Lorenzo Gaifas <brisvag@gmail.com>
2025-07-15 15:38:19 +0200

    Apply suggestions from code review
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>

--- src/napari/_qt/widgets/qt_welcome.py
+++ src/napari/_qt/widgets/qt_welcome.py
@@ -62,8 +62,8 @@ class QtWelcomeWidget(QWidget):
         self._tip = QLabel()
         self._tips = cycle(sample(TIPS, len(TIPS)))
         self._tip_timer = QTimer()
+        self._tip_timer.setInterval(10000)
         self._tip_timer.timeout.connect(self._next_tip)
-        self._next_tip()
 
         # Widget setup
         self.setAutoFillBackground(True)
@@ -208,9 +208,16 @@ class QtWelcomeWidget(QWidget):
         self._update_property('drag', False)
         self.sig_dropped.emit(event)
 
+    def showEvent(self, event):
+        super().showEvent(event)
+        self._tip_timer.start()
+
+    def hideEvent(self, event):
+        super().hideEvent()
+        self._tip_timer.stop()
+
     def _next_tip(self, event=None):
         self._tip.setText(f'Did You Know?\n{next(self._tips)}!')
-        self._tip_timer.start(5000)
 
 
 class QtWidgetOverlay(QStackedWidget):
