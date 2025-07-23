209ca7e6a185852cb7532ae3ad3a8165bdb4f415
Lorenzo Gaifas <brisvag@gmail.com>
2025-07-23 11:27:35 +0200

    better comments and add astride

--- examples/grid_mode.py
+++ examples/grid_mode.py
@@ -2,7 +2,10 @@
 Grid mode
 =========
 
-Display layers in a grid using the `viewer.grid` API.
+Display layers in a grid using the `viewer.grid` API. When grid is enabled,
+layers are arranged automatically arranged in a grid of viewboxes linked to
+the main Camera and Dims. Viewer overlays such as scale_bar can also be shown
+in a gridded manner.
 
 .. tags:: visualization-basic
 """
@@ -15,11 +18,14 @@ viewer = napari.Viewer()
 layers = viewer.add_image(data.lily(), channel_axis=2)
 
 viewer.grid.enabled = True
+# a stride of 2 means that two consecutive layers are placed in each
+# viewbox instead of just one
+viewer.grid.stride = 2
 
-# show scalebar in gridded mode
 viewer.scale_bar.visible = True
-viewer.scale_bar.gridded = True
 viewer.scale_bar.box = True
+# show scalebar in each grid instead of just once
+viewer.scale_bar.gridded = True
 
 if __name__ == '__main__':
     napari.run()
