d2134114fcddb207a120aa868f9eb58b2b32c020
Grzegorz Bokota <bokota+github@gmail.com>
2025-09-08 10:29:18 +0200

    Use single settings path for all `uv tool run`

--- src/napari/utils/_appdirs.py
+++ src/napari/utils/_appdirs.py
@@ -6,9 +6,22 @@ from functools import partial
 
 import appdirs
 
+__all__ = (
+    'user_cache_dir',
+    'user_config_dir',
+    'user_data_dir',
+    'user_log_dir',
+    'user_state_dir',
+)
+
 PREFIX_PATH = os.path.realpath(sys.prefix)
 
-sha_short = f'{os.path.basename(PREFIX_PATH)}_{hashlib.sha1(PREFIX_PATH.encode()).hexdigest()}'
+UV_POSSIBLE_PATH = appdirs.user_cache_dir('uv')
+
+if PREFIX_PATH.startswith(UV_POSSIBLE_PATH):
+    environment_marker = 'napari_uv'
+else:
+    environment_marker = f'{os.path.basename(PREFIX_PATH)}_{hashlib.sha1(PREFIX_PATH.encode()).hexdigest()}'
 
 _appname = 'napari'
 _appauthor = False
@@ -22,10 +35,10 @@ user_data_dir: Callable[[], str] = partial(
     appdirs.user_data_dir, _appname, _appauthor
 )
 user_config_dir: Callable[[], str] = partial(
-    appdirs.user_config_dir, _appname, _appauthor, sha_short
+    appdirs.user_config_dir, _appname, _appauthor, environment_marker
 )
 user_cache_dir: Callable[[], str] = partial(
-    appdirs.user_cache_dir, _appname, _appauthor, sha_short
+    appdirs.user_cache_dir, _appname, _appauthor, environment_marker
 )
 user_state_dir: Callable[[], str] = partial(
     appdirs.user_state_dir, _appname, _appauthor
