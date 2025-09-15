e00ff12cac7a13b8b83c1d4dd40ad9c8142f0067
dalthviz <16781833+dalthviz@users.noreply.github.com>
2025-09-15 12:46:23 -0500

    Simplify face color control layer to widget setup

--- src/napari/_qt/layer_controls/widgets/qt_face_color.py
+++ src/napari/_qt/layer_controls/widgets/qt_face_color.py
@@ -6,7 +6,7 @@ from napari._qt.layer_controls.widgets.qt_widget_controls_base import (
     QtWidgetControlsBase,
     QtWrappedLabel,
 )
-from napari._qt.utils import qt_signals_blocked
+from napari._qt.utils import attr_to_settr
 from napari._qt.widgets.qt_color_swatch import QColorSwatchEdit
 from napari.layers import Points, Shapes
 from napari.utils.events.event_utils import connect_setattr
@@ -42,33 +42,28 @@ class QtFaceColorControl(QtWidgetControlsBase):
         tooltip: Optional[str] = None,
     ) -> None:
         super().__init__(parent, layer)
-        # Setup layer
-        self._layer.events.current_face_color.connect(
-            self._on_current_face_color_change
-        )
-        if hasattr(self._layer, '_face'):
-            # Handle Points layer case
-            self._layer._face.events.current_color.connect(
-                self._on_current_face_color_change
-            )
-
         # Setup widgets
         self.face_color_edit = QColorSwatchEdit(
             initial_color=self._layer.current_face_color,
             tooltip=tooltip,
         )
         self.face_color_label = QtWrappedLabel(trans._('face color:'))
-        self._on_current_face_color_change()
+        # self._on_current_face_color_change()
         connect_setattr(
             self.face_color_edit.color_changed,
             self._layer,
             'current_face_color',
         )
-
-    def _on_current_face_color_change(self) -> None:
-        """Receive layer model face color change event and update color swatch."""
-        with qt_signals_blocked(self.face_color_edit):
-            self.face_color_edit.setColor(self._layer.current_face_color)
+        attr_to_settr(
+            self._layer, 'current_face_color', self.face_color_edit, 'setColor'
+        )
+        if hasattr(self._layer, '_face'):
+            attr_to_settr(
+                self._layer._face,
+                'current_color',
+                self.face_color_edit,
+                'setColor',
+            )
 
     def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
         return [(self.face_color_label, self.face_color_edit)]
