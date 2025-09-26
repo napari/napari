20a32e14608c323ab8ddb625987bd15113e04905
Grzegorz Bokota <bokota+github@gmail.com>
2025-09-26 16:43:40 +0200

    detect pixi

--- src/napari/utils/_env_detection.py
+++ src/napari/utils/_env_detection.py
@@ -46,17 +46,36 @@ def chek_if_uv_venv() -> bool:
     """
     venv_path = get_venv_path()
     if venv_path is None:
-        return False
+        raise ValueError('Not in a virtual environment')
     if not (venv_path / 'pyvenv.cfg').exists():
         return False
     return 'uv =' in (venv_path / 'pyvenv.cfg').read_text()
 
 
+def check_if_pixi_env() -> bool:
+    """Check if we are in a pixi virtual environment.
+
+    Returns
+    -------
+    bool
+        True if we are in a pixi virtual environment, False otherwise.
+    """
+    env_path = get_conda_path()
+    if env_path is None:
+        raise ValueError('Not in a conda environment')
+    if not (env_path / 'conda-meta').exists():
+        raise ValueError('Not in a conda environment')
+    return (env_path / 'conda-meta' / 'pixi_env_prefix').exists()
+
+
 def detect_environment() -> Environment:
-    if get_venv_path():
+    """Detect the current environment."""
+    if get_venv_path() is not None:
         if chek_if_uv_venv():
             return Environment.uv
         return Environment.pip
     if get_conda_path() is not None:
+        if check_if_pixi_env():
+            return Environment.pixi
         return Environment.conda
     return Environment.pip
