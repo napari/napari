49c19475fe1ec650b4c4d315cc141e615ecf8dc9
Daniel Althviz Moré <16781833+dalthviz@users.noreply.github.com>
2025-10-07 08:41:22 -0500

    Add a new `attr_to_settr` utility function and simplify layer control widgets layer to widget setup (#8274)
    # References and relevant issues
    
    Closes #8155
    
    # Description
    
    - [x] Add `attr_to_settr` utility function
    - [x] Update layer controls widgets
      - [x] QtGammaSliderControl
      - [x] QtFaceColorControl
      - [x] QtBorderColorControl
      - [x] QtEdgeColorControl
      - [x] QtLengthSpinBoxControl
      - [x] QtWidthSpinBoxControl
      - [x] QtLabelControl
      - [x] QtPreserveLabelsCheckBoxControl
    - [x] QtDepictionControl (revert to preserver indirection to handle
    `.plane` attribute)
      - [x] QtBrushSizeSliderControl
      - [x] QtContiguousCheckBoxControl
      - [x] QtContourSpinBoxControl
      - [x] QtDisplaySelectedLabelCheckBoxControl
      - [x] QtPreserveLabelsCheckBoxControl
      - [x] Double check for more widget controls that could be simplified
        - [x] QtOpacityBlendingControls
        - [x] QtImageRenderControl
        - [x] QtEdgeWidthSliderControl
    
    # Notes
    
    * I think I covered with the changes here all possible widgets that
    could use the new `attr_to_settr` utility function. I will try to double
    check if I'm missing widgets but if while checking this someone founds
    one that I missed let me know!
    * This also changes a couple of widget to use the `connect_settr`
    utility function or clean some unused methods (like QtDepictionControl,
    QtBrushSizeSliderControl and QtEdgeWidthSliderControl widgets)
    
    ---------
    
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>
    Co-authored-by: Lorenzo Gaifas <brisvag@gmail.com>

--- src/napari/_qt/layer_controls/widgets/_tracks/qt_hide_completed_tracks_checkbox.py
+++ src/napari/_qt/layer_controls/widgets/_tracks/qt_hide_completed_tracks_checkbox.py
@@ -7,7 +7,7 @@ from napari._qt.layer_controls.widgets.qt_widget_controls_base import (
     QtWidgetControlsBase,
     QtWrappedLabel,
 )
-from napari._qt.utils import checked_to_bool, qt_signals_blocked
+from napari._qt.utils import attr_to_settr, checked_to_bool
 from napari.layers import Tracks
 from napari.utils.events.event_utils import connect_setattr
 from napari.utils.translations import trans
@@ -39,6 +39,14 @@ class QtHideCompletedTracksCheckBoxControl(QtWidgetControlsBase):
 
         # Setup widgets
         self.hide_completed_tracks_checkbox = QCheckBox()
+        self._callbacks.append(
+            attr_to_settr(
+                self._layer,
+                'hide_completed_tracks',
+                self.hide_completed_tracks_checkbox,
+                'setChecked',
+            )
+        )
         connect_setattr(
             self.hide_completed_tracks_checkbox.stateChanged,
             layer,
@@ -49,13 +57,6 @@ class QtHideCompletedTracksCheckBoxControl(QtWidgetControlsBase):
         self.hide_completed_tracks_checkbox_label = QtWrappedLabel(
             trans._('hide completed:')
         )
-
-    def _on_hide_completed_tracks_change(self) -> None:
-        """Receive layer model hide_completed_tracks event and update the checkbox."""
-        with qt_signals_blocked(self.hide_completed_tracks_checkbox):
-            self.hide_completed_tracks_checkbox.setChecked(
-                self._layer.hide_completed_tracks
-            )
 
     def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
         return [
