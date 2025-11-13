230abab8de7c0a7bee85768a31a19ea949a99935
Lorenzo Gaifas <brisvag@gmail.com>
2025-11-13 19:15:22 +0100

    properly account for all system fonts, cross-platform (in theory)

--- src/napari/__init__.py
+++ src/napari/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tempfile
 from importlib import resources
 
@@ -15,17 +16,33 @@ except ImportError:
 os.environ.setdefault('SPARSE_AUTO_DENSIFY', '1')
 limit_numpy1x_threads_on_macos_arm()
 
-
 # TODO: probably move this elsewhere
 font_dir = resources.files(__package__).joinpath('resources', 'fonts')
 conf_dir = tempfile.mkdtemp()
-conf_path = os.path.join(
-    conf_dir, '10-fonts.conf'
-)  # prefix 10 is crucial so we don't override
+conf_path = os.path.join(conf_dir, 'fonts.conf')
+
+system_fonts = {}
+if sys.platform.startswith('linux'):
+    system_fonts = """
+    <include ignore_missing="yes">/etc/fonts/fonts.conf</include>
+    <include ignore_missing="yes" prefix="xdg">fonts.conf</include>
+    <include ignore_missing="yes" prefix="xdg">conf.d</include>
+    """
+elif sys.platform == 'darwin':
+    system_fonts = """
+    <include ignore_missing="yes">/System/Library/Fonts</include>
+    <include ignore_missing="yes">/Library/Fonts</include>
+    <include ignore_missing="yes">~/Library/Fonts</include>
+    """
+elif sys.platform == 'win32':
+    system_fonts = """
+    <dir>C:\\Windows\\Fonts</dir>
+    """
 conf_contents = f"""<?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
 <fontconfig>
-  <dir>{font_dir}</dir>
+    {system_fonts}
+    <dir>{font_dir}</dir>
 </fontconfig>
 """
 with open(conf_path, 'w') as f:
