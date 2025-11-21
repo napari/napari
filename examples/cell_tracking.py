fcd7d964d41ce8454901049b49490565f65e1ab2
Olusesan <olusesan.ajina@gmail.com>
2025-11-21 11:48:10 -0800

    remove bloat and update project.toml

--- examples/cell_tracking.py
+++ examples/cell_tracking.py
@@ -28,25 +28,7 @@ import napari
 tmp_dir = Path(pooch.os_cache('napari-cell-tracking-example'))
 tmp_dir.mkdir(parents=True, exist_ok=True)
 
-#Download ground truth data
-gt_url = 'https://zenodo.org/records/16884151/files/TRA.zip?download=1'
-gt_path = pooch.retrieve(url=gt_url,
-                         fname="TRA.zip",
-                         known_hash=None,
-                         path=pooch.os_cache("cell_tracking")
-                         )
-# Make ground truth sub directory
-gt_folder = tmp_dir / "TRA"
-
-# Extract compressed ground truth data
-if not gt_folder.exists():
-    print(f"Extracting {gt_path} to {gt_folder}")
-    with zipfile.ZipFile(gt_path, 'r') as zip_ref:
-        zip_ref.extractall(gt_folder)
-else:
-    print(f"Using cached ground truth at {gt_folder}")
-
-# Extract data for trackastra based silver truth
+# Extract silver truth data for trackastra
 st_url = "https://zenodo.org/records/15852284/files/masks_pred.npz?download=1"
 st_path = pooch.retrieve(
     url=st_url,
@@ -67,7 +49,6 @@ tif_path = pooch.retrieve(
 # Load the downloaded masks
 masks_npz = np.load(st_path)
 masks = masks_npz['masks']
-print(f"mask shape is {masks.shape}")
 
 # Make raw tif sub directory
 tif_folder = tmp_dir/ "01"
@@ -77,35 +58,11 @@ with zipfile.ZipFile(tif_path, 'r') as zip:
     zip.extractall(path=tif_folder)
 
 # Sort through images and stack them
-imgs = [f for f in sorted((tif_folder).rglob ("*.tif"))
-        if not f.name.startswith("._")]
-
-valid_files = []
-
-for f in imgs:
-    try:
-        with tifffile.TiffFile(f) as tif:
-            tif.pages[0]
-        valid_files.append(f)
-    except Exception as e:
-        print(f"Skipping {f.name} — not a valid TIFF ({e})")
-
+imgs = sorted(tif_folder.rglob("*.tif"))
 
 images = np.stack([
-        tifffile.imread(fn) for fn in sorted(imgs)
+        tifffile.imread(fn) for fn in imgs
         ])
-print(f"image shape is {images.shape}")
-
-# Make sure mask shape and image shape is the same
-if images.shape[0] != masks.shape[0]:
-    raise ValueError("Number of frames in imgs and masks differ")
-
-if len(masks_npz.files) > 0:
-    first_key = masks_npz.files[0]
-    masks = masks_npz['masks']
-    print(f"Loaded masks from key '{first_key}' with shape: {masks.shape}")
-else:
-    raise ValueError("No arrays found in npz file")
 
 # Initiate trackastra model
 model = Trackastra.from_pretrained("general_2d", device='cpu')
@@ -116,18 +73,10 @@ tracks_df, tracked_masks = graph_to_ctc(graph=graph, masks_original=masks)
 
 napari_tracks, napari_graph = ctc_to_napari_tracks(segmentation=tracked_masks, man_track=tracks_df)
 
-# Quick check to see if everything looks ok
-print(f"DataFrame columns: {tracks_df.columns.tolist()}")
-print(f"DataFrame shape: {tracks_df.shape}")
-print(f"First few rows:\n{tracks_df.head()}")
-
 # Add Napari viewer
 viewer = napari.Viewer()
-
-# I am not sure if we should also open ground truth data here
-#viewer.open(str(gt_folder), plugin='napari-ctc')
-
 viewer.add_labels(masks, name='Predicted Masks')
 viewer.add_tracks(napari_tracks, graph=napari_graph, name="Tracks")
 
-napari.run()
+if __name__ == "__main__":
+    napari.run()
