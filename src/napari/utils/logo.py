f8394ddd72c898c8ed4d202dda8dd760d916285c
Lorenzo Gaifas <brisvag@gmail.com>
2025-12-01 17:55:26 +0100

    make seasonal logo appear depending on date

--- src/napari/utils/logo.py
+++ src/napari/utils/logo.py
@@ -1,4 +1,5 @@
 import re
+from datetime import date
 from importlib import resources
 from pathlib import Path
 
@@ -12,12 +13,44 @@ def available_logos():
     return variants
 
 
-def get_logo_path(logo, theme):
+def _get_seasonal_logo(today=None, theme='dark'):
+    today = today or date.today()
+
+    ranges = {
+        'halloween': ((10, 25), (11, 2)),
+        'christmas': ((12, 1), (1, 6)),
+        'maythefourth': ((5, 1), (5, 10)),  # let's give it some leeway :P
+    }
+
+    theme_variants = {'maythefourth': {'dark': 'sith', 'light': 'jedi'}}
+
+    for name, ((m1, d1), (m2, d2)) in ranges.items():  # noqa: B007
+        start = date(today.year, m1, d1)
+        end = date(today.year, m2, d2)
+
+        if end < start:
+            # rolls over to the next year
+            if today >= start or today <= date(today.year, m2, d2):
+                break
+        else:
+            if start <= today <= end:
+                break
+
+    else:
+        name = 'gradient'
+
+    if name in theme_variants:
+        name = theme_variants[name].get(theme)
+
+    return name
+
+
+def get_logo_path(logo, theme, today=None):
     logo_dir = Path(resources.files('napari').joinpath('resources', 'logos'))
     if theme not in ('dark', 'light'):
         theme = 'dark'
 
     if logo == 'auto':
-        return logo_dir / f'gradient-plain-{theme}.svg'
+        logo = _get_seasonal_logo(today=today, theme=theme)
 
     return logo_dir / f'{logo}-plain-{theme}.svg'
