a02cc57ecd380549d5d93e6be43f56f4f697b5b5
Lorenzo Gaifas <brisvag@gmail.com>
2025-12-16 16:07:45 +0100

    update markers to use new system (and default to instanced)

--- src/napari/_vispy/visuals/markers.py
+++ src/napari/_vispy/visuals/markers.py
@@ -1,29 +1,26 @@
-from typing import ClassVar
+import logging
 
+from vispy import use
 from vispy.scene.visuals import Markers as BaseMarkers
 
-clamp_shader = """
-float clamped_size = clamp($v_size, $canvas_size_min, $canvas_size_max);
-float clamped_ratio = clamped_size / $v_size;
-$v_size = clamped_size;
-v_edgewidth = v_edgewidth * clamped_ratio;
-gl_PointSize = $v_size + 4. * (v_edgewidth + 1.5 * u_antialias);
-"""
+logger = logging.getLogger(__name__)
 
-old_vshader = BaseMarkers._shaders['vertex']
-new_vshader = old_vshader[:-2] + clamp_shader + '\n}'  # very ugly...
+try:
+    use(gl='gl+')
+except RuntimeError:
+    logger.warning(
+        'Could not use gl+ for instanced rendering of points.'
+        'Falling back to GL point rendering.'
+    )
+
+    rendering_method = 'points'
+else:
+    rendering_method = 'instanced'
 
 
 class Markers(BaseMarkers):
-    _shaders: ClassVar[dict[str, str]] = {
-        'vertex': new_vshader,
-        'fragment': BaseMarkers._shaders['fragment'],
-    }
-
-    def __init__(self, *args, **kwargs) -> None:
-        self._canvas_size_limits = 0, 10000
-        super().__init__(*args, **kwargs)
-        self.canvas_size_limits = 0, 10000
+    def __init__(self, *args, method=rendering_method, **kwargs) -> None:
+        super().__init__(*args, method=method, **kwargs)
 
     def _compute_bounds(self, axis, view):
         # needed for entering 3D rendering mode when a points
@@ -37,13 +34,3 @@ class Markers(BaseMarkers):
             return (pos[:, axis].min(), pos[:, axis].max())
 
         return (0, 0)
-
-    @property
-    def canvas_size_limits(self):
-        return self._canvas_size_limits
-
-    @canvas_size_limits.setter
-    def canvas_size_limits(self, value):
-        self._canvas_size_limits = value
-        self.shared_program.vert['canvas_size_min'] = value[0]
-        self.shared_program.vert['canvas_size_max'] = value[1]
