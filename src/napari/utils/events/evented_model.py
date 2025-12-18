cf12ace7d2b4d853147db325bf5dcbe01c38b7c5
Lorenzo Gaifas <brisvag@gmail.com>
2025-12-18 15:39:52 +0100

    update evented model

--- src/napari/utils/events/evented_model.py
+++ src/napari/utils/events/evented_model.py
@@ -11,6 +11,7 @@ from pydantic import (
 )
 from pydantic._internal._model_construction import ModelMetaclass
 
+from napari._pydantic_util import get_inner_type, get_outer_type
 from napari.utils.events.event import EmitterGroup, Event
 from napari.utils.misc import pick_equality_operator
 from napari.utils.translations import trans
@@ -42,18 +43,19 @@ class EventedMetaclass(ModelMetaclass):
         cls = super().__new__(mcs, name, bases, namespace, **kwargs)
         cls.__eq_operators__ = {}
         for n, f in cls.model_fields.items():
-            cls.__eq_operators__[n] = pick_equality_operator(f.type_)
+            field_type = get_outer_type(f.annotation)
+            cls.__eq_operators__[n] = pick_equality_operator(field_type)
             # If a field type has a _json_encode method, add it to the json
             # encoders for this model.
             # NOTE: a _json_encode field must return an object that can be
             # passed to json.dumps ... but it needn't return a string.
-            if hasattr(f.type_, '_json_encode'):
-                encoder = f.type_._json_encode
-                cls.model_config.json_encoders[f.type_] = encoder
+            if hasattr(field_type, '_json_encode'):
+                encoder = field_type._json_encode
+                cls.model_config.json_encoders[field_type] = encoder
                 # also add it to the base config
                 # required for pydantic>=1.8.0 due to:
                 # https://github.com/samuelcolvin/pydantic/pull/2064
-                EventedModel.model_config.json_encoders[f.type_] = encoder
+                EventedModel.model_config.json_encoders[field_type] = encoder
         # check for properties defined on the class, so we can allow them
         # in EventedModel.__setattr__ and create events
         cls.__properties__ = {}
@@ -189,18 +191,12 @@ class EventedModel(BaseModel, metaclass=EventedMetaclass):
         # simply by checking if the value is an instance of the type). If
         # False, RuntimeError will be raised on model declaration
         arbitrary_types_allowed = True
-        # whether to perform validation on assignment to attributes
         validate_assignment = True
-        # whether to treat any underscore non-class var attrs as private
-        # https://pydantic-docs.helpmanual.io/usage/models/#private-model-attributes
-        underscore_attrs_are_private = True
-        # whether to validate field defaults (default: False)
-        validate_all = True
+        validate_defaults = True
         # https://pydantic-docs.helpmanual.io/usage/exporting_models/#modeljson
         # NOTE: json_encoders are also added EventedMetaclass.__new__ if the
         # field declares a _json_encode method.
         json_encoders = _BASE_JSON_ENCODERS
-        # extra = Extra.forbid
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
@@ -210,7 +206,7 @@ class EventedModel(BaseModel, metaclass=EventedMetaclass):
         field_events = [
             name
             for name, field in self.model_fields.items()
-            if field.field_info.allow_mutation
+            if not field.frozen
         ]
 
         self._events.add(
@@ -362,8 +358,8 @@ class EventedModel(BaseModel, metaclass=EventedMetaclass):
             if isinstance(value, EventedModel):
                 getattr(self, name).reset()
             elif (
-                self.model_config.allow_mutation
-                and self.model_fields[name].field_info.allow_mutation
+                not self.model_config.frozen
+                and not self.model_fields[name].frozen
             ):
                 setattr(self, name, value)
 
@@ -453,8 +449,9 @@ def get_defaults(obj: BaseModel):
     dflt = {}
     for k, v in obj.model_fields.items():
         d = v.get_default()
-        if d is None and isinstance(v.type_, ModelMetaclass):
-            d = get_defaults(v.type_)
+        field_type = get_inner_type(v.annotation)
+        if d is None and isinstance(field_type, ModelMetaclass):
+            d = get_defaults(field_type)
         dflt[k] = d
     return dflt
 
