3432decd7e08414aa0c23231953fc47ad462ea0e
Grzegorz Bokota <bokota+github@gmail.com>
2026-01-08 11:33:41 +0100

    Add comments with explanation

--- toxfile.py
+++ toxfile.py
@@ -1,3 +1,14 @@
+"""
+This local tox plugin is to workaround bug in tox's handling of
+nested optional dependencies in pyproject.toml files. https://github.com/tox-dev/tox/issues/3561
+
+It uses the assumption that we could modify the `arguments` list in place
+to expand any `napari[extra]` dependencies into their actual requirements,
+and it will be passed to the real installer later.
+
+So this should be treated as a temporary workaround until tox fixes the issue.
+"""
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
@@ -17,7 +28,14 @@ except ImportError:
 def explode_nested_deps(
     requirements: list[Requirement],
     optional_deps: dict[str, list[str]],
-):
+) -> None:
+    """Explode any napari[extra] dependencies into their actual requirements.
+
+    For generalization, the package name should also be parameterized, but
+    since this is only a temporary workaround for napari, we hardcode it here.
+
+    It requires modifying the `requirements` list in place.
+    """
     napari_deps = [x for x in requirements if x.name == 'napari']
     if not napari_deps:
         return
@@ -38,6 +56,7 @@ def explode_nested_deps(
 def tox_on_install(
     tox_env: ToxEnv, arguments: Any, section: str, of_type: str
 ):
+    """Hook into tox install to expand nested optional dependencies."""
     if of_type == 'dependency-groups':
         pyproject_file = tox_env.core['tox_root'] / 'pyproject.toml'
         pyproject = load_toml(pyproject_file.open('rb'))
