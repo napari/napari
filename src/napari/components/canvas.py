60f023933a6e806b94fe8c442bc6654565ffd850
Lorenzo Gaifas <brisvag@gmail.com>
2026-02-02 16:59:48 +0100

    oops

---
+++ src/napari/components/canvas.py
@@ -0,0 +1,108 @@
+import numpy as np
+from pydantic import Field, PrivateAttr
+
+from napari.components.grid import GridCanvas
+from napari.components.overlays import (
+    BrushCircleOverlay,
+    Overlay,
+    ScaleBarOverlay,
+    TextOverlay,
+    WelcomeOverlay,
+    ZoomOverlay,
+)
+from napari.settings import get_settings
+from napari.utils.color import ColorValue
+from napari.utils.events import EventedDict, EventedModel
+
+DEFAULT_CANVAS_OVERLAYS = {
+    'welcome': WelcomeOverlay,
+    'scale_bar': ScaleBarOverlay,
+    'text': TextOverlay,
+    'brush_circle': BrushCircleOverlay,
+    'zoom': ZoomOverlay,
+}
+
+
+class Canvas(EventedModel):
+    """
+    Canvas evented model.
+
+    Controls canvas-related attributes, such as grid mode and canvas overlays.
+
+    Attributes
+    ----------
+    background_color_override :
+        ...
+    grid :
+        ...
+    _canvas_size: Tuple[int, int]
+        The canvas size following the Numpy convention of height x width
+    """
+
+    background_color_override: ColorValue | None = None
+    grid: GridCanvas = Field(default_factory=GridCanvas, allow_mutation=False)
+    size: tuple[int, int] = (800, 600)
+
+    _overlays: EventedDict[str, Overlay] = PrivateAttr(
+        default_factory=EventedDict
+    )
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._update_viewer_grid()
+
+        settings = get_settings()
+        settings.application.events.grid_stride.connect(
+            self._update_viewer_grid
+        )
+        settings.application.events.grid_width.connect(
+            self._update_viewer_grid
+        )
+        settings.application.events.grid_height.connect(
+            self._update_viewer_grid
+        )
+        settings.application.events.grid_spacing.connect(
+            self._update_viewer_grid
+        )
+
+        self._overlays.update(
+            {k: v() for k, v in DEFAULT_CANVAS_OVERLAYS.items()}
+        )
+
+    @property
+    def _zoom_box(self):
+        return self._overlays['zoom']
+
+    @property
+    def _brush_circle_overlay(self):
+        return self._overlays['brush_circle']
+
+    def _update_viewer_grid(self):
+        """Keep viewer grid settings up to date with settings values."""
+
+        settings = get_settings()
+
+        self.grid.stride = settings.application.grid_stride
+        self.grid.shape = (
+            settings.application.grid_height,
+            settings.application.grid_width,
+        )
+        self.grid.spacing = settings.application.grid_spacing
+
+    def _get_viewbox_size(self, n_layers):
+        """Get the size of a single viewbox (whether grid is enabled or not).
+
+        If grid.border_width > 0, that's accounted for too.
+        """
+        viewbox_size = np.array(self.size)
+        if self.grid.enabled:
+            grid_shape = np.array(self.grid.actual_shape(n_layers))
+            spacing_pixels = self.grid._compute_canvas_spacing(
+                self.size, n_layers
+            )
+            # Now calculate actual available space
+            total_gap_space = spacing_pixels * (grid_shape - 1)
+            available_space = self.size - total_gap_space
+            viewbox_size = available_space / grid_shape
+        return viewbox_size
