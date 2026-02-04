2231de81c5a98322ce5e2a065811c8b530c7efeb
MargotCh <margot.chazotte@gmail.com>
2026-02-04 14:37:51 +0100

    chore: Complete docstring of ColorBarOverlay

--- src/napari/components/overlays/colorbar.py
+++ src/napari/components/overlays/colorbar.py
@@ -11,11 +11,24 @@ class ColorBarOverlay(CanvasOverlay):
     position : CanvasPosition
         The position of the overlay in the canvas.
     visible : bool
-        If the overlay is visible or not.
+        If the overlay is visible or not. Inherited from Overlay.
     opacity : float
-        The opacity of the overlay. 0 is fully transparent.
+        The opacity of the overlay. 0 is fully transparent. Inherited from Overlay.
     order : int
-        The rendering order of the overlay: lower numbers get rendered first.
+        The rendering order of the overlay: lower numbers get rendered first. Inherited from Overlay.
+    color : ColorValue | None
+        The color of the outline and ticks of the colorbar.
+    size : tuple[float, float]
+        The size of the colorbar in pixels (width, height).
+    tick_length : float
+        The length of the ticks in pixels.
+    font_size : float
+        The font size of the tick labels.
+    blending : str
+        One of a list of preset blending modes that determines how RGB and
+        alpha values of the colorbar visual get mixed with other visuals. Defaults to 'translucent_no_depth'. Inherited from CanvasOverlay.
+    gridded : bool
+        The overlay will be duplicated across all grid cells in gridded mode. Inherited from CanvasOverlay.
     """
 
     color: ColorValue | None = None
