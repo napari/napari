cedf2bd63144e69049d469f5a59b91f3ae56ef8e
Tim Monko <timmonko@gmail.com>
2026-02-10 16:32:55 -0600

    add test in same pattern as SampleWidget

--- src/napari/_qt/widgets/_tests/test_theme_sample.py
+++ src/napari/_qt/widgets/_tests/test_theme_sample.py
@@ -1,9 +1,17 @@
-from napari._qt.widgets.qt_theme_sample import SampleWidget
+from napari._qt.widgets.qt_theme_sample import SampleWidget, ThemeColorDisplay
 
 
 def test_theme_sample(qtbot):
     """Just a smoke test to make sure that the theme sample can be created."""
     w = SampleWidget()
+    qtbot.addWidget(w)
+    w.show()
+    assert w.isVisible()
+
+
+def test_theme_color_display(qtbot):
+    """Just a smoke test to make sure that the theme color display can be created."""
+    w = ThemeColorDisplay()
     qtbot.addWidget(w)
     w.show()
     assert w.isVisible()
