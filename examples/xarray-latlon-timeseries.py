8e30e70a8b6e5e92b1a214e26e935e0134eea4e2
Grzegorz Bokota <bokota+github@gmail.com>
2026-02-19 22:27:03 +0100

    set units in timeseries example

--- examples/xarray-latlon-timeseries.py
+++ examples/xarray-latlon-timeseries.py
@@ -70,7 +70,7 @@ air_layer = viewer.add_image(
         contrast_limits=(-23 + 273, 32 + 273),  # data are in degrees Kelvin
         )
 
-viewer.layers.units = ('h', 'degrees', 'degrees')
+viewer.layers.units = ('hour', 'degrees', 'degrees')
 # set a time that overlaps both datasets
 viewer.dims.set_point(0, np.datetime64('2013-03-10T18:00:00.000000000'))
 
