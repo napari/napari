6ac53ecfda961b96dd2f75d295bb1dc04c6d8f35
MargotCh <margot.chazotte@gmail.com>
2026-02-25 18:25:01 +0100

    add url to flare bucket and rename crop to image for clarity

--- examples/heart_crop_display.py
+++ examples/heart_crop_display.py
@@ -11,9 +11,9 @@ It adds a segmentation mask and points with features.
 Thanks to Krešimir Beštak for providing the image, segmentation mask and cell data for this example.
 The full dataset is available on Synapse: https://www.synapse.org/Synapse:syn51449054
 
-More information about the dataset can be found in *Wünnemann, F., Sicklinger, F., Bestak, K. et al.
+More information about the dataset can be found in Wünnemann, F., Sicklinger, F., Bestak, K. et al.
 Spatial multiomics of acute myocardial infarction reveals immune cell infiltration through the endocardium.
-Nat Cardiovasc Res 4, 1345-1362 (2025). https://doi.org/10.1038/s44161-025-00717-y*
+Nat Cardiovasc Res 4, 1345-1362 (2025). https://doi.org/10.1038/s44161-025-00717-y
 
 .. tags:: visualization-basic
 """
@@ -29,11 +29,11 @@ import napari
 
 # download the data
 download = pooch.DOIDownloader(progressbar=True)
-doi = '10.5281/zenodo.13380203' #TODO change link to point to dataset on drive
+url = 'https://data.napari.dev/heart_example_data'
 tmp_dir = pooch.os_cache('napari-heart-example')
 os.makedirs(tmp_dir, exist_ok=True)
 data_files = {
-    'tiff_image': 'image.tif',
+    #'tiff_image': 'image.tif', # uncomment if you want to load the image as tiff instead of zarr
     'zarr_image': 'image.zarr',
     'segmentation': 'segmentation.tif',
     'cell_data': 'cell_data.csv',
@@ -42,16 +42,16 @@ data_files = {
 data_to_path = {}
 print(f'downloading data into {tmp_dir}')
 for id_, file_name in data_files.items():
-    res = pooch.retrieve(f'doi:{doi}/{file_name}', known_hash=None,  progressbar=True)
+    res = pooch.retrieve(f'{url}/{file_name}', known_hash=None,  progressbar=True)
     data_to_path[id_] = res
 
 
 # read in image, segmentation mask and cell data
 # image can be read in either as tiff or as zarr
-#crop = tifffile.imread(data_to_path['tiff_image'])
-crop = dask.array.from_zarr(data_to_path['zarr_image'])
-crop_mask = tifffile.imread(data_to_path['segmentation'])
-crop_cells = pd.read_csv(data_to_path['cell_data'])
+#image = tifffile.imread(data_to_path['tiff_image'])
+image = dask.array.from_zarr(data_to_path['zarr_image'])
+mask = tifffile.imread(data_to_path['segmentation'])
+cells = pd.read_csv(data_to_path['cell_data'])
 
 # read in metadata containing contrast limits, colormap and channel index for each channel that should be included
 channel_color_metadata = pd.read_csv(data_to_path['channel_color_metadata'])
@@ -72,15 +72,15 @@ color_cycle = {
     }
 
 # subset cell features to those that should be added to the points layer
-crop_feature = { 'label': crop_cells['label'],
-           'cell_size': crop_cells['cell_size'],
-           'cell_type': crop_cells['final_cell_type']}
+feature = { 'label': cells['label'],
+           'cell_size': cells['cell_size'],
+           'cell_type': cells['final_cell_type']}
 
 # create napari viewer and add image channel by channel with contrast limits and colormap according to channel_color_metadata
 viewer = napari.Viewer()
 for idx, name in zip(channel_color_metadata['channel_index'], channel_color_metadata['name'], strict = True):
     viewer.add_image(
-        crop[idx],
+        image[idx],
         name=name,
         rgb=False,
         contrast_limits=[channel_color_metadata['contrast_min'][channel_color_metadata['channel_index'] == idx].values[0],
@@ -90,18 +90,18 @@ for idx, name in zip(channel_color_metadata['channel_index'], channel_color_meta
     )
 
 # add segmetation mask, optionally add features which can be displayed by plugins
-viewer.add_labels(crop_mask,
+viewer.add_labels(mask,
                   name='Segmentation mask',
-                  #features=crop_feature
+                  #features=feature
                   )
 
 # add points with features, colored by cell type
 viewer.add_points(
-    crop_cells[['Y_centroid', 'X_centroid']].values,
+    cells[['Y_centroid', 'X_centroid']].values,
     name='cell_types',
-    features=crop_feature,
+    features=feature,
     face_color='cell_type',
-    size=30,
+    size=25,
     face_color_cycle=color_cycle
 )
 
