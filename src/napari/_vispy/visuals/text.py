eb5f70fa4aa05a5f52bcb82457db306702be45fd
Lorenzo Gaifas <brisvag@gmail.com>
2026-03-12 13:02:36 +0100

    dpi ratio fixed

--- src/napari/_vispy/visuals/text.py
+++ src/napari/_vispy/visuals/text.py
@@ -41,27 +41,13 @@ class Text(BaseText):
 
     def get_width_height(self) -> tuple[float, float]:
         width, height = get_text_width_height(self)
-        return width * self._vispy_dpi_ratio, height * self._vispy_dpi_ratio
+        return width, height
 
     @property
     def font_size(self) -> float:
-        return self._font_size * self.dpi_ratio
+        return self._font_size
 
     @font_size.setter
     def font_size(self, size: float) -> None:
-        adjusted_font_size = size / self.dpi_ratio
-        self._font_size = max(0.0, adjusted_font_size)
+        self._font_size = max(0.0, size)
         self.update()
-
-    @property
-    def _vispy_dpi_ratio(self) -> float:
-        # while 72 is considered the base dpi in vispy, most software assumes
-        # 96 as reference dpi, resulting in mismatch between gui and canvas
-        # when font sizes are the same. We need to account for this when
-        # we calculate things externally from vispy and
-        return 96 / 72
-
-    @property
-    def dpi_ratio(self) -> float:
-        dpi = self.transforms.dpi or 96
-        return dpi / 96
