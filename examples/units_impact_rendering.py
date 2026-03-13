ab809b3cfac6494cce77f13bfddd266e1ca38f8a
Tim Monko <timmonko@gmail.com>
2026-03-13 10:05:17 -0500

    add example description

--- examples/units_impact_rendering.py
+++ examples/units_impact_rendering.py
@@ -1,3 +1,16 @@
+"""
+Layer Units Impact Rendering
+============================
+
+This example demonstrates how units can impact rendering in napari.
+Two layers are added with different scales, however by specifying the units,
+napari is able to render them in the same physical space by the unit-aware
+library Pint. The first layer is in nanometers and the second layer is
+in micrometers, but they are rendered in the same physical space because 1 μm = 1000 nm.
+
+.. tags:: visualization-advanced, layers
+"""
+
 import skimage
 
 import napari
@@ -7,9 +20,9 @@ data = skimage.data.cells3d()
 ch1 = data[:, 0]
 ch2 = data[:, 1]
 
-
 viewer = napari.Viewer()
 viewer.add_image(ch1, units=('nm', 'nm', 'nm'), name='ch1', scale=(210, 70, 70), colormap="magenta")
 viewer.add_image(ch2, units=('μm', 'μm', 'μm'), name='ch2', scale=(0.210, 0.07, 0.07), colormap="green", blending='additive')
 
-napari.run()
+if __name__ == '__main__':
+    napari.run()
