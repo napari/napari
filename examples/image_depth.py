b42f22d6534f5bda02cc73a5c3428798e844fd09
Draga Doncila Pop <17995243+DragaDoncila@users.noreply.github.com>
2026-03-18 18:49:25 +1100

    Fix `image_depth.py` example by setting `blending` to `translucent` (#8764)
    # References and relevant issues
    Closes #8712
    
    # Description
    Change the `blending` mode of the added images in the example to
    `translucent` to show the points rendered "in between"
    the boxes.
    
    <img width="1156" height="848" alt="image"
    src="https://github.com/user-attachments/assets/1add4f1a-823c-419f-a174-e7ab4638cc3f"
    />
    
    ---------
    
    Co-authored-by: Draga Doncila <ddon0001@student.monash.edu>
    Co-authored-by: Tim Monko <timmonko@gmail.com>
    Co-authored-by: Lorenzo Gaifas <brisvag@gmail.com>

--- examples/image_depth.py
+++ examples/image_depth.py
@@ -2,6 +2,13 @@
 Image depth
 ===========
 
+Display a 3D image and 3D points layer to show how napari handles depth.
+
+The default blending for images is `translucent_no_depth`, which ignores depth and
+always draws the layer on top of lower layers. Switching to `translucent` allows
+objects in this layer to disappear behind objects in other layers depending
+on the distance from the camera.
+
 .. tags:: visualization-basic
 """
 
@@ -12,8 +19,8 @@ import napari
 im_data = np.zeros((50, 50, 50))
 im_data[30:40, 25:35, 25:35] = 1
 viewer = napari.Viewer()
-layer = viewer.add_image(im_data, colormap='magenta', rendering='iso')
-viewer.add_image(im_data, colormap='green', rendering='iso', translate=(30, 0, 0))
+layer = viewer.add_image(im_data, colormap='magenta', rendering='iso', blending='translucent')
+viewer.add_image(im_data, colormap='green', rendering='iso', blending='translucent', translate=(30, 0, 0))
 
 points_data = [
     [50, 30, 30],
@@ -23,7 +30,7 @@ points_data = [
 viewer.add_points(points_data, size=4)
 
 viewer.dims.ndisplay = 3
-viewer.camera.angles = (0, -10, 10)
+viewer.camera.angles = (0, -30, 10)
 viewer.fit_to_view()
 
 if __name__ == '__main__':
