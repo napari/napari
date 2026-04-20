62f06b4e9da23cb11627d10ed2b4479b85641c9b
Tim Monko <timmonko@gmail.com>
2026-04-19 22:47:43 -0500

    add tests for the toggling

--- src/napari/_qt/layer_controls/_tests/test_qt_surface_layer.py
+++ src/napari/_qt/layer_controls/_tests/test_qt_surface_layer.py
@@ -31,3 +31,43 @@ def test_shading_combobox(qtbot):
             qtctrl._shading_combobox_control.shading_combobox.currentText()
             == display
         )
+
+
+def test_intensity_controls_disabled_with_vertex_colors(qtbot):
+    vertex_colors = np.full((len(data), 3), 0.5)
+    layer = Surface(_SURFACE, vertex_colors=vertex_colors)
+    qtctrl = QtSurfaceControls(layer)
+    qtbot.addWidget(qtctrl)
+
+    assert all(
+        not widget.isEnabled()
+        for ctrl in qtctrl._intensity_controls
+        for _, widget in ctrl.get_widget_controls()
+    )
+    assert qtctrl._shading_combobox_control.shading_combobox.isEnabled()
+
+
+def test_intensity_controls_toggle_with_vertex_colors(qtbot):
+    layer = Surface(_SURFACE)
+    qtctrl = QtSurfaceControls(layer)
+    qtbot.addWidget(qtctrl)
+
+    assert all(
+        widget.isEnabled()
+        for ctrl in qtctrl._intensity_controls
+        for _, widget in ctrl.get_widget_controls()
+    )
+
+    layer.vertex_colors = np.full((len(data), 3), 0.5)
+    assert all(
+        not widget.isEnabled()
+        for ctrl in qtctrl._intensity_controls
+        for _, widget in ctrl.get_widget_controls()
+    )
+
+    layer.vertex_colors = None
+    assert all(
+        widget.isEnabled()
+        for ctrl in qtctrl._intensity_controls
+        for _, widget in ctrl.get_widget_controls()
+    )
