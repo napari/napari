0f8c549a44635d1da5b50a95b5a34fca346c6713
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2026-04-29 13:43:09 -0400

    Revert "Fix test?" -- maybe unneeded
    This reverts commit 85038f9d2b49fa62e06a0ab08f8ee61462299e77.

--- src/napari/_qt/_tests/test_viewer_qt_integration.py
+++ src/napari/_qt/_tests/test_viewer_qt_integration.py
@@ -11,9 +11,9 @@ import pytest
 from numpy import testing as npt
 from qtpy.QtCore import QEvent, QPointF, Qt, QUrl
 from qtpy.QtGui import QEnterEvent, QGuiApplication, QKeyEvent
-from qtpy.QtTest import QTest
 from qtpy.QtWidgets import QApplication
 
+from napari._qt._tests.test_qt_viewer import qt_viewer
 from napari._tests.utils import skip_local_popups, skip_on_win_ci
 from napari.settings import get_settings
 from napari.utils.action_manager import action_manager
@@ -348,9 +348,6 @@ def test_qt_viewer_console_focus(qtbot, make_napari_viewer):
 def test_screenshot(make_napari_viewer, qapp, qtbot):
     "Test taking a screenshot"
     viewer = make_napari_viewer(show=True)
-    qt_viewer = viewer.window._qt_viewer
-    assert QTest.qWaitForWindowExposed(viewer.window._qt_window, 5000)
-    qapp.processEvents()
     rng = np.random.default_rng(0)
     # Add image
     data = np.ones((10, 15), dtype=np.uint8)
@@ -372,7 +369,7 @@ def test_screenshot(make_napari_viewer, qapp, qtbot):
     data = 20 * rng.random((10, 4, 2))
     viewer.add_shapes(data)
     # wait for window be fully rendered
-    qtbot.waitUntil(qt_viewer.isVisible)
+    qtbot.wait_exposed(qt_viewer)
 
     # without these two lines, the shape of screenshot1 and screenshot2 differ by
     # 2 pixels. It looks like some event requires inactivity in event loop to
