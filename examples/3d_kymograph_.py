0aec4fc4fe4ddbb055238ea28c44aab4dfa98987
Aniket <148300120+Aniketsy@users.noreply.github.com>
2026-05-01 19:39:17 +0530

    Replace v (shorter) with viewer in the examples (#8940)
    Closes #8934 
    # References and relevant issues
    
    # Description
    - Replace v (shorter) with viewer in the examples since it's the default
    when you launch napari from the terminal.
    
    - need to discuss if we need to apply changes on tests also.

--- examples/3d_kymograph_.py
+++ examples/3d_kymograph_.py
@@ -151,19 +151,19 @@ for s in samples:
     print(f"Description: {s['description']}")
     s['vol'] = np.squeeze(IDR_fetch_image(s['IDRid']))
 
-v = napari.Viewer(ndisplay=3)
+viewer = napari.Viewer(ndisplay=3)
 scale = (5, 1, 1)  # "stretch" time domain
 for s in samples:
-    v.add_image(
+    viewer.add_image(
         s['vol'], name=s['description'], scale=scale, blending='opaque'
     )
 
-v.grid.enabled = True  # show the volumes in grid mode
-v.axes.visible = True  # magenta error shows time direction
+viewer.grid.enabled = True  # show the volumes in grid mode
+viewer.axes.visible = True  # magenta error shows time direction
 
 # set an oblique view angle onto the kymograph grid
-v.camera.center = (440, 880, 1490)
-v.camera.angles = (-20, 23, -50)
-v.camera.zoom = 0.17
+viewer.camera.center = (440, 880, 1490)
+viewer.camera.angles = (-20, 23, -50)
+viewer.camera.zoom = 0.17
 
 napari.run()
