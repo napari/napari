05af5debadd7c57796ce4ce6bdefd8c984218728
Lorenzo Gaifas <brisvag@gmail.com>
2026-06-17 18:00:20 +0200

    try fixing tests

--- src/napari/utils/_tests/test_tips.py
+++ src/napari/utils/_tests/test_tips.py
@@ -6,19 +6,25 @@ from napari.utils.tips import format_tip
 
 
 @pytest.mark.parametrize(
-    ('raw', 'formatted', 'formatted_mac'),
+    ('raw', 'formatted_linux', 'formatted_win', 'formatted_mac'),
     [
         (
             'press {Meta+K} to jump',
-            'press Super+K to jump',
-            'press ⌘+K to jump',
+            'press ⊞+K to jump',
+            'press ⌘K to jump',
         ),
-        ('{Space} or {Alt}', '␣ or Alt', 'Space or Alt'),  # multiple
-        ('{napari.viewer.fit_to_view}', 'Ctrl+0', '⌘+0'),  # appmodel
-        ('{napari:reset_view}', 'Ctrl+R', '⌘+R'),  # action manager
+        ('{Space} or {Alt}', '␣ or Alt', 'Space or Alt', '␣ or ⌥'),  # multiple
+        ('{napari.viewer.fit_to_view}', 'Ctrl+0', 'Ctrl+0', '⌘0'),  # appmodel
+        ('{napari:reset_view}', 'Ctrl+R', '⌘R'),  # action manager
     ],
 )
-def test_format_tips(raw, formatted, formatted_mac):
-    expected = formatted_mac if sys.platform == 'darwin' else formatted
+def test_format_tips(raw, formatted_linux, formatted_win, formatted_mac):
+    match sys.platform:
+        case 'linux':
+            expected = formatted_linux
+        case 'win32':
+            expected = formatted_win
+        case 'darwin':
+            expected = formatted_mac
 
     assert format_tip(raw) == expected
