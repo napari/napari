e8541a8dcc1565065f4eaf326b6b1b0517d0bda0
Kyle Harrington <czi@kyleharrington.com>
2026-06-19 11:18:29 -0700

    Revert "Fix flaky fullscreen test: skip on all Qt >= 6.9.0"
    This reverts commit 506d3547043cfb6041c45fa6566a59c7d7700bf6.

--- src/napari/_qt/_qapp_model/_tests/test_view_menu.py
+++ src/napari/_qt/_qapp_model/_tests/test_view_menu.py
@@ -3,7 +3,6 @@ import sys
 
 import numpy as np
 import pytest
-from packaging.version import parse as parse_version
 from qtpy import QT_VERSION
 from qtpy.QtCore import QPoint, Qt
 from qtpy.QtWidgets import QApplication
@@ -83,7 +82,7 @@ def test_toggle_axes_scale_bar_attr(
 
 @skip_local_popups
 @pytest.mark.skipif(
-    parse_version(QT_VERSION) >= parse_version('6.9.0'),
+    QT_VERSION == '6.9.0',
     reason='bug in Qt with maximized windows, https://bugreports.qt.io/browse/QTBUG-135844',
 )
 @pytest.mark.flaky(
@@ -136,7 +135,7 @@ def test_toggle_fullscreen_from_normal(make_napari_viewer, qtbot):
     reruns=2, reruns_delay=250, condition=sys.platform == 'darwin'
 )  # sometimes fails on macos CI
 @pytest.mark.skipif(
-    parse_version(QT_VERSION) >= parse_version('6.9.0'),
+    QT_VERSION == '6.9.0',
     reason='bug in Qt with maximized windows, https://bugreports.qt.io/browse/QTBUG-135844',
 )
 @pytest.mark.qt_log_level_fail('WARNING')
