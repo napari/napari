da2faab5da481434fb3140e55275081c641a2b2b
Talley Lambert <talley.lambert@gmail.com>
2026-06-20 09:00:31 -0400

    fix var

--- src/napari/_vispy/_tests/test_vispy_labels_brush_stroke_overlay.py
+++ src/napari/_vispy/_tests/test_vispy_labels_brush_stroke_overlay.py
@@ -66,7 +66,7 @@ def test_brush_stroke_circle_appears_on_right_click(MouseEvent):
 
 @pytest.mark.usefixtures('qapp')
 def test_live_paint_on_move_is_staged_not_committed(MouseEvent):
-    layer, overlay, _vispy, _data = _make_overlay()
+    layer, overlay, _vispy, data = _make_overlay()
 
     _press(MouseEvent, layer, (15, 15))
     # moves staying within the radius do not complete the stroke
