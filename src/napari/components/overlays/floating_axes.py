bd74fd22a1ce54c11b6edc4e5002e3088e94a84f
Lorenzo Gaifas <brisvag@gmail.com>
2026-06-23 12:39:47 +0200

    update docstrings

--- src/napari/components/overlays/floating_axes.py
+++ src/napari/components/overlays/floating_axes.py
@@ -23,16 +23,23 @@ class FloatingAxesOverlay(CanvasOverlay):
         If axes have arrowheads or not.
     size : float
         Size taken up by the overlay in canvas pixels.
+    position : CanvasPosition
+        The position of the overlay in the canvas.
+    box : bool
+        Whether the background box is visible or not.
+    box_color : ColorValue or None
+        Background box color. If unset, it defaults to the canvas color.
+    gridded : bool
+        The overlay will be duplicated across all grid cells in gridded mode.
     visible : bool
         If the overlay is visible or not.
     opacity : float
         The opacity of the overlay. 0 is fully transparent.
     order : int
         The rendering order of the overlay: lower numbers get rendered first.
-    position : str
-        The position of the overlay in the canvas. Must be one of
-        'top_left', 'top_right', 'bottom_left', 'bottom_right',
-        'top_center', 'bottom_center'.
+    blending : Blending
+        One of a list of preset blending modes that determines how RGB and
+        alpha values of the overlay get mixed with the visuals below.
     """
 
     labels: bool = True
