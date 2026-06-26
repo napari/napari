8bd4c92639e6c8e685d2c4e3792ce985ad021007
Aniket <148300120+Aniketsy@users.noreply.github.com>
2026-06-27 00:37:01 +0530

    fix mypy

--- src/napari/_qt/widgets/qt_tooltip.py
+++ src/napari/_qt/widgets/qt_tooltip.py
@@ -5,13 +5,13 @@ from typing import TYPE_CHECKING
 from qtpy.QtWidgets import QLabel, QToolTip
 
 if TYPE_CHECKING:
-    from qtpy.QtCore import QEvent  # type: ignore[attr-defined]
+    from qtpy.QtGui import QEnterEvent
 
 
 class QtToolTipLabel(QLabel):
     """A QLabel that provides instant tooltips on mouser hover."""
 
-    def enterEvent(self, event: QEvent) -> None:
+    def enterEvent(self, event: QEnterEvent | None) -> None:
         """Override to show tooltips instantly."""
         if self.toolTip():
             pos = self.mapToGlobal(self.contentsRect().center())
