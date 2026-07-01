2091d4fb511fa86734a3bbe7ceaf77d89591a201
Lorenzo Gaifas <brisvag@gmail.com>
2026-07-01 14:05:52 +0200

    happy mypy

--- src/napari/utils/translations.py
+++ src/napari/utils/translations.py
@@ -2,9 +2,11 @@
 #       We leave this as a shim to not break backward compatibility.
 
 import warnings
+from collections.abc import Callable
+from typing import Any
 
 
-def _warn(string, *a, **k):
+def _warn(string: str, *a: Any, **k: Any) -> str:
     warnings.warn(
         'Translations are no longer supported. Using "trans._" now just '
         'returns the input string.',
@@ -14,7 +16,7 @@ def _warn(string, *a, **k):
 
 
 class _trans_shim:
-    def __getattr__(cls, name):
+    def __getattr__(cls, name: str) -> Callable:
         return _warn
 
 
