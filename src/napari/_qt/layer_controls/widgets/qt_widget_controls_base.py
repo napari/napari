4a6a72227433aafeb819b54ef5aaa43ec6ce4456
Jacopo Abramo <jacopo.abramo@gmail.com>
2026-07-09 11:02:18 +0200

    fix(typing): resolve mypy errors

--- src/napari/_qt/layer_controls/widgets/qt_widget_controls_base.py
+++ src/napari/_qt/layer_controls/widgets/qt_widget_controls_base.py
@@ -1,10 +1,24 @@
-from abc import ABC, abstractmethod
+from __future__ import annotations
+
+from abc import ABCMeta, abstractmethod
+from typing import TYPE_CHECKING
 
 from qtpy.QtCore import QObject, Qt
 from qtpy.QtWidgets import QLabel, QWidget
 
 from napari.layers.base.base import Layer
 from napari.utils.events import disconnect_events
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+    from typing import Any
+
+    MetaBase = ABCMeta
+
+else:
+
+    class MetaBase(type(QObject), ABCMeta):
+        pass
 
 
 class QtWrappedLabel(QLabel):
@@ -13,7 +27,7 @@ class QtWrappedLabel(QLabel):
     to the right and vertically centered by default.
     """
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.setWordWrap(True)
         self.setAlignment(
@@ -21,11 +35,7 @@ class QtWrappedLabel(QLabel):
         )
 
 
-class MetaWidgetControlsBase(type(ABC), type(QObject)):
-    pass
-
-
-class QtWidgetControlsBase(QObject, ABC, metaclass=MetaWidgetControlsBase):
+class QtWidgetControlsBase(QObject, metaclass=MetaBase):
     """
     Base class that defines base methods for wrapper classes that do the
     connection of events/signals between layer attributes and Qt widgets.
@@ -46,7 +56,9 @@ class QtWidgetControlsBase(QObject, ABC, metaclass=MetaWidgetControlsBase):
         self._layer = layer
         # Track registered callbacks (defined via `attr_to_settr` for example)
         # so it is possible to disconnect them when the widget is being closed/deleted
-        self._callbacks = []
+        # TODO: what are the possible input types of the callbacks?
+        # maybe Any should do the trick?
+        self._callbacks: list[Callable[[], None]] = []
 
     @abstractmethod
     def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
