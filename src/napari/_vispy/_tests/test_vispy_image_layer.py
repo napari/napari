napari/napari

commit 381d205b1b1e713e76ce8df1b468ded29e0cbc1d

Use information about units when calculate scale of layers when render (#7889)
# References and relevant issues

depends on #7972

# Description

Prior to this PR, units were stored in metadata, but not used for
rendering. Adding two layers that represented the same physical space,
but had different units, e.g. a layer with scale=500, units='nm' and one
with scale=0.5, units='μm' wouldn't overlap correctly, even though they
should.

With this PR, layers with compatible units (i.e. those that share the
same physical dimension per axis, like all spatial), will make use of
`units` and `scale` will overlap correctly, using the smallest unit as
the rendering space.

If layers' units cannot be reconciled e.g. mixing space and temporal
units in the same dimension, we warn, and fall back to unitless
rendering.

Units can also be set globally on the layer list using
`viewer.layers.units = ('nm', 'nm')`. This overrides individual layer
units and forces all layers to be rendered in this unit space. If a new
layer is added with more dimensions than the current layers, this global
override is dropped with a warning, and we fall back to per-layer
values.

---------

Co-authored-by: Lorenzo Gaifas <brisvag@gmail.com>
Co-authored-by: Tim Monko <timmonko@gmail.com>
Co-authored-by: Draga Doncila Pop <17995243+DragaDoncila@users.noreply.github.com>

src/napari/_vispy/layers/base.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Generic, TypeVar, cast
 
 import numpy as np
+import pint
 from vispy.scene import VisualNode
 from vispy.visuals.transforms import MatrixTransform
 
@@ -56,6 +57,8 @@ class VispyBaseLayer(ABC, Generic[_L]):
         self._array_like = False
         self.node = node
         self.first_visible = False
+        self._world_units = layer.units
+        self._world_to_layer_units_scale = (1,) * layer.ndim
 
         (
             self.MAX_TEXTURE_SIZE_2D,
@@ -72,8 +75,35 @@ class VispyBaseLayer(ABC, Generic[_L]):
         self.layer.events.rotate.connect(self._on_matrix_change)
         self.layer.events.shear.connect(self._on_matrix_change)
         self.layer.events.affine.connect(self._on_matrix_change)
+        self.layer.events.units.connect(self._recalculate_units_scale)
         self.layer.experimental_clipping_planes.events.connect(
             self._on_experimental_clipping_planes_change
+        )
+
+    @property
+    def world_units(self) -> tuple[pint.Unit, ...]:
+        return self._world_units
+
+    @world_units.setter
+    def world_units(self, value: tuple[pint.Unit, ...] | None) -> None:
+        if value is None:
+            self._world_units = self.layer.units
+            self._world_to_layer_units_scale = (1,) * self.layer.ndim
+        else:
+            self._world_units = value[-self.layer.ndim :]
+            self._recalculate_units_scale()
+        self._on_matrix_change()
+
+    def _recalculate_units_scale(self):
+        """Calculate the scale factor between the layer units and the world units.
+
+        This is used to convert the layer's data coordinates to world coordinates.
+        If self._units is None, then the scale is set to 1 for all dimensions.
+        """
+        reg = pint.get_application_registry()
+        self._world_to_layer_units_scale = tuple(
+            reg.get_base_units(y)[0] / reg.get_base_units(x)[0]
+            for x, y in zip(self._world_units, self.layer.units, strict=False)
         )
 
     @property
@@ -168,8 +198,11 @@ class VispyBaseLayer(ABC, Generic[_L]):
         # convert NumPy axis ordering to VisPy axis ordering
         # by reversing the axes order and flipping the linear
         # matrix
-        translate = transform.translate[::-1]
-        matrix = transform.linear_matrix[::-1, ::-1].T
+        units_scale = [
+            self._world_to_layer_units_scale[x] for x in dims_displayed
+        ][::-1]
+        translate = transform.translate[::-1] * units_scale
+        matrix = transform.linear_matrix[::-1, ::-1].T * units_scale
 
         # The following accounts for the offset between samples at different
         # resolutions of 3D multi-scale array-like layers (e.g. images).
@@ -209,7 +242,7 @@ class VispyBaseLayer(ABC, Generic[_L]):
             # Convert NumPy axis ordering to VisPy axis ordering
             # and embed in full affine matrix
             affine_offset = np.eye(4)
-            affine_offset[-1, : len(offset)] = offset[::-1]
+            affine_offset[-1, : len(offset)] = offset[::-1] * units_scale
             affine_matrix = affine_matrix @ affine_offset
             if self.layer.multiscale:
                 # For performance reasons, when displaying multiscale images,

src/napari/_vispy/_tests/test_vispy_image_layer.py
@@ -3,6 +3,7 @@ from itertools import permutations
 import numpy as np
 import numpy.testing as npt
 import pytest
+from pint import get_application_registry
 
 from napari._vispy._tests.utils import vispy_image_scene_size
 from napari._vispy.layers.image import VispyImageLayer
@@ -257,3 +258,19 @@ def test_node_origin_is_consistent_with_multiscale(
     # full high and low resolution slices should always map to the same
     # scene origin, since this defines the start of the visible extent.
     np.testing.assert_array_equal(high_res_origin, low_res_origin)
+
+
+def test_world_units_impact_scale():
+    nm = get_application_registry().nm
+    image = Image(np.zeros((10, 10)), units=('um', 'um'))
+    vispy_image = VispyImageLayer(image)
+
+    assert vispy_image._world_to_layer_units_scale == (1, 1)
+
+    vispy_image.world_units = (nm, nm)
+    npt.assert_array_almost_equal(
+        vispy_image._world_to_layer_units_scale, (1000, 1000)
+    )
+
+    vispy_image.world_units = None
+    assert vispy_image._world_to_layer_units_scale == (1, 1)

src/napari/layers/base/base.py
@@ -5,7 +5,6 @@ import inspect
 import itertools
 import logging
 import os.path
-import typing
 import uuid
 from abc import ABC, ABCMeta, abstractmethod
 from collections import defaultdict
@@ -28,7 +27,11 @@ from napari.layers.base._base_mouse_bindings import (
     highlight_box_handles,
     transform_with_box,
 )
-from napari.layers.utils._slice_input import _SliceInput, _ThickNDSlice
+from napari.layers.utils._slice_input import (
+    _SliceInput,
+    _ThickNDSlice,
+    apply_units_to_transform,
+)
 from napari.layers.utils.interactivity_utils import (
     drag_data_to_projected_distance,
 )
@@ -131,10 +134,19 @@ class _LayerSlicingState(ABC):
         )
         self._loaded: bool = True
         self._last_slice_id: int = -1
+        self._units: tuple[pint.Unit, ...] | None = None
 
     def set_view_slice(self) -> None:
         with self.dask_optimized_slicing():
             self._set_view_slice()
+
+    def _slice_indices(
+        self, slice_input: _SliceInput, dims: Dims
+    ) -> _ThickNDSlice[float | int]:
+        """Get the indices of the current slice in the full data array."""
+        world_to_data = self.layer._data_to_world.inverse
+        world_to_data = apply_units_to_transform(world_to_data, dims.units)
+        return slice_input.data_slice(world_to_data)
 
     @property
     def data_slice(self) -> _ThickNDSlice:
@@ -144,15 +156,17 @@ class _LayerSlicingState(ABC):
             # early return to avoid evaluating data_to_world.inverse
             return _ThickNDSlice.make_full(point=(np.nan,) * self.ndim)
 
-        return self._slice_input.data_slice(
-            self.layer._data_to_world.inverse,
-        )
+        world_to_data = self.layer._data_to_world.inverse
+        world_to_data = apply_units_to_transform(world_to_data, self._units)
+
+        return self._slice_input.data_slice(world_to_data=world_to_data)
 
     def update_dims(self):
         self._slice_input = self._slice_input.with_ndim(self.ndim)
 
     def set_slice_input_from_dims(self, dims: Dims, force: bool) -> bool:
         slice_input = self.make_slice_input(dims)
+        self._units = dims.units
         return self.set_slice_input(slice_input, force)
 
     def set_slice_input(self, slice_input: _SliceInput, force: bool) -> bool:
@@ -172,13 +186,13 @@ class _LayerSlicingState(ABC):
         self,
         dims: Dims,
     ) -> _SliceInput:
-        world_slice = _ThickNDSlice.from_dims(dims)
+        world_slice = _ThickNDSlice[float].from_dims(dims)
         order_array = (
             np.arange(dims.ndim)
             if dims.order is None
             else np.asarray(dims.order)
         )
-        order = tuple(
+        order: tuple[int, ...] = tuple(
             self._world_to_layer_dims(
                 world_dims=order_array,
                 ndim_world=dims.ndim,
@@ -188,7 +202,7 @@ class _LayerSlicingState(ABC):
         return _SliceInput(
             ndisplay=dims.ndisplay,
             world_slice=world_slice[-self.ndim :],
-            order=typing.cast(tuple[int, ...], order[-self.ndim :]),
+            order=order[-self.ndim :],
         )
 
     def _world_to_layer_dims(
@@ -1188,6 +1202,7 @@ class Layer(KeymapProvider, MousemapProvider, ABC, metaclass=PostInit):
             data=extent_data,
             world=extent_world,
             step=abs(data_to_world.scale),
+            units=self.units,
         )
 
     @cached_property
@@ -1210,6 +1225,7 @@ class Layer(KeymapProvider, MousemapProvider, ABC, metaclass=PostInit):
             data=extent_data,
             world=extent_world,
             step=abs(data_to_world.scale),
+            units=self.units,
         )
 
     def _clear_extent(self) -> None:
@@ -1781,7 +1797,9 @@ class Layer(KeymapProvider, MousemapProvider, ABC, metaclass=PostInit):
 
         affine * (rotate * shear * scale + translate)
         """
-        return self._transforms[1:3].simplified
+        res = self._transforms[1:3].simplified
+        res.units = self.units
+        return res
 
     def _world_to_data_ray(self, vector: npt.ArrayLike) -> npt.NDArray:
         """Convert a vector defining an orientation from world coordinates to data coordinates.

examples/dev/units_impact_rendering.py
@@ -1,0 +1,15 @@
+import skimage
+
+import napari
+
+data = skimage.data.cells3d()
+
+ch1 = data[:, 0]
+ch2 = data[:, 1]
+
+
+viewer = napari.Viewer()
+viewer.add_image(ch1, units=('nm', 'nm', 'nm'), name='ch1', scale=(210, 70, 70), colormap="magenta")
+viewer.add_image(ch2, units=('μm', 'μm', 'μm'), name='ch2', scale=(0.210, 0.07, 0.07), colormap="green", blending='additive')
+
+napari.run()

examples/xarray-latlon-timeseries.py
@@ -49,7 +49,7 @@ def get_scale_translate(dataset, array_name):
     dims = [getattr(dataset, dim) for dim in array.dims]
     translate = [float(d[0]) for d in dims]
     scale = [float(d[1] - d[0]) for d in dims]
-    return {'scale': scale, 'translate': translate}
+    return {'scale': scale, 'translate': translate, 'units':('ns', 'degrees', 'degrees')}
 
 
 # Show the raw (not resampled) model data
@@ -70,6 +70,7 @@ air_layer = viewer.add_image(
         contrast_limits=(-23 + 273, 32 + 273),  # data are in degrees Kelvin
         )
 
+viewer.layers.units = ('hour', 'degrees', 'degrees')
 # set a time that overlaps both datasets
 viewer.dims.set_point(0, np.datetime64('2013-03-10T18:00:00.000000000'))
 

src/napari/_app_model/actions/_file.py
@@ -63,12 +63,14 @@ def add_new_points(viewer: 'ViewerModel') -> None:
         viewer.add_points(  # type: ignore[attr-defined]
             ndim=ndim,
             scale=(1,) * ndim,
+            units=viewer.layers.extent.units,
         )
     else:
         extent = viewer.layers.get_extent(viewer.layers.selection)
         viewer.add_points(  # type: ignore[attr-defined]
             ndim=len(extent.step),
             scale=extent.step,
+            units=viewer.layers.extent.units,
         )
 
 
@@ -78,12 +80,14 @@ def add_new_shapes(viewer: 'ViewerModel') -> None:
         viewer.add_shapes(  # type: ignore[attr-defined]
             ndim=ndim,
             scale=(1,) * ndim,
+            units=viewer.layers.extent.units,
         )
     else:
         extent = viewer.layers.get_extent(viewer.layers.selection)
         viewer.add_shapes(  # type: ignore[attr-defined]
             ndim=len(extent.step),
             scale=extent.step,
+            units=viewer.layers.extent.units,
         )
 
 

src/napari/_vispy/canvas.py
@@ -34,6 +34,7 @@ from napari.utils.interactions import (
     mouse_release_callbacks,
     mouse_wheel_callbacks,
 )
+from napari.utils.notifications import show_warning
 from napari.utils.theme import get_theme
 
 if TYPE_CHECKING:
@@ -204,6 +205,7 @@ class VispyCanvas:
         self._overlay_callbacks = {}
         self._last_viewbox_size = np.array((0, 0))
         self._needs_overlay_position_update = False
+        self._needs_world_units_update = False
 
         self.bgcolor = transform_color(
             get_theme(self.viewer.theme).canvas.as_hex()
@@ -260,6 +262,9 @@ class VispyCanvas:
         )
         self.viewer.layers.events.end_batch.connect(
             self._resume_scene_graph_update
+        )
+        self.viewer.layers.events.units.connect(
+            self._deferred_world_units_update
         )
         self.viewer.grid.events.stride.connect(self._update_scenegraph)
         self.viewer.grid.events.shape.connect(self._update_scenegraph)
@@ -714,6 +719,10 @@ class VispyCanvas:
             self._update_overlay_canvas_positions()
             self._needs_overlay_position_update = False
 
+        if self._needs_world_units_update:
+            self._update_world_units()
+            self._needs_world_units_update = False
+
         # sync all cameras
         for camera in (self.camera, *self.grid_cameras):
             camera.on_draw(event)
@@ -778,13 +787,29 @@ class VispyCanvas:
         napari_layer._overlays.events.added.connect(overlay_callback)
         napari_layer._overlays.events.removed.connect(overlay_callback)
         napari_layer._overlays.events.changed.connect(overlay_callback)
+        napari_layer.events.units.connect(self._deferred_world_units_update)
         self._overlay_callbacks[napari_layer] = overlay_callback
         self.viewer.camera.events.angles.connect(vispy_layer._on_camera_move)
+        self._update_world_units()
 
         # we need to trigger _on_matrix_change once after adding the overlays so that
         # all children nodes are assigned the correct transforms
         vispy_layer._on_matrix_change()
         self._update_scenegraph()
+
+    def _deferred_world_units_update(self):
+        """Defer the world units update until the next draw event."""
+        self._needs_world_units_update = True
+
+    def _update_world_units(self):
+        """Update the units of the canvas and all layers."""
+        units = self.viewer.layers.extent.units
+        if units is None:
+            show_warning(
+                'Inconsistent units across layers; units will not be used for rendering.'
+            )
+        for vispy_layer in self.layer_to_visual.values():
+            vispy_layer.world_units = units
 
     def _remove_layer(self, event: Event) -> None:
         """Upon receiving event closes the Vispy visual, deletes it and reorders the still existing layers.
@@ -804,6 +829,7 @@ class VispyCanvas:
         disconnect_events(
             layer._overlays.events, self._overlay_callbacks[layer]
         )
+        layer.events.units.disconnect(self._deferred_world_units_update)
         del self._overlay_callbacks[layer]
 
         vispy_layer = self.layer_to_visual.pop(layer)

src/napari/components/_tests/test_layers_list.py
@@ -3,11 +3,15 @@ from unittest.mock import Mock
 
 import npe2
 import numpy as np
+import numpy.testing as npt
 import pytest
+from pint import get_application_registry
 
 from napari.components import LayerList
 from napari.layers import Image
 from napari.layers.utils._link_layers import get_linked_layers
+
+REG = get_application_registry()
 
 
 def test_empty_layers_list():
@@ -611,3 +615,123 @@ def test_layer_renamed_event_connection_management():
 
     layers.remove(layer)
     assert len(layer.events.name.callbacks) == 0
+
+
+def test_update_units_in_layer():
+    layer_n = Image(
+        np.zeros((5, 5)), name='image', scale=(500, 500), units=('um', 'um')
+    )
+    layer_u = Image(
+        np.zeros((5, 5)), name='image', scale=(0.5, 0.5), units=('um', 'um')
+    )
+
+    layer_list = LayerList([layer_u, layer_n])
+    npt.assert_array_equal(layer_list.extent.step, (0.500, 0.500))
+    assert layer_list.extent.units == layer_u.units
+
+    layer_n.units = ('nm', 'nm')
+    npt.assert_almost_equal(layer_list.extent.step, (500, 500))
+    assert layer_list.extent.units == layer_n.units
+
+
+def test_incompatible_units():
+    layer_u = Image(np.zeros((5, 5)), units=('um', 'um'))
+    layer_px = Image(np.zeros((5, 5)), units=('pixels', 'pixels'))
+    layer_list = LayerList([layer_u, layer_px])
+    assert layer_list.extent.units is None
+    npt.assert_array_equal(layer_list._step_size, (1, 1))
+
+
+def test_extent_world():
+    """Test world extent after adding layers."""
+    image_1 = Image(np.zeros((7, 5)))
+    image_2 = Image(np.zeros((5, 7)))
+    ll = LayerList([image_1, image_2])
+    npt.assert_array_equal(ll._extent_world, ((0, 0), (6, 6)))
+    npt.assert_array_equal(
+        ll._extent_world_augmented, ((-0.5, -0.5), (6.5, 6.5))
+    )
+
+
+def test_default_extent_world():
+    """Test default extent after adding layers."""
+    ll = LayerList()
+    npt.assert_array_equal(ll._extent_world, ((0, 0), (511, 511)))
+    npt.assert_array_equal(
+        ll._extent_world_augmented, ((-0.5, -0.5), (511.5, 511.5))
+    )
+
+
+def test_overload_units():
+    layer_1 = Image(np.zeros((5, 5)), units=('um', 'um'))
+    layer_2 = Image(np.zeros((5, 5)), units=('um', 'um'))
+    layer_list = LayerList([layer_1, layer_2])
+    assert layer_list.extent.units == (REG.um, REG.um)
+    assert layer_list.units == (REG.um, REG.um)
+
+    layer_1.units = ('nm', 'nm')
+    assert layer_list.units == (REG.nm, REG.nm)
+    assert layer_list.extent.units == (REG.nm, REG.nm)
+    assert layer_list._get_units([x.extent for x in layer_list]) == (
+        REG.nm,
+        REG.nm,
+    )
+
+
+def test_warn_units_dimensions1():
+    layer_1 = Image(np.zeros((5, 5)), units=('um', 'um'))
+    layer_2 = Image(np.zeros((3, 5, 5)), units=('um', 'um', 'um'))
+
+    layer_list = LayerList([layer_1])
+
+    assert layer_list.units == (REG.um, REG.um)
+    layer_list.units = ('nm', 'nm')
+    assert layer_list.units == (REG.nm, REG.nm)
+
+    with pytest.warns(
+        UserWarning,
+        match='New layer has more dimensions than the current units, dropping units override',
+    ):
+        layer_list.append(layer_2)
+
+    assert layer_list.units == (REG.um, REG.um, REG.um)
+
+
+def test_warn_units_dimensions2():
+    layer_1 = Image(np.zeros((5, 5)), units=('um', 'um'))
+    layer_2 = Image(np.zeros((3, 5, 5)), units=('um', 'um', 'um'))
+
+    layer_list = LayerList([layer_1, layer_2])
+
+    with pytest.raises(
+        ValueError,
+        match='Number of units must be at least the number of dimensions',
+    ):
+        layer_list.units = ('nm', 'nm')
+
+    assert layer_list.units == (REG.um, REG.um, REG.um)
+
+
+def test_cannot_override_unit_when_inconsistent_layers():
+    layer_1 = Image(np.zeros((5, 5)), units=('um', 'um'))
+    layer_2 = Image(np.zeros((5, 5)), units=('s', 'nm'))
+    layer_list = LayerList([layer_1, layer_2])
+    assert layer_list.units is None
+    with pytest.raises(
+        ValueError,
+        match='Cannot set units when layers have inconsistent dimensionality',
+    ):
+        layer_list.units = ('nm', 'nm')
+    assert layer_list.units is None
+
+
+def test_warn_incompatible_overriding_units():
+    layer_1 = Image(np.zeros((5, 5)), units=('um', 'um'))
+    layer_list = LayerList([layer_1])
+    assert layer_list.units == (REG.um, REG.um)
+    with pytest.raises(
+        ValueError, match='On axis -2 units must be consistent'
+    ):
+        layer_list.units = ('s', 'nm')
+
+    assert layer_list.units == (REG.um, REG.um)

commit 700a36f148dc073d281b5a9e42bb28cd18ed6a32

Fix: update cached unit scale when layer data dimensionality changes (#9164) (#9181)
## References and relevant issues

Closes #9164

## Description

Bug introduced in #7889 when deriving the `_world_to_layer_units_scale`
from the layer state. A changing dimensionality of layer.data did not
update this so `_on_matrix_change` has incorrectly cached values. This
guards against a cache mismatch and rebuilds the layer state.

I thought there may be an event on the layer which could fire this, but
I couldn't find one. This `_on_matrix_change` is seemingly called
through every code path like transforms and such so I hope should cover
all instances of changing layer properties for this calculation.

I added an LLM-derived regression test which fails on main and passes
with this PR.

---------

Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>
Co-authored-by: Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

src/napari/_vispy/layers/base.py
@@ -202,6 +202,14 @@ class VispyBaseLayer(ABC, Generic[_L]):
 
     def _on_matrix_change(self):
         dims_displayed = self.layer._slice_input.displayed
+        # If the layer's dimensionality changed (e.g., data swapped from 2D
+        # to 3D), _world_to_layer_units_scale reflects the old ndim
+        # and cannot be indexed with the new dims_displayed values.  Refresh
+        # both cached unit tracking fields to match the current layer.
+        if len(self._world_to_layer_units_scale) != self.layer.ndim:
+            self._world_units = self.layer.units
+            self._world_to_layer_units_scale = (1,) * self.layer.ndim
+
         # mypy: self.layer._transforms.simplified cannot be None
         transform = self.layer._transforms.simplified.set_slice(dims_displayed)
         # convert NumPy axis ordering to VisPy axis ordering

src/napari/_vispy/_tests/test_vispy_image_layer.py
@@ -321,3 +321,28 @@ def test_world_units_impact_scale():
 
     vispy_image.world_units = None
     assert vispy_image._world_to_layer_units_scale == (1, 1)
+
+
+def test_changing_data_dimensionality_updates_units_scale():
+    """Regression test for https://github.com/napari/napari/issues/9164
+
+    When a layer's data changes dimensionality (e.g., 2D -> 3D), the
+    cached _world_to_layer_units_scale must be updated to match the
+    new ndim before _on_matrix_change tries to index it.
+    """
+    font_info = FontInfo()
+
+    # Start with 2D data
+    image = Image(np.zeros((10, 10)))
+    vispy_image = VispyImageLayer(image, font_info=font_info)
+    assert vispy_image._world_to_layer_units_scale == (1, 1)
+
+    # Changing data to 3D should not cause an IndexError in
+    # _on_matrix_change (called via _on_data_change).
+    image.data = np.zeros((5, 10, 10))
+    assert len(vispy_image._world_to_layer_units_scale) == image.ndim
+    # _on_matrix_change should succeed without IndexError
+
+    # Changing back to 2D should also work
+    image.data = np.zeros((10, 10))
+    assert len(vispy_image._world_to_layer_units_scale) == image.ndim