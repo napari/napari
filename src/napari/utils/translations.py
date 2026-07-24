9332df4f546e251a5403a17a4c0470592908eb2d
Lorenzo Gaifas <brisvag@gmail.com>
2026-07-24 13:50:26 +0200

    log instead of warning

--- src/napari/utils/translations.py
+++ src/napari/utils/translations.py
@@ -1,13 +1,15 @@
 # NOTE: Translations are deprecated!
 #       We leave this as a shim to not break backward compatibility.
 
-import warnings
+import logging
 from collections.abc import Callable
 from typing import Any
 
+logger = logging.getLogger(__name__)
 
-def _warn(string: str, *a: Any, **k: Any) -> str:
-    warnings.warn(
+
+def _log(string: str, *a: Any, **k: Any) -> str:
+    logger.debug(
         'Translations are no longer supported. Using "trans._" now just '
         'returns the input string.',
         stacklevel=2,
@@ -16,8 +18,15 @@ def _warn(string: str, *a: Any, **k: Any) -> str:
 
 
 class _trans_shim:
+    """Shim around _log.
+
+    This makes sure that if any method of this class gets called, it returns
+    its own input and logs a debug message. The point is to ensure that users
+    of `trans._`, `trans._n` etc. will not be disrupted.
+    """
+
     def __getattr__(cls, name: str) -> Callable:
-        return _warn
+        return _log
 
 
 trans = _trans_shim()
