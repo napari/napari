0d2c8bd8bea664388ce11bf9b7334c41f9d184f9
Tim Monko <timmonko@gmail.com>
2026-07-26 16:07:21 +0200

    pass the 6 layers in the viewer to the grid.actual_shape for tests

--- src/napari/_tests/test_with_screenshot.py
+++ src/napari/_tests/test_with_screenshot.py
@@ -230,21 +230,14 @@ def test_grid_mode(make_napari_viewer):
         [255, 0, 0, 255],
     ]
 
-    class _MockLayer:
-        def __init__(self, visible=True):
-            self.visible = visible
-
-    # Create 9 visible mock layers
-    layers_6 = [_MockLayer() for _ in range(6)]
-
     viewer = make_napari_viewer(show=True)
 
-    # Add images
+    # Add 6 image layers
     data = np.ones((6, 15, 15))
     viewer.add_image(data, channel_axis=0, blending='translucent')
 
     assert not viewer.grid.enabled
-    assert viewer.grid.actual_shape(layers_6) == (1, 1)
+    assert viewer.grid.actual_shape(viewer.layers) == (1, 1)
     assert viewer.grid.stride == 1
 
     # check screenshot
@@ -255,7 +248,7 @@ def test_grid_mode(make_napari_viewer):
     # enter grid view
     viewer.grid.enabled = True
     assert viewer.grid.enabled
-    assert viewer.grid.actual_shape(layers_6) == (2, 3)
+    assert viewer.grid.actual_shape(viewer.layers) == (2, 3)
     assert viewer.grid.stride == 1
 
     # check screenshot
@@ -292,7 +285,7 @@ def test_grid_mode(make_napari_viewer):
     # return to stack view
     viewer.grid.enabled = False
     assert not viewer.grid.enabled
-    assert viewer.grid.actual_shape(layers_6) == (1, 1)
+    assert viewer.grid.actual_shape(viewer.layers) == (1, 1)
     assert viewer.grid.stride == 1
 
 
