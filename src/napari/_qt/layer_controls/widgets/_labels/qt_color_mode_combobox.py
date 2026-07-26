napari/napari

commit 26132e8a82bebde7efee26f38bb98f1ada438b5e

Remove translations code (#8935)
# References and relevant issues
Discussed in several previous PRs, issues, and meetings: the translation
machinery was built a long time ago in a rush, left unmaintained and
mostly unused, and at this point adds unnecessary confusion and
complexity.

# Description
This PR removes the translation code and all of its uses.


A big chunk of the changes were automated:
- all `napari.utils.translation` imports were removed with some vim
search/replace
- all `trans._` uses were replaced with the script below. Regex turned
out to not be enough to cut it, so I played around with some AST
manipulation. I think everything makes sense, but we need more eyes on
it :P

Some manual changes were needed:
- some cleanup of corner cases for the above script
- removal of `*TRANSLATION*` dictionaries used in colormaps and various
constants/enums. This is currently a potentially *breaking change*,
because some "translations" mapped from `some_thing` to `some thing`.
I'm not sure if any of the places using this are programmatic; if only
GUI, i'd call this non-breaking. We can also add or followup with some
logic to automatically convert `_` to spaces in the gui.
- the `Language` option in the setting is also now removed. I think we
might need a migration thing here? Not sure how those work.


I suppose that since `napari.utils.translations` is public, this whole
thing is a breaking change anyways... We might just add a shim that
returns the string itself while we deprecate?

<details>
  <summary>replace_trans.py</summary>
  
  ```py
  #!/usr/bin/env python3
  
  import re
  import sys
  from pathlib import Path
  
  import libcst as cst
  
  # matches {field_name[!r/s/a][:format_spec]}
  template_field_regex = re.compile(r"\{([^{}!:]+)(![rsa])?(:[^}]*)?\}")
  
  
  def unwrap_concatenated_string(node, module):
      if isinstance(node, cst.ConcatenatedString):
          # not perfect with whitespace but ruff should handle it

src/napari/_qt/layer_controls/widgets/_labels/qt_color_mode_combobox.py
@@ -1,7 +1,7 @@
 from qtpy.QtWidgets import (
-    QComboBox,
     QWidget,
 )
+from superqt import QEnumComboBox
 
 from napari._qt.layer_controls.widgets.qt_widget_controls_base import (
     QtWidgetControlsBase,
@@ -9,11 +9,9 @@ from napari._qt.layer_controls.widgets.qt_widget_controls_base import (
 )
 from napari.layers import Labels
 from napari.layers.labels._labels_constants import (
-    LABEL_COLOR_MODE_TRANSLATIONS,
     LabelColorMode,
 )
 from napari.utils import CyclicLabelColormap
-from napari.utils.translations import trans
 
 
 class QtColorModeComboBoxControl(QtWidgetControlsBase):
@@ -42,20 +40,16 @@ class QtColorModeComboBoxControl(QtWidgetControlsBase):
         self._layer.events.colormap.connect(self._on_colormap_change)
 
         # Setup widgets
-        color_mode_comboBox = QComboBox()
-        for data, text in LABEL_COLOR_MODE_TRANSLATIONS.items():
-            data = data.value
-            color_mode_comboBox.addItem(text, data)
-
+        color_mode_comboBox = QEnumComboBox(enum_class=LabelColorMode)
         self.color_mode_combobox = color_mode_comboBox
         self._on_colormap_change()
         color_mode_comboBox.activated.connect(self.change_color_mode)
 
-        self.color_mode_combobox_label = QtWrappedLabel(trans._('color mode:'))
+        self.color_mode_combobox_label = QtWrappedLabel('color mode:')
 
     def change_color_mode(self) -> None:
         """Change color mode of label layer"""
-        if self.color_mode_combobox.currentData() == LabelColorMode.AUTO.value:
+        if self.color_mode_combobox.currentEnum() == LabelColorMode.AUTO.value:
             self._layer.colormap = self._layer._original_random_colormap
         else:
             self._layer.colormap = self._layer._direct_colormap
@@ -71,13 +65,9 @@ class QtColorModeComboBoxControl(QtWidgetControlsBase):
                 'mode.'
             )
         if isinstance(self._layer.colormap, CyclicLabelColormap):
-            self.color_mode_combobox.setCurrentIndex(
-                self.color_mode_combobox.findData(LabelColorMode.AUTO.value)
-            )
+            self.color_mode_combobox.setCurrentEnum(LabelColorMode.AUTO)
         else:
-            self.color_mode_combobox.setCurrentIndex(
-                self.color_mode_combobox.findData(LabelColorMode.DIRECT.value)
-            )
+            self.color_mode_combobox.setCurrentEnum(LabelColorMode.DIRECT)
 
     def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
         return [(self.color_mode_combobox_label, self.color_mode_combobox)]

src/napari/_qt/layer_controls/widgets/_points/qt_symbol_combobox.py
@@ -1,4 +1,5 @@
-from qtpy.QtWidgets import QComboBox, QWidget
+from qtpy.QtWidgets import QWidget
+from superqt import QEnumComboBox
 
 from napari._qt.layer_controls.widgets.qt_widget_controls_base import (
     QtWidgetControlsBase,
@@ -6,11 +7,7 @@ from napari._qt.layer_controls.widgets.qt_widget_controls_base import (
 )
 from napari._qt.utils import qt_signals_blocked
 from napari.layers import Points
-from napari.layers.points._points_constants import (
-    SYMBOL_TRANSLATION,
-    SYMBOL_TRANSLATION_INVERTED,
-)
-from napari.utils.translations import trans
+from napari.layers.points._points_constants import Symbol
 
 
 class QtSymbolComboBoxControl(QtWidgetControlsBase):
@@ -42,29 +39,17 @@ class QtSymbolComboBoxControl(QtWidgetControlsBase):
         )
 
         # Setup widgets
-        sym_cb = QComboBox()
+        sym_cb = QEnumComboBox(enum_class=Symbol)
         sym_cb.setToolTip(
-            trans._(
-                'Change the symbol of currently selected points and any added afterwards.'
-            )
+            'Change the symbol of currently selected points and any added afterwards.'
         )
-        current_index = 0
-        for index, (symbol_string, text) in enumerate(
-            SYMBOL_TRANSLATION.items()
-        ):
-            symbol_string = symbol_string.value
-            sym_cb.addItem(text, symbol_string)
-
-            if symbol_string == self._layer.current_symbol:
-                current_index = index
-
-        sym_cb.setCurrentIndex(current_index)
-        sym_cb.currentTextChanged.connect(self.change_current_symbol)
+        sym_cb.setCurrentEnum(self._layer.current_symbol)
+        sym_cb.currentEnumChanged.connect(self.change_current_symbol)
         self.symbol_combobox = sym_cb
 
-        self.symbol_combobox_label = QtWrappedLabel(trans._('symbol:'))
+        self.symbol_combobox_label = QtWrappedLabel('symbol:')
 
-    def change_current_symbol(self, text: str) -> None:
+    def change_current_symbol(self, symbol: Symbol) -> None:
         """Change marker symbol of the points on the layer model.
 
         Parameters
@@ -73,14 +58,12 @@ class QtSymbolComboBoxControl(QtWidgetControlsBase):
             Index of current marker symbol of points, eg: '+', '.', etc.
         """
         with self._layer.events.symbol.blocker(self._on_current_symbol_change):
-            self._layer.current_symbol = SYMBOL_TRANSLATION_INVERTED[text]
+            self._layer.current_symbol = symbol
 
     def _on_current_symbol_change(self) -> None:
         """Receive marker symbol change event and update the dropdown menu."""
         with qt_signals_blocked(self.symbol_combobox):
-            self.symbol_combobox.setCurrentIndex(
-                self.symbol_combobox.findData(self._layer.current_symbol.value)
-            )
+            self.symbol_combobox.setCurrentEnum(self._layer.current_symbol)
 
     def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
         return [(self.symbol_combobox_label, self.symbol_combobox)]

src/napari/_qt/layer_controls/widgets/_surface/qt_shading_combobox.py
@@ -1,7 +1,7 @@
 from qtpy.QtWidgets import (
-    QComboBox,
     QWidget,
 )
+from superqt import QEnumComboBox
 
 from napari._qt.layer_controls.widgets.qt_widget_controls_base import (
     QtWidgetControlsBase,
@@ -9,8 +9,7 @@ from napari._qt.layer_controls.widgets.qt_widget_controls_base import (
 )
 from napari._qt.utils import qt_signals_blocked
 from napari.layers import Surface
-from napari.layers.surface._surface_constants import SHADING_TRANSLATION
-from napari.utils.translations import trans
+from napari.layers.surface._surface_constants import Shading
 
 
 class QtShadingComboBoxControl(QtWidgetControlsBase):
@@ -33,23 +32,20 @@ class QtShadingComboBoxControl(QtWidgetControlsBase):
         Label for the shading value chooser widget.
     """
 
+    _layer: Surface
+
     def __init__(self, parent: QWidget, layer: Surface) -> None:
         super().__init__(parent, layer)
         # Setup layer
         self._layer.events.shading.connect(self._on_shading_change)
 
         # Setup widgets
-        shading_comboBox = QComboBox(parent)
-        for display_name, shading in SHADING_TRANSLATION.items():
-            shading_comboBox.addItem(display_name, shading)
-        index = shading_comboBox.findData(
-            SHADING_TRANSLATION[self._layer.shading]
-        )
-        shading_comboBox.setCurrentIndex(index)
-        shading_comboBox.currentTextChanged.connect(self.change_shading)
+        shading_comboBox = QEnumComboBox(parent, Shading)
+        shading_comboBox.setCurrentEnum(Shading(self._layer.shading))
+        shading_comboBox.currentEnumChanged.connect(self.change_shading)
         self.shading_combobox = shading_comboBox
 
-        self.shading_combobox_label = QtWrappedLabel(trans._('shading:'))
+        self.shading_combobox_label = QtWrappedLabel('shading:')
 
     def change_shading(self, text: str) -> None:
         """Change shading value on the surface layer.
@@ -58,16 +54,12 @@ class QtShadingComboBoxControl(QtWidgetControlsBase):
         text : str
             Name of shading mode, eg: 'flat', 'smooth', 'none'.
         """
-        self._layer.shading = self.shading_combobox.currentData()
+        self._layer.shading = self.shading_combobox.currentEnum()
 
     def _on_shading_change(self) -> None:
         """Receive layer model shading change event and update combobox."""
         with qt_signals_blocked(self.shading_combobox):
-            self.shading_combobox.setCurrentIndex(
-                self.shading_combobox.findData(
-                    SHADING_TRANSLATION[self._layer.shading]
-                )
-            )
+            self.shading_combobox.setCurrentEnum(self._layer.shading)
 
     def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
         return [(self.shading_combobox_label, self.shading_combobox)]

commit cfb9f69d0ebcb1ec1afb46568b3a23564a35e783

Fix enum breaks from translations removal (#9274)
# References and relevant issues
- bugs introduced in #8935

# Description
Removing the translation dictionaries in #8935 meant I started using the
enums directly in stuff like `EnumComboBox`. Unfortunately this only
accepts the enum itself, and layers usually return just a string; some
of the places where strings needed to be wrapped by enums slipped
through the cracks...

To get the error, run `examples/spheres_.py` and change shading.

Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>

src/napari/_qt/layer_controls/widgets/_labels/qt_color_mode_combobox.py
@@ -34,6 +34,8 @@ class QtColorModeComboBoxControl(QtWidgetControlsBase):
         Label for the color mode chooser widget.
     """
 
+    _layer: Labels
+
     def __init__(self, parent: QWidget, layer: Labels) -> None:
         super().__init__(parent, layer)
         # Setup layer

src/napari/_qt/layer_controls/widgets/_points/qt_symbol_combobox.py
@@ -30,6 +30,8 @@ class QtSymbolComboBoxControl(QtWidgetControlsBase):
         Label for the current symbol chooser widget.
     """
 
+    _layer: Points
+
     def __init__(self, parent: QWidget, layer: Points) -> None:
         super().__init__(parent, layer)
         # Setup layer
@@ -43,7 +45,7 @@ class QtSymbolComboBoxControl(QtWidgetControlsBase):
         sym_cb.setToolTip(
             'Change the symbol of currently selected points and any added afterwards.'
         )
-        sym_cb.setCurrentEnum(self._layer.current_symbol)
+        sym_cb.setCurrentEnum(Symbol(self._layer.current_symbol))
         sym_cb.currentEnumChanged.connect(self.change_current_symbol)
         self.symbol_combobox = sym_cb
 
@@ -63,7 +65,9 @@ class QtSymbolComboBoxControl(QtWidgetControlsBase):
     def _on_current_symbol_change(self) -> None:
         """Receive marker symbol change event and update the dropdown menu."""
         with qt_signals_blocked(self.symbol_combobox):
-            self.symbol_combobox.setCurrentEnum(self._layer.current_symbol)
+            self.symbol_combobox.setCurrentEnum(
+                Symbol(self._layer.current_symbol)
+            )
 
     def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
         return [(self.symbol_combobox_label, self.symbol_combobox)]

src/napari/_qt/layer_controls/widgets/_surface/qt_shading_combobox.py
@@ -59,7 +59,7 @@ class QtShadingComboBoxControl(QtWidgetControlsBase):
     def _on_shading_change(self) -> None:
         """Receive layer model shading change event and update combobox."""
         with qt_signals_blocked(self.shading_combobox):
-            self.shading_combobox.setCurrentEnum(self._layer.shading)
+            self.shading_combobox.setCurrentEnum(Shading(self._layer.shading))
 
     def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
         return [(self.shading_combobox_label, self.shading_combobox)]