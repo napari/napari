b3e738be13316bc65b0d6dd88b04a59b6247e971
Tim Monko <timmonko@gmail.com>
2026-07-28 16:58:09 +0200

    update xarray-latlon-timeseries.py

--- examples/xarray-latlon-timeseries.py
+++ examples/xarray-latlon-timeseries.py
@@ -2,8 +2,10 @@
 Displaying xarray data in napari
 ================================
 
-This example shows how to view xarray datasets in napari, including scale
-and translation information.
+This example shows how to view xarray datasets in napari.
+
+napari automatically inherits axis_labels, scale, and units (if valid Pint
+units), and translate from xarray DataArrays when available. 
 
 Currently, napari cannot display irregularly-sampled data, so the code
 assumes that the data indices are regularly spaced. If your indices are
@@ -23,64 +25,35 @@ sst = xr.tutorial.open_dataset('ersstv5')
 airtemp = xr.tutorial.open_dataset('air_temperature')
 
 
-def get_scale_translate(dataset, array_name):
-    """Get the translate/offset and scale parameters for an xarray dataset.
-
-    This code assumes that the dataset is regularly spaced. You should
-    interpolate your data if it is sampled at irregular spaces.
-
-    Parameters
-    ----------
-    dataset : xr.Dataset
-        The dataset containing the array to be displayed.
-    array_name : str
-        The name of the xarray DataArray within `dataset` to be displayed in
-        napari.
-
-    Returns
-    -------
-    param_dict : dict[str, list[float]]
-        The scale and translate parameters computed from the xarray dimension
-        indices.
-    """
-    array = getattr(dataset, array_name)
-    if array is None:
-        raise ValueError(f'{dataset} has no array with name {array_name}')
-    dims = [getattr(dataset, dim) for dim in array.dims]
-    translate = [float(d[0]) for d in dims]
-    scale = [float(d[1] - d[0]) for d in dims]
-    return {'scale': scale, 'translate': translate, 'units':('ns', 'degrees', 'degrees')}
-
-
 # Show the raw (not resampled) model data
 viewer, sst_layer = napari.imshow(
         sst.sst,
         name='sea surface temp',
-        **get_scale_translate(sst, 'sst'),
+        units=('ns', 'degrees', 'degrees'),
         colormap='magma',
         )
-viewer.scale_bar.visible = True
-viewer.dims.axis_labels = sst.sst.dims
 
 air_layer = viewer.add_image(
         airtemp.air,
         name='air temp NA',
-        **get_scale_translate(airtemp, 'air'),
+        units=('ns', 'degrees', 'degrees'),
         colormap='viridis',
         blending='additive',
         contrast_limits=(-23 + 273, 32 + 273),  # data are in degrees Kelvin
         )
 
-viewer.layers.units = ('hour', 'degrees', 'degrees')
+
 # set a time that overlaps both datasets
-viewer.dims.set_point(0, np.datetime64('2013-03-10T18:00:00.000000000'))
+viewer.layers.units = ('hour', 'degrees', 'degrees')
+viewer.dims.set_point(0, 383322)  # in hours
 
 # latitude goes from -90 (south, down) to 90 (north, up),
 # so we make sure that the camera vertical axis points up.
 viewer.camera.orientation2d = ('up', 'right')
+viewer.scale_bar.visible = True
 
 # fill the frame
-viewer.reset_view(margin=0)
+viewer.fit_to_view()
 
 
 if __name__ == '__main__':
