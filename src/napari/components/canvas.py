ac18bbc99f70e6fae936628b57c40a9570e432f2
Lorenzo Gaifas <brisvag@gmail.com>
2026-08-05 15:51:26 +0200

    revert fancy deprecation, it was never released

--- src/napari/components/canvas.py
+++ src/napari/components/canvas.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import warnings
 from enum import StrEnum
 from typing import TYPE_CHECKING, Any
 
@@ -52,17 +51,6 @@ class OverlayTiling(EventedModel):
     bottom_right: Orientation = Orientation.VERTICAL
 
 
-class _DeprecatingEventedDictNamespace(EventedDictNamespace):
-    @property
-    def floating_axes(self) -> CanvasAxesOverlay:
-        warnings.warn(
-            'viewer.canvas.overlays.floating_axes is deprecated since 0.9.0 and will be removed in 0.10.0. Use viewer.canvas.overlays.axes instead.',
-            FutureWarning,
-            stacklevel=2,
-        )
-        return self.axes
-
-
 class Canvas(EventedModel):
     """
     Canvas evented model.
@@ -86,8 +74,8 @@ class Canvas(EventedModel):
 
     background_color_override: ColorValue | None = None
     grid: GridCanvas = Field(default_factory=GridCanvas, frozen=True)
-    overlays: _DeprecatingEventedDictNamespace[CanvasOverlay] = Field(
-        default_factory=lambda: _DeprecatingEventedDictNamespace(
+    overlays: EventedDictNamespace[CanvasOverlay] = Field(
+        default_factory=lambda: EventedDictNamespace(
             {
                 'scale_bar': ScaleBarOverlay(),
                 'text': TextOverlay(),
