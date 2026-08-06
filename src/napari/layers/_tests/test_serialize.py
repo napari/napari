4e872112ae895ecd59d7b876a12e9ed66ced3d75
Lorenzo Gaifas <brisvag@gmail.com>
2026-08-06 13:00:56 +0200

    oops

--- src/napari/layers/_tests/test_serialize.py
+++ src/napari/layers/_tests/test_serialize.py
@@ -28,9 +28,6 @@ def test_attrs_arrays(Layer, data, ndim):
         # excluding `cache` which is not yet in `_get_state`
         if param == 'cache':
             continue
-        # exclude deprecated stuff:
-        if param == 'n_dimensional':
-            breakpoint()
         assert param in properties
 
     # Check new layer can be created
