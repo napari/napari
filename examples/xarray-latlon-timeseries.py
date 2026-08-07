79faceb221efd950f1b30d972a5bfb655e236c84
Tim Monko <timmonko@gmail.com>
2026-08-07 18:20:53 -0500

    cleaned up example for current implementation

--- examples/xarray-latlon-timeseries.py
+++ examples/xarray-latlon-timeseries.py
@@ -14,39 +14,43 @@ before displaying it in napari.
 
 .. tags:: visualization-advanced, layers, xarray
 """
+import pint
 import xarray as xr
 
 import napari
+
+# The tutorial datasets use CF-compliant unit strings ('degrees_north',
+# 'degrees_east') that pint does not know on its own. Register them with
+# pint's application registry so napari can recognise the lat/lon units.
+ureg = pint.get_application_registry()
+ureg.define('degrees_north = degree')
+ureg.define('degrees_east = degree')
 
 # open the xarray global sea surface temperature (40MB) and North America
 # air temperature (30MB) datasets
 sst = xr.tutorial.open_dataset('ersstv5')
 airtemp = xr.tutorial.open_dataset('air_temperature')
 
-
 # Show the raw (not resampled) model data
 viewer, sst_layer = napari.imshow(
-        sst.sst,
-        name='sea surface temp',
-        units=('ns', 'degrees', 'degrees'),
-        colormap='magma',
-        axis_labels=sst.sst.dims,
-        )
+    sst.sst,
+    name='sea surface temp',
+    colormap='magma',
+)
 
 air_layer = viewer.add_image(
-        airtemp.air,
-        name='air temp NA',
-        units=('ns', 'degrees', 'degrees'),
-        colormap='viridis',
-        blending='additive',
-        contrast_limits=(-23 + 273, 32 + 273),  # data are in degrees Kelvin
-        axis_labels=sst.sst.dims
-        )
+    airtemp.air,
+    name='air temp NA',
+    colormap='viridis',
+    blending='additive',
+    contrast_limits=(-23 + 273, 32 + 273),  # data are in degrees Kelvin
+)
 
 
-# set a time that overlaps both datasets
-viewer.layers.units = ('hour', 'degrees', 'degrees')
-viewer.dims.set_point(0, 383322)  # in hours
+# set a time point that overlaps both datasets. The time axis inherits a
+# real time unit (hours/days since 1970-01-01); napari reconciles the two
+# layers' different units, so we navigate it in hours.
+viewer.dims.set_point(0, 383322)  # hours since 1970-01-01 ≈ 2013-09-23
 
 # latitude goes from -90 (south, down) to 90 (north, up),
 # so we make sure that the camera vertical axis points up.
