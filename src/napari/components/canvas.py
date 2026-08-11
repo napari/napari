4cab72e67077c4a5b65ddf21a6d839ecde0c310f
Lorenzo Gaifas <brisvag@gmail.com>
2026-08-11 10:50:53 +0200

    Refactor floating_axes/axes into canvas_axes/scene_axes (#9363)
    # References and relevant issues
    - [zulip
    conversion](https://napari.zulipchat.com/#narrow/channel/619705-2026-naPLari-Krakow-public/topic/viewer.2Ecanvas.2Eaxes/with/614542452)
    - Depends on #9323
    - relative diff:
    https://github.com/brisvag/napari/compare/feature/scene-model...refactor/canvas-axes
    
    # Description
    The line/file count is high, but the changes are pretty small
    conceptually:
    
    - renamed `Axes` to `SceneAxes`
    - renamed `FloatingAxes` to `CanvasAxes`
    - updated action id paths to better reflect the new patterns (they were
    very outdated and linked to the original menu path). This affects how
    they appear on the command palette. Some examples:
    - `napari > window > view > Floating Axes Visible` becomes `napari >
    canvas > Toggle Canvas Axes`
    - `napari > window > view > Axes Labels` becomes `napari > scene >
    Toggle Scene Axes Labels`
    - `napari > window > view > Toggle 2D/3D Camera` becomes `napari > scene
    > Toggle 2D/3D Camera`
    - `napari > viewer > camera > Zoom Out` becomes `napari > scene > Zoom
    Out`
    - updated deprecation errors on the viewer to be more explicit about the
    current lack of a deadline (asked by @czaki in person)
    
    I have one doubt about this whole thing: does the `toggle layer
    tooltips` action even work? What is it supposed to do?
    
    PS: note that with #9323 the whole `scene` vs `canvas` split will be
    even clearer.
    
    PPS: this is unfortunately breaking public api that was just introduced
    in 0.8.0 (`viewer.floating_axes`), so I had to deprecate that too.

--- src/napari/components/canvas.py
+++ src/napari/components/canvas.py
@@ -9,9 +9,9 @@ from pydantic import Field
 from napari.components.grid import GridCanvas
 from napari.components.overlays import (
     BrushCircleOverlay,
+    CanvasAxesOverlay,
     CanvasOverlay,
     CurrentSliceOverlay,
-    FloatingAxesOverlay,
     ScaleBarOverlay,
     TextOverlay,
     ZoomOverlay,
@@ -68,7 +68,7 @@ class Canvas(EventedModel):
         A model that controls the enabling and settings of the grid mode.
     overlays : EventedDictNamespace
         A dictionary/namespace containing canvas overlays. By default, it exposes
-        publicly 'scale_bar', 'text', 'current_slice' and 'floating_axes'
+        publicly 'scale_bar', 'text', 'current_slice' and 'axes'
     overlay_tiling : OverlayTiling
         Controls for the overlay tiling direction and padding.
     size : tuple[int, int]
@@ -85,7 +85,7 @@ class Canvas(EventedModel):
                 '_brush_circle': BrushCircleOverlay(),
                 '_zoom_box': ZoomOverlay(),
                 'current_slice': CurrentSliceOverlay(),
-                'floating_axes': FloatingAxesOverlay(),
+                'axes': CanvasAxesOverlay(),
             }
         )
     )
