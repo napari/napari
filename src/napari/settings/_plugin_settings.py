f74b3a5cd52e1d433f9ee2472eec909f27aeb5d5
Tim Monko <timmonko@gmail.com>
2026-08-18 12:28:41 -0500

    sync with #9308

--- src/napari/settings/_plugin_settings.py
+++ src/napari/settings/_plugin_settings.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
+from pathlib import Path  # noqa: TC003
+
+from pydantic import Field
 from pydantic_settings import SettingsConfigDict
 
 from napari.settings._base import (
     EventedConfigFileSettings,
+    _NotSetType,
     _remove_empty_dicts,
 )
 
@@ -18,6 +22,7 @@ class PluginSettings(EventedConfigFileSettings):
         extra='ignore',
         populate_by_name=True,
     )
+    config_path: Path | _NotSetType | None = Field(None, exclude=True)
 
     def __str__(self) -> str:
         out = 'PluginSettings (defaults excluded)\n' + 34 * '-' + '\n'
