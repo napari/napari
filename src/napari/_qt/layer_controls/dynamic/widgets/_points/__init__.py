215d0fd003428c24e33efc82fe84979ce32e363f
Lorenzo Gaifas <brisvag@gmail.com>
2026-08-26 21:13:28 +0200

    remove files

--- src/napari/_qt/layer_controls/dynamic/widgets/_points/__init__.py
+++ src/napari/_qt/layer_controls/dynamic/widgets/_points/__init__.py
@@ -1,6 +1,3 @@
-from napari._qt.layer_controls.dynamic.widgets._points.qt_border_color import (
-    QtBorderColorControl,
-)
 from napari._qt.layer_controls.dynamic.widgets._points.qt_current_size_slider import (
     QtCurrentSizeSliderControl,
 )
@@ -9,7 +6,6 @@ from napari._qt.layer_controls.dynamic.widgets._points.qt_symbol_combobox import
 )
 
 __all__ = [
-    'QtBorderColorControl',
     'QtCurrentSizeSliderControl',
     'QtSymbolComboBoxControl',
 ]
