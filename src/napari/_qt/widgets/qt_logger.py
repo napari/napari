a6ae7d2631de30a324687a4da135854b6ddc385b
Lorenzo Gaifas <brisvag@gmail.com>
2026-08-27 17:45:34 +0200

    fix logger and expose functions

--- src/napari/_qt/widgets/qt_logger.py
+++ src/napari/_qt/widgets/qt_logger.py
@@ -13,7 +13,7 @@ from qtpy.QtWidgets import (
 )
 
 from napari._qt.dialogs.qt_about import QtCopyToClipboardButton
-from napari.utils._logging import LOG_STREAM, get_log_level_value
+from napari.utils.logging import _LOG_STREAM, _get_log_level_value
 
 
 class LogWidget(QWidget):
@@ -87,7 +87,7 @@ class LogWidget(QWidget):
         self.text_filter.textChanged.connect(self._on_change)
 
         # TODO: super laggy when open :/
-        LOG_STREAM.changed.connect(self._on_new_message)
+        _LOG_STREAM.changed.connect(self._on_new_message)
         self._prev_pos = None
         self.log_text_box.verticalScrollBar().rangeChanged.connect(
             self._jump_to_pos
@@ -98,23 +98,24 @@ class LogWidget(QWidget):
         self._jump_to_pos()
 
     def _on_loglevel_change(self, event=None):
-        level = get_log_level_value(event)
+        level = _get_log_level_value(event)
         logging.getLogger().setLevel(level)
 
     def _on_new_message(self, event=None):
         self._prev_pos = self._scroll_pos()
 
-        log = LOG_STREAM.get_filtered_logs_html(
+        logs = _LOG_STREAM.get_filtered_logs_html(
             self.level_filter.currentText(),
             self.text_filter.text(),
             last_only=True,
-        )[0]
-        self.log_text_box.append(log)
+        )
+        if logs:
+            self.log_text_box.append(logs[0])
 
     def _on_change(self, event=None):
         self._prev_pos = self._scroll_pos()
 
-        logs = LOG_STREAM.get_filtered_logs_html(
+        logs = _LOG_STREAM.get_filtered_logs_html(
             self.level_filter.currentText(), self.text_filter.text()
         )
         self.log_text_box.clear()
--- src/napari/utils/logging.py
+++ src/napari/utils/logging.py
@@ -15,7 +15,7 @@ if TYPE_CHECKING:
     from typing import Any
 
 
-def get_log_level_value(log_level_name: str | None) -> int:
+def _get_log_level_value(log_level_name: str | None) -> int:
     if log_level_name is None:
         return logging.NOTSET
     return logging.getLevelNamesMapping().get(log_level_name, logging.NOTSET)
@@ -35,7 +35,7 @@ class _LogStream:
 
     def write(self, log_msg: str) -> None:
         logger, level_name, time, thread, msg = log_msg.split(_LOG_SEPARATOR)
-        level_value = get_log_level_value(level_name)
+        level_value = _get_log_level_value(level_name)
         self.logs.append((logger, level_value, level_name, time, thread, msg))
         # TODO: actually save log to a file somewhere so it can be retrieved?
         self.changed()
@@ -50,9 +50,9 @@ class _LogStream:
         last_only: bool = False,
     ) -> list[str]:
         if isinstance(level, str):
-            level = get_log_level_value(level)
+            level = _get_log_level_value(level)
 
-        logs = [LOG_STREAM.logs[-1]] if last_only else LOG_STREAM.logs
+        logs = [_LOG_STREAM.logs[-1]] if last_only else _LOG_STREAM.logs
 
         selected = [
             (logger_name, level_value, *others)
@@ -77,30 +77,14 @@ class _LogStream:
         ]
 
 
-LOG_STREAM = _LogStream()
-LOG_HANDLER = logging.StreamHandler(LOG_STREAM)
-LOG_HANDLER.setFormatter(
+_LOG_STREAM = _LogStream()
+_LOG_HANDLER = logging.StreamHandler(_LOG_STREAM)
+_LOG_HANDLER.setFormatter(
     logging.Formatter(
         f'%(name)s{_LOG_SEPARATOR}%(levelname)s{_LOG_SEPARATOR}%(asctime)s{_LOG_SEPARATOR}%(threadName)s{_LOG_SEPARATOR}%(message)s'
     )
 )
-LOG_HANDLER.setLevel(logging.DEBUG)
-
-
-@contextmanager
-def register_logger_to_napari_handler(
-    module: str,
-) -> Generator[None, None, None]:
-    """
-    Register a specific module's logger to use our custom log handler.
-    """
-    logger = logging.getLogger(module)
-    # ensure the default "last resort" logging to console remains
-    if not logger.handlers and logging.lastResort:
-        logger.addHandler(logging.lastResort)
-    logger.addHandler(LOG_HANDLER)
-    yield
-    logger.removeHandler(LOG_HANDLER)
+_LOG_HANDLER.setLevel(logging.DEBUG)
 
 
 def _html_tag_for_level(level_name: str, level_value: int) -> str:
@@ -120,3 +104,35 @@ def _html_tag_for_level(level_name: str, level_value: int) -> str:
     # this is ugly AF but html is weird and I don't get it
     padding = '&nbsp;' * (8 - len(level_name))
     return f'<font style="color:{color}">{level_name}{padding}</font>'
+
+
+def register_logger_to_napari_handler(module: str) -> None:
+    """
+    Register a specific module's logger to use our custom log handler.
+    """
+    logger = logging.getLogger(module)
+    # ensure the default "last resort" logging to console remains
+    if not logger.handlers and logging.lastResort:
+        logger.addHandler(logging.lastResort)
+    logger.addHandler(_LOG_HANDLER)
+
+
+def deregister_logger_from_napari_handler(module: str) -> None:
+    """
+    Deregister a specific module's logger from using our custom log handler.
+    """
+    logger = logging.getLogger(module)
+    # fails silently
+    logger.removeHandler(_LOG_HANDLER)
+
+
+@contextmanager
+def using_napari_log_handler(
+    module: str,
+) -> Generator[None, None, None]:
+    """
+    Context manager to temporarily register a module's logger to our custom handler.
+    """
+    register_logger_to_napari_handler(module)
+    yield
+    deregister_logger_from_napari_handler(module)
