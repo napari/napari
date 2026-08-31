c09c30a60b46d03d35c2769ac4108939ec84eb16
Lorenzo Gaifas <brisvag@gmail.com>
2026-08-31 18:21:09 +0200

    import directly

--- src/napari/utils/_tests/test_logging.py
+++ src/napari/utils/_tests/test_logging.py
@@ -1,13 +1,15 @@
 import logging
 
-from napari.utils.logging import _LOG_STREAM, using_napari_log_handler
+from napari.utils import logging as napari_logging
 
 
 def test_log_stream():
-    with using_napari_log_handler(''):
+    with napari_logging.using_napari_log_handler(''):
         logger = logging.getLogger('test_logger')
         logger.setLevel('DEBUG')
         log_msg = 'NAPARI TEST LOG MESSAGE'
         logger.debug(log_msg)
 
-    assert log_msg in ''.join(_LOG_STREAM.get_filtered_logs_html())
+    assert log_msg in ''.join(
+        napari_logging._LOG_STREAM.get_filtered_logs_html()
+    )
