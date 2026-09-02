d567d907890189082f956531dfbde261bc00e392
Lorenzo Gaifas <brisvag@gmail.com>
2026-09-02 12:48:34 +0200

    add failing test

--- src/napari/utils/events/_tests/test_event_utils.py
+++ src/napari/utils/events/_tests/test_event_utils.py
@@ -1,6 +1,8 @@
 import gc
+from dataclasses import dataclass
 from unittest.mock import Mock
 
+from napari.utils.events import EventedModel, disconnect_events
 from napari.utils.events.event import Event, EventEmitter
 from napari.utils.events.event_utils import (
     connect_no_arg,
@@ -11,42 +13,104 @@ from napari.utils.events.event_utils import (
 
 def test_connect_no_arg():
     mock = Mock(['meth'])
-    emiter = EventEmitter()
-    connect_no_arg(emiter, mock, 'meth')
-    emiter(type_name='a', value=1)
+    emitter = EventEmitter()
+    connect_no_arg(emitter, mock, 'meth')
+    emitter(type_name='a', value=1)
     mock.meth.assert_called_once_with()
-    assert len(emiter.callbacks) == 1
+    assert len(emitter.callbacks) == 1
     del mock
     gc.collect()
-    assert len(emiter.callbacks) == 1
-    emiter(type_name='a', value=1)
-    assert len(emiter.callbacks) == 0
+    assert len(emitter.callbacks) == 1
+    emitter(type_name='a', value=1)
+    assert len(emitter.callbacks) == 0
 
 
 def test_connect_setattr_value():
     mock = Mock()
-    emiter = EventEmitter()
-    connect_setattr_value(emiter, mock, 'meth')
-    emiter(type_name='a', value=1)
+    emitter = EventEmitter()
+    connect_setattr_value(emitter, mock, 'meth')
+    emitter(type_name='a', value=1)
     assert mock.meth == 1
-    assert len(emiter.callbacks) == 1
+    assert len(emitter.callbacks) == 1
     del mock
     gc.collect()
-    assert len(emiter.callbacks) == 1
-    emiter(type_name='a', value=1)
-    assert len(emiter.callbacks) == 0
+    assert len(emitter.callbacks) == 1
+    emitter(type_name='a', value=1)
+    assert len(emitter.callbacks) == 0
 
 
 def test_connect_setattr():
     mock = Mock()
-    emiter = EventEmitter()
-    connect_setattr(emiter, mock, 'meth')
-    emiter(type_name='a', value=1)
+    emitter = EventEmitter()
+    connect_setattr(emitter, mock, 'meth')
+    emitter(type_name='a', value=1)
     assert isinstance(mock.meth, Event)
     assert mock.meth.value == 1
-    assert len(emiter.callbacks) == 1
+    assert len(emitter.callbacks) == 1
     del mock
     gc.collect()
-    assert len(emiter.callbacks) == 1
-    emiter(type_name='a', value=1)
-    assert len(emiter.callbacks) == 0
+    assert len(emitter.callbacks) == 1
+    emitter(type_name='a', value=1)
+    assert len(emitter.callbacks) == 0
+
+
+@dataclass
+class MyMock:
+    # mock does not work here cause it creates methods on the fly and
+    # they won't be detected by our machinery
+    all_calls = 0
+    x_calls = 0
+    s_calls = 0
+    s_all_calls = 0
+    y_calls = 0
+
+    def all(self):
+        self.all_calls += 1
+
+    def x(self):
+        self.x_calls += 1
+
+    def s(self):
+        self.s_calls += 1
+
+    def s_all(self):
+        self.s_all_calls += 1
+
+    def y(self):
+        self.y_calls += 1
+
+
+def test_disconnect_events_works():
+    mock = MyMock()
+
+    class SubModel(EventedModel):
+        y: int
+
+    class MyModel(EventedModel):
+        x: int
+        s: SubModel
+
+    model = MyModel(x=0, s={'y': 0})
+    model.events.connect(mock.all)
+    model.events.x.connect(mock.x)
+    model.events.s.connect(mock.s)
+    model.s.events.connect(mock.s_all)
+    model.s.events.y.connect(mock.y)
+
+    model.x = 1
+    model.s.y = 1
+    assert mock.all_calls == 1
+    assert mock.x_calls == 1
+    assert mock.s_calls == 0
+    assert mock.s_all_calls == 1
+    assert mock.y_calls == 1
+
+    disconnect_events(model.events, mock)
+
+    model.x = 2
+    model.s.y = 2
+    assert mock.all_calls == 1
+    assert mock.x_calls == 1
+    assert mock.s_calls == 0
+    assert mock.s_all_calls == 1
+    assert mock.y_calls == 1
