3562ea230a1a8a55518515c00880e17fa0c76107
Lorenzo Gaifas <brisvag@gmail.com>
2026-09-02 14:53:33 +0200

    more thorough test

--- src/napari/utils/events/_tests/test_event_utils.py
+++ src/napari/utils/events/_tests/test_event_utils.py
@@ -2,7 +2,9 @@ import gc
 from dataclasses import dataclass
 from unittest.mock import Mock
 
-from napari.utils.events import EventedModel, disconnect_events
+from pydantic import Field
+
+from napari.utils.events import EventedList, EventedModel, disconnect_events
 from napari.utils.events.event import Event, EventEmitter
 from napari.utils.events.event_utils import (
     _disconnect_all_events,
@@ -55,6 +57,20 @@ def test_connect_setattr():
     assert len(emitter.callbacks) == 0
 
 
+class SubModel(EventedModel):
+    y: int = 0
+
+
+class MyModel(EventedModel):
+    x: int = 0
+    sub: SubModel = Field(default_factory=SubModel)
+    ls: EventedList = Field(default_factory=lambda: EventedList([SubModel()]))
+
+    @property
+    def p(self):
+        return self.x + 1
+
+
 @dataclass
 class MyMock:
     # mock does not work here cause it creates methods on the fly and
@@ -62,9 +78,11 @@ class MyMock:
     all_calls = 0
     x_calls = 0
     p_calls = 0
-    s_calls = 0
-    s_all_calls = 0
+    sub_calls = 0
+    sub_all_calls = 0
     y_calls = 0
+    ls_all_calls = 0
+    ls_sub_calls = 0
 
     def all(self):
         self.all_calls += 1
@@ -75,99 +93,118 @@ class MyMock:
     def p(self):
         self.p_calls += 1
 
-    def s(self):
-        self.s_calls += 1
+    def sub(self):
+        self.sub_calls += 1
 
-    def s_all(self):
-        self.s_all_calls += 1
+    def sub_all(self):
+        self.sub_all_calls += 1
 
     def y(self):
         self.y_calls += 1
 
+    def ls_all(self):
+        self.ls_all_calls += 1
 
-class SubModel(EventedModel):
-    y: int
-
-
-class MyModel(EventedModel):
-    x: int
-    s: SubModel
-
-    @property
-    def p(self):
-        return self.x + 1
+    def ls_sub(self):
+        self.ls_sub_calls += 1
 
 
 def test_disconnect_events_works():
     mock = MyMock()
-    model = MyModel(x=0, s={'y': 0})
+    model = MyModel()
     model.events.connect(mock.all)
     model.events.x.connect(mock.x)
-    model.events.s.connect(mock.s)
     model.events.p.connect(mock.p)
-    model.s.events.connect(mock.s_all)
-    model.s.events.y.connect(mock.y)
+    model.events.s.connect(mock.sub)
+    model.sub.events.connect(mock.sub_all)
+    model.sub.events.y.connect(mock.y)
+    model.ls.events.connect(mock.ls_all)
+    model.ls[0].events.connect(mock.ls_sub)
+
+    # check top-level events
 
     model.x = 1
-    model.s.y = 1
     assert mock.all_calls == 1
     assert mock.x_calls == 1
     assert mock.p_calls == 1
-    assert mock.s_calls == 0
-    assert mock.s_all_calls == 1
-    assert mock.y_calls == 1
 
     # only events from one emittergroup are disconnected
     disconnect_events(model.events, mock)
 
     model.x = 2
-    model.s.y = 2
     assert mock.all_calls == 1
     assert mock.x_calls == 1
     assert mock.p_calls == 1
-    assert mock.s_calls == 0
-    assert mock.s_all_calls == 2
-    assert mock.y_calls == 2
 
-    disconnect_events(model.s.events, mock)
+    # now do the same but for a child event
+    model.sub.y = 1
+    assert mock.sub_all_calls == 1
+    assert mock.y_calls == 1
 
-    model.x = 3
-    model.s.y = 3
+    disconnect_events(model.sub.events, mock)
+
+    model.sub.y = 2
+    assert mock.sub_all_calls == 1
+    assert mock.y_calls == 1
+
+    # now with event from a container
+    model.ls[0].y = 1
+    assert mock.ls_all_calls == 1
+    assert mock.ls_sub_calls == 1
+
+    disconnect_events(model.ls.events, mock)
+    disconnect_events(model.ls[0].events, mock)
+
+    model.ls[0].y = 2
+    assert mock.ls_all_calls == 1
+    assert mock.ls_sub_calls == 1
+
+    # check everything is the same, no spilling across events
     assert mock.all_calls == 1
     assert mock.x_calls == 1
     assert mock.p_calls == 1
-    assert mock.s_calls == 0
-    assert mock.s_all_calls == 2
-    assert mock.y_calls == 2
+    assert mock.sub_calls == 0
+    assert mock.sub_all_calls == 1
+    assert mock.y_calls == 1
+    assert mock.ls_all_calls == 1
+    assert mock.ls_sub_calls == 1
 
 
 def test_disconnect_all_events_works():
     mock = MyMock()
-    model = MyModel(x=0, s={'y': 0})
+    model = MyModel()
     model.events.connect(mock.all)
     model.events.x.connect(mock.x)
-    model.events.s.connect(mock.s)
     model.events.p.connect(mock.p)
-    model.s.events.connect(mock.s_all)
-    model.s.events.y.connect(mock.y)
+    model.events.s.connect(mock.sub)
+    model.sub.events.connect(mock.sub_all)
+    model.sub.events.y.connect(mock.y)
+    model.ls.events.connect(mock.ls_all)
+    model.ls[0].events.connect(mock.ls_sub)
 
     model.x = 1
-    model.s.y = 1
+    model.sub.y = 1
+    model.ls[0].y = 1
     assert mock.all_calls == 1
     assert mock.x_calls == 1
     assert mock.p_calls == 1
-    assert mock.s_calls == 0
-    assert mock.s_all_calls == 1
+    assert mock.sub_calls == 0
+    assert mock.sub_all_calls == 1
     assert mock.y_calls == 1
+    assert mock.ls_all_calls == 1
+    assert mock.ls_sub_calls == 1
 
     _disconnect_all_events(model, mock)
 
     # everyting should be disconnected
     model.x = 2
-    model.s.y = 2
+    model.sub.y = 2
+    model.ls[0].y = 2
     assert mock.all_calls == 1
     assert mock.x_calls == 1
     assert mock.p_calls == 1
-    assert mock.s_calls == 0
-    assert mock.s_all_calls == 1
+    assert mock.sub_calls == 0
+    assert mock.sub_all_calls == 1
     assert mock.y_calls == 1
+    assert mock.ls_all_calls == 1
+    assert mock.ls_sub_calls == 1
