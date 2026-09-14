ba46cab291e3ee3c42898701fc22a6e867415dd7
Carlos Rodriguez <carlosmariorr90@hotmail.com>
2026-09-14 20:33:50 +0900

    added tooltip for locked data level extraction to resolution combobox.

--- src/napari/_qt/layer_controls/dynamic/widgets/qt_multiscale_level_control.py
+++ src/napari/_qt/layer_controls/dynamic/widgets/qt_multiscale_level_control.py
@@ -11,6 +11,7 @@ from napari._qt.layer_controls.dynamic.widgets.qt_widget_controls_base import (
 )
 from napari._qt.utils import qt_signals_blocked
 from napari.utils.misc import human_readable_size
+from napari.utils.tips import format_tip
 
 if TYPE_CHECKING:
     from napari.layers import Image, Labels
@@ -71,6 +72,15 @@ class QtMultiscaleLevelControl(QtWidgetControlsBase):
         super().__init__(layers, parent)
 
         self.level_combobox = QComboBox(parent)
+        self.level_combobox.setToolTip(
+            format_tip(
+                'You can extract a locked data level as a new layer '
+                'by opening the layer list context menu with right click '
+                "and selecting 'Extract locked multiscale level'."
+                '\nAlternatively use the command palette ( {napari.window.view.toggle_command_palette} )'
+                " and run 'Extract locked multiscale level'."
+            )
+        )
         self.level_label = QtWrappedLabel('resolution:')
 
         # Only set up and show widgets if layer is multiscale
