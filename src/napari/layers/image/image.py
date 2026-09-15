afb4c54dda44ac01079d37bb919df12468e57375
Lorenzo Gaifas <brisvag@gmail.com>
2026-09-15 17:21:29 +0200

    Update src/napari/layers/image/image.py
    Co-authored-by: Aniket <148300120+Aniketsy@users.noreply.github.com>

--- src/napari/layers/image/image.py
+++ src/napari/layers/image/image.py
@@ -543,7 +543,7 @@ class Image(IntensityVisualizationMixin, ScalarFieldBase):
             image = np.max(image, axis=0)
 
         # float16 not supported by ndi.zoom
-        dtype = np.dtype(image.dtype)  # pyrefly: ignore [no-matching-overload]
+        dtype = np.dtype(image.dtype)
         if dtype in [np.dtype(np.float16)]:
             image = image.astype(np.float32)
 
