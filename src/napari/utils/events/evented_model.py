0c672c3f25c103649f7d1c3de9fd3fabbb10c5fc
Grzegorz Bokota <bokota+github@gmail.com>
2026-09-17 15:06:50 +0200

    fix dependency cycle

--- src/napari/utils/events/evented_model.py
+++ src/napari/utils/events/evented_model.py
@@ -130,7 +130,7 @@ def _get_property_dependence_from_code(
         elif dep in cls.__properties__:
             res.update(
                 _get_property_dependence_from_code(
-                    cls, cls.__properties__[dep], res
+                    cls, cls.__properties__[dep], res | visited
                 )
             )
     return res
