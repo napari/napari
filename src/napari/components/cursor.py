2b6166d8c24bcabf40449145f73193392d614fcf
Lorenzo Gaifas <brisvag@gmail.com>
2026-09-17 18:15:13 +0200

    properties on cursor

--- src/napari/components/cursor.py
+++ src/napari/components/cursor.py
@@ -7,11 +7,19 @@ from napari.utils.events import EventedModel
 class Cursor(EventedModel):
     """Cursor object with position and properties of the cursor.
 
+    Canvas-related attributes are Read-Only and set internally by napari.
+
     Attributes
     ----------
     position : tuple of float
         Position of the cursor in world coordinates. If the cursor is outside of,
         the canvas, then the last known position is stored instead.
+    canvas_position : tuple of int or None
+        Position of the cursor in canvas pixel coordinates (y, x).
+        None when cursor is outside the canvas.
+    view_direction : Optional[np.ndarray]
+        The vector describing the direction of the camera in the scene
+        in world coordinates. This is None when viewing in 2D.
     viewbox : tuple[int, int] or None
         Position of the cursor in the grid.
     scaled : bool
@@ -30,16 +38,29 @@ class Cursor(EventedModel):
             * pointing: A finger for pointing
             * standard: The standard cursor
             # crosshair: A crosshair
-    _view_direction : Optional[np.ndarray]
-        The vector describing the direction of the camera in the scene
-        in world coordinates.
-        This is None when viewing in 2D.
     """
 
     # fields
     position: tuple[float, ...] = (1.0, 1.0)
-    viewbox: tuple[int, int] | None = None
     scaled: bool = True
     size: float = 1.0
     style: CursorStyle = CursorStyle.STANDARD
-    _view_direction: np.ndarray | None = None
+    _canvas_position: tuple[int, int] | None = None
+    _view_direction: np.ndarray[tuple[int], np.dtype[np.floating]] | None = (
+        None
+    )
+    _viewbox: tuple[int, int] | None = None
+
+    @property
+    def canvas_position(self) -> tuple[int, int] | None:
+        return self._canvas_position
+
+    @property
+    def view_direction(
+        self,
+    ) -> np.ndarray[tuple[int], np.dtype[np.floating]] | None:
+        return self._view_direction
+
+    @property
+    def viewbox(self) -> tuple[int, int] | None:
+        return self._viewbox
