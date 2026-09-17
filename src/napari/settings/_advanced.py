a34f574429fd074ae3b781022a1f2325553ec603
Lorenzo Gaifas <brisvag@gmail.com>
2026-09-17 21:46:58 +0200

    default on

--- src/napari/settings/_advanced.py
+++ src/napari/settings/_advanced.py
@@ -5,7 +5,7 @@ from napari.settings._base import EventedSettings
 
 class AdvancedSettings(EventedSettings):
     multisampling: bool = Field(
-        False,
+        True,
         title='Enable global multisampling.',
         description='Multisampling (antialiasing) improves quality by rendering at higher resolution to reduce aliasing, at the cost of some performance.',
         json_schema_extra={'requires_restart': True},
