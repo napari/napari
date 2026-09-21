ff9a844d17f42805d956b02b94f6d195def7467f
Grzegorz Bokota <bokota+github@gmail.com>
2026-09-21 18:53:42 +0200

    Revert type annotations changes

--- src/napari/utils/migrations.py
+++ src/napari/utils/migrations.py
@@ -416,7 +416,7 @@ def deprecated_class_name(
     _OldClass.__module__ = new_class.__module__
     _OldClass.__name__ = previous_name
     _OldClass.__qualname__ = previous_name
-    _OldClass.__new__.__signature__ = prealloc_signature  # type: ignore [attr-defined]
+    _OldClass.__new__.__signature__ = prealloc_signature  # pyrefly: ignore [missing-attribute]
 
     return _OldClass
 
@@ -449,7 +449,7 @@ class _DeprecatingDict(UserDict[str, Any]):
         key = self._maybe_rename_key(key)
         return self.data.__getitem__(key)
 
-    def __setitem__(self, key: str, value: Any) -> None:
+    def __setitem__(self, key: str, value: Any) -> None:  # pyrefly: ignore [bad-override-param-name]
         key = self._maybe_rename_key(key)
         return self.data.__setitem__(key, value)
 
