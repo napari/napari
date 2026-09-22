433d872b5b8c22276afa5393c933b5fe12403e74
Grzegorz Bokota <bokota+github@gmail.com>
2026-09-23 00:08:28 +0200

    fix comment

--- src/napari/utils/events/event.py
+++ src/napari/utils/events/event.py
@@ -192,7 +192,7 @@ class Event:
         """Shorter string representation"""
         return self.__class__.__name__
 
-    # mypy fix for dynamic attribute access
+    # pyrefly fix for dynamic attribute access
     def __getattr__(self, name: str) -> Any:
         return object.__getattribute__(self, name)
 
